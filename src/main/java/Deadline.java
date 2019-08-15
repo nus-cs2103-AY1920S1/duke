41daa5618efa3c4f20143d9afc64776ab0e642d3
zhixianggg <zhixiangg.o@gmail.com>
2019-08-15 17:52:55 +0800

    level-4 updated

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,29 @@
+public class Deadline extends Task {
+
+	private String datetime;
+
+	public Deadline(String taskName, String datetime) {
+		super(taskName);
+		this.datetime = datetime;
+	}
+
+	@Override
+	public String getType() {
+		return "D";
+	}
+
+	public String getDatetime() {
+		return datetime;
+	}
+
+	public void setDatetime(String datetime) {
+		this.datetime = datetime;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("[%s][%s] %s (by: %s)",
+				this.getType() ,isDone ? "\u2713": "\u2718",
+				this.taskName, this.datetime);
+	}
+}
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+
+    protected String taskName;
+    protected boolean isDone;
+
+    public Task(String name) {
+        this.isDone = false;
+        this.taskName = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s][%s] %s", this.getType() ,isDone ? "\u2713": "\u2718", taskName );
+    }
+
+    public void done() {
+        this.isDone = true;
+    }
+
+    public String getType() {
+    	return " ";
+    }
+
+
+
+}

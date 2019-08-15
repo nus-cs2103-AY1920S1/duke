864caa24b81119e34514457abe76096f123052ea
zhixianggg <zhixiangg.o@gmail.com>
2019-08-15 13:19:20 +0800

    level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,28 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
+
+        ArrayList<String> arr = new ArrayList<>();
 
         System.out.println("Hello I'm Duke! \nWhat can I do for you?");
         Scanner sc = new Scanner(System.in);
 
         String command = sc.nextLine();
         while (! command.toLowerCase().equals("bye")) {
-            System.out.println(command);
+
+            if (command.equals("list")) {
+                int index  = 1;
+                for (String item : arr) {
+                    System.out.println(String.format("%d. %s", index ,item));
+                    index++;
+                }
+            } else {
+                arr.add(command);
+                System.out.println(String.format("added: %s", command));
+            }
+
             command = sc.nextLine();
         }
 

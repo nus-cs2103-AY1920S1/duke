840b02ce72177cadc58d7521e29990cdbaf87a48
aidilfbk <e0303295@u.nus.edu>
2019-08-17 02:11:25 +0800

    Implemented Add and List tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+
 public class Duke {
     public static void main(String[] args) {
+        List<String> tasks = new ArrayList<>();
         UserInterface ui = new UserInterface();
 
         ui.printBlock("Hello! I'm Duke\n" +
                 "What can I do for you?");
         ui.println();
 
-        while (true) {
+        boolean isDone = false;
+        while (!isDone) {
             final String input = ui.nextLine();
 
-            if (input.equals("bye")) {
+            switch (input) {
+            case "bye":
+                isDone = true;
                 ui.printBlock("Bye. Hope to see you again soon!");
                 break;
-            } else {
-                ui.printBlock(input);
+            case "list":
+                StringJoiner taskListDisplay = new StringJoiner(System.lineSeparator());
+                int listIdx = 1;
+                for (String task : tasks) {
+                    String formattedTask = String.format("%d. %s", listIdx, task);
+                    taskListDisplay.add(formattedTask);
+                    listIdx++;
+                }
+                ui.printBlock(taskListDisplay.toString());
+                break;
+            default:
+                tasks.add(input);
+                ui.printBlock(String.format("added: %s", input));
                 ui.println();
             }
         }

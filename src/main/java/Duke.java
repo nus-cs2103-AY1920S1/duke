7ce583cba45a370491047bba57ee5c862b6e16f7
briyanii <e0310729@u.nus.edu>
2019-08-16 21:50:33 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
 	public static void main(String[] args) {
@@ -9,26 +11,53 @@ public class Duke {
 			+ "| |_| | |_| |   <  __/\n"
 			+ "|____/ \\__,_|_|\\_\\___|\n";
 		String lineBreak = "    ___________________________________________________________\n";
-		String exitCommand = "bye";
-		System.out.println(
-				logo 
-				+ lineBreak
-				+ "     Hello! I'm Duke\n"
-				+ "     What can I do for you?\n"
-				+ lineBreak);
-		String command = scanner.next();
-		while (!command.equals(exitCommand)) {
-			System.out.println(
-					lineBreak
-					+ "     "
-					+ command
-					+ "\n"
-					+ lineBreak);
-			command = scanner.next();
+		final String exitCommand = "bye";
+		String greetingMessage = logo 
+			+ lineBreak
+			+ "     Hello! I'm Duke\n"
+			+ "     What can I do for you?\n"
+			+ lineBreak;
+		String exitMessage = lineBreak
+			+ "     Bye. Hope to see you again soon!\n"
+			+ lineBreak;
+		boolean takingInput = true;
+		List<String> taskList = new ArrayList<String>();
+
+		// print greeting message
+		System.out.println(greetingMessage);
+
+		// store console input as string
+		String command = scanner.nextLine();
+		String output = "";
+
+		// if input is not the exit command ("bye") process the input
+		while (takingInput) {
+			switch(command) {
+				case exitCommand:
+					System.out.println(exitMessage);
+					takingInput = false;
+					break;
+				case "list":
+					int count = 0;
+					System.out.print(lineBreak);
+					for (String task : taskList) {
+						count++;
+						System.out.println("     " + count + ". " + task);
+					}
+					System.out.println(lineBreak);
+					break;
+				default:
+					System.out.println(lineBreak
+							+ "     added: "
+							+ command
+							+ "\n"
+							+ lineBreak);
+					taskList.add(command);
+					break;
+			}
+			if (takingInput) {
+				command = scanner.nextLine();
+			}
 		}
-		System.out.println(
-				lineBreak
-				+ "     Bye. Hope to see you again soon!\n"
-				+ lineBreak);
 	}
 }

55d0ca73ef9a2efd0a844ef2dc2e8a143a3ef3d4
briyanii <e0310729@u.nus.edu>
2019-08-16 23:33:42 +0800

    Mark as Done, and Classes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;
-import java.util.List;
-import java.util.ArrayList;
 
 public class Duke {
 	public static void main(String[] args) {
@@ -11,7 +9,6 @@ public class Duke {
 			+ "| |_| | |_| |   <  __/\n"
 			+ "|____/ \\__,_|_|\\_\\___|\n";
 		String lineBreak = "    ___________________________________________________________\n";
-		final String exitCommand = "bye";
 		String greetingMessage = logo 
 			+ lineBreak
 			+ "     Hello! I'm Duke\n"
@@ -21,42 +18,48 @@ public class Duke {
 			+ "     Bye. Hope to see you again soon!\n"
 			+ lineBreak;
 		boolean takingInput = true;
-		List<String> taskList = new ArrayList<String>();
 
 		// print greeting message
 		System.out.println(greetingMessage);
 
 		// store console input as string
-		String command = scanner.nextLine();
-		String output = "";
+		String input = scanner.nextLine();
 
 		// if input is not the exit command ("bye") process the input
 		while (takingInput) {
-			switch(command) {
-				case exitCommand:
+			String[] inputSplit = input.split(" ", 2);
+
+			switch(inputSplit[0]) {
+				case "bye":
 					System.out.println(exitMessage);
 					takingInput = false;
 					break;
 				case "list":
-					int count = 0;
 					System.out.print(lineBreak);
-					for (String task : taskList) {
-						count++;
-						System.out.println("     " + count + ". " + task);
+					for (Task task : Task.taskList) {
+						System.out.println("     " + task);
 					}
 					System.out.println(lineBreak);
+					break;
+				case "done":
+					Task currentTask = Task.taskList.get(Integer.parseInt(inputSplit[1]) - 1);
+					currentTask.complete();
+					System.out.println(lineBreak
+							+ "     Nice! I've marked this task as done:\n"
+							+ "       " + currentTask.toString().split("[.]", 2)[1] + "\n"
+							+ lineBreak);
 					break;
 				default:
 					System.out.println(lineBreak
 							+ "     added: "
-							+ command
+							+ input
 							+ "\n"
 							+ lineBreak);
-					taskList.add(command);
+					Task.createTask(input);
 					break;
 			}
 			if (takingInput) {
-				command = scanner.nextLine();
+				input = scanner.nextLine();
 			}
 		}
 	}

49cb0e468aa67b2e7a5e6991ad6593b5134b3be3
Nanosync <10258208+Nanosync@users.noreply.github.com>
2019-08-17 02:01:12 +0800

    Bug fix for null task, added getters/setters

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,8 +7,20 @@ public class Task {
         this.isDone = false;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718"); // return tick or X symbols
+    }
+
+    public void markAsUnfinished() {
+        isDone = false;
     }
 
     public void markAsDone() {
--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -25,10 +25,10 @@ public class TaskManager {
 
         if (task != null) {
             task.markAsDone();
-        }
 
-        System.out.println(" Nice! I've marked this task as done: ");
-        System.out.println(" " + task);
+            System.out.println(" Nice! I've marked this task as done: ");
+            System.out.println(" " + task);
+        }
     }
 
     public void printTasks() {

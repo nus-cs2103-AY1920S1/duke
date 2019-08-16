3305dfa674f4abd76f0c3a9d8807c614d17053cc
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2019-08-16 17:02:43 +0800

    Increment Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -30,6 +30,18 @@ public class Duke {
                 sendLine();
                 sendTasks();
                 sendLine();
+            } else if (input.startsWith("done")) {
+                String[] parsedData = input.split(" ");
+                if (parsedData.length < 2) {
+                    sendLine();
+                    sendMessage("Error: Please key in the index of the task!");
+                    sendLine();
+                } else {
+                    String todoIndex = parsedData[1];
+                    sendLine();
+                    completeTask(todoIndex);
+                    sendLine();
+                }
             } else {
                 sendLine();
                 addTask(input);
@@ -53,10 +65,27 @@ public class Duke {
         }
     }
 
+    public static void completeTask(String todoIndex) {
+        int index = Integer.parseInt(todoIndex) - 1;
+        if (index < 0 || index >= counter) {
+            sendMessage("Error: Please key in a valid index!");
+        } else {
+            Task task = tasks[index];
+            task.completed = true;
+            sendMessage("Nice! I've marked this task as done:");
+            sendMessage("  [✓] " + task.toString());
+        }
+    }
+
     public static void sendTasks() {
         for (int tasknum = 0; tasknum < counter; tasknum ++) {
-            String todo = tasks[tasknum].toString();
-            sendMessage((tasknum + 1) + ". " + todo);
+            Task task = tasks[tasknum];
+            String todo = task.toString();
+            if (task.completed) {
+                sendMessage((tasknum + 1) + ".[✓] " + todo);
+            } else {
+                sendMessage((tasknum + 1) + ".[✗] " + todo);
+            }
         }
     }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,8 +1,10 @@
 public class Task {
     public String todo;
+    public boolean completed;
 
     public Task(String todo) {
         this.todo = todo;
+        this.completed = false;
     }
 
     public String toString() {

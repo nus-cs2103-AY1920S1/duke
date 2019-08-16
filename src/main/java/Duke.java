fc8573fa3d12cfac8c03de0f1fe06f0bad60414f
eugene teu <eugeneteu@gmail.com>
2019-08-16 22:26:04 +0800

    implemented level 4 code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -36,8 +36,75 @@ public class Duke {
                     System.out.println("Nice! I've marked this task as done: ");
                     System.out.println(taskToModify);
                     argument = myScanner.nextLine();
+                } else if (argumentArray[0].equals("todo")) {
+                    //form back string
+                    String toDoTaskString = "";
+                    for (int i = 1; i < argumentArray.length; i++) {
+                        toDoTaskString += argumentArray[i];
+                        toDoTaskString += " ";
 
-                } else {
+                    }
+                    //.trim() to remove trailing space
+                    Task toDoTask = new ToDo(toDoTaskString.trim());
+                    store.add(toDoTask);
+                    printGotIt();
+                    System.out.println(" " + toDoTask.toString());
+                    printNow(store.size());
+                    argument = myScanner.nextLine();
+                } else if (argumentArray[0].equals("deadline")) {
+                    //form back string , description stops at /by
+                    //date time starts from /by
+                    String deadlineTaskDescriptionString = "";
+                    String deadlineTaskDateAndTimeString = "";
+                    boolean createDesc = true;
+                    for (int i = 1; i < argumentArray.length; i++) {
+                        if (argumentArray[i].equals("/by")) {
+                            createDesc = false;
+                        } else if (createDesc) {
+                            deadlineTaskDescriptionString += argumentArray[i];
+                            deadlineTaskDescriptionString += " ";
+                        } else {
+                            deadlineTaskDateAndTimeString += argumentArray[i];
+                            deadlineTaskDateAndTimeString += " ";
+
+                        }
+                    }
+
+                    //use .trim() method to eliminate trailing white space
+                    Task deadlineTask = new Deadline(deadlineTaskDescriptionString.trim(), deadlineTaskDateAndTimeString.trim());
+                    store.add(deadlineTask);
+                    printGotIt();
+                    System.out.println(" " + deadlineTask.toString());
+                    printNow(store.size());
+                    argument = myScanner.nextLine();
+                } else if (argumentArray[0].equals("event")) {
+                    //form back string , description stops at /at
+                    //date time starts from /at
+                    String eventTaskDescriptionString = "";
+                    String eventTaskDateAndTimeString = "";
+                    boolean createDesc = true;
+                    for (int i = 1; i < argumentArray.length; i++) {
+                        if (argumentArray[i].equals("/at")) {
+                            createDesc = false;
+                        } else if (createDesc) {
+                            eventTaskDescriptionString += argumentArray[i];
+                            eventTaskDescriptionString += " ";
+                        } else {
+                            eventTaskDateAndTimeString += argumentArray[i];
+                            eventTaskDateAndTimeString += " ";
+
+                        }
+                    }
+                    //use of .trim() to avoid trailing whitespace
+                    Task eventTask = new Event(eventTaskDescriptionString.trim(), eventTaskDateAndTimeString.trim());
+                    store.add(eventTask);
+                    printGotIt();
+                    System.out.println(" " + eventTask.toString());
+                    printNow(store.size());
+                    argument = myScanner.nextLine();
+                }
+
+                else {
                     Task incomingTask = new Task(argument);
                     store.add(incomingTask);
                     System.out.println("added: " + argument);
@@ -49,6 +116,15 @@ public class Duke {
         //exiting program
         System.out.println("Bye. Hope to see you again soon!");
         myScanner.close();
+    }
+
+    //print common methods to call
+    static void printGotIt() {
+        System.out.println("Got it. I've added this task:");
+    }
+
+    static void printNow(int length) {
+        System.out.println("Now you have " + length + " tasks in the list.");
     }
 
 

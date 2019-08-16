7faa65af5b54e8c9ad6ee539d72e0cce92300a32
Ryan Chew <ryanchewjingrong1997@gmail.com>
2019-08-16 19:03:16 +0800

    Added Greet, Echo, Exit features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
+
+	private static final String[] initialGreeting = new String[]{
+		"Hello! I'm Duke",
+		"What can I do for you?"
+	};
+
+	private static final String lineRule =
+		"____________________________________________________________";
+
+	private static void say(String... lines) {
+		System.out.println(lineRule);
+		for(String line : lines) {
+			System.out.println(line);
+		}
+		System.out.println(lineRule);
+		System.out.println();
+	}
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+		say(initialGreeting);
+		Scanner s = new Scanner(System.in);
+		while(s.hasNextLine()) {
+			String userInput = s.nextLine();
+			if("bye".equals(userInput.trim())) {
+				say("Bye. Hope to see you again soon!");
+				break;
+			}
+			say(userInput);
+		}
     }
 }

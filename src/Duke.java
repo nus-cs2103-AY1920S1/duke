3689ff4e1e212e414dfe44dfa4d24e267042e49a
Gary Lim <garylyp@hotmail.com>
2019-08-17 01:23:38 +0800

    Level 2. Add, List
    Changes made in this commit:
    1) Created a list to add and monitor input items.

--- src/Duke.java
+++ src/Duke.java
@@ -13,20 +13,26 @@ public class Duke {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
+        List<String> itemList = new ArrayList<>();
         List<String> greeting = new ArrayList<>();
         greeting.add("Hello! I'm Duke");
         greeting.add("What can I do for you?");
-        printLinesWithDivider(greeting);
+        printLinesWithDivider(greeting, false);
 
         while (true) {
             Scanner sc = new Scanner(System.in);
-            String input = sc.next();
+            String input = sc.nextLine();
             if (input.contains("bye")) {
-                String byeMessage = "Bye. Hope to see you again soon!";
-                printWithDivider(byeMessage);
+                String response = "Bye. Hope to see you again soon!";
+                printWithDivider(response);
                 break;
+            } else if (input.contains("list")) {
+                printLinesWithDivider(itemList, true);
+
             } else {
-                printWithDivider(input);
+                String response = "added: " + input;
+                itemList.add(input);
+                printWithDivider(response);
             }
         }
 
@@ -39,10 +45,14 @@ public class Duke {
         System.out.print("\n");
     }
 
-    private static void printLinesWithDivider(List<String> lines) {
+    private static void printLinesWithDivider(List<String> lines, boolean includeIndex) {
         System.out.print(divider);
         for (int i = 0; i < lines.size(); i++) {
-            System.out.print("\t " + lines.get(i) + "\n");
+            System.out.print("\t ");
+            if (includeIndex) {
+                System.out.print((i+1) + ". ");
+            }
+            System.out.print(lines.get(i) + "\n");
         }
         System.out.print(divider);
         System.out.print("\n");

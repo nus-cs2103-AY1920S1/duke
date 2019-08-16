00f677e5ff3d07a2fd270c533d9431748e890b9e
Matt Lee <crazoter@gmail.com>
2019-08-16 17:35:07 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class Duke {
     public static void main(String[] args) {
@@ -15,7 +14,7 @@ public class Duke {
         */
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         Scanner sc = new Scanner(System.in);
-        List<String> list = new ArrayList<>();
+        List<Task> taskList = new ArrayList<>();
         String s;
         whileloop: while (true) {
             s = sc.nextLine();
@@ -23,13 +22,30 @@ public class Duke {
                 case "bye"://exit
                     break whileloop;
                 case "list"://list
-                    for (int i = 0; i < list.size(); i++) {
-                        System.out.println((i+1) + ". " + list.get(i));
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < taskList.size(); i++) {
+                        StringBuilder sb = new StringBuilder();
+                        Task t = taskList.get(i);
+                        sb.append(i+1).append(". ").append(t.getStatusWithDescription());
+                        System.out.println(sb);
                     }
                     break;
-                default://Add item
-                    list.add(s);
-                    System.out.println("added: " + s);
+                default:
+                    if (s.indexOf("done ") == 0) {//Set task to done
+                        try {
+                            int index = Integer.parseInt(s.replace("done ", ""));
+                            index--;//0 Bounded
+                            Task t = taskList.get(index);
+                            t.markAsDone(true);
+                            System.out.println("Nice! I've marked this task as done: ");
+                            System.out.println(t.getStatusWithDescription());
+                        } catch (NumberFormatException | IndexOutOfBoundsException e) {
+                            System.out.println(e);
+                        }
+                    } else {//Add item
+                        taskList.add(new Task(s));
+                        System.out.println("added: " + s);
+                    }
                     break;
             }
         }

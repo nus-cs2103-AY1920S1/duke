8242b8e0ff97edb7bcf98d5c6c39810d86f38f0d
Andy Lam <andyylam1997@gmail.com>
2019-08-16 20:51:45 +0800

    Fix Exception

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,5 +1,13 @@
 public class DukeException extends Exception {
+    private String e;
+
     DukeException(String e) {
         super(e);
+        this.e = e;
+    }
+
+    @Override
+    public String toString() {
+        return this.e;
     }
 }

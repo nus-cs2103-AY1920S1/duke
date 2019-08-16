ad2f58615c7a904f7a956f82a124f61b0d99eaa7
Andy Lam <andyylam1997@gmail.com>
2019-08-16 19:20:27 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -5,13 +6,22 @@ public class Duke {
         String welcomeMessage = "Hello! I'm Duke\n" + "What can I do for you?";
         System.out.println(welcomeMessage);
         Scanner reader = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+
         while (true) {
             String currentLine = reader.nextLine();
             if (currentLine.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!\n");
+                System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (currentLine.equals("list")) {
+                int count = 1;
+                for (String s: list) {
+                    System.out.println("" + count + ". " + s);
+                    count++;
+                }
             } else {
-                System.out.println(currentLine);
+                list.add(currentLine);
+                System.out.println("added: " + currentLine);
             }
         }
     }

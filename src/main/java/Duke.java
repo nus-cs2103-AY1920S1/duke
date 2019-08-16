a48c7feaff429969518e9d00417b47740bfa5b35
halfwhole <limziwei2@gmail.com>
2019-08-16 17:29:22 +0800

    Finish Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> store = new ArrayList<>();
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
@@ -12,8 +14,14 @@ public class Duke {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < store.size(); i++) {
+                    System.out.println((i + 1) + ". " + store.get(i));
+                }
+            } else {
+                store.add(input);
+                System.out.println("added: " + input);
             }
-            System.out.println(input);
         }
     }
 }

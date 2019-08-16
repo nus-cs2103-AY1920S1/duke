6d5c9ed9331ce6cd45b63d7973b05173ed801808
Lee Liak Ghee <liakify@gmail.com>
2019-08-16 23:50:36 +0800

    Created duke project, added greeting + command echoing + exit command.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String line = "\t____________________________________________________________";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        dukePrint("Hello from\n" + logo + "\nWhat can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        while (runCommand(sc.nextLine())) {
+
+        }
+    }
+
+    private static void dukePrint(String s) {
+        System.out.println(line);
+        String[] arr = s.split("\n");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("\t " + arr[i]);
+        }
+        System.out.println(line + "\n");
+    }
+
+    private static boolean runCommand(String s) {
+        switch (s) {
+            case "bye":
+                dukePrint("Bye. Hope to see you again soon!");
+                return false;
+            default:
+                dukePrint(s);
+                return true;
+        }
     }
 }

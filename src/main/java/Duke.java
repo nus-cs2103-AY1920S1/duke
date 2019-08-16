5aa69ac9f7e94433539524759eed9ec7aaf7afd2
Nanosync <10258208+Nanosync@users.noreply.github.com>
2019-08-17 01:01:54 +0800

    Implemented level 2 (add, list)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
+    static List<String> items = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -12,12 +16,12 @@ public class Duke {
             if (command.equals("bye")) {
                 printExitMessage();
                 break;
+            } else if (command.equals("list")) {
+                printItems();
+                continue;
             }
 
-            printSeparator();
-            System.out.println(" " + command);
-            printSeparator();
-            System.out.println();
+            addItem(command);
         }
     }
 
@@ -36,6 +40,29 @@ public class Duke {
     public static void printExitMessage() {
         printSeparator();
         System.out.println(" Bye. Hope to see you again soon!");
+        printSeparator();
+        System.out.println();
+    }
+
+    public static void addItem(String item) {
+        if (item.isEmpty()) {
+            return;
+        }
+
+        items.add(item);
+
+        printSeparator();
+        System.out.println("added: " + item);
+        printSeparator();
+        System.out.println();
+    }
+
+    public static void printItems() {
+        printSeparator();
+        for (int i = 0; i < items.size(); i++) {
+            String output = String.format("%d. %s", i + 1, items.get(i));
+            System.out.println(output);
+        }
         printSeparator();
         System.out.println();
     }

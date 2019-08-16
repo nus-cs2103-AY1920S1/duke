ee9e2ccf2adcf79bfdab8c1ec7d488dabe472c6c
eugene teu <eugeneteu@gmail.com>
2019-08-16 22:09:24 +0800

    implemented level 3 code, removed indentation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,28 +14,40 @@ public class Duke {
         System.out.println("What can I do for you?");
 
         //intialize an arraylist to store strings
-        List<String> store = new ArrayList<>();
+        List<Task> store = new ArrayList<>();
         Scanner myScanner = new Scanner(System.in);
         String argument = myScanner.nextLine();
         //when user input is not bye
         while (!argument.equals("bye") && !argument.equals("Bye")) {
             if (argument.equals("list")) {
                 int num = 1;
-                System.out.println("Here are the tasks in your lists:");
-                for (String i : store) {
-                    System.out.println( " " + num + ": " + i);
+                System.out.println("Here are the tasks in your list:");
+                for (Task i : store) {
+                    System.out.println(+ num + ". " + i);
                     num++;
                 }
                 argument = myScanner.nextLine();
             } else {
-                System.out.println(" added: " + argument);
-                store.add(argument);
-                argument = myScanner.nextLine();
+                String[] argumentArray = argument.split(" ");
+                if (argumentArray[0].equals("done")) {
+                    int index = Integer.valueOf(argumentArray[1]) - 1;
+                    Task taskToModify = store.get(index);
+                    taskToModify.markAsDone();
+                    System.out.println("Nice! I've marked this task as done: ");
+                    System.out.println(taskToModify);
+                    argument = myScanner.nextLine();
+
+                } else {
+                    Task incomingTask = new Task(argument);
+                    store.add(incomingTask);
+                    System.out.println("added: " + argument);
+                    argument = myScanner.nextLine();
+                }
             }
         }
 
         //exiting program
-        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("Bye. Hope to see you again soon!");
         myScanner.close();
     }
 

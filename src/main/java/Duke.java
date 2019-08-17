b9a79e7b66a2915b0caa52c5c5f0ad758de015d9
Shuming <geshuming2@gmail.com>
2019-08-17 22:52:08 +0800

    Implement Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+        }
+
+        sc.close();
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }

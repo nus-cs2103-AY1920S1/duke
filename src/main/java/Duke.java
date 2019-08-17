109dc4652ba30a8a3b74803f08e086ef67f6f191
Ang Ze Yu <angzeyu@gmail.com>
2019-08-18 00:34:25 +0800

    Level 2. Add, List implementation with simple test case

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Duke {
+
     public static void main(String[] args) {
         String horizontalLine = "____________________________________________________________\n";
         String greetingMsg = horizontalLine
@@ -11,21 +12,40 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in);
 
+        String[] inputs = new String[100];
         String input;
         String smallIndent = " ";
-        while (true) {
-            input = scanner.nextLine();
+        int nextTaskIndex = 0;
 
-            System.out.print(horizontalLine);
-            if (input.equals("bye")) {
-                System.out.println(" Bye. Hope to see you again soon!");
-                System.out.println(horizontalLine);
-                break;
-            } else {
-                System.out.println(smallIndent + input);
-                System.out.println(horizontalLine);
+        mainLoop:
+            while (true) {
+                input = scanner.nextLine();
+
+                System.out.print(horizontalLine);
+
+                switch (input) {
+                    case "list":
+                        printInputArray(inputs, nextTaskIndex - 1);
+                        System.out.println(horizontalLine);
+                        break;
+                    case "bye":
+                        System.out.println(" Bye. Hope to see you again soon!");
+                        System.out.println(horizontalLine);
+                        break mainLoop;
+                    default:
+                        inputs[nextTaskIndex] = input;
+                        nextTaskIndex++;
+                        System.out.println(" added: " + input);
+                        System.out.println(horizontalLine);
+                        break;
+                }
             }
-        }
 
+    }
+
+    private static void printInputArray(String[] inputs, int n) {
+        for (int i = 0; i <= n; i++) {
+            System.out.printf(" %d. %s\n", i + 1, inputs[i]);
+        }
     }
 }

4fb1e046bff0cc5c47671f252b1dba2ff5f35685
Tuan Ding Wei <tuandingwei@gmail.com>
2019-08-17 14:02:17 +0800

    Add Greet, Echo and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,19 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello I am\n" + logo);
+        System.out.println("What can I do for you?");
+
+        //Echo
+        Scanner sc = new Scanner(System.in);
+        String input = "nil";
+        while (input.equals("bye") == false) {
+            input = sc.nextLine();
+            System.out.println(input + "\n");
+            input = input.toLowerCase();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
+
     }
 }

9c7efa7daf5be75521b2e988a8f0a06f3405d364
rebase123zxc <ac3ofskyz@gmail.com>
2019-08-17 23:38:22 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Duke {
+
+    private static String stringFormatter(String s, String line) {
+        String newString = line
+                + String.format("\t %s\n", s)
+                + line;
+        return newString;
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+        String line = "\t____________________________________________________________\n";
+        String greeting = line
+                + "\t Hello! I'm Duke\n"
+                + "\t What can I do for you?\n"
+                + line;
+        System.out.println(greeting);
+        while (!sc.hasNext("bye")) {
+            String s = sc.nextLine();
+            System.out.println(stringFormatter(s, line));
+        }
+        System.out.println(stringFormatter("Bye. Hope to see you again soon!", line));
     }
 }

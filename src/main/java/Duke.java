eab7c8583ecf44eaa22c89f36081e648245d3090
Chow Jia Ying <chowjiaying211@gmail.com>
2019-08-17 23:28:41 +0800

    Indent all messages displayed by program

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,35 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static final String LINE = "    ____________________________________________________________";
+    public static final String TABS = "     ";
+
+    /**
+     * Prints out system messages after formatting
+     * @param command
+     */
+    public static void formatDisplayedMessages(String command) {
+        System.out.println(LINE);
+        String[] parsedCommands = command.split("\n");
+        for (String line : parsedCommands) {
+            System.out.println(TABS + line);
+        }
+        System.out.println(LINE);
+    }
+
     public static void main(String[] args) {
         String greeting = "Hello! I'm Duke\n" + "What can I do for you?\n";
-        System.out.println(greeting);
+        formatDisplayedMessages(greeting);
         Scanner sc = new Scanner(System.in); //gets commands from user
         String userCommand = sc.nextLine();
 
         //if command is not bye, echo userCommand
         while (!userCommand.equals("bye")) {
-            System.out.println(userCommand);
+            formatDisplayedMessages(userCommand);
             userCommand = sc.nextLine();
         }
 
         //program runs this command when userCommand is bye
-        System.out.println("Bye. Hope to see you again soon!");
+        formatDisplayedMessages("Bye. Hope to see you again soon!");
     }
 }

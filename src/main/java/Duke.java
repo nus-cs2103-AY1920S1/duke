89be77111a6095346c53d24eaa74222365ca82ea
davidcwh <dcwh12321@gmail.com>
2019-08-17 14:29:35 +0800

    Level-2 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,7 @@ public class Duke {
     public static void runDuke() {
         String line = "    _____________________________________________";
         String currentCommand = "";
+        ArrayList<String> addedItems = new ArrayList<>();
 
         System.out.println(line);
         System.out.println("     Hello! I'm Duke");
@@ -15,9 +16,18 @@ public class Duke {
         while (!currentCommand.equals("bye")) {
             currentCommand = scanner.nextLine();
             if (!currentCommand.equals("bye")) {
-                System.out.println(line);
-                System.out.println("     " + currentCommand);
-                System.out.println(line);
+                if (!currentCommand.equals("list")) {
+                    addedItems.add(currentCommand);
+                    System.out.println(line);
+                    System.out.println("     added: " + currentCommand);
+                    System.out.println(line);
+                } else {
+                    System.out.println(line);
+                    for (int i = 0; i < addedItems.size(); i++) {
+                        System.out.println("     " + (i + 1) + ". " + addedItems.get(i));
+                    }
+                    System.out.println(line);
+                }
             }
         }
 

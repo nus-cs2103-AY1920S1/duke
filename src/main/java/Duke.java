c51c8422dbc391b2abe7bb738148991afabcfe15
kohyida1997 <kohyida1997@gmail.com>
2019-08-17 23:41:39 +0800

    level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,12 +3,15 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Duke {
+    //constants for UI messages
     final static String greet = "Hello! I'm Duke\nWhat can I do for you?";
     final static String goodbye = "Bye. Hope to see you again!";
+    final static String niceAdded = "Nice! I've marked this task as done:";
+
 
     public static void main(String[] args) throws Exception {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<String> listOfTasks = new ArrayList<>();
+        ArrayList<Task> listOfTasks = new ArrayList<>();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -20,19 +23,30 @@ public class Duke {
         String command = input.readLine();
 
         while(!command.equals("bye")){
-            switch (command)
+            String[] tokens = command.split(" ");
+            switch (tokens[0])
             {
                 case "list":
                     //"list" logic
                     int size = listOfTasks.size();
+                    System.out.println("Here are your tasks in your list:");
                     for(int i = 0; i < size; i++){
-                        String curr = listOfTasks.get(i);
-                        System.out.println(i+1 + "." + curr);
+                        Task curr = listOfTasks.get(i);
+                        System.out.println(i+1 + "." + curr.toString());
                     }
                     break;
+                case "done":
+                    //done logic
+                    int toComplete = Integer.parseInt(command.split(" ")[1]) - 1;
+                    Task curr = listOfTasks.get(toComplete);
+                    listOfTasks.get(toComplete).completeTask();
+                    System.out.println(niceAdded);
+                    System.out.println(curr.toString());
+                    break;
                 default:
+                    //add task logic
                     System.out.println("added: " + command);
-                    listOfTasks.add(command);
+                    listOfTasks.add(new Task(command));
                     break;
             }
             command = input.readLine();
---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+    public static int totalTasks = 0;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public void completeTask(){
+        isDone = true;
+    }
+
+    @Override
+    public String toString(){
+        return "[" + getStatusIcon() + "]" + " " + description;
+    }
+
+}

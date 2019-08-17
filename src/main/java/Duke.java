457876dc8ef4fe9963b99c64a41e2342c7947d8a
boqiang <bobo_qiang@hotmail.com>
2019-08-17 23:22:48 +0800

    Completed Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,12 +6,15 @@ public class Duke {
     public static final String START_HORIZONTAL_LINE = "   ____________________________________________________________";
     public static final String END_HORIZONTAL_LINE = "   ____________________________________________________________\n";
     public static final String COMMAND_INDENTATION = "    ";
+    public static final String COMPLETION_INDENTATION = "  ";
     public static final String BYE_MESSAGE = COMMAND_INDENTATION + "Bye. Hope to see you again soon!";
     public static final String GREETING_MESSAGE = COMMAND_INDENTATION + "Hello! I'm Duke\n" +
             COMMAND_INDENTATION + "What can I do for you?";
-    public static final String ADDED_MESSAGE = "added: ";
+    public static final String ADDED_MESSAGE = COMMAND_INDENTATION + "added: ";
+    public static final String LIST_MESSAGE = COMMAND_INDENTATION + "Here are the tasks in your list:";
+    public static final String DONE_MESSAGE = COMMAND_INDENTATION + "Nice! I've marked this task as done:";
 
-    private List<String> commandHistoryList = new ArrayList<>();
+    private List<Task> taskList = new ArrayList<>();
     private Scanner myScanner;
     public static void main(String[] args) {
         Duke myDuke = new Duke();
@@ -25,8 +28,8 @@ public class Duke {
         myScanner = new Scanner(System.in);
         displayGreetingMessage();
         while (myScanner.hasNextLine()) {
-            String command = readCommand();
-            executeCommand(command);
+            String[] userInput = readCommand();
+            executeCommand(userInput);
         }
     }
 
@@ -48,49 +51,86 @@ public class Duke {
     }
 
     /**
-     * Reads user command by the user.
+     * Reads user command entered by the user.
      * @return commands entered by the user
      */
-    public String readCommand() {
-        return myScanner.nextLine().trim();
+    public String[] readCommand() {
+        return myScanner.nextLine().trim().split(" ");
     }
 
     /**
-     * Saves command entered by the user.
+     * Saves command as task name.
      * @param command the last command entered by the user
      */
     public void addCommandsEntered(String command) {
-        commandHistoryList.add(command);
+        Task myTask = new Task(command);
+        taskList.add(myTask);
     }
 
     /**
      * Runs the commands entered by the user.
-     * @param command is the last command entered by the user
+     * @param commands is the last command entered by the user
      */
-    public void executeCommand(String command) {
-        if (command.equals("bye")) {
-            printLines(START_HORIZONTAL_LINE, BYE_MESSAGE, END_HORIZONTAL_LINE);
-            System.exit(0);
-        } else if (command.equals("list")) {
-            String formattedString = getCommandsHistory();
-            printLines(START_HORIZONTAL_LINE, COMMAND_INDENTATION + formattedString, END_HORIZONTAL_LINE);
+    public void executeCommand(String ... commands) {
+        if (commands[0].equals("bye") && commands.length == 1) {
+            performsByeCommand();
+        } else if (commands[0].equals("list") && commands.length == 1) {
+            performsListCommand();
+        } else if (commands[0].equals("done") && commands.length == 2) {
+            performsDoneCommand(Integer.parseInt(commands[1]));
         } else {
-            addCommandsEntered(command);
-            printLines(START_HORIZONTAL_LINE, COMMAND_INDENTATION + ADDED_MESSAGE + command, END_HORIZONTAL_LINE);
+            performsOthersCommand(String.join(" ", commands));
         }
     }
 
     /**
-     * Formats the history of user commands.
-     * @return formatted history of commands
+     * Performs others command
+     * @param command is the last command entered by the user
      */
-    public String getCommandsHistory() {
+    public void performsOthersCommand(String command) {
+        addCommandsEntered(command);
+        printLines(START_HORIZONTAL_LINE, ADDED_MESSAGE + command, END_HORIZONTAL_LINE);
+    }
+
+    /**
+     * Performs the command "done"
+     * @param itemNum is the index of the task list
+     */
+    public void performsDoneCommand(int itemNum) {
+        taskList.get(itemNum - 1).completeTask();
+        printLines(START_HORIZONTAL_LINE, DONE_MESSAGE,
+                COMMAND_INDENTATION + COMPLETION_INDENTATION + taskList.get(itemNum - 1).toString(), END_HORIZONTAL_LINE);
+    }
+
+    /**
+     * Performs the command "list"
+     */
+    public void performsListCommand() {
+        String formattedString = getTasks();
+        printLines(START_HORIZONTAL_LINE, LIST_MESSAGE, COMMAND_INDENTATION + formattedString,
+                END_HORIZONTAL_LINE);
+    }
+
+    /**
+     * Performs the command "bye"
+     */
+    public void performsByeCommand() {
+        printLines(START_HORIZONTAL_LINE, BYE_MESSAGE, END_HORIZONTAL_LINE);
+        System.exit(0);
+    }
+
+    /**
+     * Formats the list of tasks.
+     * @return formatted list of tasks
+     */
+    public String getTasks() {
         StringBuilder myStringBuilder = new StringBuilder();
-        for (int i = 0; i < commandHistoryList.size(); i++) {
-            if (i < commandHistoryList.size() - 1) {
-                myStringBuilder.append(i + 1).append(". ").append(commandHistoryList.get(i)).append("\n").append(COMMAND_INDENTATION);
-            } else if (i == commandHistoryList.size() - 1) {
-                myStringBuilder.append(i + 1).append(". ").append(commandHistoryList.get(i));
+        for (int i = 0; i < taskList.size(); i++) {
+            if (i < taskList.size() - 1) {
+                myStringBuilder.append(i + 1).append(".").append(taskList.get(i)).append("\n")
+                        .append(COMMAND_INDENTATION);
+            } else if (i == taskList.size() - 1) {
+                myStringBuilder.append(i + 1).append(".").append(taskList.get(i));
             }
         }
         return myStringBuilder.toString();
---
+++ src/main/java/Task.java
@@ -0,0 +1,33 @@
+public class Task {
+    private String name;
+    private boolean isDone;
+    public Task(String name) {
+        this.name = name;
+        isDone = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean getStatus() {
+        return isDone;
+    }
+
+    public void completeTask() {
+        this.isDone = true;
+    }
+
+    public String getStatusIcon() {
+        return isDone ? "\u2713" : "\u2718";
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + getName();
+    }
+}

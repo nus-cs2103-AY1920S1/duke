98cdda104822c115f6ac8c3a1f23040471fd20ce
davidcwh <dcwh12321@gmail.com>
2019-08-17 19:01:55 +0800

    Level-6 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -28,11 +28,33 @@ public class Duke {
                         System.out.println(line);
                     } else if (currentCommand.length() >= 6 && currentCommand.substring(0, 4).equals("done")) { // Marking a task as done
                         int taskNumber = Integer.parseInt(currentCommand.substring(5)) - 1;
-                        addedItems.get(taskNumber).markAsDone();
-                        System.out.println(line);
-                        System.out.println("     Nice! I've marked this task as done:");
-                        System.out.println("       " + addedItems.get(taskNumber));
-                        System.out.println(line);
+
+                        if (taskNumber >= 0 && taskNumber < addedItems.size()) {
+                            addedItems.get(taskNumber).markAsDone();
+                            System.out.println(line);
+                            System.out.println("     Nice! I've marked this task as done:");
+                            System.out.println("       " + addedItems.get(taskNumber));
+                            System.out.println(line);
+                        } else {
+                            throw new InvalidCommandDukeException("☹ OOPS!!! There is no task labelled that number.");
+                        }
+
+                    } else if (currentCommand.length() >= 8 && currentCommand.substring(0, 6).equals("delete")) { // Deleting a task in the list
+                        int taskNumber = Integer.parseInt(currentCommand.substring(7)) - 1;
+
+                        if (taskNumber >= 0 && taskNumber < addedItems.size()) {
+                            Task removedTask = addedItems.get(taskNumber);
+                            addedItems.remove(taskNumber);
+                            System.out.println(line);
+                            System.out.println("     Noted. I've removed this task:");
+                            System.out.println("       " + removedTask);
+                            System.out.println("     Now you have " + addedItems.size() + (addedItems.size() > 1 ? " tasks in the list." : " task in the list."));
+                            System.out.println(line);
+                        } else {
+                            throw new InvalidCommandDukeException("☹ OOPS!!! There is no task labelled that number.");
+                        }
+
+
                     } else if (!currentCommand.equals("")){ // Adding new task to the list
                         String[] tempArray = currentCommand.split(" ");
                         Task currentTask = null;

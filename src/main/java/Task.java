40b45cf0327e5df0312ecfa9da01fd9e3b6b0e35
Chow Jia Ying <chowjiaying211@gmail.com>
2019-08-18 00:40:46 +0800

    Set Tasks as done and output tasks with their statuses

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,22 @@
 public class Task {
     String description = "";
+    boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Marks the current Task as done
+     */
+    public void markAsDone() {
+        isDone = true;
     }
 
     @Override
     public String toString() {
-        return description;
+        String statusIcon = isDone ? "\u2713" : "\u2718";
+        return statusIcon + description;
     }
 }

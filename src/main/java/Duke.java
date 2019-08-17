ceb6aa0bfeb14622addec5b73b34a0e00645bcf3
CoderStellaJ <40347063+CoderStellaJ@users.noreply.github.com>
2019-08-17 20:14:22 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,35 +1,64 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
+    private String[] mylist;
+    private int size;
+
+    public Duke() {
+        mylist = new String[100];
+        size = 0;
         greet();
-        Scanner sc = new Scanner(System.in);
-        String query = sc.next();
-        while(!query.equals("bye")) {
-            echo(query);
-            query = sc.next();
+    }
+
+    public void add(String str) {
+        mylist[size] = str;
+        size++;
+        print("\tadded: "+str);
+    }
+
+    public void list() {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < size; i++) {
+            if(i != 0) sb.append("\n");
+            sb.append("\t"+(i+1)+". "+mylist[i]);
         }
-        exit();
+        print(sb.toString());
     }
 
-    public static void echo(String str) {
-        System.out.println("\t____________________________________________________________");
-        System.out.println("\t"+str);
-        System.out.println("\t____________________________________________________________");
-
+    public void exit() {
+        print("\tBye. Hope to see you again soon!");
     }
 
-    public static void greet() {
+    private void print(String str) {
+        System.out.println("\t____________________________________________________________");
+        System.out.println(str);
+        System.out.println("\t____________________________________________________________");
+    }
+
+    private void greet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        echo("Hello! I'm Duke\n\tWhat can I do for you?");
+        print("\tHello! I'm Duke\n\tWhat can I do for you?");
     }
 
-    public static void exit() {
-        echo("Bye. Hope to see you again soon!");
+    public static void main(String[] args) {
+        Duke duke = new Duke();
+        Scanner sc = new Scanner(System.in);
+        String query = sc.next();
+        while(!query.equals("bye")) {
+            if(query.equals("list")) {
+                duke.list();
+            }else {
+                duke.add(query);
+            }
+            query = sc.next();
+        }
+        duke.exit();
     }
 }

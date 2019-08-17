7f68e9ee8e9f795d8be4b2a19cd07dd0346f86e6
Shuming <geshuming2@gmail.com>
2019-08-17 23:01:54 +0800

    Implement Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> listOfInputs = new ArrayList<>();
 
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
@@ -10,8 +12,17 @@ public class Duke {
             String input = sc.nextLine();
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                int index = 1;
+                for (String i : listOfInputs) {
+                    System.out.printf("%d. %s\n", index, i);
+                    index++;
+                }
+                listOfInputs.add(input);
+            } else {
+                listOfInputs.add(input);
+                System.out.println(input);
             }
-            System.out.println(input);
         }
 
         sc.close();

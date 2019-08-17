9b68f8b4e02a94b01e04ab52d01e8d26e7c92c10
Chow Jia Ying <chowjiaying211@gmail.com>
2019-08-18 01:55:34 +0800

    Represent ToDo tasks with a ToDo class

---
+++ src/main/java/ToDo.java
@@ -0,0 +1,8 @@
+/**
+ * This class represents ToDo tasks which is a type of Task.
+ */
+public class ToDo extends Task {
+    public ToDo(String description) {
+        super(description);
+    }
+}

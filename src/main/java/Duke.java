e7883e1e0dbf2391e937c0ff084f102a876d7038
CarbonGrid <nextgentechno@hotmail.com>
2019-08-18 02:44:44 +0800

    Constructed initial skeleton of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    private static void cout(String str){
+        System.out.println("    ____________________________________________________________\n     " +
+                str.replaceAll("\n", "\n     ") +
+                "\n    ____________________________________________________________\n");
+    }
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        cout("Hello! I'm Duke\nWhat can I do for you?");
+
+        while(true){
+            String instr = in.nextLine().trim();
+            if(instr.equals("bye"))
+                break;
+            cout(instr);
+        }
+
+        cout("Bye. Hope to see you again soon!");
     }
 }

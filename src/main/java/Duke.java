7ecb2483a2b688fc00627c417d9be8d879d4740a
xinmingzh <xinming.zh@gmail.com>
2019-08-18 03:57:40 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String input;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("    " + "____________________________________________________________");
+        System.out.println("    " + "Hello! I'm Duke");
+        System.out.println("    " + "What can I do for you?");
+        System.out.println("    " + "____________________________________________________________\n");
+
+        while (true) {
+            input = sc.next();
+            if (input.equals("bye")) {
+                System.out.println("    " + "____________________________________________________________");
+                System.out.println("    " + "Bye. Hope to see you again soon!");
+                System.out.println("    " + "____________________________________________________________\n");
+                break;
+            }
+            else if (input.equals("list")) {
+                System.out.println("    " + "____________________________________________________________");
+                System.out.println("    " + "list");
+                System.out.println("    " + "____________________________________________________________\n");
+            }
+            else if (input.equals("blah")) {
+                System.out.println("    " + "____________________________________________________________");
+                System.out.println("    " + "blah");
+                System.out.println("    " + "____________________________________________________________\n");
+            }
+        }
     }
 }

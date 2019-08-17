5cc275cc9b9f70f4ed6615169c0d25d261e3cac7
Chow Jia Ying <chowjiaying211@gmail.com>
2019-08-18 01:57:28 +0800

    Represent Event tasks with an Event class

---
+++ src/main/java/Event.java
@@ -0,0 +1,8 @@
+/**
+ * This class represents Event tasks which is a type of Task.
+ */
+public class Event extends Task {
+    public Event(String description) {
+        super(description);
+    }
+}

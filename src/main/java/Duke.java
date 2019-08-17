35be53266a5355994d7171ee0bf3b801f5722bf8
weiboon <lumweiboon@gmail.com>
2019-08-18 00:53:04 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Duke {
     public static void main(String[] args) {
     	Scanner scanner = new Scanner(System.in);
+    	String[] itemList = new String[100];
+    	int itemCounter = 0;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -11,9 +14,28 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         String userInput = scanner.nextLine();
         while (!userInput.equals("bye")) {
-        	System.out.println(userInput);
+        	if (userInput.equals("list")) {
+        		String prettifiedList = listConverter(itemList);
+        		System.out.println(prettifiedList);
+			} else {
+        		itemList[itemCounter] = userInput;
+        		itemCounter++;
+				System.out.println("added: " + userInput);
+			}
         	userInput = scanner.nextLine();
 		}
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.print("Bye. Hope to see you again soon!");
     }
+
+    private static String listConverter(String[] list) {
+    	StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < list.length; i++)
+		{
+			if (list[i] != null) {
+				sb.append(i + 1 + ". " + list[i]);
+				sb.append("\n");
+			}
+		}
+		return sb.toString().trim();
+	}
 }

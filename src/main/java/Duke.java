423a2e001a531f3d3fa0b801dafccb9e655c8b69
marqueurs404 <marcus.ong404@gmail.com>
2019-08-17 17:28:59 +0800

    Duke.java: Implemented greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        initialize();
+        chat();
+    }
+
+    public static void chat() {
+        String greeting = "\tHello! I'm Duke\n\tWhat can I do for you?";
+        String bye = "\tBye. Hope to see you again soon!";
+
+        reply(greeting);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        while (!input.equalsIgnoreCase("bye")) {
+            reply("\t" + input);
+            input = sc.next();
+        }
+        reply(bye);
+    }
+
+    public static void initialize() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+    }
+
+    public static void reply(String message) {
+        System.out.println("\t____________________________________________________________");
+        System.out.println(message);
+        System.out.println("\t____________________________________________________________");
     }
 }

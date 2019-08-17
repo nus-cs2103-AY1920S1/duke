5aefd078150dff89c7248fff2d0f55b59e59d3c6
Larry Law <larrylaw97@gmail.com>
2019-08-17 20:45:57 +0800

    chore: level 1 greet echo exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
+    static Scanner sc;
+    
+    private static void levelOne() {
+        boolean exit = false;
+        String exitMessage = "Bye. Hope to see you again soon!";
+        while (!exit) {
+            String input = sc.next();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+        System.out.println(exitMessage);
+    }
+    
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +24,9 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+        sc = new Scanner(System.in);
+        Duke.levelOne();
+        sc.close();
     }
 }

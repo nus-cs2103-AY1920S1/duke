da5d57b9eba181ade6ff789dcad8e05dfdf8634b
Shawn <shawnlum123@gmail.com>
2019-08-17 15:23:47 +0800

    Completed Level-1: Greet, Echo, Exit
    Commit for the 1st Stage

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,55 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static final String MESSAGE_PADDING  = "     ";
+    private static final String MESSAGE_BOUNDARY = "    ____________________________________________________________";
+
+    private void printResponse(String response) {
+        System.out.println(MESSAGE_BOUNDARY);
+        System.out.print(MESSAGE_PADDING);
+        System.out.println(response);
+        System.out.println(MESSAGE_BOUNDARY);
+        System.out.println("");
+    }
+
+    private boolean shouldContinueChat(String query) {
+        return !query.equals("bye");
+    }
+
+    private String findResponse(String query, boolean continueChat) {
+        if (!continueChat) {
+            return "Bye. Hope to see you again soon!";
+        }
+
+        return query;
+    }
+
+
+    public void spin() {
+        boolean continueChat = true;
+        Scanner myscanner = new Scanner(System.in);  // Create a Scanner object
+
+        printResponse("Hello! I'm Duke\n" + MESSAGE_PADDING + "What can I do for you?");
+
+        do {
+            //Get query from user
+            String userQuery = myscanner.nextLine();
+
+            //Find Response
+            continueChat = shouldContinueChat(userQuery);
+            String dukeResponse = findResponse(userQuery, continueChat);
+
+            //Print Response
+            printResponse(dukeResponse);
+
+        } while (continueChat);
+        myscanner.close();
+    }
+
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke myObj = new Duke();
+        myObj.spin();
     }
 }

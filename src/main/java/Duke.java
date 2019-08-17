5496c235f597a1f5e7512ee195d9ac8afb74df48
kohyida1997 <kohyida1997@gmail.com>
2019-08-17 23:58:33 +0800

    level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,7 +7,11 @@ public class Duke {
     final static String greet = "Hello! I'm Duke\nWhat can I do for you?";
     final static String goodbye = "Bye. Hope to see you again!";
     final static String niceAdded = "Nice! I've marked this task as done:";
+    final static String gotIt = "Got it. I've added this task:";
 
+    private static String printNumTasks(){
+        return "Now you have " + Task.totalTasks + " tasks in the list.";
+    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
@@ -24,6 +28,8 @@ public class Duke {
 
         while(!command.equals("bye")){
             String[] tokens = command.split(" ");
+            String toAdd = "";
+            Task newTask;
             switch (tokens[0])
             {
                 case "list":
@@ -43,10 +49,50 @@ public class Duke {
                     System.out.println(niceAdded);
                     System.out.println(curr.toString());
                     break;
+                case "todo":
+                    for(int j = 1; j < tokens.length; j++){
+                        toAdd = toAdd + tokens[j] + " ";
+                    }
+                    newTask = new Todo(toAdd.trim());
+                    System.out.println(gotIt);
+                    System.out.println(" " + newTask.toString());
+                    listOfTasks.add(newTask);
+                    System.out.println(printNumTasks());
+                    break;
+                case "deadline":
+                    String date = "";
+                    boolean dateFlag = false;
+                    for(int m = 1; m < tokens.length; m++) {
+                        if (tokens[m].equals("/by")) {
+                            dateFlag = true;
+                        } else {
+                            if (dateFlag == false) toAdd = toAdd + tokens[m] + " ";
+                            else date = date + tokens[m] + " ";
+                        }
+                    }
+                    newTask = new Deadline(toAdd.trim(), date.trim());
+                    System.out.println(gotIt);
+                    System.out.println(" " + newTask.toString());
+                    listOfTasks.add(newTask);
+                    System.out.println(printNumTasks());
+                    break;
+                case "event":
+                    String timing = " ";
+                    boolean timeFlag = false;
+                    for(int z = 1; z < tokens.length; z++){
+                        if(tokens[z].equals("/at")) timeFlag = true;
+                        else {
+                            if(timeFlag == false) toAdd = toAdd + tokens[z] + " ";
+                            else timing = timing + tokens[z] + " ";
+                        }
+                    }
+                    newTask = new Event(toAdd.trim(), timing.trim());
+                    System.out.println(gotIt);
+                    System.out.println(" " + newTask.toString());
+                    listOfTasks.add(newTask);
+                    System.out.println(printNumTasks());
+                    break;
                 default:
-                    //add task logic
-                    System.out.println("added: " + command);
-                    listOfTasks.add(new Task(command));
                     break;
             }
             command = input.readLine();

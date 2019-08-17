b4ba574981da92507ed7a70fa85c08b2a498c507
Lim Kang Yee <kangyee2408@gmail.com>
2019-08-17 11:09:06 +0800

    Add printing of greeting and echo-ing of command to main in Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,5 +11,12 @@ public class Duke {
 
         String greeting = "Hello! I'm Duke\n"
                 + "What can I do for you?\n";
+        System.out.println(greeting);
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println(command);
+            command = sc.nextLine();
+        }
     }
 }

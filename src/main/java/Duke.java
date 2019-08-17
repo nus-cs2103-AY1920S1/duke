4a6d62563fbfb7853bb3f48b3edcd7cf19a0f657
Ang Ze Yu <angzeyu@gmail.com>
2019-08-18 02:17:02 +0800

    Level 3. Mark as Done & A-Classes extension implementation, with fixed runtest.sh and new test input.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,51 +1,91 @@
 import java.util.Scanner;
 
 public class Duke {
-
+    public static String horizontalLine = "____________________________________________________________\n";
+    
     public static void main(String[] args) {
-        String horizontalLine = "____________________________________________________________\n";
-        String greetingMsg = horizontalLine
+        String greetingMsg = Duke.horizontalLine
                 + " Hello! I'm Duke\n"
                 + " What can I do for you?\n"
-                + horizontalLine;
+                + Duke.horizontalLine;
         System.out.println(greetingMsg);
 
         Scanner scanner = new Scanner(System.in);
 
-        String[] inputs = new String[100];
-        String input;
-        String smallIndent = " ";
+        Task[] tasks = new Task[100];
+        String[] inputs;
         int nextTaskIndex = 0;
 
         mainLoop:
             while (true) {
-                input = scanner.nextLine();
+                inputs = scanner.nextLine().split("\\s+");
 
-                System.out.print(horizontalLine);
+                System.out.print(Duke.horizontalLine);
 
-                switch (input) {
-                    case "list":
-                        printInputArray(inputs, nextTaskIndex - 1);
-                        System.out.println(horizontalLine);
-                        break;
-                    case "bye":
-                        System.out.println(" Bye. Hope to see you again soon!");
-                        System.out.println(horizontalLine);
-                        break mainLoop;
-                    default:
-                        inputs[nextTaskIndex] = input;
-                        nextTaskIndex++;
-                        System.out.println(" added: " + input);
-                        System.out.println(horizontalLine);
-                        break;
+                if (inputs.length == 1) {
+                    switch (inputs[0]) {
+                        case "list":
+                            printTaskArray(tasks);
+                            break;
+                        case "bye":
+                            System.out.println(" Bye. Hope to see you again soon!");
+                            System.out.println(Duke.horizontalLine);
+                            break mainLoop;
+                        default:
+                            Task task = new Task(inputs[0]);
+                            tasks[nextTaskIndex] = task;
+                            nextTaskIndex++;
+                            System.out.println(" added: " + inputs[0]);
+                            System.out.println(Duke.horizontalLine);
+                            break;
+                    }
+                } else if (inputs.length == 2) {
+                    if (inputs[0].equals("done")) {
+                        try {
+                            Integer taskIndex = Integer.parseInt(inputs[1]);
+                            if (taskIndex == null) {
+                                throw new NullPointerException();
+                            }
+
+                            tasks[--taskIndex].setDone(true);
+                            printTaskDone(tasks[taskIndex]);
+
+                        } catch (NumberFormatException e) {
+                            System.out.println("Specified task number is invalid.");
+                            System.out.println(Duke.horizontalLine);
+                        } catch (IndexOutOfBoundsException | NullPointerException e) {
+                            System.out.println("Task is not within outputs printed by list!");
+                        }
+                    }
                 }
+
+
             }
 
     }
 
-    private static void printInputArray(String[] inputs, int n) {
-        for (int i = 0; i <= n; i++) {
-            System.out.printf(" %d. %s\n", i + 1, inputs[i]);
+    private static void printTaskDone(Task task) {
+        if (task.isDone) {
+            System.out.println(" Nice! I've marked this task as done:");
+            System.out.printf("   %s\n", task.getStatusText());
+            System.out.println(Duke.horizontalLine);
         }
+    }
+
+    private static void printTaskArray(Task[] tasks) {
+        System.out.println(" Here are the tasks in your list:");
+
+        int taskIndex = 1;
+        for (Task task : tasks) {
+            if (task == null) {
+                break;
+            }
+            System.out.printf(" %d.%s\n",
+                    taskIndex,
+                    task.getStatusText());
+            taskIndex++;
+        }
+
+        System.out.println(Duke.horizontalLine);
     }
 }

c87b2b81a4e2c3357b158a75d1c11c7851237989
Chow Jia Ying <chowjiaying211@gmail.com>
2019-08-17 22:10:38 +0800

    Greet the user on startup, echo user messages and display messages on exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = "Hello! I'm Duke\n" + "What can I do for you?\n";
+        System.out.println(greeting);
+        Scanner sc = new Scanner(System.in); //gets commands from user
+        String userCommand = sc.nextLine();
+
+        //if command is not bye, echo userCommand
+        while (!userCommand.equals("bye")) {
+            System.out.println(userCommand);
+            userCommand = sc.nextLine();
+        }
+
+        //program runs this command when userCommand is bye
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }

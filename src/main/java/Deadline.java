511374583eee55904c35075315e8796340a4f709
Chow Jia Ying <chowjiaying211@gmail.com>
2019-08-18 02:03:36 +0800

    Represent Deadline tasks with a Deadline class

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,8 @@
+/**
+ * This class represents Deadline tasks which is a type of Task.
+ */
+public class Deadline extends Task {
+    public Deadline(String description) {
+        super(description);
+    }
+}

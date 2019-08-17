e55a30e26f973b21ac44d570508c21086e2e7d26
davidcwh <dcwh12321@gmail.com>
2019-08-17 15:01:35 +0800

    Level-3 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class Duke {
 
     public static void runDuke() {
-        String line = "    _____________________________________________";
+        String line = "    ____________________________________________________________";
         String currentCommand = "";
-        ArrayList<String> addedItems = new ArrayList<>();
+        ArrayList<Task> addedItems = new ArrayList<>();
 
         System.out.println(line);
         System.out.println("     Hello! I'm Duke");
@@ -16,18 +16,28 @@ public class Duke {
         while (!currentCommand.equals("bye")) {
             currentCommand = scanner.nextLine();
             if (!currentCommand.equals("bye")) {
-                if (!currentCommand.equals("list")) {
-                    addedItems.add(currentCommand);
+
+                if (currentCommand.equals("list")) {
+                    System.out.println(line);
+                    System.out.println("     Here are the tasks in your list:");
+                    for (int i = 0; i < addedItems.size(); i++) {
+                        System.out.println("     " + (i + 1) + "." + addedItems.get(i));
+                    }
+                    System.out.println(line);
+                } else if (currentCommand.substring(0, 4).equals("done")) {
+                    int taskNumber = Integer.parseInt(currentCommand.substring(5)) - 1;
+                    addedItems.get(taskNumber).markAsDone();
+                    System.out.println(line);
+                    System.out.println("     Nice! I've marked this task as done:");
+                    System.out.println("       " + addedItems.get(taskNumber));
+                    System.out.println(line);
+                } else {
+                    addedItems.add(new Task(currentCommand));
                     System.out.println(line);
                     System.out.println("     added: " + currentCommand);
                     System.out.println(line);
-                } else {
-                    System.out.println(line);
-                    for (int i = 0; i < addedItems.size(); i++) {
-                        System.out.println("     " + (i + 1) + ". " + addedItems.get(i));
-                    }
-                    System.out.println(line);
                 }
+
             }
         }
 

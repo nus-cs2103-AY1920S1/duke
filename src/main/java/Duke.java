84a546b5df5924b7f68cb1f0e3f0294850caf9d9
Larry Law <larrylaw97@gmail.com>
2019-08-18 10:05:08 +0800

    fix: duke instance of static

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -78,6 +78,41 @@ public class Duke {
         System.out.println(exitMessage);
         sc.close();
     }
+
+    public void run() {
+        Scanner sc = new Scanner(System.in);
+        boolean exit = false;
+        String exitMessage = "Bye. Hope to see you again soon!";
+        String doneMessage = "Nice! I've marked this task as done:";
+        List<Task> l = new ArrayList<>();
+        while (!exit) {
+            String input = sc.next();
+            switch (input) {
+                case "list":
+                    for (int i = 0; i < l.size(); i++) {
+                        Task t = l.get(i);
+                        System.out.println(String.format("%d.%s", i + 1, t));
+                    }
+                    break;
+                case "bye":
+                    exit = true;
+                    break;
+                case "done":
+                    int i = sc.nextInt();
+                    l.get(i - 1).setDone(true);
+                    System.out.println(doneMessage);
+                    System.out.println(l.get(i-1));
+                    break;
+                default:
+                    input += sc.nextLine();
+                    Task t = new Task(input);
+                    l.add(t);
+                    System.out.println("added: " + input);
+            }
+        }
+        System.out.println(exitMessage);
+        sc.close();
+    }
     
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -89,6 +124,8 @@ public class Duke {
         System.out.println("What can I do for you?");
         // Duke.levelOne();
         // Duke.levelTwo();
-        Duke.levelThree();
+        // Duke.levelThree();
+        Duke d = new Duke();
+        d.run();
     }
 }

258c0ad81847cdc6fa0d931865b02d7c876a044e
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2019-08-18 19:19:02 +0800

    Add ability to echo user command and handle exit command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,47 @@
+import java.util.Scanner;
+
 public class Duke {
 
     public static final String LONG_LINE = "____________________________________________________________";
     public static final String GREETING = "Hello! I'm Duke\nWhat can I do for you?";
+    public static final String BYE_STR = "Bye. Hope to see you again soon!";
+    public static final String BYE_CMD = "bye";
 
     public static void main(String[] args) {
         printGreeting();
+
+        Scanner input = new Scanner(System.in);
+        String command = input.next();
+
+        while (!command.equals(BYE_CMD)) {
+            processCommand(command);
+            command = input.next();
+        }
+
+        input.close();
+        printGoodbye();
+    }
+
+    public static void processCommand(String command) {
+        System.out.println(
+            LONG_LINE
+            + "\n"
+            + command
+            + "\n"
+            + LONG_LINE
+            + "\n"
+        );
+    }
+
+    public static void printGoodbye() {
+        System.out.println(
+            LONG_LINE
+            + "\n"
+            + BYE_STR
+            + "\n"
+            + LONG_LINE
+            + "\n"
+        );
     }
 
     public static void printGreeting() {

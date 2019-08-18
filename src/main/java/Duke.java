3cbff5307bad964345b6f8856c75f1ecde51136d
dalsontws <dalsontws@gmail.com>
2019-08-18 16:58:56 +0800

    Level-1 Completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,36 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        //Store command-line input as String
+        String input = "";
+
+        //Read command-line input with Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //Initial opening introduction and prompt for user input
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello! I'm Duke");
+        System.out.println("     What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("");
+
+        //Check for last statement
+        while (!input.equals("bye")) {
+
+            // Get entire line of input from command-line
+            input = scanner.nextLine();
+
+            //Echo all commands entered by user, except "bye", exit loop
+            if (input.equals("bye")) break;
+            System.out.println("    ____________________________________________________________");
+            System.out.println("     " + input);
+            System.out.println("    ____________________________________________________________");
+            System.out.println("");
+        }
+        //Closing statement
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
     }
 }

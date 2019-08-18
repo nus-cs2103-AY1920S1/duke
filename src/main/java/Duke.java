de7092891fc5c126240232601b997488a7109167
Clarence Koh <ckoh.clarence.96@gmail.com>
2019-08-18 23:21:36 +0800

    add mark as done function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,26 +3,33 @@ import java.util.ArrayList;
 
 public class Duke {
     static final ArrayList<Task> listOfInputs = new ArrayList<>(100);
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        while(true) {
+        while (true) {
             Scanner input = new Scanner(System.in);
             String userInput = input.nextLine();
-            if(userInput.equals("bye")) {
+            if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (userInput.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
                 int counter = 0;
-                for(Task item : listOfInputs) {
+                for (Task item : listOfInputs) {
                     counter++;
                     System.out.println(counter + ".[" + item.getStatusIcon() + "] " + item);
                 }
             } else {
-                Task userTask = new Task(userInput);
-                System.out.println("added: " + userTask);
-                listOfInputs.add(userTask);
+                String[] task = userInput.split(" ");
+                if (task[0].equals("done")) {
+                    int taskNumber = Integer.parseInt(task[1]);
+                    listOfInputs.get(taskNumber - 1).markedAsDone();
+                } else {
+                    Task userTask = new Task(userInput);
+                    System.out.println("added: " + userTask);
+                    listOfInputs.add(userTask);
+                }
             }
         }
 

c6ebc81077643aa08311e45a81a662d91927071c
Russell Loh <e0310763@u.nus.edu>
2019-08-18 20:53:27 +0800

    Completed Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 public class Duke {
 
-    public static ArrayList<String> storage = new ArrayList<>();
+    public static ArrayList<Task> storage = new ArrayList<>();
+
     public static void main(String[] args) {
         /*
         String logo = " ____        _        \n"
@@ -17,19 +18,32 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        while(!input.equals("bye")) {
-            if(input.equals("list")){
+        while(!input.equals("bye")) { //Read input until 'bye' command is entered
+            if(input.contains("done")){
+                int taskNo = Integer.parseInt(
+                                input.replace("done", "")
+                                .replace(" ", "")); //Removing 'done' and empty spaces
+                Task task = storage.get(taskNo - 1); //Minus 1 because the displayed list starts at 1
+                task.markAsDone();
+
+                printOutput("Nice! I've marked this task as done: \n  " + task);
+            }
+            else if (input.equals("list")) {
                 String listOutput = "";
-                for(int i = 0; i < storage.size(); i++){
-                    listOutput += (i+1) + ". " + storage.get(i);
-                    if(i+1 != storage.size()){
+                for (int i = 0; i < storage.size(); i++) {
+                    //Get tasks
+                    Task task = storage.get(i);
+
+                    listOutput += (i + 1) + "." + task;
+
+                    if (i + 1 != storage.size()) {
                         listOutput += "\n";
                     }
                 }
                 printOutput(listOutput);
-            }
-            else {
-                storage.add(input);
+            } else {
+                Task newTask = new Task(input);
+                storage.add(newTask);
                 printOutput("added: " + input);
             }
             input = sc.nextLine();

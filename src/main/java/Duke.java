46075adf8ac699f7b1b7d0ee6f416894af0b0a80
Larry Law <larrylaw97@gmail.com>
2019-08-18 13:12:31 +0800

    refactor: duke static to instance

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,53 +3,57 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Duke {
+    List<Task> l;
+
+    public void list() {
+        for (int i = 0; i < l.size(); i++) {
+            Task t = l.get(i);
+            System.out.println(String.format("%d.%s", i + 1, t));
+        }
+    }
+
+    public void done(int i) {
+        String doneMessage = "Nice! I've marked this task as done:";
+        l.get(i - 1).setDone(true);
+        System.out.println(doneMessage);
+        System.out.println(l.get(i - 1));
+    }
+
+    public void add(String task) {
+        l.add(new Task(task));
+        System.out.println("added: " + task);
+    }
 
     public void run() {
         Scanner sc = new Scanner(System.in);
         boolean exit = false;
         String exitMessage = "Bye. Hope to see you again soon!";
-        String doneMessage = "Nice! I've marked this task as done:";
-        List<Task> l = new ArrayList<>();
+        l = new ArrayList<>();
         while (!exit) {
             String input = sc.next();
             switch (input) {
-                case "list":
-                    for (int i = 0; i < l.size(); i++) {
-                        Task t = l.get(i);
-                        System.out.println(String.format("%d.%s", i + 1, t));
-                    }
-                    break;
-                case "bye":
-                    exit = true;
-                    break;
-                case "done":
-                    int i = sc.nextInt();
-                    l.get(i - 1).setDone(true);
-                    System.out.println(doneMessage);
-                    System.out.println(l.get(i-1));
-                    break;
-                default:
-                    input += sc.nextLine();
-                    Task t = new Task(input);
-                    l.add(t);
-                    System.out.println("added: " + input);
+            case "list":
+                this.list();
+                break;
+            case "bye":
+                exit = true;
+                break;
+            case "done":
+                this.done(sc.nextInt());
+                break;
+            default:
+                this.add(input + sc.nextLine());
             }
         }
         System.out.println(exitMessage);
         sc.close();
     }
-    
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = " ____        _        \n" + "|  _ \\ _   _| | _____ \n" + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n" + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
-        // Duke.levelOne();
-        // Duke.levelTwo();
-        // Duke.levelThree();
         Duke d = new Duke();
         d.run();
     }

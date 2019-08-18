2713bfcf3bc2680d1b2cf5b68eac75003a39ae06
kwekke <kwekkeeen@gmail.com>
2019-08-19 02:25:57 +0800

    Duke.java add Level2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,7 @@ public class Duke {
 
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     String[] token;
+
     String logo = " ____        _        \n"
             + "|  _ \\ _   _| | _____ \n"
             + "| | | | | | | |/ / _ \\\n"
@@ -12,14 +13,35 @@ public class Duke {
             + "|____/ \\__,_|_|\\_\\___|\n";
     String underline = "____________________________________________________________\n";
 
+    ArrayList<String> tasks = new ArrayList<String>();
+
     private void run() throws IOException {
         System.out.println(underline + "Hello from\n" + logo + "\nWhat can i do for you?\n" + underline);
 
         token = br.readLine().split(" ");
         while(!(token[0].equals("bye"))) {
-            System.out.println(underline + token[0] + "\n" + underline);
-            token = br.readLine().split(" ");
-        }
+            if(token[0].equals("list")) {
+                System.out.print(underline);
+                for(int i = 0 ; i < tasks.size() ; i++ ) {
+                    System.out.println( (i+1) + ". " + tasks.get(i));
+                }
+                System.out.print(underline);
+                token = br.readLine().split(" ");
+            } else {
+                StringBuilder task = new StringBuilder();
+                for( int i = 0 ; i < token.length ; i++) {
+                    if( i != (token.length - 1 ) ) {
+                        task.append(token[i]);
+                        task.append(" ");
+                    } else {
+                        task.append(token[i]);
+                    }
+                }
+                tasks.add(task.toString());
+                System.out.println(underline + "added: " + task + "\n" + underline);
+                token = br.readLine().split(" ");
+                }
+            }
         System.out.print(underline + "Bye. Hope to see you again soon!\n" + underline);
     }
 

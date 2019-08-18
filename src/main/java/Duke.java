9be4b0eefc4d239b0ed08f31aa694fe9b5c4bd91
linyutinglyt <lin_yuting@u.nus.edu>
2019-08-18 16:20:09 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -27,13 +27,47 @@ public class Duke {
             String command = sc.next();
 
             if (command.equals("done")) {
-                int index = Integer.valueOf(sc.next()) - 1;
+                if (!sc.hasNextInt()) {
+                    System.out.println(indent + line);
+                    System.out.println(indent + new NoTaskException());
+                    System.out.println(indent + line + "\n");
+                    continue;
+                }
+                int index = sc.nextInt() - 1;
+                if (index >= taskList.size()) {
+                    System.out.println(indent + line);
+                    System.out.println(indent + new NoTaskException());
+                    System.out.println(indent + line + "\n");
+                    continue;
+                }
                 Task t = taskList.get(index);
                 t.markAsDone();
                 System.out.println(indent + line);
                 System.out.println("     Nice! I've marked this task as done: ");
-                System.out.println("       [" + t.getStatusIcon() + "] " + t.getDescription());
+                System.out.println(indent + "  " + t);
                 System.out.println(indent + line + "\n");
+            } else if (command.equals("delete")) {
+                if (!sc.hasNextInt()) {
+                    System.out.println(indent + line);
+                    System.out.println(indent + new NoTaskException());
+                    System.out.println(indent + line + "\n");
+                    continue;
+                }
+                int index = sc.nextInt() - 1;
+                if (index >= taskList.size()) {
+                    System.out.println(indent + line);
+                    System.out.println(indent + new NoTaskException());
+                    System.out.println(indent + line + "\n");
+                    continue;
+                }
+                Task t = taskList.get(index);
+                taskList.remove(index);
+                System.out.println((indent + line));
+                System.out.println("     Noted. I've removed this task:");
+                System.out.println(indent + "  " + t);
+                System.out.println(indent + " Now you have "  + taskList.size() + " tasks in the list.");
+                System.out.println(indent + line + "\n");
+
             } else if (command.equals("bye")) {
                 System.out.println(bye);
             } else if (command.equals("list")) {
@@ -50,13 +84,48 @@ public class Duke {
                 String words = sc.nextLine();
 
                 if (command.equals("todo")) {
+                    if (words.split(" ").length == 0 || words.equals("")) {
+                        System.out.println(indent + line);
+                        System.out.println(indent + new EmptyCommandException("todo"));
+                        System.out.println(indent + line + "\n");
+                        continue;
+                    }
                     t = new Todo(words.trim());
                 } else if (command.equals("event")) {
+                    if (words.split(" ").length == 0 || words.equals("")) {
+                        System.out.println(indent + line);
+                        System.out.println(indent + new EmptyCommandException("event"));
+                        System.out.println(indent + line + "\n");
+                        continue;
+                    }
                     String[] details = words.split(" /at ");
+                    if (details.length == 1) {
+                        System.out.println(indent + line);
+                        System.out.println(indent + new NoDateException("event"));
+                        System.out.println(indent + line + "\n");
+                        continue;
+                    }
                     t = new Event(details[0].trim(), details[1].trim());
-                } else {
+                } else if (command.equals("deadline")){
+                    if (words.split(" ").length == 0  || words.equals("")) {
+                        System.out.println(indent + line);
+                        System.out.println(indent + new EmptyCommandException("deadline"));
+                        System.out.println(indent + line + "\n");
+                        continue;
+                    }
                     String[] details = words.split(" /by ");
+                    if (details.length == 1) {
+                        System.out.println(indent + line);
+                        System.out.println(indent + new NoDateException("deadline"));
+                        System.out.println(indent + line + "\n");
+                        continue;
+                    }
                     t = new Deadline(details[0].trim(), details[1].trim());
+                } else {
+                    System.out.println(indent + line);
+                    System.out.println(indent + new UnknownCommandExeption());
+                    System.out.println(indent + line + "\n");
+                    continue;
                 }
                 taskList.add(t);
                 System.out.println(indent + line);
--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,2 +1,48 @@
-public class DukeException {
+public class DukeException extends Exception {
+
+    public DukeException() {
+    }
+}
+
+class EmptyCommandException extends DukeException {
+    private String type;
+
+    public  EmptyCommandException(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        if (this.type.equals("event")) {
+            return " ☹ OOPS!!! The description of an event cannot be empty.";
+        } else {
+            return " ☹ OOPS!!! The description of a " + this.type + " cannot be empty.";
+        }
+    }
+}
+
+class UnknownCommandExeption extends DukeException {
+    @Override
+    public String toString() {
+        return " ☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
+    }
+}
+
+class NoDateException extends DukeException {
+    private String type;
+
+    public NoDateException(String type) {
+        this.type = type;
+    }
+    @Override
+    public String toString() {
+        return " ☹ OOPS!!! The " + this.type + " must have a date.";
+    }
+
+}
+
+class NoTaskException extends DukeException {
+    public String toString() {
+        return " ☹ OOPS!!! The task does not exist.";
+    }
 }

0540ee7c6f9b36a26641ff2bd9876fc97263b474
lye-jw <lyejianwen@gmail.com>
2019-08-18 18:39:41 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scan = new Scanner(System.in);
+        String logo = "      ____        _        \n"
+                + "     |  _ \\ _   _| | _____ \n"
+                + "     | | | | | | | |/ / _ \\\n"
+                + "     | |_| | |_| |   <  __/\n"
+                + "     |____/ \\__,_|_|\\_\\___|\n";
+        String separationLine = "    ____________________________________________________________";
+        System.out.println(separationLine + "\n" + logo + "\n     Hello! I'm Duke\n     What can I do for you?\n"
+                + separationLine + "\n");
+        String userInput = scan.nextLine();
+        while(!"bye".equals(userInput)) {
+            System.out.println(separationLine + "\n     " + userInput + "\n" + separationLine + "\n");
+            userInput = scan.nextLine();
+        }
+        System.out.println(separationLine + "\n     Bye. Hope to see you again soon!\n" + separationLine);
     }
 }

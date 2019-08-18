82062521b8d53975aeb943459eec7e2ee07b19d7
seahlynn <e0324409@u.nus.edu>
2019-08-18 23:28:10 +0800

    increment 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,19 +6,23 @@ public class Duke {
         System.out.println("Hello! I'm Duke" + "\n" + "What can I do for you?");
         Scanner sc = new Scanner(System.in);
         ArrayList<String> taskList = new ArrayList<>();
+        ArrayList<String> checkList = new ArrayList<>();
 
         while(!(command = sc.nextLine()).equals("bye")) {
-            if(!command.equals("list")) {
-                taskList.add(command);
-                System.out.println("added: " + command);
-            } else {
-                int num = 1;
+            if(command.equals("list")) {
                 for(int i=0; i<taskList.size(); i++) {
-                    System.out.println(num + ". " + taskList.get(i));
-                    num++;
+                    System.out.println((i+1) + ".[" + checkList.get(i) + "] " + taskList.get(i));
                 }
+            } else if((command.split(" ")[0]).equals("done")) {
+                int numChange = Integer.parseInt(command.split(" ")[1]) - 1;
+                checkList.set(numChange, "✓");
+                System.out.println("Nice! I've marked this task as done:" + "\n" +
+                        "[✓] " + taskList.get(numChange));
+            } else {
+                taskList.add(command);
+                checkList.add("✗");
+                System.out.println("added: " + command);
             }
-
         }
 
         System.out.println("Bye. Hope to see you again soon!");

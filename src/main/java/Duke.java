e20d8d2609f9a9d9c8cc809bc7cefd8ec5679e35
SoilingRogue <yuan.xinran.stanley@gmail.com>
2019-08-18 22:47:57 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean bye = false;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String line = "    ____________________________________________________________";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println(line + "\n" + "     Hello! I'm Duke\n     What can I do for you?\n" + line);
+
+        while (!bye) {
+            String command = sc.next();
+            if (!command.equals("bye")) {
+                System.out.println(line + "\n     " + command + "\n" + line);
+            } else {
+                bye = true;
+                System.out.println(line + "\n" + "     Bye. Hope to see you again soon!\n" + line);
+            }
+        }
     }
 }

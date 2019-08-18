0afa2e9a29dc64e5df8d158803501512ae1aac92
Oscar-B-Liang <dedicus545@gmail.com>
2019-08-18 19:50:36 +0800

    Starting-off

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
+    private void draw_line() {
+        for (int i = 0; i < 100; i++) {
+            System.out.print("_");
+        }
+        System.out.print("\n");
+    }
+
+    private void repeat() {
+        Scanner sc = new Scanner(System.in);
+        String next_string = sc.next();
+        draw_line();
+        System.out.println(next_string);
+        draw_line();
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"

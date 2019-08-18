eb4b6a95657fe0f15d1cebe7330c7ec5b40e8f61
lye-jw <lyejianwen@gmail.com>
2019-08-18 19:03:45 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -11,9 +12,20 @@ public class Duke {
         String separationLine = "    ____________________________________________________________";
         System.out.println(separationLine + "\n" + logo + "\n     Hello! I'm Duke\n     What can I do for you?\n"
                 + separationLine + "\n");
+
+        ArrayList<String> inputStore = new ArrayList<>(100);
         String userInput = scan.nextLine();
         while(!"bye".equals(userInput)) {
-            System.out.println(separationLine + "\n     " + userInput + "\n" + separationLine + "\n");
+            if ("list".equals(userInput)) {
+                System.out.println(separationLine);
+                for (String s : inputStore) {
+                    System.out.println("     " + (inputStore.indexOf(s) + 1) + ". " + s);
+                }
+                System.out.println(separationLine);
+            } else {
+                inputStore.add(userInput);
+                System.out.println(separationLine + "\n     added: " + userInput + "\n" + separationLine + "\n");
+            }
             userInput = scan.nextLine();
         }
         System.out.println(separationLine + "\n     Bye. Hope to see you again soon!\n" + separationLine);

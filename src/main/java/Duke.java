80e4249e45820829b1d9d2d40d4f9866f56eaca1
Cheong Siu Hong <cheongsiuhong@gmail.com>
2019-08-19 00:57:06 +0800

    Added basic echo for level 1.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    private Scanner scanner;
+    private boolean running;
+
+    public Duke() {
+        scanner = new Scanner(System.in);
+        running = true;
+    }
+
+    public void start() {
+        String input;
+        say("Hello! I'm Duke\nWhat can I do for you?");
+        while (running) {
+            input = scanner.next();
+            if (input.equals("bye")) {
+                exit();
+            } else {
+                say(input);
+            }
+        }
+    }
+
+    private void exit() {
+        this.running = false;
+        say("Bye. Hope to see you again soon!");
+    }
+
+    private void say(String sequence) {
+        String indent = sequence.replaceAll("(?m)^", "\t");
+        System.out.printf(
+                "       ____________________________________________________________\n     %s\n       ____________________________________________________________\n",
+                indent);
     }
 }

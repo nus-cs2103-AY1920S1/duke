cc1cd651d2f4d5edede990dc12d48954083b73c4
seahlynn <e0324409@u.nus.edu>
2019-08-18 22:46:17 +0800

    increment 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,9 +5,20 @@ public class Duke {
         String command;
         System.out.println("Hello! I'm Duke" + "\n" + "What can I do for you?");
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> taskList = new ArrayList<>();
 
         while(!(command = sc.nextLine()).equals("bye")) {
-            System.out.println(command);
+            if(!command.equals("list")) {
+                taskList.add(command);
+                System.out.println("added: " + command);
+            } else {
+                int num = 1;
+                for(int i=0; i<taskList.size(); i++) {
+                    System.out.println(num + ". " + taskList.get(i));
+                    num++;
+                }
+            }
+
         }
 
         System.out.println("Bye. Hope to see you again soon!");

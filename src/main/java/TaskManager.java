2764d992389140d3abe91745e1f4bf96af563a1d
Kyzure <kyzurene@gmail.com>
2019-08-18 11:52:44 +0800

    Level 2

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,21 +1,39 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 class TaskManager {
   private Scanner sc;
+  private ArrayList<String> list;
 
   public TaskManager() {
     sc = new Scanner(System.in);
+    list = new ArrayList<String>();
   }
 
   public void initializeTasks() {
     while(sc.hasNextLine()) {
       String input = sc.nextLine();
+      instantiateTask(input);
       if(input.equals("bye")) {
-        System.out.println("Bye. Hope to see you again soon!");
-        return;
-      } else {
-        System.out.println(input);
+        break;
       }
+    }
+  }
+
+  private void instantiateTask(String input) {
+    switch(input) {
+      case "list" :
+        for(int i = 0; i < list.size(); i++) {
+          System.out.println((i + 1) + ". " + list.get(i));
+        }
+        break;
+      case "bye" :
+        System.out.println("Bye. Hope to see you again soon!");
+        break;
+      default :
+        list.add(input);
+        System.out.println("Added: " + input);
+        break;
     }
   }
 }

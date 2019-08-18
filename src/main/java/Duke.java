31173afb090c9633db95a5a85d35fbcba656d654
Lawnce Goh <lawnce@gmail.com>
2019-08-19 02:57:08 +0800

    Level-3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,26 +5,43 @@ public class Duke {
 
     public static void greet() {
         String input;
-        ArrayList<String> arr = new ArrayList<>();
+        int pos;
+        ArrayList<Task> arr = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         while(sc.hasNext()) {
-            input = sc.nextLine();
-            if (input.equals("bye")) {
-                System.out.println("_____________________________________________________");
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("_____________________________________________________");
-                return;
-            } else if (input.equals("list")) {
-                System.out.println("_____________________________________________________");
-                for (int i = 0; i < arr.size(); i++) {
-                    System.out.println(i+1 + ". " + arr.get(i));
-                }
-                System.out.println("_____________________________________________________");
-            } else {
-                System.out.println("_____________________________________________________");
-                arr.add(input);
-                System.out.println("added: " + input);
-                System.out.println("_____________________________________________________");
+            input = sc.next();
+            switch (input) {
+                case ("bye") :
+                    System.out.println("_____________________________________________________");
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println("_____________________________________________________");
+                    return;
+
+                case ("list") :
+                    System.out.println("_____________________________________________________");
+                    System.out.println("Here are the tasks in your list:");
+
+                    for (int i = 0; i < arr.size(); i++) {
+                        System.out.println(i+1 + "." + arr.get(i));
+                    }
+                    System.out.println("_____________________________________________________");
+                    break;
+
+                case ("done") :
+                    pos = sc.nextInt();
+                    arr.get(pos - 1).markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(arr.get(pos - 1));
+                    break;
+
+                default :
+                    System.out.println("_____________________________________________________");
+                    String rest = sc.nextLine();
+                    input = input + rest;
+                    Task t = new Task(input);
+                    arr.add(t);
+                    System.out.println("added: " + input);
+                    System.out.println("_____________________________________________________");
             }
         }
     }

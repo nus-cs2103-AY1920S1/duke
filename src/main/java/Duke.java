aa1691b9c3fd61d7b51ada11afdc2d7781cb8d49
tohcejasmine <e0104958@u.nus.edu>
2019-08-19 00:00:41 +0800

    level 2: add, list (able to add, store and list tasks)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,12 @@ import java.util.Scanner;
 
 public class Duke {
     //Constructor
+    private String[] taskList;
+    private int numTask;
     public Duke() {
+        //Fixed size array
+        taskList = new String[100];
+        numTask = 0;
     }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -19,25 +24,44 @@ public class Duke {
         duke.run();
     }
 
-    boolean inProgram;
+    private boolean inProgram;
     private void run() {
         //Initialise Scanner object
         Scanner input = new Scanner(System.in);
         //Continually receive commands until exit
         inProgram = true;
         while (inProgram) {
-            String userCommand = input.next();
-            if (userCommand.equals("bye")) {
-                System.out.println("-----");
-                System.out.println("    " + "Bye. Hope to see you again soon!");
-                System.out.println("-----");
-                inProgram = false;
-            } else {
-                System.out.println("-----");
-                System.out.println("    " + userCommand);
-                System.out.println("-----");
+            String userCommand = input.nextLine();
+            switch (userCommand) {
+                case ("bye"):
+                    display("Bye. Hope to see you again soon!");
+                    //Exit program
+                    inProgram = false;
+                    break;
+                case ("list"):
+                    if (numTask == 0) {
+                        display("You have no tasks yet!");
+                    } else {
+                        System.out.println("-----");
+                        //List out added tasks
+                        for (int i = 0; i < numTask; i++) {
+                            System.out.println((i+1) + ". " + taskList[i]);
+                        }
+                        System.out.println("-----");
+                    }
+                    break;
+                default:
+                    taskList[numTask] = userCommand;
+                    numTask++;
+                    display("added: " + userCommand);
             }
         }
+    }
+
+    private void display(String response) {
+        System.out.println("-----");
+        System.out.println("    " + response);
+        System.out.println("-----");
     }
 
 

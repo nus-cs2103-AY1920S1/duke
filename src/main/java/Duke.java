b3becbdd52abd1e9ac1498bebb8b8030f38554ef
Kenneth Fung <fungchenyu@rocketmail.com>
2019-08-18 12:56:59 +0800

    Implement Duke Level 5 Errors handled: - empty description for done, todo, deadline, and event - invalid command - incorrect format for deadline and event time - invalid task number for done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,10 +9,10 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
 
         Task[] tasks = new Task[100];
-        int num_tasks = 0;
+        int numTasks = 0;
 
-        String input = "";
-        String command = "";
+        String input;
+        String command;
 
         while (true) {
             input = sc.nextLine();
@@ -24,26 +23,54 @@ public class Duke {
 
             switch (command) {
                 case "list":
-                    String task = "";
+                    String task;
                     // print all tasks in list
                     System.out.println("Here are the task(s) in your list:");
-                    for (int i = 0; i < num_tasks; i++) {
+                    for (int i = 0; i < numTasks; i++) {
                         task = String.format("%d.%s", (i + 1), tasks[i]);
                         System.out.println(task);
                     }
+                    if (numTasks == 0) {
+                        System.out.println("There are no tasks in your list right now.");
+                    }
                     break;
                 case "done":
-                    int task_number = Integer.parseInt(input.split(" ")[1]);
-                    tasks[task_number - 1].markDone();
+                    int taskNumber;
+                    try {
+                        taskNumber = Integer.parseInt(input.split(" ")[1]);
+                    } catch (Exception e) {
+                        System.out.println("Error: Please indicate the task's number to mark as done. e.g. 'done 1'");
+                        continue;
+                    }
+                    try {
+                        tasks[taskNumber - 1].markDone();
+                    } catch (Exception e) {
+                        System.out.println("Error: Task number invalid.");
+                        continue;
+                    }
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(String.format("  %s", tasks[task_number - 1]));
+                    System.out.println(String.format("  %s", tasks[taskNumber - 1]));
                     break;
                 case "todo":
                 case "deadline":
                 case "event":
-                    String task_description = input.split(" ", 2)[1];
-                    AddTask(command, task_description, tasks, num_tasks);
-                    num_tasks++;
+                    String taskDescription;
+                    try {
+                        taskDescription = input.split(" ", 2)[1];
+                    } catch (Exception e) {
+                        System.out.println("Error: The description of a " + command + " cannot be empty.");
+                        continue;
+                    }
+                    try {
+                        AddTask(command, taskDescription, tasks, numTasks);
+                    } catch (Exception e) {
+                        System.out.println(e);
+                        continue;
+                    }
+                    numTasks++;
+                    break;
+                default:
+                    System.out.println("I'm sorry, but I don't know what that means :(");
                     break;
             }
         }
@@ -51,27 +78,33 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    private static void AddTask(String type, String description, Task[] tasks, int num_tasks) {
-        Task new_task = null;
+    private static void AddTask(String type, String description, Task[] tasks, int numTasks) throws DukeException {
+        Task newTask = null;
 
         switch (type) {
             case "todo":
-                new_task = new ToDo(description);
+                newTask = new ToDo(description);
                 break;
             case "deadline":
                 String[] descriptionDeadline = description.split(" /by ", 2);
-                new_task = new Deadline(descriptionDeadline[0], descriptionDeadline[1]);
+                if (descriptionDeadline.length < 2) {
+                    throw new DukeException("Deadline format incorrect, should be e.g. do /by time");
+                }
+                newTask = new Deadline(descriptionDeadline[0], descriptionDeadline[1]);
                 break;
             case "event":
                 String[] descriptionTime = description.split(" /at ", 2);
-                new_task = new Event(descriptionTime[0], descriptionTime[1]);
+                if (descriptionTime.length < 2) {
+                    throw new DukeException("Event format incorrect, should be e.g. event /at time");
+                }
+                newTask = new Event(descriptionTime[0], descriptionTime[1]);
                 break;
         }
 
-        tasks[num_tasks] = new_task;
+        tasks[numTasks] = newTask;
         String message = String.format(
                 "Got it. I've added this task:\n  %s\nNow you have %d task(s) in the list.",
-                new_task, num_tasks + 1
+                newTask, numTasks + 1
         );
         System.out.println(message);
     }

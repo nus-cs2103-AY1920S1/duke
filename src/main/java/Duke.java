cfacaa95b0c82efa8e3c360c5457c66c81f10cb0
robytanama <tanamaroby@gmail.com>
2019-08-18 18:36:49 +0800

    Increment Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,24 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String lines = "    ____________________________________________________________";
+        System.out.println(lines);
+        System.out.println("     Hello! I'm Duke");
+        System.out.println("     What can I do for you?");
+        System.out.println(lines);
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            String inputMessage = sc.nextLine();
+            System.out.println(lines);
+            if (!inputMessage.equals("bye")) {
+                System.out.println("     " + inputMessage);
+                System.out.println(lines);
+                System.out.println();
+                continue;
+            }
+            System.out.println("     Bye. Hope to see you again soon!");
+            System.out.println(lines);
+            System.exit(0);
+        }
     }
 }

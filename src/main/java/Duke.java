45ae2b4504e5507dd81ff98166ce4d3ff37d18d1
joloong <joelloong@gmail.com>
2019-08-18 17:58:50 +0800

    Complete Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,15 +16,15 @@ public class Duke {
 
         String textInput = sc.nextLine();
         while (!textInput.equals("bye")) {
-            if (textInput.equals("list")) {
-                String isPlural = counter == 1 ? "is" : "are";
-                String taskIfPlural = counter == 1 ? "task" : "tasks";
-                System.out.println("Here " + isPlural + " the " + taskIfPlural + " in your list:");
-                for (int i = 1; i <= counter; i++) {
-                    System.out.println(i + "." + task[i - 1]);
-                }
-            } else {
-                if (textInput.startsWith("done")) {
+            try {
+                if (textInput.equals("list")) {
+                    String isPlural = counter == 1 ? "is" : "are";
+                    String taskIfPlural = counter == 1 ? "task" : "tasks";
+                    System.out.println("Here " + isPlural + " the " + taskIfPlural + " in your list:");
+                    for (int i = 1; i <= counter; i++) {
+                        System.out.println(i + "." + task[i - 1]);
+                    }
+                } else if (textInput.startsWith("done")) {
                     int completedIndex = Integer.parseInt(textInput.replaceFirst("done ", "")) - 1;
                     Task markAsDoneTask = task[completedIndex];
                     markAsDoneTask.markAsDone();
@@ -32,39 +32,49 @@ public class Duke {
                     System.out.println("Nice! I've marked this task as done:");
                     System.out.println(markAsDoneTask);
                 } else if (textInput.startsWith("todo")) {
+                    if (textInput.equals("todo") || textInput.equals("todo ")) {
+                        throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
+                    }
                     String description = textInput.replaceFirst("todo ", "");
                     task[counter] = new Todo(description);
-
-                    System.out.println("Got it. I've added this task:\n" + task[counter]);
-                    counter++;
-                    String taskIfPlural = counter == 1 ? "task" : "tasks";
-                    System.out.println("Now you have " + counter + " " + taskIfPlural + " in the list.");
+                    counter = (new Duke()).printAddedTask(task, counter);
                 } else if (textInput.startsWith("deadline")) {
+                    if (textInput.equals("deadline") || textInput.equals("deadline ")) {
+                        throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
+                    }
                     String removeTaskWord = textInput.replaceFirst("deadline ", "");
                     String[] taskSplit = removeTaskWord.split(" /by ");
                     task[counter] = new Deadline(taskSplit[0], taskSplit[1]);
-
-                    System.out.println("Got it. I've added this task:\n" + task[counter]);
-                    counter++;
-                    String taskIfPlural = counter == 1 ? "task" : "tasks";
-                    System.out.println("Now you have " + counter + " " + taskIfPlural + " in the list.");
+                    counter = (new Duke()).printAddedTask(task, counter);
                 } else if (textInput.startsWith("event")) {
+                    if (textInput.equals("event") || textInput.equals("event ")) {
+                        throw new DukeException("OOPS!!! The description of a event cannot be empty.");
+                    }
+
                     String removeTaskWord = textInput.replaceFirst("event ", "");
                     String[] taskSplit = removeTaskWord.split(" /at ");
                     task[counter] = new Event(taskSplit[0], taskSplit[1]);
-
-                    System.out.println("Got it. I've added this task:\n" + task[counter]);
-                    counter++;
-                    String taskIfPlural = counter == 1 ? "task" : "tasks";
-                    System.out.println("Now you have " + counter + " " + taskIfPlural + " in the list.");
+                    counter = (new Duke()).printAddedTask(task, counter);
+                } else {
+                    throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
+            } catch (DukeException e) {
+                System.err.println(e);
             }
-
             textInput = sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
 
         // Close the scanner
         sc.close();
+
+    }
+
+    private int printAddedTask(Task[] task, int counter) {
+        System.out.println("Got it. I've added this task:\n" + task[counter]);
+        counter++;
+        String taskIfPlural = counter == 1 ? "task" : "tasks";
+        System.out.println("Now you have " + counter + " " + taskIfPlural + " in the list.");
+        return counter;
     }
 }

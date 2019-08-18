0e3667cd0ecf6f268cd6b4a86931ceb6800c5fd1
jonchan51 <32188705+jonchan51@users.noreply.github.com>
2019-08-18 23:50:54 +0800

    Configure Gradle

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,9 @@
 public class Duke {
+
+    /**
+     * Main function to start the Duke app.
+     * @param args the setup arguments
+     */
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"

b5d06b60dc56f0f34c9ef23012a0401fbc34bc64
SQwQ <33057619+SQwQ@users.noreply.github.com>
2019-08-18 20:22:29 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,12 +2,15 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo + "What can I do for you?\n");
+
+         */
+        System.out.println("Hello I'm Duke" + "What can I do for you?\n");
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
             String curr = sc.nextLine();

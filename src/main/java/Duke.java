b2e2f941a321179e19b226432a66a71fd5629031
joloong <joelloong@gmail.com>
2019-08-18 18:26:03 +0800

    Change from array to ArrayList

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
@@ -9,7 +10,7 @@ public class Duke {
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Task[] task = new Task[100];
+        ArrayList<Task> task = new ArrayList<>();
         int counter = 0;
 
         System.out.println("Hello I'm Duke\n" + "What can I do for you?");
@@ -22,11 +23,11 @@ public class Duke {
                     String taskIfPlural = counter == 1 ? "task" : "tasks";
                     System.out.println("Here " + isPlural + " the " + taskIfPlural + " in your list:");
                     for (int i = 1; i <= counter; i++) {
-                        System.out.println(i + "." + task[i - 1]);
+                        System.out.println(i + "." + task.get(i - 1));
                     }
                 } else if (textInput.startsWith("done")) {
                     int completedIndex = Integer.parseInt(textInput.replaceFirst("done ", "")) - 1;
-                    Task markAsDoneTask = task[completedIndex];
+                    Task markAsDoneTask = task.get(completedIndex);
                     markAsDoneTask.markAsDone();
 
                     System.out.println("Nice! I've marked this task as done:");
@@ -36,7 +37,7 @@ public class Duke {
                         throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
                     }
                     String description = textInput.replaceFirst("todo ", "");
-                    task[counter] = new Todo(description);
+                    task.add(new Todo(description));
                     counter = (new Duke()).printAddedTask(task, counter);
                 } else if (textInput.startsWith("deadline")) {
                     if (textInput.equals("deadline") || textInput.equals("deadline ")) {
@@ -44,7 +45,7 @@ public class Duke {
                     }
                     String removeTaskWord = textInput.replaceFirst("deadline ", "");
                     String[] taskSplit = removeTaskWord.split(" /by ");
-                    task[counter] = new Deadline(taskSplit[0], taskSplit[1]);
+                    task.add(new Deadline(taskSplit[0], taskSplit[1]));
                     counter = (new Duke()).printAddedTask(task, counter);
                 } else if (textInput.startsWith("event")) {
                     if (textInput.equals("event") || textInput.equals("event ")) {
@@ -53,7 +54,7 @@ public class Duke {
 
                     String removeTaskWord = textInput.replaceFirst("event ", "");
                     String[] taskSplit = removeTaskWord.split(" /at ");
-                    task[counter] = new Event(taskSplit[0], taskSplit[1]);
+                    task.add(new Event(taskSplit[0], taskSplit[1]));
                     counter = (new Duke()).printAddedTask(task, counter);
                 } else {
                     throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
@@ -70,8 +71,8 @@ public class Duke {
 
     }
 
-    private int printAddedTask(Task[] task, int counter) {
-        System.out.println("Got it. I've added this task:\n" + task[counter]);
+    private int printAddedTask(ArrayList<Task> task, int counter) {
+        System.out.println("Got it. I've added this task:\n" + task.get(counter));
         counter++;
         String taskIfPlural = counter == 1 ? "task" : "tasks";
         System.out.println("Now you have " + counter + " " + taskIfPlural + " in the list.");

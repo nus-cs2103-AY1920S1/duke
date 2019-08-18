aee5eea03f4a585f5558aaa5261f2790c7d81746
CarbonGrid <nextgentechno@hotmail.com>
2019-08-19 01:51:40 +0800

    Added ToDos, Events, Deadlines & Improved Input Handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,7 +17,7 @@ public class Duke {
     }
 
     private void cout(String str) {
-        if(!str.endsWith("\n"))
+        if (!str.endsWith("\n"))
             str += '\n';
         System.out.println("    ____________________________________________________________\n     " +
                 str.replaceAll("\n(?!$)", "\n     ") +
@@ -42,16 +42,29 @@ public class Duke {
                     this.cout(sb.toString());
                     break;
                 default:
-                    if (instr.startsWith("done")) {
-                        Task t = this.ls.get(Integer.parseInt(instr.substring(5)));
-                        t.setDone();
-                        this.cout("Nice! I've marked this task as done:\n" + t);
-                    } else {
-                        Task t = new Task(instr);
+                    Task t = null;
+                    if (instr.startsWith("deadline ")) {
+                        String[] temp = instr.substring(9).split(" /by ");
+                        if(temp.length == 2)
+                            t = new Deadline(temp[0], temp[1]);
+                    } else if (instr.startsWith("event ")) {
+                        String[] temp = instr.substring(6).split(" /at ");
+                        if(temp.length == 2)
+                            t = new Event(temp[0], temp[1]);
+                    } else if (instr.startsWith("todo ")) {
+                        t = new Todo(instr.substring(5));
+                    }
+                    if (t != null) {
                         this.ls.add(t);
                         this.cout("Got it. I've added this task:\n  " +
                                 t +
                                 "Now you have " + (this.ls.size() - 1) + " tasks in the list.");
+                    }
+
+                    if (instr.startsWith("done ")) {
+                        t = this.ls.get(Integer.parseInt(instr.substring(5)));
+                        t.setDone();
+                        this.cout("Nice! I've marked this task as done:\n" + t);
                     }
                     break;
             }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,9 +1,9 @@
-public class Task {
+public abstract class Task {
     protected String description;
     protected boolean isDone;
 
     public Task(String description) {
-        this.description = description;
+        this.description = description.trim();
         this.isDone = false;
     }
 
@@ -17,6 +17,49 @@ public class Task {
 
     @Override
     public String toString() {
-        return "[" + this.getStatusIcon() + "] " + this.description + '\n';
+        return "[" + this.getStatusIcon() + "] " +
+                this.description;
+    }
+}
+
+class Deadline extends Task {
+
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by.trim();
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")\n";
+    }
+}
+
+class Event extends Task {
+
+    protected String at;
+
+    public Event(String description, String at) {
+        super(description);
+        this.at = at.trim();
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (at: " + this.at + ")\n";
+    }
+}
+
+class Todo extends Task {
+
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString() + '\n';
     }
 }

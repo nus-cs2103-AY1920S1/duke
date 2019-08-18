3f103f4a611f269f5ab9fe3c00ef8becb4f58dd4
Shuming <geshuming2@gmail.com>
2019-08-18 10:38:01 +0800

    Refactor: use switch case to handle input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,30 +2,45 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    protected static ArrayList<String> listOfInputs = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> listOfInputs = new ArrayList<>();
 
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
         while (sc.hasNextLine()) {
             String input = sc.nextLine();
+
             if (input.equals("bye")) {
                 break;
-            } else if (input.equals("list")) {
-                int index = 1;
-                for (String i : listOfInputs) {
-                    System.out.printf("%d. %s\n", index, i);
-                    index++;
-                }
-            } else {
-                listOfInputs.add(input);
-                System.out.println(input);
+            }
+
+            switch (input) {
+            case "list":
+                handleList();
+                break;
+            default:
+                handleInput(input);
+                break;
             }
         }
 
         sc.close();
 
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static void handleList() {
+        int index = 1;
+        for (String i : listOfInputs) {
+            System.out.printf("%d. %s\n", index, i);
+            index++;
+        }
+    }
+
+    private static void handleInput(String input) {
+        listOfInputs.add(input);
+        System.out.println(input);
     }
 }

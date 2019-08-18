836e9675270fa79aa08c87d5f51ef65ba2235db6
weiboon <lumweiboon@gmail.com>
2019-08-18 18:37:41 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 public class Duke {
     public static void main(String[] args) {
     	Scanner scanner = new Scanner(System.in);
-    	String[] itemList = new String[100];
-    	int itemCounter = 0;
+    	Task[] taskArray = new Task[100];
+    	int taskCounter = 0;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -13,29 +13,72 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         String userInput = scanner.nextLine();
-        while (!userInput.equals("bye")) {
+        String[] splitUserInput = userInput.split(" ");
+        String command = splitUserInput[0];
+
+        while (!command.equals("bye")) {
         	if (userInput.equals("list")) {
-        		String prettifiedList = listConverter(itemList);
+        		String prettifiedList = listConverter(taskArray);
         		System.out.println(prettifiedList);
+			} else if (command.equals("done")) {
+        		Integer taskNumber = Integer.parseInt(splitUserInput[1]);
+				String output = taskArray[taskNumber].markCompleted();
+				System.out.println(output);
 			} else {
-        		itemList[itemCounter] = userInput;
-        		itemCounter++;
+        		taskArray[taskCounter] = new Task(userInput);
+        		taskCounter++;
 				System.out.println("added: " + userInput);
 			}
         	userInput = scanner.nextLine();
+			splitUserInput = userInput.split(" ");
+			command = splitUserInput[0];
 		}
         System.out.print("Bye. Hope to see you again soon!");
     }
 
-    private static String listConverter(String[] list) {
+    private static String listConverter(Task[] list) {
     	StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < list.length; i++)
 		{
 			if (list[i] != null) {
-				sb.append(i + 1 + ". " + list[i]);
+				sb.append(i + 1 + ". " + list[i].toString());
 				sb.append("\n");
 			}
 		}
 		return sb.toString().trim();
+	}
+}
+
+class Task
+{
+	String taskName;
+	boolean doneStatus;
+
+	public Task(String taskName) {
+		this.taskName = taskName;
+		this.doneStatus = false;
+	}
+
+	public String markCompleted() {
+		this.doneStatus = true;
+		StringBuilder sb = new StringBuilder("Nice! I've marked this task as done:\n");
+		sb.append("    ");
+		sb.append("[✓]");
+		sb.append(" ");
+		sb.append(taskName);
+		return sb.toString();
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		if (doneStatus == true) {
+			sb.append("[✓]");
+		} else {
+			sb.append("[✗]");
+		}
+		sb.append(" ");
+		sb.append(taskName);
+		return sb.toString();
 	}
 }

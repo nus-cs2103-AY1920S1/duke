32adb28806c2168f3c29e761fafcef08003b8f76
Evon Dong Bing Bing <e0322896@u.nus.edu>
2019-08-18 17:20:11 +0800

    Added greet, echo, exit functions for level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if (!input.equals("bye")) {
+                echo(input);
+            } else {
+                exit();
+                break;
+            }
+        }
+    }
+
+    public static void greet() {
+        String message = "Hello! I'm Duke\n" +
+                "What can I do for you?";
+        System.out.println(message);
+    }
+
+    public static void echo(String s) {
+        System.out.println(s);
+    }
+
+    public static void exit() {
+        String byeMessage = "Bye. Hope to see you again soon!";
+        System.out.println(byeMessage);
     }
 }
+

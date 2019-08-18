24ad764f155709600db9d617769125cdfce3d5a2
Chen Hui <e0322956@u.nus.edu>
2019-08-18 23:32:30 +0800

    Implement Level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,7 +6,7 @@ public class Duke {
     private String LINES = "\n" + "____________________________________________________________" + "\n";
     private ArrayList list = new ArrayList<Task>();
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Duke duke = new Duke();
         duke.start();
     }
@@ -18,36 +18,58 @@ public class Duke {
 
     private void run() {
         String s = sc.nextLine();
-        while (!s.equals("bye")) {
-            if (s.equals("list")) {
-                // List task
-                System.out.print(LINES);
-                System.out.println("Here are the task in your list:");
-                for (int i = 0; i < list.size() - 1; i++) {
-                    System.out.println(i + 1 + ". " + list.get(i));
-                }
-                System.out.print(list.size() + ". " + list.get(list.size() - 1));
-                System.out.println(LINES);
-            } else {
-                String[] strArr = s.split(" ");
-                if (strArr[0].equals("done")) {
-                    // Done task
-                    ((Task) list.get(Integer.parseInt(strArr[1]) - 1)).markAsDone();
-                    System.out.println(display(
-                            "Nice! I've marked this task as done:\n"
-                                    + ((Task) list.get(Integer.parseInt(strArr[1]) - 1))
 
-                    ));
+        while (!s.equals("bye")) {
+            String[] strArr = s.split(" ");
+
+            if (s.equals("list")) {
+                list();
+
+            } else if (strArr[0].equals("done")) {
+                done(strArr);
+
+            } else {
+                Task task = new Task("");
+
+                if (strArr[0].equals("todo")) {
+                    task = new ToDo(s.substring(5));
+
+                } else if (strArr[0].equals("deadline")) {
+                    String[] temp = s.split("/by");
+                    task = new Deadline(temp[0].substring(9).trim(), temp[1].trim());
+
+                } else if (strArr[0].equals("event")) {
+                    String[] temp = s.split("/at");
+                    task = new Event(temp[0].substring(6).trim(), temp[1].trim());
                 } else {
-                    // Add task
-                    Task task = new Task(s);
-                    System.out.println(display("added: " + s));
-                    list.add(task);
+                    // Throw some exception here
                 }
+                list.add(task);
+                System.out.println(display("Got it. I've added this task:\n  " + task
+                        + "\nNow you have " + list.size() + " tasks in the list."));
             }
             s = sc.nextLine();
         }
         exit();
+    }
+
+    private void list() {
+        System.out.print(LINES);
+        System.out.println("Here are the task in your list:");
+        for (int i = 0; i < list.size() - 1; i++) {
+            System.out.println(i + 1 + "." + list.get(i));
+        }
+        System.out.print(list.size() + "." + list.get(list.size() - 1));
+        System.out.println(LINES);
+    }
+
+    private void done(String[] strArr) {
+        ((Task) list.get(Integer.parseInt(strArr[1]) - 1)).markAsDone();
+        System.out.println(display(
+                "Nice! I've marked this task as done:\n"
+                        + ((Task) list.get(Integer.parseInt(strArr[1]) - 1))
+
+        ));
     }
 
     private void exit() {

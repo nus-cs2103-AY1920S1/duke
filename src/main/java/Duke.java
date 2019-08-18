7254ff217ac45474fee32529b83524907ee850f4
Chow Jia Ying <chowjiaying211@gmail.com>
2019-08-18 15:18:26 +0800

    Handle Errors due to incorrect user inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -29,54 +29,67 @@ public class Duke {
 
         //while loop that takes in user inputs
         while (true) {
-            System.out.println(LINE);
-            //program runs this command when userCommand is bye
-            if (userCommand.equals("bye")) {
-                System.out.println(TABS + "Bye. Hope to see you again soon!");
+            try {
                 System.out.println(LINE);
-                break;
-            } else if (userCommand.equals("list")) {
-                System.out.println(TABS + "Here are the tasks in your list:");
-                //program displays all Tasks in taskList
-                String displayedMessage = "";
-                Integer index = 1;
-                for (Task taskItem : taskList) {
-                    displayedMessage += TABS + index.toString() + "." + taskItem + "\n";
-                    index++;
-                }
-                System.out.print(displayedMessage);
-            } else if (userCommand.startsWith("done")) {
-                //program marks a Task as done
-                System.out.println(TABS + "Nice! I've marked this task as done: ");
-                int taskNumber = Character.getNumericValue(userCommand.charAt(5));
-                Task taskCompleted = taskList.get(taskNumber - 1);
-                taskCompleted.markAsDone();
-                System.out.println(TABS + "  " + taskCompleted.toString());
-            } else {
-                //program checks what type of task it is
-                Task userTask;
-                if (userCommand.startsWith("todo")) {
-                    String task = userCommand.substring(5);
-                    userTask = new ToDo(task);
-                } else if (userCommand.startsWith("deadline")) {
-                    String task = userCommand.replaceFirst("deadline ", "");
-                    String[] taskInformation = task.split(" /by ");
-                    userTask = new Deadline(taskInformation[0], taskInformation[1]);
+                //program runs this command when userCommand is bye
+                if (userCommand.equals("bye")) {
+                    System.out.println(TABS + "Bye. Hope to see you again soon!");
+                    System.out.println(LINE);
+                    break;
+                } else if (userCommand.equals("list")) {
+                    System.out.println(TABS + "Here are the tasks in your list:");
+                    //program displays all Tasks in taskList
+                    String displayedMessage = "";
+                    Integer index = 1;
+                    for (Task taskItem : taskList) {
+                        displayedMessage += TABS + index.toString() + "." + taskItem + "\n";
+                        index++;
+                    }
+                    System.out.print(displayedMessage);
+                } else if (userCommand.startsWith("done")) {
+                    //program marks a Task as done
+                    System.out.println(TABS + "Nice! I've marked this task as done: ");
+                    int taskNumber = Character.getNumericValue(userCommand.charAt(5));
+                    Task taskCompleted = taskList.get(taskNumber - 1);
+                    taskCompleted.markAsDone();
+                    System.out.println(TABS + "  " + taskCompleted.toString());
                 } else {
-                    String task = userCommand.replaceFirst("event ", "");
-                    String[] taskInformation = task.split(" /at ");
-                    userTask = new Event(taskInformation[0], taskInformation[1]);
-                }
-                taskList.add(userTask);
-                System.out.println(TABS + "Got it. I've added this task: ");
-                System.out.println(TABS + "  " + userTask.toString());
+                    //program checks what type of task it is
+                    Task userTask;
+                    if (userCommand.startsWith("todo")) {
+                        String task = userCommand.replaceFirst("todo", "");
+                        if (task.isEmpty()) {
+                            throw new EmptyToDoDescriptionException("The description of a todo cannot be empty. ");
+                        } else {
+                            //removes space in the beginning
+                            task = task.replaceFirst(" ", "");
+                            userTask = new ToDo(task);
+                        }
+                    } else if (userCommand.startsWith("deadline")) {
+                        String task = userCommand.replaceFirst("deadline ", "");
+                        String[] taskInformation = task.split(" /by ");
+                        userTask = new Deadline(taskInformation[0], taskInformation[1]);
+                    } else if (userCommand.startsWith("event")) {
+                        String task = userCommand.replaceFirst("event ", "");
+                        String[] taskInformation = task.split(" /at ");
+                        userTask = new Event(taskInformation[0], taskInformation[1]);
+                    } else {
+                        throw new UnknownCommandException("I'm sorry, but I don't know what that means :-(");
+                    }
+                    taskList.add(userTask);
+                    System.out.println(TABS + "Got it. I've added this task: ");
+                    System.out.println(TABS + "  " + userTask.toString());
 
-                if (taskList.size() == 1) {
-                    System.out.printf("%sNow you have %d task in the list.\n", TABS, taskList.size());
-                } else {
-                    System.out.printf("%sNow you have %d tasks in the list.\n", TABS, taskList.size());
+                    if (taskList.size() == 1) {
+                        System.out.printf("%sNow you have %d task in the list.\n", TABS, taskList.size());
+                    } else {
+                        System.out.printf("%sNow you have %d tasks in the list.\n", TABS, taskList.size());
+                    }
                 }
+            } catch (DukeException ex) {
+                System.out.println(TABS + "☹ OOPS!!! " + ex.getMessage());
             }
+
             System.out.println(LINE);
             userCommand = sc.nextLine();
         }

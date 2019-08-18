cfa19dd8e5d8b52d29b2a698eb7d9891dab08b3b
chowyiyin <yiyin.chow@gmail.com>
2019-08-18 15:46:17 +0800

    Add Level 1 Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,41 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        drawLine();
+        greetUser();
+        drawLine();
+        Scanner input = new Scanner(System.in);
+        while (input.hasNext()) {
+            String userInput = input.next();
+            if (userInput.equals("bye")) {
+                sayBye();
+                input.close();
+                break;
+            } else {
+                echo(userInput);
+            }
+        }
+    }
+
+    public static void sayBye() {
+        drawLine();
+        System.out.println("\t Bye. Hope to see you again soon!");
+        drawLine();
+    }
+    public static void echo(String input) {
+        drawLine();
+        System.out.println("\t " + input);
+        drawLine();
+    }
+    public static void printHeading(String heading) {
+        System.out.println("\n" + heading);
+    }
+
+    public static void drawLine() {
+        System.out.println("\t---------------------------------------");
+    }
+
+    public static void greetUser() {
+        System.out.println("\tHello! I'm Duke\n\tWhat can I do for you?");
     }
 }

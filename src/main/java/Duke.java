17655a1f9f756b171e81c720a0f7e560ef74257a
Chen Hui <e0322956@u.nus.edu>
2019-08-18 22:36:29 +0800

    Implement Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 public class Duke {
     private Scanner sc = new Scanner(System.in);
     private String LINES = "\n" + "____________________________________________________________" + "\n";
-    private ArrayList list = new ArrayList<String>();
+    private ArrayList list = new ArrayList<Task>();
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         Duke duke = new Duke();
         duke.start();
     }
@@ -18,17 +18,32 @@ public class Duke {
 
     private void run() {
         String s = sc.nextLine();
-        while(!s.equals("bye")) {
+        while (!s.equals("bye")) {
             if (s.equals("list")) {
+                // List task
                 System.out.print(LINES);
+                System.out.println("Here are the task in your list:");
                 for (int i = 0; i < list.size() - 1; i++) {
                     System.out.println(i + 1 + ". " + list.get(i));
                 }
                 System.out.print(list.size() + ". " + list.get(list.size() - 1));
                 System.out.println(LINES);
             } else {
-                System.out.println(display("added: " + s));
-                list.add(s);
+                String[] strArr = s.split(" ");
+                if (strArr[0].equals("done")) {
+                    // Done task
+                    ((Task) list.get(Integer.parseInt(strArr[1]) - 1)).markAsDone();
+                    System.out.println(display(
+                            "Nice! I've marked this task as done:\n"
+                                    + ((Task) list.get(Integer.parseInt(strArr[1]) - 1))
+
+                    ));
+                } else {
+                    // Add task
+                    Task task = new Task(s);
+                    System.out.println(display("added: " + s));
+                    list.add(task);
+                }
             }
             s = sc.nextLine();
         }

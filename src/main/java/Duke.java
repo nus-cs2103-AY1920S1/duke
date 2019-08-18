f29e890ce8abbe78a392a690492f03091ec66e3c
Clarence Koh <ckoh.clarence.96@gmail.com>
2019-08-18 21:40:28 +0800

    adds completed increment level 2 functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class Duke {
+    static final ArrayList<String> listOfInputs = new ArrayList<>(100);
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         while(true) {
             Scanner input = new Scanner(System.in);
-            String userInput = input.next();
+            String userInput = input.nextLine();
             if(userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (userInput.equals("list")) {
+                int counter = 0;
+                for(String item : listOfInputs) {
+                    counter++;
+                    System.out.println(counter + ". " + item);
+                }
             } else {
-                System.out.println(userInput);
+                System.out.println("added: " + userInput);
+                listOfInputs.add(userInput);
             }
         }
 

189bc7e15b1297ff32c862a7cfd59feed59af556
Evon Dong Bing Bing <e0322896@u.nus.edu>
2019-08-18 17:23:46 +0800

    Allowed adding of tasks and added list functions for level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         greet();
+        String[] inputArr = new String[100];
         Scanner sc = new Scanner(System.in);
+        int i = 0;
         while (sc.hasNextLine()) {
             String input = sc.nextLine();
-            if (!input.equals("bye")) {
+            if(input.equals("list")) {
+                listItems(inputArr);
+            } else if (!input.equals("bye")) {
                 echo(input);
+                inputArr[i] = input;
+                i++;
             } else {
                 exit();
                 break;
@@ -22,12 +28,18 @@ public class Duke {
     }
 
     public static void echo(String s) {
-        System.out.println(s);
+        System.out.println("added: " + s);
     }
 
     public static void exit() {
         String byeMessage = "Bye. Hope to see you again soon!";
         System.out.println(byeMessage);
+    }
+
+    public static void listItems(String[] arr) {
+        for (int j = 0; (j < arr.length) && arr[j] != null; j++) {
+            System.out.println(j + 1 + ". " + arr[j]);
+        }
     }
 }
 

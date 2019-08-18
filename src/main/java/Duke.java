c807e030218efa38db6026c8f5cf7fa22093c5f9
Jiang Yuxin <jyx11011@gmail.com>
2019-08-18 12:39:02 +0800

    Add the ability to store commands and list commands when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -16,14 +17,22 @@ public class Duke {
         String question = "What can I do for you?";
         printBlock(greeting, question);
 
+        ArrayList<String> list = new ArrayList<>();
         Scanner in = new Scanner(System.in);
         while (true) {
             String command = in.nextLine();
             if (command.equals("bye")) {
                 printBlock("Bye. Hope to see you again soon!");
                 break;
+            } else if (command.equals("list")) {
+                String[] text = new String[list.size()];
+                for (int i = 0; i < list.size(); i++) {
+                    text[i] = (i + 1) + ". " + list.get(i);
+                }
+                printBlock(text);
             } else {
-                printBlock(command);
+                list.add(command);
+                printBlock("added: " + command);
             }
         }
     }

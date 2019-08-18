6cc6fa07881329d29e3489fff33bf5eac0d41100
Lim Kang Yee <kangyee2408@gmail.com>
2019-08-18 15:53:50 +0800

    Add delete and exception handling to delete. Add more exception handling for todo and done for incorrect user input which was not done in the previous level.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -14,7 +15,7 @@ public class Duke {
         System.out.println(greeting);
         Scanner sc = new Scanner(System.in);
         String command = sc.nextLine();
-        Task[] tasks = new Task[100];
+        ArrayList<Task> tasks = new ArrayList<>();
         int index = 0;
         while (!command.equals("bye")) {
             try {
@@ -22,8 +23,40 @@ public class Duke {
                     String error = "\u2639 OOPS!!! I'm sorry but I don't know what that means :-(\n";
                     throw new DukeException(error);
                 }
+                if (command.length() >= 6 && command.substring(0, 6).equals("delete")) { //for deleting task
+                    if (command.contains(" ")) { //throw for no task number and there is just trailing whitespaces
+                        String res = command.replace(" ", "");
+                        if (res.length() == 6) {
+                            String error = "\u2639 OOPS!!! Please input the task number you would like to delete.\n";
+                            throw new DukeException(error);
+                        }
+                    } else if (command.length() == 6) { // throw for no task number
+                        String error = "\u2639 OOPS!!! Please input the task number you would like to delete.\n";
+                        throw new DukeException(error);
+                    }
+                    int curr = Integer.parseInt(command.substring(7));
+                    if (index == 0) { //check if list has no task
+                        String error = "\u2639 OOPS!!! You do not have any tasks in your list.\n";
+                        throw new DukeException(error);
+                    } else if (curr > index) {//check if index is within list size
+                        String error = "\u2639 OOPS!!! You do not have that task in your list. Call 'list' to see all your tasks :-)\n";
+                        throw new DukeException(error);
+                    }
 
-                if (!command.equals("list") && !command.substring(0, 4).equals("done")) { //for adding new task
+
+                    String markAsDoneMsg = "Noted. I've removed this task:\n" +
+                            tasks.get(curr - 1);
+                    tasks.remove(curr - 1);
+                    System.out.println(markAsDoneMsg);
+                    index--;
+                    String statusOfList;
+                    if (index == 1) {
+                        statusOfList = "Now you have " + index + " task in the list.\n";
+                    } else {
+                        statusOfList = "Now you have " + index + " tasks in the list.\n";
+                    }
+                    System.out.println(statusOfList);
+                } else if (!command.equals("list") && !command.substring(0, 4).equals("done")) { //for adding new task
                     if (command.length() >= 8 && !command.substring(0, 4).equals("todo") && !command.substring(0, 5).equals("event") && !command.substring(0, 8).equals("deadline")) {
                         String error = "\u2639 OOPS!!! I'm sorry but I don't know what that means :-(\n";
                         throw new DukeException(error);
@@ -38,13 +71,22 @@ public class Duke {
                         if (command.length() == 4) { //check to throw for no description
                             String error = "\u2639 OOPS!!! The description of todo cannot be empty.\n";
                             throw new DukeException(error);
+                        } else if (!command.substring(4,5).equals(" ")) { //throw for no description and there is just trailing whitespaces
+                            String error = "\u2639 OOPS!!! Please input a whitespace between the command 'todo' and your task description for me to keep track of it correctly :-)\n";
+                            throw new DukeException(error);
+                        } else if (command.contains(" ")) {
+                            String res = command.replaceAll(" ", "");
+                            if (res.length() == 4) {
+                                String error = "\u2639 OOPS!!! The description of todo cannot be empty.\n";
+                                throw new DukeException(error);
+                            }
                         }
-                        tasks[index] = new Todo(command.substring(5));
+                        tasks.add(new Todo(command.substring(5)));
                     } else if (command.substring(0, 5).equals("event")) {
                        if (command.length() == 5) { //check to throw for no description
                            String error = "\u2639 OOPS!!! The description of event cannot be empty.\n";
                            throw new DukeException(error);
-                       } else if (!command.substring(5,6).equals(" ")) {
+                       } else if (!command.substring(5,6).equals(" ")) { //throw for no description and there is just trailing whitespaces
                            String error = "\u2639 OOPS!!! Please input a whitespace between the command 'event' and your task description for me to keep track of it correctly :-)\n";
                            throw new DukeException(error);
                        } else if (command.contains(" ")) {
@@ -61,14 +103,13 @@ public class Duke {
                            String error = "\u2639 OOPS!!! You would need to schedule a date and time duration for this event using '/at'.\n";
                            throw new DukeException(error);
                        }
-                        //check to throw for no time range???
                         String[] arr = command.split(" /at ", 2);
-                        tasks[index] = new Event(arr[0].substring(6), arr[1]);
+                        tasks.add(new Event(arr[0].substring(6), arr[1]));
                     } else if (command.substring(0, 8).equals("deadline")) {
                         if (command.length() == 8) { //check to throw for no description
                             String error = "\u2639 OOPS!!! The description of deadline cannot be empty.\n";
                             throw new DukeException(error);
-                        } else if (!command.substring(8, 9).equals(" ")) {
+                        } else if (!command.substring(8, 9).equals(" ")) { //throw for no description and there is just trailing whitespaces
                             String error = "\u2639 OOPS!!! Please input a whitespace between the command 'deadline' and your task description for me to keep track of it correctly :-)\n";
                             throw new DukeException(error);
                         } else if (command.contains(" ")) {
@@ -86,13 +127,13 @@ public class Duke {
                             throw new DukeException(error);
                         }
                         String[] arr = command.split(" /by ", 2);
-                        tasks[index] = new Deadline(arr[0].substring(9), arr[1]);
+                        tasks.add(new Deadline(arr[0].substring(9), arr[1]));
                     } else { //throw exception for wrong command
                         String error = "\u2639 OOPS!!! I'm sorry but I don't know what that means :-(\n";
                         throw new DukeException(error);
                     }
                     String commandMsg = "Got it. I've added this task:\n"
-                            + tasks[index];
+                            + tasks.get(index);
 
                     index++;
                     System.out.println(commandMsg);
@@ -104,20 +145,30 @@ public class Duke {
                     }
                     System.out.println(statusOfList);
                 } else if (command.substring(0, 4).equals("done")) { //for marking task as done
+                    if (command.contains(" ")) { //throw for no task number and there is just trailing whitespaces
+                        String res = command.replace(" ", "");
+                        if (res.length() == 4) {
+                            String error = "\u2639 OOPS!!! Please input the task number you would like to mark as done.\n";
+                            throw new DukeException(error);
+                        }
+                    } else if (command.length() == 4) { //throw for no task number
+                        String error = "\u2639 OOPS!!! Please input the task number you would like to mark as done.\n";
+                        throw new DukeException(error);
+                    }
                     int curr = Integer.parseInt(command.substring(5));
                     if (curr > index) {//check if index is within list size
                         String error = "\u2639 OOPS!!! You do not have that task in your list. Call 'list' to see all your tasks :-)\n";
                         throw new DukeException(error);
                     }
-                    tasks[curr - 1].markAsDone();
+                    tasks.get(curr - 1).markAsDone();
                     String markAsDoneMsg = "Nice! I've marked this task as done:\n" +
-                            "[" + tasks[curr - 1].getStatusIcon() + "] " + tasks[curr - 1].getDescription() + "\n";
+                            "[" + tasks.get(curr - 1).getStatusIcon() + "] " + tasks.get(curr - 1).getDescription() + "\n";
                     System.out.println(markAsDoneMsg);
                 } else { //for listing tasks out
                     String listMsg = "Here are the tasks in your list:";
                     System.out.println(listMsg);
                     for (int i = 0; i < index; i++) {
-                        Task task = tasks[i];
+                        Task task = tasks.get(i);
                         String taskMsg = (i + 1) + ". " + task;
                         System.out.println(taskMsg);
                     }

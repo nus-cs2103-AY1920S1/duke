ae5e7d8e0285cd0f1ec6575322dcb44af66444e0
choonx99 <choonx99@hotmail.com>
2019-08-18 22:11:11 +0800

    added greet, read input and goodbye functions for level1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("First commit");
+        greet();
+        readInput();
+        goodbye();
+    }
+
+    public static void greet() {
+        System.out.println("Hello! I'm Duke\nWhat can i do for you?");
+    }
+
+    public static void goodbye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void readInput() {
+        Scanner input = new Scanner(System.in);
+
+        String userInput = input.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            userInput = input.nextLine();
+        }
     }
 }

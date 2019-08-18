9a771fccf3cdfb69a7ce3255dabb50106186327d
tohcejasmine <e0104958@u.nus.edu>
2019-08-19 01:00:47 +0800

    level 3: mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,21 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     //Constructor
-    private String[] taskList;
-    private int numTask;
+    private ArrayList<Task> taskList;
     public Duke() {
-        //Fixed size array
-        taskList = new String[100];
-        numTask = 0;
+        taskList = new ArrayList<>();
     }
     public static void main(String[] args) {
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        //System.out.println("Hello from\n" + logo);
-
+        System.out.println("Hello from\n" + logo);
+        */
         //First greetings
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         System.out.println("=====");
@@ -31,29 +30,31 @@ public class Duke {
         //Continually receive commands until exit
         inProgram = true;
         while (inProgram) {
-            String userCommand = input.nextLine();
-            switch (userCommand) {
+            String userInput = input.nextLine();
+            //Split input by " " to identify command by first word
+            String[] splitInput = userInput.split(" ");
+            switch (splitInput[0]) {
                 case ("bye"):
                     display("Bye. Hope to see you again soon!");
                     //Exit program
                     inProgram = false;
                     break;
                 case ("list"):
-                    if (numTask == 0) {
+                    if (taskList.isEmpty()) {
                         display("You have no tasks yet!");
                     } else {
-                        System.out.println("-----");
-                        //List out added tasks
-                        for (int i = 0; i < numTask; i++) {
-                            System.out.println((i+1) + ". " + taskList[i]);
-                        }
-                        System.out.println("-----");
+                        displayTasks();
                     }
                     break;
+                case ("done"):
+                    //TODO check for valid inputs (integer valid in list)
+                    //Number of task in list to mark done
+                    int taskNum = Integer.parseInt(splitInput[1]);
+                    displayMarkDone(taskNum);
+                    break;
                 default:
-                    taskList[numTask] = userCommand;
-                    numTask++;
-                    display("added: " + userCommand);
+                    taskList.add(new Task(userInput));
+                    display("added: " + userInput);
             }
         }
     }
@@ -64,5 +65,27 @@ public class Duke {
         System.out.println("-----");
     }
 
+    //List out added tasks
+    private void displayTasks() {
+        System.out.println("-----");
+        System.out.println("Here are the tasks in your list:");
+        for (Task task : taskList) {
+            System.out.println("    " +
+                    (taskList.indexOf(task)+1) + "." +
+                    "[" + task.getStatusIcon() + "] " +
+                    task.description);
+        }
+        System.out.println("-----");
+    }
 
+    private void displayMarkDone(int doneIdx) {
+        Task doneTask = taskList.get(doneIdx-1);
+        doneTask.markDone();
+        System.out.println("-----");
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("    " +
+                "[" + doneTask.getStatusIcon() + "] " +
+                doneTask.description);
+        System.out.println("-----");
+    }
 }

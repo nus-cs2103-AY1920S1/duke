e50c11fc9de58e07bef36e5ff1b52d15a845b09a
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2019-08-18 19:08:46 +0800

    Add code to print greeting on startup

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,20 @@
 public class Duke {
+
+    public static final String LONG_LINE = "____________________________________________________________";
+    public static final String GREETING = "Hello! I'm Duke\nWhat can I do for you?";
+
     public static void main(String[] args) {
+        printGreeting();
+    }
+
+    public static void printGreeting() {
+        System.out.println(
+            LONG_LINE
+            + "\n"
+            + GREETING
+            + "\n"
+            + LONG_LINE
+            + "\n"
+        );
     }
 }

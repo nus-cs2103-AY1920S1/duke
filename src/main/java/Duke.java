ec650c5dc01d8ba047e23ae826fec4ce31bbcd6d
Q-gabe <gabriel.ong@u.nus.edu>
2019-08-18 14:06:13 +0800

    Implemented Level-2 (Add, List).

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,24 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
         greetHello();
 
         String input;
         // Run input loop
-        while (!(input = sc.next()).equals("bye")) {
-            echo(input);
+        while (!(input = sc.nextLine()).equals("bye")) {
+            switch (input){
+                case "list":
+                    readList(list);
+                    break;
+                default:
+                    addToList(input, list);
+                    break;
+            }
         }
 
         greetBye();
@@ -33,7 +42,23 @@ public class Duke {
         printOutput("Bye. Hope to see you again soon!");
     }
 
-    public static void echo(String echoInput) {
-        printOutput(echoInput);
+    public static void addToList(String input, ArrayList<String> list) {
+        list.add(input);
+        printOutput("added: " + input);
+    }
+
+    public static void readList(ArrayList<String> list) {
+        int count = 1;
+        StringBuilder output = new StringBuilder();
+
+        // List and print all items stored
+        for (String item: list) {
+            output.append(count++ + ". " + item + '\n');
+        }
+
+        // Remove terminal newline character
+        output.deleteCharAt(output.toString().length()-1);
+        
+        printOutput(output.toString());
     }
 }

36370e3671d5d4e8623c55af3eed332087515970
tohcejasmine <e0104958@u.nus.edu>
2019-08-18 23:16:57 +0800

    level 1: greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,44 @@
+import java.util.Scanner;
+
 public class Duke {
+    //Constructor
+    public Duke() {
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //System.out.println("Hello from\n" + logo);
+
+        //First greetings
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        System.out.println("=====");
+        Duke duke = new Duke();
+        duke.run();
     }
+
+    boolean inProgram;
+    private void run() {
+        //Initialise Scanner object
+        Scanner input = new Scanner(System.in);
+        //Continually receive commands until exit
+        inProgram = true;
+        while (inProgram) {
+            String userCommand = input.next();
+            if (userCommand.equals("bye")) {
+                System.out.println("-----");
+                System.out.println("    " + "Bye. Hope to see you again soon!");
+                System.out.println("-----");
+                inProgram = false;
+            } else {
+                System.out.println("-----");
+                System.out.println("    " + userCommand);
+                System.out.println("-----");
+            }
+        }
+    }
+
+
 }

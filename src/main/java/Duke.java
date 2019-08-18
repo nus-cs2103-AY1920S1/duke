1696ff35a9f1c73394956ec91e629772f275302a
Bruce Ong <bruceskellator@hotmail.com>
2019-08-19 01:29:17 +0800

    src/main/java/Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String[] itemsLst = new String[100];
+        int currentIndex = 0;
+
         String hLine = "    ____________________________________________________________";
 
         System.out.println(hLine);
@@ -18,10 +22,19 @@ public class Duke {
                 System.out.println("    Bye. Hope to see you again soon!");
                 System.out.printf("%s\n\n", hLine);
                 break;
+            } else {
+                System.out.println(hLine);
+                if (command.equals("list")) {
+                    for (int i = 0; i < currentIndex; i++) {
+                        System.out.printf("    %d. %s\n", i + 1, itemsLst[i]);
+                    }
+                } else { // if command is neither list nor bye
+                    itemsLst[currentIndex] = command;
+                    currentIndex++;
+                    System.out.printf("    added: %s\n", command);
+                }
+                System.out.printf("%s\n\n", hLine);
             }
-            System.out.println(hLine);
-            System.out.printf("    %s\n", command);
-            System.out.printf("%s\n\n", hLine);
 
         }
 

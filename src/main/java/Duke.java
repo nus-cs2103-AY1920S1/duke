5a8a7a6738c1740827e0cff55ff83c7d43d378b7
Bruce Ong <bruceskellator@hotmail.com>
2019-08-19 02:05:44 +0800

    src/main/java/Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] itemsLst = new String[100];
+        Task[] itemsLst = new Task[100];
         int currentIndex = 0;
 
         String hLine = "    ____________________________________________________________";
@@ -25,13 +25,24 @@ public class Duke {
             } else {
                 System.out.println(hLine);
                 if (command.equals("list")) {
+                    System.out.println("    Here are the tasks in your list:");
                     for (int i = 0; i < currentIndex; i++) {
-                        System.out.printf("    %d. %s\n", i + 1, itemsLst[i]);
+                        System.out.printf("    %d.[%s] %s\n",
+                            i + 1, itemsLst[i].getStatusIcon(), itemsLst[i].description);
                     }
                 } else { // if command is neither list nor bye
-                    itemsLst[currentIndex] = command;
-                    currentIndex++;
-                    System.out.printf("    added: %s\n", command);
+                    Scanner tempSc = new Scanner(command);
+                    // if command is done
+                    if (tempSc.hasNext() && tempSc.next().equals("done")) {
+                        int tempInt = tempSc.nextInt() - 1;
+                        itemsLst[tempInt].markAsDone();
+                        System.out.printf("    Nice! I've marked this task as done:\n      [%s] %s\n"
+                            , itemsLst[tempInt].getStatusIcon(), itemsLst[tempInt].description);
+                    } else {
+                        itemsLst[currentIndex] = new Task(command);
+                        System.out.printf("    added:[%s] %s\n", itemsLst[currentIndex].getStatusIcon(), command);
+                        currentIndex++;
+                    }
                 }
                 System.out.printf("%s\n\n", hLine);
             }

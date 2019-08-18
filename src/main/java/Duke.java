bf3e58482750de49ca85d4d9484ff1fadc6a0d93
seahlynn <e0324409@u.nus.edu>
2019-08-18 22:42:33 +0800

    increment 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
+        String command;
         System.out.println("Hello! I'm Duke" + "\n" + "What can I do for you?");
         Scanner sc = new Scanner(System.in);
 
-        while(command = sc.nextLine() != "bye") {
+        while(!(command = sc.nextLine()).equals("bye")) {
             System.out.println(command);
         }
 
-        System.out.println("bye!");
-        System.exit(0);
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }

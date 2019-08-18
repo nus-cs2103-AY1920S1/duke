94794600ddeb8042408bf20ca491543a71d69e3c
Bruce Ong <bruceskellator@hotmail.com>
2019-08-19 01:07:14 +0800

    src/main/java/Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        String hLine = "    ____________________________________________________________";
+
+        System.out.println(hLine);
+        System.out.println("    Hello! I'm Duke\n    What can I do for you?");
+        System.out.printf("%s\n\n", hLine);
+
+        while(true) {
+
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println(hLine);
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.printf("%s\n\n", hLine);
+                break;
+            }
+            System.out.println(hLine);
+            System.out.printf("    %s\n", command);
+            System.out.printf("%s\n\n", hLine);
+
+        }
+
     }
 }

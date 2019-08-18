b798a447217b3fecfb47589e2e425a8179f652f4
Russell Loh <e0310763@u.nus.edu>
2019-08-18 22:23:03 +0800

    Completed A-Exceptions. Added DukeException class

---
+++ src/main/java/DukeException.java
@@ -0,0 +1,7 @@
+public class DukeException extends Exception{
+    public DukeException(String message){
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     ☹ OOPS!!! " + message);
+        System.out.println("    ____________________________________________________________");
+    }
+}

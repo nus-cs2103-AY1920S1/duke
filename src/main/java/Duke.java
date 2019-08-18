bf6bba3ea338cf90aa7e0ff6facdc5256e083794
Eugene Lim <nexolute@gmail.com>
2019-08-18 11:44:45 +0800

    Add features for Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,40 +13,71 @@ public class Duke {
         String command = scanner.nextLine();
 
         while (!command.equals("bye")) {
-            if (command.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + tasks.get(i).toString());
-                }
-            } else if (command.startsWith("done")) {
-                int index = Integer.parseInt(command.split(" ")[1]);
-                tasks.get(index - 1).markAsDone();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(tasks.get(index - 1).toString());
-            } else {
-                String type = command.split(" ", 2)[0];
-                String description = command.split(" ", 2)[1];
-                Task task;
-
-                if (type.equals("todo")) {
-                    task = new Todo(description);
+            try {
+                if (command.equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i).toString());
+                    }
+                    System.out.println();
+                } else if (command.startsWith("done")) {
+                    int index = Integer.parseInt(command.split(" ")[1]);
+                    tasks.get(index - 1).markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(tasks.get(index - 1).toString());
+                    System.out.println();
+                } else if (command.startsWith("todo")) {
+                    try {
+                        String description = command.split(" ", 2)[1];
+                        Task task = new Todo(description);
+                        tasks.add(task);
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(task.toString());
+                        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                        System.out.println();
+                    } catch (IndexOutOfBoundsException e) {
+                        throw new EmptyDescriptionException();
+                    }
                 } else if (command.startsWith("event")) {
-                    String at = description.split(" /at ", 2)[1];
-                    description = description.split(" /at ", 2)[0];
-                    task = new Event(description, at);
+                    try {
+                        String description = command.split(" ", 2)[1];
+                        String at = description.split(" /at ", 2)[1];
+                        description = description.split(" /at ", 2)[0];
+                        Task task = new Event(description, at);
+                        tasks.add(task);
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(task.toString());
+                        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                        System.out.println();
+                    } catch (IndexOutOfBoundsException e) {
+                        throw new EmptyDescriptionException();
+                    }
+                } else if (command.startsWith("deadline")) {
+                    try {
+                        String description = command.split(" ", 2)[1];
+                        String by = description.split(" /by ", 2)[1];
+                        description = description.split(" /by ", 2)[0];
+                        Task task = new Event(description, by);
+                        tasks.add(task);
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(task.toString());
+                        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                        System.out.println();
+                    } catch (IndexOutOfBoundsException e) {
+                        throw new EmptyDescriptionException();
+                    }
                 } else {
-                    String by = description.split(" /by ", 2)[1];
-                    description = description.split(" /by ", 2)[0];
-                    task = new Deadline(description, by);
+                    throw new InvalidCommandException();
                 }
-
-                tasks.add(task);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(task.toString());
+            } catch(InvalidCommandException e) {
+                System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                System.out.println();
+            } catch(EmptyDescriptionException e) {
+                System.out.println("☹ OOPS!!! The description of a task cannot be empty.");
+                System.out.println();
+            } finally {
+                command = scanner.nextLine();
             }
-
-            System.out.println();
-            command = scanner.nextLine();
         }
 
         System.out.println("Bye. Hope to see you again soon!");

c478c431a8c2e3d97577e65fa491e94b76146664
lumos309 <tan.yugin1@gmail.com>
2019-08-18 22:00:20 +0800

    Implement add, list features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,13 @@
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
+        ArrayList<String> tasks = new ArrayList<>();
+        Scanner sc;
+        Boolean exit = false;
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -11,12 +17,24 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
-        Scanner sc;
         sc = new Scanner(System.in);
         String response = sc.nextLine();
-        while (!response.equals("bye")) {
-            System.out.println(response);
-            response = sc.nextLine();
+        while (!exit) {
+            if (response.equals("bye")) exit = true;
+            else if (response.equals("list")) {
+                Iterator<String> iter = tasks.iterator();
+                int i = 1;
+                while (iter.hasNext()) {
+                    System.out.println(i + ": " + iter.next());
+                    i++;
+                }
+                response = sc.nextLine();
+            }
+            else {
+                tasks.add(response);
+                System.out.println("added: " + response);
+                response = sc.nextLine();
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
 

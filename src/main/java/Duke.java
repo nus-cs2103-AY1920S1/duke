0410976fadc0fc6fca04a20cc9af90b3799002bd
chowyiyin <yiyin.chow@gmail.com>
2019-08-18 16:26:44 +0800

    Add Task Class and Add Level 3 Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    public static ArrayList<String> actions = new ArrayList<>();
+    public static ArrayList<Task> actions = new ArrayList<>();
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -22,12 +22,25 @@ public class Duke {
                 break;
             } else if (userInput.equals("list")) {
                 printList();
-            } else {
+            } else if (userInput.contains("done")) {
+                String listActionIndex = (userInput.split(" "))[1];
+                int arrayActionIndex = Integer.parseInt(listActionIndex) - 1;
+                markAsDone(arrayActionIndex);
+
+            }else {
                 addToList(userInput);
             }
         }
     }
 
+    public static void markAsDone(int index) {
+        Task action = actions.get(index);
+        action.setDone();
+        drawLine();
+        System.out.println("\t Nice! I've marked this task as done: ");
+        System.out.println("\t\t " + action);
+        drawLine();
+    }
     public static void sayBye() {
         drawLine();
         System.out.println("\t Bye. Hope to see you again soon!");
@@ -36,6 +49,7 @@ public class Duke {
 
     public static void printList() {
         drawLine();
+        System.out.println("\t Here are the tasks in your list: ");
         for (int i = 0; i < actions.size(); i++) {
             System.out.println("\t " + (i + 1) + ". " + actions.get(i));
         }
@@ -43,7 +57,7 @@ public class Duke {
     }
     public static void addToList(String input) {
         drawLine();
-        actions.add(input);
+        actions.add(new Task(input));
         System.out.println("\t added: " + input);
         drawLine();
     }

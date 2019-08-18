f8a8c0a07e9c75143f718325ca93216c0796d3d9
SoilingRogue <yuan.xinran.stanley@gmail.com>
2019-08-18 22:58:37 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,11 @@
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean bye = false;
+        LinkedList<String> lst = new LinkedList<>();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -15,12 +17,26 @@ public class Duke {
         System.out.println(line + "\n" + "     Hello! I'm Duke\n     What can I do for you?\n" + line);
 
         while (!bye) {
-            String command = sc.next();
-            if (!command.equals("bye")) {
-                System.out.println(line + "\n     " + command + "\n" + line);
-            } else {
-                bye = true;
-                System.out.println(line + "\n" + "     Bye. Hope to see you again soon!\n" + line);
+            String command = sc.nextLine();
+            switch (command) {
+                case "bye":
+                    bye = true;
+                    System.out.println(line + "\n" + "     Bye. Hope to see you again soon!\n" + line);
+                    break;
+                case "list":
+                    System.out.println(line);
+                    int size = lst.size();
+                    for (int i = 1; i <= size; i++) {
+                        System.out.printf("     %d. %s\n", i, lst.peek());
+                        String temp = lst.removeFirst();
+                        lst.addLast(temp);
+                    }
+                    System.out.println(line);
+                    break;
+                default:
+                    lst.addLast(command);
+                    System.out.println(line + "\n     added: " + command + "\n" + line);
+                    break;
             }
         }
     }

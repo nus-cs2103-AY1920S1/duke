76d99edf5269e03abf0f6b4dc832d6da2f5800a4
Sam-limyr <nacuniel@gmail.com>
2019-08-20 00:09:03 +0800

    Added completion status for individual tasks.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,17 +2,9 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    public static void printStoredArray(ArrayList<String> taskList) {
-        int counter = 1;
-        for (String task : taskList) {
-            System.out.println(counter + ". " + task);
-            counter++;
-        }
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> taskList = new ArrayList<>(100);
+        ArrayList<Task> taskList = new ArrayList<>(100);
 
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         while (true) {
@@ -20,10 +12,18 @@ public class Duke {
             if (input.equals("bye")) {
                 break;
             } else if (input.equals("list")) {
-                printStoredArray(taskList);
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println((i + 1) + ". " + taskList.get(i));
+                }
+            } else if (input.startsWith("done")) {
+                int index = Integer.parseInt(input.split(" ")[1]);
+                taskList.get(index - 1).markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(taskList.get(index - 1));
             } else {
                 System.out.println("added: " + input);
-                taskList.add(input);
+                taskList.add(new Task(input));
             }
         }
         System.out.println("Bye. Hope to see you again soon!");

26d1a0a7be9f06f9e1022766f2d0f621677c6461
abhinav <ambhinav@yahoo.com>
2019-08-19 20:48:51 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,43 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+
+        greeting();
+
+        while(sc.hasNextLine()) {
+            String command = sc.nextLine();
+
+            switch(command.toLowerCase()) {
+                case "bye":
+                    handleExit();
+                    return;
+                
+                default:
+                    handleDefault(command.toLowerCase());
+                    break;
+            }
+        }
     }
+
+    public static void addBorder(String input) {
+
+        String border = "____________________________________________________________";
+
+        System.out.println(border + "\n\n" + input + "\n" + border + "\n");
+    }
+
+    public static void greeting() {
+        addBorder("Hello! I'm Duke\n" + "What can I do for you?");
+    }
+
+    public static void handleDefault(String input) {
+        addBorder(input);
+    }
+
+    public static void handleExit() {
+        addBorder("Bye. Hope to see you again soon!");
+    }
+
 }

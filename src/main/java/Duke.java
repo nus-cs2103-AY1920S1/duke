1ae5267e2d6ff0e7c2170296c500396693183475
dragontho <paultho97@gmail.com>
2019-08-19 16:19:41 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,9 +2,13 @@ import java.util.Scanner;
 
 public class Duke {
     private static final String border = "____________________________________________________________";
+    private static String[] strList = new String[100];
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+        int pointer = 0;
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -14,19 +18,34 @@ public class Duke {
                 + border + "\n\nHello! I'm Duke\n" + "What can I do for you?\n" + border + "\n");
 
         while (true) {
-            String str = sc.next();
-            if (!str.equals("bye")) {
-                System.out.println(echo(str));
-            } else {
+            String str = sc.nextLine();
+            if (str.equals("bye")) {
                 break;
+            } else if (str.equals("list")) {
+                outputList(pointer, strList);
+            } else {
+                System.out.println(addToList(pointer, str));
+                pointer++;
             }
         }
 
         System.out.println(border + "\n\nBye. Hope to see you again soon!" + "\n" + border);
 
+        sc.close();
+
     }
 
-    public static String echo(String string) {
-        return border + "\n\n" + string + "\n" + border + "\n";
+    public static String addToList(int pointer, String string) {
+        strList[pointer] = string;
+        return border + "\n\n" + "added: " + string + "\n" + border + "\n";
     }
+
+    public static void outputList(int pointer, String[] strList) {
+        System.out.println(border + "\n");
+        for (int i = 1; i < pointer + 1; i++) {
+            System.out.println(i + ". " + strList[i - 1]);
+        }
+        System.out.println(border + "\n");
+    }
+
 }

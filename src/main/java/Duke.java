c30ac05400d6ed5c5bea39a45e785fd3456f5e72
limyuhui <yuhuilim1999@gmail.com>
2019-08-19 13:35:45 +0800

    Level-1: Basic structure implemented

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greetings = "Hello! I'm Duke\nWhat can I do for you?";
+        System.out.println(greetings);
+
+        Scanner input = new Scanner(System.in);
+        String command = input.next();
+
+        while (!command.equals("bye")) {
+            System.out.println(command);
+            command = input.next();
+        }
+
+        String farewell = "Bye. Hope to see you again soon!";
+        System.out.println(farewell);
     }
 }

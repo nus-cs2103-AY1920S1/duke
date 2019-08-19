08a4c57ff5a04dcdabe1a6737259a294ac9fe5cc
Tan_Jun_Bang <tan_jun_bang@hotmail.com>
2019-08-19 23:10:04 +0800

    Level 2. Add, List

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,18 +11,28 @@ public class Main {
         System.out.println("\t  What can I do for you?");
         System.out.println(horizontal_line);
 
+        //store the list
+        ArrayList<String> strArr = new ArrayList<>();
+
         //take in inputs
         while (sc.hasNext()) {
-            String inputStr = sc.next();
+            String inputStr = sc.nextLine();
 
             if (inputStr.equals("bye")) {
                 System.out.println(horizontal_line);
                 System.out.println("\t  Bye. Hope to see you again soon!");
                 System.out.println(horizontal_line);
                 break;
-            } else {
+            } else if (inputStr.equals("list")) {
                 System.out.println(horizontal_line);
-                System.out.println("\t  " + inputStr);
+                for (int i = 0; i < strArr.size(); i++) {
+                    System.out.println("\t  "+ (i + 1) +". " + strArr.get(i));
+                }
+                System.out.println(horizontal_line);
+            } else {
+                strArr.add(inputStr);
+                System.out.println(horizontal_line);
+                System.out.println("\t  added: " + inputStr);
                 System.out.println(horizontal_line);
             }
         }

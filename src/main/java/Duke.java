50a219ec4beaf8dfb7835e15635a2b0b2c15ed9e
Kendrick Ang <phantom2457@hotmail.com>
2019-08-19 11:59:22 +0800

    Add Duke level 2 functionality - Store whatever text entered by the user - Display all text back to the user when requested - Maximum of 100 lines of input text

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Duke {
@@ -16,21 +17,42 @@ public class Duke {
     private static final String EXIT_MESSAGE =
             "\tBye. Hope to see you again soon!\n";
 
+    public LinkedList<String> tasks;
+
+    public Duke() {
+        this.tasks = new LinkedList<>();
+    }
+
     public void run() {
         System.out.println(LOGO + TOP_SEPARATOR + GREET_MESSAGE + BOTTOM_SEPARATOR);
 
         Scanner sc = new Scanner(System.in);
         while (true) {
-            String cmd = sc.next();
+            String cmd = sc.nextLine();
             switch (cmd) {
+                case "list":
+                    System.out.println(TOP_SEPARATOR + formatTasks(tasks) + BOTTOM_SEPARATOR);
+                    break;
                 case "bye":
                     System.out.println(TOP_SEPARATOR + EXIT_MESSAGE + BOTTOM_SEPARATOR);
                     return;
                 default:
+                    tasks.addLast(cmd);
                     System.out.println(TOP_SEPARATOR + "\t" + cmd + "\n" + BOTTOM_SEPARATOR);
                     break;
             }
         }
+    }
+
+    // Takes a list of Strings and numbers them in order, in a top-down list format.
+    public String formatTasks(LinkedList<String> tasks) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        for (String task: tasks) {
+            i++;
+            sb.append("\t").append(i).append(". ").append(task).append("\n");
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {

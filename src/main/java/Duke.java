c9763e8ea25012b0fe6358a31898db9d84a89a3a
jonchan51 <32188705+jonchan51@users.noreply.github.com>
2019-08-19 12:04:50 +0800

    Allow Duke to echo commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,8 @@
-public class Duke {
+import java.util.Scanner;
 
+public class Duke {
     /**
-     * Main function to start the Duke app.
+     * Setup Duke.
      * @param args the setup arguments
      */
     public static void main(String[] args) {
@@ -11,5 +12,45 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Duke duke = new Duke();
+        duke.run();
+    }
+
+    /**
+     * Run Duke.
+     */
+    public void run() {
+        sayGreeting();
+        String command;
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            command = sc.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            printFormatted(command);
+        }
+        sayBye();
+    }
+
+    private void sayGreeting() {
+        printFormatted("Hello! I'm Duke\nWhat can I do for you?");
+    }
+
+    private void sayBye() {
+        printFormatted("Bye. Hope to see you again soon!");
+    }
+
+    private void printFormatted(String output) {
+        String horLine = "\t____________________________________________________________";
+        String[] lines = output.split("\n");
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(horLine + "\n");
+        for (String line : lines) {
+            stringBuilder.append("\t " + line + "\n");
+        }
+        stringBuilder.append(horLine + "\n");
+        System.out.println(stringBuilder);
     }
 }

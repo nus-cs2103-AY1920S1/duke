47d01e1ed3f9edc203f81980b6d76474db7a3d07
Joshua Wong <joshwonghc@gmail.com>
2019-08-19 16:33:53 +0800

    Implement level 3 code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 
 public class Duke {
-    private String[] list;
-    private int currentIndex;
+    private List<Task> list;
+    private PrintStream printStream;
 
     /**
      * Main method.
@@ -23,8 +27,22 @@ public class Duke {
      * Constructor.
      */
     public Duke() {
-        this.list = new String[100];
-        this.currentIndex = 0;
+        this.list = new ArrayList<Task>();
+        this.printStream = this.initPrintStream();
+    }
+
+    /**
+     * Initialize printStream.
+     * @return printStream PrintStream
+     */
+    PrintStream initPrintStream() {
+        PrintStream printStream; 
+        try {
+            printStream = new PrintStream(System.out, true, "UTF-8");
+        } catch (UnsupportedEncodingException error) {
+            printStream = new PrintStream(System.out, true);
+        }
+        return printStream;
     }
 
     /**
@@ -33,19 +51,47 @@ public class Duke {
      * @return boolean
      */
     boolean handleCommand(String command) {
-        switch (command) {
+
+        String keyword = command.split(" ")[0];
+        switch (keyword) {
         case "bye":
             System.out.println("Bye. Hope to see you again soon!");
             return true;
         case "list":
-            for (int i = 0; i < this.currentIndex; i++) {
-                System.out.printf("    %d. %s\n", i + 1, this.list[i]);
-            }
+            this.handleListCommand();
+            break;
+        case "done":
+            this.handleDoneCommand(command);
             break;
         default:
-            this.list[this.currentIndex++] = command;
-            System.out.printf("    added: %s\n", command);
+            Task taskToAdd = new Task(command);
+            this.list.add(taskToAdd);
+            this.printStream.printf("    added: %s\n", taskToAdd);
         }
         return false;
+    }
+
+    /**
+     * Handle List command.
+     */
+    void handleListCommand() {
+        System.out.printf("    Here are the tasks in your list:\n");
+        int length = this.list.size();
+        for (int i = 0; i < length; i++) {
+            this.printStream.printf("    %d.%s\n", i + 1, this.list.get(i));
+        }
+    }
+
+    /**
+     * Handle Done command.
+     * @param command String
+     */
+    void handleDoneCommand(String command) {
+        String[] doneArr = command.split(" ");
+        int indexToEdit = Integer.parseInt(doneArr[1]);
+        Task task = this.list.get(indexToEdit - 1);
+        task.markDone();
+        System.out.printf("    Nice! I've marked this task as done:\n");
+        this.printStream.printf("      %s\n", task); 
     }
 }

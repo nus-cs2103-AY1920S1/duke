d36fafa320adc567b48c8c52e4cd9e9e774f5ea1
marcfyk <marcjfyk@gmail.com>
2019-08-19 11:24:35 +0800

    Modified Duke class behaviour.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,6 +6,13 @@ import java.io.IOException;
  * Duke Class.
  */
 public class Duke {
+    /** TextList to store user input. */
+    private static TextList textList = new TextList();
+    /** Line for responses. */
+    private static String line = "    ____________________________________________________________";
+    /** Indentation for response */
+    private static String indent = "     ";
+
     /**
      * Main Method.
      * @param args Arguments.
@@ -19,7 +26,7 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            greetEchoExit(br);
+            run(br);
             br.close();
         } catch (IOException e) {
             System.out.println(e);
@@ -27,51 +34,56 @@ public class Duke {
     }
 
     /**
-     * Prints out greetings, then echos commands by user, and prints exit response when command is "bye".
+     * Prints out greetings, then responds to the users input, then exits when user responds "bye".
      * @param br BufferedReader to read user commands.
      */
-    private static void greetEchoExit(BufferedReader br) throws IOException {
+    private static void run(BufferedReader br) throws IOException {
         greeting();
         String input = br.readLine();
         while (input != null && !input.equals("bye")) {
-            echo(input);
+            if (input.equals("list")) {
+                printList();
+            } else {
+                addText(input);
+            }
             input = br.readLine();
         }
         exit();
     }
 
     /**
-     * Formats a response with indentation and lines.
-     * @param response The string that is being formatted.
-     * @return The formatted string from the response parameter.
+     * Adds text to the text list and prints response.
+     * @param text String text that will be added.
      */
-    private static String formatResponse(String response) {
-        String indent = "    ";
-        String line = indent + "____________________________________________________________";
-        String output = indent + " " + response;
-        return String.format("%s\n%s\n%s\n", line, output, line);
+    private static void addText(String text) {
+        textList.add(text);
+        String response = String.format("%s\n     added: %s\n%s\n", line, text, line);
+        System.out.println(response);
+    }
+
+    /**
+     * Prints out the text list.
+     */
+    private static void printList() {
+        String response = String.format("%s\n%s\n%s\n", line, textList, line);
+        System.out.println(response);
     }
 
     /**
      * Prints out the greeting.
      */
     private static void greeting() {
-        String response = "Hello! I'm Duke\n     What can I do for you?";
-        System.out.println(formatResponse(response));
-    }
-
-    /**
-     * Echo string response by printing formatted response.
-     * @param response The string that will be in the response.
-     */
-    private static void echo(String response) {
-        System.out.println(formatResponse(response));
+        String message = indent + "Hello! I'm Duke\n" + indent + "What can I do for you?";
+        String response = String.format("%s\n%s\n%s\n", line, message, line);
+        System.out.println(response);
     }
 
     /**
      * Exit string response by printing the standard bye response.
      */
     private static void exit() {
-        System.out.println(formatResponse("Bye. Hope to see you again soon!"));
+        String message = indent + "Bye. Hope to see you again soon!";
+        String response = String.format("%s\n%s\n%s\n", line, message, line);
+        System.out.println(response);
     }
 }

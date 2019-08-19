2a9538c6ad5c79458f3621d336f82ac36bab1ce7
JunHong <teojunhong96@gmail.com>
2019-08-19 23:24:39 +0800

    Add Exception to various block to prevent breaking of code.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -40,17 +40,34 @@ public class Duke {
             //adding items to arraylist for listing purpose
             if (input.toLowerCase().contains("done")) {
                 //getting the number for item
-                int itemNum = Integer.parseInt(input.substring(input.length() - 1));
-                Task curr = items.get(itemNum - 1);
-                curr.markAsDone();
-                //forming the message
-                sb.append(border + "\n");
-                sb.append("Nice! I've marked this task as done:\n");
-                sb.append(curr + "\n");
-                sb.append(border + "\n");
-                System.out.println(sb.toString());
-                sb.setLength(0);
-                input = sc.nextLine();
+                try {
+                    int itemNum = Integer.parseInt(input.substring(input.length() - 1));
+                    Task curr = items.get(itemNum - 1);
+                    curr.markAsDone();
+                    //forming the message
+                    sb.append(border + "\n");
+                    sb.append("Nice! I've marked this task as done:\n");
+                    sb.append(curr + "\n");
+                    sb.append(border + "\n");
+                    System.out.println(sb.toString());
+                    sb.setLength(0);
+                    input = sc.nextLine();
+                } catch(IndexOutOfBoundsException e) {
+                    sb.append(border + "\n");
+                    sb.append("Invalid number. Number not listed. \n");
+                    sb.append(border + "\n");
+                    System.out.println(sb.toString());
+                    sb.setLength(0);
+                    input = sc.nextLine();
+                } catch (NumberFormatException e) {
+                    sb.append(border + "\n");
+                    sb.append("No input detected. Please enter a number. \n");
+                    sb.append(border + "\n");
+                    System.out.println(sb.toString());
+                    sb.setLength(0);
+                    input = sc.nextLine();
+                }
+
             } else if (input.toLowerCase().equals("list")) {
                 sb.append(border + "\n");
                 //list out all items in arraylist items
@@ -64,50 +81,73 @@ public class Duke {
                 input = sc.nextLine();
             } else if (input.toLowerCase().contains("todo")) {
                 //adding an item
-                Todo newTask = new Todo(input.substring(5));
-                items.add(newTask);
-                sb.append(border + "\n");
-                sb.append("Got it. I've added this task: \n");
-                sb.append(newTask + "\n");
-                sb.append("Now you have " + items.size() + " tasks in the list.\n");
-                sb.append(border + "\n");
-                System.out.println(sb.toString());
-                sb.setLength(0);
-                input = sc.nextLine();
+                try {
+                    Todo newTask = new Todo(input.substring(5));
+                    items.add(newTask);
+                    sb.append(border + "\n");
+                    sb.append("Got it. I've added this task: \n");
+                    sb.append(newTask + "\n");
+                    sb.append("Now you have " + items.size() + " tasks in the list.\n");
+                    sb.append(border + "\n");
+                    System.out.println(sb.toString());
+                    sb.setLength(0);
+                    input = sc.nextLine();
+                } catch (StringIndexOutOfBoundsException e) {
+                    sb.append(border + "\n");
+                    sb.append("Todo must have valid description\n");
+                    sb.append(border + "\n");
+                    System.out.println(sb.toString());
+                    sb.setLength(0);
+                    input = sc.nextLine();
+                }
             } else if (input.toLowerCase().contains("deadline")) {
-                int date = input.indexOf("/by");
-                //split input into [deadline] [description] [date]
-                Deadline newTask = new Deadline(input.substring(9, date), input.substring(date + 3));
-                items.add(newTask);
-                sb.append(border + "\n");
-                sb.append("Got it. I've added this task: \n");
-                sb.append(newTask + "\n");
-                sb.append("Now you have " + items.size() + " tasks in the list.\n");
-                sb.append(border + "\n");
-                System.out.println(sb.toString());
-                sb.setLength(0);
-                input = sc.nextLine();
+                try {
+                    int date = input.indexOf("/by");
+                    //split input into [deadline] [description] [date]
+                    Deadline newTask = new Deadline(input.substring(9, date), input.substring(date + 3));
+                    items.add(newTask);
+                    sb.append(border + "\n");
+                    sb.append("Got it. I've added this task: \n");
+                    sb.append(newTask + "\n");
+                    sb.append("Now you have " + items.size() + " tasks in the list.\n");
+                    sb.append(border + "\n");
+                    System.out.println(sb.toString());
+                    sb.setLength(0);
+                    input = sc.nextLine();
+                } catch (StringIndexOutOfBoundsException e) {
+                    sb.append(border + "\n");
+                    sb.append("Invalid Deadline's arguments \n");
+                    sb.append(border + "\n");
+                    System.out.println(sb.toString());
+                    sb.setLength(0);
+                    input = sc.nextLine();
+                }
+
             } else if (input.toLowerCase().contains("event")) {
-                int time = input.indexOf("/at");
-                //split input into [event] [description] [timing]
-                Event newTask = new Event(input.substring(6, time), input.substring(time + 3));
-                items.add(newTask);
-                sb.append(border + "\n");
-                sb.append("Got it. I've added this task: \n");
-                sb.append(newTask + "\n");
-                sb.append("Now you have " + items.size() + " tasks in the list.\n");
-                sb.append(border + "\n");
-                System.out.println(sb.toString());
-                sb.setLength(0);
-                input = sc.nextLine();
+                try {
+                    int time = input.indexOf("/at");
+                    //split input into [event] [description] [timing]
+                    Event newTask = new Event(input.substring(6, time), input.substring(time + 3));
+                    items.add(newTask);
+                    sb.append(border + "\n");
+                    sb.append("Got it. I've added this task: \n");
+                    sb.append(newTask + "\n");
+                    sb.append("Now you have " + items.size() + " tasks in the list.\n");
+                    sb.append(border + "\n");
+                    System.out.println(sb.toString());
+                    sb.setLength(0);
+                    input = sc.nextLine();
+                } catch (StringIndexOutOfBoundsException e) {
+                    sb.append(border + "\n");
+                    sb.append("Invalid Event's arguments \n");
+                    sb.append(border + "\n");
+                    System.out.println(sb.toString());
+                    sb.setLength(0);
+                    input = sc.nextLine();
+                }
             } else {
-                //remainder input
-                Task newTask = new Task(input);
-                items.add(newTask);
                 sb.append(border + "\n");
-                sb.append("Got it. I've added this task: \n");
-                sb.append(newTask + "\n");
-                sb.append("Now you have " + items.size() + " tasks in the list.\n");
+                sb.append("Unable to understand. Invalid Input. \n");
                 sb.append(border + "\n");
                 System.out.println(sb.toString());
                 sb.setLength(0);

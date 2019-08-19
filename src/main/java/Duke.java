3f30e0e66a0460812f65c998e2cf44fc5fd6c859
JunHong <teojunhong96@gmail.com>
2019-08-19 20:12:36 +0800

    Creating initial greetings

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,11 @@
+
 public class Duke {
+
+    /**
+     * Main method for executing Duke.
+     *
+     *
+     */
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +13,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String border = "----------------------------";
+        StringBuilder sb = new StringBuilder();
+        sb.append(border + "\n");
+        sb.append("Hello! I'm Duke\n");
+        sb.append("What can I do for you?\n");
+        sb.append(border + "\n");
+        String greetings = sb.toString();
+        System.out.println(greetings);
+
     }
 }

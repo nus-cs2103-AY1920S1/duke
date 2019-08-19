515a3da30b12460fba0d9f3a6317ffab12aa0e1c
ErwinChan <erwinchan1997@gmail.com>
2019-08-19 22:00:58 +0800

    Implemented Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,14 +5,26 @@ public class Duke {
         String border = "____________________________________________________________";
         System.out.println(border + "\nHello! I'm Duke\nWhat can I do for you?\n" + border);
 
+        String[] userList = new String[100];
+        int counter = 0;
         Scanner scanner = new Scanner(System.in);
 
         while(true){
-            String input = scanner.next();
+            String input = scanner.nextLine();
             if(input.equals("bye")){
                 System.out.println(border + "\nBye. Hope to see you again soon!\n" + border);
+            }else if(input.equals("list")){
+                int itemNumber = 1;
+                System.out.println(border);
+                for(int i = 0; i < counter; i++){
+                    System.out.println(itemNumber + ". " + userList[i]);
+                    itemNumber ++;
+                }
+                System.out.println(border);
             }else{
-                System.out.println(border + "\n" + input + "\n" + border);
+                userList[counter] = input;
+                counter ++;
+                System.out.println(border + "\n" + "added: " + input + "\n" + border);
             }
 
         }

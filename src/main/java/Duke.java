4d8376d9c2cabe5c4698d9ff80409afc51df6d2d
Jeun Hoe Poon <e0325449@u.nus.edu>
2019-08-20 00:08:19 +0800

    Add Greet, Echo, Exit features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,53 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String input;
+    private static boolean running;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        while (running) {
+            readInput();
+            processInput();
+        }
+    }
+
+    public static void greet() {
+        running = true;
+        System.out.println(
+                "    ____________________________________________________________\n" +
+                        "     Hello! I'm Duke\n" +
+                        "     What can I do for you?\n" +
+                        "    ____________________________________________________________\n");
+    }
+
+    public static void readInput() {
+        Scanner sc = new Scanner(System.in);
+        input = sc.next();
+    }
+
+    public static void processInput() {
+        switch (input) {
+            case "bye":
+                System.out.println(
+                        "    ____________________________________________________________\n" +
+                                "     Bye. Hope to see you again soon!\n" +
+                                "    ____________________________________________________________"
+                );
+                running = false;
+                break;
+
+            default:
+                System.out.println(
+                        "    ____________________________________________________________\n" +
+                                String.format("     %s\n", input) +
+                                "    ____________________________________________________________\n"
+                );
+        }
     }
 }

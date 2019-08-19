a881ef266eaf40417ba6ad0f27141810c5374479
tohcejasmine <e0104958@u.nus.edu>
2019-08-19 21:53:45 +0800

    level 4: todos, events, deadlines (add support for tracking 3 types of tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,10 @@
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    //Constructor
     private ArrayList<Task> taskList;
+    //Constructor
     public Duke() {
         taskList = new ArrayList<>();
     }
@@ -27,13 +28,15 @@ public class Duke {
     private void run() {
         //Initialise Scanner object
         Scanner input = new Scanner(System.in);
+        String userInput;
+        Task newTask;
         //Continually receive commands until exit
         inProgram = true;
         while (inProgram) {
-            String userInput = input.nextLine();
-            //Split input by " " to identify command by first word
-            String[] splitInput = userInput.split(" ");
-            switch (splitInput[0]) {
+            String firstCommand = input.next();
+            String taskDescription;
+            //Identify command by first word
+            switch (firstCommand) {
                 case ("bye"):
                     display("Bye. Hope to see you again soon!");
                     //Exit program
@@ -46,46 +49,109 @@ public class Duke {
                         displayTasks();
                     }
                     break;
+                case ("todo"):
+                    //TODO check for valid input (no whitespace only)
+                    if (input.hasNext()) {
+                        //Ignore starting whitespace
+                        taskDescription = input.nextLine().substring(1);
+                        newTask = new ToDo(taskDescription);
+                        taskList.add(newTask);
+                        displayResponse(newTask);
+                    } else {
+                        display("Please write something after \"todo\"!");
+                    }
+                    break;
+                case ("deadline"):
+                    //TODO check for valid input
+                    // (no whitespace only, has "/by")
+                    if (input.hasNext()) {
+                        userInput = input.nextLine().substring(1);
+                        int byIdx = userInput.indexOf("/by");
+                        taskDescription = userInput.substring(0, byIdx-1);
+                        String by = userInput.substring(byIdx+4);
+                        newTask = new Deadline(taskDescription, by);
+                        taskList.add(newTask);
+                        displayResponse(newTask);
+                    } else {
+                        display("Please write something after \"deadline\"!");
+                    }
+                    break;
+                case ("event"):
+                    //TODO check for valid input
+                    // (no whitespace only, has "/at")
+                    if (input.hasNext()) {
+                        userInput = input.nextLine().substring(1);
+                        int atIdx = userInput.indexOf("/at");
+                        taskDescription = userInput.substring(0, atIdx-1);
+                        String at = userInput.substring(atIdx+4);
+                        newTask = new Event(taskDescription, at);
+                        taskList.add(newTask);
+                        displayResponse(newTask);
+                    } else {
+                        display("Please write something after \"event\"!");
+                    }
+                    break;
                 case ("done"):
-                    //TODO check for valid inputs (integer valid in list)
-                    //Number of task in list to mark done
-                    int taskNum = Integer.parseInt(splitInput[1]);
-                    displayMarkDone(taskNum);
+                    //TODO check for valid inputs
+                    // (integer valid in list)
+                    if (input.hasNext()) {
+                        int doneNum = input.nextInt();
+                        Task doneTask = taskList.get(doneNum-1);
+                        doneTask.markDone();
+                        displayMarkDone(doneTask);
+                    } else {
+                        display("Please write something after \"event\"!");
+                    }
                     break;
                 default:
-                    taskList.add(new Task(userInput));
-                    display("added: " + userInput);
+                    display("\"" + firstCommand + "\"" +
+                            " is not a valid command. Try again.\n\n" +
+                            indentString +
+                            "List of valid commands: \n" +
+                            indentString +
+                            "todo, deadline, event, list, done, bye");
             }
         }
     }
 
+    private String indentString = "    ";
+
+    //Sandwich text between -----s
     private void display(String response) {
         System.out.println("-----");
-        System.out.println("    " + response);
+        System.out.println(indentString + response);
         System.out.println("-----");
     }
 
-    //List out added tasks
+    //List out added tasks ('list')
     private void displayTasks() {
         System.out.println("-----");
-        System.out.println("Here are the tasks in your list:");
+        System.out.println(indentString + "Here are the tasks in your list:");
         for (Task task : taskList) {
-            System.out.println("    " +
+            //Format: 1. [T/D/E][v/x] task-description (by/at: ...)
+            System.out.println(indentString +
                     (taskList.indexOf(task)+1) + "." +
-                    "[" + task.getStatusIcon() + "] " +
-                    task.description);
+                    task.toString());
         }
         System.out.println("-----");
     }
 
-    private void displayMarkDone(int doneIdx) {
-        Task doneTask = taskList.get(doneIdx-1);
-        doneTask.markDone();
+    //Acknowledgement receipt for commands ('todo'/'deadline'/'event')
+    private void displayResponse(Task task) {
+        System.out.println("-----");
+        System.out.println(indentString + "Got it. I've added this task:");
+        System.out.println(indentString + task.toString());
+        System.out.println(indentString +
+                "Now you have " + taskList.size() +
+                (taskList.size() == 1? " task":" tasks") +
+                " in the list.");
+        System.out.println("-----");
+    }
+
+    private void displayMarkDone(Task doneTask) {
         System.out.println("-----");
         System.out.println("Nice! I've marked this task as done:");
-        System.out.println("    " +
-                "[" + doneTask.getStatusIcon() + "] " +
-                doneTask.description);
+        System.out.println("    " + doneTask.toString());
         System.out.println("-----");
     }
 }

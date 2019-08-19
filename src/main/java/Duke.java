c0e92db7852565c59d4b342e0f0fcdb5a2409887
jeongyh99 <jeong.yuhan@gmail.com>
2019-08-19 11:34:55 +0800

    Commit Level 1 by jeongyh99

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        //dukeSayHello();
+        greetings();
+
+        //level 1
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String input = sc.next();
+            if (!input.equals("bye")) {
+                System.out.println(input);
+            }
+            else {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+        }
+    }
+
+    private static void greetings() {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+
+    private static void dukeSayHello() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        //this is a comment 2
-
     }
 }

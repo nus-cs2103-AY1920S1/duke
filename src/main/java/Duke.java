6a53f817c9d1f41b1c4ff11adad6ccc8eaadac2f
ROHITREDDYBALAM <sairohitreddyb@gmail.com>
2019-08-19 12:58:40 +0800

    Level1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public Duke(){}
+    private void Greet(){
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
+    }
+
+    private void Echo(String a){
+        System.out.println(a);
+    }
+    private void Exit(){
+        System.out.println("Bye. Hope to see you again soon!");
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke d = new Duke();
+        Scanner scan = new Scanner(System.in);
+        d.Greet();
+
+        while(scan.hasNextLine()){
+            String a = scan.nextLine();
+            if(a.equals("bye")){
+                d.Exit();
+                break;
+            }
+            else{
+                d.Echo(a);
+            }
+
+        }
+
     }
 }

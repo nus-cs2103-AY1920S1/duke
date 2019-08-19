49f8d4c902e25cecf076759893cead9b75ea7a4e
febee99 <febee99@gmail.com>
2019-08-19 17:11:56 +0800

    Minor fixes, changed next to nextLine

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         String line = "____________________________________________________________";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,11 +11,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(line + "\n" + logo);
-        System.out.println("Hello! I'm Duke\n What can I do for you?\n" + line);
-        String input = sc.next();
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n" + line);
+
+        // Reads in input
+        String input = sc.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(line + "\n" + input + "\n" + line);
-            input = sc.next();
+            System.out.println(line + "\n");
+            System.out.println(input + "\n" + line);
+            input = sc.nextLine();
         }
         System.out.println(line + "\n Bye. Hope to see you again soon! \n" + line);
     }

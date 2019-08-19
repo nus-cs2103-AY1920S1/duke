199e262e720cb9c72ded3735a275cc720d30e7a1
minpyaemoe <minpyaemoe@gmail.com>
2019-08-19 11:33:52 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String logo = "____        _        \n"
+                + "\t |  _ \\ _   _| | _____ \n"
+                + "\t | | | | | | | |/ / _ \\\n"
+                + "\t | |_| | |_| |   <  __/\n"
+                + "\t |____/ \\__,_|_|\\_\\___|\n";
+
+        printOutput(" " + logo + "\n\t Hello! I'm Duke" + "\n" + "\t What can I do for you?");
+
+        while(sc.hasNext()) {
+            String input = sc.next();
+            if(input.equals("bye")) {
+                printOutput("Bye. Hope to see you again soon!");
+                return;
+            } else {
+                printOutput(input);
+            }
+        }
+    }
+
+    public static void printOutput(String output) {
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\t " + output);
+        System.out.println("\t____________________________________________________________");
+        System.out.println();
     }
 }
+

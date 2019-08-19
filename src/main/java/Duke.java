74d403ff0a18bccaf1df508c897450c334089961
jakq <42934003+jakq@users.noreply.github.com>
2019-08-19 18:38:52 +0800

    Level-1 : Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,8 @@
+/* Name: Ang Kai Qi
+   MatricNo: A0190206N
+ */
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +11,23 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Duke.greet();
+        Duke.echo();
+    }
+
+    static void greet() {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+
+    static void echo() {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        while (!s.equals("bye")) {
+            System.out.println(s);
+            s = sc.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+        sc.close();
     }
 }

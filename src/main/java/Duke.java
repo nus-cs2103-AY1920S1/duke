27fae28f59b45a3c89bb6ab795f0809168711a0d
marcusteh1238 <marcusteh1238@gmail.com>
2019-08-19 19:35:16 +0800

    Revert "Completed Level 1-1"
    This reverts commit 41f6550d96c0c42abc6a5b4e5d49ab6869230abf.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,39 +1,10 @@
-import java.util.Scanner;
-
 public class Duke {
     public static void main(String[] args) {
-        welcomeMessage();
-
-        Scanner scanner = new Scanner(System.in);
-        String command = "";
-
-        while (!command.equals("bye")) {
-            command = scanner.nextLine();
-            processCommand(command);
-        }
-        scanner.close();
-        System.exit(1);
-    }
-
-    public static void processCommand(String command) {
-        System.out.println("\t____________________________________________________________");
-        if (command.equals("bye")) {
-            System.out.println("\tBye. Hope to see you again soon!");
-        } else {
-            System.out.println("\t" + command);
-        }
-        System.out.println("\t____________________________________________________________");
-    }
-
-    public static void welcomeMessage() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("\t____________________________________________________________");
-        System.out.println("\tHello! I'm Duke\n\tWhat can I do for you?");
-        System.out.println("\t____________________________________________________________");
     }
 }

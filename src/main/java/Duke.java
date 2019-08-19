b2c6775934537c3eaaa7412102e6ee603b14a392
CowSaysBaa <mememe479@gmail.com>
2019-08-19 16:49:16 +0800

    Added: indentations and horizontal lines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Duke\nWhat can i do for you?");
+        String border = "    ____________________________________________________________";
+        System.out.println(border + "\n" + "     Hello! I'm Duke\n     What can i do for you?" + "\n" + border);
 
         ToDoList work = new ToDoList();
         work.run();
--- src/main/java/ToDoList.java
+++ src/main/java/ToDoList.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class ToDoList {
     public void run() {
         Scanner sc = new Scanner(System.in);
+        String border = "    ____________________________________________________________";
 
         String input = sc.nextLine();
         while (!input.equals("bye")){
-            System.out.println(input);
+            System.out.println(border + "\n" + "     " + input + "\n" + border);
             input = sc.nextLine();
         }
         if (input.equals("bye")){
-            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println(border + "\n" + "     Bye. Hope to see you again soon!" + "\n" +border);
         }
     }
 }

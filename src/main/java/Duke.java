b1adf18d22f06190a28bf54a8e673b72fa234f28
jon-chua <jonchua97@gmail.com>
2019-08-20 00:14:05 +0800

    Implement Level-5: Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,7 +18,7 @@ public class Duke {
     }
 
     private Duke() {
-        this.list = new ArrayList<Task>();
+        this.list = new ArrayList<>();
     }
 
     private void runDuke() {
@@ -26,28 +26,75 @@ public class Duke {
 
         boolean isDone = false;
         while (!isDone) {
-            String input = sc.nextLine();
+            String input = sc.nextLine().trim();
 
-            if (isExitCommand(input)) {
-                isDone = true;
-            } else if (isListCommand(input)) {
-                printList();
-            } else if (isDoneCommand(input)) {
-                int oneBasedIndex = Integer.parseInt(input.replace("done ", ""));
-                markTaskAsDone(oneBasedIndex);
-            } else if (isAddTodoCommand(input)) {
-                String todo = input.replace("todo ", "");
-                addTodoToList(todo);
-            } else if (isAddDeadlineCommand(input)) {
-                String[] deadline = input.replace("deadline ", "").split(" /by ");
-                addDeadlineToList(deadline[0], deadline[1]);
-            } else if (isAddEventCommand(input)) {
-                String[] event = input.replace("event ", "").split(" /at ");
-                addEventToList(event[0], event[1]);
+            try {
+                if (isExitCommand(input)) {
+                    isDone = true;
+                } else if (isListCommand(input)) {
+                    printList();
+                } else if (isDoneCommand(input)) {
+                    String newInput = input.replaceFirst("done", "").trim();
+                    int oneBasedIndex = validateDoneInput(newInput);
+                    markTaskAsDone(oneBasedIndex);
+                } else if (isAddTodoCommand(input)) {
+                    String todo = input.replaceFirst("todo", "").trim();
+                    validateTodo(todo);
+                    addTodoToList(todo);
+                } else if (isAddDeadlineCommand(input)) {
+                    String[] deadline = validateEventOrDeadline(input, "deadline", "/by");
+                    addDeadlineToList(deadline[0], deadline[1]);
+                } else if (isAddEventCommand(input)) {
+                    String[] event = validateEventOrDeadline(input, "event", "/at");
+                    addEventToList(event[0], event[1]);
+                } else {
+                    throw new DukeException("\u2639 OOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
+            } catch (DukeException e) {
+                printMessage(e.getMessage());
             }
         }
 
         printExitMessage();
+    }
+
+    private int validateDoneInput(String doneInput) throws DukeException {
+        // Checks that the string is not empty and is an integer
+        if (doneInput.isEmpty() || !doneInput.matches("-?(0|[1-9]\\d*)")) {
+            throw new DukeException("\u2639 OOPS!!! The index to remove cannot be blank or not an integer.");
+        }
+
+        int oneBasedIndex = Integer.parseInt(doneInput);
+        if (oneBasedIndex < 1 || oneBasedIndex > list.size()) {
+            throw new DukeException("\u2639 OOPS!!! The index to remove cannot be less than 0 or "
+                    + "greater than the length of the list.");
+        }
+        return oneBasedIndex;
+    }
+
+    private void validateTodo(String todo) throws DukeException {
+        if (todo.isEmpty()) {
+            throw new DukeException("\u2639 OOPS!!! The description of a todo cannot be empty.");
+        }
+    }
+
+    private String[] validateEventOrDeadline(String input, String textToReplace, String textToSplit) throws DukeException {
+        // Removes textToReplace from input,
+        // and finally split it by textToSplit
+        String[] splitInput = input.replaceFirst(textToReplace, "")
+                .trim().split(textToSplit);
+
+        // Trims all whitespace from the resulting split
+        for (int i = 0; i < splitInput.length; i++) {
+            splitInput[i] = splitInput[i].trim();
+        }
+
+        // Event or deadline should be of length 2 after splitting and both should not be blank
+        if (splitInput.length != 2 || splitInput[0].isBlank() || splitInput[1].isBlank()) {
+            throw new DukeException("\u2639 OOPS!!! I had trouble processing that input.\n" +
+                    "\tPlease make sure that the task description and dates are not empty!");
+        }
+        return splitInput;
     }
 
     private void markTaskAsDone(int oneBasedIndex) {
@@ -100,19 +147,19 @@ public class Duke {
     }
 
     private boolean isDoneCommand(String input) {
-        return input.startsWith("done ");
+        return input.startsWith("done");
     }
 
     private boolean isAddTodoCommand(String input) {
-        return input.startsWith("todo ");
+        return input.startsWith("todo");
     }
 
     private boolean isAddDeadlineCommand(String input) {
-        return input.startsWith("deadline ");
+        return input.startsWith("deadline");
     }
 
     private boolean isAddEventCommand(String input) {
-        return input.startsWith("event ");
+        return input.startsWith("event");
     }
 
     private void printGreetingMessage() {

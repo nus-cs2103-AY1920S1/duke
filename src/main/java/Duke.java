baae64d3cb987ecdf041808162a57b998e67ace4
czkay <chong.zi.kang@gmail.com>
2019-08-19 16:59:13 +0800

    Added Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,8 +7,16 @@ public class Duke {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
         String s;
+        List<String> ls = new ArrayList<>();
         while (!(s = sc.nextLine()).equals("bye")) {
-            System.out.println(s);
+            if (s.equals("list")) {
+                for (int i = 1; i <= ls.size(); i++) {
+                    System.out.println(String.format("%d. %s", i, ls.get(i - 1)));
+                }
+            } else {
+                ls.add(s);
+                System.out.println("added: " + s);
+            }
         }
 
         System.out.println("Bye. Hope to see you again soon!");

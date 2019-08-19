277c49662b5b37ca2e71025247b180dbd57b3b5f
jxofficial <jxkeng119@gmail.com>
2019-08-19 17:37:09 +0800

    Updated formatting for Task toString() method Implemented markAsCompleted method

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,15 +1,26 @@
 public class Task {
     private int taskID;
     private String taskName;
+    private boolean isCompleted;
 
     public Task(int taskID, String taskName) {
         this.taskID = taskID;
         this.taskName = taskName;
+        this.isCompleted = false;
     }
 
 
+    public void markAsCompleted() {
+        this.isCompleted = true;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
     @Override
     public String toString() {
-        return taskID + ". " + taskName;
+        char symbol = isCompleted ? '✓' : '✗';
+        return "\t " + taskID + ".[" + symbol + "] " + taskName;
     }
 }

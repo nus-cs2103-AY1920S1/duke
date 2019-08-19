35ce2da49af95434f1376db4859abf9469ff8e39
Crisgu <E0318787@u.nus.edu>
2019-08-19 23:06:53 +0800

    lv1 cris 2019/08/19 22:57

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class Duke {
+    /*
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -7,4 +10,30 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
     }
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello! I'm Duke\n" +
+                           "     What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+        while(sc.hasNextLine()){
+            String s = sc.nextLine();
+            if (s.equalsIgnoreCase("bye")){
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     Bye. Hope to see you again soon!");
+                System.out.println("    ____________________________________________________________");
+                break;
+
+            }
+            else{
+                System.out.println("    ____________________________________________________________");
+            System.out.println("     "+s);
+                System.out.println("    ____________________________________________________________");
+            }
+        }
+
+
+    }
+
 }

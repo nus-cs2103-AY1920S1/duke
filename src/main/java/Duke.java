4da380295bf5e30356eef2b810a42a70f9cbae48
Zhong Jun <zhongjun@jorked.com>
2019-08-19 13:42:34 +0800

    Level 2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] items = new String[100];
+        int counter = 0;
 
         String hello = "Hello! I'm Duke\nWhat can I do for you?";
         System.out.println(hello);
@@ -13,8 +15,19 @@ public class Duke {
             if (next.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (next.equals("list")) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < counter; i++) {
+                    sb.append(i + 1);
+                    sb.append(". ");
+                    sb.append(items[i]);
+                    sb.append('\n');
+                }
+                System.out.print(sb);
             } else {
-                System.out.println(next);
+                items[counter] = next;
+                counter++;
+                System.out.println("added: " + next);
             }
         }
 

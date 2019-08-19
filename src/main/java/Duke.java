57cb9f24787b8418193e46685cc2db094c9af673
choonx99 <choonx99@hotmail.com>
2019-08-19 12:00:16 +0800

    added Duke exception classes, added function to handle input by throwing exceptions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Duke {
+    public static ArrayList<Task> taskList = new ArrayList<>();
+
     public static void main(String[] args) {
         greet();
         readInput();
@@ -19,15 +21,25 @@ public class Duke {
 
     public static void readInput() {
         Scanner input = new Scanner(System.in);
-        ArrayList<Task> taskList = new ArrayList<>();
-
         String userInput = input.nextLine();
 
         while (!userInput.equals("bye")) {
             if (userInput.equals("list")) {
                 printTaskList(taskList);
                 userInput = input.nextLine();
-            } else if (userInput.startsWith("done")) {
+                continue;
+            }
+            String[] userInputSplit = userInput.split(" ");
+
+            try {
+                handleInput(userInputSplit[0], userInput);
+            } catch (DukeException ex) {
+                System.out.println(ex.getMessage());
+                userInput = input.nextLine();
+                continue;
+            }
+
+            if (userInput.startsWith("done")) {
                 int taskNumber = Integer.parseInt(userInput.substring(5));
                 Task selectedTask = taskList.get(taskNumber - 1);
                 selectedTask.markAsDone();
@@ -35,18 +47,75 @@ public class Duke {
                 userInput = input.nextLine();
             } else {
                 if (userInput.startsWith("todo")) {
-                    addTaskToTaskList(taskList, new ToDo(userInput.substring(5)));
+                    String description = userInput.substring(5);
+                    addTaskToTaskList(taskList, new ToDo(description));
                 } else if (userInput.startsWith("deadline")) {
                     String description = userInput.substring(9, userInput.indexOf('/') - 1);
                     String by = userInput.substring(14 + description.length());
+                    try {
+                        handleInput("by", by);
+                    } catch (DukeException ex) {
+                        System.out.println(ex.getMessage());
+                    }
                     addTaskToTaskList(taskList, new Deadline(description, by));
                 } else if (userInput.startsWith("event")) {
                     String description = userInput.substring(6, userInput.indexOf('/') - 1);
                     String at = userInput.substring(11 + description.length());
+                    try {
+                        handleInput("at", at);
+                    } catch (DukeException ex) {
+                        System.out.println(ex.getMessage());
+                    }
                     addTaskToTaskList(taskList, new Event(description, at));
                 }
                 userInput = input.nextLine();
             }
+        }
+    }
+
+    public static void handleInput(String type, String userInput) throws EmptyFieldException, InvalidInputException{
+        if (type.equals("done")) {
+            if (userInput.substring(4).isBlank()) {
+                throw new EmptyFieldException("☹ OOPS!!! The task number cannot be empty.");
+            } else if (Integer.parseInt(userInput.substring(5)) > taskList.size()) {
+                throw new InvalidInputException("☹ OOPS!!! You do not have that many tasks yet.");
+            }
+        } else if (type.equals("todo")) {
+            if (userInput.substring(4).isBlank()) {
+                throw new EmptyFieldException("☹ OOPS!!! The description of a todo cannot be empty.");
+            }
+        } else if (type.equals("deadline")) {
+            if (userInput.substring(8).isBlank()) {
+                throw new EmptyFieldException("☹ OOPS!!! The new deadline cannot be empty.");
+            } else if (!userInput.contains("/by")) {
+                throw new EmptyFieldException("☹ OOPS!!! The deadline of a deadline cannot be empty.");
+            }
+            String description = userInput.substring(9, userInput.indexOf('/'));
+            if (description.isBlank()) {
+                throw new EmptyFieldException("☹ OOPS!!! The description of a deadline cannot be empty.");
+            }
+        } else if (type.equals("by")) {
+            if (userInput.isBlank()) {
+                throw new EmptyFieldException("☹ OOPS!!! The deadline of a deadline cannot be empty.");
+            }
+        } else if (type.equals("event")) {
+            if (userInput.substring(5).isBlank()) {
+                throw new EmptyFieldException("☹ OOPS!!! The new event cannot be empty.");
+            } else if (!userInput.contains("/at")) {
+                throw new EmptyFieldException("☹ OOPS!!! The 'at' field of an event cannot be empty.");
+            }
+            String description = userInput.substring(6, userInput.indexOf('/'));
+            if (description.isBlank()) {
+                throw new EmptyFieldException("☹ OOPS!!! The description of an event cannot be empty.");
+            }
+        } else if (type.equals("at")) {
+            if (userInput.isBlank()) {
+                throw new EmptyFieldException("☹ OOPS!!! The 'at' field of an event cannot be empty.");
+            }
+        } else if (type.isBlank() && userInput.isBlank()) {
+            throw new EmptyFieldException("☹ OOPS!!! I'm sorry, but you have to input something");
+        } else {
+            throw new InvalidInputException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
         }
     }
 

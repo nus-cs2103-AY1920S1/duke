788ea245e31bfd9e9da55ffdef5de8eb56c68992
SoilingRogue <yuan.xinran.stanley@gmail.com>
2019-08-19 22:36:48 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -16,10 +17,9 @@ public class Duke {
         return String.format("Now you have %d tasks in the list.", list.size());
     }
 
-    private static void addTask(String task, LinkedList<Task> list) {
+    private static void generateTaskMessage(String message, String task, LinkedList<Task> list) {
         LinkedList<String> taskToAdd = new LinkedList<>();
-        String taskString = "Got it. I've added this task:";
-        taskToAdd.addLast(taskString);
+        taskToAdd.addLast(message);
         taskToAdd.addLast(task);
         taskToAdd.addLast(getTasks(list));
         printStandard(taskToAdd);
@@ -32,6 +32,9 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String addString = "Got it. I've added this task:";
+        String delString = "Noted. I've removed this task:";
+
         System.out.println("Hello from\n" + logo);
 
         LinkedList<Task> lst = new LinkedList<>();
@@ -67,19 +70,34 @@ public class Duke {
                     printStandard(list);
                     break;
                 case "done":
-                    int val = Integer.parseInt(arr[1]);
-                    Task temp = lst.get(val - 1);
-                    temp.markAsDone();
-                    LinkedList<String> done = new LinkedList<>();
-                    done.addLast("Nice! I've marked this task as done:");
-                    done.addLast(String.format("%s", temp));
-                    printStandard(done);
+                    try {
+                        int val = Integer.parseInt(arr[1]);
+                        if (arr.length > 2) throw new InputMismatchException();
+                        Task temp = lst.get(val - 1);
+                        temp.markAsDone();
+                        LinkedList<String> done = new LinkedList<>();
+                        done.addLast("Nice! I've marked this task as done:");
+                        done.addLast(String.format("%s", temp));
+                        printStandard(done);
+                    } catch (IndexOutOfBoundsException e) {
+                        error.addLast("☹ OOPS!!! The index to be completed is not within the task list size.");
+                        printStandard(error);
+                        error.removeFirst();
+                    } catch (NumberFormatException e) {
+                        error.addLast("☹ OOPS!!! Please enter a valid number.");
+                        printStandard(error);
+                        error.removeFirst();
+                    } catch (InputMismatchException e) {
+                        error.addLast("☹ OOPS!!! Please enter a single number.");
+                        printStandard(error);
+                        error.removeFirst();
+                    }
                     break;
                 case "todo":
                     try {
                         Todo todoTask = Todo.of(next);
                         lst.addLast(todoTask);
-                        addTask(String.format("%s", todoTask), lst);
+                        generateTaskMessage(addString, String.format("%s", todoTask), lst);
                     } catch (DukeException e) {
                         error.addLast(e.getMessage());
                         printStandard(error);
@@ -91,7 +109,7 @@ public class Duke {
                         String[] deadlineArr = next.split("/by");
                         Deadline deadlineTask = Deadline.of(deadlineArr[0], deadlineArr[1]);
                         lst.addLast(deadlineTask);
-                        addTask(String.format("%s", deadlineTask), lst);
+                        generateTaskMessage(addString, String.format("%s", deadlineTask), lst);
                     } catch (DukeException e) {
                         error.addLast(e.getMessage());
                         printStandard(error);
@@ -103,9 +121,29 @@ public class Duke {
                         String[] eventArr = next.split("/at");
                         Event eventTask = Event.of(eventArr[0], eventArr[1]);
                         lst.addLast(eventTask);
-                        addTask(String.format("%s", eventTask), lst);
+                        generateTaskMessage(addString, String.format("%s", eventTask), lst);
                     } catch (DukeException e) {
                         error.addLast(e.getMessage());
+                        printStandard(error);
+                        error.removeFirst();
+                    }
+                    break;
+                case "delete":
+                    try {
+                        int del = Integer.parseInt(arr[1]);
+                        if (arr.length > 2) throw new InputMismatchException();
+                        Task removed = lst.remove(del - 1);
+                        generateTaskMessage(delString, String.format("%s", removed), lst);
+                    } catch (IndexOutOfBoundsException e) {
+                        error.addLast("☹ OOPS!!! The index to delete is not within the task list size.");
+                        printStandard(error);
+                        error.removeFirst();
+                    } catch (NumberFormatException e) {
+                        error.addLast("☹ OOPS!!! Please enter a valid number.");
+                        printStandard(error);
+                        error.removeFirst();
+                    } catch (InputMismatchException e) {
+                        error.addLast("☹ OOPS!!! Please enter a single number.");
                         printStandard(error);
                         error.removeFirst();
                     }

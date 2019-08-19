aa46268a3a407a7ed7043fc4a0823927281f5eda
Icesiolz <Icesiolz@users.noreply.github.com>
2019-08-19 11:22:48 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,26 +6,37 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
 
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
 
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
-        int i = 1;
         while (sc.hasNextLine()) {
+            System.out.println("____________________________________________________________");
             String input = sc.nextLine();
-            switch(input) {
-                case "bye":
-                    System.out.println("Bye. Hope to see you again soon!");
-                    break;
-                case "list":
-                    for (String s: list) {
-                        System.out.println(i + ". " + s);
-                        i++;
+            String[] words = input.split(" ");
+            if (words[0].equals("done")) {
+                int index = Integer.parseInt(words[1]);
+                Task t = list.get(index - 1);
+                t.markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + t.toString());
+            } else {
+                    switch (input) {
+                        case "bye":
+                            System.out.println("Bye. Hope to see you again soon!");
+                            System.out.println("____________________________________________________________");
+                            break;
+                        case "list":
+                            System.out.println("Here are the tasks in your list:");
+                            for (int i = 1; i <= list.size(); i++) {
+                                System.out.println(i + "." + list.get(i-1).toString());
+                            }
+                            break;
+                        default:
+                            list.add(new Task(input));
+                            System.out.println("added: " + input);
+
                     }
-                    break;
-                default:
-                    list.add(input);
-                    System.out.println("added: " + input);
             }
         }
     }

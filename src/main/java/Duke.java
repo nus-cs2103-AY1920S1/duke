900fd26a04d8bd6ab889895bfe6d5e5a6a351c14
krusagiz <acgmelvin@gmail.com>
2019-08-19 13:46:38 +0800

    [Level-2] Add, List ~done~

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,11 @@
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Duke {
     static final String welcomeMsg = "Hello! I'm Duke\n" +
             "What can I do for you?";
     static final String exitMsg = "Bye. Hope to see you again soon!";
+    static final LinkedList<Task> taskList = new LinkedList<Task>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -22,13 +24,33 @@ public class Duke {
             String input;
             input = sc.nextLine();
             switch (input) {
+                case "list":
+                    listTasks();
+                    break;
                 case "bye":
-                    CmdInterface.printHBars(exitMsg);
+                    exitApp();
                     isGoodbye = true;
                     break;
                 default:
-                    CmdInterface.printHBars(input);
+                    addTask(input);
             }
         }
+    }
+
+    public static void exitApp() {
+        CmdInterface.printHBars(exitMsg);
+    }
+    public static void addTask(String taskName) {
+        taskList.add(new Task(taskName));
+        CmdInterface.printHBars("added: " + taskName);
+    }
+
+    public static void listTasks() {
+        StringBuilder sb = new StringBuilder();
+        int i = 1;
+        for (Task task : taskList) {
+            sb.append(i++ + ". " + task.getTaskName() + "\n");
+        }
+        CmdInterface.printHBars(sb.toString());
     }
 }

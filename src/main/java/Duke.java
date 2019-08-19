5203b3c01e1f75b494d85fea17f19a6ef2015775
Bruce Ong <bruceskellator@hotmail.com>
2019-08-19 09:52:04 +0800

    src/main/java/Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        // test
         Scanner scanner = new Scanner(System.in);
 
         Task[] itemsLst = new Task[100];

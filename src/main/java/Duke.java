973d5583d709d4ba1d3503f20bc1e21f4f3fdf77
hoholyin <holyin97@gmail.com>
2019-08-19 23:42:32 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,42 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static String[] list = new String[100];
+    private static int count = 0;
     public static void main(String[] args) {
+        welcomeMessage();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+          String input = sc.nextLine();
+          if (input.equals("bye")) {
+            byeMessage();
+            break;
+          } else if (input.equals("list")) {
+            displayList();
+          } else {
+            addToList(input);
+            echo(input);
+          }
+        }
+        sc.close();
+    }
+
+    static void displayList() {
+      String line = "    ________________________" 
+          + "____________________________________";
+      System.out.println(line);
+      for (int i = 0; i < count; i++) {
+        int index = i + 1;
+        System.out.println("    " + index + ". " + list[i]);
+      }
+      System.out.println(line);
+    }
+
+    static void addToList(String s) {
+      list[count] = s;
+      count++;
+    }
+    static void welcomeMessage() {
         String logo = "     ____        _        \n"
                 + "    |  _ \\ _   _| | _____ \n"
                 + "    | | | | | | | |/ / _ \\\n"
@@ -13,21 +48,21 @@ public class Duke {
         System.out.println("    Hello from\n" + logo);
         System.out.println("    What can I do for you?");
         System.out.println(line + "\n");
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-          String input = sc.next();
-          if (input.equals("bye")) {
-            echo("Bye. Hope to see you again soon!");
-            break;
-          }
-          echo(input);
-        }
     }
+
+    static void byeMessage() {
+        String line = "    ________________________" 
+          + "____________________________________";
+        System.out.println(line);
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println(line + "\n");
+    }
+    
     public static void echo(String s) {
         String line = "    ________________________" 
             + "____________________________________";
         System.out.println(line);
-        System.out.println("    " + s);
+        System.out.println("     added: " + s);
         System.out.println(line + "\n");
         return;
     }

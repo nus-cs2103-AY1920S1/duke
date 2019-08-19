a01b757c0ebfe479e2f6fd818b8343e6da7c1ae4
seahlynn <e0324409@u.nus.edu>
2019-08-19 15:42:17 +0800

    increment 5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,10 +15,14 @@ public class Duke {
                         System.out.println((i+1) + "." + taskList.get(i).toString());
                     }
                 } else if((command.split(" ")[0]).equals("done")) {
-                    int numChange = Integer.parseInt(command.split(" ")[1]) - 1;
-                    taskList.get(numChange).markAsDone();
-                    System.out.println("Nice! I've marked this task as done:" + "\n" +
-                            taskList.get(numChange).toString());
+                    if(command.split(" ").length == 1) {
+                        throw new DukeException("☹ OOPS!!! Please indicate which task you have completed.");
+                    } else {
+                        int numChange = Integer.parseInt(command.split(" ")[1]) - 1;
+                        taskList.get(numChange).markAsDone();
+                        System.out.println("Nice! I've marked this task as done:" + "\n" +
+                                taskList.get(numChange).toString());
+                    }
                 } else if((command.split(" ")[0]).equals("todo")||(command.split(" ")[0]).equals("deadline")||(command.split(" ")[0]).equals("event")) {
                     String type = command.split(" ")[0];
 
@@ -33,36 +37,37 @@ public class Duke {
                                     + "\n" + "Now you have " + numTasks + " tasks in the list.");
                         } else if(type.equals("deadline")) {
                             String[] commandSplit= command.split("/");
-                            Task newTask = new Deadline(commandSplit[0].substring(9), commandSplit[1].substring(3));
-                            taskList.add(newTask);
-                            int numTasks = taskList.size();
-                            System.out.println("Got it. I've added this task:" + "\n" + newTask.toString()
-                                    + "\n" + "Now you have " + numTasks + " tasks in the list.");
+                            if(commandSplit.length == 1) {
+                                throw new DukeException(" ☹ OOPS!!! Please enter a deadline for your task.");
+                            } else{
+                                Task newTask = new Deadline(commandSplit[0].substring(9), commandSplit[1].substring(3));
+                                taskList.add(newTask);
+                                int numTasks = taskList.size();
+                                System.out.println("Got it. I've added this task:" + "\n" + newTask.toString()
+                                        + "\n" + "Now you have " + numTasks + " tasks in the list.");
+                            }
                         } else {
                             String[] commandSplit= command.split("/");
-                            Task newTask = new Events(commandSplit[0].substring(6), commandSplit[1].substring(3));
-                            taskList.add(newTask);
-                            int numTasks = taskList.size();
-                            System.out.println("Got it. I've added this task:" + "\n" + newTask.toString()
-                                    + "\n" + "Now you have " + numTasks + " tasks in the list.");
+                            if(commandSplit.length == 1) {
+                                throw new DukeException(" ☹ OOPS!!! Please enter a time for your task.");
+                            } else{
+                                Task newTask = new Events(commandSplit[0].substring(6), commandSplit[1].substring(3));
+                                taskList.add(newTask);
+                                int numTasks = taskList.size();
+                                System.out.println("Got it. I've added this task:" + "\n" + newTask.toString()
+                                        + "\n" + "Now you have " + numTasks + " tasks in the list.");
+                            }
                         }
-
                     }
-
                 } else {
                     throw new DukeException(" ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
-
             } catch (DukeException ex){
                 System.out.println(ex.getMessage());
             }
-
         }
-
         System.out.println("Bye. Hope to see you again soon!");
     }
-
-
 }
 
 

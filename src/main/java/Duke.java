1348c8739dcd1f381fb5c6ac689bbb54a9182b85
jeongyh99 <jeong.yuhan@gmail.com>
2019-08-19 20:44:22 +0800

    Commit Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,7 +7,6 @@ public class Duke {
         //dukeSayHello();
         greetings();
 
-        //level 4
         Scanner sc = new Scanner(System.in);
         ItemsList itemsList = new ItemsList();
         while (sc.hasNext()) {
@@ -24,27 +23,51 @@ public class Duke {
                 Task task = itemsList.getTaskAtIndex(itemIndex - 1);
                 task.markAsDone();
             }
-            else { // new item
+            else if (command.equals("todo") || command.equals("deadline") || command.equals("event")){ // new item
                 addNewItem(command, sc, itemsList);
+            }
+            else {
+                System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                sc.nextLine();
             }
         }
     }
 
     private static void addNewItem(String command, Scanner sc, ItemsList itemsList) {
         String item = sc.nextLine().trim();
+        if (item.equals("")) {
+            System.out.printf("☹ OOPS!!! The description of a %s cannot be empty.\n", command);
+            return;
+        }
         Task task;
         if (command.equals("todo")) {
             task = new ToDo(item);
         }
         else if (command.equals("deadline")) {
-            String[] itemSlashDeadline = item.split("/");
-            String deadline = itemSlashDeadline[1].substring(3);
-            task = new Deadline(itemSlashDeadline[0].trim(), deadline);
+            try {
+                String[] itemSlashDeadline = item.split("/");
+                String deadline = itemSlashDeadline[1].substring(3);
+                task = new Deadline(itemSlashDeadline[0].trim(), deadline);
+            }
+            catch (ArrayIndexOutOfBoundsException ex) {
+                System.out.println("Invalid deadline input");
+                return;
+            }
         }
-        else { //command.equals("event")
-            String[] itemsSlashTiming = item.split("/");
-            String timing = itemsSlashTiming[1].substring(3);
-            task = new Event(itemsSlashTiming[0].trim(), timing);
+        else if (command.equals("event")) {
+            try {
+                String[] itemsSlashTiming = item.split("/");
+                String timing = itemsSlashTiming[1].substring(3);
+                task = new Event(itemsSlashTiming[0].trim(), timing);
+            }
+            catch (ArrayIndexOutOfBoundsException ex){
+                System.out.println("Invalid event input");
+                return;
+            }
+        }
+        else {
+            System.out.println("Task type can only be of type todo, deadline or event");
+            return;
         }
         itemsList.addItem(task);
     }

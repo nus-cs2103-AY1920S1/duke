91b5a29214d78e72cb855b2ce465cd59d72beaca
goh.sining <gohsining99@gmail.com>
2019-08-19 15:31:28 +0800

    Add support for tracking 3 types of tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -20,29 +20,42 @@ public class Duke {
      */
     public static void activeChat() {
         Scanner sc = new Scanner(System.in);
+        String input = sc.next();
 
-        String input = sc.nextLine();
         while (!input.equals("bye")) {
-            String[] splitInput = input.split(" ");
             if (input.equals("list")) {
                 printList();
-            } else if (splitInput[0].equals("done")) {
-                int taskNum = Integer.parseInt(splitInput[1]);
-                Task target = memory[taskNum - 1];
-                target.markAsDone();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("[" + target.getStatusIcon() + "] " + target.description);
-            } else {
-                Task a = new Task(input);
-                memory[index - 1] = a;
-                System.out.println("added: " + input);
+            } else if (input.equals("done")) {
+                int taskNum = sc.nextInt();
+                doneTask(taskNum);
+            } else if (input.equals("todo")) {
+                String TodoTask = sc.nextLine();
+                toDoTask(TodoTask);
+                index++;
+            } else if (input.equals("event")) {
+                String EventTask = sc.nextLine();
+                eventTask(EventTask);
+                index++;
+            } else if (input.equals("deadline")) {
+                String DeadlineTask = sc.nextLine();
+                deadlineTask(DeadlineTask);
                 index++;
             }
-            input = sc.nextLine();
+            input = sc.next();
         }
         if (input.equals("bye")) {
             System.out.println("Bye. Hope to see you again soon!");
         }
+    }
+
+    /*
+    This method marks the targeted task as done and provides confirmation to the user through output.
+     */
+    public static void doneTask(int taskNum) {
+        Task target = memory[taskNum - 1];
+        target.markAsDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("[" + target.getStatusIcon() + "] " + target.description);
     }
 
     /*
@@ -53,28 +66,43 @@ public class Duke {
         for (int i = 0; i < index - 1; i++) {
             int id = i + 1;
             Task current = memory[i];
-            System.out.println(id + ".[" + current.getStatusIcon() + "] " + current.description);
+            System.out.println(id + "." + current.toString());
         }
     }
+
+    /*
+    This method creates a ToDo object and adds it into memory.
+     */
+    public static void toDoTask(String a) {
+        ToDo newTodo = new ToDo(a);
+        memory[index - 1] = newTodo;
+        printTask(newTodo);
+    }
+
+    /*
+    This method creates an Event object and adds it into memory.
+     */
+    public static void eventTask(String b) {
+        String[] details = b.split("/at");
+        Event newEvent = new Event(details[0], details[1]);
+        memory[index - 1] = newEvent;
+        printTask(newEvent);
+    }
+
+    /*
+    This method creates a deadlineTask and adds it into memory.
+     */
+    public static void deadlineTask(String c) {
+        String[] details = c.split("/by");
+        Deadline newDeadline = new Deadline(details[0], details[1]);
+        memory[index - 1] = newDeadline;
+        printTask(newDeadline);
+    }
+
+    public static void printTask(Task task) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println(task);
+        System.out.println("Now you have " + index + " tasks in the list.");
+    }
 }
 
-/*
-A child class of Object which contains the description of the Task and whether the Task has been done.
- */
-class Task {
-    protected String description;
-    protected boolean isDone;
-
-    public Task(String description) {
-        this.description = description;
-        this.isDone = false;
-    }
-
-    public String getStatusIcon() {
-        return (isDone ? "\u2713" : "\u2718");
-    }
-
-    public void markAsDone() {
-        this.isDone = true;
-    }
-}

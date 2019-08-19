e99b2fa51c1bbea37b2138d6fd79e16878a0ef3b
Crisgu <E0318787@u.nus.edu>
2019-08-19 23:42:36 +0800

    lv1 cris 2019/08/19 23:42

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -12,28 +13,55 @@ public class Duke {
     }
      */
     public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
         Scanner sc = new Scanner(System.in);
-        System.out.println("    ____________________________________________________________");
+        boolean flag = true;
+        drawline();
         System.out.println("     Hello! I'm Duke\n" +
                            "     What can I do for you?");
-        System.out.println("    ____________________________________________________________");
-        while(sc.hasNextLine()){
+        drawline();
+        while(flag){
             String s = sc.nextLine();
-            if (s.equalsIgnoreCase("bye")){
-                System.out.println("    ____________________________________________________________");
-                System.out.println("     Bye. Hope to see you again soon!");
-                System.out.println("    ____________________________________________________________");
-                break;
+            switch (s) {
+                case "bye" :
+                    bye();
+                    flag = false;
+                    break;
+                case "list" :
+                    list(list);
+                    break;
+                default:
+                    echo(list, s);
+                    break;
 
-            }
-            else{
-                System.out.println("    ____________________________________________________________");
-            System.out.println("     "+s);
-                System.out.println("    ____________________________________________________________");
             }
         }
 
 
+    }
+
+    public static void drawline(){
+        System.out.println("    ____________________________________________________________");
+    }
+    public static void bye(){
+        drawline();
+        System.out.println("     Bye. Hope to see you again soon!");
+        drawline();
+
+    }
+    public static void list(ArrayList<String>list){
+        drawline();
+        for(int n=0; n<list.size();n++) {
+            int m = n+1;
+            System.out.println("     " + m + ". " + list.get(n));
+        }
+        drawline();
+    }
+    public static void echo(ArrayList<String>list,String s){
+        list.add(s);
+        drawline();
+        System.out.println("     added: "+s);
+        drawline();
     }
 
 }

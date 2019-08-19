c3c0c233da3fb133023b5a6cbfa48e0356db263e
jakq <42934003+jakq@users.noreply.github.com>
2019-08-19 18:54:49 +0800

    Level-2 : Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,8 +2,11 @@
    MatricNo: A0190206N
  */
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    static Scanner sc = new Scanner(System.in);
+    static ArrayList<String> arrList = new ArrayList<String>();
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -12,7 +15,7 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         Duke.greet();
-        Duke.echo();
+        Duke.addList();
     }
 
     static void greet() {
@@ -20,14 +23,33 @@ public class Duke {
         System.out.println("What can I do for you?");
     }
 
-    static void echo() {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
+    static void addList() {
+        String s = Duke.sc.nextLine();
         while (!s.equals("bye")) {
-            System.out.println(s);
-            s = sc.nextLine();
+            if (s.equals("list")) {
+                Duke.checkList();
+            } else {
+                Duke.echo(s);
+            }
+            s = Duke.sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
-        sc.close();
+        Duke.sc.close();
+    }
+
+    static void checkList() {
+        if (arrList.isEmpty()) {
+            return;
+        }
+        int i = 1;
+        for (String s : arrList) {
+            System.out.println(i + ". " + s);
+            i++;
+        }
+    }
+
+    static void echo(String s) {
+        System.out.println("added: " + s);
+        arrList.add(s);
     }
 }

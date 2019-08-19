6b62f6ed065907b88f66ea9a0ad0bbec05bc7bd8
ellieyee <ellieyeewenna@gmail.com>
2019-08-19 15:04:52 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,12 +10,27 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        addList();
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    /**
+     * Adds commands to lists and prints them out when requested
+     */
+    static void addList() {
+        List<String> list = new ArrayList<String>();
         Scanner sc = new Scanner(System.in);
         String command = sc.nextLine();
         while(!command.equals("bye")) {
-            System.out.println(command);
+            if(command.equals("list")) {
+                for(int i = 0; i < list.size(); i++) {
+                    System.out.println((i+1) + ". " + list.get(i));
+                }
+            } else {
+                list.add(command);
+                System.out.println("added: " + command);
+            }
             command = sc.nextLine();
         }
-        System.out.println("Bye. Hope to see you again soon!");
     }
 }

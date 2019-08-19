303d14625bdc26a9c31d10b9d35885092b2ba18f
ckb055 <chenkaibin7@gmail.com>
2019-08-19 21:39:46 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,6 +17,7 @@ public class Duke {
         while (true) {
             String next = sc.nextLine();
             String firstWord = next.split(" ")[0];
+            //String remaining = next.split(" ", 2)[1];
             if (next.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
             } else if (next.equals("list")) {
@@ -28,9 +29,32 @@ public class Duke {
                 tasks.get(index - 1).markAsDone();
                 System.out.println("Nice! I've marked this task as done\n" + tasks.get(index - 1));
             } else {
-                Task task = new Task(next);
-                tasks.add(task);
-                System.out.println("added: " + next);
+                String remaining = next.split(" ", 2)[1];
+                String desc = remaining.split("/")[0];
+                switch (firstWord) {
+                    case "todo":
+                        Task todo = new ToDo(desc);
+                        tasks.add(todo);
+                        System.out.println("Got it. I've added this task: \n\t"
+                                + todo + "\n" + "Now you have " + tasks.size() + " tasks in the list.");
+                        break;
+                    case "deadline":
+                        String dueDateDeadline = remaining.split("/by ")[1];
+                        Task deadline = new Deadline(desc, dueDateDeadline);
+                        tasks.add(deadline);
+                        System.out.println("Got it. I've added this task: \n\t"
+                                + deadline + "\n" + "Now you have " + tasks.size() + " tasks in the list.");
+                        break;
+                    case "event":
+                        String dueDateEvent = remaining.split("/at ")[1];
+                        Task event = new Event(desc, dueDateEvent);
+                        tasks.add(event);
+                        System.out.println("Got it. I've added this task: \n\t"
+                                + event + "\n" + "Now you have " + tasks.size() + " tasks in the list.");
+                        break;
+                    default:
+                        System.out.println("error, no such command");
+                }
             }
         }
     }

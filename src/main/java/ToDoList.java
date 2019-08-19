e1f1da1b60ad4e1f167dde4a531765d5cd5d7faa
CowSaysBaa <mememe479@gmail.com>
2019-08-19 17:14:37 +0800

    Updated: ToDoList.java to accomplish task 2

--- src/main/java/ToDoList.java
+++ src/main/java/ToDoList.java
@@ -4,10 +4,22 @@ public class ToDoList {
     public void run() {
         Scanner sc = new Scanner(System.in);
         String border = "    ____________________________________________________________";
+        String[] arr = new String[100];
+        int counter = 0;
 
         String input = sc.nextLine();
         while (!input.equals("bye")){
-            System.out.println(border + "\n" + "     " + input + "\n" + border);
+            if(input.equals("list")){
+                System.out.println(border);
+                for (int i = 0; i < counter; i++){
+                    System.out.println("     " + (i + 1) + ". " + arr[i]);
+                }
+                System.out.println(border);
+            } else {
+                System.out.println(border + "\n" + "     added: " + input + "\n" + border);
+                arr[counter] = input;
+                counter ++;
+            }
             input = sc.nextLine();
         }
         if (input.equals("bye")){

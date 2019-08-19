1a14c39bb16dd6f22fc43a3a0f9fa4aab03c7f2e
goh.sining <gohsining99@gmail.com>
2019-08-19 14:31:56 +0800

    Add tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,12 @@
+//level 2
+import java.util.Scanner;
+
 import java.util.Scanner;
 
 public class Duke {
+    static String[] memory = new String[100];
+    static int index = 1;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -17,18 +23,27 @@ public class Duke {
      */
     public static void activeChat() {
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
+
+        String input = sc.nextLine();
         while (!input.equals("bye")) {
             if (input.equals("list")) {
-                System.out.println("list");
-            } else if (input.equals("blah")) {
-                System.out.println("blah");
+                printList();
+            } else {
+                memory[index - 1] = input;
+                System.out.println("added: " + input);
+                index++;
             }
-            input = sc.next();
+            input = sc.nextLine();
         }
         if (input.equals("bye")) {
             System.out.println("Bye. Hope to see you again soon!");
         }
     }
-}
 
+    public static void printList() {
+        for (int i = 0; i < index - 1; i++) {
+            int id = i + 1;
+            System.out.println(id + ". " + memory[i]);
+        }
+    }
+}

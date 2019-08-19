63de287d02c546cdf8e5709d94d83eccd984479c
seeziyang <44959961+seeziyang@users.noreply.github.com>
2019-08-19 11:38:35 +0800

    Add Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,7 +8,7 @@ public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input;
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -20,12 +20,21 @@ public class Duke {
 
         while (sc.hasNext()) {
             input = sc.nextLine();
+            String[] inputWordArr = input.split(" ");
+
             if (input.equals("bye")) {
                 break;
             } else if (input.equals("list")) {
                 printTasks(tasks);
+            } else if (inputWordArr[0].equals("done")) {
+                Task task = tasks.get(Integer.parseInt(inputWordArr[1]) - 1);
+                task.markAsDone();
+                System.out.println(horizontalLine);
+                System.out.println("     Nice! I've marked this task as done:");
+                System.out.println("       " + task);
+                System.out.println(horizontalLine);
             } else {
-                tasks.add(input);
+                tasks.add(new Task(input));
 
                 System.out.println(horizontalLine);
                 System.out.println("     added: " + input);
@@ -46,11 +55,11 @@ public class Duke {
         System.out.println();
     }
 
-    private static void printTasks(ArrayList<String> tasks) {
+    private static void printTasks(ArrayList<Task> tasks) {
         System.out.println(horizontalLine);
 
         int id = 1;
-        for (String task : tasks) {
+        for (Task task : tasks) {
             System.out.println("     " + id + ". " + task);
             id++;
         }
@@ -59,6 +68,7 @@ public class Duke {
     }
 
     private static void printExit() {
+        // Exit
         System.out.println(horizontalLine);
         System.out.println("     Bye. Hope to see you again soon!");
         System.out.println(horizontalLine);

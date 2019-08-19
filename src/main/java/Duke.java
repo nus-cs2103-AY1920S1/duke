ed2a7fd7ac42e4192c1c49599bde7f6a61c851ab
goh.sining <gohsining99@gmail.com>
2019-08-19 13:21:00 +0800

    Echo commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,27 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+        activeChat();
+    }
+
+    /*
+    This method queries for user input and returns the desired result.
+     */
+    public static void activeChat() {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        while (!input.equals("bye")) {
+            if (input.equals("list")) {
+                System.out.println("list");
+            } else if (input.equals("blah")) {
+                System.out.println("blah");
+            }
+            input = sc.next();
+        }
+        if (input.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+        }
     }
 }
+

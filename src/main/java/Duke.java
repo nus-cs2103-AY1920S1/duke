6b8b2c95eb6b146ef79533108400ebca18781443
chanjunren <chanjunren@gmail.com>
2019-08-19 17:40:49 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -9,13 +10,21 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> items = new ArrayList<>();
+
         while (true) {
             String input = sc.nextLine();
-            if (input.equals("bye")) {
+            if (input.equals("list")) {
+                for (int i = 0; i < items.size(); i++) {
+                    System.out.println(i + 1 + ". " + items.get(i));
+                }
+            }
+            else if (input.equals("bye")) {
                 System.out.println("Bye! Hope to see you again!");
                 return;
             }
-            System.out.println(input);
+            items.add(input);
+            System.out.println("added: " + input);
         }
     }
 }

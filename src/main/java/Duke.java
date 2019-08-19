83fbd3ba21aec2adb967cbc2a6c2895b9993b2a4
seeziyang <44959961+seeziyang@users.noreply.github.com>
2019-08-19 11:13:13 +0800

    Add Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,35 +1,64 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    private static String horizontalLine =
+            "    ____________________________________________________________";
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input;
+        ArrayList<String> tasks = new ArrayList<>();
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String horizontalLine = "    ____________________________________________________________";
 
-        // Welcome message
+        printGreeting();
+
+        while (sc.hasNext()) {
+            input = sc.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else if (input.equals("list")) {
+                printTasks(tasks);
+            } else {
+                tasks.add(input);
+
+                System.out.println(horizontalLine);
+                System.out.println("     added: " + input);
+                System.out.println(horizontalLine);
+                System.out.println();
+            }
+        }
+
+        printExit();
+    }
+
+    private static void printGreeting() {
+        // Greet
         System.out.println(horizontalLine);
         System.out.println("     Hello! I'm Duke");
         System.out.println("     What can I do for you?");
         System.out.println(horizontalLine);
         System.out.println();
+    }
 
-        // Echo user's commands
-        input = sc.next();
-        while (!input.equals("bye")) {
-            System.out.println(horizontalLine);
-            System.out.println("     " + input);
-            System.out.println(horizontalLine);
-            System.out.println();
-            input = sc.next();
+    private static void printTasks(ArrayList<String> tasks) {
+        System.out.println(horizontalLine);
+
+        int id = 1;
+        for (String task : tasks) {
+            System.out.println("     " + id + ". " + task);
+            id++;
         }
 
-        // Exit message
+        System.out.println(horizontalLine);
+    }
+
+    private static void printExit() {
         System.out.println(horizontalLine);
         System.out.println("     Bye. Hope to see you again soon!");
         System.out.println(horizontalLine);

229b722277c3ca81dc3017af3fa53a60a9d74968
goh.sining <gohsining99@gmail.com>
2019-08-19 15:31:56 +0800

    Add support for tracking 3 types of tasks

---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+/*
+A child class of Object which contains the description of the Task and whether the Task has been done.
+ */
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718");
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}

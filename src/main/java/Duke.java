716a443b744db1208bd5746363c4ba641f20c669
fps <universityforfang@gmail.com>
2019-08-20 00:34:58 +0800

    Level1GreetEchoExit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,12 @@
 public class Duke {
+    static void printline(){
+        String line =  "\t____________________________________________________________";
+        System.out.println(line);
+    }
+
+    static void printnewline(){
+        System.out.println("\n");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +14,26 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String name = "Duke";
+        printline();
+        System.out.println("\tHello, I'm " + name);
+        System.out.println("\tWhat can I do for you?");
+        printline();
+        printnewline();
+        System.out.println("list");
+        printline();
+        System.out.println("\tlist");
+        printline();
+        printnewline();
+        System.out.println("blah");
+        printline();
+        System.out.println("\tblah");
+        printline();
+        printnewline();
+        System.out.println("bye");
+        printline();
+        System.out.println("\tBye. Hope to see you again soon!");
+        printline();
+
     }
 }

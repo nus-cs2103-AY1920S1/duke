b9e758b9845ac88e2d817d99c4a160b532d18a82
ellieyee <ellieyeewenna@gmail.com>
2019-08-19 14:53:56 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,16 +2,20 @@
  * My <b>class</b>.
  *
  */
+import java.util.*;
 public class Duke {
     /**
      * The long and detailed explanation what the method does.
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        while(!command.equals("bye")) {
+            System.out.println(command);
+            command = sc.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }

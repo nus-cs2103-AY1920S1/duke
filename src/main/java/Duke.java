6122095bc449e44e8258dfe4ff2fd28849c00221
MC <dandford@gmail.com>
2019-08-19 15:05:42 +0800

    Completed Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class Duke {
+    private ArrayList<String> textEntered;
+
+    public Duke(){
+        textEntered = new ArrayList<String>(100);
+    }
+
     public static void main(String[] args) {
         Duke dukebot = new Duke();
         dukebot.greetUser();
@@ -13,11 +19,27 @@ public class Duke {
 
     private void echoUser(){
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
+        String input = scanner.nextLine();
         while(!input.equalsIgnoreCase("Bye")){
-            System.out.println(input + "\n");
-            input = scanner.next();
+            if(input.equalsIgnoreCase("list")) {
+                printRecord();
+            } else{
+                addToRecord(input);
+            }
+            input = scanner.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!\n");
+    }
+
+    private void addToRecord(String recordEntry){
+        this.textEntered.add(recordEntry);
+        System.out.println("added: " + recordEntry + "\n");
+    }
+
+    private void printRecord(){
+        for(int i = 0; i < textEntered.size(); i++){
+            System.out.print((i + 1) + ". " + textEntered.get(i) + "\n");
+        }
+        System.out.println();
     }
 }

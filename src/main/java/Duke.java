f61a3e1ed4d4c3318a0b79d79502207c7db34d95
bernicechio <bernicechiohuiyin@gmail.com>
2019-08-20 15:38:23 +0800

    Duke.java: Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -9,20 +10,20 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
+        ArrayList<String> alist = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("____________________________________________________________\n" +
-                " Hello! I'm Duke\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n");
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
         while (scanner.hasNext()) {
-            String input = scanner.next();
-            if(!input.equals("bye")) {
-                System.out.println("____________________________________________________________\n" +
-                       " " + input + "\n" + "____________________________________________________________\n");
+            String input = scanner.nextLine();
+            if(input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!\n");
+            } else if(input.equals("list")) {
+                for(int i = 1; i <= alist.size(); i++) {
+                    System.out.println(i + ". " + alist.get(i - 1));
+                }
             } else {
-                System.out.println("____________________________________________________________\n" +
-                        " Bye. Hope to see you again soon!\n" +
-                        "____________________________________________________________\n");
+                alist.add(input);
+                System.out.println("added: " + input + "\n");
             }
         }
     }

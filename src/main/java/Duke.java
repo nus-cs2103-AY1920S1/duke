14138b12e15171e545e4e27b41e2df1e8032d026
qiujiaaa <ooiqiujia@hotmail.com>
2019-08-20 21:51:03 +0800

    update Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
@@ -5,7 +6,12 @@ public class Duke {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
         printWelcomeMsg();
+
+        String[] list = new String[100];
+        Arrays.fill(list, "");
+        int count = 0;
         String command = sc.nextLine();
 
         while (!command.equals("")) {
@@ -14,8 +20,19 @@ public class Duke {
                 System.out.println("     Bye. Hope to see you again soon!");
                 printHorizontalLine();
                 break;
+            } else if (command.equals("list")) {
+                for (int i = 0; i < 100; i++) {
+                    if (!list[i].equals("")) {
+                        System.out.println("     " + (i+1) + ". " + list[i]);
+                    } else {
+                        break;
+                    }
+                }
+                printHorizontalLine();
             } else {
-                System.out.println("     " + command);
+                list[count] = command;
+                count++;
+                System.out.println("     added: " + command);
                 printHorizontalLine();
             }
             command = sc.nextLine();

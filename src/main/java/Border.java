a54d089b62aa4d61e2826fd7e7aeaf425e90143c
yuhongtay <yuhongtay@gmail.com>
2019-08-20 11:07:03 +0800

    Level -3, implements ability to mark task as done

--- src/main/java/Border.java
+++ src/main/java/Border.java
@@ -2,7 +2,7 @@ public class Border {
     public String toString(){
         String output = "    ";
         for (int n = 0 ; n < 60 ; n ++){
-            output += "-";
+            output += "_";
         }
         return output;
     }
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,16 +12,14 @@ public class Duke {
         Task_List schedule = new Task_List();
         Scanner sc = new Scanner(System.in);
         System.out.println(new Border());
-        System.out.println("    Hello! I'm Duke \n    What can I do for you?\n");
+        System.out.println("    Hello! I'm Duke \n    What can I do for you?");
         System.out.println(new Border() + "\n");
         String input = sc.nextLine();
-        while (! input.equals("bye")){
-            if (input.equals("list")) {
-                System.out.println(schedule);
-            }
+        while (!input.equals("bye")){
             schedule.add(input);
             input = sc.nextLine();
         }
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println((new Border()) + "\n     Bye. Hope to see you again soon! \n" + (new Border()));
+
     }
 }

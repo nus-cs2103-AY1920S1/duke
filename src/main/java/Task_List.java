40405c1a5737d81ef80624cf4b922cd800892e7a
yuhongtay <yuhongtay@gmail.com>
2019-08-20 11:07:03 +0800

    Level -3, v1.0

--- src/main/java/Task_List.java
+++ src/main/java/Task_List.java
@@ -1,25 +1,44 @@
 public class Task_List {
-    String [] schedule = new String [100];
+    Task [] schedule = new Task [100];
     int task_Num = 0;
+
     public void add(String task){
-        if (!task.equals("list")){
-            schedule[task_Num++] = task;
-            System.out.println("\n" + new Border());
-            System.out.println("     added: " + task);
-            System.out.println("\n" + new Border() + "\n");
+        String[] word_Arr = task.split(" ");
+        switch (word_Arr[0]){
+            case "list":
+                System.out.println(this);
+                break;
+
+            case "done":
+                schedule[Integer.parseInt(word_Arr[1]) - 1].markAsDone();
+                System.out.println(new Border());
+                System.out.println("     Nice! I've marked this task as done: ");
+                System.out.println("       " + schedule[Integer.parseInt(word_Arr[1]) - 1].toString());
+                System.out.println(new Border());
+                break;
+
+            default:
+                schedule[task_Num++] = new Task(task);
+                System.out.println(new Border());
+                System.out.println("     added: " + task);
+                System.out.println(new Border() + "\n");
+                break;
         }
     }
 
     public String toString(){
-        String output = "\n" + (new Border()) + "\n";
+        String output = (new Border()) + "\n" + "     Here are the tasks in your list: \n";
         for (int index = 0; index <= task_Num; index ++){
-            String task = schedule[index];
+            Task task = schedule[index];
             if (task == null){
                 break;
             } else {
-                output += ("     " + (index + 1) + ". " + schedule[index] + "\n");
+                /*System.out.println(index + 1);
+                System.out.println(schedule[index]);
+                String task = schedule[index].toString();*/
+                output += ("     " + (index + 1) + "." + task.toString() + "\n");
             }
         }
-        return output + "\n" + (new Border());
+        return output + (new Border()) + "\n";
     }
 }

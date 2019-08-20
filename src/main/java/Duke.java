581efb82ada908eba5a31f0464e365c9160df7ba
MauriceTXS <maurice456123@gmail.com>
2019-08-20 18:45:23 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+
+        horizontalLine();
+        greet();
+        horizontalLine();
+
+        while(scanner.hasNext()) {
+            String echo = scanner.nextLine();
+
+            if (echo.equals("bye")) {
+                horizontalLine();
+                System.out.println("Bye. Hope to see you again soon!");
+                horizontalLine();
+                break;
+            }
+
+            horizontalLine();
+            System.out.println(echo);
+            horizontalLine();
+
+        }
+
     }
+
+    public static void horizontalLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void greet() {
+        System.out.println("Hello I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+
 }

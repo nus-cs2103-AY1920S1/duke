a10c6003b81ca92a417550df967a0e943943a02f
Rui Zhi <gohruizhi@u.nus.edu>
2019-08-20 20:46:58 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,20 +8,35 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
         System.out.println("Hello from\n" + logo + "\n What can I do for you? \n" );
 
         while (sc.hasNext()) {
             String userInput = sc.nextLine();
+            String[] words = userInput.split(" ");
+            String firstWord = words[0];
             if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (userInput.equals("list")){
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.print((i+1) + ". " + list.get(i) + "\n");
+                    Task currTask = list.get(i);
+                    System.out.print((i+1) + ". " + currTask.toString() + "\n");
+                }
+            } else if (firstWord.equals("done")) {
+                try {
+                    int position = Integer.parseInt(words[1]) - 1;
+                    Task currTask = list.get(position);
+                    currTask.doTask();
+                    System.out.println("Nice! I've marked this task as done: \n " +
+                            currTask.toString() + "\n");
+                } catch (Exception ex) {
+                    System.out.println("Please try again.");
+
                 }
             } else {
-                list.add(userInput);
+                Task task = new Task(userInput);
+                list.add(task);
                 System.out.println("added: " + userInput + "\n");
             }
         }

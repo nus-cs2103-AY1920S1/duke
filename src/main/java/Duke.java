00606542318411548c444afa6f9d453867fc259a
woon17 <shufa17@gmail.com>
2019-08-20 19:10:55 +0800

    recode it

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,17 @@
 import java.util.Date;
+import java.util.Scanner;
 
 public class Duke {
-public static void run(){
-    System.out.println("herre");
-}
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        Date today = new Date();
-        run();
-        System.out.println(args[0]);
+        Scanner sc = new Scanner(System.in);
+
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from " + logo);
+        System.out.println(sc.nextLine());
     }
 }

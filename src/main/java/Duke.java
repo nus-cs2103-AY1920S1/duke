54f91d7879d5ebb7b5e012f1189e4db18d552bd4
NgWenJunFelix <felixng97@gmail.com>
2019-08-20 12:24:57 +0800

    Add, List added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        ArrayList<String> actions = new ArrayList<String>(100);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -11,11 +12,20 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         while(input.hasNext()) {
-            String command = input.next();
+            String command = input.nextLine();
             if(command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else System.out.println(command);
+            } else if (command.equals("list") && actions.size() == 0) {
+                System.out.println("No text found.");
+            } else if (command.equals("list")) {
+                for(int i = 0; i < actions.size(); i++) {
+                    System.out.println((i + 1) + ". " + actions.get(i));
+                }
+            } else {
+                actions.add(command);
+                System.out.println("added: " + command);
+            }
         }
     }
 }

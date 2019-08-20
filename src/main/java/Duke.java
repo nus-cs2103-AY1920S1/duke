77ae0f116a33b04aa6cb0bc093a295b28f9c28a5
Sahilgat <nisgat@live.com>
2019-08-20 15:50:06 +0800

    Fix bug with level 1 to account for more than one word inputs and partially implemented level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 //Test comment
 public class Duke {
+    private ArrayList<String> tasks = new ArrayList<String>();
     /*
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -15,14 +17,28 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
     }
+    public void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+    /*
+    public void list() {
+        for(int i = 1; i <= this.tasks.size(); i++) {
+            System.out.println(i + ". " + this.tasks.get(i - 1));
+        }
+    }
+    public void add(String task) {
+        this.tasks.add(task);
+        System.out.println("added: " + task);
+    }
+    */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Duke duke = new Duke();
         duke.introduction();
-        while (sc.hasNext()) {
-            String command = sc.next();
+        while (sc.hasNextLine()) {
+            String command = sc.nextLine();
             if(command.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+                duke.exit();
                 return;
             } else {
                 System.out.println(command);

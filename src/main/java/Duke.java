02af0a7b32323a358ac296d4ad1075fb6ada77a5
Joshua Seet <joshu5797@gmail.com>
2019-08-20 16:57:21 +0800

    Level 1: Duke class is will prompt for entry from user until he enters "bye". Upon entering "bye", the program will end.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
+
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello I'm\n" + logo + "\nWhat can I do for you?" );
+
+        String command = promptEntry();
+
+        while(!command.equals("bye")){
+            printCommand(command);
+            command = promptEntry();
+        }
+        printCommand("Bye. Hope to see you again soon!");
+
     }
+
+    public static void printCommand(String command) {
+        System.out.println(command);
+    }
+
+    public static String promptEntry() {
+       return sc.nextLine();
+    }
+
 }

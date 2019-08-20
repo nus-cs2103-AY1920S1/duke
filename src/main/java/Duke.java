74c7fd582d4406dccee61be99dd19c06a4cd98e9
Chee Yuan <chong-1997@hotmail.com>
2019-08-20 22:18:56 +0800

    Configure gradle to accept input from stdin, Complete level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String intro = "Hello! I'm Duke\n" +
+                "What can I do for you?\n";
+
+        System.out.println(intro);
+        String input;
+        while(!(input = sc.nextLine()).equals("bye")) {
+            System.out.println(wrap(input));
+        };
+        String endMessage = "Bye. Hope to see you again soon!";
+        System.out.println(wrap(endMessage));
+    }
+
+    private static String wrap(String str) {
+        return "    ____________________________________________________________\n"
+                + "    " + str
+                + "\n    ____________________________________________________________";
     }
 }

2280c784e1a863990ba389c274c2452226840631
yyuanxin <e0318874@u.nus.edu>
2019-08-20 23:23:50 +0800

    add, lists

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,11 +8,24 @@ public class Duke {
         System.out.println(liner + "\n" + greeting + "\n" + liner);
 
         String input;
+        String[] tasks = new String[100];
+        int counter = 0;
         Scanner sc = new Scanner(System.in);
         while (!(input = sc.nextLine()).equals("bye")) {
-            System.out.println(liner);
-            System.out.println("     " + input);
-            System.out.println(liner);
+            if (input.equals("list")) {
+                System.out.println(liner);
+                for (int i = 0; i < counter; i++ ) {
+                    int num = i + 1;
+                    System.out.println("     " + num + ". " + tasks[i]);
+                }
+                System.out.println(liner);
+            } else {
+                tasks[counter] = input;
+                System.out.println(liner);
+                System.out.println("     added: " + input);
+                System.out.println(liner);
+                counter++;
+            }
         }
 
         System.out.println(liner + "\n     Bye. Hope to see you again soon!\n" + liner);

12629d2eab08c128d1e61e7945bcf246f3af14a9
tswuxia <tswuxia@gmail.com>
2019-08-20 16:27:10 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo);*/
+        Greet();
+        Detecting();
+    }
+    public static void Greet(){
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+    }
+    public static void Detecting() {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            String cmd = sc.nextLine();
+            if (cmd.toLowerCase().equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(cmd);
+            }
+        }
     }
 }

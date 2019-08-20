45f4ba79cf54a32be4992a44024d00c0733b6c0b
tswuxia <tswuxia@gmail.com>
2019-08-20 17:32:13 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,20 +19,29 @@ public class Duke {
     public static void Detecting() {
         Scanner sc = new Scanner(System.in);
         List<String> toDo = new LinkedList<>();
+        List<String> done = new LinkedList<>();
         while(true) {
             String cmd = sc.nextLine();
+            Scanner cmdSc = new Scanner(cmd);
             if (cmd.toLowerCase().equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (cmdSc.next().toLowerCase().equals("done")) {
+                int numDone = Integer.parseInt(cmdSc.next()) - 1;
+                done.set(numDone, "V");
+                System.out.println("Nice! I've marked this task as done:\n[V] " + toDo.get(numDone));
             } else {
                 switch (cmd.toLowerCase()) {
                     case "list":
+                        System.out.println("Here are the tasks in your list:");
                         for (int i = 1; i <= toDo.size(); i++) {
-                            System.out.println(i + ". " + toDo.get(i-1));
+                            System.out.println(i + ". " + "[" + done.get(i-1) + "] " + toDo.get(i-1));
                         }
                         break;
+
                     default:
                         toDo.add(cmd);
+                        done.add("X");
                         System.out.println("added: " + cmd);
                 }
             }

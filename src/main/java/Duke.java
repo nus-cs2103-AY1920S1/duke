55ad0c7f5b8b1ebdad4e9ba436804ecdf5ce43d8
Ni Tianzhen <nitianzhen1210@gmail.com>
2019-08-20 20:57:20 +0800

    Complete level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,29 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private static ArrayList<String> myList = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         printMessage("Hello! I'm Duke\nWhat can I do for you?");
         while (true) {
             String input = in.nextLine();
-            if (!input.equals("bye")) {
-                printMessage(input);
-            } else {
+            if (input.equals("bye")) {
                 printMessage("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                StringBuilder myBuilder = new StringBuilder();
+                for (int i = 1; i <= myList.size(); i++) {
+                    myBuilder.append(i + ". " + myList.get(i - 1));
+                    if (i < myList.size()) {
+                        myBuilder.append("\n");
+                    }
+                }
+                printMessage(myBuilder.toString());
+            } else {
+                printMessage("added: " + input);
+                myList.add(input);
             }
         }
         in.close();

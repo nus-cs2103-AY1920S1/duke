bb9825cc376b916672062be232963d6030a4b1f0
alxkohh <alxkohh@gmail.com>
2019-08-20 23:06:07 +0800

    Level 1 completed
    Indentation and horizontal lines not implemented as they are optional.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String initialMessage = "Hello! I'm Duke\nWhat can I do for you?";
+        System.out.println(initialMessage);
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if (input.equals("list")) {
+                System.out.println("list");
+            } else if (input.equals("blah")) {
+                System.out.println("blah");
+            } else if (input.equals(("bye"))) {
+                System.out.println("Bye. Hope to see you again soon!");
+                return;
+            } else {
+                System.out.println("Invalid input!");
+            }
+        }
+
     }
 }

ad1f0ccca5b3d78422951e506be83401c60841a3
kxyoke <kxyoke@gmail.com>
2019-08-20 11:43:55 +0800

    Added echo and exit feature (as per Level 1 requirements)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String divider = "    " + "-".repeat(61);
+
+    private static void dukeRespond(String... inputs) {
+        System.out.println(divider);
+        for (String str : inputs) {
+            System.out.println("    " + str);
+        }
+        System.out.println(divider);
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +17,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        dukeRespond("Hello! I'm Duke", "What can I do for you?");
+
+        //start listening for user input
+        Scanner sc = new Scanner(System.in);
+        String userCmd = sc.nextLine();
+        //System.out.println(userCmd);
+        while (!userCmd.equals("bye")) {
+            //lvl1: echo cmd
+            dukeRespond(userCmd);
+            userCmd = sc.nextLine();
+        }
+        dukeRespond("Bye. Hope to see you again soon!");
+
+        //clear resources
+        sc.close();
     }
 }

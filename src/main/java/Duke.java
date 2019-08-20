9bb4a732b5b21e86cd501ab6b67f98a58070c595
evelynejuliet <evelynejuliet@gmail.com>
2019-08-21 01:19:34 +0800

    Finish Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -8,13 +9,22 @@ public class Duke {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
+        ArrayList list = new ArrayList<>();
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         Scanner sc = new Scanner(System.in);
-        String word = sc.next();
+        String word = sc.nextLine();
         while (!word.equals("bye")) {
-            System.out.println(word);
-            word = sc.next();
+            if (!word.equals("list")) {
+                System.out.println("added: " + word);
+                list.add(word);
+                word = sc.nextLine();
+            } else {
+                for (int i = 1; i <= list.size(); i++) {
+                    System.out.println(i + ". " + list.get(i - 1));
+                }
+                word = sc.nextLine();
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
     }

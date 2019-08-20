6ce9f6774eaa3a4bd967589a1891932ef25ddd94
febee99 <febee99@gmail.com>
2019-08-20 16:54:57 +0800

    Added DukeException Class, Added try-catch blocks * throw exception "cannot be empty" when inputs are in the incorrect format * throw exception "don't know what that means" when input is not event/todo/deadline/list/done/bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
-import org.jetbrains.annotations.NotNull;
-
+import java.security.cert.CertificateRevokedException;
 import java.util.Scanner;
 import java.util.LinkedList;
 
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException {
         Scanner sc = new Scanner(System.in);
         LinkedList<Task> allTasks = new LinkedList<>();
         String line = "____________________________________________________________";
@@ -20,41 +19,57 @@ public class Duke {
         Task t = new Task(sc.nextLine());
         while (!t.description.equals("bye")) {
             System.out.println(line);
-            if (t.description.equals("list")) {
-                // Print all existing items in the list
-                System.out.println("Here are the tasks in your list:");
-                allTasks.forEach(x -> System.out.println((allTasks.indexOf(x) + 1) + ". " + x));
-            } else if (t.description.contains("done")) {
-                // Mark item of interest as done
-                int indexToMark = Integer.parseInt(t.description.split(" ")[1]);
-                allTasks.get(indexToMark - 1).markAsDone();
-            } else if (t.description.contains("todo")) {
-                //Process to-do-task
-                String description = t.description.split("todo")[1];
-                System.out.println(addTask(new ToDo(description), allTasks));
-            } else if (t.description.contains("deadline")) {
-                //Process deadline-task
-                String description = t.description.split("deadline")[1];
-                String[] details = description.split("/by");
-                System.out.println(addTask(new Deadline(details[0], details[1]), allTasks));
-            } else if (t.description.contains("event")) {
-                //Process event-task
-                String description = t.description.split("event")[1];
-                String[] details = description.split("/at");
-                System.out.println(addTask(new Event(details[0], details[1]), allTasks));
-            } else {
-                // Add item to list
-                allTasks.add(t);
-                System.out.println("added: " + t.description);
+            try {
+                if (t.description.equals("list")) {
+                    // Print all existing items in the list
+                    System.out.println("Here are the tasks in your list:");
+                    allTasks.forEach(x -> System.out.println((allTasks.indexOf(x) + 1) + ". " + x));
+                } else if (t.description.contains("done")) {
+                    // Mark item of interest as done
+                    int indexToMark = Integer.parseInt(t.description.split(" ")[1]);
+                    allTasks.get(indexToMark - 1).markAsDone();
+                } else if (t.description.contains("todo")) {
+                    //Process to-do-task
+                    try {
+                        String description = t.description.split("todo")[1];
+                        System.out.println(addTask(new ToDo(description), allTasks));
+                    } catch (ArrayIndexOutOfBoundsException a) {
+                        throw new DukeException("The description of a todo cannot be empty");
+                    }
+                } else if (t.description.contains("deadline")) {
+                    //Process deadline-task
+                    try {
+                        String description = t.description.split("deadline")[1];
+                        String[] details = description.split("/by");
+                        System.out.println(addTask(new Deadline(details[0], details[1]), allTasks));
+                    } catch (ArrayIndexOutOfBoundsException a) {
+                        throw new DukeException("The description of a deadline cannot be empty.");
+                    }
+                } else if (t.description.contains("event")) {
+                    //Process event-task
+                    try {
+                        String description = t.description.split("event")[1];
+                        String[] details = description.split("/at");
+                        System.out.println(addTask(new Event(details[0], details[1]), allTasks));
+                    } catch (ArrayIndexOutOfBoundsException a) {
+                        throw new DukeException("The description of an event cannot be empty");
+                    }
+                } else {
+                    //Throw Exception
+                    throw new DukeException("I'm sorry, but I don't know what that means :-( ");
+                }
+            } catch (DukeException e) {
+                System.err.println(e);
+            } finally {
+                // Print a horizontal line and require next input
+                System.out.println(line);
+                t = new Task(sc.nextLine());
             }
-            // Print a horizontal line and require next input
-            System.out.println(line);
-            t = new Task(sc.nextLine());
         }
         System.out.println(line + "\n Bye. Hope to see you again soon! \n" + line);
     }
 
-    public static String addTask(Task t, @NotNull LinkedList<Task> allTasks) {
+    public static String addTask(Task t, LinkedList<Task> allTasks) {
         allTasks.add(t);
         StringBuilder sb = new StringBuilder("Got it. I've added this task:\n" + t);
         if (allTasks.size() == 1) {

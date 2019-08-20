7ddc57193370d0892af5f7d65809e6ebc3823aac
czkay <chong.zi.kang@gmail.com>
2019-08-20 22:21:40 +0800

    Revert "Added Level-6"
    This reverts commit 2a85d60e6b2727d0e1e6580b502748dd19a292c9.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,18 +14,11 @@ public class Duke {
                 for (int i = 1; i <= ls.size(); i++) {
                     System.out.println(String.format("%d. %s", i, ls.get(i - 1)));
                 }
-            } else if (s.startsWith("done ")){
+            } else if (s.contains("done ")){
                 int index = Integer.parseInt(s.substring(5)) - 1;
                 ls.get(index).markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println(ls.get(index));
-            } else if (s.startsWith("delete ")){
-                int index = Integer.parseInt(s.substring(7)) - 1;
-                Task deletedTask = ls.remove(index);
-                Task.decrementNumber();
-                System.out.println("Noted. I've removed this task:");
-                System.out.println(deletedTask);
-                System.out.println(String.format("Now you have %d tasks in the list.", Task.getNumberOfTasks()));
             } else {
                 try {
                     Task newTask;

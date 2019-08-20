52393cabf22dc79a082de5768338c6ec2ab02f88
alxkohh <alxkohh@gmail.com>
2019-08-20 23:38:03 +0800

    Modified Level 1
    Modified Duke to be able to echo commands entered by user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,15 +9,11 @@ public class Duke {
 
         while (sc.hasNextLine()) {
             String input = sc.nextLine();
-            if (input.equals("list")) {
-                System.out.println("list");
-            } else if (input.equals("blah")) {
-                System.out.println("blah");
-            } else if (input.equals(("bye"))) {
+            if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 return;
             } else {
-                System.out.println("Invalid input!");
+                System.out.println(input);
             }
         }
 

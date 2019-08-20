ab5068b6a3ef49c3960c00867c9cc5b4d4277ab3
dorcastan <dorcas.tann@gmail.com>
2019-08-20 20:59:48 +0800

    Add: Ability to store user input before displaying
    Text input from user is stored in a list (for now) and can be displayed
    upon request using the command "list".

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
     public static final String logo = " ____        _        \n"
@@ -9,16 +11,28 @@ public class Duke {
 
     private static final Scanner scanner = new Scanner(System.in);
 
+    private static final List<String> inputList = new ArrayList<>();
+
+    private static void process(String input) {
+        if (input.equals("list")) {
+            DukeFormatter.prettyPrint(inputList);
+        } else {
+            inputList.add(input);
+            DukeFormatter.prettyPrint("added: " + input);
+        }
+    }
+
     /**
-     * Echoes user input without filtering.
+     * Echoes user input without input validation.
      * @param args  Standard args
      */
+    // TODO: add input validation
     public static void main(String[] args) {
         DukeFormatter.prettyPrint("Hello! I'm Duke\nWhat can I do for you?");
         while (scanner.hasNext()) {
             String userInput = scanner.nextLine();
             if (userInput.equals("bye")) { break; }
-            DukeFormatter.prettyPrint(userInput);
+            process(userInput);
         }
         DukeFormatter.prettyPrint("Bye. Hope to see you again soon!");
     }

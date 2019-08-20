97f51b7948ff56f85450e78e1fbbeea891e6f27a
Jolyn Tan <jolyn.tan.sq@gmail.com>
2019-08-20 14:54:41 +0800

    Implemented Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        //Greets user
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+
+        //Echos input
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = scan.nextLine();
+        }
+
+        //Exit
+        System.out.println("Bye. Hope to see you again soon!");
+
     }
 }

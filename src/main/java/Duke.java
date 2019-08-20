a0a1ca21d1169d149a189d2b0ba03666cbcc01fc
Klement Tan <klementtan@gmail.com>
2019-08-21 03:06:39 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.security.InvalidParameterException;
 import java.util.*;
 
 public class Duke {
@@ -12,34 +13,37 @@ public class Duke {
         TasksHolder tasksHolder = new TasksHolder();
         while (scanner.hasNext()) {
             String line = scanner.nextLine();
-            if ( line.equals("bye") ) {
+            if (line.equals("bye")) {
                 System.out.println("  Bye. Hope to see you again soon!");
                 break;
-            } else if (line.equals("list")){
+            } else if (line.equals("list")) {
                 tasksHolder.printTasks();
-            } else if(line.contains(" ") && line.split(" ")[0].equals("done")){
+            } else if (line.contains(" ") && line.split(" ")[0].equals("done")) {
                 int index = Integer.valueOf(line.split(" ")[1]);
                 tasksHolder.taskDone(index);
-            } else if (line.contains("/")){
+            } else if (line.contains("/")) {
                 String taskCat = line.split(" ")[0];
-                if(taskCat.equals("deadline")){
+                if (taskCat.equals("deadline")) {
                     String endDate = line.split("/")[1].split(" ")[1];
                     String taskName = line.split("/")[0].split("deadline ")[1];
                     Task currTask = new Deadline(taskName, endDate);
                     tasksHolder.addTask(currTask);
-                } else if (taskCat.equals("event")){
+                } else if (taskCat.equals("event")) {
                     String eventDate = line.split("/")[1].split("at ")[1];
                     String taskName = line.split("/")[0].split("event ")[1];
                     Task currTask = new Event(taskName, eventDate);
                     tasksHolder.addTask(currTask);
                 }
-            } else if(line.contains(" ") && line.split(" ")[0].equals("todo")) {
+            } else if (line.contains(" ") && line.split(" ")[0].equals("todo")) {
                 String taskName = line.split("todo ")[1];
                 Task currTask = new Todo(taskName);
                 tasksHolder.addTask(currTask);
             } else {
-                Task currTask = new Task(line);
-                tasksHolder.addTask(currTask);
+                if(line.equals("todo")){
+                    System.out.println("OOPS!!! The description of a todo cannot be empty.");
+                } else {
+                    System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
             }
         }
     }

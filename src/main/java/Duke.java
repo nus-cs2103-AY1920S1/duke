53e14cf9805a6406f92fafdef3a657b9203adcca
JunHong <teojunhong96@gmail.com>
2019-08-20 21:25:28 +0800

    Create generateMessage() to reduce repeated code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,9 @@ import java.util.ArrayList;
 
 public class Duke {
 
+    private static String border = "-------------------------------------";
+    private static ArrayList<Task> items = new ArrayList<Task>();
+
     /**
      * Main method for executing Duke.
      *
@@ -15,8 +18,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
-        String border = "-------------------------------------";
 
         //Greetings before program
         StringBuilder sb = new StringBuilder();
@@ -31,7 +32,6 @@ public class Duke {
         sb.setLength(0);
         //Scanner obj for input
         Scanner sc = new Scanner(System.in);
-        ArrayList<Task> items = new ArrayList<Task>();
 
         //Loop till user input 'bye'
         String input = sc.nextLine();
@@ -42,7 +42,7 @@ public class Duke {
                 //getting the number for item
                 try {
                     int itemNum = Integer.parseInt(input.substring(input.length() - 1));
-                    Task curr = items.get(itemNum - 1);
+                    Task curr = items.get( itemNum - 1);
                     curr.markAsDone();
                     //forming the message
                     sb.append(border + "\n");
@@ -52,7 +52,7 @@ public class Duke {
                     System.out.println(sb.toString());
                     sb.setLength(0);
                     input = sc.nextLine();
-                } catch(IndexOutOfBoundsException e) {
+                } catch (IndexOutOfBoundsException e) {
                     sb.append(border + "\n");
                     sb.append("Invalid number. Number not listed. \n");
                     sb.append(border + "\n");
@@ -67,8 +67,7 @@ public class Duke {
                     sb.setLength(0);
                     input = sc.nextLine();
                 }
-
-            } else if (input.toLowerCase().contains("delete")){
+            } else if (input.toLowerCase().contains("delete")) {
                 //getting the number for item
                 try {
                     int itemNum = Integer.parseInt(input.substring(input.length() - 1));
@@ -83,7 +82,7 @@ public class Duke {
                     System.out.println(sb.toString());
                     sb.setLength(0);
                     input = sc.nextLine();
-                } catch(IndexOutOfBoundsException e) {
+                } catch (IndexOutOfBoundsException e) {
                     sb.append(border + "\n");
                     sb.append("Invalid number. Number not listed. \n");
                     sb.append(border + "\n");
@@ -114,13 +113,8 @@ public class Duke {
                 try {
                     Todo newTask = new Todo(input.substring(5));
                     items.add(newTask);
-                    sb.append(border + "\n");
-                    sb.append("Got it. I've added this task: \n");
-                    sb.append(newTask + "\n");
-                    sb.append("Now you have " + items.size() + " tasks in the list.\n");
-                    sb.append(border + "\n");
-                    System.out.println(sb.toString());
-                    sb.setLength(0);
+                    String message = generateMessage(newTask);
+                    System.out.println(message);
                     input = sc.nextLine();
                 } catch (StringIndexOutOfBoundsException e) {
                     sb.append(border + "\n");
@@ -136,13 +130,8 @@ public class Duke {
                     //split input into [deadline] [description] [date]
                     Deadline newTask = new Deadline(input.substring(9, date), input.substring(date + 3));
                     items.add(newTask);
-                    sb.append(border + "\n");
-                    sb.append("Got it. I've added this task: \n");
-                    sb.append(newTask + "\n");
-                    sb.append("Now you have " + items.size() + " tasks in the list.\n");
-                    sb.append(border + "\n");
-                    System.out.println(sb.toString());
-                    sb.setLength(0);
+                    String message = generateMessage(newTask);
+                    System.out.println(message);
                     input = sc.nextLine();
                 } catch (StringIndexOutOfBoundsException e) {
                     sb.append(border + "\n");
@@ -159,13 +148,8 @@ public class Duke {
                     //split input into [event] [description] [timing]
                     Event newTask = new Event(input.substring(6, time), input.substring(time + 3));
                     items.add(newTask);
-                    sb.append(border + "\n");
-                    sb.append("Got it. I've added this task: \n");
-                    sb.append(newTask + "\n");
-                    sb.append("Now you have " + items.size() + " tasks in the list.\n");
-                    sb.append(border + "\n");
-                    System.out.println(sb.toString());
-                    sb.setLength(0);
+                    String message = generateMessage(newTask);
+                    System.out.println(message);
                     input = sc.nextLine();
                 } catch (StringIndexOutOfBoundsException e) {
                     sb.append(border + "\n");
@@ -193,5 +177,20 @@ public class Duke {
         String conclude = sb.toString();
         System.out.println(conclude);
 
+    }
+
+    /**
+     * Message generator for task with indentation and border.
+     *
+     *
+     */
+    private static String generateMessage(Task current) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(border + "\n");
+        sb.append("Got it. I've added this task: \n");
+        sb.append(current + "\n");
+        sb.append("Now you have " + items.size() + " tasks in the list.\n");
+        sb.append(border + "\n");
+        return sb.toString();
     }
 }

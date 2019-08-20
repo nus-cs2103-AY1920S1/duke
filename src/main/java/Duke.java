f28ee1a5e2f4f6c96b39967a1f3ad895ab32e188
Lim Daekoon <daekoon_lim@hotmail.com>
2019-08-20 18:17:47 +0800

    Created PrettyPrint class to handle printing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,24 @@
 package seedu.duke;
 
+import seedu.duke.PrettyPrint;
+import seedu.duke.EventHandler;
+/**
+ * Main class of this project.
+ * Personal assistant bot to help you keep track of tasks.
+ */
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
+
+  public static void main(String[] args) {
+
+    String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    System.out.println("Hello from\n" + logo);
+
+    PrettyPrint.printBlock(new String[] {"Hello! I'm Duke.", "What can I do for you?"});
+
     }
 }
---
+++ src/main/java/PrettyPrint.java
@@ -0,0 +1,24 @@
+package seedu.duke;
+
+public class PrettyPrint {
+  static String line = "--------------------------------------------";
+  static String indent = "    ";
+
+  public static void print(String output) {
+    System.out.println(indent + output);
+  }
+
+  public static void printBlock(String[] outputs) {
+    print(line);
+    for (String output : outputs) {
+      print(output);
+    }
+    print(line);
+  }
+
+  public static void printBlock(String output) {
+     print(line);
+     print(output);
+     print(line);
+  }
+}

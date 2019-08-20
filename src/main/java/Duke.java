6ce7d51c9d0f8c90b317483d67ecc840196e21f2
qiujiaaa <ooiqiujia@hotmail.com>
2019-08-20 22:18:27 +0800

    update Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -9,9 +11,8 @@ public class Duke {
 
         printWelcomeMsg();
 
-        String[] list = new String[100];
-        Arrays.fill(list, "");
-        int count = 0;
+
+        List<Task> list = new LinkedList<>();
         String command = sc.nextLine();
 
         while (!command.equals("")) {
@@ -21,28 +22,38 @@ public class Duke {
                 printHorizontalLine();
                 break;
             } else if (command.equals("list")) {
-                for (int i = 0; i < 100; i++) {
-                    if (!list[i].equals("")) {
-                        System.out.println("     " + (i+1) + ". " + list[i]);
-                    } else {
-                        break;
-                    }
-                }
-                printHorizontalLine();
+                printTasks(list);
+            } else if (command.length() > 4 && command.substring(0, 4).equals("done")) {
+                doneTask(list, command);
             } else {
-                list[count] = command;
-                count++;
+                list.add(new Task(command));
                 System.out.println("     added: " + command);
-                printHorizontalLine();
             }
+            printHorizontalLine();
             command = sc.nextLine();
         }
 
         sc.close();
     }
 
-    private static void printWelcomeMsg() {
+    private static void doneTask(List<Task> list, String command) {
+        String[] done = command.split(" ");
+        int number = Integer.valueOf(done[1]);
+        list.get(number - 1).markAsDone();
+        System.out.println("     Nice! I've marked this task as done: ");
+        System.out.println("       " + list.get(number - 1));
+    }
 
+    private static void printTasks(List<Task> list) {
+        System.out.println("     Here are the tasks in your list:");
+        int count = 0;
+        for (Task task:list) {
+            count++;
+            System.out.println("     " + count + "." + task);
+        }
+    }
+
+    private static void printWelcomeMsg() {
         printHorizontalLine();
         System.out.println("     Hello! I'm Duke\n" + "     What can I do for you?");
         printHorizontalLine();

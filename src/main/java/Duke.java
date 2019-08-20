7214c70af7f320a89c454d984fbf62ae12804c29
dawo5010 <dawo@Daniels-MBP.net>
2019-08-20 15:02:17 +0800

    Achieved Level-1: Added greet, echo and exit.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    static String divider = "\t____________________________________________________________";
+    static String intro = "\tHello! I'm Duke\n\tWhat can I do for you?";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +12,24 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println(divider);
+        System.out.println(intro);
+        System.out.println(divider);
+
+        Scanner scanner = new Scanner(System.in);
+        boolean run = true;
+
+        while (run) {
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                run = false;
+                command = "Bye. Hope to see you again soon!";
+            }
+            System.out.println(divider);
+            System.out.println("\t" + command);
+            System.out.println(divider);
+        }
     }
 }

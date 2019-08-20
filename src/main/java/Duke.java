353ce9cbfe0cc63d8d266b3502bdb8df479e7df2
Na-Nazhou <nazhou.na9@gmail.com>
2019-08-20 19:55:21 +0800

    Add the ability to store the task entered and display the list of tasks upon request

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+
+    private static ArrayList<String> list = new ArrayList<>();
+
     public static void main(String[] args) {
         String greet = "Hello! I'm Duke\nWhat can I do for you?\n";
         System.out.println(greet);
@@ -13,7 +17,15 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } 
-            System.out.println(input);
+
+            if (input.equals("list")) {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println((i + 1) + ". " + list.get(i));
+                }
+            } else {
+                list.add(input);
+                System.out.println("added: " + input);
+            }
             System.out.println();
         }
     }

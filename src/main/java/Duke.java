07563f2f5d50196b54f9452559f05fa4d3a5e9cb
NgWenJunFelix <felixng97@gmail.com>
2019-08-20 11:43:31 +0800

    Greet, Echo, Exit added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        while(input.hasNext()) {
+            String command = input.next();
+            if(command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else System.out.println(command);
+        }
     }
 }

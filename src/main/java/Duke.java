ff809811d74cf48e4ceb14cc2d121eed4db9c983
TheRealRavager <iskandar@u.nus.edu>
2019-08-21 02:53:36 +0800

    Replace echo functionality with storage of user input. Add list command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,39 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    private static ArrayList<String> storedInputs = new ArrayList<>(100);
     public static void main(String[] args) {
         greet();
-        echoWhileInputNotBye();
+        respondToInput();
     }
 
     private static void greet() {
         dukeReply("Hello! My name is Duke!\nHow may I help you?");
     }
 
-    private static void echoWhileInputNotBye() {
+    private static void respondToInput() {
         Scanner sc = new Scanner(System.in);
         String userInput = sc.nextLine();
-        while (!userInput.equals("bye")) {
-            dukeReply(userInput);
+        while (!userInput.toLowerCase().equals("bye")) {
+            if (userInput.equals("list")) {
+                int n = 1;
+                String finalOutput = "";
+                boolean first = true;
+                for (String storedInput : storedInputs) {
+                    // May require refactor if performance is undesirably poor due to many string concatenations (review at towards end of project)
+                    if (!first) {
+                        finalOutput += "\n";
+                    }
+                    first = false;
+                    finalOutput += n + ". " + storedInput;
+                    n += 1;
+                }
+                dukeReply(finalOutput);
+            } else {
+                storedInputs.add(userInput);
+                dukeReply("added: " + userInput);
+            }
             userInput = sc.nextLine();
         }
         dukeReply("Till next time, goodbye!");

19cce2a91d7f8dc36d5707584c37a7dadda6a1d2
Joanna-YJA <joannajoules@gmail.com>
2019-08-20 16:51:02 +0800

    Level 1 updates

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+    public void respond() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! I'm Duke\n" +
+                "What can I do for you?");
+
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+
+            switch (input) {
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+
+                default:
+                    System.out.println(input);
+            }
+        }
     }
+
+    public static void main(String[] args) {
+        Duke D1 = new Duke();
+        D1.respond();
+    }
+
 }

87dc2a3309b626f9478571e329679b9bd3ad8e66
Ni Tianzhen <nitianzhen1210@gmail.com>
2019-08-20 21:14:14 +0800

    Complete level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static ArrayList<String> myList = new ArrayList<>();
+    private static ArrayList<Task> myList = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -14,16 +14,22 @@ public class Duke {
                 break;
             } else if (input.equals("list")) {
                 StringBuilder myBuilder = new StringBuilder();
+                myBuilder.append("Here are the tasks in your list:\n");
                 for (int i = 1; i <= myList.size(); i++) {
-                    myBuilder.append(i + ". " + myList.get(i - 1));
+                    Task myTask = myList.get(i - 1);
+                    myBuilder.append(i + "." + myTask);
                     if (i < myList.size()) {
                         myBuilder.append("\n");
                     }
                 }
                 printMessage(myBuilder.toString());
+            } else if (input.contains("done")) {
+                int myNum = Integer.valueOf(input.substring(5));
+                myList.get(myNum - 1).markAsDone();
+                printMessage("Nice! I've marked this task as done:\n  " + myList.get(myNum - 1));
             } else {
                 printMessage("added: " + input);
-                myList.add(input);
+                myList.add(new Task(input));
             }
         }
         in.close();

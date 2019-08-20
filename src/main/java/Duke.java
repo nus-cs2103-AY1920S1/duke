1f1116574b4651baf91d94910fe10ba7e06e4502
Chee Yuan <chong-1997@hotmail.com>
2019-08-21 00:13:13 +0800

    Level 2 bug fix

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,32 +1,34 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.StringJoiner;
+import java.util.*;
 
 public class Duke {
+    public static final String indent = "    ";
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String intro = "Hello! I'm Duke\n" +
-                "What can I do for you?\n";
-        List<String> history = new ArrayList<>();
+                "What can I do for you?";
+        List<Task> history = new ArrayList<>();
 
-        System.out.println(intro);
+        System.out.println(wrap(intro));
         String input;
+        String output;
         while(!(input = sc.nextLine()).equals("bye")) {
             switch(input) {
                 case "list":
-                    System.out.println(historyToString(history));
+                    output = historyToString(history);
+                    break;
                 default:
-                    history.add(input);
-                    System.out.println(wrap("added: " + input));
+                    history.add(new Task(input));
+                    output = "added: " + input;
             }
+            System.out.println(wrap(output));
         };
 
         String endMessage = "Bye. Hope to see you again soon!";
         System.out.println(wrap(endMessage));
     }
 
-    private static String historyToString(List<String> history) {
+    private static String historyToString(List<Task> history) {
         StringJoiner sj = new StringJoiner("\n");
         for(int i = 0; i < history.size(); i++) {
             sj.add((i + 1) + ". " + history.get(i));
@@ -35,8 +37,11 @@ public class Duke {
     }
 
     private static String wrap(String str) {
-        return "    ____________________________________________________________\n"
-                + "    " + str
-                + "\n    ____________________________________________________________";
+        String wrappedString = "____________________________________________________________\n"
+                + str
+                + "\n" + "____________________________________________________________";
+        String[] indentedStrings = Arrays.stream(wrappedString.split("\n")).map(s -> indent + s).toArray(String[]::new);
+
+        return String.join("\n", indentedStrings);
     }
 }

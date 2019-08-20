290006d684ac675a1ca888e6544a9d435095aea3
Junwei <junwei.97@hotmail.com>
2019-08-20 19:53:44 +0800

    Added support for adding to list & listing items

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 
+import java.util.ArrayList;
+
 public class Duke {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        ArrayList<Task> taskList = new ArrayList<>();
 
         String logo = " ____        _        \n" + "|  _ \\ _   _| | _____ \n" + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n" + "|____/ \\__,_|_|\\_\\___|\n";
@@ -18,16 +21,25 @@ public class Duke {
         // Echo commands until bye is entered
 
         while (true) {
-            String command = sc.next();
+            String str = sc.nextLine();
 
-            if (command.equals("bye")) {
+            if (str.equals("list")) {
+                System.out.println("\t____________________________________________________________");
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println("\n\t" + (i + 1) + ". " + taskList.get(i).toString());
+                }
+                System.out.println("\t____________________________________________________________\n");
+            } else if (str.equals("bye")) {
                 System.out.println("\t____________________________________________________________");
                 System.out.println("\n\tBye. Hope to see you again soon!");
                 System.out.println("\t____________________________________________________________\n");
                 break;
             } else {
+                // Create task
+                Task t = new Task(str);
+                taskList.add(t);
                 System.out.println("\t____________________________________________________________");
-                System.out.println("\n\t" + command);
+                System.out.println("\n\t" + "added: " + str);
                 System.out.println("\t____________________________________________________________\n");
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Task {
+
+  private String description;
+
+  // Constructor
+  public Task(String description) {
+    this.description = description;
+  }
+
+  public String toString() {
+    return this.description;
+  }
+}

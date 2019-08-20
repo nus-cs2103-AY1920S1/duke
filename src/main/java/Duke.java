0cbd5901884b74d17b34f0060ce925e97607e270
zhangxuan97 <zxrox@yahoo.com.sg>
2019-08-21 01:50:02 +0800

    Duke: add Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> todo = new ArrayList<>();
 
         System.out.println("    ____________________________________________________________\n" +
                 "     Hello! I'm Duke\n" +
@@ -12,17 +12,30 @@ public class Duke {
 
         String cmd = sc.nextLine();
         while (!cmd.equals("bye")) {
-            if (cmd.equals("list")) {
+
+            String arr[] = cmd.split(" ", 2);
+            String firstWord = arr[0];
+
+            if (firstWord.equals("list")) {
                 System.out.println("    ____________________________________________________________");
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println("     " + (i + 1) + ". " + tasks.get(i));
+                System.out.println("     Here are the tasks in your list:");
+                for (int i = 0; i < todo.size(); i++) {
+                    System.out.println("     " + (i + 1) + ". " + todo.get(i));
                 }
+                System.out.println("    ____________________________________________________________");
+            } else if (firstWord.equals("done")){
+                int taskNum = Integer.parseInt(arr[1]);
+                Task currTask = todo.get(taskNum-1);
+                currTask.finishTask();
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     Nice! I've marked this task as done:");
+                System.out.println("       " + currTask);
                 System.out.println("    ____________________________________________________________");
             } else {
                 System.out.println("    ____________________________________________________________");
                 System.out.println("     added: " + cmd);
                 System.out.println("    ____________________________________________________________");
-                tasks.add(cmd);
+                todo.add(new Task(cmd));
             }
             cmd = sc.nextLine();
         }
@@ -32,4 +45,19 @@ public class Duke {
                 "    ____________________________________________________________\n");
 
     }
+}
+
+class Task {
+    String _name;
+    String _status;
+
+    public Task(String name) {
+        _name = name;
+        _status = "✗";
+    }
+
+    public String toString() {  return "[" + _status + "] " + _name; }
+
+    public void finishTask() {_status = "✓";}
+
 }

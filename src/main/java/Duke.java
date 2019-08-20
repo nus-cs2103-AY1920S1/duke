2ab03ab817c77a0824a79b764c6f0641bc59a115
Jolyn Tan <jolyn.tan.sq@gmail.com>
2019-08-20 15:15:33 +0800

    Implemented Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -12,12 +14,23 @@ public class Duke {
         //Greets user
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
-        //Echos input
+
         Scanner scan = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<String>();
         String input = scan.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(input);
-            input = scan.nextLine();
+            //Adding the command "list" to list all added tasks
+            if (input.equals("list")) {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(i+1 + ". " + list.get(i));
+                }
+                input = scan.nextLine();
+            } else {
+                //Adds input to list
+                list.add(input);
+                System.out.println("added: " + input);
+                input = scan.nextLine();
+            }
         }
 
         //Exit

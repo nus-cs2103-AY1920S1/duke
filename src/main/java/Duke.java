0e6dd161edbb4e75638b5049e0291771e01a37dd
NgWenJunFelix <felixng97@gmail.com>
2019-08-20 13:45:12 +0800

    Mark as Done and Task Class added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        ArrayList<String> actions = new ArrayList<String>(100);
+        ArrayList<Task> actions = new ArrayList<Task>(100);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -19,13 +19,27 @@ public class Duke {
             } else if (command.equals("list") && actions.size() == 0) {
                 System.out.println("No text found.");
             } else if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for(int i = 0; i < actions.size(); i++) {
-                    System.out.println((i + 1) + ". " + actions.get(i));
+                    Task current = actions.get(i);
+                    System.out.println((i + 1) +  ".[" + current.getStatusIcon() + "] " + current.getDescription());
+                }
+            } else if (command.startsWith("done")) {
+                int taskNumber = Integer.valueOf(command.substring(command.length() - 1)).intValue();
+                if(taskNumber > actions.size()) {
+                    System.out.println("No task found.");
+                } else {
+                    System.out.println("Nice! I've marked this task as done:");
+                    Task current = actions.get(taskNumber - 1);
+                    current.markAsDone();
+                    System.out.println("[" + current.getStatusIcon() + "] "+ current.getDescription());
                 }
             } else {
-                actions.add(command);
+                actions.add(new Task(command));
                 System.out.println("added: " + command);
             }
         }
     }
 }
+
+

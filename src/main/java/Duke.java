68e61cb25a10abee87b3ae0591a5893ffed2bf2b
woon17 <shufa17@gmail.com>
2019-08-21 00:55:40 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,15 +7,51 @@ public class Duke {
     public static int idx = 0;
     public static String upperLine = "____________________________________________________________\n";
     public static String lowerLine = "____________________________________________________________\n";
+    public static int size;
 
+    public static String getDescription(String cmd){
+     String[] curr = cmd.split(" ");
+     return curr[1] + " " + curr[2];
+    }
+
+    public static void childFeature(String cmd){
+        System.out.print(upperLine);
+        System.out.println("Got it. I've added this task: ");
+        String firstWord = cmd.split(" ")[0];
+        if(firstWord.equals("todo")){
+            myList[idx] = new Todo(getDescription(cmd));
+        }else if(firstWord.equals("deadline")){
+            String by = cmd.split("/by ")[1];
+            myList[idx] = new Deadline(getDescription(cmd), by);
+        }else{
+            String at = cmd.split("/at ")[1];
+            myList[idx] = new Deadline(getDescription(cmd), at);
+        }
+        System.out.println(myList[idx]);
+        idx++;
+        size++;
+        System.out.println("Now you have "+ size + " tasks in the list.");
+        System.out.println(lowerLine);
+    }
     public static void addFeature(String cmd){
 
         myList[idx] = new Task(cmd);
         idx++;
+        size++;
         message = upperLine + "add: " + cmd + "\n" + lowerLine;
         System.out.println(message);
     }
-    public static void doneFeature(String num_string){
+
+    public static void tryFeature(String cmd) {
+        String firstWord = cmd.split(" ")[0];
+        if(firstWord.equals("todo") || firstWord.equals("deadline")
+                ||firstWord.equals("event")){
+            childFeature(cmd);
+        }else{
+            addFeature(cmd);
+        }
+    }
+        public static void doneFeature(String num_string){
         int i = Integer.parseInt(num_string);
         System.out.print(upperLine);
         System.out.println("Nice! I've marked this task as done: ");
@@ -23,6 +59,7 @@ public class Duke {
         current.markAsDone();
         System.out.println(current);
         System.out.println(lowerLine);
+        size++;
 
     }
     public static void listFeature(){
@@ -57,7 +94,7 @@ public class Duke {
                         cmd.split(" ")[0].equals("done")){
                     doneFeature(cmd.split(" ")[1]);
                 }else{
-                    addFeature(cmd);
+                    tryFeature(cmd);
                 }
             }
         }

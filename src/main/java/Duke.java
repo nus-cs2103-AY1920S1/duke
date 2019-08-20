f42000d9d2bec09ce3aeb6ebeb8224ef392d1d17
junhuplim <limjunhup1997@gmail.com>
2019-08-20 23:19:38 +0800

    Level- 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,16 +2,30 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
+    static class Task {
+        protected boolean isDone = false;
+        protected String taskName;
+
+        public Task(String taskName) {
+            this.taskName = taskName;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "\u2713" : " \u2718");
+        }
+
+        public void complete() {
+            this.isDone = true;
+        }
+
+        public String toString() {
+            return this.getStatusIcon() + " " + this.taskName;
+        }
+    }
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke \nWhat can I do for you?");
         Scanner scanner  = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         while (scanner.hasNextLine()) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
@@ -19,14 +33,21 @@ public class Duke {
                 scanner.close();
                 break;
             } else if (input.equals("list")) {
-                int count = 1;
-                for (String s: list) {
-                    System.out.println(count + ". " + s);
-                    count++;
+                System.out.println("Here are the tasks in your list:");
+                for(int i = 0; i < tasks.size(); i++) {
+                    System.out.println(i + 1 + "." + tasks.get(i));
                 }
-            } else {
+            } else if (input.contains("done")) {
+                String[] inputs = input.split(" ");
+                int number =  Integer.parseInt(inputs[1]) - 1;
+                tasks.get(number).complete();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks.get(number));
+            }
+            else {
+                Task task = new Task(input);
                 System.out.println("added: " + input);
-                list.add(input);
+                tasks.add(task);
             }
         }
     }

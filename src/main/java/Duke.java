ca91e9d19f14c04040752604a74a6b2e71d7a439
bernicechio <bernicechiohuiyin@gmail.com>
2019-08-20 17:05:28 +0800

    Duke.java: Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,12 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class Duke {
+public class Duke extends Task {
+
+    public Duke(String description) {
+        super(description);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,19 +15,30 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        ArrayList<String> alist = new ArrayList<>();
+        ArrayList<Task> alist = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
         while (scanner.hasNext()) {
             String input = scanner.nextLine();
-            if(input.equals("bye")) {
+            String[] parts = input.split(" ");
+            String part1 = parts[0];
+            if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!\n");
-            } else if(input.equals("list")) {
-                for(int i = 1; i <= alist.size(); i++) {
-                    System.out.println(i + ". " + alist.get(i - 1));
+            } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:\n");
+                for (int i = 1; i <= alist.size(); i++) {
+                    Task task = alist.get(i - 1);
+                    System.out.println(i + ". [" + task.getStatusIcon() + "] " + task.description);
                 }
-            } else {
-                alist.add(input);
+            } else if (part1.equals("done")) {
+                String part2 = parts[1];
+                int index = Integer.parseInt(part2);
+                Task task = alist.get(index - 1);
+                task.markDone();
+                System.out.println("Nice! I've marked this task as done: \n" +
+                        "[" + task.getStatusIcon() + "] " + task.description);
+            }else {
+                alist.add(new Task(input));
                 System.out.println("added: " + input + "\n");
             }
         }

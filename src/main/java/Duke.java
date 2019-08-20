80e30917af3d1cb98fe957d210e1c70e92124bac
woon17 <shufa17@gmail.com>
2019-08-20 19:44:13 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,13 +5,35 @@ public class Duke {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String message;
+//
+////        String logo = " ____        _        \n"
+////                + "|  _ \\ _   _| | _____ \n"
+////                + "| | | | | | | |/ / _ \\\n"
+////                + "| |_| | |_| |   <  __/\n"
+////                + "|____/ \\__,_|_|\\_\\___|\n";
+////        System.out.println("Hello from " + logo);
+//        System.out.println(sc.nextLine());
 
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from " + logo);
-        System.out.println(sc.nextLine());
+
+        String upperLine = "____________________________________________________________\n";
+        String greet = "Hello! I'm Duke\n"
+        + "What can I do for you?\n";
+
+        String lowerLine = "____________________________________________________________\n";
+        greet = upperLine + greet + lowerLine;
+        System.out.println(greet);
+        while(true){
+            String cmd = sc.next();
+            if(cmd.equals("bye")){
+                message = upperLine + "Bye. Hope to see you again soon!\n" + lowerLine;
+                System.out.println(message);
+                break;
+            }else{
+                message = upperLine + cmd + "\n" + lowerLine;
+                System.out.println(message);
+            }
+        }
+
     }
 }

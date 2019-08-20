f945e195d68c2f67cb6fab72bd70f49f7ca1168b
Gihun Ko <rhrlgns96@gmail.com>
2019-08-20 13:39:41 +0800

    level-3

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,15 +1,17 @@
 package seedu.duke;
 
+import seedu.duke.Task;
+
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
 
 public class Duke {
     private static String greetingMsg =
-            "    ____________________________________________________________\n" +
-            "     Hello! I'm Duke\n" +
-            "     What can I do for you?\n" +
-            "    ____________________________________________________________";
+            "____________________________________________________________\n" +
+            "Hello! I'm Duke\n" +
+            "What can I do for you?\n" +
+            "____________________________________________________________";
     private static String logo =
               " ____        _        \n"
             + "|  _ \\ _   _| | _____ \n"
@@ -19,29 +21,47 @@ public class Duke {
 
     private static void run() {
         Scanner sc = new Scanner(System.in);
-        List<String> list = new ArrayList<>();
+        List<Task> list = new ArrayList<>();
 
         System.out.println(greetingMsg);
         boolean exit = false;
         while (!exit) {
             String input = sc.nextLine();
+            String arr[] = input.split(" ");
+            System.out.println("____________________________________________________________");
             if (input.equals("bye")) {
                 exit = true;
             } else if (input.equals("list")) {
                 displayList(list);
+            } else if (arr[0].equals("done")) {
+                int index = Integer.valueOf(arr[1]) - 1;
+                list.get(index).markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                displayTask(list, index);
+
             } else {
-                list.add(input);
-                System.out.println("added: " + input + "\n");
+                list.add(new Task(input));
+                System.out.println("added: " + input);
             }
+            System.out.println("____________________________________________________________");
         }
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 
-    private static void displayList(List<String> list) {
+    private static void displayList(List<Task> list) {
         int index = 0;
-        for (String l : list) {
+        for (Task task : list) {
             index++;
-            System.out.println(index + ". " + l);
+            System.out.println(index + "." +
+                    task.getStatusIcon() + " " +
+                    task.getDescription());
+        }
+    }
+
+    private static void displayTask(List<Task> list, int index) {
+        if (index >= 0) {
+            System.out.println(list.get(index));
         }
     }
 
---
+++ src/main/java/seedu/duke/Task.java
@@ -0,0 +1,45 @@
+package seedu.duke;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description, boolean isDone) {
+        this.description = description;
+        this.isDone = isDone;
+    }
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public void markAsDone() {
+        setDone(true);
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[\u2713]": "[\u2718]");
+    }
+
+    @Override
+    public String toString() {
+        return getStatusIcon() + " " + description;
+    }
+}

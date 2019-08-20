fd140de818e2dc814d467e7cf1fa6d43d44a750f
Sahilgat <nisgat@live.com>
2019-08-20 15:23:27 +0800

    Add echo commands and exit on "bye"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+//Test comment
 public class Duke {
+    /*
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +9,24 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+    }
+    */
+    public void introduction() {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Duke duke = new Duke();
+        duke.introduction();
+        while (sc.hasNext()) {
+            String command = sc.next();
+            if(command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                return;
+            } else {
+                System.out.println(command);
+            }
+        }
     }
 }

7a4a326de3f7eea6081f0b52a79272ca41b6f276
jerryk1997 <jerry@aAa.local>
2019-08-20 14:58:18 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        greet();
+        String line = line();
+        String exit = "Bye. Hope to see you again soon!";
+        String input = sc.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.printf("    %s%n    %s%n    %s%n", line, input, line);
+            input = sc.nextLine();
+        }
+
+        System.out.printf("    %s%n    %s%n    %s%n", line, exit, line);
+    }
+
+    public static String line() {
+        StringBuilder line = new StringBuilder();
+
+        for (int i = 0; i < 35; i++) {
+            line.append("_");
+        }
+
+        String stringLine = line.toString();
+        return stringLine;
+    }
+
+    public static void greet() {
+        String line = line();
+        String greeting = "Hello! I'm Duke\n" +
+                "    What can I do for you?";
+
+        System.out.printf("    %s%n    %s%n    %s%n", line, greeting, line);
     }
 }

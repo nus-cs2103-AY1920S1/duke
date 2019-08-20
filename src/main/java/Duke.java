0399d95ddab3bb6eb602ee7b6a7cda00eb8b57b2
joannasara <joannasarasusan@gmail.com>
2019-08-21 01:19:33 +0800

    Finish level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Duke {
@@ -7,14 +8,27 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        System.out.println("Hello from\n" + logo);
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+
+        LinkedList<String> lst = new LinkedList<>();
+
         Scanner sc = new Scanner(System.in);
         String word = sc.next();
+
         while (!word.equals("bye")) {
-            System.out.println(word);
+            if (word.equals("list")) {
+                int counter = 1;
+                for (String s: lst) {
+                    System.out.println(counter + ". " + s);
+                    counter++;
+                }
+            } else {
+                System.out.println("added: " + word);
+                lst.add(word);
+            }
             word = sc.next();
         }
         System.out.println("Bye. Hope to see you again soon!");

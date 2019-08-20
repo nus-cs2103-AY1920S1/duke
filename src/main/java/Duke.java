56abb4eae596ec4101841e33c1fa58d63797cf7d
honhaochen <haochenhon@hotmail.com>
2019-08-20 23:30:05 +0800

    level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,20 +13,28 @@ public class Duke {
 
         //Setup Scanner and List
         Scanner sc = new Scanner(System.in);
-        List<String> store = new ArrayList<String>();
+        List<Task> store = new ArrayList<Task>();
 
         String userInput = sc.nextLine();
         while (!userInput.equals("bye")) {
             if (userInput.equals("list")) {
                 System.out.println(horizontalLine);
+                System.out.println("     Here are the tasks in your list:");
                 int i = 1;
-                for (String item : store) {
-                    System.out.println("     "+ i + ". " + item);
+                for (Task task : store) {
+                    System.out.println("     " + i + ". [" + task.getStatusIcon() + "] " + task);
                     i++;
                 }
                 System.out.println(horizontalLine + "\n");
+            } else if (userInput.length() == 6 && userInput.substring(0,4).equals("done")) {
+                Task task = store.get(Integer.parseInt(userInput.charAt(5) + "") - 1);
+                task.markAsDone();
+                System.out.println(horizontalLine);
+                System.out.println("     Nice! I've marked this task as done:");
+                System.out.println("       [" + task.getStatusIcon() + "] " + task);
+                System.out.println(horizontalLine + "\n");
             } else {
-                store.add(userInput);
+                store.add(new Task(userInput));
                 System.out.println(horizontalLine);
                 System.out.println("     " + "added: " + userInput);
                 System.out.println(horizontalLine + "\n");

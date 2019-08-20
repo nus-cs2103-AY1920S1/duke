b7e8b36fd9a601e15a02e2d01179d51101d75bd8
ManasVegi <manasvegi@gmail.com>
2019-08-20 14:28:14 +0800

    level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        //hr is horizontal row
         String hr = "___________________________________";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -12,6 +14,7 @@ public class Duke {
         System.out.println(hr);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         System.out.println(hr);
+        ArrayList<String> arr = new ArrayList<>();
         while(true) {
             String command = sc.next();
             System.out.println(hr);
@@ -19,8 +22,13 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.print(hr);
                 break;
+            } else if(command.equals("list")) {
+                for(int i = 0; i < arr.size(); i++) {
+                    System.out.println(arr.get(i));
+                }
             } else {
-                System.out.println(command);
+                arr.add((arr.size() + 1) + ". " + command);
+                System.out.println("added: " + command);
             }
             System.out.println(hr);
         }

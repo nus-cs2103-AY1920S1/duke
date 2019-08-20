1ac9f40b5041b895ecccf0524162b87a6ad864e2
alxkohh <alxkohh@gmail.com>
2019-08-21 00:02:27 +0800

    Level 2 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
+import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    private static List<String> dukeList = new ArrayList<>();
+
     public static void main(String[] args) {
         String initialMessage = "Hello! I'm Duke\nWhat can I do for you?";
         System.out.println(initialMessage);
@@ -12,10 +16,21 @@ public class Duke {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 return;
+            } else if (input.equals("list")) {
+                displayDukeList();
             } else {
-                System.out.println(input);
+                dukeList.add(input);
+                System.out.println("added: " + input);
             }
         }
-
     }
+
+    public static void displayDukeList() {
+        for (int i = 0; i < dukeList.size(); i++) {
+            int itemIndex = i + 1;
+            String itemDisplay = itemIndex + ". " + dukeList.get(i);
+            System.out.println(itemDisplay);
+        }
+    }
+
 }

e97f570ab403b521e3f43d549dfa1d5abf37d1b9
Zhang Dongjun <dexterzhang430@hotmail.com>
2019-08-20 21:49:37 +0800

    add Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class Duke {
+    /*
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +9,35 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+    }
+    */
+    public void greet() {
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+    }
+
+    public void run() {
+        boolean flag = true;
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            String cmd = sc.next();
+            switch(cmd) {
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    flag = false;
+                    break;
+                default:
+                    System.out.println(cmd);
+                    break;
+            }
+            if (!flag) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Duke duke = new Duke();
+        duke.greet();
+        duke.run();
     }
 }

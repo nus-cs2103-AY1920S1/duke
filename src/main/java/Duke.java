c487966e0cd31e5412cac31069d6f4352dc90064
MichaelaTSH <michaelatang99@gmail.com>
2019-08-20 17:47:48 +0800

    Add greeting, echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
+    static String line = "____________________________________________________________";
+
     public static void main(String[] args) {
+        intro();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String s = sc.nextLine();
+            if (s.toLowerCase().equals("bye")) {
+                System.out.println(line + "\n Bye. Hope to see you again soon!\n" + line);
+                break;
+            }
+            respond(s);
+        }
+    }
+
+    public static void intro() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(line + "\n" + logo + " Hello! I'm Duke\n What can I do for you?\n" + line);
+    }
+
+    public static void respond(String s) {
+        System.out.println(line + "\n " + s + "\n" + line);
     }
 }

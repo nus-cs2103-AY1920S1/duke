e7a2cca013e37de3f505b9ac40506c64181310ac
MauriceTXS <maurice456123@gmail.com>
2019-08-20 23:10:34 +0800

    Level-3. Add two new classes, Task and Task Manager.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,39 +3,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Duke {
+    private static Integer totalTask = 0;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        List<String> books = new ArrayList<>();
 
-        horizontalLine();
+        Scanner scanner = new Scanner(System.in);
+        TaskManager taskManager = new TaskManager();
+
         greet();
-        horizontalLine();
-        System.out.println();
 
         while(scanner.hasNext()) {
-            String echo = scanner.nextLine();
+            String event = scanner.nextLine();
 
-            if (echo.equals("bye")) {
-                horizontalLine();
-                System.out.println("Bye. Hope to see you again soon!");
-                horizontalLine();
+            if (event.equals("bye")) {
+                exit();
                 break;
-            }
-
-            if (echo.equals("list")) {
-                horizontalLine();
-                int counter = 0;
-                for (String items : books) {
-                    counter++;
-                    System.out.println(counter + ". " + items);
-                }
-                horizontalLine();
+            } else if (event.equals("list")) {
+                printList(taskManager);
+            } else if (event.startsWith("done")) {
+                String[] words = event.split("\\s");
+                int index = Integer.parseInt(words[1]);
+                doneTask(taskManager, index);
             } else {
-                horizontalLine();
-                books.add(echo);
-                System.out.println("added: " + echo);
-                horizontalLine();
-                System.out.println();
+                addTask(taskManager, event);
+                totalTask++;
             }
 
         }
@@ -47,8 +37,45 @@ public class Duke {
     }
 
     public static void greet() {
+        horizontalLine();
         System.out.println("Hello I'm Duke");
         System.out.println("What can I do for you?");
+        horizontalLine();
+        System.out.println();
+    }
+
+    public static void exit() {
+        horizontalLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        horizontalLine();
+    }
+
+    public static void addTask(TaskManager taskManager, String event) {
+        horizontalLine();
+        taskManager.addTask(new Task(event));
+        System.out.println("added: " + event);
+        horizontalLine();
+        System.out.println();
+    }
+
+    public static void printList(TaskManager taskManager) {
+        horizontalLine();
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < totalTask; i++) {
+            System.out.println(i+1 + ".[" + taskManager.getTask(i).getStatusIcon() + "] " + taskManager.getTask(i).getDescription());
+        }
+        horizontalLine();
+        System.out.println();
+    }
+
+    public static void doneTask(TaskManager taskManager, int index) {
+        index = index - 1;
+        taskManager.doneTask(index);
+        horizontalLine();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("  [" + taskManager.getTask(index).getStatusIcon() + "] " + taskManager.getTask(index).getDescription());
+        horizontalLine();
+        System.out.println();
     }
 
 }
---
+++ src/main/java/TaskManager.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskManager {
+    List<Task> taskList;
+
+    public TaskManager() {
+        this.taskList = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        taskList.add(task);
+    }
+
+    public Task getTask(int i) {
+        return taskList.get(i);
+    }
+
+    public void doneTask(int i) {
+        taskList.get(i).changeStatusTrue();
+    }
+
+}

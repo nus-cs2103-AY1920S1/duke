c22a9ee67d7d2d3dfb31dcc7c4b914db51f48764
huiminlim <e0036379@u.nus.edu>
2019-08-21 00:31:30 +0800

    Added: Greeting and Goodbye message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        */
+
+        Scanner sc = new Scanner(System.in);
+
+        String greeting = "Hello! I'm Duke\nWhat can I do for you?";
+        String goodbye = "Bye. Hope to see you again soon!";
+        System.out.println(greeting);
+
+        // Start reading input
+        while(sc.hasNextLine()){
+            String readInput = sc.nextLine();
+            if(readInput.equals("Bye")) {
+                System.out.println(goodbye);
+                break;
+            }
+        }
     }
 }

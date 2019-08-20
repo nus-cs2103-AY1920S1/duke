e4f8b5e5f4c7801d5b578eec964279a83c4ccb62
qiujiaaa <ooiqiujia@hotmail.com>
2019-08-20 21:43:26 +0800

    update Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+        printWelcomeMsg();
+        String command = sc.nextLine();
+
+        while (!command.equals("")) {
+            printHorizontalLine();
+            if (command.equals("bye")) {
+                System.out.println("     Bye. Hope to see you again soon!");
+                printHorizontalLine();
+                break;
+            } else {
+                System.out.println("     " + command);
+                printHorizontalLine();
+            }
+            command = sc.nextLine();
+        }
+
+        sc.close();
+    }
+
+    private static void printWelcomeMsg() {
+
+        printHorizontalLine();
+        System.out.println("     Hello! I'm Duke\n" + "     What can I do for you?");
+        printHorizontalLine();
+    }
+
+    private static void printHorizontalLine() {
+        System.out.println("    ____________________________________________________________");
     }
 }

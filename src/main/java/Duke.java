1e97788bb7d13a73cc483978a78531c8c4174861
zhangxuan97 <zxrox@yahoo.com.sg>
2019-08-21 00:56:04 +0800

    Duke: add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("    ____________________________________________________________\n" +
+                "     Hello! I'm Duke\n" +
+                "     What can I do for you?\n" +
+                "    ____________________________________________________________");
+
+        String cmd = sc.nextLine();
+        while (!cmd.equals("bye")) {
+            System.out.println("    ____________________________________________________________");
+            System.out.println("     " + cmd);
+            System.out.println("    ____________________________________________________________");
+            cmd = sc.nextLine();
+        }
+        System.out.println("    ____________________________________________________________\n" +
+                "     Bye. Hope to see you again soon!\n" +
+                "    ____________________________________________________________");
     }
 }

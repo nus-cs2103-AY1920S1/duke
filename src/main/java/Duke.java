8e94d956bf97c91069a756956a78b73febcd96ee
Sam-limyr <nacuniel@gmail.com>
2019-08-20 17:07:57 +0800

    Extended functionality of Tasks by adding subclasses.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,21 +9,40 @@ public class Duke {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         while (true) {
             String input = scanner.nextLine();
-            if (input.equals("bye")) {
+            if (input.equals("bye")) { // First, check if 'bye' is called
                 break;
-            } else if (input.equals("list")) {
+            } else if (input.equals("list")) { // Then, check if 'list' is called
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskList.size(); i++) {
                     System.out.println((i + 1) + ". " + taskList.get(i));
                 }
-            } else if (input.startsWith("done")) {
-                int index = Integer.parseInt(input.split(" ")[1]);
+            } else if (input.startsWith("done")) { // Then, check if task is done
+                int index = Integer.parseInt(input.split(" ", 2)[1]);
                 taskList.get(index - 1).markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println(taskList.get(index - 1));
-            } else {
-                System.out.println("added: " + input);
-                taskList.add(new Task(input));
+            } else { // If not, is assumed to be a new Task.
+                System.out.println("Got it. I've added this task:");
+                String taskType = input.split(" ", 2)[0]; // Obtains type of task
+                String taskWords = input.split(" ", 2)[1]; // Remaining content of task
+                if (taskType.equals("todo")) { // For to-do tasks, task content is all content
+                    Task currentTask = new ToDoTask((taskWords));
+                    taskList.add(currentTask);
+                    System.out.println("  " + currentTask);
+                } else {
+                    String taskContent = taskWords.split(" /", 2)[0];
+                    String taskTime = taskWords.split(" /by | /at ", 2)[1]; // But for other tasks, time must be parsed
+                    if (taskType.equals("deadline")) {
+                        Task currentTask = new DeadlineTask(taskContent, taskTime);
+                        taskList.add(currentTask);
+                        System.out.println("  " + currentTask);
+                    } else if (taskType.equals("event")) {
+                        Task currentTask = new EventTask(taskContent, taskTime);
+                        taskList.add(currentTask);
+                        System.out.println("  " + currentTask);
+                    }
+                }
+                System.out.println("Now you have " + taskList.size() + " tasks in the list.");
             }
         }
         System.out.println("Bye. Hope to see you again soon!");

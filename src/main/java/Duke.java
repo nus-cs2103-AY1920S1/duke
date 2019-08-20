cc63d8f977f655e5cfa7c6b7159070ddbaf03afc
alxkohh <alxkohh@gmail.com>
2019-08-21 02:58:39 +0800

    Level 3 completed
    Level 3 completed, application now makes use of the newly created Task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    private static List<String> dukeList = new ArrayList<>();
+    private static List<Task> dukeList = new ArrayList<>();
 
     public static void main(String[] args) {
         String initialMessage = "Hello! I'm Duke\nWhat can I do for you?";
@@ -13,14 +13,25 @@ public class Duke {
 
         while (sc.hasNextLine()) {
             String input = sc.nextLine();
+            String[] inputSplit = input.split(" ");
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 return;
             } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 displayDukeList();
+            } else if (inputSplit[0].equals("done")) {
+                int inputIndex = Integer.parseInt(inputSplit[1]);
+                int actualListIndex = inputIndex - 1;
+                Task targetTask = dukeList.get(actualListIndex);
+                targetTask.markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("["+ targetTask.getStatusIcon() + "] "
+                                    + targetTask.getDescription());
             } else {
-                dukeList.add(input);
-                System.out.println("added: " + input);
+                Task t = new Task(input);
+                dukeList.add(t);
+                System.out.println("added: " + t.getDescription());
             }
         }
     }
@@ -28,9 +39,9 @@ public class Duke {
     public static void displayDukeList() {
         for (int i = 0; i < dukeList.size(); i++) {
             int itemIndex = i + 1;
-            String itemDisplay = itemIndex + ". " + dukeList.get(i);
+            String itemDisplay = itemIndex + ".[" + dukeList.get(i).getStatusIcon() + "] "
+                                 + dukeList.get(i).getDescription();
             System.out.println(itemDisplay);
         }
     }
-
 }

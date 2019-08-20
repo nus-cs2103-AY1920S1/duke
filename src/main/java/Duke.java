17e814be9fe49216906ed756eb34fe84869361bc
JohnNzj <johnng1@hotmail.com>
2019-08-20 18:17:08 +0800

    Add Task.java, Mark tasks as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,19 +10,26 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         while (sc.hasNext()) {
             String command = sc.next();
             if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 return;
             } else if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 1; i <= tasks.size(); i++) {
-                    System.out.println(i + ". " + tasks.get(i - 1));
+                    System.out.println(i + "." + tasks.get(i - 1));
                 }
+            } else if (command.equals("done")) {
+                int number = sc.nextInt();
+                System.out.println("Nice! I've marked this task as done:");
+                tasks.get(number - 1).markAsDone();
+                System.out.println("  " + tasks.get(number - 1));
             } else {
                 String task = command + sc.nextLine();
-                tasks.add(task);
+                Task t = new Task(task);
+                tasks.add(t);
                 System.out.println("added: " + task);
             }
         }

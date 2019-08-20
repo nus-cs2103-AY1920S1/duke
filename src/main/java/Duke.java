f451921820d51d4f6ac4de74e4e5c2c0af175d72
hooncp <hooncheep@gmail.com>
2019-08-20 22:22:00 +0800

    level 1 of Duke program

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
+    public void greet() {
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+    }
+
+    public void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        duke.greet();
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String command = scanner.next();
+            if (command.equals("bye")) {
+                break;
+            } else {
+                System.out.println(command);
+            }
+        }
+        duke.bye();
     }
 }

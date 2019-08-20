f20a4b3c00e4a2ba1fc198c1ecf46497c4dd96be
Chee Yuan <chong-1997@hotmail.com>
2019-08-20 22:33:12 +0800

    Finish level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.StringJoiner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String intro = "Hello! I'm Duke\n" +
                 "What can I do for you?\n";
+        List<String> history = new ArrayList<>();
 
         System.out.println(intro);
         String input;
         while(!(input = sc.nextLine()).equals("bye")) {
-            System.out.println(wrap(input));
+            switch(input) {
+                case "list":
+                    System.out.println(historyToString(history));
+                default:
+                    history.add(input);
+                    System.out.println(wrap("added: " + input));
+            }
         };
+
         String endMessage = "Bye. Hope to see you again soon!";
         System.out.println(wrap(endMessage));
+    }
+
+    private static String historyToString(List<String> history) {
+        StringJoiner sj = new StringJoiner("\n");
+        for(int i = 0; i < history.size(); i++) {
+            sj.add((i + 1) + ". " + history.get(i));
+        }
+        return sj.toString();
     }
 
     private static String wrap(String str) {

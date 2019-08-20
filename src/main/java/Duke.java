bad3192d58fd9b01010675d27f4ed23e8c5fae73
Sahilgat <nisgat@live.com>
2019-08-20 16:56:41 +0800

    Add level 3 features: Mark as done ability and formatting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 //Test comment
 public class Duke {
-    private ArrayList<String> tasks = new ArrayList<String>();
+    private ArrayList<Task> tasks = new ArrayList<Task>();
     /*
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -21,27 +21,42 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
     public void list() {
+        System.out.println("Here are the tasks in your list:");
         for(int i = 1; i <= this.tasks.size(); i++) {
-            System.out.println(i + ". " + this.tasks.get(i - 1));
+            Task curr_task = this.tasks.get(i-1);
+            System.out.println(i + ".[" + curr_task.getStatusIcon() + "] " + curr_task);
         }
     }
-    public void add(String task) {
+    public void add(Task task) {
         this.tasks.add(task);
         System.out.println("added: " + task);
+    }
+    public void done(int task_num) {
+        Task curr_task = this.tasks.get(task_num - 1);
+        curr_task.setDone();
+        System.out.println("Nice! I've marked this task as done: ");
+        System.out.println("  " + "[" + curr_task.getStatusIcon() + "] " + curr_task);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Duke duke = new Duke();
         duke.introduction();
-        while (sc.hasNextLine()) {
+        while(sc.hasNextLine()) {
             String command = sc.nextLine();
             if(command.equals("bye")) {
                 duke.exit();
                 return;
-            } else if (command.equals("list")) {
+            } else if(command.equals("list")) {
                 duke.list();
+            } else if(command.startsWith("done ")) {
+                String[] splited = command.split(" ");
+                if(splited[1].matches("^[0-9]*[1-9][0-9]*$") && splited.length == 2) {
+                    duke.done(Integer.parseInt(splited[1]));
+                } else {
+                    duke.add(new Task(command));
+                }
             } else {
-                duke.add(command);
+                duke.add(new Task(command));
             }
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    protected boolean isDone;
+    protected String name;
+
+    public Task(String name) {
+        this.isDone = false;
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "✓" : "✗"); //return tick or X symbols
+    }
+
+    public void setDone() {
+        this.isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+}

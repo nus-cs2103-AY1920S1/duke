75d0e93b569dbe35393d60b504835d0b0ee8d1ae
DivineDX <chia.dexun97@gmail.com>
2019-08-20 12:03:56 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,45 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+    private Scanner sc = new Scanner(System.in);
+
+    String lineWrap(String input) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("____________________________________________________________\n");
+        sb.append(input + "\n");
+        sb.append("____________________________________________________________\n");
+        return sb.toString();
+    }
+
+    private String greet() {
+        StringBuilder sb = new StringBuilder();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        sb.append(logo);
+        sb.append("Hello! I'm Duke\n");
+        sb.append("What can I do for you?");
+        return sb.toString();
     }
+
+    private String bye() {
+        return lineWrap("Bye. Hope to see you again soon!");
+    }
+
+    void start() {
+        System.out.println(greet());
+        while(sc.hasNextLine()) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                System.out.println(bye());
+                break;
+            } else {
+                System.out.println(lineWrap(command));
+            }
+        }
+    }
+
+
 }

59dc307637dc0f680895edecde0c6ccbc6d46a39
Gihun Ko <rhrlgns96@gmail.com>
2019-08-20 12:48:17 +0800

    Level-2

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,13 +1,17 @@
 package seedu.duke;
 
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
-    private static String greetingMsg = "    ____________________________________________________________\n" +
+    private static String greetingMsg =
+            "    ____________________________________________________________\n" +
             "     Hello! I'm Duke\n" +
             "     What can I do for you?\n" +
             "    ____________________________________________________________";
-    private static String logo = " ____        _        \n"
+    private static String logo =
+              " ____        _        \n"
             + "|  _ \\ _   _| | _____ \n"
             + "| | | | | | | |/ / _ \\\n"
             + "| |_| | |_| |   <  __/\n"
@@ -15,17 +19,32 @@ public class Duke {
 
     private static void run() {
         Scanner sc = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
+
         System.out.println(greetingMsg);
         boolean exit = false;
         while (!exit) {
-            String input = sc.next();
+            String input = sc.nextLine();
             if (input.equals("bye")) {
                 exit = true;
-            } else
-                System.out.println(input + "\n");
+            } else if (input.equals("list")) {
+                displayList(list);
+            } else {
+                list.add(input);
+                System.out.println("added: " + input + "\n");
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
+
+    private static void displayList(List<String> list) {
+        int index = 0;
+        for (String l : list) {
+            index++;
+            System.out.println(index + ". " + l);
+        }
+    }
+
     public static void main(String[] args) {
         run();
     }

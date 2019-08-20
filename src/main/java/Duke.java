44ebeeec904821459f07ecbb79c72e07be3a2824
Zhang Dongjun <dexterzhang430@hotmail.com>
2019-08-20 22:42:38 +0800

    add Duke.java, Task.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,30 +12,39 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
     }
     */
-    ArrayList<String> store = new ArrayList<>();
+    ArrayList<Task> store = new ArrayList<>();
 
     public void greet() {
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
     }
 
     public void run() {
         boolean flag = true;
         Scanner sc = new Scanner(System.in);
         while(sc.hasNextLine()) {
-            String cmd = sc.nextLine();
-            switch(cmd) {
+            String cmd = sc.nextLine(); //Read the command
+            String head[] = cmd.split(" "); //Break the command into array
+            switch(head[0]) { //Read the first key word
                 case "bye":
                     System.out.println("Bye. Hope to see you again soon!");
                     flag = false;
                     break;
                 case "list":
                     for(int i = 0; i < store.size(); ++i) {
-                        System.out.println(i + 1 + ". " + store.get(i));
+                        System.out.println(i + 1 + "." + store.get(i));
                     }
+                    break;
+                case "done":
+                    System.out.println("Nice! I've marked this task as done:");
+                    int num = Integer.parseInt(head[1]);
+                    Task newTask = store.get(num);
+                    newTask.markAsDone();
+                    store.set(num, newTask);
+                    System.out.println(store.get(num));
                     break;
                 default:
                     System.out.println("added: " + cmd);
-                    store.add(cmd);
+                    store.add(new Task(cmd));
                     break;
             }
             if (!flag) {

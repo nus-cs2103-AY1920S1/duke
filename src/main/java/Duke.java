386e34f4c98819c1d36bc5d81535c78324ed11f4
Na-Nazhou <nazhou.na9@gmail.com>
2019-08-20 20:33:25 +0800

    Add the ability to mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class Duke {
 
-    private static ArrayList<String> list = new ArrayList<>();
+    private static ArrayList<Task> list = new ArrayList<>();
 
     public static void main(String[] args) {
         String greet = "Hello! I'm Duke\nWhat can I do for you?\n";
@@ -19,11 +19,23 @@ public class Duke {
             } 
 
             if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.println((i + 1) + ". " + list.get(i));
+                    Task task = list.get(i);
+                    System.out.println((i + 1) + "." + task);
+                }
+            } else if (input.matches("^done \\d+$")) {
+                int taskId = Integer.parseInt(input.split(" ")[1]); 
+                try {
+                    Task task = list.get(taskId - 1);
+                    task.markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  " + task);
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("There is no task with ID " + taskId + ". Please enter a valid ID.");
                 }
             } else {
-                list.add(input);
+                list.add(new Task(input));
                 System.out.println("added: " + input);
             }
             System.out.println();
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -11,11 +11,12 @@ public class Task {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
     }
 
-    public String getDescriptionWithStatus() {
-        return "[" + getStatusIcon() + "] " + description;
-    }
-
     public void markAsDone() {
         this.isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
     }
 }

fe8edd7cf3d5ade7d78322085cf83f6d1bb10555
chanjunren <chanjunren@gmail.com>
2019-08-20 14:06:11 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,21 +10,31 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> items = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         while (true) {
             String input = sc.nextLine();
+            String[] splitString = input.split(" ");
             if (input.equals("list")) {
-                for (int i = 0; i < items.size(); i++) {
-                    System.out.println(i + 1 + ". " + items.get(i));
+                System.out.println("Here are the tasks in your list:");
+
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(i + 1 + ".[" + tasks.get(i).getStatusIcon()
+                            + "] " + tasks.get(i).getDescription());
                 }
-            }
-            else if (input.equals("bye")) {
+            } else if (splitString[0].equals("done")) {
+                int taskNum = Integer.valueOf(splitString[1]) - 1;
+                tasks.get(taskNum).markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  [" + tasks.get(taskNum).getStatusIcon() + "] " +
+                        tasks.get(taskNum).getDescription());
+            } else if (input.equals("bye")) {
                 System.out.println("Bye! Hope to see you again!");
                 return;
+            } else {
+                tasks.add(new Task(input));
+                System.out.println("added: " + input);
             }
-            items.add(input);
-            System.out.println("added: " + input);
         }
     }
 }

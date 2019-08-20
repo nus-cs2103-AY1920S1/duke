7f0b77773016463042f9dcbe706e534d62213e13
LimYiJie <e0325578@u.nus.edu>
2019-08-20 23:16:38 +0800

    Level-2 Completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,35 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
+        //Level 2
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
+        ArrayList<String> events = new ArrayList<>();
+        String input = sc.nextLine();
         System.out.print("    ____________________________________________________________\n" +
                 "     Hello! I'm Duke\n" +
                 "     What can I do for you?\n" +
                 "    ____________________________________________________________\n");
         while (!input.equalsIgnoreCase("bye")) {
-            System.out.print("    ____________________________________________________________\n" +
-                    "    "+ input + "\n" +
-                    "    ____________________________________________________________\n");
-            input = sc.next();
+            if (input.equals("list")) {
+                String result = "";
+                for (int i = 0; i < events.size(); i = i + 1) {
+                    result = result + "    " + (i + 1) + ". " + events.get(i) + "\n";
+                }
+                result = result.equals("") ? "\n" : result;
+                System.out.print("    ____________________________________________________________\n" +
+                         result +
+                        "    ____________________________________________________________\n");
+                input = sc.nextLine();
+                continue;
+            } else {
+                System.out.print("    ____________________________________________________________\n" +
+                        "    added: "+ input + "\n" +
+                        "    ____________________________________________________________\n");
+                events.add(input);
+                input = sc.nextLine();
+            }
         }
 
         System.out.print("    ____________________________________________________________\n" +

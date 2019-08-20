06b1b7a80979236a6ab61a0c508930bfdc969c3a
ckb055 <chenkaibin7@gmail.com>
2019-08-20 19:23:57 +0800

    Level 5. Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -28,32 +28,51 @@ public class Duke {
                 int index = Integer.parseInt(next.split(" ")[1]);
                 tasks.get(index - 1).markAsDone();
                 System.out.println("Nice! I've marked this task as done\n" + tasks.get(index - 1));
+            } else if (firstWord.equals("todo") || firstWord.equals("deadline") || firstWord.equals("event")){
+                try {
+                    String[] arrCommands = next.split(" ");
+                    if (arrCommands.length == 1) {
+                        throw new DukeException("☹ OOPS!!! The description of a " + firstWord + " cannot be empty.");
+                    }
+                    // remaining is the remaining words after the first command, e.g. todo
+                    String remaining = next.split(" ", 2)[1];
+                    // desc is the description of the task
+                    String desc = remaining.split("/")[0];
+                    switch (firstWord) {
+                        case "todo":
+                            Task todo = new ToDo(desc);
+                            tasks.add(todo);
+                            System.out.println("Got it. I've added this task: \n\t"
+                                    + todo + "\n" + "Now you have " + tasks.size() + " tasks in the list.");
+                            break;
+                        case "deadline":
+                            String dueDateDeadline = remaining.split("/by ")[1];
+                            Task deadline = new Deadline(desc, dueDateDeadline);
+                            tasks.add(deadline);
+                            System.out.println("Got it. I've added this task: \n\t"
+                                    + deadline + "\n" + "Now you have " + tasks.size() + " tasks in the list.");
+                            break;
+                        case "event":
+                            String dueDateEvent = remaining.split("/at ")[1];
+                            Task event = new Event(desc, dueDateEvent);
+                            tasks.add(event);
+                            System.out.println("Got it. I've added this task: \n\t"
+                                    + event + "\n" + "Now you have " + tasks.size() + " tasks in the list.");
+                            break;
+                        default:
+                            throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    }
+                } catch (DukeException de) {
+                    System.out.println(de);
+                } catch (Exception e) {
+                    System.out.println("General Error");
+                }
             } else {
-                String remaining = next.split(" ", 2)[1];
-                String desc = remaining.split("/")[0];
-                switch (firstWord) {
-                    case "todo":
-                        Task todo = new ToDo(desc);
-                        tasks.add(todo);
-                        System.out.println("Got it. I've added this task: \n\t"
-                                + todo + "\n" + "Now you have " + tasks.size() + " tasks in the list.");
-                        break;
-                    case "deadline":
-                        String dueDateDeadline = remaining.split("/by ")[1];
-                        Task deadline = new Deadline(desc, dueDateDeadline);
-                        tasks.add(deadline);
-                        System.out.println("Got it. I've added this task: \n\t"
-                                + deadline + "\n" + "Now you have " + tasks.size() + " tasks in the list.");
-                        break;
-                    case "event":
-                        String dueDateEvent = remaining.split("/at ")[1];
-                        Task event = new Event(desc, dueDateEvent);
-                        tasks.add(event);
-                        System.out.println("Got it. I've added this task: \n\t"
-                                + event + "\n" + "Now you have " + tasks.size() + " tasks in the list.");
-                        break;
-                    default:
-                        System.out.println("error, no such command");
+                try {
+                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
+                catch (DukeException de) {
+                    System.out.println(de);
                 }
             }
         }

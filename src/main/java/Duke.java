42904a9a81b1b5796d090bcf49412a9b21da9694
kensonoenqh <KimberlyOen@Oens-MacBook-Air.local>
2019-08-20 14:35:43 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printline("Hello! I'm Duke\n     What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        while (!input.equals("bye")) {
+            printline(input);
+            input = sc.next();
+        }
+        printline("Bye. Hope to see you again soon!");
+    }
+
+    public static void printline(String s) {
+        String indentedline = "    ____________________________________________________________";
+        System.out.println(indentedline);
+        System.out.println("     " + s);
+        System.out.println(indentedline);
     }
 }

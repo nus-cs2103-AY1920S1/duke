c244c072f7727afc5c604229290dcf8994a1d16a
Wang Xueting <wang.xueting@u.nus.edu>
2019-08-21 20:05:17 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Duke {
 
-    static String[] taskList = new String[100];
-    static int number = 0;
+    static Task[] taskList = new Task[100];
+    static int totalNumber = 0;
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -18,32 +18,60 @@ public class Duke {
         System.out.println("    ____________________________________________________________");
 
         Scanner sc = new Scanner(System.in);
-        addList(sc);
+        readList(sc);
     }
 
-    public static void addList(Scanner sc) {
+    public static void readList(Scanner sc) {
         while(sc.hasNext()) {
             String command = sc.nextLine();
+            String[] words = command.split(" ", 2);
+            String action = words[0];
+
             if (command.equals("bye")) {
-                System.out.println("    ____________________________________________________________");
-                System.out.println("     Bye. Hope to see you again soon!");
-                System.out.println("    ____________________________________________________________");
+                printBye();
                 return;
+            } else if (action.equals("done")) {
+                int doneIndex = Integer.parseInt(words[1]);
+                doneTask(doneIndex);
             } else if (command.equals("list")) {
-                System.out.println("    ____________________________________________________________");
-                for (int i = 0; i < number; i++) {
-                    int index = i + 1;
-                    System.out.println("     " + index + ". " + taskList[i]);
-                }
-                System.out.println("    ____________________________________________________________");
+                printList();
             } else {
-                taskList[number] = command;
-                number++;
-                System.out.println("    ____________________________________________________________");
-                System.out.println("     added: " + command);
-                System.out.println("    ____________________________________________________________");
+                addTask(command);
             }
         }
+    }
+
+    public static void printList() {
+        System.out.println("    ____________________________________________________________");
+        for (int i = 0; i < totalNumber; i++) {
+            int index = i + 1;
+            System.out.println("     " + index + ". " + taskList[i]);
+        }
+        System.out.println("    ____________________________________________________________");
+
+    }
+
+    public static void addTask(String command) {
+        taskList[totalNumber] = new Task(command);
+        totalNumber++;
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     added: " + command);
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void doneTask(int num) {
+        Task currentTask = taskList[num - 1];
+        currentTask.markAsDone();
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Nice! I've marked this task as done: ");
+        System.out.println("       " + currentTask);
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void printBye() {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
     }
 
 }

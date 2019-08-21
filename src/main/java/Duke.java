c3d6612832224a97d383ba3ebe1c734d24d83afd
Chaitanya Baranwal <chaitanyabaranwal@gmail.com>
2019-08-21 16:59:14 +0800

    Add level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+     
+        Scanner sc = new Scanner(System.in);
+
+        // Print initial welcome string 
+        System.out.println("    --------------------------------------------------------");
+        System.out.println(" Hello! I'm Duke :)\n What can I do for you?");
+        System.out.println("    --------------------------------------------------------");
+
+        // Run till user types bye
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println("    --------------------------------------------------------");
+            System.out.println("     " + input);
+            System.out.println("    --------------------------------------------------------");
+            input = sc.nextLine();
+        }
+
+        // Print exit string
+        System.out.println("    --------------------------------------------------------");
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("    --------------------------------------------------------");
     }
 }

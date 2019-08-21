ef37aab3329c320af3c06c76a430bc9f1467303d
sandydays <sandhyar725@gmail.com>
2019-08-22 01:36:53 +0800

    Add features of list of todos, adding to this list, and displaying list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,24 @@
 import java.util.Scanner;
+import java.util.LinkedList;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        LinkedList<String> todos = new LinkedList<>();
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         String userInput = sc.nextLine();
 
         while (!userInput.equals("bye")) {
-            System.out.println(userInput);
+            switch (userInput) {
+                case "list":
+                    for (int i = 0; i < todos.size(); i++) {
+                        System.out.println((i + 1) + ": " + todos.get(i));
+                    }
+                    break;
+                default:
+                    System.out.println("added: " + userInput);
+                    todos.add(userInput);
+            }
             userInput = sc.nextLine();
         }
 

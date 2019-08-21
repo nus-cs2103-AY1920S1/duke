6ee1b7e06fa290bf84fdd26d8870c970cc7b271a
victorvic54 <sacchirro@yahoo.com>
2019-08-21 20:42:31 +0800

    Finished Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,14 +12,18 @@ public class Duke {
         String inputText = sc.nextLine();
 
         while (!inputText.equals("bye")) {
+            String[] keyList = inputText.split(" ", 2);
+            String actionKey = keyList[0];
+
             if (inputText.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 int counter = 1;
 
                 for (Task subTask: taskList) {
-                    System.out.println(counter + ".[" + subTask.getStatusIcon() + "] " + subTask.getDescription());
+                    System.out.println(counter + ". " + subTask);
                     counter++;
                 }
-            } else if (inputText.split(" ")[0].equals("done")) {
+            } else if (actionKey.equals("done")) {
                 System.out.println("Nice! I've marked this task as done: ");
 
                 int index = Integer.parseInt(inputText.split(" ")[1]);
@@ -28,10 +32,35 @@ public class Duke {
 
                 System.out.println("[" + selectedTask.getStatusIcon() + "] " + selectedTask.getDescription());
             } else {
-                System.out.println("added: " + inputText);
+                String descriptionKey = "";
 
-                Task newTask = new Task(inputText);
-                taskList.add(newTask);
+                try {
+                    descriptionKey = keyList[1];
+
+                    System.out.println("Got it. I've added this task: ");
+                    if (actionKey.equals("deadline")) {
+                        String[] contentList = descriptionKey.split(" /by ");
+                        Deadline newDeadline = new Deadline(contentList[0], contentList[1]);
+                        System.out.println(newDeadline);
+                        taskList.add(newDeadline);
+                    } else if (actionKey.equals("event")) {
+                        String[] contentList = descriptionKey.split(" /at ");
+                        Event newEvent = new Event(contentList[0], contentList[1]);
+                        System.out.println(newEvent);
+                        taskList.add(newEvent);
+                    } else if (actionKey.equals("todo")) {
+                        Todo newTodo = new Todo(inputText.split(" ", 2)[1]);
+                        System.out.println(newTodo);
+                        taskList.add(newTodo);
+                    } else {
+                        Task newTask = new Task(inputText.split(" ", 2)[1]);
+                        System.out.println(newTask);
+                        taskList.add(newTask);
+                    }
+                    System.out.println("Now you have " + taskList.size() + " tasks in the list.");
+                } catch (ArrayIndexOutOfBoundsException ex) {
+                    // command error
+                }
             }
 
             inputText = sc.nextLine();

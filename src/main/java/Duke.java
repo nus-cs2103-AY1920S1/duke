492e0ccc37602540772f7801de1a53b89565632a
huiminlim <e0036379@u.nus.edu>
2019-08-21 22:58:58 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+enum Day{
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+}
+
 public class Duke {
     private static final String HORIZONTAL_LINE = "\t____________________________________________________________";
 
@@ -35,9 +39,9 @@ public class Duke {
                 String printList = "Here are the tasks in your list:\n";
 
                 for(int i = 0; i < listCounter; i++){
-                    String temp = "\t\t" + (i + 1) + "." +
-                            list.get(i).getStatusIcon() + " " + list.get(i).getTaskItem();
-                    printList = printList + temp + "\n";
+                    String temp = "\t\t" + (i + 1) + "." + list.get(i).getItemInfo();
+                    printList = printList + temp;
+                    if(i != listCounter - 1) printList = printList + "\n";
                 }
 
                 System.out.println(processText(printList));
@@ -56,16 +60,42 @@ public class Duke {
                 System.out.println(processText(tempPrint));
             }
 
+            else if(readInput.toLowerCase().equals("event")){
+                String input = sc.nextLine().trim();
+                String[] tokenList = input.split("/");
+
+                list.add(new Event(tokenList[0], tokenList[1], false));
+                listCounter++;
+
+                String tempPrint = "Got it. I've added this task:\n\t\t" +
+                        list.get(listCounter - 1).getItemInfo() +
+                        "\n\tNow you have " + listCounter + " tasks in the list.";
+
+                System.out.println(processText(tempPrint));
+            }
+
+            else if(readInput.toLowerCase().equals("deadline")){
+                String input = sc.nextLine().trim();
+                String[] tokenList = input.split("/");
+
+                list.add(new Deadline(tokenList[0], tokenList[1], false));
+                listCounter++;
+
+                String tempPrint = "Got it. I've added this task:\n\t\t" +
+                        list.get(listCounter - 1).getItemInfo() +
+                        "\n\tNow you have " + listCounter + " tasks in the list.";
+
+                System.out.println(processText(tempPrint));
+            }
+
             else if(readInput.toLowerCase().equals("done")){
                 // Might need to implement exception handling
                 int indexDone = sc.nextInt();
 
                 list.get(--indexDone).setDone();
 
-                String doneMessage = "Nice! I've marked this task as done: \n\t";
-                System.out.println(processText(doneMessage +
-                        list.get(indexDone).getStatusIcon() + " " +
-                        list.get(indexDone).getTaskItem()));
+                String doneMessage = "Nice! I've marked this task as done: \n\t\t";
+                System.out.println(processText(doneMessage + list.get(indexDone).getItemInfo()));
             }
 
             else{
@@ -103,7 +133,7 @@ class Task{
     // Non-default Constructor
     public Task(String taskItem, boolean isDone){
         this.isDone = isDone;
-        this.taskItem = taskItem;
+        this.taskItem = taskItem.trim();
     }
 
     public void setDone(){
@@ -118,12 +148,16 @@ class Task{
     public String getTaskItem(){
         return taskItem;
     }
+
+    public String getItemInfo(){
+        return getStatusIcon() + " " + getTaskItem();
+    }
 }
 
 class ToDo extends Task{
 
     public ToDo(String toDo, boolean isDone){
-        super(toDo, isDone);
+        super(toDo.trim(), isDone);
     }
 
     @Override
@@ -131,4 +165,66 @@ class ToDo extends Task{
         if(isDone) return "[T][" + TICK + "]";
         else return "[T][" + CROSS + "]";
     }
+}
+
+class Deadline extends Task{
+    private String deadline;
+    private boolean isDeadlineProcessed;
+
+    public Deadline(String event, String timing, boolean isDeadlineProcessed){
+        super(event.trim(), false);
+        this.deadline = timing.trim();
+        this.isDeadlineProcessed = isDeadlineProcessed;
+    }
+
+    @Override
+    public String getStatusIcon() {
+        if (isDone) return "[D][" + TICK + "]";
+        else return "[D][" + CROSS + "]";
+    }
+
+    public String getDeadline(){
+        return this.deadline;
+    }
+
+    @Override
+    public String getItemInfo(){
+        if(!isDeadlineProcessed) {
+            deadline = deadline.substring(3);
+            isDeadlineProcessed = true;
+        }
+        return getStatusIcon() + " " + getTaskItem() + " (by: " + getDeadline() + ")";
+    }
+}
+
+class Event extends Task {
+    private String timing;
+    private boolean isTimingProcessed;
+
+    public Event(String event, String timing, boolean isDone) {
+        super(event.trim(), isDone);
+        this.timing = timing;
+        this.isTimingProcessed = false;
+    }
+
+    @Override
+    public String getStatusIcon() {
+        if (isDone) return "[E][" + TICK + "]";
+        else return "[E][" + CROSS + "]";
+    }
+
+    public String getTiming() {
+        return timing;
+    }
+
+    @Override
+    public String getItemInfo(){
+
+        if(!isTimingProcessed) {
+            timing = timing.substring(3);
+            isTimingProcessed = true;
+        }
+        return getStatusIcon() + " " + getTaskItem() + " (at: " + getTiming() + ")";
+    }
+
 }

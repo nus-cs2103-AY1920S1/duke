e744edb3b66a85a824ba611349f1092412148407
hooncp <hooncheep@gmail.com>
2019-08-21 23:47:18 +0800

    integrated Event Todo and Deadline to Duke and complete level 5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -30,12 +30,13 @@ public class Duke {
     public void add(Task task) {
         numberOfTasks++;
         tasks.add(task);
-        String outputString = "added: " + task.getDescription();
+        String outputString = "Got it. I've added this task: \n" + task.toString();
         System.out.println(outputString);
+        System.out.println("Now you have " + numberOfTasks + " tasks in the list.");
     }
 
     public void done(int number) {
-        Task task = tasks.get(number - 1);
+        Task task = Duke.tasks.get(number - 1);
         task.setDone();
         System.out.println("Nice! I've marked this task as done: ");
         System.out.println(task.toString());
@@ -48,20 +49,35 @@ public class Duke {
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
             if (command.equalsIgnoreCase("bye")) {
+                duke.bye();
                 break;
             } else if (command.equalsIgnoreCase("list")){
                 duke.list();
             } else {
                 String[] commandSplit = command.split(" ");
+                String deadline = "deadline";
+                String event = "event";
+                String todo = "todo";
                 if (!commandSplit[0].equalsIgnoreCase("done")) {
-                    Task task = new Task(command);
-                    duke.add(task);
+                    if (commandSplit[0].equalsIgnoreCase(deadline)) {
+                        String[] detail = command.substring(deadline.length()).split("/by");
+                        Task addTask = new Deadline(detail[0], detail[1]);
+                        duke.add(addTask);
+                    } else if (commandSplit[0].equalsIgnoreCase(event)) {
+                        String[] detail = command.substring(event.length()).split("/at");
+                        Task addTask =  new Event(detail[0], detail[1]);
+                        duke.add(addTask);
+                    } else {
+                        Task addTask = new Todo(command.substring(todo.length()));
+                        duke.add(addTask);
+                    }
+
                 } else {
                     int index = Integer.parseInt(commandSplit[1]);
                     duke.done(index);
                 }
             }
         }
-        duke.bye();
+
     }
 }

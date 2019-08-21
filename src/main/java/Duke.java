ae72b4222164038aea70e12727ff331faafc791a
charliechoong <e0202817@u.nus.edu>
2019-08-21 15:06:54 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,17 @@
 import java.util.Scanner;
 public class Duke {
+    private static int numTasks = 0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] taskList = new String[100];
-        Integer numTasks = new Integer(0);
 
         printReply("Hello! I'm Duke\n\tWhat can I do for you?");
         String command = sc.nextLine();
         while(!command.equals("bye")){
-            if(command.equals("list")) {
-                String listReply = "";
-                for (int i = 0; i < numTasks; i++) {
-                    listReply += (i + 1) + ". " + taskList[i];
-                    if (i != numTasks - 1)
-                        listReply += "\n\t";
-                }
-                printReply(listReply);
-            }
-            else {
-                addToList(command, taskList, numTasks);
-                numTasks++;
-            }
+            if(command.equals("list"))
+                printList(taskList);
+            else
+                addToList(command, taskList);
             command = sc.next();
         }
         printReply("Bye. Hope to see you again soon!");
@@ -29,8 +20,17 @@ public class Duke {
         System.out.println("\t________________________________________\n\t" + reply
                            + "\n\t________________________________________");
     }
-    public static void addToList(String task, String[] taskList, Integer numTasks){
-        taskList[numTasks] = task;
+    public static void addToList(String task, String[] taskList){
+        taskList[numTasks++] = task;
         printReply("added: " + task);
+    }
+    public static void printList(String[] taskList){
+        String listReply = "";
+        for(int i=0; i<numTasks; i++){
+            listReply += (i+1) + ". " + taskList[i];
+            if(i != numTasks-1)
+                listReply += "\n\t";
+            printReply(listReply);
+        }
     }
 }

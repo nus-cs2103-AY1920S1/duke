ebed79200655dc5daa4af92df35c4f71d2301cd7
charliechoong <e0202817@u.nus.edu>
2019-08-21 14:22:19 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,10 +2,25 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] taskList = new String[100];
+        Integer numTasks = new Integer(0);
+
         printReply("Hello! I'm Duke\n\tWhat can I do for you?");
-        String command = sc.next();
+        String command = sc.nextLine();
         while(!command.equals("bye")){
-            printReply(command);
+            if(command.equals("list")) {
+                String listReply = "";
+                for (int i = 0; i < numTasks; i++) {
+                    listReply += (i + 1) + ". " + taskList[i];
+                    if (i != numTasks - 1)
+                        listReply += "\n\t";
+                }
+                printReply(listReply);
+            }
+            else {
+                addToList(command, taskList, numTasks);
+                numTasks++;
+            }
             command = sc.next();
         }
         printReply("Bye. Hope to see you again soon!");
@@ -13,5 +28,9 @@ public class Duke {
     public static void printReply(String reply){
         System.out.println("\t________________________________________\n\t" + reply
                            + "\n\t________________________________________");
+    }
+    public static void addToList(String task, String[] taskList, Integer numTasks){
+        taskList[numTasks] = task;
+        printReply("added: " + task);
     }
 }

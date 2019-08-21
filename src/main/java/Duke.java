de0eb2451e81a9123a539a5cf8214f5e84cecae8
Na-Nazhou <nazhou.na9@gmail.com>
2019-08-21 17:40:54 +0800

    Improve variable naming and error messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,36 +6,42 @@ public class Duke {
     private static ArrayList<Task> list = new ArrayList<>();
 
     public static void main(String[] args) {
-        String greet = "Hello! I'm Duke\nWhat can I do for you?\n";
-        System.out.println(greet);
+        // Greet
+        String greeting = "Hello! I'm Duke\nWhat can I do for you?\n";
+        System.out.println(greeting);
 
         Scanner scanner = new Scanner(System.in);
 
         while (scanner.hasNextLine()) {
-            String input = scanner.nextLine();
-            if (input.equals("bye")) {
+            String command = scanner.nextLine();
+
+            // Exit
+            if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
+
             try {
-                run(input);
+                run(command);
             } catch (DukeException e) {
                 System.out.println(e);
             }
+
+            // Empty line separator at the end of the command response
             System.out.println();
         }
     }
 
-    private static void run(String input) throws DukeException {
-        if (input.equals("list")) {
+    private static void run(String command) throws DukeException {
+        if (command.trim().equals("list")) {
             showTasks();
-        } else if (input.matches("^done\\s+\\d+$")) {
-            int taskId = Integer.parseInt(input.split("\\s+")[1]);
+        } else if (command.matches("^done\\s+\\d+$")) {
+            int taskId = Integer.parseInt(command.split("\\s+")[1]);
             completeTask(taskId);
-        } else if (input.matches("^(todo|deadline|event).*")) {
-            addTask(input);
-        } else if (input.matches("^delete\\s+\\d+$")) {
-            int taskId = Integer.parseInt(input.split("\\s+")[1]);
+        } else if (command.matches("^(todo|deadline|event).*")) {
+            addTask(command);
+        } else if (command.matches("^delete\\s+\\d+$")) {
+            int taskId = Integer.parseInt(command.split("\\s+")[1]);
             deleteTask(taskId);
         } else {
             throw new DukeException("I'm sorry, but I don't know what that means :-(");
@@ -43,10 +49,14 @@ public class Duke {
     }
 
     private static void showTasks() {
-        System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < list.size(); i++) {
-            Task task = list.get(i);
-            System.out.println((i + 1) + "." + task);
+        if (list.size() == 0) {
+            System.out.println("There are currently no task in your list.");
+        } else {
+            System.out.println("Here are the tasks in your list:");
+            for (int i = 0; i < list.size(); i++) {
+                Task task = list.get(i);
+                System.out.printf("%d.%s\n", (i + 1), task);
+            }
         }
     }
 
@@ -55,54 +65,54 @@ public class Duke {
             Task task = list.get(taskId - 1);
             task.markAsDone();
             System.out.println("Nice! I've marked this task as done:");
-            System.out.println("  " + task);
+            System.out.printf("  %s\n", task);
         } catch (IndexOutOfBoundsException e) {
             throw new DukeException(String.format(
-                    "Task No.%d is not present in the list. Please enter a valid task ID.", taskId));
+                    "Task No.%d is not present in your list. Please enter a valid task ID.", taskId));
         }
     }
 
-    private static void addTask(String input) throws DukeException {
-        String[] inputBreakDown = input.trim().split("\\s+", 2);
-        String taskType = inputBreakDown[0];
+    private static void addTask(String command) throws DukeException {
+        String[] commandArgs = command.trim().split("\\s+", 2);
+        String taskType = commandArgs[0];
 
         Task newTask;
 
         switch(taskType) {
             case "todo":
-                if (inputBreakDown.length < 2 ) {
+                if (commandArgs.length < 2 ) {
                     throw new DukeException("The description of a todo cannot be empty.");
                 }
 
-                newTask = new Todo(inputBreakDown[1]);
+                newTask = new Todo(commandArgs[1]);
                 break;
             case "deadline":
-                if (inputBreakDown.length < 2 || inputBreakDown[1].equals("/by")) {
+                if (commandArgs.length < 2 || commandArgs[1].equals("/by")) {
                     throw new DukeException("The description and the due time of a deadline cannot be empty.");
                 }
-                if (inputBreakDown[1].indexOf("/by") == 0) {
+                if (commandArgs[1].startsWith("/by")) {
                     throw new DukeException("The description of a deadline cannot be empty.");
                 }
-                if (!inputBreakDown[1].contains("/by") || inputBreakDown[1].matches(".*/by$")) {
+                if (!commandArgs[1].contains("/by") || commandArgs[1].endsWith("/by")) {
                     throw new DukeException("The due time of a deadline cannot be empty.");
                 }
 
-                String[] deadlineBreakDown = inputBreakDown[1].split("\\s*/by\\s*", 2);
-                newTask = new Deadline(deadlineBreakDown[0], deadlineBreakDown[1]);
+                String[] deadlineArgs = commandArgs[1].split("\\s*/by\\s*", 2);
+                newTask = new Deadline(deadlineArgs[0], deadlineArgs[1]);
                 break;
             case "event":
-                if (inputBreakDown.length < 2 || inputBreakDown[1].equals("/at")) {
+                if (commandArgs.length < 2 || commandArgs[1].equals("/at")) {
                     throw new DukeException("The description and the time of an event cannot be empty.");
                 }
-                if (inputBreakDown[1].indexOf("/at") == 0) {
+                if (commandArgs[1].startsWith("/at")) {
                     throw new DukeException("The description of an event cannot be empty.");
                 }
-                if (!inputBreakDown[1].contains("/at") || inputBreakDown[1].matches(".*/at$")) {
+                if (!commandArgs[1].contains("/at") || commandArgs[1].endsWith("/at")) {
                     throw new DukeException("The time of an event cannot be empty.");
                 }
 
-                String[] eventBreakDown = inputBreakDown[1].split("\\s*/at\\s*", 2);
-                newTask = new Event(eventBreakDown[0], eventBreakDown[1]);
+                String[] eventArgs = commandArgs[1].split("\\s*/at\\s*", 2);
+                newTask = new Event(eventArgs[0], eventArgs[1]);
                 break;
             default:
                 throw new DukeException("I'm sorry, but I don't know what that means :-(");
@@ -110,7 +120,7 @@ public class Duke {
 
         list.add(newTask);
         System.out.println("Got it. I've added this task:");
-        System.out.println("  " + newTask);
+        System.out.printf("  %s\n", newTask);
         int total = list.size();
         System.out.printf("Now you have %d task%s in the list.\n", total, total > 1 ? "s" : "");
     }
@@ -120,12 +130,12 @@ public class Duke {
             Task task = list.get(taskId - 1);
             list.remove(task);
             System.out.println("Noted. I've removed this task:");
-            System.out.println("  " + task);
+            System.out.printf("  %s\n", task);
             int total = list.size();
             System.out.printf("Now you have %d task%s in the list.\n", total, total > 1 ? "s" : "");
         } catch (IndexOutOfBoundsException e) {
             throw new DukeException(String.format(
-                    "Task No.%d is not present in the list. Please enter a valid task ID.", taskId));
+                    "Task No.%d is not present in your list. Please enter a valid task ID.", taskId));
         }
     }
 }

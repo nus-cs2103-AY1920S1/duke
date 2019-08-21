4e35abd6caaad18541b5d0bdc699a047fe4c1a30
dalisc <42921826+dalisc@users.noreply.github.com>
2019-08-21 10:57:50 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,30 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String line = "________________________________________";
+        String indent = "    ";
         System.out.println("Hello from\n" + logo);
+        System.out.println(indent + line);
+        System.out.println(indent + "Hello! I'm Duke.\n" + indent + "What can I do for you today?");
+        System.out.println(indent + line);
+
+        Scanner sc = new Scanner(System.in);
+        boolean chat = true;
+        while(chat) {
+            String command = sc.next();
+            switch (command) {
+                case "bye":
+                    System.out.println(indent + line);
+                    System.out.println(indent + "Bye! Hope to see you again soon.");
+                    System.out.println(indent + line);
+                    chat = false;
+                    break;
+                default:
+                    System.out.println(indent + line);
+                    System.out.println(indent + command);
+                    System.out.println(indent + line);
+            }
+        }
+
     }
 }

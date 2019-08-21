bc44059d7e28ea55cedd113a044c13417fcfb0c0
alexwxh <e0273907@u.nus.edu>
2019-08-22 00:07:07 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
+import java.util.*;
 public class Duke {
+    private static void print(String message){
+        System.out.println(">>"+message);
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +10,16 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner sc= new Scanner(System.in);
+        while(sc.hasNext()){
+            String input=sc.next();
+            if(input.equals("bye")) {
+                break;
+            }else{
+                print(input);
+            }
+        }
+        String exitmsg="Bye. Hope to see you again.";
+        print(exitmsg);
     }
 }

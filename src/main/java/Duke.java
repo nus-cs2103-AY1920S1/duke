ed663603792a5106f9d0aa0abae8f0f70d8464dc
Murugesan Karthika <mdkkarthika@gmail.com>
2019-08-21 22:02:35 +0800

    Merge branch 'gradle'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,22 +14,32 @@ public class Duke {
         */
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        String[] arr = new String[100];
+
+        //Task t = new Task("read book");
+        Task[] arr = new Task[100];
         Scanner sc = new Scanner(System.in);
         int i = 0;
         while(sc.hasNext()) {
-            String input = sc.nextLine();
+            String input = sc.next();
             if(input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if(input.equals("list")){
+            } else if(input.equals("list")){ //go through the array
+                System.out.println("Here are the tasks in your list: ");
                 for(int j = 0; j < i; j++){
-                    System.out.println((j+1) + ". " + arr[j]);
+                    System.out.println((j+1) + "." + arr[j]);
                 }
+            } else if(input.equals("done")) {
+                int num = sc.nextInt();
+                System.out.println("Nice! I've marked this task as done: ");
+                arr[num-1].markAsDone();
+                System.out.println(arr[num]);
             } else {
-                arr[i] = input;
+                String activity = input + sc.nextLine();
+                Task t = new Task(activity);
+                arr[i] = t;
                 i++;
-                System.out.println("added: " + input);
+                System.out.println("added: " + activity);
             }
         }
     }

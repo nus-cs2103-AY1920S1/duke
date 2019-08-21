017c63d5d7edd8b123cf28da8c5116f2a0822208
charliechoong <e0202817@u.nus.edu>
2019-08-21 16:56:01 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,34 +3,39 @@ public class Duke {
     private static int numTasks = 0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] taskList = new String[100];
+        Task[] taskList = new Task[100];
 
-        printReply("Hello! I'm Duke\n\tWhat can I do for you?");
+        printReply("Hello! I'm Duke\n\t What can I do for you?");
         String command = sc.nextLine();
         while(!command.equals("bye")){
+            String[] parts = command.split(" ");
             if(command.equals("list"))
                 printList(taskList);
+            else if(parts[0].equals("done")){
+                int taskNum = Integer.valueOf(parts[1]);
+                printReply(taskList[taskNum-1].markAsDone());
+            }
             else
-                addToList(command, taskList);
-            command = sc.next();
+                addToList(new Task(command), taskList);
+            command = sc.nextLine();
         }
         printReply("Bye. Hope to see you again soon!");
     }
     public static void printReply(String reply){
-        System.out.println("\t________________________________________\n\t" + reply
+        System.out.println("\t________________________________________\n\t " + reply
                            + "\n\t________________________________________");
     }
-    public static void addToList(String task, String[] taskList){
+    public static void addToList(Task task, Task[] taskList){
         taskList[numTasks++] = task;
-        printReply("added: " + task);
+        printReply("added: " + task.description);
     }
-    public static void printList(String[] taskList){
-        String listReply = "";
-        for(int i=0; i<numTasks; i++){
-            listReply += (i+1) + ". " + taskList[i];
-            if(i != numTasks-1)
-                listReply += "\n\t";
-            printReply(listReply);
+    public static void printList(Task[] taskList){
+        String listReply = "Here are the tasks in your list:\n\t ";
+        for(int i=0; i<numTasks; i++) {
+            listReply += (i + 1) + ".[" + taskList[i].getStatusIcon() + "] " + taskList[i].description;
+            if (i != numTasks - 1)
+                listReply += "\n\t ";
         }
+        printReply(listReply);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,20 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description){
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon(){
+        return (isDone? "\u2713": "\u2718");
+    }
+
+    public String markAsDone(){
+        this.isDone = true;
+        String reply = "Nice! I've marked this task as done:\n\t ";
+        reply += "  [\u2713] " + this.description;
+        return reply;
+    }
+}

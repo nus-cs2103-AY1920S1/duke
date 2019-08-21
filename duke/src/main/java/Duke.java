20778fbe73bdd7a127a0b36f4088420610dfd1fa
Mandy Wang <shunflower320@gmail.com>
2019-08-21 11:28:43 +0800

    Level-3 complete

--- duke/src/main/java/Duke.java
+++ duke/src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
-	  ArrayList<String> list = new ArrayList<>();
+	  ArrayList<Task> list = new ArrayList<>();
       /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -11,18 +11,24 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 	*/
-		System.out.println("Hello! I'm Duke\n What can I do for you?");
+		System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
 		Scanner scanner = new Scanner(System.in);
 		String next = scanner.nextLine();
 
 		while (!next.equals("bye")) {
 		  if (next.equals("list")) {
+		  	System.out.println("Here are the tasks in your list:");
 			for (int i = 0; i < list.size(); i++) {
-			  System.out.println((i+1) + ". " + list.get(i));
+			  System.out.println((i+1) + "." + list.get(i).getStatus());
 			}
+		  } else if (next.contains("done")) {
+		  	int index = Integer.valueOf(next.substring(next.length()-1));
+		  	Task task = list.get(index-1);
+		  	task.markAsDone();
+		  	System.out.println("Nice! I've marked this task as done:\n  " + task.getStatus());
 		  } else {
-			list.add(next);
+			list.add(new Task(next));
 			System.out.println("added: " + next);
 		  }
 		  next = scanner.nextLine();
---
+++ duke/src/main/java/Task.java
@@ -0,0 +1,17 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public String getStatus() {
+        return (isDone ? "[\u2713] " + this.description : "[\u2718] " + this.description); //return tick or X symbols
+    }
+}

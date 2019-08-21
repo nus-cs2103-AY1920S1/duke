3c1a1e51254819e16c76a64be832d98aa80a7d8f
shyanjie <shyanjieteo@hotmail.com>
2019-08-21 17:14:52 +0800

    Implement todo, deadline and events functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    public static final String INDENT_COMMENT = "    ";
+    public static final String INDENT_TASK = "      ";
     public static List<Task> task = new ArrayList<>();
+    public static int itemNo = 0;
 
     public static void printLine() {
         System.out.println("   ____________________________________________________________");
@@ -17,57 +20,121 @@ public class Duke {
                 + "    | |_| | |_| |   <  __/\n"
                 + "    |____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo);
-        System.out.println("    Hello! I'm Duke");
-        System.out.println("    What can I do for you?");
+        System.out.println(INDENT_COMMENT + "Hello! I'm Duke");
+        System.out.println(INDENT_COMMENT + "What can I do for you?");
         printLine();
         System.out.println("");
     }
 
     public static void readInput() {
         Scanner input = new Scanner(System.in);
+
         while (input.hasNextLine()) {
             String line = input.nextLine();
 
             if (line.equals("bye")) {
                 printLine();
-                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println(INDENT_COMMENT + "Bye. Hope to see you again soon!");
                 printLine();
                 System.out.println("");
                 break;
             }
 
-            switch (line) {
+            String command = "";
+            String rest = "";
+
+            if (!line.contains(" ")) {
+                command = line;
+            } else {
+                String[] data = line.split(" ", 2);
+                command = data[0];
+                rest = data[1];
+            }
+
+            switch (command) {
                 case "list":
-                    int index = 1;
-                    printLine();
-                    System.out.println("    " + "Here are the tasks in your list:");
-                    for (int i = 0; i < task.size(); i++) {
-                        System.out.println("    " + index++ + "." + task.get(i));
-                    }
-                    printLine();
-                    System.out.println("");
+                    commandList();
                     break;
 
-                default:
-                    String[] data = line.split(" ");
-                    if (data[0].equals("done")) {
-                        printLine();
-                        System.out.println("    " + "Nice! I've marked this task as done:");
-                        int item = Integer.parseInt(data[1]);
-                        Task t = task.get(--item);
-                        t.markAsDone();
-                        System.out.println("      " + t);
-                        printLine();
-                        System.out.println("");
-                    } else {
-                        task.add(new Task(line));
-                        printLine();
-                        System.out.println("    " + "added: " + line);
-                        printLine();
-                        System.out.println("");
-                    }
+                case "done":
+                    commandDone(rest);
+                    break;
+
+                case "todo":
+                    commandTodo(rest);
+                    break;
+
+                case "deadline":
+                    commandDeadline(rest);
+                    break;
+
+                case "event":
+                    commandEvent(rest);
+                    break;
+
             }
         }
+    }
+
+    public static void commandList() {
+        int index = 1;
+        printLine();
+        System.out.println(INDENT_COMMENT + "Here are the tasks in your list:");
+        for (int i = 0; i < task.size(); i++) {
+            System.out.println("    " + index++ + "." + task.get(i));
+        }
+        printLine();
+        System.out.println("");
+    }
+
+    public static void commandDone(String data) {
+        printLine();
+        System.out.println(INDENT_COMMENT + "Nice! I've marked this task as done:");
+        int item = Integer.parseInt(data);
+        Task t = task.get(--item);
+        t.markAsDone();
+        System.out.println("      " + t);
+        printLine();
+        System.out.println("");
+    }
+
+    public static void commandTodo(String data) {
+        task.add(new Todo(data));
+        printLine();
+        System.out.println(INDENT_COMMENT + "Got it. I've added this task: ");
+        System.out.println(INDENT_TASK + task.get(itemNo));
+        itemNo++;
+        System.out.println(INDENT_COMMENT + "Now you have " + itemNo + " tasks in the list.");
+        printLine();
+        System.out.println("");
+    }
+
+    public static void commandDeadline(String data) {
+        String[] result = data.split("/by");
+        String achieve = result[0].trim();
+        String timeline = result[1].trim();
+        task.add(new Deadline(achieve, timeline));
+        printLine();
+        System.out.println(INDENT_COMMENT + "Got it. I've added this task: ");
+        System.out.println(INDENT_TASK + task.get(itemNo));
+        itemNo++;
+        System.out.println(INDENT_COMMENT + "Now you have " + itemNo + " tasks in the list.");
+        printLine();
+        System.out.println("");
+    }
+
+    public static void commandEvent(String data) {
+        String[] result = data.split("/at");
+        String achieve = result[0].trim();
+        String timeline = result[1].trim();
+        task.add(new Event(achieve, timeline));
+        printLine();
+        System.out.println(INDENT_COMMENT + "Got it. I've added this task: ");
+        System.out.println(INDENT_TASK + task.get(itemNo));
+        itemNo++;
+        System.out.println(INDENT_COMMENT + "Now you have " + itemNo + " tasks in the list.");
+        printLine();
+        System.out.println("");
     }
 
     public static void main(String[] args) {

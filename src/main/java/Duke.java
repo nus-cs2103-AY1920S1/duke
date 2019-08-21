53593a606182b3bcf3149bac3a7ef41ad2f9a23d
hellodommy <dominique@u.nus.edu>
2019-08-21 15:06:32 +0800

    Add Level-3 (Mark as Done)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.Arrays;
+import java.util.regex.Matcher;
 
 public class Duke {
 
-    public static ArrayList<String> myList = new ArrayList<>();
+    public static ArrayList<Task> myList = new ArrayList<>();
 
     public static void printWelcome() {
         System.out.println("    ____________________________________________________________");
@@ -27,11 +30,27 @@ public class Duke {
         System.out.println("");
     }
 
+    public static void printDone(String command) {
+        Pattern pattern = Pattern.compile("(\\d)");
+        Matcher matcher = pattern.matcher(command);
+        int currStep = 0;
+        while (matcher.find()) {
+            currStep = Integer.parseInt(matcher.group(1));
+        }
+        Task currTask = myList.get(currStep - 1);
+        currTask.markAsDone();
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Nice! I've marked this task as done:");
+        System.out.println("       [" + currTask.getStatusIcon() + "] " + currTask.description);
+        System.out.println("    ____________________________________________________________");
+    }
+
     public static void printList() {
         System.out.println("    ____________________________________________________________");
+        System.out.println("     Here are the tasks in your list:");
         int counter = 1;
-        for (String item : myList) {
-            System.out.println("     " + counter + ". " + item);
+        for (Task task : myList) {
+            System.out.println("     " + counter + ".[" + task.getStatusIcon() + "] " + task.description);
             counter++;
         }
         System.out.println("    ____________________________________________________________");
@@ -51,8 +70,11 @@ public class Duke {
                 System.exit(0);
             } else if (currLine.equals("list")) {
                 printList();
+            } else if (currLine.matches("done \\d")) {
+                printDone(currLine);
             } else {
-                myList.add(currLine);
+                Task currTask = new Task(currLine);
+                myList.add(currTask);
                 printAdded(currLine);
             }
         }

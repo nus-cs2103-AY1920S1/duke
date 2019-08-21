0a46ec93a3e83a452f2d59a869b84cec190b2ecd
madanalogy <Ahmed_Bahajjaj@u.nus.edu>
2019-08-21 10:29:13 +0800

    Leve-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+/**
+ *  Week 2 IP Deliverable
+ *
+ *  @author Ahmed Bahajjaj
+ */
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo);*/
+        Scanner sc = new Scanner(System.in);
+        boolean hi = true;
+        String input = "";
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        while(hi) {
+            input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                hi = false;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }

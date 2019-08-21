5d7213c5a438121c81c48f766af1eb441c44b863
dalisc <42921826+dalisc@users.noreply.github.com>
2019-08-22 00:05:29 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,7 +10,7 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String line = "________________________________________";
         String indent = "    ";
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
 
         System.out.println("Hello from\n" + logo);
         System.out.println(indent + line);
@@ -20,7 +20,10 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         boolean chat = true;
         while(chat) {
-            String command = sc.nextLine();
+            String input = sc.nextLine();
+            input = input.trim();
+            String[] inputArr = input.split("\\s+");
+            String command = inputArr[0];
             switch (command) {
                 case "bye":
                     System.out.println(indent + line);
@@ -32,15 +35,44 @@ public class Duke {
                     System.out.println(indent + line);
                     for (int i = 0; i < list.size(); i++) {
                         int k = i + 1;
-                        System.out.println(indent + k + ". " + list.get(i));
+                        Task task = list.get(i);
+                        System.out.println(indent + k + ".[" + task.getStatusIcon() + "] " + task.description);
                     }
                     System.out.println(indent + line);
                     break;
+                case "done":
+                    if (inputArr.length == 2) {
+                        try {
+                            int num = Integer.parseInt(inputArr[1]);
+                            Task task = list.get(num - 1);
+                            task.markAsDone();
+                            System.out.println(indent + line);
+                            System.out.println(indent + "Nice! I've marked this task as done:");
+                            System.out.println(indent + indent + "[" + task.getStatusIcon() + "] " + task.description);
+                            System.out.println(indent + line);
+                        } catch (IndexOutOfBoundsException e) {
+                            System.out.println(indent + line);
+                            System.out.println(indent + "Oops! This task number does not exist.");
+                            System.out.println(indent + line);
+                        } catch (NumberFormatException e) {
+                            System.out.println(indent + line);
+                            System.out.println(indent + "Oops! Please specify the task number that you want to mark as done.");
+                            System.out.println(indent + line);
+                        }
+                    } else {
+                        System.out.println(indent + line);
+                        System.out.println(indent + "Oops! Invalid command. Please specify only one task number that you want to mark as done.");
+                        System.out.println(indent + line);
+                    }
+                    break;
                 default:
-                    list.add(command);
-                    System.out.println(indent + line);
-                    System.out.println(indent + "added: " + command);
-                    System.out.println(indent + line);
+                    if (!input.equals("")) {
+                        Task task = new Task(input);
+                        list.add(task);
+                        System.out.println(indent + line);
+                        System.out.println(indent + "added: " + input);
+                        System.out.println(indent + line);
+                    }
             }
         }
 

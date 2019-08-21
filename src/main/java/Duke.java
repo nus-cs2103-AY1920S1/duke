3b4eace2fc49b182b1fc4e04d76a6f2b41e1b797
bjhoohaha <cvwo271828182845045>
2019-08-22 00:05:04 +0800

    Level 1
    -Implemented Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greet();
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        while(!input.equals("bye")) {
+            System.out.println("    ------------------------------------------------------------");
+            System.out.println("    " + input);
+            System.out.println("    ------------------------------------------------------------");
+            input = sc.next();
+        }
+        bye();
     }
+    private static void greet() {
+        System.out.println("    Hello! I'm Duke");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ------------------------------------------------------------");
+    }
+
+    private static void bye() {
+        System.out.println("    ------------------------------------------------------------");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ------------------------------------------------------------");
+    }
+
 }

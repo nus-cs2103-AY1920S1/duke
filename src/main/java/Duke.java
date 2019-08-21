73367a334eb43dbc4725b40e947a942e7f33e1bb
kelvinnharris <kelvinnharris@gmail.com>
2019-08-21 14:52:29 +0800

    Finish Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,23 +10,34 @@ public class Duke {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Task> list = new ArrayList<>();
+
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        Scanner sc = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>();
         String text = sc.nextLine();
+
         while (!text.equals("bye")) {
             if (text.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 IntStream.rangeClosed(1, list.size()).forEach(x -> {
-                    System.out.println(x + ". " + list.get(x - 1));
+                    Task task = list.get(x - 1);
+                    System.out.println(x + ".[" + task.getStatusIcon()  + "] " + task.getTaskName());
                 });
             } else {
-                System.out.println("added: " + text);
-                list.add(text);
+                String[] words = text.split(" ");
+                if (words[0].equals("done")) {
+                    list.get(Integer.parseInt(words[1]) - 1).setDone(true);
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  [\u2713] " + list.get(Integer.parseInt(words[1]) - 1).getTaskName());
+                } else {
+                    System.out.println("added: " + text);
+                    list.add(new Task(text));
+                }
             }
             text = sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
-
     }
 }

7ab3d7f7681889f6a5e93beda9d327baa218679d
ryantay232 <ryantay232@gmail.com>
2019-08-21 18:38:05 +0800

    Add Task.java. Modified Duke.java for Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> taskList = new ArrayList<String>();
+        ArrayList<Task> taskList = new ArrayList<Task>();
         String lines = "    ____________________________________________________________\n";
         String indent = "    ";
         String logo = lines
@@ -17,22 +17,31 @@ public class Duke {
                     + "    What can I do for you?   \n"
                     + lines;
         System.out.println(logo);
-        String comd = sc.nextLine();
-        while (!comd.equals("bye")) {
-            if (comd.isEmpty()) {}
-            else if (comd.equals("list")) {
+        String command = sc.nextLine();
+        String[] comd = command.split("\\s");
+        while (!comd[0].equals("bye")) {
+            if (comd[0].isEmpty()) {}
+            else if (comd[0].equals("list")) {
                 System.out.print(lines);
                 for (int i = 0; i < taskList.size(); i++) {
-                    System.out.println(indent + (i + 1) + ". " + taskList.get(i));
+                    System.out.println(indent + (i + 1) + "." + taskList.get(i));
                 }
                 System.out.println(lines);
-            } else {
-                taskList.add(comd);
+            } else if (comd[0].equals("done")) {
+                int n = Integer.parseInt(comd[1]) - 1;
+                taskList.get(n).markAsDone();
                 System.out.print(lines);
-                System.out.println(indent + "added: " + comd);
+                System.out.println(indent   + "Nice! I've marked this task as done: \n" +
+                                   indent   + "  " + taskList.get(n));
+                System.out.println(lines);
+            } else {
+                taskList.add(new Task(command));
+                System.out.print(lines);
+                System.out.println(indent + "added: " + command);
                 System.out.println(lines);
             }
-            comd = sc.nextLine();
+            command = sc.nextLine();
+            comd = command.split("\\s");
         }
         System.out.println(lines + indent + "Bye. Hope to see you again soon!\n" + lines);
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        isDone = false;
+    }
+
+    public String getStatusIcon() {
+        //return (isDone ? "Y" : "N");
+        return (isDone ? "\u2713" : "\u2718");
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}

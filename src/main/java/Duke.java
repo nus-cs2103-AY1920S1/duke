21b30d1d809e48673d0464bbd8a52e9bbc3f021c
yeodonghan <yeodonghan@gmail.com>
2019-08-22 05:05:48 +0800

    Revert "Level-6"
    This reverts commit 7b939e42c1ecca60cce3b304b993506e3222175f.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,7 +10,7 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         */
         Scanner sc = new Scanner(System.in);
-        List<Task> list = new ArrayList<>();
+        Task[] list = new Task[100];
         int pos = 0;
 
         System.out.println("Hello! I'm Duke");
@@ -22,9 +22,9 @@ public class Duke {
                 int number = sc.nextInt();
                 int pos1 = number - 1;
                 String dummy1 = sc.nextLine();
-                list.get(pos1).markAsDone();
+                list[pos1].markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println(list.get(pos1));
+                System.out.println(list[pos1]);
             } else if (sc.hasNext("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 return;
@@ -36,7 +36,7 @@ public class Duke {
                         throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
                     }
                     Task curr = new Todo(description);
-                    list.add(curr);
+                    list[pos] = curr;
                     pos++;
                     System.out.println("Got it. I've added this task:");
                     System.out.println(curr);
@@ -49,7 +49,7 @@ public class Duke {
                 String[] split = new String[2];
                 split = sc.nextLine().split("/by");
                 Task curr = new Deadline(split[0], split[1]);
-                list.add(curr);
+                list[pos] = curr;
                 pos++;
                 System.out.println("Got it. I've added this task:");
                 System.out.println(curr);
@@ -59,20 +59,10 @@ public class Duke {
                 String[] split = new String[2];
                 split = sc.nextLine().split("/at");
                 Task curr = new Event(split[0], split[1]);
-                list.add(curr);
+                list[pos] = curr;
                 pos++;
                 System.out.println("Got it. I've added this task:");
                 System.out.println(curr);
-                System.out.println("Now you have " + pos + " tasks in the list.");
-            } else if (sc.hasNext("delete")){
-                String dummy = sc.next();
-                int line = sc.nextInt();
-                String dummy1 = sc.nextLine();
-                Task curr = list.get(line - 1);
-                System.out.println("Noted. I've removed this task:");
-                System.out.println(curr);
-                list.remove(line - 1);
-                pos--;
                 System.out.println("Now you have " + pos + " tasks in the list.");
             } else {
                 try {
@@ -80,11 +70,10 @@ public class Duke {
                     if (!input.equals("list")) {
                         throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                     }
-                    System.out.println("Here are the tasks in your list:");
                     int k = 0;
                     while (k != pos) {
                         int bullet = k + 1;
-                        System.out.println(bullet + "." + list.get(k));
+                        System.out.println(bullet + "." + list[k]);
                         k++;
                     }
                 } catch (DukeException e) {

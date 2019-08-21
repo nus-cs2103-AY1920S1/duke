55749eb31be168955f733482aebf6801416c30d1
xinmingzh <xinming.zh@gmail.com>
2019-08-21 17:51:21 +0800

    A-TextUiTesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,32 +3,41 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    String indent = "     ";
+
     public static void main(String[] args) {
-        String desc, input, by, duration, indent = "    ";
-        String[] inputs, inputFormatted;
+        Duke newDuke = new Duke();
+        newDuke.run();
+    }
+
+    public void run() {
+        String desc;
+        String input;
+        String by;
+        String duration;
+        String[] inputs;
+        String[] inputFormatted;
         Task task;
         Scanner sc = new Scanner(System.in);
         List<Task> list = new ArrayList<>();
 
-
-        System.out.println(indent + "____________________________________________________________");
-        System.out.println(indent + " Hello! I'm Duke");
-        System.out.println(indent + " What can I do for you?");
-        System.out.println(indent + "____________________________________________________________\n");
+        printIntro();
 
         while (true) {
             input = sc.nextLine();
             inputs = input.split(" ", 2);
-            System.out.println(indent + "____________________________________________________________");
+            UILine();
             // Exit program
             if (inputs[0].equals("bye")) {
-                System.out.println(indent + " Bye. Hope to see you again soon!");
+                System.out.println(indent + "Bye. Hope to see you again soon!");
                 break;
             }
             // Show list of events
             else if (inputs[0].equals("list")) {
-                System.out.println(indent + " Here are the tasks in your list:");
-                for (Task t: list) System.out.println(indent + " " + (list.indexOf(t) + 1) + ". " + t);
+                System.out.println(indent + "Here are the tasks in your list:");
+                for (Task t: list) {
+                    System.out.println(indent + (list.indexOf(t) + 1) + ". " + t);
+                }
             }
             // Mark as Done
             else if (inputs[0].equals("done")) {
@@ -36,13 +45,15 @@ public class Duke {
                 try {
                     task = list.get(i - 1);
                     task.setDone();
-                    System.out.println(indent + " Nice! I've marked this task as done: ");
+                    System.out.println(indent + "Nice! I've marked this task as done: ");
                     System.out.println(indent + "   " + task);
                 } catch (IndexOutOfBoundsException e) {
-                    System.out.println(indent + " Invalid index, please try again");
+                    System.out.println(indent + "Invalid index, please try again");
                 }
             }
-            else if (inputs[0].equals("todo") || inputs[0].equals("deadline") || inputs[0].equals("event")) {
+            else if (inputs[0].equals("todo")
+                    || inputs[0].equals("deadline")
+                    || inputs[0].equals("event")) {
                 try {
                     if (inputs[0].equals("todo")) {
                         task = new Todo(inputs[1]);
@@ -56,15 +67,26 @@ public class Duke {
                         task = new Event(inputFormatted[0], inputFormatted[1]);
                     }
                     list.add(task);
-                    System.out.println(indent + " Got it. I've added this task:");
+                    System.out.println(indent + "Got it. I've added this task:");
                     System.out.println(indent + "   " + task);
-                    System.out.println(indent + " Now you have " + list.size() + " tasks in the list.");
+                    System.out.println(indent + "Now you have " + list.size() + "tasks in the list.");
                 } catch (Exception e) {
-                    System.out.println(indent + " Please enter date info using '/by' for deadline and '/at' for event");
+                    System.out.println(indent + "Please enter date info using '/by' for deadline and '/at' for event");
                 }
             }
-
-            System.out.println(indent + "____________________________________________________________\n");
+            UILine();
+            System.out.println();
         }
+    }
+
+    private void printIntro() {
+        UILine();
+        System.out.println(indent + "Hello! I'm Duke");
+        System.out.println(indent + "What can I do for you?");
+        UILine();
+        System.out.println();
+    }
+    private void UILine() {
+        System.out.println("    ____________________________________________________________");
     }
 }

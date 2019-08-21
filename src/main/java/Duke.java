3bfde8c304e9c8127cc7fe69d3ea2c7da50a7fa9
dalisc <42921826+dalisc@users.noreply.github.com>
2019-08-22 00:59:09 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,21 +24,22 @@ public class Duke {
             input = input.trim();
             String[] inputArr = input.split("\\s+");
             String command = inputArr[0];
+
+            System.out.println(indent + line);
             switch (command) {
                 case "bye":
-                    System.out.println(indent + line);
                     System.out.println(indent + "Bye! Hope to see you again soon.");
-                    System.out.println(indent + line);
                     chat = false;
                     break;
                 case "list":
-                    System.out.println(indent + line);
+                    if (list.size() == 0) {
+                        System.out.println(indent + "You have not added any tasks to the list!");
+                    }
                     for (int i = 0; i < list.size(); i++) {
                         int k = i + 1;
                         Task task = list.get(i);
-                        System.out.println(indent + k + ".[" + task.getStatusIcon() + "] " + task.description);
+                        System.out.println(indent + k + ". " + task);
                     }
-                    System.out.println(indent + line);
                     break;
                 case "done":
                     if (inputArr.length == 2) {
@@ -46,35 +47,58 @@ public class Duke {
                             int num = Integer.parseInt(inputArr[1]);
                             Task task = list.get(num - 1);
                             task.markAsDone();
-                            System.out.println(indent + line);
                             System.out.println(indent + "Nice! I've marked this task as done:");
-                            System.out.println(indent + indent + "[" + task.getStatusIcon() + "] " + task.description);
-                            System.out.println(indent + line);
+                            System.out.println(indent + indent + task);
                         } catch (IndexOutOfBoundsException e) {
-                            System.out.println(indent + line);
                             System.out.println(indent + "Oops! This task number does not exist.");
-                            System.out.println(indent + line);
                         } catch (NumberFormatException e) {
-                            System.out.println(indent + line);
                             System.out.println(indent + "Oops! Please specify the task number that you want to mark as done.");
-                            System.out.println(indent + line);
                         }
                     } else {
-                        System.out.println(indent + line);
                         System.out.println(indent + "Oops! Invalid command. Please specify only one task number that you want to mark as done.");
-                        System.out.println(indent + line);
+                    }
+                    break;
+                case "todo":
+                    Task task = new ToDo(input);
+                    list.add(task);
+                    printAddedTask(task, list);
+                    break;
+                case "event":
+                    try {
+                        String[] eventCommand = input.split("/");
+                        String eventDescript = eventCommand[0].substring(6);
+                        String at = eventCommand[1].substring(3);
+                        Task eventTask = new Event(eventDescript, at);
+                        list.add(eventTask);
+                        printAddedTask(eventTask, list);
+                    } catch (Exception e) {
+                        System.out.println(indent + "Oops! You did not specify when the event is.");
+                    }
+                    break;
+                case "deadline":
+                    try {
+                        String[] deadlineCommand = input.split("/");
+                        String deadlineDescript = deadlineCommand[0].substring(9);
+                        String by = deadlineCommand[1].substring(3);
+                        Task deadlineTask = new Deadline(deadlineDescript, by);
+                        list.add(deadlineTask);
+                        printAddedTask(deadlineTask, list);
+                    } catch (Exception e) {
+                        System.out.println(indent + "Oops! You did not specify when the deadline is.");
                     }
                     break;
                 default:
-                    if (!input.equals("")) {
-                        Task task = new Task(input);
-                        list.add(task);
-                        System.out.println(indent + line);
-                        System.out.println(indent + "added: " + input);
-                        System.out.println(indent + line);
-                    }
+                    System.out.println(indent + "Sorry! I don't know what that means.");
             }
+            System.out.println(indent + line);
         }
 
+    }
+
+    public static void printAddedTask(Task task, ArrayList list) {
+        String indent = "    ";
+        System.out.println(indent + "Got it. I've added this task:");
+        System.out.println(indent + indent + task);
+        System.out.println(indent + "Now you have " + list.size() + " task(s) in the list.");
     }
 }

2a2f0ca83b351f698bd25342c44bf38db6080810
Wang Xueting <wang.xueting@u.nus.edu>
2019-08-21 17:17:45 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    static String[] taskList = new String[100];
+    static int number = 0;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -14,20 +18,29 @@ public class Duke {
         System.out.println("    ____________________________________________________________");
 
         Scanner sc = new Scanner(System.in);
-        echo(sc);
+        addList(sc);
     }
 
-    public static void echo(Scanner sc) {
+    public static void addList(Scanner sc) {
         while(sc.hasNext()) {
-            String command = sc.next();
+            String command = sc.nextLine();
             if (command.equals("bye")) {
                 System.out.println("    ____________________________________________________________");
                 System.out.println("     Bye. Hope to see you again soon!");
                 System.out.println("    ____________________________________________________________");
                 return;
-            } else {
+            } else if (command.equals("list")) {
                 System.out.println("    ____________________________________________________________");
-                System.out.println("     " + command);
+                for (int i = 0; i < number; i++) {
+                    int index = i + 1;
+                    System.out.println("     " + index + ". " + taskList[i]);
+                }
+                System.out.println("    ____________________________________________________________");
+            } else {
+                taskList[number] = command;
+                number++;
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     added: " + command);
                 System.out.println("    ____________________________________________________________");
             }
         }

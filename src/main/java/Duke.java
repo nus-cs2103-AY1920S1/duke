2bc47375929b13e2da661d9466d39d4a00ba2cfa
jakq <42934003+jakq@users.noreply.github.com>
2019-08-21 20:30:07 +0800

    Level-3 : Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -68,34 +68,3 @@ public class Duke {
                 + "  " + t;
     }
 }
-
-class Task {
-    protected static int total;
-    protected int idx;
-    protected String description;
-    protected boolean isDone;
-
-    public Task(String description) {
-        total++;
-        this.idx = total;
-        this.description = description;
-        this.isDone = false;
-    }
-
-    public int getIdx() {
-        return this.idx;
-    }
-
-    public void setDone() {
-        this.isDone = true;
-    }
-
-    public String getStatusIcon() {
-        return (isDone ? "[\u2713]" : "[\u2718]"); //return tick or X symbols
-    }
-
-    @Override
-    public String toString() {
-        return getStatusIcon() + " " + this.description;
-    }
-}

94d42d7aff957111d58c365e9976c76b983cbbf0
TheRealRavager <iskandar@u.nus.edu>
2019-08-21 19:41:29 +0800

    Add status (done/not done) to tasks and method to mark a task as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    private static ArrayList<String> storedInputs = new ArrayList<>(100);
+    private static ArrayList<Task> tasks = new ArrayList<>(100);
     public static void main(String[] args) {
         greet();
         respondToInput();
@@ -15,23 +15,26 @@ public class Duke {
     private static void respondToInput() {
         Scanner sc = new Scanner(System.in);
         String userInput = sc.nextLine();
+        // May require refactor if performance is undesirably poor due to many string concatenations (review at towards end of project)
+        // Should refactor by extract method
         while (!userInput.toLowerCase().equals("bye")) {
             if (userInput.equals("list")) {
-                int n = 1;
                 String finalOutput = "";
                 boolean first = true;
-                for (String storedInput : storedInputs) {
-                    // May require refactor if performance is undesirably poor due to many string concatenations (review at towards end of project)
+                for (int i = 0; i < tasks.size(); i++) {
                     if (!first) {
                         finalOutput += "\n";
                     }
                     first = false;
-                    finalOutput += n + ". " + storedInput;
-                    n += 1;
+                    finalOutput += i + 1 + ". " + tasks.get(i).getInfo();
                 }
-                dukeReply(finalOutput);
+                dukeReply("Here are the tasks in your list:\n" + finalOutput);
+            } else if (userInput.length() > 4 && userInput.substring(0, 5).equals("done ")) {
+                Task task = tasks.get(Integer.parseInt(userInput.substring(5)) - 1);
+                task.markAsDone();
+                dukeReply("Successfully marked the following task as done:\n" + task.getInfo());
             } else {
-                storedInputs.add(userInput);
+                tasks.add(new Task(userInput));
                 dukeReply("added: " + userInput);
             }
             userInput = sc.nextLine();

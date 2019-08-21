7d4249499d5a7569ed7a84443772e0d0def8e16e
zephyr <shejiayu1998@qq.com>
2019-08-22 01:43:34 +0800

    Implement Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello! I'm \n" + logo + "What can I do for you?\n");
+
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if (input.strip().toLowerCase().equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!\n");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+
+        sc.close();
     }
 }

86a547a7c78814dfd067029a0ca13585e46f5bfd
dorcastan <dorcas.tann@gmail.com>
2019-08-21 13:37:37 +0800

    Fix: Change indentation for Duke's messages
    Increases indentation for Duke's output text from 4 to 5 spaces to
    match project specification.

--- src/main/java/DukeFormatter.java
+++ src/main/java/DukeFormatter.java
@@ -1,48 +1,53 @@
 import java.util.List;
 
 class DukeFormatter {
-    /**
-     * Horizontal line of width 60 characters, for demarcating Duke's responses
-     */
-    protected static final String horizontalLine =
+    private static final String horizontalLine =
             "____________________________________________________________";
 
-    /**
-     * 4 space indentation for pretty-printing Duke's responses
-     */
-    protected static final String indent = "    ";
+    private static final String indent = "    ";
 
+    /**
+     * Prints a horizontal line of width 60 characters.
+     */
+    private static void printBorder() {
+        System.out.println(indent + horizontalLine);
+    }
+
+    /**
+     * Prints the given text with indentation of five spaces.
+     * @param text
+     */
     private static void print(String text) {
-        System.out.println(indent + text);
+        System.out.println(indent + " " + text);
     }
 
     /**
      * Prints the given text with a horizontal line above and below the text
-     * and four spaces of indentation throughout.
+     * and appropriate indentation.
      * @param text      Formatted text with appropriate line breaks. Maximum
      *                  width for each line is 58 characters.
      */
     static void prettyPrint(String text) {
         String[] lines = text.split("\n");
-        print(horizontalLine);
+        printBorder();
         for (String line : lines) {
             print(line);
         }
-        print(horizontalLine);
+        printBorder();
     }
 
     /**
      * Prints the items in the given list with their corresponding index
      * numbers. A horizontal line is printed above and below the list, and
-     * output is indented by four spaces throughout.
+     * output is indented throughout.
      * @param list      A List of Tasks to be printed.
      */
-    // TODO: Wrap text for longer user input
+     // TODO: Wrap text for longer user input
      static void prettyPrint(List<Task> list) {
-        print(horizontalLine);
+        printBorder();
         for (int i = 1; i <= list.size(); i++) {
             print(i + "." + list.get(i - 1).toString());
         }
-        print(horizontalLine);
-    }
+        printBorder();
+     }
 }

e4c2319e147f6fbed14903f1a1bc0b6ed3794f40
nishcafe <46063774+nishcafe@users.noreply.github.com>
2019-08-21 19:11:47 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Task> commands = new ArrayList<Task>();
+        ArrayList<String> commands = new ArrayList<String>();
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         while(scanner.hasNextLine()) {
             String input = scanner.nextLine();
@@ -12,15 +12,13 @@ public class Duke {
                 scanner.close();
                 break;
             } else if(input.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
                 int count = 1;
-                for (Task s : commands) {
+                for (String s : commands) {
                     System.out.println(count + ". " + s);
                     count++;
                 }
             } else {
-                Task t = new Task(input);
-                commands.add(t);
+                commands.add(input);
                 System.out.println("added: " + input);
             }
         }

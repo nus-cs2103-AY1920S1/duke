9c76c9ea743130d359ad08f76527830ecc7ad612
nishcafe <46063774+nishcafe@users.noreply.github.com>
2019-08-21 20:27:25 +0800

    Level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,27 +7,48 @@ public class Duke {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         while(scanner.hasNextLine()) {
             String input = scanner.nextLine();
-            if (input.equals("bye")) {
+            String[] inputArr = input.split(" ");
+            String command = inputArr[0];
+            if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 scanner.close();
                 break;
-            } else if(input.equals("list")) {
+            } else if(command.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
                 int count = 1;
                 for (Task s : commands) {
                     System.out.println(count + ". " + s);
                     count++;
                 }
-            } else if (input.contains("done")) {
-                String[] inArr = input.split(" ");
-                int index = Integer.parseInt(inArr[1]) - 1;
+            } else if (command.equals("done")) {
+                int index = Integer.parseInt(inputArr[1]) - 1;
                 Task doneTask = commands.get(index);
                 doneTask.markAsDone();
                 System.out.println("Nice! I've marked this task as done:\n  " + doneTask);
+            } else if (command.equals("deadline")) {
+                String dL = input.split(" ", 2)[1];
+                String[] taskDeadLine = dL.split(" /by ");
+                String taskD = taskDeadLine[0];
+                String by = taskDeadLine[1];
+                Task tt = new Deadline(taskD, by);
+                commands.add(tt);
+                System.out.println("Got it. I've added this task:\n  " + tt
+                                   + "\nNow you have " + commands.size() + " tasks in the list.");
+            } else if(command.equals("event")) {
+                String eEvent = input.split(" ", 2)[1];
+                String[] taskEvent = eEvent.split(" /at ");
+                String taskE = taskEvent[0];
+                String at = taskEvent[1];
+                Task ee = new Event(taskE, at);
+                commands.add(ee);
+                System.out.println("Got it. I've added this task:\n  " + ee
+                                   + "\nNow you have " + commands.size() + " tasks in the list.");
             } else {
-                Task t = new Task(input);
+                String todoT = input.split(" ", 2)[1];
+                Task t = new Todo(todoT);
                 commands.add(t);
-                System.out.println("added: " + input);
+                System.out.println("Got it. I've added this task:\n  " + t
+                        + "\nNow you have " + commands.size() + " tasks in the list.");
             }
         }
     }

1f4003b34f26a5abf79baf1d15900e6809340f6e
Isaac Quek <isaacquek97@gmail.com>
2019-08-22 04:18:14 +0800

    Completed Lv3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,46 +4,56 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Duke {
+
+    public static List<Task> lst = new ArrayList<>();
+
     public static void main(String[] args) {
         List<String> start = new ArrayList<>();
-        List<String> lst = new ArrayList<>();
+
 
         start.add("Hello! I'm Duke");
         start.add("What can I do for you?");
-    	printStart(start);
+    	printInput(start);
 
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
 
         	String input = sc.nextLine();
-
-            switch (input) {
-                case ("bye"):
-                    printEnd("Bye. Hope to see you again soon!");
-                    break;
-
-                case ("list"):
-                    printNumberList(lst);
-                    break;
-
-                default:
-                    printInput(input);
-                    lst.add(input);
+            String[] tokens = input.split(" ");
+            if (tokens[0].equals("bye")) {
+                printEnd("Bye. Hope to see you again soon!");
+                break;
+            } else if (tokens[0].equals("list")) {
+                printNumberList(lst);
+            } else if (tokens[0].equals("done")) {
+                doTask(tokens[1]);
+//                char rawPos = input.charAt(input.length()-1);
+//                int pos = Integer.parseInt(Character.toString(rawPos)) - 1;
+//                Task task = lst.get(pos);
+//                task.doTask();
+//                List<String> inst = List.of("Nice! I've marked this task as done: ",
+//                        "  "+task.toString());
+//                //System.out.println("Nice! I've marked this task as done: ");
+//                //System.out.println(this);
+            } else {
+                Task task = new Task(input);
+                printInput(task);
+                lst.add(task);
             }
 
         }
 
     }
 
-    public static void printInput(String input) {
+    public static void printInput(Task input) {
     	System.out.println("    ____________________________________________________________");
-    	System.out.println(String.format("     added: %s",input));
+    	System.out.println(String.format("     added:%s",input));
     	System.out.println("    ____________________________________________________________");
     	System.out.println();
 
     }
 
-    public static void printStart(List<String> start) {
+    public static void printInput(List<String> start) {
         System.out.println("    ____________________________________________________________");
         for (String input : start) {
             System.out.println(String.format("     %s",input));
@@ -62,15 +72,32 @@ public class Duke {
     }
 
 
-    public static void printNumberList(List<String> lst) {
+    public static void printNumberList(List<Task> lst) {
         System.out.println("    ____________________________________________________________");
+        System.out.println("     Here are the tasks in your list:");
         for (int i = 0; i < lst.size(); i++) {
-            System.out.println(String.format("     %d. %s",i+1,lst.get(i)));
+            System.out.println(String.format("     %d.%s",i+1,lst.get(i)));
         }
 
         System.out.println("    ____________________________________________________________");
         System.out.println();
     }
+
+    public static void doTask(String str) {
+        //char rawPos = input.charAt(input.length()-1);
+        //int pos = Integer.parseInt(Character.toString(rawPos)) - 1;
+        int pos = Integer.parseInt(str)-1;
+        Task task = lst.get(pos);
+        task.doTask();
+        List<String> inst = List.of("Nice! I've marked this task as done: ",
+                "  "+task.toString());
+        printInput(inst);
+        //System.out.println("Nice! I've marked this task as done: ");
+        //System.out.println(this);
+    }
+
+
+
 
 
    
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "✓" : "✗" );
+        //return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public String toString() {
+        return String.format("[%s] %s", getStatusIcon(), getDescription());
+    }
+
+    public void doTask() {
+        this.isDone = true;
+    }
+}

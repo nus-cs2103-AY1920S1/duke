4fea6fe760a88a976796e328b3cbf98bfa39761b
Isaac Quek <isaacquek97@gmail.com>
2019-08-22 01:26:07 +0800

    Level 1 Completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,48 @@
+import java.util.Scanner;
+import java.lang.StringBuilder;
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        List<String> lst = new ArrayList<>();
+        lst.add("Hello! I'm Duke");
+        lst.add("What can I do for you?");
+    	printInput(lst);
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+
+        	String input = sc.next();
+
+        	if (input.equals("bye")) {
+        		printInput("Bye. Hope to see you again soon!");
+        		break;
+        	} else {
+        		printInput(input);
+        	}
+
+        }
+
     }
+
+    public static void printInput(String input) {
+    	System.out.println("    ____________________________________________________________");
+    	System.out.println(String.format("     %s",input));
+    	System.out.println("    ____________________________________________________________");
+    	System.out.println();
+
+    }
+
+    public static void printInput(List<String> lst) {
+        System.out.println("    ____________________________________________________________");
+        for (String input : lst) {
+            System.out.println(String.format("     %s",input));
+        }
+
+  
+        System.out.println("    ____________________________________________________________");
+        System.out.println();
+    } 
 }

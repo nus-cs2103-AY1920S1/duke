600aec0b3c1a968280bdb953711b00b4d668b80e
Chaitanya Baranwal <chaitanyabaranwal@gmail.com>
2019-08-21 17:18:40 +0800

    Add level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,12 +11,17 @@ public class Duke {
         System.out.println(" Hello! I'm Duke :)\n What can I do for you?");
         System.out.println("    --------------------------------------------------------");
 
+        // initialize a TaskList
+        TaskList tl = new TaskList();
+
         // Run till user types bye
         String input = sc.nextLine();
         while (!input.equals("bye")) {
-            System.out.println("    --------------------------------------------------------");
-            System.out.println("     " + input);
-            System.out.println("    --------------------------------------------------------");
+            if (input.equals("list")) {
+                tl.listTasks();
+            } else {
+                tl.addTask(input);
+            }
             input = sc.nextLine();
         }
 
@@ -23,5 +29,31 @@ public class Duke {
         System.out.println("    --------------------------------------------------------");
         System.out.println("     Bye. Hope to see you again soon!");
         System.out.println("    --------------------------------------------------------");
+    }
+}
+
+class TaskList {
+    // ArrayList to maintain list of tasks
+    ArrayList<String> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<String>();
+    }
+
+    // add tasks
+    public void addTask(String task) {
+        tasks.add(task);
+        System.out.println("    --------------------------------------------------");
+        System.out.println("     " + "added: " + task);
+        System.out.println("    --------------------------------------------------");
+    }
+
+    // list tasks
+    public void listTasks() {
+        System.out.println("    --------------------------------------------------");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println("     " + Integer.toString(i+1) + ". " + tasks.get(i));
+        }
+        System.out.println("    --------------------------------------------------");
     }
 }

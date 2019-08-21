16ebd0b4516c2f05f941248bf73633bc49375f56
MichaelaTSH <michaelatang99@gmail.com>
2019-08-21 11:43:05 +0800

    Add list, adding to list, listing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    static String[] array;
+    static int count;
     static String line = "____________________________________________________________";
 
     public static void main(String[] args) {
         intro();
+        array = new String[100];
+        count = 0;
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
             String s = sc.nextLine();
@@ -12,7 +17,7 @@ public class Duke {
                 System.out.println(line + "\n Bye. Hope to see you again soon!\n" + line);
                 break;
             }
-            respond(s);
+            addTask(s);
         }
     }
 
@@ -22,10 +27,20 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(line + "\n" + logo + " Hello! I'm Duke\n What can I do for you?\n" + line);
+        System.out.println(line + "\n" + logo + "\n Hello! I'm Duke\n What can I do for you?\n" + line);
     }
 
-    public static void respond(String s) {
-        System.out.println(line + "\n " + s + "\n" + line);
+    public static void addTask(String s) {
+        if (s.toLowerCase().equals("list")) {
+            System.out.println(line);
+            for (int i = 1; i <= count; i++) {
+                System.out.println(" " + i + ". " + array[i - 1]);
+            }
+            System.out.println(line);
+        } else {
+            array[count] = s;
+            count += 1;
+            System.out.println(line + "\n added: " + s + "\n" + line);
+        }
     }
 }

5e4aaff0113dce1a21367f6c9ff6692c1a9284b4
Lee Ee Jian <e0309900@u.nus.edu>
2019-08-21 12:54:39 +0800

    Level 5, added try catch blocks to identify InputMismatchException

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -42,6 +42,13 @@ public class Duke {
                     Task taskToMarkAsDone = duke.tasks.get(Integer.parseInt(cmdList[1]) - 1);
                     taskToMarkAsDone.markAsDone();
                     System.out.println(taskToMarkAsDone);
+                } else if (keyword.equals("delete")) {
+                    Task taskToBeRemoved = duke.tasks.get(Integer.parseInt(cmdList[1]) - 1);
+                    duke.tasks.remove(taskToBeRemoved);
+                    System.out.println("Noted. I've removed this task: ");
+                    System.out.print("  ");
+                    System.out.println(taskToBeRemoved);
+                    System.out.println("Now you have " + duke.tasks.size() + " in the list.");
                 } else { // it is a new Task
                     Task newTaskToBeAdded = duke.handleNewTask(keyword, cmd);
                     duke.tasks.add(newTaskToBeAdded);
@@ -51,7 +58,7 @@ public class Duke {
                     System.out.println("Now you have " + duke.tasks.size() + " tasks in the list.");
                 }
 
-            } catch (Exception e) {
+            } catch (InputMismatchException e) {
                 System.out.println("☹ OOPS!!! " + e.getMessage());
             }
             printLine();
@@ -82,17 +89,26 @@ public class Duke {
 
     private Task handleNewTask(String keyword, String cmd) throws InputMismatchException {
         if (keyword.equals("deadline")) {
-            String descriptionAndTime = cmd.substring(10);
-            String[] details = descriptionAndTime.split(" /by");
+            String descriptionAndTime = cmd.substring(8);
+            String[] details = descriptionAndTime.trim().split(" /by");
+            if (descriptionAndTime.isEmpty() || details.length <= 1) {
+                throw new InputMismatchException("The description of a deadline cannot be empty.");
+            }
             return new Deadline(details[0], details[1]);
 
         } else if (keyword.equals("event")) {
-            String descriptionAndTime = cmd.substring(6);
-            String[] details = descriptionAndTime.split(" /at");
+            String descriptionAndTime = cmd.substring(5);
+            String[] details = descriptionAndTime.trim().split(" /at");
+            if (descriptionAndTime.isEmpty() || details.length <= 1) {
+                throw new InputMismatchException("The description of an event cannot be empty.");
+            }
             return new Event(details[0], details[1]);
 
-        } else if(keyword.equals("Todo")){
-            String description = cmd.substring(5); //words after todo
+        } else if (keyword.equals("todo")) {
+            String description = cmd.substring(4); //words after todo
+            if (description.isEmpty()) {
+                throw new InputMismatchException("The description of a todo cannot be empty.");
+            }
             return new Todo(description);
         } else {
             throw new InputMismatchException("I'm sorry, but I don't know what that means :-(");

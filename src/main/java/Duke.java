979a4882c6fbcf64866fa022e55af27aac8c4fa1
ryantay232 <ryantay232@gmail.com>
2019-08-21 17:04:49 +0800

    Modified Duke.java for Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> taskList = new ArrayList<String>();
         String lines = "    ____________________________________________________________\n";
         String indent = "    ";
         String logo = lines
@@ -14,11 +16,23 @@ public class Duke {
                     + "    Hello! I'm Duke          \n"
                     + "    What can I do for you?   \n"
                     + lines;
-        System.out.print(logo);
-        String comd = sc.next();
+        System.out.println(logo);
+        String comd = sc.nextLine();
         while (!comd.equals("bye")) {
-            System.out.println(lines + indent+ comd + "\n" + lines);
-            comd = sc.next();
+            if (comd.isEmpty()) {}
+            else if (comd.equals("list")) {
+                System.out.print(lines);
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println(indent + (i + 1) + ". " + taskList.get(i));
+                }
+                System.out.println(lines);
+            } else {
+                taskList.add(comd);
+                System.out.print(lines);
+                System.out.println(indent + "added: " + comd);
+                System.out.println(lines);
+            }
+            comd = sc.nextLine();
         }
         System.out.println(lines + indent + "Bye. Hope to see you again soon!\n" + lines);
     }

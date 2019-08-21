202767f432648f0561b73564f3e02e51ce6c9a57
choongyx <yongxin.choong98@gmail.com>
2019-08-21 22:00:16 +0800

    Handle empty and invalid inputs for done, todo, deadline and event. DukeException class created.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,43 +15,110 @@ public class Duke {
         Task[] taskList = new Task[100];
         int numCommands = 0;
 
-        while (!command.equals("bye")){
-            if (command.equals("list")){
+        while (!command.equals("bye")) {
+            //List tasks
+            if (command.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
-                for (int i = 1; i <= numCommands; i++){
-                    System.out.println(i + "."+ taskList[i-1]);
+                for (int i = 1; i <= numCommands; i++) {
+                    System.out.println(i + "." + taskList[i - 1]);
                 }
             }
-            else if (command.length() >= 4 && command.substring(0,4).equals("done")){
-                int taskNumber = Integer.parseInt(command.substring(5).split(" ")[0]);
-                if (taskNumber > 0 && taskNumber <= numCommands) {
-                    taskList[taskNumber-1].markAsDone();
-                    System.out.println("Nice! I've marked this task as done: ");
-                    System.out.println(taskList[taskNumber-1]);
+            //Mark tasks as done
+            else if (command.length() >= 4 && command.substring(0, 4).equals("done")) {
+                try {
+                    try {
+                        //Error if user inputs spaces
+                        if (command.substring(5).split(" ")[0].equals("")){
+                            throw new DukeException("☹ OOPS!!! Task number cannot be empty.");
+                        }
+                        int taskNumber = Integer.parseInt(command.substring(5).split(" ")[0]);
+                        //Check if task number is valid
+                        if (taskNumber > 0 && taskNumber <= numCommands) {
+                            taskList[taskNumber - 1].markAsDone();
+                            System.out.println("Nice! I've marked this task as done: ");
+                            System.out.println(taskList[taskNumber - 1]);
+                        } else {
+                            throw new DukeException("☹ OOPS!!! Task number is invalid.");
+                        }
+                    }
+                    //If user input for task number is empty
+                    catch (IndexOutOfBoundsException err) {
+                        throw new DukeException("☹ OOPS!!! Task number cannot be empty.");
+                    }
+                    //If non-numeric input given for task number
+                    catch (NumberFormatException err) {
+                        throw new DukeException("☹ OOPS!!! Task number is invalid.");
+                    }
+                }
+                //Catch error so that program does not terminate
+                catch (DukeException err) {
+                    System.out.println(err.getMessage());
                 }
             }
-            else if (command.length() >= 4 && command.substring(0,4).equals("todo")){
-                numCommands += 1;
-                taskList[numCommands-1] = new Todo(command.substring(5));
-                System.out.println("Got it. I've added this task: ");
-                System.out.println(taskList[numCommands-1]);
-                System.out.println("Now you have "  + numCommands + " tasks in the list.");
+            //For Todo tasks
+            else if (command.length() >= 4 && command.substring(0, 4).equals("todo")) {
+                try {
+                    try {
+                        //Check if description is empty (does not check when user input
+                        //multiple spaces as the description.
+                        if (!command.substring(5).equals((""))){
+                            taskList[numCommands] = new Todo(command.substring(5));
+                            numCommands += 1;
+                            System.out.println("Got it. I've added this task: ");
+                            System.out.println(taskList[numCommands - 1]);
+                            System.out.println("Now you have " + numCommands + " tasks in the list.");
+                        }
+                        else {
+                            throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                        }
+                    } catch (IndexOutOfBoundsException err) {
+                        throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                    }
+                } catch (DukeException err) {
+                    System.out.println(err.getMessage());
+                }
             }
-            else if (command.length() >= 8 && command.substring(0,8).equals("deadline")){
-                numCommands += 1;
-                String[] commandLine = command.substring(9).split(" /by ");
-                taskList[numCommands-1] = new Deadline(commandLine[0], commandLine[1]);
-                System.out.println("Got it. I've added this task: ");
-                System.out.println(taskList[numCommands-1]);
-                System.out.println("Now you have "  + numCommands + " tasks in the list.");
+            //For deadline tasks
+            else if (command.length() >= 8 && command.substring(0, 8).equals("deadline")) {
+                try {
+                    try {
+                        //Check if description is empty (does not check when user input
+                        //multiple spaces as the description.
+                        String[] commandLine = command.substring(9).split(" /by ");
+                        taskList[numCommands] = new Deadline(commandLine[0], commandLine[1]);
+                        numCommands += 1;
+                        System.out.println("Got it. I've added this task: ");
+                        System.out.println(taskList[numCommands - 1]);
+                        System.out.println("Now you have " + numCommands + " tasks in the list.");
+                    } catch (IndexOutOfBoundsException err) {
+                        throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
+                    }
+                } catch (DukeException err) {
+                    System.out.println(err.getMessage());
+                }
+            } else if (command.length() >= 5 && command.substring(0, 5).equals("event")) {
+                try {
+                    try {
+                        String[] commandLine = command.substring(6).split(" /at ");
+                        taskList[numCommands] = new Event(commandLine[0], commandLine[1]);
+                        numCommands += 1;
+                        System.out.println("Got it. I've added this task: ");
+                        System.out.println(taskList[numCommands - 1]);
+                        System.out.println("Now you have " + numCommands + " tasks in the list.");
+                    } catch (IndexOutOfBoundsException err) {
+                        throw new DukeException("☹ OOPS!!! The description of an event cannot be empty.");
+                    }
+                } catch (DukeException err) {
+                    System.out.println(err.getMessage());
+                }
             }
-            else if (command.length() >= 5 && command.substring(0,5).equals("event")){
-                numCommands += 1;
-                String[] commandLine = command.substring(6).split(" /at ");
-                taskList[numCommands-1] = new Event(commandLine[0], commandLine[1]);
-                System.out.println("Got it. I've added this task: ");
-                System.out.println(taskList[numCommands-1]);
-                System.out.println("Now you have "  + numCommands + " tasks in the list.");
+            //For other commands
+            else {
+                try {
+                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                } catch (DukeException err) {
+                    System.out.println(err.getMessage());
+                }
             }
 
             command = scanner.nextLine();

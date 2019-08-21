681622b4f094f4f036bb7f1fa4be3756ac5daedc
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2019-08-21 23:32:52 +0800

    Implement error handling

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -22,36 +22,59 @@ public class Duke {
         echo("Here are the tasks in your list:\n" + taskList.toString());
     }
 
-    private static void handleAddTask(final Task task) {
+    private static void handleAddTask(final Task task) throws DukeException {
         if (taskList.addTask(task)) {
             echo("Got it. I've added this task:\n"
                 + "  " + task.toString() + "\n"
                 + "Now you have " + taskList.size() + " tasks in the list");
         } else {
-            echo("Failed to add task :-(");
+            throw new DukeException("Error: Failed to add task");
         }
     }
 
-    private static void handleTodo(String arguments) {
+    private static void handleTodo(final String arguments) throws DukeException {
+        if (arguments.isBlank()) {
+            throw new DukeException("Error: The description of a task cannot "
+                + "be empty");
+        }
         handleAddTask(new Todo(arguments));
     }
 
-    private static void handleDeadline(String arguments) {
-        String[] splitArgs = arguments.split("\\s+/by\\s+", 2);
+    private static void handleDeadline(final String arguments) throws DukeException {
+        String[] splitArgs = arguments.split("\\s*/by\\s*", 2);
+        if (splitArgs[0].isBlank()) {
+            throw new DukeException("Error: The description of a task cannot "
+                + "be empty");
+        } else if (splitArgs.length == 1) {
+            throw new DukeException("Error: Deadline cannot be empty");
+        }
         Deadline deadline = new Deadline(splitArgs[0], splitArgs[1]);
         handleAddTask(deadline);
     }
 
-    private static void handleEvent(String arguments) {
-        String[] splitArgs = arguments.split("\\s+/at\\s+", 2);
+    private static void handleEvent(final String arguments) throws DukeException {
+        String[] splitArgs = arguments.split("\\s*/at\\s*", 2);
+        if (splitArgs[0].isBlank()) {
+            throw new DukeException("Error: The description of a task cannot "
+                + "be empty");
+        } else if (splitArgs.length == 1) {
+            throw new DukeException("Error: Event time cannot be empty");
+        }
         Event event = new Event(splitArgs[0], splitArgs[1]);
         handleAddTask(event);
     }
 
-    private static void handleDone(String arguments) {
-        Task task = taskList.getTask(Integer.parseInt(arguments) - 1);
-        task.markAsDone();
-        echo("Nice! I've marked this task as done:\n  " + task);
+    private static void handleDone(final String arguments) throws DukeException {
+        try {
+            int taskNumber = Integer.parseInt(arguments) - 1;
+            Task task = taskList.getTask(taskNumber);
+            task.markAsDone();
+            echo("Nice! I've marked this task as done:\n  " + task);
+        } catch (NumberFormatException e) {
+            throw new DukeException("Error: Invalid task number format");
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException("Error: Task does not exist");
+        }
     }
 
     public static void main(String[] args) {
@@ -66,28 +89,31 @@ public class Duke {
             String command = split[0].toLowerCase();
             // If split.length == 1, there are no arguments
             String arguments = (split.length == 1) ? "" : split[1];
-            switch (command) {
-                case "list":
-                    handleList();
-                    break;
-                case "bye":
-                    echo("Bye. Hope to see you again soon!");
-                    return;
-                case "todo":
-                    handleTodo(arguments);
-                    break;
-                case "deadline":
-                    handleDeadline(arguments);
-                    break;
-                case "event":
-                    handleEvent(arguments);
-                    break;
-                case "done":
-                    handleDone(arguments);
-                    break;
-                default:
-                    echo("Unrecognised command :-(");
-                    break;
+            try {
+                switch (command) {
+                    case "list":
+                        handleList();
+                        break;
+                    case "bye":
+                        echo("Bye. Hope to see you again soon!");
+                        return;
+                    case "todo":
+                        handleTodo(arguments);
+                        break;
+                    case "deadline":
+                        handleDeadline(arguments);
+                        break;
+                    case "event":
+                        handleEvent(arguments);
+                        break;
+                    case "done":
+                        handleDone(arguments);
+                        break;
+                    default:
+                        throw new DukeException("Error: Unrecognised command");
+                }
+            } catch (DukeException e) {
+                echo(e.getMessage());
             }
         }
     }

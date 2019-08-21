55d70cc56ecf6d5ba44baea2dea82e715ccd2817
hooncp <hooncheep@gmail.com>
2019-08-21 16:55:31 +0800

    Completed level 2 of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,35 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    public ArrayList<String> tasks;
+
+    public Duke() {
+        tasks = new ArrayList<>();
+    }
+
     public void greet() {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
     }
 
     public void bye() {
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    //Lists out all the tasks in Duke
+    public void list() {
+        int number = 1;
+        for (String task : tasks) {
+            String outputString = number + ". " + task;
+            System.out.println(outputString);
+            number++;
+        }
+    }
+
+    public void add(String task) {
+        tasks.add(task);
+        String outputString = "added: " + task;
+        System.out.println(outputString);
     }
 
     public static void main(String[] args) {
@@ -17,8 +40,10 @@ public class Duke {
             String command = scanner.nextLine();
             if (command.equals("bye")) {
                 break;
+            } else if (command.equals("list")){
+                duke.list();
             } else {
-                System.out.println(command);
+                duke.add(command);
             }
         }
         duke.bye();

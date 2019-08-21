fb0ca85856b1a6dd40026ce125538479b89fd4fd
shyanjie <shyanjieteo@hotmail.com>
2019-08-21 11:42:45 +0800

    Implement Level 1 - Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,38 +2,40 @@ import java.util.Scanner;
 
 public class Duke {
     public static void printLine() {
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
     }
 
     public static void printGreet() {
         printLine();
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "     ____        _        \n"
+                + "    |  _ \\ _   _| | _____ \n"
+                + "    | | | | | | | |/ / _ \\\n"
+                + "    | |_| | |_| |   <  __/\n"
+                + "    |____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        System.out.println("    Hello! I'm Duke");
+        System.out.println("    What can I do for you?");
         printLine();
+        System.out.println("");
     }
 
     public static void readInput() {
         Scanner input = new Scanner(System.in);
 
         while (input.hasNextLine()) {
-            printLine();
-
             String line = input.nextLine();
-
             if (line.equals("bye")) {
                 break;
             }
-            System.out.println(line);
             printLine();
+            System.out.println("    " + line);
+            printLine();
+            System.out.println("");
         }
-        System.out.println("Bye. Hope to see you again soon!");
         printLine();
+        System.out.println("    Bye. Hope to see you again soon!");
+        printLine();
+        System.out.println("");
     }
 
     public static void main(String[] args) {

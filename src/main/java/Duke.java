3a7a107d79532dcf135869711f085c7999bde862
caesarpjz <caesarpjz@gmail.com>
2019-08-21 22:31:25 +0800

    Done Level 1. Added Greeting, Echo and Exit.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,48 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "     ____        _        \n"
+                    + "    |  _ \\ _   _| | _____ \n"
+                    + "    | | | | | | | |/ / _ \\\n"
+                    + "    | |_| | |_| |   <  __/\n"
+                    + "    |____/ \\__,_|_|\\_\\___|\n";
+
+        Duke duke = new Duke();
+        printLine();
+        System.out.println("    Hello! I'm\n" + logo + "\n" + "    What can I do for you?");
+        printLine();
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String text = sc.next();
+            if (text.equals("bye")) {
+                printBye();
+                break;
+            } else {
+                echoFn(text);
+            }
+        }
+    }
+
+    public static void printIndent() {
+        System.out.println("   ");
+    }
+
+    public static void printLine() {
+        System.out.println("    ___________________________________________________________________");
+    }
+
+    public static void echoFn(String str) {
+        printLine();
+        System.out.println("    " + str);
+        printLine();
+    }
+
+    public static void printBye() {
+        printLine();
+        System.out.println("    " + "Bye. Hope to see you again soon!");
+        printLine();
     }
 }

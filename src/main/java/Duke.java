3c7a450a7696245bf379c160b550be4629ba3fcb
Mo Zongran <mozongran@gmail.com>
2019-08-22 00:06:20 +0800

    First commit, level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+
+        printFormattedText("Hello! I'm Duke\n" +
+                "    What can I do for you?");
+
+        while (!input.equals("bye")) {
+            input = sc.next();
+            if (input.equals("bye")) break;
+            printFormattedText(input);
+        }
+
+        printFormattedText("Bye. Hope to see you again soon!");
+    }
+
+    private static void printFormattedText(String text) {
+        printDivider();
+        indent();
+        System.out.println(text);
+        printDivider();
+    }
+
+    private static void printDivider() {
+        indent();
+        for (int i = 0; i < 50; i++) {
+            System.out.print("_");
+        }
+        System.out.println();
+    }
+
+    private static void indent() {
+        for (int i = 0; i < 4; i++) System.out.print(" ");
     }
 }

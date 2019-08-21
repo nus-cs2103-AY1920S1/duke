cbdebc762f4266b50b8def4e4151953c68a65bfc
yuhongtay <yuhongtay@gmail.com>
2019-08-21 15:59:20 +0800

    Level-5, v1.0

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,8 +1,11 @@
 public class Event extends Task{
     String date_Time;
-    public Event(String description){
+    public Event(String description) throws DukeException{
         super(description);
         int divider = description.indexOf("/at");
+        if (divider == -1 || (divider == description.length() - 3)){
+            throw new DukeException((new Border()) + "\n     ☹ OOPS!!! The date/time cannot be empty.\n" + (new Border()));
+        }
         date_Time = description.substring(divider + 4, description.length());
         super.description = super.description.substring(0, divider);
     }

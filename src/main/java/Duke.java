0cbbd15ed566f49fc181c0cd25ec10acae9734d2
PHILIP ALEXANDER BOEDIMAN <philipsenang@gmail.com>
2019-08-22 03:49:14 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -21,15 +21,48 @@ public class Duke {
                 if (list.size() == 0) {
                     System.out.println("Nothing added yet");
                 } else {
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 1; i <= list.size(); i++) {
                         System.out.println("" + i + "." + list.get(i - 1));
                     }
                 }
             } else if (str.substring(0, 4).equals("done")) {
                 Integer taskNum = Integer.valueOf(str.substring(5));
-                Task currTask = list.get(taskNum-1);
+                Task currTask = list.get(taskNum - 1);
                 currTask.markAsDone();
                 System.out.println("Nice! I've marked this task as done:" + "\n" + "[\u2713] " + currTask.getDescription());
+            } else if (str.substring(0, 4).equals("todo")) {
+                String message = "Got it. I've added this task:";
+                Task toDoTask = new Task(str.substring(5));
+                list.add(toDoTask);
+                toDoTask.setTypeOfTask("todo");
+                System.out.println(message);
+                System.out.println(" " + toDoTask);
+                System.out.println("Now you have " + list.size() + " tasks in the list");
+            } else if (str.length() >= 8 && str.substring(0, 8).equals("deadline")) {
+                String message = "Got it. I've added this task:";
+                int index = str.indexOf('/') + 1;
+                Task deadlineTask = new Task(str.substring(9, index - 2));
+                list.add(deadlineTask);
+                deadlineTask.setTypeOfTask("deadline");
+                String preposition = str.substring(index, index + 2);
+                String time = "(" + preposition + ": " + str.substring(index + 3) + ")";
+                deadlineTask.addTime(time);
+                System.out.println(message);
+                System.out.println(" " + deadlineTask);
+                System.out.println("Now you have " + list.size() + " tasks in the list.");
+            } else if (str.length() >= 5 && str.substring(0, 5).equals("event")) {
+                String message = "Got it. I've added this task:";
+                int index = str.indexOf('/') + 1;
+                Task eventTask = new Task(str.substring(6, index - 2));
+                list.add(eventTask);
+                eventTask.setTypeOfTask("event");
+                String preposition = str.substring(index, index + 2);
+                String time = "(" + preposition + ": " + str.substring(index + 3) + ")";
+                eventTask.addTime(time);
+                System.out.println(message);
+                System.out.println(" " + eventTask);
+                System.out.println("Now you have " + list.size() + " tasks in the list.");
             } else {
                 System.out.println("added: " + str);
                 list.add(new Task(str));
@@ -37,5 +70,4 @@ public class Duke {
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
-
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,21 +2,49 @@ public class Task {
 
     protected String description;
     protected boolean isDone;
+    protected String typeOfTask = "";
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
+
+    public void setTypeOfTask(String typeOfTask) {
+        if (typeOfTask.equals("todo")) {
+            this.typeOfTask = "T";
+        } else if (typeOfTask.equals("event")) {
+            this.typeOfTask = "E";
+        } else {
+            this.typeOfTask = "D";
+        }
+    }
+
+    public String getTypeOfTask() {
+        return this.typeOfTask;
+    }
+
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718");
     }
+
     public void markAsDone() {
         this.isDone = true;
     }
+
     public String getDescription() {
         return this.description;
     }
+
+    public void addTime(String description) {
+        this.description = new String(this.description + " " + description);
+    }
+
+
     public String toString() {
-        return "[" + this.getStatusIcon() + "] " + this.description;
+        if (this.typeOfTask.equals("")) {
+            return "[" + this.getStatusIcon() + "] " + this.description;
+        } else {
+            return "[" + this.typeOfTask + "]" + "[" + this.getStatusIcon() + "] " + this.description;
+        }
     }
 }

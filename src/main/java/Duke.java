92a00bd2aebe0c685e351cc0957cc599283a2cef
Kalsyc <klassiclegend@gmail.com>
2019-08-21 16:20:54 +0800

    Add Greet, Echo, Exit functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,19 +6,24 @@ public class Duke {
         //program start
         final String horizontalLine = "\t____________________________________________________________";
         System.out.println(horizontalLine);
-        System.out.println("\tHello! I'm Duke\n\tWhat can I do for you?");
+        System.out.println(formatText("Hello! I'm Duke"));
+        System.out.println(formatText("What can I do for you?"));
         System.out.println(horizontalLine);
         //import scanner + logic
         Scanner scanner = new Scanner(System.in);
         while (!scanner.hasNext("bye")){
             String command = scanner.next();
             System.out.println(horizontalLine);
-            System.out.println("\t"+command);
+            System.out.println(formatText(command));
             System.out.println(horizontalLine);
         }
-        String exitMessage = "\tBye. Hope to see you again soon!";
+        String exitMessage = "Bye. Hope to see you again soon!";
         System.out.println(horizontalLine);
-        System.out.println(exitMessage);
+        System.out.println(formatText(exitMessage));
         System.out.println(horizontalLine);
+    }
+
+    private static String formatText(String text){
+        return "\t " + text;
     }
 }

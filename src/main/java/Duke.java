b808e56ecbd6c6601a412b644a1c4544dc88d3f0
arjavibahety <arjavibahety@gmail.com>
2019-08-22 01:03:39 +0800

    added exception handling to Duke class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Duke {
@@ -5,7 +6,7 @@ public class Duke {
      * The duke project.
      */
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -29,45 +30,83 @@ public class Duke {
             while (!echo.equals("bye")) {
                 String[] echoArr = echo.split(" ");
                 System.out.println(line);
-                if(echo.equals("list")) {
-                    System.out.println("    Here are the tasks in your list:");
-                    for(int i = 0; i < counter; i++) {
-                        System.out.println("    " + (i + 1) + ". " + list[i]);
+                try {
+                    if (echo.equals("list")) {
+                        System.out.println("    Here are the tasks in your list:");
+                        for (int i = 0; i < counter; i++) {
+                            System.out.println("    " + (i + 1) + ". " + list[i]);
+                        }
+                    } else if (echoArr[0].equals("done")) {
+                        list[Integer.parseInt(echoArr[1]) - 1].markAsDone();
+                        System.out.println("    Nice! I've marked this task as done: ");
+                        System.out.println("     " + list[Integer.parseInt(echoArr[1]) - 1]);
+                    } else if (echoArr[0].equals("deadline") || echoArr[0].equals("todo") || echoArr[0].equals("event")) {
+                        if (echoArr[0].equals("deadline")) {
+                            try {
+                                if (echoArr.length == 1) {
+                                    throw new DukeException("☹ OOPS!!! The description of deadline cannot be empty");
+                                } else if (!echo.contains("/by")) {
+                                    throw new DukeException("☹ OOPS!!! The deadline must be completed by a certain date");
+                                } else {
+                                    list[counter] = new Deadline(echo.substring(echo.indexOf(" ") + 1, echo.indexOf("/") - 1),
+                                            echo.substring(echo.indexOf("/by") + 4));
+                                }
+                            } catch (DukeException exp) {
+                                System.out.println("    " + exp.getMessage());
+                            }
+                        } else if (echoArr[0].equals("todo")) {
+                            try {
+                                if (echoArr.length == 1) {
+                                    throw new DukeException("☹ OOPS!!! The description of deadline cannot be empty");
+                                } else {
+                                    list[counter] = new Todo(echo.substring(echo.indexOf(" ") + 1));
+                                }
+                            } catch (DukeException exp) {
+                                System.out.println("    " + exp.getMessage());
+                            }
+                        } else if (echoArr[0].equals("event")) {
+                            try {
+                                if (echoArr.length == 1) {
+                                    throw new DukeException("☹ OOPS!!! The description of event cannot be empty");
+                                } else if (!echo.contains("/at")) {
+                                    throw new DukeException("☹ OOPS!!! The event must be at by a certain date");
+                                } else {
+                                    list[counter] = new Event(echo.substring(echo.indexOf(" ") + 1, echo.indexOf("/") - 1),
+                                            echo.substring(echo.indexOf("/at") + 4));
+                                }
+                            } catch (DukeException exp) {
+                                System.out.println("    " + exp.getMessage());
+                            }
+                        }
+
+                        if (list[counter] != null) {
+                            System.out.println("    Got it. I've added this task:");
+                            System.out.println("     " + list[counter]);
+                            counter++;
+                            System.out.println("    Now you have " + counter + " in the list.");
+                        }
+                    } else {
+                        throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                     }
-                } else if(echoArr[0].equals("done")) {
-                    list[Integer.parseInt(echoArr[1]) - 1].markAsDone();
-                    System.out.println("    Nice! I've marked this task as done: ");
-                    System.out.println("     " + list[Integer.parseInt(echoArr[1]) - 1]);
-                } else {
-                    if(echoArr[0].equals("deadline")) {
-                        list[counter] = new Deadline(echo.substring(echo.indexOf(" ") + 1, echo.indexOf("/") - 1),
-                                                     echo.substring(echo.indexOf("/by") + 4));
-                    } else if(echoArr[0].equals("todo")) {
-                        list[counter] = new Todo(echo.substring(echo.indexOf(" ") + 1));
-                    } else if(echoArr[0].equals("event")) {
-                        list[counter] = new Event(echo.substring(echo.indexOf(" ") + 1, echo.indexOf("/") - 1),
-                                echo.substring(echo.indexOf("/at") + 4));
-                    }
-                    System.out.println("    Got it. I've added this task:");
-                    System.out.println("     " + list[counter]);
-                    counter++;
-                    System.out.println("    Now you have " + counter + " in the list.");
+                } catch (DukeException exp) {
+                    System.out.println("    " + exp.getMessage());
                 }
+
                 System.out.println(line);
-                if(sc.hasNextLine()) {
+                if (sc.hasNextLine()) {
                     echo = sc.nextLine();
                 } else {
                     break;
                 }
-
             }
 
-            if (echo.equals("bye")) {
-                System.out.println(line);
-                System.out.println("    Bye. Hope to see you again soon!");
-                System.out.println(line);
-            }
         }
 
+        if (echo.equals("bye")) {
+            System.out.println(line);
+            System.out.println("    Bye. Hope to see you again soon!");
+            System.out.println(line);
+        }
     }
 }
+

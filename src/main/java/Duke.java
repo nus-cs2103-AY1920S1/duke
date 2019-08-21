96486db21d8a50b092806146f9e2f74d9af3a4e7
choongyx <yongxin.choong98@gmail.com>
2019-08-21 20:07:39 +0800

    Created Task class and add mark as done feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,18 +12,27 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine();
-        String[] taskList = new String[100];
+        Task[] taskList = new Task[100];
         int numCommands = 0;
 
         while (!command.equals("bye")){
             if (command.equals("list")){
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 1; i <= numCommands; i++){
-                    System.out.println(i + ". "+ taskList[i-1]);
+                    System.out.println(i + "."+ taskList[i-1]);
+                }
+            }
+            else if (command.length() >= 4 && command.substring(0,4).equals("done")){
+                int taskNumber = Character.getNumericValue(command.charAt(command.length()-1));
+                if (taskNumber > 0 && taskNumber <= numCommands) {
+                    taskList[taskNumber-1].markAsDone();
+                    System.out.println("Nice! I've marked this task as done: ");
+                    System.out.println(taskList[taskNumber-1]);
                 }
             }
             else {
                 numCommands += 1;
-                taskList[numCommands-1] = command;
+                taskList[numCommands-1] = new Task(command);
                 System.out.println("added: " + command);
             }
             command = scanner.nextLine();

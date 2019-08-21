c57b8f7f3eefb5b8c3c447e9940bf13e1d1d0d3b
Taha <taha.jailani@gmail.com>
2019-08-22 01:08:56 +0800

    Level-2

--- src/main/java/MainManager.java
+++ src/main/java/MainManager.java
@@ -1,18 +1,40 @@
 import java.util.Scanner;
 
 public class MainManager {
-    public MainManager() {};
+    private String[] list;
+    private int counter;
+
+    public MainManager() {
+        this.list = new String[100];
+        counter = 0;
+    }
 
     public void run() {
         Scanner sc = new Scanner(System.in);
 
         while(sc.hasNext()) {
             String toPrint = sc.nextLine();
+            switch(toPrint) {
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+
+                case "list":
+                    for(String curr : list) {
+                        if(curr != null) {
+                            System.out.println(curr);
+                        }
+                    }
+                    break;
+
+                default:
+                    list[counter] = ++counter + ". " + toPrint;
+                    System.out.println("added: " + toPrint);
+                    break;
+            }
+
             if(toPrint.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else {
-                System.out.println(toPrint);
             }
         }
     }

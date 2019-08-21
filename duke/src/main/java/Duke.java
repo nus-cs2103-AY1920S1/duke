6f00efa961d21e7bffbdf9fee2413ae82c5e632f
Mandy Wang <shunflower320@gmail.com>
2019-08-21 15:05:31 +0800

    Revert "Revert "Level-4 complete""
    This reverts commit 95773c0a63b8cb564dbd6335d1504616cd45189c.

--- duke/src/main/java/Duke.java
+++ duke/src/main/java/Duke.java
@@ -14,24 +14,50 @@ public class Duke {
 		System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
 		Scanner scanner = new Scanner(System.in);
-		String next = scanner.nextLine();
+		String next = scanner.next(); // no longer need nextline because adding comes with type of task
 
 		while (!next.equals("bye")) {
 		  if (next.equals("list")) {
 		  	System.out.println("Here are the tasks in your list:");
 			for (int i = 0; i < list.size(); i++) {
-			  System.out.println((i+1) + "." + list.get(i).getStatus());
+			  System.out.println((i+1) + "." + list.get(i).toString());
 			}
-		  } else if (next.contains("done")) {
-		  	int index = Integer.valueOf(next.substring(next.length()-1));
+		  } else if (next.equals("done")) {
+		  	int index = scanner.nextInt(); // since scanner only took in the word done
 		  	Task task = list.get(index-1);
 		  	task.markAsDone();
-		  	System.out.println("Nice! I've marked this task as done:\n  " + task.getStatus());
+		  	System.out.println("Nice! I've marked this task as done:\n  " + task.toString());
 		  } else {
-			list.add(new Task(next));
-			System.out.println("added: " + next);
+		  	Task task = new Task("");
+		  	switch(next) {
+				case "todo":
+					String desc = scanner.nextLine().trim(); // assuming it takes whatever remains on the line before crossing over \n
+					task = new Todo(desc);
+					break;
+
+				case "deadline":
+					String line = scanner.nextLine().trim();
+					int indexStart = line.indexOf(" ");
+					int indexBy = line.indexOf("/");
+					String desc2 = line.substring(0, indexBy-1); // start after space, end before space before /
+					String by = line.substring(indexBy+1);
+					task = new Deadline(desc2, by);
+					break;
+
+				case "event":
+					String line2 = scanner.nextLine().trim();
+					int indexStart2 = line2.indexOf(" ");
+					int indexAt = line2.indexOf("/");
+					String desc3 = line2.substring(0, indexAt-1); // start after space, end before space before /
+					String at = line2.substring(indexAt+1);
+					task = new Event(desc3, at);
+					break;
+		  	}
+		  	list.add(task);
+		  	System.out.println("Got it. I've added this task:\n  " + task.toString()); // not overridden???
+		  	System.out.println("Now you have " + list.size() + " tasks in the list.");
 		  }
-		  next = scanner.nextLine();
+		  next = scanner.next();
 		}
 
 		System.out.println("Bye. Hope to see you again soon!");

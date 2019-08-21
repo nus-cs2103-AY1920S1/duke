91f608db7a780ae25bd7f7db6c6d8d452cf1193a
Murugesan Karthika <mdkkarthika@gmail.com>
2019-08-21 17:43:17 +0800

    Merge branch 'gradle'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,14 +14,22 @@ public class Duke {
         */
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        String[] arr = new String[100];
         Scanner sc = new Scanner(System.in);
+        int i = 0;
         while(sc.hasNext()) {
             String input = sc.nextLine();
             if(input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if(input.equals("list")){
+                for(int j = 0; j < i; j++){
+                    System.out.println((j+1) + ". " + arr[j]);
+                }
             } else {
-                System.out.println(input);
+                arr[i] = input;
+                System.out.println("added: " + input);
+                i++;
             }
         }
     }

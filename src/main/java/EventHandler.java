9fc20ea4e39d003cee3e553db0dbfe5ffdf4b981
Lim Daekoon <daekoon_lim@hotmail.com>
2019-08-21 19:11:48 +0800

    Added functionality to store, and list tasks

--- src/main/java/EventHandler.java
+++ src/main/java/EventHandler.java
@@ -1,19 +1,41 @@
 package seedu.duke;
 
 import seedu.duke.PrettyPrint;
+import java.util.ArrayList;
 
 public class EventHandler {
 
+  ArrayList<String> TodoList;
+
   public EventHandler() {
+    TodoList = new ArrayList<String>();
   }
 
   public void run(String command) {
     switch(command) {
       case "bye":
         return;
+      case "list":
+        list(command);
+        break;
       default:
-        PrettyPrint.printBlock(command);
+        add(command);
         break;
     }
   }
+
+  public void add(String todo) {
+    TodoList.add(todo);
+    String output = "added: " + todo;
+    PrettyPrint.printBlock(new String[] {output});
+  }
+
+  public void list(String command) {
+    String[] outputs = TodoList.toArray(new String[TodoList.size()]);
+    for (int z = 0; z < TodoList.size(); z++) {
+      outputs[z] = String.format("%3d. " + outputs[z], z + 1);
+    }
+    PrettyPrint.printBlock(outputs);
+  }
+
 }

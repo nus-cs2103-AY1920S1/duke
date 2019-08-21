e70fdb3253829b04697554a80ce2bba6560de345
alxkohh <alxkohh@gmail.com>
2019-08-21 15:16:31 +0800

    Level 5 completed
    Application can now handle errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -28,35 +28,29 @@ public class Duke {
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println(targetTask);
             } else if (inputSplit[0].equals("todo")) {
-                String todoDescription = input.substring(5);
-                Task t = new Todo(todoDescription);
-                dukeList.add(t);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(t);
-                System.out.println("Now you have " + dukeList.size() + " tasks in the list.");
+                try {
+                    handleInputTodo(input);
+                } catch (DukeException e) {
+                    System.out.println(e.getMessage());
+                }
             } else if (inputSplit[0].equals("deadline")) {
-                int slashLocation = slashLocator(input);
-                int firstIndex = slashLocation - 1;
-                int secondIndex = slashLocation + 4;
-                String deadlineDescription = input.substring(9, firstIndex);
-                String deadlineBy = input.substring(secondIndex);
-                Task t = new Deadline(deadlineDescription, deadlineBy);
-                dukeList.add(t);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(t);
-                System.out.println("Now you have " + dukeList.size() + " tasks in the list.");
+                try {
+                    handleInputDeadline(input);
+                } catch (DukeException e) {
+                    System.out.println(e.getMessage());
+                }
             } else if (inputSplit[0].equals("event")) {
-                int slashLocation = slashLocator(input);
-                int firstIndex = slashLocation - 1;
-                int secondIndex = slashLocation + 4;
-                String eventDescription = input.substring(6, firstIndex);
-                String eventAt = input.substring(secondIndex);
-                Task t = new Event(eventDescription, eventAt);
-                dukeList.add(t);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(t);
-                System.out.println("Now you have " + dukeList.size() + " tasks in the list.");
+                try {
+                    handleInputEvent(input);
+                } catch (DukeException e) {
+                    System.out.println(e.getMessage());
+                }
             } else {
+                try {
+                    handleInputUnrecognised(input);
+                } catch (DukeException e) {
+                    System.out.println(e.getMessage());
+                }
             }
         }
     }
@@ -72,4 +66,56 @@ public class Duke {
     public static int slashLocator(String sentence) {
         return sentence.indexOf("/");
     }
+
+    public static void handleInputTodo(String inputTodo) throws DukeException {
+        if (inputTodo.length() == 4) {
+            throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+        } else {
+            String todoDescription = inputTodo.substring(5);
+            Task t = new Todo(todoDescription);
+            dukeList.add(t);
+            System.out.println("Got it. I've added this task:");
+            System.out.println(t);
+            System.out.println("Now you have " + dukeList.size() + " tasks in the list.");
+        }
+    }
+
+    public static void handleInputDeadline(String inputDeadline) throws DukeException {
+        if (inputDeadline.length() == 8) {
+            throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
+        } else {
+            int slashLocation = slashLocator(inputDeadline);
+            int firstIndex = slashLocation - 1;
+            int secondIndex = slashLocation + 4;
+            String deadlineDescription = inputDeadline.substring(9, firstIndex);
+            String deadlineBy = inputDeadline.substring(secondIndex);
+            Task t = new Deadline(deadlineDescription, deadlineBy);
+            dukeList.add(t);
+            System.out.println("Got it. I've added this task:");
+            System.out.println(t);
+            System.out.println("Now you have " + dukeList.size() + " tasks in the list.");
+        }
+    }
+
+    public static void handleInputEvent(String inputEvent) throws DukeException {
+        if (inputEvent.length() == 5) {
+            throw new DukeException("☹ OOPS!!! The description of a event cannot be empty.");
+        } else {
+            int slashLocation = slashLocator(inputEvent);
+            int firstIndex = slashLocation - 1;
+            int secondIndex = slashLocation + 4;
+            String eventDescription = inputEvent.substring(6, firstIndex);
+            String eventAt = inputEvent.substring(secondIndex);
+            Task t = new Event(eventDescription, eventAt);
+            dukeList.add(t);
+            System.out.println("Got it. I've added this task:");
+            System.out.println(t);
+            System.out.println("Now you have " + dukeList.size() + " tasks in the list.");
+        }
+    }
+
+    public static void handleInputUnrecognised(String inputUnrecognised) throws DukeException {
+        throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+    }
+
 }

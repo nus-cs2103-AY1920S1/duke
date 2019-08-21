f0c778317d8e622e1a0edf7486962100c1be8f7c
victorvic54 <sacchirro@yahoo.com>
2019-08-21 19:17:08 +0800

    Finished Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,19 @@
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Duke {
+    public static void printList(LinkedList<String> taskList) {
+        int counter = 1;
+
+        for (String subList: taskList) {
+            System.out.println(counter + ". " + subList);
+            counter++;
+        }
+    }
+
     public static void main(String[] args) {
+        LinkedList<String> taskList = new LinkedList<>();
+
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
@@ -9,7 +21,13 @@ public class Duke {
         String inputText = sc.nextLine();
 
         while (!inputText.equals("bye")) {
-            System.out.println(inputText);
+            if (inputText.equals("list")) {
+                printList(taskList);
+            } else {
+                System.out.println("added: " + inputText);
+                taskList.add(inputText);
+            }
+
             inputText = sc.nextLine();
         }
 

25d792c8fce3b1297abe880887d44029d0446957
Aulud <44989315+Aulud@users.noreply.github.com>
2019-08-21 23:40:50 +0800

    Add opening greeting and ability to echo user commands or exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,25 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+
+        String intro = "Hello! I'm Duke\nWhat can I do for you?\n";
+        System.out.println(intro);
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String command = sc.nextLine().trim();
+            // Terminate the bot if the 'bye' command is issued
+            if (command.equals("bye")) {
+                break;
+            } else {
+                // Echo the input command
+                System.out.println(command + "\n");
+            }
+        }
+
+        // Explicitly closes the Scanner and input stream
+        sc.close();
     }
 }

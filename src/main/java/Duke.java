c49a522baed604c1f6e07c9bf8e150c0f5feb034
Liu Zechu <liuzechu2013@gmail.com>
2019-08-21 23:58:59 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String LINE = "    ____________________________________________________________\n";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        greet();
+        getUserInput();
+    }
+
+    private static void greet() {
+        String greeting = LINE
+                + "     Hello! I'm Duke\n"
+                + "     What can I do for you?\n"
+                + LINE;
+
+        System.out.println(greeting);
+    }
+
+    private static void getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        if (userInput.equals("bye")) {
+            String farewellMessage = LINE
+                    + "     Bye. Hope to see you again soon!\n"
+                    + LINE;
+            System.out.println(farewellMessage);
+        } else {
+            System.out.println(LINE + "     " + userInput + "\n" + LINE);
+            getUserInput();
+        }
     }
 }

ddaef0b86b24a1eae42a630792edcd03b97f1885
jhchen <chenjiehan333@gmail.com>
2019-08-22 03:18:29 +0800

    Complete Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
+    private static ArrayList<String> list = new ArrayList<String>(100);
+
     public static void main(String[] args) throws IOException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -12,27 +16,37 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         greet();
-        String input;
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 
-            input = br.readLine();
-            while (!input.equals("bye")) {
-                echo(input);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        while(!input.equals("bye")) {
+            if(!input.equals("list")) {
+                addList(input);
+                input = br.readLine();
+            } else {
+                readList();
                 input = br.readLine();
             }
         }
 
-        if (input.equals("bye")) {
-            exit();
+        exit();
+    }
+
+    private static void readList() {
+        for(int i = 1;i <= list.size();i++) {
+            System.out.println(i + ". " + list.get(i - 1));
         }
     }
 
-    private static void exit() {
-        System.out.println("Bye! Hope to see you again soon!");
+    private static void addList(String input) {
+        list.add(input);
+        System.out.println("added: " + input);
     }
 
-    private static void echo(String input) {
-        System.out.println(input);
+
+    private static void exit() {
+        System.out.println("Bye! Hope to see you again soon!");
     }
 
     private static void greet() {

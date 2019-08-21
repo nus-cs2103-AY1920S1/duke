2443aa63b5ec0b38ed1dc444d5aa9fb351b9d00a
timothyleong97 <timothy.leong@u.nus.edu>
2019-08-21 23:19:04 +0800

    implemented level 3. However, unicode formatting seems to be a bug on my workstation.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Duke {
@@ -14,32 +16,63 @@ public class Duke {
      * @param args takes in arguments.
      */
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
 
         print("Hello! I'm Duke\nWhat can I do for you?");
+
         Scanner sc = new Scanner(System.in);
-        String[] arr = new String[100];
+        Task[] arr = new Task[100];
         int index = 0;
+
         while (sc.hasNextLine()) {
             String str = sc.nextLine();
+
             if (str.equals("bye")) {
                 print("Bye. Hope to see you again soon!");
                 break;
             } else if (str.equals("list")) {
+                print("Here are the tasks in your list:");
                 for (int i = 0; i < index; i++) {
                     print((i + 1) + ". " + arr[i]);
                 }
+            } else if (str.startsWith("done")) {
+                //str = "done 1" / "done 5" but str is 1-indexed
+                String[] instruction = str.split(" ");
+                int whichTask = Integer.parseInt(instruction[1]) - 1;
+                arr[whichTask].markAsDone();
+                print("Nice! I've marked this task as done:");
+                print(arr[whichTask].toString());
             } else {
-                arr[index++] = str;
+                arr[index++] = new Task(str);
                 print("added: " + str);
+
             }
 
         }
     }
 
+    /**
+     * Inner class to Duke. A Task class representing a task with a isDone component.
+     */
+    static class Task {
+        private String description;
+        private boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        private String getStatusIcon() {
+            return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+        }
+
+        public void markAsDone() {
+            isDone = true;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + getStatusIcon() + "]" + " " + description;
+        }
+    }
 }

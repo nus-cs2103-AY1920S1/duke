501d7653faa19983eb01505b0ae372fc48f33d0a
brianyenna <brianyen.na@gmail.com>
2019-08-21 11:18:04 +0800

    Edited runDuke() method to handle addition of input and listing of input.
    All the input is stored in an ArrayList called storedInput (instance variable of Duke)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    private ArrayList<String> storedInput = new ArrayList<String>();
+
     public static void main(String[] args) {
         Duke d = new Duke();
 
@@ -19,12 +22,33 @@ public class Duke {
                 case "bye":
                     contRunning = false;
                     break;
+                case "list":
+                    listStoredInput();
+                    break;
                 default:
-                    System.out.println("\t____________________________________________________________");
-                    System.out.println("\t" + s);
-                    System.out.println("\t____________________________________________________________");
+                    addInputToStore(s);
             }
         }
+    }
+
+    public void addInputToStore(String input) {
+        this.storedInput.add(input);
+
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tadded: " + input);
+        System.out.println("\t____________________________________________________________");
+    }
+
+    public void listStoredInput() {
+        System.out.println("\t____________________________________________________________");
+
+        int counter = 1;
+        for (String input : this.storedInput) {
+            System.out.println("\t" + counter + ". " + input);
+            counter++;
+        }
+
+        System.out.println("\t____________________________________________________________");
     }
 
     public void initDuke() {

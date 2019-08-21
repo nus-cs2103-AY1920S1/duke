9eb249609f579a52bffb6ea50e3001cd98e9948b
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2019-08-22 00:33:29 +0800

    Add method to mark task done at TaskManager

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -8,9 +8,16 @@ public class TaskManager {
         System.out.println("added: " + task);
     }
 
+    public void markAsDone(int index) {
+        index--;
+        tasks.get(index).markAsDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("  " + tasks.get(index));
+    }
+
     public void printTasks() {
         for (int i = 0; i < tasks.size(); ++i) {
-            System.out.println((i + 1) + ". " + tasks.get(i));
+            System.out.println((i + 1) + "." + tasks.get(i));
         }
     }
 }

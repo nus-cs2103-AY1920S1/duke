0492263dab8f96d6f97f0f9f109e5824c1ea0fd4
shyanjie <shyanjieteo@hotmail.com>
2019-08-21 11:55:46 +0800

    Implement Level 2 - Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static String[] data = new String[100];
+
     public static void printLine() {
         System.out.println("   ____________________________________________________________");
     }
@@ -21,21 +23,33 @@ public class Duke {
 
     public static void readInput() {
         Scanner input = new Scanner(System.in);
-
+        int item = 0;
         while (input.hasNextLine()) {
             String line = input.nextLine();
+
             if (line.equals("bye")) {
+                printLine();
+                System.out.println("    Bye. Hope to see you again soon!");
+                printLine();
+                System.out.println("");
                 break;
+            } else if (line.equals("list")) {
+                int index = 1;
+                printLine();
+                for (int i = 0; i < item; i++) {
+                    System.out.println("    " + index++ + "." + data[i]);
+                }
+                printLine();
+                System.out.println("");
+            } else {
+                data[item] = line;
+                printLine();
+                System.out.println("    " + "added: " + line);
+                printLine();
+                System.out.println("");
+                item++;
             }
-            printLine();
-            System.out.println("    " + line);
-            printLine();
-            System.out.println("");
         }
-        printLine();
-        System.out.println("    Bye. Hope to see you again soon!");
-        printLine();
-        System.out.println("");
     }
 
     public static void main(String[] args) {

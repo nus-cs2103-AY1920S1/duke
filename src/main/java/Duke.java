68a7aea1c0c94924a44e4913742e6ffff16b0bc8
caesarpjz <caesarpjz@gmail.com>
2019-08-22 00:55:53 +0800

    Done Level 2. Added List function and Task function.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,41 +8,66 @@ public class Duke {
                     + "    | |_| | |_| |   <  __/\n"
                     + "    |____/ \\__,_|_|\\_\\___|\n";
 
-        Duke duke = new Duke();
+        // Prints out greeting of the chatbot.
         printLine();
-        System.out.println("    Hello! I'm\n" + logo + "\n" + "    What can I do for you?");
+        printIndent();
+        System.out.println("Hello! I'm\n" + logo + "\n" + "    What can I do for you?");
         printLine();
 
         Scanner sc = new Scanner(System.in);
-
+        // String array to store text entered by user.
+        String[] list = new String[100];
+        // Counter to count total number of items in array.
+        int n = 0;
         while (sc.hasNext()) {
-            String text = sc.next();
+            String text = sc.nextLine();
             if (text.equals("bye")) {
                 printBye();
                 break;
+            } else if (text.equals("list")) {
+                printLine();
+                for (int i = 1; i <= n; i++) {
+                    printIndent();
+                    System.out.println(i + ". " + list[i-1]);
+                }
+                printLine();
             } else {
-                echoFn(text);
+                addToList(list, text, n);
+                printAdds(text);
+                n++;
             }
         }
     }
 
-    public static void printIndent() {
-        System.out.println("   ");
+    // Prints Indentation.
+    private static void printIndent() {
+        System.out.print("    ");
     }
 
-    public static void printLine() {
-        System.out.println("    ___________________________________________________________________");
+    // Prints the line. For better organisation.
+    private static void printLine() {
+        printIndent();
+        System.out.println("___________________________________________________________________");
     }
 
-    public static void echoFn(String str) {
+    // Adds text into the string array.
+    private static void addToList(String[] list, String str, int num) {
+        list[num] = str;
+    }
+
+    // Ends the chatbot.
+    private static void printBye() {
         printLine();
-        System.out.println("    " + str);
+        printIndent();
+        System.out.println("Bye. Hope to see you again soon!");
         printLine();
     }
 
-    public static void printBye() {
+    // Shows users what has been added into the list/string array.
+    private static void printAdds(String str) {
         printLine();
-        System.out.println("    " + "Bye. Hope to see you again soon!");
+        printIndent();
+        System.out.println("added: " + str);
         printLine();
     }
 }

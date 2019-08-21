063410cd77177e63b295cb064d6efa8017b87e4c
yeodonghan <yeodonghan@gmail.com>
2019-08-22 05:08:36 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,25 +24,46 @@ public class Duke {
                 String dummy1 = sc.nextLine();
                 list[pos1].markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("[" + list[pos1].getStatusIcon() + "] " + list[pos1]);
+                System.out.println(list[pos1]);
+            } else if (sc.hasNext("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                return;
+            } else if (sc.hasNext("todo")) {
+                String dummy = sc.next();
+                Task curr = new Todo(sc.nextLine());
+                list[pos] = curr;
+                pos++;
+                System.out.println("Got it. I've added this task:");
+                System.out.println(curr);
+                System.out.println("Now you have " + pos + " tasks in the list.");
+            } else if (sc.hasNext("deadline")) {
+                String dummy = sc.next();
+                String[] split = new String[2];
+                split = sc.nextLine().split("/by");
+                Task curr = new Deadline(split[0], split[1]);
+                list[pos] = curr;
+                pos++;
+                System.out.println("Got it. I've added this task:");
+                System.out.println(curr);
+                System.out.println("Now you have " + pos + " tasks in the list.");
+            } else if (sc.hasNext("event")) {
+                String dummy = sc.next();
+                String[] split = new String[2];
+                split = sc.nextLine().split("/at");
+                Task curr = new Event(split[0], split[1]);
+                list[pos] = curr;
+                pos++;
+                System.out.println("Got it. I've added this task:");
+                System.out.println(curr);
+                System.out.println("Now you have " + pos + " tasks in the list.");
             } else {
                 String input = sc.nextLine();
-                if (input.equals("bye")) {
-                    System.out.println("Bye. Hope to see you again soon!");
-                    return;
-                } else if (input.equals("list")) {
-                    System.out.println("Here are the tasks in your list:");
-                    int k = 0;
-                    while (k != pos) {
-                        int bullet = k + 1;
-                        System.out.println(bullet + ".[" + list[k].getStatusIcon() + "] " + list[k]);
-                        k++;
-                    }
-                } else {
-                    Task task = new Task(input);
-                    list[pos] = task;
-                    System.out.println("added: " + input);
-                    pos++;
+                System.out.println("Here are the tasks in your list:");
+                int k = 0;
+                while (k != pos) {
+                    int bullet = k + 1;
+                    System.out.println(bullet + "." + list[k]);
+                    k++;
                 }
             }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -17,6 +17,8 @@ public class Task {
     }
 
     public String toString() {
-        return this.description;
+        return "[" + this.getStatusIcon() + "] " + this.description;
     }
+
+
 }

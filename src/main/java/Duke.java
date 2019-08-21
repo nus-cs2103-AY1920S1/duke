b6920e08b323610ae8ff9a9f4847df31e77e1d51
gachia <massreduction@outlook.com>
2019-08-21 11:58:19 +0800

    Level-1
    Completed Level 1:
    Add Greet, Echo and Exit to Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,17 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        final String lineSpace = "_______________________________";
+        String startMessage = lineSpace + "\nHello! I'm Duke\nWhat can I do for you?" + lineSpace;
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            String userInput = sc.nextLine();
+            if(userInput.equals("bye")){
+                System.out.println(lineSpace + "\nBye. Hope to see you again soon!\n" + lineSpace);
+                break;
+            }
+            System.out.println(lineSpace + "\n" + userInput + "\n" + lineSpace);
+        }
+
     }
 }

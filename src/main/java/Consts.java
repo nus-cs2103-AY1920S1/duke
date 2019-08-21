0ef888d36436a6b5905d82057d7edd0c4e3fe4c0
Yeo Tong <helloimyeotong@gmail.com>
2019-08-21 16:31:53 +0800

    Implement Level 1 Greet, Echo, Exit

---
+++ src/main/java/Consts.java
@@ -0,0 +1,12 @@
+public final class Consts {
+    public static final String LOGO = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
+    //To prevent users from creating an instance of this class
+    private Consts() {
+        throw new AssertionError();
+    }
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,71 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String input = "";
+        Scanner sc = new Scanner(System.in);
+
+        printLogo();
+        printIntro();
+
+        do {
+            if (!sc.hasNextLine()) {
+                continue;
+            }
+
+            input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                printExitMsg();
+            } else {
+                printResponse(input);
+            }
+        } while (!input.equals("bye"));
+    }
+
+    //prints the logo for the chat bot
+    private static void printLogo() {
+        System.out.println(Consts.LOGO);
+    }
+
+    //prints the indentation used for the output
+    private static void printIndentation() {
+        System.out.print("\t");
+    }
+
+    //prints line with indentation in front
+    private static void printLine() {
+        printIndentation();
+        System.out.println("____________________________________________________________");
+    }
+
+    //prints the intro text
+    private static void printIntroText() {
+        printIndentation();
+        System.out.println(" Hello! I'm Duke");
+        printIndentation();
+        System.out.println(" What can I do for you?");
+    }
+
+    private static void printIntro() {
+        printLine();
+        printIntroText();
+        printLine();
+        System.out.println();
+    }
+
+    private static void printExitMsg() {
+        printLine();
+        printIndentation();
+        System.out.println(" Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    private static void printResponse(String response) {
+        printLine();
+        printIndentation();
+        System.out.println(" " + response);
+        printLine();
+        System.out.println();
     }
 }

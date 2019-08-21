84145e3b27007ef95ed5a882e3ee79abfa1e2f87
arjavibahety <arjavibahety@gmail.com>
2019-08-21 13:43:17 +0800

    completed level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -22,20 +22,25 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String echo = "";
         int counter = 0;
-        String[] list = new String[100];
+        Task[] list = new Task[100];
 
         if (sc.hasNextLine()) {
             echo = sc.nextLine();
-
             while (!echo.equals("bye")) {
+                String[] echoArr = echo.split(" ");
                 System.out.println(line);
                 if(echo.equals("list")) {
+                    System.out.println("    Here are the tasks in your list:");
                     for(int i = 0; i < counter; i++) {
                         System.out.println("    " + (i + 1) + ". " + list[i]);
                     }
+                } else if(echoArr[0].equals("done")) {
+                    list[Integer.parseInt(echoArr[1]) - 1].markAsDone();
+                    System.out.println("    Nice! I've marked this task as done: ");
+                    System.out.println("     " + list[Integer.parseInt(echoArr[1]) - 1]);
                 } else {
                     System.out.println("    added: " + echo);
-                    list[counter] = echo;
+                    list[counter] = new Task(echo);
                     counter++;
                 }
                 System.out.println(line);

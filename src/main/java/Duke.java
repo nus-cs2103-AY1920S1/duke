77ea7863eb152ea8b90340fe675503dfba1179d2
blimyj <blimyj21@gmail.com>
2019-08-21 14:34:42 +0800

    Completed level 1 w/o proper formatting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String dividerLine = new String("\u2500").repeat(80).concat("\n");
+        String startMessage = "  Hello! I'm Duke\n"
+                + "  What can I do for you?\n";
+        String logo = "   ____        _        \n"
+                + "  |  _ \\ _   _| | _____ \n"
+                + "  | | | | | | | |/ / _ \\\n"
+                + "  | |_| | |_| |   <  __/\n"
+                + "  |____/ \\__,_|_|\\_\\___|\n"
+                + startMessage
+                + dividerLine;
+        System.out.print(dividerLine + "  Hello from\n" + logo);
+
+        //Take in input
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        //Check if bye
+        while (!input.equals("bye")){
+            // Else echo input
+            System.out.print(dividerLine
+                    + "  "
+                    + input
+                    + "\n"
+                    + dividerLine);
+
+            input = sc.nextLine();
+        }
+        // Exit if bye
+        System.out.println(dividerLine
+                + "  Bye. Hope to see you again soon!\n"
+                + dividerLine);
+
     }
 }

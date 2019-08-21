d8acea0328f85a57880e301e048f017260a01439
limhawjia <hawjiaa@gmail.com>
2019-08-22 01:58:38 +0800

    Fixed some indentation issues

--- src/main/java/util/DukeOutput.java
+++ src/main/java/util/DukeOutput.java
@@ -1,10 +1,10 @@
 package util;
 
 public class DukeOutput {
-    private static String HORIZONTAL_DIVIDER = "\t____________________________________________________________\n";
+    private static String HORIZONTAL_DIVIDER = "    ____________________________________________________________\n";
 
     public static void printMessage(String message) {
-        String output = HORIZONTAL_DIVIDER + message.replaceAll("(?m)^", "\t ") +
+        String output = HORIZONTAL_DIVIDER + message.replaceAll("(?m)^", "     ") +
                 "\n" + HORIZONTAL_DIVIDER;
 
         System.out.println(output);

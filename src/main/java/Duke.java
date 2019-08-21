c9939aa551ec50a1fb0b80fa2167b9e90a32f5c2
MichaelaTSH <michaelatang99@gmail.com>
2019-08-21 15:25:55 +0800

    Add ToDos, Events, and Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -8,29 +7,39 @@ public class Duke {
 
     public static void main(String[] args) {
         intro();
-        array = new Task[100];
-        count = 0;
         Scanner sc = new Scanner(System.in);
+
         while (sc.hasNext()) {
-            String s = sc.next();
+            String s = sc.next().toLowerCase();
+
             if (s.equals("done")) {
-                int task = sc.nextInt() - 1;
-                markAsDone(task);
-                Task t = array[task];
-                System.out.println(line + "\n Nice! I've marked this task as done: \n   [" + t.getStatusIcon() +
-                        "] " + t.description + "\n" + line);
-            } else {
-                s += sc.nextLine();
-                if (s.toLowerCase().equals("bye")) {
+                markAsDone(sc.nextInt() - 1);
+            }
+
+            else if (s.equals("todo") || s.equals("deadline") || s.equals("event")) {
+                process(s, sc.nextLine().trim());
+            }
+
+            else if (s.toLowerCase().equals("list")) {
+                System.out.println(line);
+                for (int i = 1; i <= count; i++) {
+                    System.out.println(" " + i + ". " + array[i - 1].toString());
+                }
+                System.out.println(line);
+            }
+
+            else {
+                if (s.equals("bye")) {
                     System.out.println(line + "\n Bye. Hope to see you again soon!\n" + line);
                     break;
                 }
-                process(s);
             }
         }
     }
 
     public static void intro() {
+        array = new Task[100];
+        count = 0;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -39,21 +48,30 @@ public class Duke {
         System.out.println(line + "\n" + logo + "\n Hello! I'm Duke\n What can I do for you?\n" + line);
     }
 
-    public static void process(String s) {
-        if (s.toLowerCase().equals("list")) {
-            System.out.println(line);
-            for (int i = 1; i <= count; i++) {
-                System.out.println(" " + i + ". [" + array[i - 1].getStatusIcon() + "] " + array[i - 1].description);
-            }
-            System.out.println(line);
-        } else {
-            array[count] = new Task(s);
-            count += 1;
-            System.out.println(line + "\n added: " + s + "\n" + line);
+    public static void process(String s, String des) {
+        Task task = new Task("null");
+        if (s.equals("todo")) {
+            task = new ToDo(des);
+        } else if (s.equals("deadline")) {
+            String[] arr = des.split("/by");
+            task = new Deadline(arr[0].trim(), arr[1].trim());
+        } else if (s.equals("event")) {
+            String[] arr = des.split("/at");
+            task = new Event(arr[0].trim(), arr[1].trim());
         }
+        array[count] = task;
+        count += 1;
+        printTask(task);
+    }
+
+    public static void printTask(Task t) {
+        System.out.println(line + "\n Got it. I've added this task: \n  " +
+                t.toString() + "\n Now you have " + count + " tasks in the list.\n" + line);
     }
 
     public static void markAsDone(int i) {
         array[i].done();
+        Task t = array[i];
+        System.out.println(line + "\n Nice! I've marked this task as done: \n " + t.toString() + "\n" + line);
     }
 }

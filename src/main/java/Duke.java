5ea9a650c51eea826763d9cd775d4a756f5bd552
brianyenna <brianyen.na@gmail.com>
2019-08-21 10:50:13 +0800

    Refactored the code to have initDuke() and terminateDuke() commands.
    Purpose of this is separation of concerns: different functions deal with different parts of the Duke code.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke d = new Duke();
+
+        d.initDuke();
+        d.terminateDuke();
+
+    }
+
+    public void initDuke() {
+        System.out.println("\t____________________________________________________________\n"
+                          +"\tHello! I'm Duke\n"
+                          +"\tWhat can I do for you?\n"
+                          +"\t____________________________________________________________\n");
+
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+    }
+
+    public void terminateDuke() {
+        System.out.println("\t____________________________________________________________\n"
+                          +"\tBye. Hope to see you again soon!\n"
+                          +"\t____________________________________________________________\n");
     }
 }

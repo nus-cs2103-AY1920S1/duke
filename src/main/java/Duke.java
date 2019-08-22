90edb764a4743859adfcf75f703616a8960fdb59
Francis <francislow_6@hotmail.com>
2019-08-22 17:14:39 +0800

    Add mark done function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        ArrayList<String> userinputs = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
         // Greet user
@@ -13,18 +13,31 @@ public class Duke {
         String userinput = scanner.nextLine();
 
         while (!userinput.equals("bye")) {
+            // List
             if (userinput.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 // Output current items in list
-                for (int i = 0; i < userinputs.size(); i++) {
+                for (int i = 0; i < tasks.size(); i++) {
                     int currentItemNumber = i + 1;
-                    String currentUserinput = userinputs.get(i);
-                    System.out.println(currentItemNumber + ". " + currentUserinput);
+                    Task currentTask = tasks.get(i);
+                    System.out.println(currentItemNumber + ".[" + currentTask.getStatusIcon() + "] " + currentTask.getName());
                 }
             }
             else {
-                // Add user input to list and output
-                userinputs.add(userinput);
-                System.out.println("added: " + userinput);
+                // Done
+                String[] words = userinput.split(" ");
+                if (words[0].equals("done")) {
+                    int itemId = Integer.parseInt(words[1]);
+                    Task currentTask = tasks.get(itemId - 1);
+                    currentTask.setDone(true);
+                    System.out.println("Nice! I've marked this task as done:\n[✓] " + currentTask.getName());
+                }
+                // Add
+                else {
+                    // Add user input to list and output
+                    tasks.add(new Task(userinput, false));
+                    System.out.println("added: " + userinput);
+                }
             }
             // Ask for next userinput
             userinput = scanner.nextLine();

f1bcc0396c9aacfad2b9a6b22ff310ac5483a94a
ReignOfComputer <roc@reignofcomputer.com>
2019-08-22 12:58:47 +0800

    W2 - Level-2
    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> repeatList = new ArrayList();
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
@@ -15,8 +17,13 @@ public class Duke {
             if (repeatStr.equals("bye")) {
                 byeHit = true;
                 System.out.println("Bye. Hope to see you again soon!");
+            } else if (repeatStr.equals("list")) {
+                for (int i = 1; i <= repeatList.size(); i++) {
+                    System.out.println(i + ". " + repeatList.get(i - 1));
+                }
             } else {
-                System.out.println(repeatStr);
+                repeatList.add(repeatStr);
+                System.out.println("added: " + repeatStr);
             }
         }
     }

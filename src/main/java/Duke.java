b6efa46599da7b444f0c878f97f546096577968d
cheeyou <youchee1999@gmail.com>
2019-08-22 23:26:05 +0800

    added basic Duke echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,37 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println();
+
+
+        while(reader.hasNext()) {
+            String inputLine = reader.next();
+            StringBuilder lineBuilder = new StringBuilder("     ");
+            for(int i = 0; i < 59; i++) {
+                lineBuilder.append("_");
+            }
+            if(!inputLine.equals("bye")) {
+                StringBuilder temp = new StringBuilder("     ");
+                temp.append(inputLine);
+                System.out.println(lineBuilder.toString());
+                System.out.println(temp.toString());
+                System.out.println(lineBuilder.toString());
+            } else {
+                System.out.println(lineBuilder.toString());
+                System.out.println("     Bye. Hope to see you again soon!");
+                System.out.println(lineBuilder.toString());
+                break;
+            }
+        }
     }
 }

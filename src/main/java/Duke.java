672ef283beab2f86db9d421c45ee17f937636a9c
cherylnqj <cheryl.nqj@gmail.com>
2019-08-22 21:29:28 +0800

    Support adding todos

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,9 +12,10 @@ public class Duke {
         printIndentedString("Hello! I'm Duke\n" +
                 indent + " " + "What can I do for you?", indent);
         String input = sc.nextLine();
+        String[] inputArr = input.split(" ");
 
-        while (!input.equals("bye")) {
-            if (input.equals("list")) {
+        while (!inputArr[0].equals("bye")) {
+            if (inputArr[0].equals("list")) {
                 listString = "";
                 for (int i = 0; i < listSize; i++) {
                     listString += listArr[i];
@@ -23,18 +24,25 @@ public class Duke {
                     }
                 }
                 printIndentedString(listString, indent);
-            } else if(input.split(" ")[0].equals("done")){
-                listPointer = Integer.parseInt(input.split(" ")[1]);
+            } else if(inputArr[0].equals("done")){
+                listPointer = Integer.parseInt(inputArr[1]);
                 listArr[listPointer - 1].markAsDone();
                 printIndentedString("Nice! I've marked this task as done: \n"
                         + indent + "   " + listArr[listPointer - 1].getStatusIcon() +
                         ' ' + listArr[listPointer - 1].getDescription(), indent);
             } else {
-                listArr[listSize] = new Task(input, (listSize + 1));
-                listSize++;
-                printIndentedString("added: " + input, indent);
+                String befTaskAddMessage = "Got it. I've added this task: \n" + indent + "   ";
+                String aftTaskAddMessage = "Now you have " + (listSize + 1) + " tasks in the list.";
+                if(inputArr[0].equals("todo")) {
+                    listArr[listSize] = new Task(input, (listSize + 1), "todo");
+                    printIndentedString(befTaskAddMessage + listArr[listSize] + "\n " + indent + aftTaskAddMessage, indent);
+                    listSize++;
+                } else if(inputArr[0].equals("deadline")) {
+
+                }
             }
             input = sc.nextLine();
+            inputArr = input.split(" ");
         }
         printIndentedString("Bye. Hope to see you again soon!", indent);
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,23 @@
 public class Task {
     protected String description;
     protected int listIndex;
+    protected String taskType;
     protected boolean isDone;
+    protected String dateTime;
 
-    public Task(String description, int listIndex) {
+    public Task(String description, int listIndex, String taskType) {
         this.description = description;
         this.listIndex = listIndex;
+        this.taskType = taskType;
         this.isDone = false;
+    }
+
+    public Task(String description, int listIndex, String taskType, String dateTime) {
+        this.description = description;
+        this.listIndex = listIndex;
+        this.taskType = taskType;
+        this.isDone = false;
+        this.dateTime = dateTime;
     }
 
     public String getStatusIcon() {
@@ -21,12 +32,28 @@ public class Task {
         return listIndex;
     }
 
+    public String getTaskType() {
+        if(taskType.equals("todo")) {
+            return "[T]";
+        } else if(taskType.equals("deadline")) {
+            return "[D]";
+        } else if(taskType.equals("event")) {
+            return "[E]";
+        } else {
+            return "";
+        }
+    }
+
     public void markAsDone() {
         this.isDone = true;
     }
 
+    public String toStringWithIndex() {
+        return getListIndex() +  ". " + getTaskType() + " " + getStatusIcon() + " " + getDescription();
+    }
+
     @Override
     public String toString() {
-        return getListIndex() +  ". " + getStatusIcon() + " " + getDescription();
+        return getTaskType() + " " + getStatusIcon() + " " + getDescription();
     }
 }

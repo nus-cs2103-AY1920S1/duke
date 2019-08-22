939dd6f7302de2cdcb16f88dd73ade838e057370
etlow <42794246+etlow@users.noreply.github.com>
2019-08-22 10:53:47 +0800

    Add printing of user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,7 @@
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +10,27 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        printMessage(List.of("Hello! I'm Duke", "What can I do for you?"));
+
+        Scanner input = new Scanner(System.in);
+        boolean end = false;
+        while (!end && input.hasNext()) {
+            String command = input.nextLine();
+            if (command.equals("bye")) {
+                end = true;
+                printMessage(List.of("Bye. Hope to see you again soon!"));
+            } else {
+                printMessage(List.of(command));
+            }
+        }
+    }
+
+    private static void printMessage(List<String> messages) {
+        System.out.println("    ____________________________________________________________");
+        for (String message : messages) {
+            System.out.println("     " + message);
+        }
+        System.out.println("    ____________________________________________________________\n");
     }
 }

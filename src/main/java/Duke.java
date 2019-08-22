98636a9d68bd470479cdcc74a5c3f5b3e8929d3e
shawnkoh <shawnkoh@me.com>
2019-08-22 23:24:34 +0800

    Update the commands to support the new task types

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,9 +2,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private static ArrayList<Task> tasks;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<Task> tasks = new ArrayList<>();
+        tasks = new ArrayList<>();
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -18,10 +20,32 @@ public class Duke {
         boolean readingCommands = true;
         while (readingCommands) {
             String input = sc.nextLine();
-            String[] inputWords = input.split(" ");
+            String[] inputWords = input.split(" ", 2);
             String command = inputWords[0];
 
             switch (command) {
+                case "todo": {
+                    String description = inputWords[1];
+                    addTask(new ToDo(description));
+                    break;
+                }
+
+                case "deadline": {
+                    String[] params = inputWords[1].split(" /by ");
+                    String description = params[0];
+                    String by = params[1];
+                    addTask(new Deadline(description, by));
+                    break;
+                }
+
+                case "event": {
+                    String[] params = inputWords[1].split(" /at ");
+                    String description = params[0];
+                    String at = params[1];
+                    addTask(new Event(description, at));
+                    break;
+                }
+
                 case "list": {
                     for (int i = 1; i <= tasks.size(); i++) {
                         Task task = tasks.get(i - 1);
@@ -53,6 +77,17 @@ public class Duke {
             }
         }
         sc.close();
+    }
+
+    private static void addTask(Task task) {
+        tasks.add(task);
+        System.out.println("Got it. I've added this task:");
+        System.out.println(task);
+        if (tasks.size() == 1) {
+            System.out.println("Now you have 1 task in the list.");
+        } else {
+            System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+        }
     }
 }
 

caeedc925540c6a07be653850c94c86a68fc09b6
cherylnqj <cheryl.nqj@gmail.com>
2019-08-22 13:54:59 +0800

    Create and add to list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,14 +4,26 @@ public class Duke {
     public static void main(String[] args) {
         String indent = "    ";
         Scanner sc = new Scanner(System.in);
+        String[] listArr = new String[100]; // max list length: 100
+        int listIndex = 1;
+        String listString;
 
         printIndentedString("Hello! I'm Duke\n" +
                 indent + " " + "What can I do for you?", indent);
-
         String input = sc.nextLine();
 
         while (!input.equals("bye")) {
-            printIndentedString(input, indent);
+            if(input.equals("list")) {
+                listString = "";
+                for(int i = 0; i < (listIndex - 1); i++) {
+                    listString += listArr[i];
+                }
+                printIndentedString(listString, indent);
+            } else {
+                listArr[listIndex - 1] = listIndex + ". " + input;
+                listIndex++;
+                printIndentedString("added: " + input, indent);
+            }
             input = sc.nextLine();
         }
         printIndentedString("Bye. Hope to see you again soon!", indent);

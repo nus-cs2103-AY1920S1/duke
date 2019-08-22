9c29adcb6d1bb75a820f3ec50304181951fd6338
Wang Tianyi <e0322921@u.nus.edu>
2019-08-22 12:29:37 +0800

    modify Bot.java, Duke.java

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,17 +1,35 @@
-public class Bot {
-    public String response;
+import java.util.ArrayList;
+import java.util.List;
 
-    public void echo(String s) {
-        this.response = s;
+public class Bot {
+    private String response;
+    private List<String> tasks = new ArrayList<String>();
+
+    public void add(String s) {
+        this.response = "added: " + s;
+        tasks.add(s);
+        System.out.println(this);
     }
 
     public void exit() {
         this.response = "Bye. Hope to see you again soon!";
+        System.out.println(this);
     }
 
     public void greet() {
         this.response = "Hello! I'm Duke\n" +
                 "     What can I do for you?";
+        System.out.println(this);
+    }
+
+    public void list() {
+        String taskList = "";
+        for (int i = 0; i < this.tasks.size(); i++) {
+            taskList += (i + 1) + ". " + tasks.get(i) ;
+            if (i != this.tasks.size() - 1) taskList += "\n     ";
+        }
+        this.response = taskList;
+        System.out.println(this);
     }
 
     public String toString() {
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,21 +4,20 @@ public class Duke {
     private static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-                Bot b = new Bot();
-                b.greet();
-                System.out.println(b);
+        Bot b = new Bot();
+        b.greet();
 
-                        while(true) {
-                        String command = sc.nextLine();
-                        if (command.equals("bye")) {
-                                b.exit();
-                                System.out.println(b);
-                                break;
-                            } else {
-                                b.echo(command);
-                                System.out.println(b);
-                            }
-                    }
+        while(true) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                b.exit();
+                break;
+            } else if (command.equals("list")){
+                b.list();
+            } else {
+                b.add(command);
+            }
+        }
 
     }
 }

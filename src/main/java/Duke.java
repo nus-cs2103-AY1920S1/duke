3ebf3d0d40fc7138a1bedadeb29edba126b095b9
Tianyi <42670338+tysng@users.noreply.github.com>
2019-08-22 10:02:42 +0800

    Add greeting, echoing message, and farewell message interaction

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+    public void run() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greetings = "Hello! I'm Duke\n" + "What can I do for you?";
+        String farewell = "Bye. Hope to see you again soon!";
+
+        Scanner sc = new Scanner(System.in);
+
+        // start interaction
+        this.handleOutput(greetings);
+        while (sc.hasNext()) {
+            String command = sc.nextLine();
+            switch (command) {
+                case "bye": this.handleOutput(farewell);
+                return;
+                default: this.handleOutput(command);
+            }
+        }
+        sc.close();
+    }
+
+    private void handleOutput(String inputString) {
+        String divider = "____________________________________________________________";
+        System.out.println(divider);
+        System.out.println(inputString);
+        System.out.println(divider);
+        System.out.println();
     }
 }

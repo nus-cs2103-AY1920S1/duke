5361b1e49587836ec9151cf257c36e5ef4dc2fde
PHILIP ALEXANDER BOEDIMAN <philipsenang@gmail.com>
2019-08-22 14:13:59 +0800

    Level 5. Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,6 +10,15 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
+        Duke duke = new Duke();
+        try {
+            duke.run();
+        } catch (DukeException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void run() throws DukeException {
         Scanner sc = new Scanner(System.in);
         ArrayList<Task> list = new ArrayList<Task>();
 
@@ -32,25 +41,33 @@ public class Duke {
                 currTask.markAsDone();
                 System.out.println("Nice! I've marked this task as done:" + "\n" + "[\u2713] " + currTask.getDescription());
             } else if (str.substring(0, 4).equals("todo")) {
-                String message = "Got it. I've added this task:";
-                Task toDoTask = new Task(str.substring(5));
-                list.add(toDoTask);
-                toDoTask.setTypeOfTask("todo");
-                System.out.println(message);
-                System.out.println(" " + toDoTask);
-                System.out.println("Now you have " + list.size() + " tasks in the list");
+                if (str.length() == 4) {
+                    throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
+                } else {
+                    String message = "Got it. I've added this task:";
+                    Task toDoTask = new Task(str.substring(5));
+                    list.add(toDoTask);
+                    toDoTask.setTypeOfTask("todo");
+                    System.out.println(message);
+                    System.out.println(" " + toDoTask);
+                    System.out.println("Now you have " + list.size() + " tasks in the list");
+                }
             } else if (str.length() >= 8 && str.substring(0, 8).equals("deadline")) {
-                String message = "Got it. I've added this task:";
-                int index = str.indexOf('/') + 1;
-                Task deadlineTask = new Task(str.substring(9, index - 2));
-                list.add(deadlineTask);
-                deadlineTask.setTypeOfTask("deadline");
-                String preposition = str.substring(index, index + 2);
-                String time = "(" + preposition + ": " + str.substring(index + 3) + ")";
-                deadlineTask.addTime(time);
-                System.out.println(message);
-                System.out.println(" " + deadlineTask);
-                System.out.println("Now you have " + list.size() + " tasks in the list.");
+                if (str.length() == 8) {
+                    throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
+                } else {
+                    String message = "Got it. I've added this task:";
+                    int index = str.indexOf('/') + 1;
+                    Task deadlineTask = new Task(str.substring(9, index - 2));
+                    list.add(deadlineTask);
+                    deadlineTask.setTypeOfTask("deadline");
+                    String preposition = str.substring(index, index + 2);
+                    String time = "(" + preposition + ": " + str.substring(index + 3) + ")";
+                    deadlineTask.addTime(time);
+                    System.out.println(message);
+                    System.out.println(" " + deadlineTask);
+                    System.out.println("Now you have " + list.size() + " tasks in the list.");
+                }
             } else if (str.length() >= 5 && str.substring(0, 5).equals("event")) {
                 String message = "Got it. I've added this task:";
                 int index = str.indexOf('/') + 1;
@@ -63,6 +80,8 @@ public class Duke {
                 System.out.println(message);
                 System.out.println(" " + eventTask);
                 System.out.println("Now you have " + list.size() + " tasks in the list.");
+            } else if (str.equals("blah")) {
+                throw new DukeException("OOPS!!! I'm sorry, but i don't know what that means :-(");
             } else {
                 System.out.println("added: " + str);
                 list.add(new Task(str));
@@ -70,4 +89,12 @@ public class Duke {
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
+}
+
+class DukeException extends Exception {
+
+    public DukeException(String message) {
+        super(message);
+    }
+
 }

7ddbf859eaa53ba137b00f016b75ff84ddb28587
limhawjia <hawjiaa@gmail.com>
2019-08-22 16:36:57 +0800

    Added test for textUI

---
+++ text-ui-test/runtest.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+   # create bin directory if it doesn't exist
+   if [ ! -d "../bin" ]
+   then
+       mkdir ../bin
+   fi
+
+   # delete output from previous run if exists
+   if [ -e "./ACTUAL.TXT" ]
+   then
+       rm ACTUAL.TXT
+   fi
+
+   # compile the code into the bin folder, terminates if error occurred
+   if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/Duke.java
+   then
+       echo "********** BUILD FAILURE **********"
+       exit 1
+   fi
+
+   # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
+   java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+
+   # compare the output to the expected output and deletes output
+   diff ACTUAL.TXT EXPECTED.TXT
+   if [ $? -eq 0 ]
+   then
+       echo "Test result: PASSED"
+       rm ACTUAL.TXT
+       exit 0
+   else
+       echo "Test result: FAILED"
+       rm ACTUAL.TXT
+       exit 1
+   fi

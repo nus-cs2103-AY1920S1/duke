5af355b3b9656f514b7db11a990af0738e077e41
Wang Tianyi <e0322921@u.nus.edu>
2019-08-22 21:21:10 +0800

    add delete function

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -11,9 +11,9 @@ public class Bot {
         String taskDes = "";
         String taskTime = "";
 
-        for(int i = 1; i < arr.length; i++){
+        for(int i = 1; i < arr.length; i++){//get task description
             if(arr[i].length() >= 1 && arr[i].charAt(0) == '/') {
-                for(int j = i + 1; j < arr.length; j++) {
+                for(int j = i + 1; j < arr.length; j++) { //get task time
                     taskTime += " " + arr[j];
                 }
                 break;
@@ -23,6 +23,7 @@ public class Bot {
         }
         taskDes = taskDes.trim();
         taskTime = taskTime.trim();
+        //handle exceptions
         if (!(taskType.equals("todo") || taskType.equals("deadline") || taskType.equals("event"))) {
             throw new DukeException("    ____________________________________________________________\n     " +
                     "\u2639" + " OOPS!!! I'm sorry, but I don't know what that means :-(" +
@@ -38,6 +39,7 @@ public class Bot {
                 "\u2639" + " OOPS!!! The time of a " + taskType + " cannot be empty." +
                 "\n    ____________________________________________________________\n");
 
+        //creat task and add to tasks list
         Task task;
         if (taskType.equals("todo")) {
             task = new ToDo(taskDes);
@@ -50,35 +52,47 @@ public class Bot {
             tasks.add(task);
         } else {return;};
 
+        //update response
         this.response = "Got it. I've added this task:\n       " + task
                 + "\n     Now you have " + tasks.size() + " tasks in the list.";
         System.out.println(this);
     }
 
     public void exit() {
+        //update response
         this.response = "Bye. Hope to see you again soon!";
         System.out.println(this);
     }
 
     public void greet() {
+        //update response
         this.response = "Hello! I'm Duke\n" +
                 "     What can I do for you?";
         System.out.println(this);
     }
 
     public void list() {
+        //get the list of tasks in the arraylist
         String taskList = "Here are the tasks in your list:\n     ";
         for (int i = 0; i < this.tasks.size(); i++) {
             taskList += (i + 1) + "." + tasks.get(i) ;
             if (i != this.tasks.size() - 1) taskList += "\n     ";
         }
-        this.response = taskList;
+        this.response = taskList; //update response
         System.out.println(this);
     }
 
     public void done(int n) {
-        this.tasks.get(n - 1).mark();
-        this.response = "Nice! I've marked this task as done:\n       " + this.tasks.get(n - 1);
+        this.tasks.get(n - 1).mark(); //mark the corresponding task as done;
+        this.response = "Nice! I've marked this task as done:\n       " + this.tasks.get(n - 1); //update response
+        System.out.println(this);
+    }
+
+    public void delete (int n) {
+        Task task = this.tasks.get(n - 1);
+        this.tasks.remove(task); //delete task from list
+        this.response = "Noted. I've removed this task: \n       " + task
+                + "\n     Now you have " + tasks.size() + " tasks in the list."; //update response
         System.out.println(this);
     }
 
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,7 +8,7 @@ public class Duke {
         b.greet();
 
         while(true) {
-            String command = sc.nextLine();
+            String command = sc.nextLine();//read user input
             if (command.equals("bye")) {
                 b.exit();
                 break;
@@ -16,6 +16,8 @@ public class Duke {
                 b.list();
             } else if (command.length() >= 4 && command.substring(0,4).equals("done")){
                 b.done(Integer.valueOf(command.substring(5)));
+            } else if (command.length() >= 6 && command.substring(0,6).equals("delete")){
+                b.delete(Integer.valueOf(command.substring(7)));
             } else {
                 try{b.add(command);} catch (DukeException e){System.out.println(e.getMessage());};
             }

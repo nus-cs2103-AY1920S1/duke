ce3c6a2bb6f06853097e2ae07930d53c6bc53292
ReignOfComputer <roc@reignofcomputer.com>
2019-08-22 13:13:17 +0800

    W2 - Level-3 | A-Classes
    Mark as Done
    Use a class to represent tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> repeatList = new ArrayList();
+        ArrayList<Task> repeatList = new ArrayList();
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
@@ -18,11 +20,17 @@ public class Duke {
                 byeHit = true;
                 System.out.println("Bye. Hope to see you again soon!");
             } else if (repeatStr.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 1; i <= repeatList.size(); i++) {
                     System.out.println(i + ". " + repeatList.get(i - 1));
                 }
+            } else if (repeatStr.contains("done")) {
+                int target = parseInt(repeatStr.replaceAll("\\D+","")) - 1;
+                repeatList.get(target).done();
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println("  " + repeatList.get(target));
             } else {
-                repeatList.add(repeatStr);
+                repeatList.add(new Task(repeatStr));
                 System.out.println("added: " + repeatStr);
             }
         }

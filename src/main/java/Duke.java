bb6961d0f1b664f8b5b8cb9959b88c5e93bca9f0
Mo Zongran <mozongran@gmail.com>
2019-08-22 11:54:45 +0800

    Level 5 commit: minimal level

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,18 +11,26 @@ public class Duke {
                 "    What can I do for you?");
 
         while (true) {
-            input = sc.next().trim();
-            if (input.equals("bye"))
-                break;
-            else if (input.contains("done")) {
-                int index = Integer.parseInt(sc.next());
-                printFormattedText(todo.markTaskDone(index));
-            }
-            else if (input.equals("list"))
-                printFormattedText(todo.displayList());
-            else {
-                String remainingInput = sc.nextLine();
-                printFormattedText(todo.addTask(input, remainingInput));
+            try {
+                input = sc.next().trim();
+                if (input.equals("bye"))
+                    break;
+                else if (input.contains("done")) {
+                    int index = Integer.parseInt(sc.next());
+                    printFormattedText(todo.markTaskDone(index));
+                } else if (input.equals("list"))
+                    printFormattedText(todo.displayList());
+                else if (input.equals("todo")
+                        || input.equals("deadline")
+                        || input.equals("event")) {
+                    String remainingInput = sc.nextLine();
+                    if (remainingInput.isBlank()) {
+                        throw new DukeException("     \u2639 OOPS!!! The description of a " + input + " cannot be empty");
+                    }
+                    printFormattedText(todo.addTask(input, remainingInput));
+                } else throw new DukeException("     \u2639 OOPS!!! I'm sorry, but I don't know what that means :-(");
+            } catch (DukeException e) {
+            printFormattedText(e.getMessage());
             }
         }
 

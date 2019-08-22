3ac3da48df7743ac56a6bfd00abd5d28468f3178
bakwxh <omega1653@gmail.com>
2019-08-22 16:03:09 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+	static ArrayList<String> memory;
+	static String gap = "    ";
+	static String line = "____________________________________________________________";
 	
 	public static void main(String[] args) {
-		String gap = "    ";
-		String line = "____________________________________________________________";
+		memory = new ArrayList<>();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -16,22 +19,40 @@ public class Duke {
 		System.out.println(gap + " Hello! I'm Duke");
 		System.out.println(gap + " What can I do for you?");
 		System.out.println(gap + line);
+		System.out.println();
 		
 		Scanner sc = new Scanner(System.in);
 		while (sc.hasNext()) {
-			String command = sc.nextLine();
-			if (!command.toLowerCase().equals("bye")) {
-				System.out.println(gap + line);
-				System.out.println(gap + " " + command);
-				System.out.println(gap + line);
-			} else {
+			String rawInput = sc.nextLine();
+			
+			if (rawInput.toLowerCase().equals("bye")) {
 				System.out.println(gap + line);
 				System.out.println(gap + " " + "Bye. Hope to see you again soon!");
 				System.out.println(gap + line);
+				System.out.println();
 				break;
+			} else if (rawInput.toLowerCase().equals("list")) {
+				System.out.println(gap + line);
+				list();
+				System.out.println(gap + line);
+				System.out.println();
+			} else {
+				System.out.println(gap + line);
+				System.out.println(gap + " " + "added: " + rawInput);
+				System.out.println(gap + line);
+				System.out.println();
+				memory.add(rawInput);
 			}
 		}
 		
 		sc.close();
+	}
+	
+	private static void list() {
+		int counter = 1;
+		for (String s : memory) {
+			System.out.println(gap + " " + counter + ". " + s);
+			counter++;
+		}
 	}
 }

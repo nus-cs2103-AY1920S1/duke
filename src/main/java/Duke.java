21e9268bced532c7fd11b6306f97a43c05ad6099
Eshwar Kamalapathy <eshwareverlast@gmail.com>
2019-08-22 17:05:23 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] textArr = new String[100];
+        int count = 0;
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
@@ -12,8 +14,14 @@ public class Duke {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    int index = i + 1;
+                    System.out.println(index + ". " + textArr[i]);
+                }
             } else {
-                System.out.println(input);
+                textArr[count] = input;
+                count++;
             }
             input = sc.nextLine();
         }

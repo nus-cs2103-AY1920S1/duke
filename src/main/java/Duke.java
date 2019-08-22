5c92d361cff08a202610626688a833dd263a7015
chen-xi-cx <chenxi_95@hotmail.com>
2019-08-22 08:09:33 +0800

    Add Level-2 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,36 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private ArrayList<String> myList;
+
     public Duke() {
+        myList = new ArrayList<String>();
     }
 
     private void run() {
-        String input, response;
         boolean canEnd;
 
         Scanner myScanner = new Scanner(System.in);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         canEnd = false;
         while (!canEnd) {
+            String input, response = "";
             input = myScanner.nextLine();
             if (input.equals("bye")) {
                 response = "Bye. Hope to see you again soon!";
+                System.out.println(response);
                 canEnd = true;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < myList.size(); i = i + 1) {
+                    int number = i + 1;
+                    System.out.println(number + ". " + myList.get(i));
+                }
             } else {
-                response = input;
+                myList.add(input);
+                response = "added: " + input;
+                System.out.println(response);
             }
-            System.out.println(response);
         }
     }
 

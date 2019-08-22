bea8009beaf0e3221a64e1477e20252f4788080f
Joshua Seet <joshu5797@gmail.com>
2019-08-22 15:16:50 +0800

    Level 6- User now can delete tasks off the tasks list, using the "delete" keyword.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -82,6 +82,11 @@ try {
                 }
                 break;
 
+            case "delete":
+                int taskToBeDeleted = sc.nextInt() -1;
+                deleteTask(taskToBeDeleted, taskList);
+                break;
+
             case "done":
                 int taskNumber = sc.nextInt() - 1;
                 completeTask(taskNumber, taskList);
@@ -148,6 +153,19 @@ try {
                     "Now you have " + list.size() + " task in the list. ");
         }
     }
+
+    public static void deleteTask(int current, ArrayList<Task> list) {
+        Task deleted = list.get(current);
+        list.remove(current);
+        if(list.size() > 1) {
+            System.out.println("Noted. I've removed this task: \n" + "   " + deleted.toString() + "\n" +
+                    "Now you have " + list.size() + " tasks in the list. ");
+        } else {
+            System.out.println("Noted. I've removed this task: \n" + "   " + deleted.toString() + "\n" +
+                    "Now you have " + list.size() + " task in the list. ");
+        }
+    }
+
 
 
 }

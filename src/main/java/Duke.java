6b80af71cc2de176e5525ce023bb951381cc1690
Xiaodong <xdjiaxd@gmail.com>
2019-08-22 08:50:37 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,16 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String in;
+        while (true){
+            in = sc.nextLine().trim();
+            if (in.equals("bye")) {
+                System.out.println("Bye!");
+                break;
+            } else {
+                System.out.println(in); 
+            }
+        }
     }
 }

c74ffaba150847e3658b9140b0d4591adaa1cc5f
john0227 <37623568+john0227@users.noreply.github.com>
2019-08-22 21:51:50 +0800

    Updated error handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -60,64 +60,73 @@ public class Duke {
                 } else if (command.startsWith("done")) {
                     // Mark Task at index specified as done
                     try {
-                        int index = Integer.parseInt(command.substring(5));
+                        int index = Integer.parseInt(command.substring(5).trim());
                         taskList.markAsDoneTaskAt(index);
                         System.out.println(DUKE_TAB4 + DUKE_MARK_AS_DONE
                                 + DUKE_TAB4 + DUKE_TAB2
                                 + taskList.getTaskAt(index).getStatus());
                     } catch (StringIndexOutOfBoundsException e) {
                         throw new DukeIllegalArgumentException("☹ OOPS!!! You must include an index.");
+                    } catch (NumberFormatException e) {
+                        throw new DukeIllegalIndexException("☹ OOPS!!! The index must be a number.");
                     }
                 } else if (command.startsWith("delete")) {
                     try {
-                        int index = Integer.parseInt(command.substring(7));
+                        int index = Integer.parseInt(command.substring(7).trim());
                         System.out.println(DUKE_TAB4 + DUKE_DELETE_TASK
                                 + DUKE_TAB4 + DUKE_TAB2 + taskList.deleteTaskAt(index).getStatus()
                                 + String.format("\n" + DUKE_TAB4 + DUKE_NUMBER_OF_TASKS,
                                 taskList.getSize()));
                     } catch (StringIndexOutOfBoundsException e) {
                         throw new DukeIllegalArgumentException("☹ OOPS!!! You must include an index.");
+                    } catch (NumberFormatException e) {
+                        throw new DukeIllegalIndexException("☹ OOPS!!! The index must be a number.");
                     }
                 } else if (command.startsWith("todo")) {
                     try {
-                        command = command.substring(5);
+                        command = command.substring(5).trim();
+                        if (command.length() == 0) {
+                            throw new DukeIllegalArgumentException("☹ OOPS!!! The description of a todo "
+                                                                   + "cannot be empty.");
+                        }
                     } catch (StringIndexOutOfBoundsException e) {
                         throw new DukeIllegalArgumentException("☹ OOPS!!! The description of a todo cannot be empty.");
                     }
                     this.addTask(new TodoTask(command));
                 } else if (command.startsWith("deadline")) {
                     try {
-                        String[] cmd = command.substring(9).split(" /by ");
-
+                        String[] cmd = command.substring(9).split("/by ", -1);
+                        cmd[0] = cmd[0].trim();
+                        if (cmd[0].length() == 0) {
+                            // If description of deadline is empty
+                            throw new DukeIllegalArgumentException("☹ OOPS!!! The description of a deadline "
+                                    + "cannot be empty.");
+                        }
                         if (cmd.length == 1) {
                             // If deadline task does not have a due date
                             throw new DukeIllegalArgumentException("☹ OOPS!!! Deadline dates must be "
                                                                    + "specified with /by.");
                         }
-                        if (cmd[0].trim().length() == 0) {
-                            // If description of deadline is empty
-                            throw new DukeIllegalArgumentException("☹ OOPS!!! The description of a deadline "
-                                    + "cannot be empty.");
-                        }
-                        this.addTask(new DeadlineTask(cmd[0], cmd[1]));
+                        this.addTask(new DeadlineTask(cmd[0], cmd[1].trim()));
                     } catch (StringIndexOutOfBoundsException e) {
                         throw new DukeIllegalArgumentException("☹ OOPS!!! The description of a deadline "
                                 + "cannot be empty.");
                     }
                 } else if (command.startsWith("event")) {
                     try {
-                        String[] cmd = command.substring(6).split(" /at ");
+                        String[] cmd = command.substring(6).split("/at ", -1);
+                        cmd[0] = cmd[0].trim();
+                        if (cmd[0].length() == 0) {
+                            // If description of deadline is empty
+                            throw new DukeIllegalArgumentException("☹ OOPS!!! The description of an event "
+                                    + "cannot be empty.");
+                        }
                         if (cmd.length == 1) {
                             // If deadline task does not have a due date
                             throw new DukeIllegalArgumentException("☹ OOPS!!! Event dates must be "
                                                                    + "specified with /at.");
                         }
-                        if (cmd[0].trim().length() == 0) {
-                            // If description of deadline is empty
-                            throw new DukeIllegalArgumentException("☹ OOPS!!! The description of an event "
-                                    + "cannot be empty.");
-                        }
-                        this.addTask(new EventTask(cmd[0], cmd[1]));
+                        this.addTask(new EventTask(cmd[0], cmd[1].trim()));
                     } catch (StringIndexOutOfBoundsException e) {
                         throw new DukeIllegalArgumentException("☹ OOPS!!! The description of an event "
                                 + "cannot be empty.");
@@ -128,14 +137,14 @@ public class Duke {
             } catch (DukeIllegalIndexException | DukeIllegalCommandException | DukeIllegalArgumentException e) {
                 System.out.println(DUKE_TAB4 + e);
             } finally {
-                System.out.print(DUKE_LINE);
+                System.out.println(DUKE_LINE);
             }
         }
 
         // Greet the user and quit program
-        System.out.println(DUKE_LINE
-                           + DUKE_TAB4 + DUKE_BYE
-                           + DUKE_LINE);
+        System.out.print(DUKE_LINE
+                         + DUKE_TAB4 + DUKE_BYE
+                         + DUKE_LINE);
     }
     
 }

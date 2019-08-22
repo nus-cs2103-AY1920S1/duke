9e4dadf23d4defee3bbc0a665b682bbf85e4a301
cheeyou <youchee1999@gmail.com>
2019-08-22 23:30:40 +0800

    added list to store messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,36 +1,50 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-
-
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
+        Scanner reader = new Scanner(System.in);
+        ArrayList<String> stringList = new ArrayList<>();
 
-        while(reader.hasNext()) {
-            String inputLine = reader.next();
-            StringBuilder lineBuilder = new StringBuilder("     ");
-            for(int i = 0; i < 59; i++) {
-                lineBuilder.append("_");
-            }
-            if(!inputLine.equals("bye")) {
-                StringBuilder temp = new StringBuilder("     ");
-                temp.append(inputLine);
-                System.out.println(lineBuilder.toString());
-                System.out.println(temp.toString());
-                System.out.println(lineBuilder.toString());
-            } else {
+        StringBuilder lineBuilder = new StringBuilder("     ");
+        for (int i = 0; i < 59; i++) {
+            lineBuilder.append("_");
+        }
+        while (reader.hasNext()) {
+            String inputLine = reader.nextLine();
+            if (inputLine.equals("bye")) {
                 System.out.println(lineBuilder.toString());
                 System.out.println("     Bye. Hope to see you again soon!");
                 System.out.println(lineBuilder.toString());
                 break;
+            } else if (inputLine.equals("list")) {
+                System.out.println(lineBuilder.toString());
+                if (stringList.isEmpty()) {
+                    System.out.println("Sorry, there are no tasks in the list");
+                } else {
+                    int counter = 1;
+                    for (String string : stringList) {
+                        System.out.println("     " + counter + ". " + string);
+                    }
+                }
+                System.out.println(lineBuilder.toString());
+            } else {  //all other commands
+                stringList.add(inputLine);
+                StringBuilder temp = new StringBuilder("     ");
+                temp.append("added: ");
+                temp.append(inputLine);
+                System.out.println(lineBuilder.toString());
+                System.out.println(temp.toString());
+                System.out.println(lineBuilder.toString());
+                System.out.println();
             }
         }
     }

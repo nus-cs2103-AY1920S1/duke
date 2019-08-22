5088ad286ea615031304a413a9e3fb1fee1c3f82
shawnkoh <shawnkoh@me.com>
2019-08-22 23:46:54 +0800

    Add execption handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,61 +19,65 @@ public class Duke {
 
         boolean readingCommands = true;
         while (readingCommands) {
-            String input = sc.nextLine();
-            String[] inputWords = input.split(" ", 2);
-            String command = inputWords[0];
+            try {
+                String input = sc.nextLine();
+                String[] inputWords = input.split(" ", 2);
+                String command = inputWords[0];
 
-            switch (command) {
-                case "todo": {
-                    String description = inputWords[1];
-                    addTask(new ToDo(description));
-                    break;
-                }
-
-                case "deadline": {
-                    String[] params = inputWords[1].split(" /by ");
-                    String description = params[0];
-                    String by = params[1];
-                    addTask(new Deadline(description, by));
-                    break;
-                }
-
-                case "event": {
-                    String[] params = inputWords[1].split(" /at ");
-                    String description = params[0];
-                    String at = params[1];
-                    addTask(new Event(description, at));
-                    break;
-                }
-
-                case "list": {
-                    for (int i = 1; i <= tasks.size(); i++) {
-                        Task task = tasks.get(i - 1);
-                        System.out.println(i + ". " + task);
+                switch (command) {
+                    case "todo": {
+                        String description = inputWords[1];
+                        addTask(new ToDo(description));
+                        break;
                     }
-                    break;
-                }
 
-                case "done": {
-                    int taskId = Integer.parseInt(inputWords[1]) - 1;
-                    Task task = tasks.get(taskId);
-                    task.markAsDone();
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(task);
-                    break;
-                }
+                    case "deadline": {
+                        String[] params = inputWords[1].split(" /by ");
+                        String description = params[0];
+                        String by = params[1];
+                        addTask(new Deadline(description, by));
+                        break;
+                    }
 
-                case "bye": {
-                    readingCommands = false;
-                    System.out.println("Bye. Hope to see you again soon!");
-                    break;
-                }
+                    case "event": {
+                        String[] params = inputWords[1].split(" /at ");
+                        String description = params[0];
+                        String at = params[1];
+                        addTask(new Event(description, at));
+                        break;
+                    }
 
-                default: {
-                    Task task = new Task(input);
-                    tasks.add(task);
-                    System.out.println("added: " + task.description);
+                    case "list": {
+                        for (int i = 1; i <= tasks.size(); i++) {
+                            Task task = tasks.get(i - 1);
+                            System.out.println(i + ". " + task);
+                        }
+                        break;
+                    }
+
+                    case "done": {
+                        int taskId = Integer.parseInt(inputWords[1]) - 1;
+                        Task task = tasks.get(taskId);
+                        task.markAsDone();
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println(task);
+                        break;
+                    }
+
+                    case "bye": {
+                        readingCommands = false;
+                        System.out.println("Bye. Hope to see you again soon!");
+                        break;
+                    }
+
+                    default: {
+                        throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    }
                 }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
             }
         }
         sc.close();

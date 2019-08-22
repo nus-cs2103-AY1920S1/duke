4829fbf79695e71f9194b1e370d543e286e6dae8
SakshiPradyumn <e0313722@u.nus.edu>
2019-08-22 12:56:42 +0800

    Modified Duke.java to echo what user enters

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo);*/
+
+        Scanner S = new Scanner(System.in);
+        System.out.println("    ____________________________________________________________\n" +
+                "     Hello! I'm Duke\n" +
+                "     What can I do for you?\n" +
+                "    ____________________________________________________________\n");
+        String inp = S.next();
+        System.out.println("    ____________________________________________________________\n    "+ inp +
+                "\n    ____________________________________________________________");
     }
 }

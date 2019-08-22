12ab0360b3063b0c58db14616a3b32b94b74961e
Tan Yuanhong <leotan.yh@gmail.com>
2019-08-22 17:05:34 +0800

    Finished level 2.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -12,13 +14,23 @@ public class Duke {
                 "     What can I do for you?\n" +
                 "    ____________________________________________________________");
         Scanner sc = new Scanner(System.in);
+        List<String> data = new ArrayList<>();
         while (true) {
             String userInput = sc.nextLine();
             if (userInput.matches("bye")) {
                 printWithIndentation("Bye. Hope to see you again soon!");
                 break;
+            } else if (userInput.matches("list")) {
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < data.size(); i++) {
+                    if(i != 0)
+                        builder.append("\n" + "     ");
+                    builder.append(i+1).append(". ").append(data.get(i));
+                }
+                printWithIndentation(builder.toString());
             } else {
-                printWithIndentation(userInput);
+                data.add(userInput);
+                printWithIndentation("added: " + userInput);
             }
         }
     }

571dabb1e9204e73b4f6f7c6a65a72afdee53fa7
kerwin97 <kerwinlim@Kerwins-MacBook-Pro.local>
2019-08-22 23:30:55 +0800

    Level4 Code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -42,6 +42,75 @@ public class Duke {
             		System.out.println("Nice! I've marked this task as done: ");
             		System.out.println(t);
             		printLineS();
+            	} else if (command.substring(0, 4).equals("todo")) {
+            		String[] td = command.split(" ");
+            		String com = td[1];
+            		if (td.length > 1) {
+            			for (int i = 2; i < td.length; i ++) {
+	            			com = com + " " + td[i];
+	            		}           			
+            		} else {
+            			
+            		}
+            		cmd[count] = new Todo(com);
+            		printLine();
+            		System.out.println("Got it. I've added this task:");
+            		System.out.println(cmd[count]);
+            		count++;
+            		System.out.println("Now you have " + count + " tasks in the list.");
+            		printLineS(); 
+            	} else if (command.substring(0, 5).equals("event")) {
+            		String[] eve = command.split(" ");
+            		String com = "";
+            		String eventdate = "";
+            		for (int i = 1; i < eve.length; i ++) {
+            			if (eve[i].equals("/at")) {
+            				eventdate = eve[i+1];
+            				for (int j = i+2; j < eve.length; j ++) {
+            					eventdate = eventdate + " " + eve[j];
+            				}
+            				break;
+            			} else {
+            				if (com == "") {
+            					com = eve[i];
+            				} else {
+            					com = com + " " + eve[i];
+            				}
+            			}
+            		}
+            		cmd[count] = new Event(com, eventdate);
+            		printLine();
+            		System.out.println("Got it. I've added this task:");
+            		System.out.println(cmd[count]);
+            		count++;
+            		System.out.println("Now you have " + count + " tasks in the list.");
+            		printLineS(); 
+            	} else if (command.substring(0, 8).equals("deadline")) {
+            		String[] eve = command.split(" ");
+            		String com = "";
+            		String deadline = "";
+            		for (int i = 1; i < eve.length; i ++) {
+            			if (eve[i].equals("/by")) {
+            				deadline = eve[i+1];
+            				for (int j = i+2; j < eve.length; j ++) {
+            					deadline = deadline + " " + eve[j];
+            				}
+            				break;
+            			} else {
+            				if (com == "") {
+            					com = eve[i];
+            				} else {
+            					com = com + " " + eve[i];
+            				}
+            			}
+            		}
+            		cmd[count] = new Deadline(com, deadline);
+            		printLine();
+            		System.out.println("Got it. I've added this task:");
+            		System.out.println(cmd[count]);
+            		count++;
+            		System.out.println("Now you have " + count + " tasks in the list.");
+            		printLineS(); 
             	} else {
 	            	cmd[count] = new Task(command);
 	                printLine();

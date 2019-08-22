4019554c6a98536bcc0e61c5abb3501e4c63d026
envere <keithtxhh@gmail.com>
2019-08-22 17:48:43 +0800

    level 3

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,23 @@
-public class Task {
+class Task {
+
+    boolean isDone = false;
+    String taskName;
+
+    Task(String taskName) {
+        this.taskName = taskName;
+    }
+
+    void markAsDone() {
+        isDone = true;
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("  [✓] " + taskName);
+    }
+
+    @Override
+    public String toString() {
+        if (isDone) {
+            return "[✓] " + taskName;
+        }
+        return "[✗] " + taskName;
+    }
 }
--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 class ToDo {
-    private ArrayList<String> todoList = new ArrayList<>();
+    private ArrayList<Task> todoList = new ArrayList<>();
 
     ToDo() {
         System.out.println("Hello! I'm Duke");
@@ -13,13 +13,20 @@ class ToDo {
         Scanner sc = new Scanner(System.in);
         String nextCommand = sc.nextLine();
         while (!nextCommand.equals("bye")) {
-            if (nextCommand.equals("list")) {
+            String[] splitCommand = nextCommand.split(" ");
+            if (splitCommand.length == 1 && splitCommand[0].equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < todoList.size(); i++) {
-                    System.out.println(i + 1 + ". " + todoList.get(i));
+                    System.out.println(i + 1 + "." + todoList.get(i));
                 }
                 nextCommand = sc.nextLine();
+            } else if (splitCommand[0].equals("done")) {
+                int index = Integer.parseInt(splitCommand[1]) - 1;
+                todoList.get(index).markAsDone();
+                nextCommand = sc.nextLine();
             } else {
-                todoList.add(nextCommand);
+                Task newTask = new Task(nextCommand);
+                todoList.add(newTask);
                 System.out.println("added: " + nextCommand);
                 nextCommand = sc.nextLine();
             }

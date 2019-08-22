0d72825a9407bf62c50b661d0c3bba9ace7e277e
Liu Zechu <liuzechu2013@gmail.com>
2019-08-22 16:10:51 +0800

    Level-5: Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -52,58 +52,114 @@ public class Duke {
                 getUserInput();
                 break;
             case "done":
-                int index = Integer.parseInt(userInput.split("\\s")[1]);
-                Task taskDone = tasks.get(index - 1);
-                taskDone.markAsDone();
-                System.out.print(LINE);
-                System.out.println("     Nice! I've marked this task as done: ");
-                System.out.println("       " + taskDone.getStatusIcon() + " " + taskDone);
-                System.out.print(LINE);
-                getUserInput();
+                try {
+                    int index = Integer.parseInt(userInput.split("\\s")[1]);
+                    Task taskDone = tasks.get(index - 1);
+                    taskDone.markAsDone();
+                    System.out.print(LINE);
+                    System.out.println("     Nice! I've marked this task as done: ");
+                    System.out.println("       " + taskDone.getStatusIcon() + " " + taskDone);
+                    System.out.print(LINE);
+                    getUserInput();
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.print(LINE);
+                    System.out.println("     ☹ OOPS!!! The task you want to mark as done doesn't exist.");
+                    System.out.print(LINE);
+                    getUserInput();
+                }
                 break;
             case "todo":
-                String messageTodo = userInput.split("todo ")[1];
-                Task newTaskTodo = new Task(messageTodo, TaskType.TODO);
-                tasks.add(newTaskTodo);
-                System.out.print(LINE);
-                System.out.println(addTaskMessage);
-                System.out.println("       [T][✗] " + newTaskTodo);
-                System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
-                System.out.print(LINE);
-                getUserInput();
+                try {
+                    String messageTodo = userInput.split("todo ")[1];
+                    if (messageTodo.trim().length() == 0) {
+                        throw new EmptyDescriptionException("The description of a todo cannot be empty.");
+                    }
+                    Task newTaskTodo = new Task(messageTodo, TaskType.TODO);
+                    tasks.add(newTaskTodo);
+                    System.out.print(LINE);
+                    System.out.println(addTaskMessage);
+                    System.out.println("       [T][✗] " + newTaskTodo);
+                    System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.print(LINE);
+                    getUserInput();
+                } catch (ArrayIndexOutOfBoundsException | EmptyDescriptionException e) {
+                    System.out.print(LINE);
+                    System.out.println("     ☹ OOPS!!! The description of a todo cannot be empty.");
+                    System.out.print(LINE);
+                    getUserInput();
+                }
                 break;
             case "deadline":
-                String[] deadlineMessageAndTime = userInput.split("deadline ")[1].split(" /by ");
-                String messageDeadline = deadlineMessageAndTime[0];
-                Task newTaskDeadline = new Task(messageDeadline, TaskType.DEADLINE);
-                newTaskDeadline.setTime(deadlineMessageAndTime[1]);
-                tasks.add(newTaskDeadline);
-                System.out.print(LINE);
-                System.out.println(addTaskMessage);
-                System.out.println("       [D][✗] " + newTaskDeadline);
-                System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
-                System.out.print(LINE);
-                getUserInput();
+                try {
+                    String[] deadlineMessageAndTime = userInput.split("deadline ")[1].split(" /by ");
+                    String messageDeadline = deadlineMessageAndTime[0];
+                    if (messageDeadline.trim().length() == 0) {
+                        throw new EmptyDescriptionException("The description of a deadline cannot be empty.");
+                    }
+                    if (!userInput.split("deadline ")[1].contains("/by")) {
+                        throw new DukeException("A deadline requires the '/by' keyword.");
+                    }
+                    Task newTaskDeadline = new Task(messageDeadline, TaskType.DEADLINE);
+                    newTaskDeadline.setTime(deadlineMessageAndTime[1]);
+                    tasks.add(newTaskDeadline);
+                    System.out.print(LINE);
+                    System.out.println(addTaskMessage);
+                    System.out.println("       [D][✗] " + newTaskDeadline);
+                    System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.print(LINE);
+                    getUserInput();
+                } catch (ArrayIndexOutOfBoundsException | EmptyDescriptionException e) {
+                    System.out.print(LINE);
+                    System.out.println("     ☹ OOPS!!! The description of a deadline cannot be empty.");
+                    System.out.print(LINE);
+                    getUserInput();
+                } catch (DukeException e) {
+                    System.out.print(LINE);
+                    System.out.println("     ☹ OOPS!!! "  + e);
+                    System.out.print(LINE);
+                    getUserInput();
+                }
                 break;
             case "event":
-                String[] eventMessageAndTime = userInput.split("event ")[1].split(" /at ");
-                String messageEvent = eventMessageAndTime[0];
-                Task newTaskEvent = new Task(messageEvent, TaskType.EVENT);
-                newTaskEvent.setTime(eventMessageAndTime[1]);
-                tasks.add(newTaskEvent);
-                System.out.print(LINE);
-                System.out.println(addTaskMessage);
-                System.out.println("       [E][✗] " + newTaskEvent);
-                System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
-                System.out.print(LINE);
-                getUserInput();
+                try {
+                    String[] eventMessageAndTime = userInput.split("event ")[1].split(" /at ");
+                    String messageEvent = eventMessageAndTime[0];
+                    if (messageEvent.trim().length() == 0) {
+                        throw new EmptyDescriptionException("The description of an event cannot be empty.");
+                    }
+                    if (!userInput.split("event ")[1].contains("/at")) {
+                        throw new DukeException("An event requires the '/at' keyword.");
+                    }
+                    Task newTaskEvent = new Task(messageEvent, TaskType.EVENT);
+                    newTaskEvent.setTime(eventMessageAndTime[1]);
+                    tasks.add(newTaskEvent);
+                    System.out.print(LINE);
+                    System.out.println(addTaskMessage);
+                    System.out.println("       [E][✗] " + newTaskEvent);
+                    System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.print(LINE);
+                    getUserInput();
+                } catch (ArrayIndexOutOfBoundsException | EmptyDescriptionException e) {
+                    System.out.print(LINE);
+                    System.out.println("     ☹ OOPS!!! The description of an event cannot be empty.");
+                    System.out.print(LINE);
+                    getUserInput();
+                } catch (DukeException e) {
+                    System.out.print(LINE);
+                    System.out.println("     ☹ OOPS!!! " + e);
+                    System.out.print(LINE);
+                    getUserInput();
+                }
                 break;
             default:
-                // not dealt with as of now
-                tasks.add(new Task(userInput, TaskType.TODO));
-                System.out.print(LINE + "     added: " + userInput + "\n" + LINE);
-                getUserInput();
+                try {
+                    throw new DukeException("I'm sorry, but I don't know what that means :-(");
+                } catch (DukeException e) {
+                    System.out.print(LINE);
+                    System.out.println("     ☹ OOPS!!! " + e);
+                    System.out.print(LINE);
+                    getUserInput();
+                }
         }
-
     }
 }

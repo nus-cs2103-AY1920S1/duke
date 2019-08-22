cdf2b0b3a47eadb2bb9a1cbf26fa45dbe4b7fcf5
anqichen9856 <>
2019-08-22 18:07:39 +0800

    Add the ability to store text entered by the user and display them in order when the user types "list".

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,34 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    public static void printList(ArrayList<String> list) {
+        int idx = 1;
+        for (String s : list) {
+            System.out.println(idx + ". " + s);
+            idx++;
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+
+        ArrayList<String> commands = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         String command;
         while(!(command = sc.nextLine()).equals("bye")) {
-            System.out.println(command);
+            if(command.equals("list")) {
+                printList(commands);
+            } else {
+                commands.add(command);
+                System.out.println("added: " + command);
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
     }

4588bcd2401e832e550c9b4b587b3f158d31d950
michelleykw <michelleykw99@gmail.com>
2019-08-22 19:38:10 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,19 +3,26 @@ import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<String>();
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
         while (sc.hasNext()) {
-            String command = sc.nextLine();
-            if (command.equals("list")) {
-                System.out.println("list");
-            } else if (command.equals("blah")) {
+            String text = sc.nextLine();
+            if (text.equals("list")) {
+                int length = list.size();
+                for (int i = 1; i <= length; i++) {
+                    System.out.println(i + ". " + list.get(i - 1));
+                }
+            } else if (text.equals("blah")) {
                 System.out.println("blah");
-            } else if (command.equals("bye")) {
+            } else if (text.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else {
+                list.add(text);
+                System.out.println("added: " + text);
             }
         }
 

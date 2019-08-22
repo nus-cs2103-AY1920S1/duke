56b9787ffc8350a5593893fb1ebeedf04771ed45
Tan Yee Jian <tanyeejian@gmail.com>
2019-08-22 23:54:13 +0800

    Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,11 +5,11 @@ import java.util.List;
 public class Duke {
     public static void main(String[] args) {
         String logo =
-                  " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+            " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
         // System.out.println("Hello from\n" + logo);
         Duke d = new Duke();
         d.greeting();
@@ -22,7 +22,7 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
     }
 
-    private ArrayList<String> cache = new ArrayList<>();
+    private ArrayList<Task> cache = new ArrayList<>();
     private void listen() {
         Scanner listener = new Scanner(System.in);
         while(listener.hasNext()) {
@@ -30,20 +30,30 @@ public class Duke {
             if (line.equals("bye")) {
                 System.out.println("Bye. Hope to see you again!");
                 break;
-            } else {
-
-                switch (line) {
-                    case "list":
-                        for (int i=0; i<cache.size(); i++) {
-                            System.out.printf("%d. %s\n", i+1, cache.get(i));
-                        }
-                        break;
-                    default: 
-                        cache.add(line); 
-                        System.out.printf("added: %s\n", line);
-                        break;
+            } else if (line.equals("list")) {
+                showList();
+            } else if (line.length() >= 6 && line.substring(0,5).equals("done ")) {
+                try {
+                    Task t = cache.get(Integer.parseInt(line.substring(5))-1);
+                    t.markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(t);
+                } catch (NullPointerException e) {
+                    System.out.println("Hey bro why no number?");
+                } catch (Exception e) {
+                    System.out.println("The format is: 'done <number>'. Please try again.");
                 }
+            } else {
+                cache.add(new Task(line)); 
+                System.out.printf("added: %s\n", line);
             }
+        }
+    }
+
+    private void showList() {
+        for (int i=0; i<cache.size(); i++) {
+            Task t = cache.get(i);
+            System.out.printf("%d.[%s] %s\n", i+1, t.getStatusIcon(), t.getDesc());
         }
     }
 }

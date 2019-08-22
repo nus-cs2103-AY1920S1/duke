c181c078e3e7a2d465b23b2d6bb3eab7707c8798
Eshwar Kamalapathy <eshwareverlast@gmail.com>
2019-08-22 19:50:39 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.awt.*;
+import javax.swing.*;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] textArr = new String[100];
+        ArrayList<Task> taskList = new ArrayList<Task>();
         int count = 0;
 
         System.out.println("Hello! I'm Duke");
@@ -15,13 +18,19 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (input.equals("list")) {
-                for (int i = 0; i < count; i++) {
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < taskList.size(); i++) {
                     int index = i + 1;
-                    System.out.println(index + ". " + textArr[i]);
+                    System.out.println(index + "." + taskList.get(i));
                 }
+            } else if (input.contains("done")) {
+                int index = Character.getNumericValue(input.charAt(5)) - 1;
+                (taskList.get(index)).setStatus();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(taskList.get(index));
             } else {
-                textArr[count] = input;
                 count++;
+                taskList.add(new Task(input));
             }
             input = sc.nextLine();
         }

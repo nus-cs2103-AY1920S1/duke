b02ab8d6cff471ec194b20eefa228a79a8ce8032
hello <linmeian@gmail.com>
2019-08-22 16:52:04 +0800

    bug fixes
    fixed the code to make it work properly

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,13 +6,13 @@ public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<String> list = new ArrayList<String>();
-        System.out.println("Hello! I'm Duke \n What can I do for you?");
+        System.out.println("Hello! I'm Duke \nWhat can I do for you?");
         while(sc.hasNext()) {
-           String next = sc.next(); 
-           if(next == "bye") {
+           String next = sc.nextLine(); 
+           if(next.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-           } else if (next == "list") {
+           } else if (next.equals("list")) {
                int numOfOp = list.size();
                for (int i = 1; i <= numOfOp; i++) {
                    System.out.println(i + ". " + list.get(i - 1));

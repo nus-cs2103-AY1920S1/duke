2306a8135d13c57f36d387353c4867ed7b03bf8b
alexwxh <e0273907@u.nus.edu>
2019-08-22 14:57:35 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,17 @@
 import java.util.*;
 public class Duke {
+    private static ArrayList<String> todolist=new ArrayList<String>();
     private static void print(String message){
         System.out.println(">>"+message);
+    }
+    private static void list(){
+        StringBuilder outputmsg=new StringBuilder("List:");
+        int i=todolist.size();
+        for(int x=1; x<=i;x++) {
+            outputmsg.append("\n  "+ x + "." + todolist.get(x - 1));
+        }
+        String output=outputmsg.toString();
+        print(output);
     }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -12,11 +22,13 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         Scanner sc= new Scanner(System.in);
         while(sc.hasNext()){
-            String input=sc.next();
+            String input=sc.nextLine();
             if(input.equals("bye")) {
                 break;
+            }else if(input.equals("list")){
+                list();
             }else{
-                print(input);
+                todolist.add(input);
             }
         }
         String exitmsg="Bye. Hope to see you again.";

9decbfec3038e011e249e3187b95952a646dba1e
cherylnqj <cheryl.nqj@gmail.com>
2019-08-22 13:31:12 +0800

    Add indentation and horizontal line

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,23 +2,25 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What can I do for you?");
-
+        String indent = "    ";
         Scanner sc = new Scanner(System.in);
+
+        printIndentedString("Hello! I'm Duke\n" +
+                indent + " " + "What can I do for you?", indent);
+
         String input = sc.nextLine();
 
-        while(!input.equals("bye")) {
-            System.out.println(input);
-
+        while (!input.equals("bye")) {
+            printIndentedString(input, indent);
             input = sc.nextLine();
         }
+        printIndentedString("Bye. Hope to see you again soon!", indent);
+    }
 
-        System.out.println("Bye. Hope to see you again soon!");
+    public static void printIndentedString(String string, String indent) {
+        System.out.println(indent + "____________________________________________________________");
+        System.out.println(indent + " " + string);
+        System.out.println(indent + "____________________________________________________________");
+        System.out.println();
     }
 }

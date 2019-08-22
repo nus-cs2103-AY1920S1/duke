7a9bf4c3848a4727e0fb8a3490b75f6c86b7ddc1
Murugesan Karthika <mdkkarthika@gmail.com>
2019-08-22 08:57:17 +0800

    Merge branch 'gradle'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -21,26 +21,30 @@ public class Duke {
         int i = 0;
         while(sc.hasNext()) {
             String input = sc.next();
-            if(input.equals("bye")) {
+            if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if(input.equals("list")){ //go through the array
+            } else if (input.equals("list")) { //go through the array
                 System.out.println("Here are the tasks in your list: ");
-                for(int j = 0; j < i; j++){
-                    System.out.println((j+1) + "." + arr[j]);
+                for (int j = 0; j < i; j++) {
+                    System.out.println((j + 1) + "." + arr[j]);
                 }
-            } else if(input.equals("done")) {
+            } else if (input.equals("done")) {
                 int num = sc.nextInt();
                 System.out.println("Nice! I've marked this task as done: ");
-                arr[num-1].markAsDone();
+                arr[num - 1].markAsDone();
                 System.out.println(arr[num]);
-            } else if(input.equals("todo")) {
-                arr[i] = new Todo(sc.nextLine());
+            } else if (input.equals("todo")) {
+                String descTodo = sc.nextLine();
+                if (descTodo.isEmpty()) {
+                    throw new IllegalArgumentException("OOPS!!! The description of a todo cannot be empty.");
+                }
+                arr[i] = new Todo(descTodo);
                 System.out.println("Got it. I've added this task: ");
                 System.out.println(" " + arr[i]);
-                System.out.println("Now you have " + (i+1) + " tasks in the list.");
+                System.out.println("Now you have " + (i + 1) + " tasks in the list.");
                 i++;
-            } else if(input.equals("deadline")) {
+            } else if (input.equals("deadline")) {
                 String rem = sc.nextLine();
                 String[] descriptionNDate = rem.split("/by");
                 String description = descriptionNDate[0];
@@ -48,9 +52,9 @@ public class Duke {
                 arr[i] = new Deadline(description, by);
                 System.out.println("Got it. I've added this task: ");
                 System.out.println(" " + arr[i]);
-                System.out.println("Now you have " + (i+1) + " tasks in the list.");
+                System.out.println("Now you have " + (i + 1) + " tasks in the list.");
                 i++;
-            } else if(input.equals("event")) {
+            } else if (input.equals("event")) {
                 String rest = sc.nextLine();
                 String[] descriptionNAt = rest.split("/at");
                 String desc = descriptionNAt[0];
@@ -58,15 +62,10 @@ public class Duke {
                 arr[i] = new Event(desc, at);
                 System.out.println("Got it. I've added this task: ");
                 System.out.println(" " + arr[i]);
-                System.out.println("Now you have " + (i+1) + " tasks in the list.");
+                System.out.println("Now you have " + (i + 1) + " tasks in the list.");
                 i++;
-            }
-            else {
-                String activity = input + sc.nextLine();
-                Task t = new Task(activity);
-                arr[i] = t;
-                i++;
-                System.out.println("added: " + activity);
+            } else {
+                throw new IllegalArgumentException("OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
         }
     }

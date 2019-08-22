e4468fd74ca5225c21f57bc176fa85a54bfcea97
ReignOfComputer <roc@reignofcomputer.com>
2019-08-22 12:39:38 +0800

    W2 - Level-1
    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        boolean byeHit = false;
+        String repeatStr = "";
+
+        while (!byeHit) {
+            repeatStr = sc.nextLine();
+            if (repeatStr.equals("bye")) {
+                byeHit = true;
+                System.out.println("Bye. Hope to see you again soon!");
+            } else {
+                System.out.println(repeatStr);
+            }
+        }
     }
 }

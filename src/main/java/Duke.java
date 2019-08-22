abe210d058b0ec4602b2ee40aca098bedc6c0765
leeyiyuan <leeyiyuan@hotmail.com>
2019-08-22 19:25:53 +0800

    Implement add and list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,12 @@
 package com.leeyiyuan;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
+        ArrayList<String> items = new ArrayList<String>();        
+        
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
         Scanner scanner = new Scanner(System.in);
@@ -12,8 +15,16 @@ public class Duke {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < items.size(); i++) {
+                    System.out.println(String.format(
+                                "%d. %s", 
+                                i + 1, 
+                                items.get(i)));
+                }
             } else {
-                System.out.println(input);
+                items.add(input);
+                System.out.println(String.format("added: %s", input));
             }
         }
     }

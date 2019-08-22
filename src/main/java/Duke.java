de9be5c3489ce8b54e4415a3c047c697cca97cf6
bakwxh <omega1653@gmail.com>
2019-08-22 14:59:01 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+	
+	public static void main(String[] args) {
+		String gap = "    ";
+		String line = "____________________________________________________________";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-    }
+        
+		System.out.println(gap + line);
+		System.out.println(gap + " Hello! I'm Duke");
+		System.out.println(gap + " What can I do for you?");
+		System.out.println(gap + line);
+		
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			String command = sc.nextLine();
+			if (!command.toLowerCase().equals("bye")) {
+				System.out.println(gap + line);
+				System.out.println(gap + " " + command);
+				System.out.println(gap + line);
+			} else {
+				System.out.println(gap + line);
+				System.out.println(gap + " " + "Bye. Hope to see you again soon!");
+				System.out.println(gap + line);
+				break;
+			}
+		}
+		
+		sc.close();
+	}
 }

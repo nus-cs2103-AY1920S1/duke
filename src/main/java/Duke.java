8209ba2a00c2847a6b5ebea572722daf7a10c726
hello <linmeian@gmail.com>
2019-08-22 23:01:41 +0800

    added error checking
    added error checks for empty todos/deadline/events + nonsensical commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,7 +12,7 @@ public class Duke {
            String[] command = next.split(" ");
            if(command[0].equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
-                break;
+                continue;
            } else if (command[0].equals("list")) {
                int numOfOp = list.size();
                for (int i = 1; i <= numOfOp; i++) {
@@ -27,14 +27,34 @@ public class Duke {
                String time = "";
                if(index != -1) {
                    time = next.substring(index + 1);
+                   if(time.equals("")) {
+                       System.out.println("☹ OOPS!!! The time cannot be empty.");
+                       break;
+                   }
                }
                String desc;
                if(command[0].equals("deadline")) {
                    desc = next.substring(9, index - 1) + " ";
+                   if(desc.equals(" ")) {
+                       System.out.println("☹ OOPS!!! The description for deadline cannot be empty.");
+                       continue;
+                   }
                } else if(command[0].equals("event")) {
                    desc = next.substring(6, index - 1) + " ";
+                   if(desc.equals(" ")) {
+                        System.out.println("☹ OOPS!!! The description for event cannot be empty.");
+                        continue;
+                   }
+               } else if(command[0].equals("todo")) {
+                   desc = next.substring(4);
+                   if (desc.equals("")) {
+                       System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+                   } else {
+                       desc = desc.substring(1);
+                   }
                } else {
-                   desc = next.substring(5);
+                   System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                   continue;
                }
                Tasks nextTask = new Tasks(desc, command[0], time); 
                list.add(nextTask);

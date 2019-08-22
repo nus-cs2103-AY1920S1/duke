7372b67f4d19e9718c3afd1a9aba81a2c4bc70ea
ShiHaoYap <shihaoyap@gmail.com>
2019-08-22 18:29:03 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> inputList = new ArrayList<>();
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -22,10 +24,17 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("___________________________________");
                 break;
+            } else if (inputString.equals("list")) {
+                System.out.println("___________________________________");
+                for (int i = 0; i < inputList.size(); i++) {
+                    System.out.println(i+1 + ". " + inputList.get(i));
+                }
+                System.out.println("___________________________________");
             } else {
                 System.out.println("___________________________________");
-                System.out.println(inputString);
+                System.out.println("added: " + inputString);
                 System.out.println("___________________________________");
+                inputList.add(inputString);
             }
         }
     }

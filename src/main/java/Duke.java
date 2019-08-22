f764eeac87a6d00f911d2b09a509026b254a9f58
ManasVegi <manasvegi@gmail.com>
2019-08-22 14:43:16 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ public class Duke {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         //hr is horizontal row
-        String hr = "___________________________________";
+        String hr = "______________________________________________________________________";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -26,41 +26,88 @@ public class Duke {
                 temp.markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println("  " + temp);
+                System.out.println(hr);
             } else if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.print(hr);
+                System.out.println(hr);
                 break;
             } else if(command.equals("list")) {
                 for(int i = 0; i < arr.size(); i++) {
                     Task temp = arr.get(i);
                     System.out.println((i + 1) + ". " + temp);
                 }
+                System.out.println(hr);
             } else {
                 if (order.equals("todo")) {
-                    Task temp = new ToDo(command.split("todo ")[1]);
-                    arr.add(temp);
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(" " + temp);
-                    System.out.println("Now you have " + arr.size() + " tasks in the list.");
+                    try {
+                        if (commandWords.length == 1) {
+                            throw new DukeException(" :( OOPS!!! The description of a todo cannot be empty.");
+                        } else {
+                            Task temp = new ToDo(command.split("todo ")[1]);
+                            arr.add(temp);
+                            System.out.println("Got it. I've added this task:");
+                            System.out.println(" " + temp);
+                            System.out.println("Now you have " + arr.size() + " tasks in the list.");
+                        }
+                    } catch (DukeException de) {
+                        System.err.println(de.getMessage());
+                    } finally {
+                        System.out.println(hr);
+                    }
                 } else if (order.equals("deadline")) {
-                    String instruction = command.split("deadline ")[1];
-                    String[] details = instruction.split(" /by ");
-                    Task temp = new Deadline(details[0], details[1]);
-                    arr.add(temp);
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(" " + temp);
-                    System.out.println("Now you have " + arr.size() + " tasks in the list.");
+                    try {
+                        if (commandWords.length == 1) {
+                            throw new DukeException(" :( OOPS!!! The description of a deadline cannot be empty.");
+                        } else {
+                            if (command.split("/by").length == 1) {
+                                throw new DukeException(" :( OOPS!!! A deadline must have a time.");
+                            } else {
+                                String instruction = command.split("deadline ")[1];
+                                String[] details = instruction.split(" /by ");
+                                Task temp = new Deadline(details[0], details[1]);
+                                arr.add(temp);
+                                System.out.println("Got it. I've added this task:");
+                                System.out.println(" " + temp);
+                                System.out.println("Now you have " + arr.size() + " tasks in the list.");
+                            }
+                        }
+                    } catch (DukeException de) {
+                        System.err.println(de.getMessage());
+                    } finally {
+                        System.out.println(hr);
+                    }
+                } else if (order.equals("event")){
+                    try {
+                        if (commandWords.length == 1) {
+                            throw new DukeException(" :( OOPS!!! The description of a event cannot be empty.");
+                        } else {
+                            if (command.split("/at").length == 1) {
+                                throw new DukeException(" :( OOPS!!! An event must have a time.");
+                            } else {
+                                String instruction = command.split("event ")[1];
+                                String[] details = instruction.split(" /at ");
+                                Task temp = new Event(details[0], details[1]);
+                                arr.add(temp);
+                                System.out.println("Got it. I've added this task:");
+                                System.out.println(" " + temp);
+                                System.out.println("Now you have " + arr.size() + " tasks in the list.");
+                            }
+                        }
+                    } catch (DukeException de) {
+                        System.err.println(de.getMessage());
+                    } finally {
+                        System.out.println(hr);
+                    }
                 } else {
-                    String instruction = command.split("event ")[1];
-                    String[] details = instruction.split(" /at ");
-                    Task temp = new Event(details[0], details[1]);
-                    arr.add(temp);
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(" " + temp);
-                    System.out.println("Now you have " + arr.size() + " tasks in the list.");
+                    try {
+                        throw new DukeException(" :( OOPS!!! I'm sorry but I don't know what that means :-(");
+                    } catch (DukeException de) {
+                        System.err.println(de.getMessage());
+                    } finally {
+                        System.out.println(hr);
+                    }
                 }
             }
-            System.out.println(hr);
         }
     }
 }

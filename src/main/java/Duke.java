fc33b39362cb9a59e935fda47a0ebcffc79c13e2
Zhong Jun <zhongjun@jorked.com>
2019-08-22 22:12:48 +0800

    Level 6: Delete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,8 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    public static Task[] items = new Task[100];
+    public static ArrayList<Task> items = new ArrayList<>();
     public static int counter = 0;
 
     public static void main(String[] args) {
@@ -33,34 +34,43 @@ public class Duke {
             for (int i = 0; i < counter; i++) {
                 sb.append(i + 1);
                 sb.append(".");
-                sb.append(items[i]);
+                sb.append(items.get(i));
                 sb.append('\n');
             }
             System.out.print(sb);
         } else if (next.startsWith("done")) {
             String[] parts = next.split(" ");
             int num = Integer.parseInt(parts[1]);
-            items[num - 1].setDone(true);
-            String output = "Nice! I've marked this task as done:\n  " + items[num - 1];
+            Task task = items.get(num - 1);
+            task.setDone(true);
+            String output = "Nice! I've marked this task as done:\n  " + task;
+            System.out.println(output);
+        } else if (next.startsWith("delete")) {
+            String[] parts = next.split(" ");
+            int num = Integer.parseInt(parts[1]);
+            Task removed = items.remove(num - 1);
+            counter--;
+            String output = "Noted. I've removed this task:\n  " + removed;
+            output += "\nNow you have " + counter + " tasks in the list.";
             System.out.println(output);
         } else {
             if (next.startsWith("todo")) {
                 if (next.length() < 6) {
                     throw new DukeException("The description of a todo cannot be empty.");
                 } else {
-                    items[counter] = new Todo(next.substring(5));
+                    items.add(new Todo(next.substring(5)));
                 }
             } else if (next.startsWith("deadline")) {
                 String[] parts = next.split(" /by ");
-                items[counter] = new Deadline(parts[0].substring(9), parts[1]);
+                items.add(new Deadline(parts[0].substring(9), parts[1]));
             } else if (next.startsWith("event")) {
                 String[] parts = next.split(" /at ");
-                items[counter] = new Event(parts[0].substring(6), parts[1]);
+                items.add(new Event(parts[0].substring(6), parts[1]));
             } else {
                 throw new DukeException("I'm sorry, but I don't know what that means :-(");
             }
 
-            String output = "Got it. I've added this task:\n  " + items[counter];
+            String output = "Got it. I've added this task:\n  " + items.get(counter);
             output += "\nNow you have " + (counter + 1) + " tasks in the list.";
             System.out.println(output);
             counter++;

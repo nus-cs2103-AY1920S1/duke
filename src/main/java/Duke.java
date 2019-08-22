3621f02c6ce65bc45ab778d6e21e78e6a18f8724
Xiaodong <xdjiaxd@gmail.com>
2019-08-22 09:13:10 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,20 +3,31 @@ import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> storage = new ArrayList<String>();
         String in;
-        while (true){
+inputloop:
+        while(true){
             in = sc.nextLine().trim();
-            if (in.equals("bye")) {
-                System.out.println("Bye!");
-                break;
-            } else {
-                System.out.println(in); 
+            switch(in){
+                case "bye":
+                    System.out.println("Bye!");
+                    break inputloop;
+
+                case "list":
+                    for(int i = 1; i <= storage.size(); i++) {
+                        System.out.println(i + ". " + in);
+                    }
+                    break;
+
+                default:
+                    System.out.println("added: " + in);
+                    storage.add(in);
             }
         }
     }

9b90e7ca3a546de6b47c369cb61230223f348314
uggi121 <sudharshanmadhavan@outlook.com>
2019-08-22 20:31:20 +0800

    Add Tasks' subclasses.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
     private static Scanner sc;
-    private static Task[] tasks = new Task[100];
+    private static ArrayList<Task> tasks = new ArrayList<>();
     private static int numberOfTasks = 0;
 
     public static void main(String[] args) {
@@ -36,39 +37,94 @@ public class Duke {
         } else if (input.startsWith("done")) {
             evaluateDone(input);
         } else {
-            dukeOutput("added: " + input);
             addTask(input);
         }
     }
 
     public static void evaluateDone(String input) {
-        String number = input.substring(5, input.length());
-        int taskNumber = Integer.parseInt(number);
+        String number = input.substring(4, input.length()).strip();
         if (number.isEmpty()) {
             dukeOutput("Invalid input!");
-        } else if (taskNumber > numberOfTasks) {
-            dukeOutput("Task doesn't exist");
         } else {
-            tasks[taskNumber - 1].markAsDone();
-            String output = "Nice! I've marked this task as done:"
-                    + "\n  " + tasks[taskNumber - 1].toString();
-            dukeOutput(output);
+            int taskNumber = Integer.parseInt(number);
+            if (taskNumber > numberOfTasks) {
+                dukeOutput("Task doesn't exist");
+            } else {
+                tasks.get(taskNumber - 1).markAsDone();
+                String output = "Nice! I've marked this task as done:"
+                        + "\n  " + tasks.get(taskNumber - 1).toString();
+                dukeOutput(output);
+            }
         }
     }
 
     public static void addTask(String description) {
-        tasks[numberOfTasks++] = new Task(description);
+        String[] tokens = description.split("\\s+");
+        String taskType = tokens[0];
+        System.out.println(taskType);
+        if (taskType.equalsIgnoreCase("Event")) {
+            addEvent(description);
+        } else if (taskType.equalsIgnoreCase("Todo")) {
+            addTodo(description);
+        } else if (taskType.equalsIgnoreCase("Deadline")) {
+            addDeadline(description);
+        } else {
+            dukeOutput("Invalid task!");
+        }
+    }
+
+    public static void addEvent(String description) {
+        int indexOfAt = description.indexOf("/at");
+        String desc = description.substring(5, indexOfAt).strip();
+        String at = description.substring(indexOfAt + 3).strip();
+        tasks.add(new Event(desc, at));
+        numberOfTasks++;
+        String output = "Got it. I've added this task:\n"
+                + "  " + tasks.get(numberOfTasks - 1).toString()
+                + "\n" + getNumberOfTasks();
+        dukeOutput(output);
+    }
+
+    public static void addTodo(String description) {
+        String[] tokens = description.split("\\s+");
+        StringBuilder desc = new StringBuilder();
+        for (int i = 1; i < tokens.length; i++) {
+            desc.append(tokens[i] + " ");
+        }
+        String newDescription = desc.toString().strip();
+        tasks.add(new Todo(newDescription));
+        numberOfTasks++;
+        String output = "Got it. I've added this task:\n"
+                + "  " + tasks.get(numberOfTasks - 1).toString()
+                + "\n" + getNumberOfTasks();
+        dukeOutput(output);
+    }
+
+    public static void addDeadline(String description) {
+        int indexOfAt = description.indexOf("/by");
+        String desc = description.substring(8, indexOfAt).strip();
+        String by = description.substring(indexOfAt + 3).strip();
+        tasks.add(new Deadline(desc, by));
+        numberOfTasks++;
+        String output = "Got it. I've added this task:\n"
+                + "  " + tasks.get(numberOfTasks - 1).toString()
+                + "\n" + getNumberOfTasks();
+        dukeOutput(output);
     }
 
     public static void printTasks() {
         StringBuilder output = new StringBuilder();
         for (int i = 0; i < numberOfTasks; i++) {
-            output.append((i + 1) + ". " + tasks[i].toString());
+            output.append((i + 1) + ". " + tasks.get(i).toString());
             if (i != numberOfTasks - 1) {
                 output.append("\n");
             }
         }
         dukeOutput(output.toString());
+    }
+
+    public static String getNumberOfTasks() {
+        return "Now you have " + numberOfTasks + " tasks in the list.";
     }
 
     public static void dukeOutput(String out) {

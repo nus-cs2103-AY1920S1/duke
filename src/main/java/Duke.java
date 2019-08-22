47514be09d74cb9ea10c967e8378222bff6b74bd
fadisng <fadisng1997@gmail.com>
2019-08-22 14:30:36 +0800

    Level - 5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -12,7 +12,8 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         ArrayList<Task> list = new ArrayList<>();
         System.out.println("Hello! I'm Duke \nWhat can I do for you?");
-        String next = sc.next();
+        String[] arr = sc.nextLine().split(" ");
+        String next = arr[0];
         int count = 0;
         while (!next.equals("bye")) {
             if (next.equals("list")) {
@@ -22,32 +23,52 @@ public class Duke {
                     listCount++;
                 }
             } else if (next.equals("done")) {
-                int number = sc.nextInt() - 1;
+                int number = Integer.parseInt(arr[1]) - 1;
                 Task taskDone = list.get(number);
                 taskDone.markAsDone();
                 System.out.println("Nice! I've marked this task as done:\n  " + taskDone);
             } else {
                 count++;
-                String description = sc.nextLine();
+                String description = "";
                 Task t;
-                System.out.println("Got it. I've added this task:");
-                if (next.equals("todo")) {
-                    t = new Todos(description.trim());
-                } else if (next.equals("deadline")) {
-                    int index = description.indexOf("/");
-                    String byWhen = description.substring(index + 4, description.length());
-                    String desc = description.substring(1, index - 1);
-                    t = new Deadline(desc, byWhen);
-                } else {
-                    int index = description.indexOf("/");
-                    String at = description.substring(index + 4, description.length());
-                    String desc = description.substring(1, index - 1);
-                    t = new Event(desc, at);
+                try {
+                    if (next.equals("todo") || next.equals("deadline") || next.equals("event")) {
+                        if (arr.length == 1) {
+                            throw new EmptyDescriptionException("☹ OOPS!!! The description of a " + next + " cannot be empty.");
+                        } else {
+                            for (int i = 1; i < arr.length; i++) {
+                                description += " " + arr[i];
+                            }
+                        }
+                    } else {
+                        throw new UnknownTaskException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    }
+                    System.out.println("Got it. I've added this task:");
+                    if (next.equals("todo")) {
+                        t = new Todos(description.trim());
+                    } else if (next.equals("deadline")) {
+                        int index = description.indexOf("/");
+                        String byWhen = description.substring(index + 4, description.length());
+                        String desc = description.substring(1, index - 1);
+                        t = new Deadline(desc, byWhen);
+                    } else {
+                        int index = description.indexOf("/");
+                        String at = description.substring(index + 4, description.length());
+                        String desc = description.substring(1, index - 1);
+                        t = new Event(desc, at);
+                    }
+                    list.add(t);
+                    if (count == 1) {
+                        System.out.println("  " + t + "\nNow you have " + count + " task in the list.");
+                    } else {
+                        System.out.println("  " + t + "\nNow you have " + count + " tasks in the list.");
+                    }
+                } catch (DukeException e) {
+                    System.out.println(e.getMessage());
                 }
-            list.add(t);
-            System.out.println("  " + t + "\nNow you have " + count + " tasks in the list.");
             }
-            next = sc.next();
+            arr = sc.nextLine().split(" ");
+            next = arr[0];
         }
         System.out.println("Bye. Hope to see you again soon!");
     }

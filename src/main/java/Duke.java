0870ff456fd1ca4d03daf6e45270908b733096a6
anqichen9856 <>
2019-08-22 18:18:01 +0800

    Add the ability to mark tasks as done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,12 +2,37 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    public static void printList(ArrayList<String> list) {
+    public static void printList(ArrayList<Task> list) {
         int idx = 1;
-        for (String s : list) {
-            System.out.println(idx + ". " + s);
+        System.out.println("Here are the tasks in your list:");
+        for (Task t : list) {
+            System.out.println(idx + ". " + t);
             idx++;
         }
+    }
+
+    public static void processor(){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Task> tasks = new ArrayList<>();
+        String command;
+        while(!(command = sc.nextLine()).equals("bye")) {
+            if(command.equals("list")) {
+                printList(tasks);
+            } else {
+                String[] words = command.split(" ");
+                if (words[0].equals("done")) {
+                    int i = Integer.parseInt(words[1]);
+                    Task t = tasks.get(i - 1);
+                    t.markAsDone();
+                    System.out.println("Nice! I've marked this task as done: \n  " + t);
+                } else {
+                    Task t = new Task(command);
+                    tasks.add(t);
+                    System.out.println("added: " + command);
+                }
+            }
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 
     public static void main(String[] args) {
@@ -18,18 +43,6 @@ public class Duke {
             + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-
-        ArrayList<String> commands = new ArrayList<>();
-        Scanner sc = new Scanner(System.in);
-        String command;
-        while(!(command = sc.nextLine()).equals("bye")) {
-            if(command.equals("list")) {
-                printList(commands);
-            } else {
-                commands.add(command);
-                System.out.println("added: " + command);
-            }
-        }
-        System.out.println("Bye. Hope to see you again soon!");
+        processor();
     }
 }

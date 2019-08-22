9e632a5e5761933457743ae13d2c586339741d3a
atharvjoshi <atharvjoshi@ymail.com>
2019-08-22 15:23:15 +0800

    Created driver class for Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * Encapsulates attributes and behaviour of Duke, a personal assistant chatbot.
+ *
+ * @author atharvjoshi
+ * @contributors j-lum, damithc
+ * @version CS2103 AY19/20 Sem 1 iP Week 2
+ */
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
+    // a unique duke logo
+    private final String logo;
+
+    public Duke() {
+        logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    }
+
+    public String getLogo() {
+        return logo;
     }
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Main driver class for Duke, a personal assistant chatbot.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Duke duke = new Duke();
+        System.out.println(duke.getLogo());
+    }
+}

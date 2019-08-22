8def4dce4a568b13de9c24d86b55ad5fd743f6ac
SimNee <chuasimnee@Chuas-MacBook-Pro.local>
2019-08-22 21:46:35 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,7 +9,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        String greet = "___________________________________________________________\n" +
+        String greet = "____________________________________________________________\n" +
                 "     Hello! I'm Duke\n" +
                 "     What can I do for you?\n" +
                 "____________________________________________________________";
@@ -32,8 +32,8 @@ public class Duke {
                         "Here are the tasks in your list:");
                 int count = 1;
                 for (Task t : taskList) {
-                    //System.out.println(count + "." + t.getDescription());
-                    System.out.printf("[%s]%d. %s%n", t.getStatusIcon(), count, t.description);
+                    System.out.println(count + "." + t.toString());
+                    //System.out.printf("[%s]%d. %s%n", t.getStatusIcon(), count, t.description);
                     count++;
                 }
                 System.out.println("____________________________________________________________\n");
@@ -48,13 +48,58 @@ public class Duke {
                 System.out.println("____________________________________________________________\n");
                 echo = sc.nextLine();
 
-            } else {
-                Task newTask = new Task(echo);
-                taskList.add(newTask);
-                System.out.println("____________________________________________________________\n" +
-                        "added: " + echo + "\n" +
-                        "____________________________________________________________");
+            } else if (echo.startsWith("todo")) {
+                if (echo.length() == 4) {
+                    System.out.println("____________________________________________________________\n" +
+                            "     ☹ OOPS!!! The description of a todo cannot be empty.\n" +
+                            "____________________________________________________________");
+                } else {
+                    System.out.println("____________________________________________________________\n" +
+                            "Got it. I've added this task: ");
+                    Task todoTask = new Todo(echo.substring(5));
+                    taskList.add(todoTask);
+                    System.out.println(todoTask.toString());
+                    System.out.println("Now you have " +  taskList.size() + " tasks in the list.");
+                    System.out.println("____________________________________________________________\n");
+                }
                 echo = sc.nextLine();
+
+            } else if (echo.startsWith("deadline")) {
+                if (echo.length() == 8) {
+                    System.out.println("____________________________________________________________\n" +
+                            "     ☹ OOPS!!! The description of a deadline cannot be empty.\n" +
+                            "____________________________________________________________");
+                } else {
+                    int indexOfSlash = echo.indexOf("/");
+                    Task deadlineTask = new Deadline(echo.substring(9, indexOfSlash - 1), echo.substring(indexOfSlash + 4));
+                    taskList.add(deadlineTask);
+                    System.out.println("____________________________________________________________\n" +
+                            "Got it. I've added this task: ");
+                    System.out.println(deadlineTask.toString());
+                    System.out.println("Now you have " +  taskList.size() + " tasks in the list.");
+                    System.out.println("____________________________________________________________\n");
+                }
+                echo = sc.nextLine();
+
+            } else if (echo.startsWith("event")) {
+                if (echo.length() == 5) {
+                    System.out.println("____________________________________________________________\n" +
+                            "     ☹ OOPS!!! The description of an event cannot be empty.\n" +
+                            "____________________________________________________________");
+                } else {
+                    int indexOfSlash = echo.indexOf("/");
+                    Task eventTask = new Event(echo.substring(6, indexOfSlash - 1), echo.substring(indexOfSlash + 4));
+                    taskList.add(eventTask);
+                    System.out.println("____________________________________________________________\n" +
+                            "Got it. I've added this task: ");
+                    System.out.println(eventTask.toString());
+                    System.out.println("Now you have " + taskList.size() + " tasks in the list.");
+                    System.out.println("____________________________________________________________\n");
+                }
+                echo = sc.nextLine();
+            } else {
+                System.out.println("error");
+                break;
             }
         }
     }

1fe547d7027016b56ca4921262c4d73cb3dd4d52
Chen Su <chensu@comp.nus.edu.sg>
2019-08-22 10:48:42 +0800

    Edit Duke.java for Level2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,36 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    private static String[] list = new String[100];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
-        String command = sc.next();
+        String command = sc.nextLine();
+        int counter = 0;
         while (!command.equals("bye")) {
-            System.out.println(command);
-            command = sc.next();
+            if (command.equals("list")) {
+                showList(counter);
+            } else {
+                list[counter] = command;
+                System.out.println("added: " + command);
+                counter++;
+            }
+            command = sc.nextLine();
         }
         if (command.equals("bye")) {
             System.out.println("Bye. Hope to see you again soon!");
-            sc.close();
+            sc.close(); //exit the program
+        }
+    }
+
+    // To print out the items stored in the list.
+    public static void showList(int counter) {
+        for (int i = 0; i < counter; i++) {
+            System.out.println((i+1) + ". " + list[i]);
         }
     }
 }

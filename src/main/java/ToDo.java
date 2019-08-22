dd033e753233a00a1e76e355a3199f69ed35e8d0
envere <keithtxhh@gmail.com>
2019-08-22 16:54:08 +0800

    level 2

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,27 +1,29 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class ToDo {
+class ToDo {
+    private ArrayList<String> todoList = new ArrayList<>();
 
-    public ToDo() {
+    ToDo() {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
     }
 
-    public void run() {
+    void run() {
         Scanner sc = new Scanner(System.in);
         String nextCommand = sc.nextLine();
         while (!nextCommand.equals("bye")) {
-            switch (nextCommand) {
-                case "list":
-                    System.out.println("list");
-                    nextCommand = sc.nextLine();
-                    break;
-                default:
-                    System.out.println(nextCommand);
-                    nextCommand = sc.nextLine();
+            if (nextCommand.equals("list")) {
+                for (int i = 0; i < todoList.size(); i++) {
+                    System.out.println(i + 1 + ": " + todoList.get(i));
+                }
+                nextCommand = sc.nextLine();
+            } else {
+                todoList.add(nextCommand);
+                System.out.println("added: " + nextCommand);
+                nextCommand = sc.nextLine();
             }
         }
-
         System.out.println("Bye! Hope to see you again soon!");
     }
 }

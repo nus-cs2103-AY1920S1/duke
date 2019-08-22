80cd8a89b7fe67083ed9226e64b9fb7715aaac4f
shawnkoh <shawnkoh@me.com>
2019-08-22 22:17:01 +0800

    Add Task class, list command and ability to store new tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,43 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Task> tasks = new ArrayList<>();
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        Scanner sc = new Scanner(System.in);
-        while (true) {
+
+        boolean readingCommands = true;
+        while (readingCommands) {
             String command = sc.nextLine();
-            if (command.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            } else {
-                System.out.println(command);
+            switch (command) {
+                case "list":
+                    for (int i = 1; i <= tasks.size(); i++) {
+                        Task task = tasks.get(i - 1);
+                        System.out.println(i + ". " + task);
+                    }
+                    break;
+
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    readingCommands = false;
+                    break;
+
+                default:
+                    Task task = new Task(command);
+                    tasks.add(task);
+                    System.out.println("added: " + task);
             }
         }
         sc.close();
     }
 }
+

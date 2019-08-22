6ae5cd7431e013f5192be6a8f6debc2ae3c31bb3
blimyj <blimyj21@gmail.com>
2019-08-22 10:20:56 +0800

    Added Exceptions and completed minimal for Level 5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -31,93 +31,113 @@ public class Duke {
             Task newTask = new Task("");
 
             //If list print all tasks
-            switch (inputArray[0]) {
-            case "list":
-                System.out.print(dividerLine);
-
-                //Print each task
-                Iterator<Task> tasksIterator = tasks.iterator();
-                Task currTask = new Task("");
-
-                for (int i = 1; i <= tasks.size(); i++) {
-                    currTask = tasksIterator.next();
-
-                    System.out.print("  "
-                            + i
-                            + ". "
-                            + currTask
-                            + "\n");
+            try {
+                switch (inputArray[0]) {
+                    case "list":
+                        System.out.print(dividerLine);
+    
+                        //Print each task
+                        Iterator<Task> tasksIterator = tasks.iterator();
+                        Task currTask = new Task("");
+    
+                        for (int i = 1; i <= tasks.size(); i++) {
+                            currTask = tasksIterator.next();
+    
+                            System.out.print("  "
+                                    + i
+                                    + ". "
+                                    + currTask
+                                    + "\n");
+                        }
+    
+                        System.out.print(dividerLine);
+                        break;
+                    case "done":
+                        if (inputArray.length < 2) {
+                            throw new DukeException("OOPS!!! You need to specify the number of the task " 
+                                    + "you want to mark as done!");
+                        }
+                        int taskNum = Integer.parseInt(inputArray[1]);
+                        int taskIndex = taskNum - 1;
+    
+                        Task modifiedTask = tasks.get(taskIndex);
+                        modifiedTask.markAsDone();
+    
+                        //Printing
+                        System.out.print(dividerLine);
+    
+                        System.out.print("  Nice! I've marked this task as done:\n");
+                        System.out.print("    "
+                                + modifiedTask
+                                + "\n");
+    
+                        System.out.print(dividerLine);
+                        break;
+                    case "todo":
+                        if (inputArray.length < 2) {
+                            throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
+                        }
+                        newTask = new Todo(inputArray[1]);
+                        tasks.add(newTask);
+                        System.out.print(dividerLine
+                                + "  Got it. I've added this task:\n"
+                                + "    "
+                                + newTask
+                                + "\n"
+                                + "  Now you have "
+                                + tasks.size()
+                                + " task(s) in the list."
+                                + "\n"
+                                + dividerLine);
+                        break;
+                    case "event":
+                        if (inputArray.length < 2) {
+                            throw new DukeException("OOPS!!! The description of a event cannot be empty.");
+                        }
+                        cmdArgs = inputArray[1].split(" /at ", 2);
+                        newTask = new Event(cmdArgs[0], cmdArgs[1]);
+                        tasks.add(newTask);
+                        System.out.print(dividerLine
+                                + "  Got it. I've added this task:\n"
+                                + "    "
+                                + newTask
+                                + "\n"
+                                + "  Now you have "
+                                + tasks.size()
+                                + " task(s) in the list."
+                                + "\n"
+                                + dividerLine);
+                        break;
+                    case "deadline":
+                        if (inputArray.length < 2) {
+                            throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
+                        }
+                        cmdArgs = inputArray[1].split(" /by ", 2);
+                        newTask = new Deadline(cmdArgs[0], cmdArgs[1]);
+                        tasks.add(newTask);
+                        System.out.print(dividerLine
+                                + "  Got it. I've added this task:\n"
+                                + "    "
+                                + newTask
+                                + "\n"
+                                + "  Now you have "
+                                + tasks.size()
+                                + " task(s) in the list."
+                                + "\n"
+                                + dividerLine);
+                        break;
+                    default:
+                        throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
-
-                System.out.print(dividerLine);
-                break;
-            case "done":
-                int taskNum = Integer.parseInt(inputArray[1]);
-                int taskIndex = taskNum - 1;
-
-                Task modifiedTask = tasks.get(taskIndex);
-                modifiedTask.markAsDone();
-
-                //Printing
-                System.out.print(dividerLine);
-
-                System.out.print("  Nice! I've marked this task as done:\n");
-                System.out.print("    "
-                        + modifiedTask
-                        + "\n");
-
-                System.out.print(dividerLine);
-                break;
-            case "todo":
-                newTask = new Todo(inputArray[1]);
-                tasks.add(newTask);
+            } catch (DukeException e) {
                 System.out.print(dividerLine
-                        + "  Got it. I've added this task:\n"
-                        + "    "
-                        + newTask
-                        + "\n"
-                        + "  Now you have "
-                        + tasks.size()
-                        + " task(s) in the list."
+                        + "  ☹ "
+                        + e.getMessage()
                         + "\n"
                         + dividerLine);
-                break;
-            case "event":
-                cmdArgs = inputArray[1].split(" /at ", 2);
-                newTask = new Event(cmdArgs[0], cmdArgs[1]);
-                tasks.add(newTask);
-                System.out.print(dividerLine
-                        + "  Got it. I've added this task:\n"
-                        + "    "
-                        + newTask
-                        + "\n"
-                        + "  Now you have "
-                        + tasks.size()
-                        + " task(s) in the list."
-                        + "\n"
-                        + dividerLine);
-                break;
-            case "deadline":
-                cmdArgs = inputArray[1].split(" /by ", 2);
-                newTask = new Deadline(cmdArgs[0], cmdArgs[1]);
-                tasks.add(newTask);
-                System.out.print(dividerLine
-                        + "  Got it. I've added this task:\n"
-                        + "    "
-                        + newTask
-                        + "\n"
-                        + "  Now you have "
-                        + tasks.size()
-                        + " task(s) in the list."
-                        + "\n"
-                        + dividerLine);
-                break;
-            default:
-                System.out.println("Invalid Command");
-                break;
+            } finally {
+                input = sc.nextLine();
             }
-
-            input = sc.nextLine();
         }
         // Exit if bye
         System.out.print(dividerLine

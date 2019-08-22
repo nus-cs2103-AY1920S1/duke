0fcb572f74fc6b6e99f905ac496cef27b745dcf6
TheRealRavager <iskandar@u.nus.edu>
2019-08-22 12:25:53 +0800

    Add validation related methods to `Task`

--- src/main/java/duke/task/Task.java
+++ src/main/java/duke/task/Task.java
@@ -2,15 +2,52 @@ package duke.task;
 
 import duke.exception.InvalidTaskException;
 
+import java.util.LinkedList;
+
 public abstract class Task {
     protected String description;
     protected boolean isDone;
 
     public Task(String description) throws InvalidTaskException {
-        validateDescriptionNotBlank(description);
         this.description = description;
         this.isDone = false;
+        validate();
     }
+
+    // Validations
+
+    public void validate() throws InvalidTaskException {
+        LinkedList<String> validityErrors = getValidityErrors();
+        if (!isValid(validityErrors)) {
+            throw new InvalidTaskException(prettifyValidityErrors(validityErrors));
+        }
+    }
+
+    public LinkedList<String> getValidityErrors() {
+        LinkedList<String> validityErrors = new LinkedList<>();
+        if (description.isBlank()) {
+            validityErrors.add("Description cannot be blank.");
+        }
+        return validityErrors;
+    }
+
+    protected String prettifyValidityErrors(LinkedList<String> validityErrors) {
+        String prettyErrors = validityErrors.pop();
+        for (String error : validityErrors) {
+            prettyErrors += "\n" + error;
+        }
+        return prettyErrors;
+    }
+
+    public boolean isValid() {
+        return getValidityErrors().isEmpty();
+    }
+
+    protected boolean isValid(LinkedList<String> validityErrors) {
+        return validityErrors.isEmpty();
+    }
+
+    // Getter/setters
 
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
@@ -22,13 +59,6 @@ public abstract class Task {
 
     public void markAsDone() {
         isDone = true;
-    }
-
-    private void validateDescriptionNotBlank(String description) throws InvalidTaskException {
-        if (description.isEmpty()) {
-            // TODO: replace 'task' with the specific type of task, e.g. Todo
-            throw new InvalidTaskException("Task description cannot be blank!");
-        }
     }
 
     @Override

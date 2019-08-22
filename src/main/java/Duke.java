ff5609db54219e4ae43cf8ea4482fefea23022bc
SakshiPradyumn <e0313722@u.nus.edu>
2019-08-22 20:23:25 +0800

    Level-2 Final

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
-public class Duke {
 
+public class Check {
     public static void main(String[] args) {
         /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,15 +10,29 @@ public class Duke {
         System.out.println("Hello from\n" + logo);*/
 
         Scanner S = new Scanner(System.in);
+        String listOfAct[] = new String[100];
+        int i = 0;
         System.out.println("    ____________________________________________________________\n" +
                 "     Hello! I'm Duke\n" +
                 "     What can I do for you?\n" +
                 "    ____________________________________________________________\n");
-        String inp = S.next();
+        String inp = S.nextLine();
         while(!(inp.equals("bye"))) {
-            System.out.println("    ____________________________________________________________\n     " + inp +
-                    "\n    ____________________________________________________________");
-            inp = S.next();
+            if(inp.equals("list")) {
+                System.out.println("    ____________________________________________________________");
+                for (int j = 0; j < i; j++) {
+                    System.out.println("     " + (j + 1) + ". " + listOfAct[j]);
+                }
+                System.out.println("    ____________________________________________________________\n");
+                inp = S.nextLine();
+                continue;
+            }
+
+            System.out.println("    ____________________________________________________________\n" +
+                    "     added: " + inp + "\n" +
+                    "    ____________________________________________________________\n");
+            listOfAct[i++] = inp;
+            inp = S.nextLine();
         }
         System.out.println("    ____________________________________________________________\n" +
                 "     Bye. Hope to see you again soon!\n" +

c1bda4482ba1a71d3d7621b7765b29cfaabce75d
Liu Zechu <liuzechu2013@gmail.com>
2019-08-22 15:16:59 +0800

    Level-4: ToDo, Event, Deadline

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,7 +13,7 @@ public class Duke {
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
 
-        // initialise items list
+        // initialise tasks list
         tasks = new ArrayList<>();
 
         greet();
@@ -30,6 +30,7 @@ public class Duke {
     }
 
     private static void getUserInput() {
+        String addTaskMessage = "     Got it. I've added this task: ";
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
         String firstWord = userInput.split("\\s")[0];
@@ -45,7 +46,7 @@ public class Duke {
                 System.out.println("     Here are the tasks in your list:");
                 for (int i = 0; i < tasks.size(); i++) {
                     Task currentTask = tasks.get(i);
-                    System.out.println("     " + Integer.toString(i + 1) + ".[" + currentTask.getStatusIcon() + "] " + currentTask);
+                    System.out.println("     " + Integer.toString(i + 1) + "." + currentTask.getTypeIcon() + currentTask.getStatusIcon() + " " + currentTask);
                 }
                 System.out.print(LINE);
                 getUserInput();
@@ -56,12 +57,50 @@ public class Duke {
                 taskDone.markAsDone();
                 System.out.print(LINE);
                 System.out.println("     Nice! I've marked this task as done: ");
-                System.out.println("       [" + taskDone.getStatusIcon() + "] " + taskDone);
+                System.out.println("       " + taskDone.getStatusIcon() + " " + taskDone);
+                System.out.print(LINE);
+                getUserInput();
+                break;
+            case "todo":
+                String messageTodo = userInput.split("todo ")[1];
+                Task newTaskTodo = new Task(messageTodo, TaskType.TODO);
+                tasks.add(newTaskTodo);
+                System.out.print(LINE);
+                System.out.println(addTaskMessage);
+                System.out.println("       [T][✗] " + newTaskTodo);
+                System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
+                System.out.print(LINE);
+                getUserInput();
+                break;
+            case "deadline":
+                String[] deadlineMessageAndTime = userInput.split("deadline ")[1].split(" /by ");
+                String messageDeadline = deadlineMessageAndTime[0];
+                Task newTaskDeadline = new Task(messageDeadline, TaskType.DEADLINE);
+                newTaskDeadline.setTime(deadlineMessageAndTime[1]);
+                tasks.add(newTaskDeadline);
+                System.out.print(LINE);
+                System.out.println(addTaskMessage);
+                System.out.println("       [D][✗] " + newTaskDeadline);
+                System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
+                System.out.print(LINE);
+                getUserInput();
+                break;
+            case "event":
+                String[] eventMessageAndTime = userInput.split("event ")[1].split(" /at ");
+                String messageEvent = eventMessageAndTime[0];
+                Task newTaskEvent = new Task(messageEvent, TaskType.EVENT);
+                newTaskEvent.setTime(eventMessageAndTime[1]);
+                tasks.add(newTaskEvent);
+                System.out.print(LINE);
+                System.out.println(addTaskMessage);
+                System.out.println("       [E][✗] " + newTaskEvent);
+                System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
                 System.out.print(LINE);
                 getUserInput();
                 break;
             default:
-                tasks.add(new Task(userInput));
+                // not dealt with as of now
+                tasks.add(new Task(userInput, TaskType.TODO));
                 System.out.print(LINE + "     added: " + userInput + "\n" + LINE);
                 getUserInput();
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,14 +1,38 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+    protected TaskType type;
+    protected String time;
 
-    public Task(String description) {
+    public Task(String description, TaskType type) {
         this.description = description;
         this.isDone = false;
+        this.type = type;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
     }
 
     public String getStatusIcon() {
-        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+        return (isDone ? "[\u2713]" : "[\u2718]"); //return tick or X symbols
+    }
+
+    public String getTypeIcon() {
+        String result = "";
+        switch (type) {
+            case TODO:
+                result = "T";
+                break;
+            case DEADLINE:
+                result = "D";
+                break;
+            case EVENT:
+                result = "E";
+                break;
+        }
+
+        return "[" + result + "]";
     }
 
     public void markAsDone() {
@@ -17,6 +41,15 @@ public class Task {
 
     @Override
     public String toString() {
-        return description;
+        switch (type) {
+            case TODO:
+                return description;
+            case DEADLINE:
+                return description + " (by: " + time + ")";
+            case EVENT:
+                return description + " (at: " + time + ")";
+            default:
+                return description;
+        }
     }
 }

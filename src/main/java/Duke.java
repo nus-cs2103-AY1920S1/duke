d935e04d58ede969c08231b5a75fc993451b4496
hello <linmeian@gmail.com>
2019-08-22 13:42:24 +0800

    Update Duke.java
    Add echo and exit command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,15 @@ public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Duke \n What can I do for you?");
-        
+        while(sc.hasNext()) {
+           String next = sc.next(); 
+           if(next == "bye") {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+           } else {
+                System.out.println(next);
+           }
+        }
         
     }
 }

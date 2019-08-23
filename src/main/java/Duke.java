69912cc374bb2d9c66cddcd7269294f8d7250939
AhmadHatziq <e0031550@u.nus.edu>
2019-08-23 22:05:57 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -10,24 +11,43 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        String underscore = "    ____________________________________________________________";
+        String underscore = "    ____________________________________________________________" + "\n" ;
         String intro = underscore +
                        "      Hello! I'm Duke " + "\n" +
                        "      What can I do for you?" + "\n" +
                        underscore ;
         System.out.println(intro);
 
+        // Creates an ArrayList
+        ArrayList<String> tasks = new ArrayList<String>();
+
+        // Creates scanner object to handle input
         Scanner in = new Scanner(System.in);
         String command = in.nextLine();
         String output = "";
 
         while ( !command.equals("bye") ){
-            output = underscore + "\n" + "      " + command + "\n" + underscore + "\n";
-            System.out.print(output);
+
+            if ( command.equals("list")){
+                output = underscore;
+                for (int i = 0; i < tasks.size(); i++){
+                    output +=  "     " + (i+1) + ". " + tasks.get(i) + "\n";
+                }
+                output += underscore;
+
+                System.out.println(output);
+            } else {
+                tasks.add(command);
+                output = underscore + "     " + "added: " + command + "\n" + underscore;
+                System.out.println(output);
+            }
+
             command = in.nextLine();
+            output = "";
         }
 
-        output = underscore + "\n" + "      " + "Bye. Hope to see you again soon!" + "\n" + underscore + "\n";
+        // Prints goodbye sequence
+        output = underscore + "\n" + "     " + "Bye. Hope to see you again soon!" + "\n" + underscore + "\n";
         System.out.print(output);
     }
 }

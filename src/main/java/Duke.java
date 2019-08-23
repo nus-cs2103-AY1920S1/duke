391fe37c1e0f34f5d9e97edb00988f285cf81b5f
lingzw <lingzw64@gmail.com>
2019-08-23 22:18:53 +0800

    implemented level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import com.sun.source.tree.WhileLoopTree;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +8,23 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo + "    ____________________________________________________________\n" +
+                "     Hello! I'm Duke\n" +
+                "     What can I do for you?\n" +
+                "    ____________________________________________________________");
+
+        Scanner input = new Scanner(System.in);
+        String user = input.nextLine();
+
+        while (!user.equals("bye")) {
+            System.out.println("    ____________________________________________________________\n" +
+                    "     " + user +"\n" +
+                    "    ____________________________________________________________");
+            user =input.nextLine();
+        }
+
+        System.out.print("    ____________________________________________________________\n" +
+                "     Bye. Hope to see you again soon!\n" +
+                "    ____________________________________________________________");
     }
 }

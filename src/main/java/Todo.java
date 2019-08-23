68d1a6c268a879b6d92c7cb803353cec1df66b2f
lrchema <hema@Hemas-MacBook-Pro-2.local>
2019-08-23 22:39:18 +0800

    Read and write task list to file

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -5,4 +5,7 @@ public class Todo extends Task{
     public String toString(){
         return "[T]"+super.toString();
     }
+    public String toFile(){
+        return "T,"+super.toFile();
+    }
 }

46c55c6a505d9e9db90971f7d10d8a1853219c74
Ryan Chew <ryanchewjingrong1997@gmail.com>
2019-08-23 16:32:03 +0800

    Implement list add and show features.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.function.Function;
 
 public class Duke {
 
@@ -11,24 +15,59 @@ public class Duke {
 		"____________________________________________________________";
 
 	private static void say(String... lines) {
+		say(Arrays.asList(lines).iterator());
+	}
+	private static void say(Iterator<String> lines) {
+		//Print start of reply line
 		System.out.println(lineRule);
-		for(String line : lines) {
-			System.out.println(line);
+
+		//Print each given line
+		while(lines.hasNext()) {
+			System.out.println(lines.next());
 		}
+		//Print end of reply line, and extra empty line
 		System.out.println(lineRule);
 		System.out.println();
 	}
 
+	static class CounterDecorator implements Function<String, String> {
+		private int counter;
+		public CounterDecorator(int start) {
+			this.counter = start;
+		}
+		public String apply(String str) {
+			String ret = String.format("%d: %s", this.counter, str);
+			this.counter++;
+			return ret;
+		}
+	}
+
     public static void main(String[] args) {
-		say(initialGreeting);
+		//Initialize input.
 		Scanner s = new Scanner(System.in);
+		ArrayList<String> storageList = new ArrayList<>();
+
+		//Start off greeting the user.
+		say(initialGreeting);
+
+		//While there is still input from user
+inputLoop:
 		while(s.hasNextLine()) {
-			String userInput = s.nextLine();
-			if("bye".equals(userInput.trim())) {
+			//Read single line of user input, and remove extra spaces
+			String userInput = s.nextLine().trim();
+
+			switch(userInput) {
+			case "bye": 
 				say("Bye. Hope to see you again soon!");
+				break inputLoop;
+			case "list":
+				say(storageList.stream().map(new CounterDecorator(1)).iterator());
+				break;
+			default:
+				say("added: " + userInput);
+				storageList.add(userInput);
 				break;
 			}
-			say(userInput);
 		}
     }
 }

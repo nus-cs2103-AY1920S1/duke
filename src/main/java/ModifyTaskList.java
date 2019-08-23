d4cb596a8e105fff6648505deb53eb6e97a9cfcb
TanZhanMing <e0349703@u.nus.edu>
2019-08-23 15:34:41 +0800

    Level 6 + enums

---
+++ src/main/java/ModifyTaskList.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class ModifyTaskList {
+
+    public static void modifyTaskList (ArrayList<Task> taskList, Task t, Duke.Action action){
+        if (action == Duke.Action.ADD) {
+            taskList.add(t);
+            System.out.println("Got it. I've added this task:");
+            System.out.println(taskList.get(taskList.size() - 1).toString());
+            System.out.println("Now you have " + taskList.size() + " tasks in the list.\n");
+        }
+    }
+    public static void modifyTaskList (ArrayList<Task> taskList, int taskNumber, Duke.Action action){
+        if (action == Duke.Action.REMOVE) {
+            try {
+                System.out.println("Noted. I've removed this task:\n" + taskList.get(taskNumber).toString());
+                taskList.remove(taskNumber);
+                System.out.println("Now you have " + taskList.size() + " tasks in the list.\n");
+            }
+            catch (IndexOutOfBoundsException err){
+                System.out.println("You only have " + taskList.size() + " tasks, please choose a number from that\n");
+            }
+        }
+        if (action == Duke.Action.DONE){
+            try {
+                taskList.get(taskNumber).setDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(taskList.get(taskNumber).toString() + "\n");
+            }
+            catch (IndexOutOfBoundsException err){
+                System.out.println("You only have " + taskList.size() + " tasks, please choose a number from that\n");
+            }
+        }
+    }
+}

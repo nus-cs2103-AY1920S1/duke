dc571e82742969d080865d63ee4b0d3cefc13b7f
cheongliyuan <e0008064@u.nus.edu>
2019-08-23 15:12:03 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,57 @@
+/* Java beginner here. Please point out any any 'weird' or unconventional coding style! Thank you -Li Yuan*/
+
+import java.util.Scanner;
+
+class User {
+    final String EXIT_COMMAND = "bye";
+
+    private String currentInput = "";
+
+    public User() {
+        System.out.println("    ____________________________________________________________\n" +
+                "     Hello! I'm Duke\n" +
+                "     What can I do for you?\n" +
+                "    ____________________________________________________________\n");
+    }
+
+    public String getCurrentInput() {
+        return this.currentInput;
+    }
+
+    public void setCurrentInput(String currentInput) {
+        this.currentInput = currentInput;
+    }
+
+    public void printCurrentInput() {
+        System.out.println("    ____________________________________________________________\n" +
+                "     " + this.getCurrentInput() + "\n" +
+                "    ____________________________________________________________");
+    }
+
+    public void sayByeToUser() {
+        System.out.println("    ____________________________________________________________\n" +
+                "     Bye. Hope to see you again soon!\n" +
+                "    ____________________________________________________________\n");
+    }
+
+    //Saw Shui Yao's post, thought was a good detail. hence equalsIgnoreCase.
+    public boolean inputIsBye (){
+        return this.currentInput.equalsIgnoreCase(EXIT_COMMAND);
+    }
+
+}
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        User user = new User();
+        Scanner input = new Scanner(System.in);
+
+        while(!user.inputIsBye()) {
+            user.setCurrentInput(input.nextLine());
+            if (!user.inputIsBye()) {
+                user.printCurrentInput();
+            }
+        }
+        user.sayByeToUser();
     }
 }

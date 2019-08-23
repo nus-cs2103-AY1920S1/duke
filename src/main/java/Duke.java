a39274f5df8d09e8f66241f034a8d08c82b0bc5f
AhmadHatziq <e0031550@u.nus.edu>
2019-08-23 22:51:51 +0800

    Level 3, add the ability to mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,28 +18,45 @@ public class Duke {
                        underscore ;
         System.out.println(intro);
 
-        // Creates an ArrayList
-        ArrayList<String> tasks = new ArrayList<String>();
+        // Creates an ArrayList of Task objects
+        ArrayList<Task> tasks = new ArrayList<Task>();
 
         // Creates scanner object to handle input
         Scanner in = new Scanner(System.in);
         String command = in.nextLine();
         String output = "";
+        int taskNum = -1;
 
         while ( !command.equals("bye") ){
 
             if ( command.equals("list")){
-                output = underscore;
+
+                output = underscore + "     Here are the tasks in your list:\n";
                 for (int i = 0; i < tasks.size(); i++){
-                    output +=  "     " + (i+1) + ". " + tasks.get(i) + "\n";
+                    output +=  "     " + (i+1) + ".[" + tasks.get(i).getStatusIcon() + "] " + tasks.get(i).description + "\n";
                 }
                 output += underscore;
 
                 System.out.println(output);
+
+            } else if ( command.substring(0,4).equals("done")){
+
+                taskNum = Integer.parseInt(command.substring(5));
+                taskNum--; // ArrayList index == taskNum - 1
+                tasks.get(taskNum).setDone();
+
+                output = underscore + "     Nice! I've marked this task as done:\n" +
+                        "       [" + tasks.get(taskNum).getStatusIcon() + "] " + tasks.get(taskNum).description +
+                        "\n" + underscore;
+
+                System.out.println(output);
+
             } else {
-                tasks.add(command);
+
+                tasks.add(new Task(command));
                 output = underscore + "     " + "added: " + command + "\n" + underscore;
                 System.out.println(output);
+
             }
 
             command = in.nextLine();
@@ -50,4 +67,8 @@ public class Duke {
         output = underscore + "\n" + "     " + "Bye. Hope to see you again soon!" + "\n" + underscore + "\n";
         System.out.print(output);
     }
+
 }
+
+
+

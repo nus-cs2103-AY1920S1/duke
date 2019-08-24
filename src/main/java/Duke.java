ca118072d76331d175fce776290795d9046e4f5d
Gary Lim <garylyp@hotmail.com>
2019-08-24 22:12:12 +0800

    Minor refactoring to task adding processes.
    Changes made in this commit:
    1) Separate the handling of actions and the adding of Tasks to
       taskList.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
 package main.java;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -9,18 +12,21 @@ public class Duke {
 
     private static String divider = "    ____________________________________________________________\n";
     private static List<Task> taskList = new ArrayList<>();
+    private static File file;
 
-    public static void main(String[] args) throws DukeException {
+    public static void main(String[] args) throws DukeException, FileNotFoundException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|q\\_\\___|\n";
+                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.print(divider);
         System.out.print("\t Hello! I'm Duke\n");
         System.out.print("\t What can I do for you?\n");
         System.out.print(divider);
+
+//        loadData();
 
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
@@ -44,20 +50,17 @@ public class Duke {
 
             } else if (action.equals("todo")) {
                 System.out.print(divider);
-                String taskName = sc.nextLine().trim();
-                addNewTodo(taskName);
+                onTodoActionCalled(sc);
                 System.out.print(divider);
 
             } else if (action.equals("deadline")) {
                 System.out.print(divider);
-                String taskName = sc.nextLine().trim();
-                addNewDeadline(taskName);
+                onDeadlineActionCalled(sc);
                 System.out.print(divider);
 
             } else if (action.equals("event")) {
                 System.out.print(divider);
-                String taskName = sc.nextLine().trim();
-                addNewEvent(taskName);
+                onEventActionCalled(sc);
                 System.out.print(divider);
 
             } else if (action.equals("delete")) {
@@ -138,13 +141,13 @@ public class Duke {
         }
     }
 
-    private static void addNewTodo(String taskName) {
+    private static void onTodoActionCalled(Scanner sc) {
+        String taskName = sc.nextLine().trim();
         try {
             if (taskName.isEmpty()) {
                 throw new EmptyDescriptionException();
             }
-            Task newTask = new Todo(taskName);
-            taskList.add(newTask);
+            Task newTask = addNewTodo(taskName, false);
             printAddTaskMessage(newTask);
 
         } catch (EmptyDescriptionException e) {
@@ -153,15 +156,15 @@ public class Duke {
 
     }
 
-    private static void addNewDeadline(String taskName) {
+    private static void onDeadlineActionCalled(Scanner sc) {
+        String taskName = sc.nextLine().trim();
         String[] taskInfo = taskName.split("\\s*/by\\s*");
         try {
 
             if (taskName.isEmpty() || taskInfo[0].trim().isEmpty()) {
                 throw new EmptyDescriptionException();
             }
-            Task newTask = new Deadline(taskInfo[0], taskInfo[1]);
-            taskList.add(newTask);
+            Task newTask = addNewDeadline(taskInfo[0], taskInfo[1], false);
             printAddTaskMessage(newTask);
 
         } catch (EmptyDescriptionException e) {
@@ -171,34 +174,92 @@ public class Duke {
             System.out.printf("     ☹ OOPS!!! There must be exactly one argument before and\n" +
                     "     one argument after the keyword %s.\n", "/by");
         }
-
-
     }
 
-    private static void addNewEvent(String taskName) {
+    private static void onEventActionCalled(Scanner sc) {
+        String taskName = sc.nextLine().trim();
         String[] taskInfo = taskName.split("\\s*/at\\s*");
         try {
             if (taskName.isEmpty() || taskInfo[0].trim().isEmpty()) {
                 throw new EmptyDescriptionException();
             }
-            Task newTask = new Event(taskInfo[0], taskInfo[1]);
-            taskList.add(newTask);
+            Task newTask = addNewEvent(taskInfo[0], taskInfo[1], false);
             printAddTaskMessage(newTask);
-
         } catch (EmptyDescriptionException e) {
             System.out.printf("     ☹ OOPS!!! The description of a %s cannot be empty.\n", "event");
-
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.printf("     ☹ OOPS!!! There must be exactly one argument before and\n" +
                     "     one argument after the keyword %s.\n", "/at");
         }
-
     }
+
+    private static Task addNewTodo(String taskName, boolean isDone) {
+        Task newTask = new Todo(taskName);
+        if (isDone) {
+            newTask.setDone();
+        } else {
+            newTask.setNotDone();
+        }
+        taskList.add(newTask);
+        return newTask;
+    }
+
+    private static Task addNewEvent(String taskName, String additionalInfo, boolean isDone) {
+        Task newTask = new Event(taskName, additionalInfo);
+        if (isDone) {
+            newTask.setDone();
+        } else {
+            newTask.setNotDone();
+        }
+        taskList.add(newTask);
+        return newTask;
+    }
+
+    private static Task addNewDeadline(String taskName, String additionalInfo, boolean isDone) {
+        Task newTask = new Deadline(taskName, additionalInfo);
+        if (isDone) {
+            newTask.setDone();
+        } else {
+            newTask.setNotDone();
+        }
+        taskList.add(newTask);
+        return newTask;
+    }
+
 
     private static void printAddTaskMessage(Task task) {
         System.out.print("     Got it. I've added this task:\n");
         System.out.printf("       %s\n", task);
         System.out.printf("     Now you have %d tasks in the list.\n", taskList.size());
+    }
+
+    private static void loadData() throws FileNotFoundException {
+        file = new File("data/fruits.txt");
+        if (file.exists()) {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNext()) {
+                String[] taskInfo = sc.nextLine().split("\\s*|\\s*");
+                switch (taskInfo[0]) {
+                    case "T":
+                        addNewTodo(taskInfo[2], taskInfo[1].equals("1"));
+                        break;
+
+                    case "D":
+                        addNewDeadline(taskInfo[2], taskInfo[3], taskInfo[1].equals("1"));
+                        break;
+
+                    case "E":
+                        addNewEvent(taskInfo[2], taskInfo[3], taskInfo[1].equals("1"));
+                        break;
+                }
+            }
+        } else {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }

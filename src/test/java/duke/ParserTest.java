8502095553ab5181890b53bb15d6d594228ef604
Tan Yuanhong <leotan.yh@gmail.com>
2019-08-24 19:23:47 +0800

    Added JUnit test for parser

---
+++ src/test/java/duke/ParserTest.java
@@ -0,0 +1,22 @@
+package duke;
+
+import duke.command.ExitCommand;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ParserTest {
+    @Test
+    public void parseTest() {
+        try {
+            assertTrue(Parser.parse("bye") instanceof ExitCommand);
+            assertThrows(DukeException.class, () -> {
+                        Parser.parse("byesth");
+                    }
+            );
+        } catch (DukeException e) {
+            e.printStackTrace();
+        }
+    }
+}

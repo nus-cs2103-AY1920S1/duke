d465c87c14e2482b873bc41670a7d2f7b6440271
aidilfbk <e0303295@u.nus.edu>
2019-08-24 16:10:37 +0800

    AddCommand instances now trigger arguments parsing on instance creation
    Previously triggered on calling the execute() function

--- src/main/java/AddCommand.java
+++ src/main/java/AddCommand.java
@@ -4,19 +4,20 @@ import java.util.List;
 abstract class AddCommand implements Command {
     protected final String command;
     protected final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+    protected final Task task;
 
     public AddCommand(String command) {
         this.command = command;
+        task = instantiateTask();
     }
 
     abstract protected Task instantiateTask();
 
     public void execute(List<Task> tasks, Ui ui, Storage storage) {
-        final Task t = instantiateTask();
-        tasks.add(t);
+        tasks.add(task);
 
         storage.save(tasks);
-        ui.displaySuccessfullyAddedTask("Got it. I've added this task: ", t, tasks.size());
+        ui.displaySuccessfullyAddedTask("Got it. I've added this task: ", task, tasks.size());
 
     }
 }

53462a23c9599d9a7204fbaaa842c7a2379e8ef2
Russell Loh <e0310763@u.nus.edu>
2019-08-24 23:21:13 +0800

    -Changed String type to LocalDateTime type for Deadline and Event -Changed date print out for Deadline and Event classes to print the date string

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,20 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class Deadline extends Task {
 
-    private String by;
+    private LocalDateTime by;
     public Deadline(String description, String by){
         super(description);
 
-        this.by = by;
+        this.by = LocalDateTime.parse(by,
+                    DateTimeFormatter.ofPattern("[d/MM/yyyy HHmm][dd/M/yyyy HHmm][d/M/yyyy HHmm][dd/MM/yyyy HHmm]"));
+
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("MMMM dd, yyyy 'at' HHmm", Locale.US)) + ")";
     }
 }

a6b0667f694c584aecc6134a3872804879c51ac5
chowyiyin <yiyin.chow@gmail.com>
2019-08-24 11:38:09 +0800

    Add DukeException Class

---
+++ src/main/java/DukeException.java
@@ -0,0 +1,12 @@
+public class DukeException extends Exception {
+
+    protected String message;
+
+    public DukeException(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

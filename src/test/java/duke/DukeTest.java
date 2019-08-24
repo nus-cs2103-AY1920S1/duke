10faaae7310b93ae040b1738e90bf58922673a37
Matt Lee <crazoter@gmail.com>
2019-08-25 00:17:04 +0800

    Added gradle, checkstyle, shadowjar, junit

---
+++ src/test/java/duke/DukeTest.java
@@ -0,0 +1,12 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DukeTest {
+    @Test
+    public void dummyTest(){
+        assertEquals(2, 2);
+    }
+}

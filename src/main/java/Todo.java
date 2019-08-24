34c48a3eb45d9f3593b293da75b47e06851d1311
hoholyin <holyin97@gmail.com>
2019-08-24 23:11:15 +0800

    Level-7

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,13 +1,17 @@
 
 public class Todo extends Task {
-  public Todo(String n, int index) {
-    super(n, index);
+  public Todo(String name, int index) {
+      super(name, index);
+  }
+
+  public Todo (String name, int index, boolean completed) {
+      super(name, index, completed);
   }
   @Override
   public String toString() {
-    String result = "[T][";
-    result = this.completed ? result + "✓]" : result + "✗]";
-    result += " " + this.name;
-    return result;
+      String result = "[T][";
+      result = this.completed ? result + "✓]" : result + "✗]";
+      result += " " + this.name;
+      return result;
   }
 }

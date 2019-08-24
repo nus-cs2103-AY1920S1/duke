92a5be98572aa04973ff8c2b75f55ba57324bc18
Jeremy <tan_kai_qun97@yahoo.com.sg>
2019-08-24 17:21:55 +0800

    Change Echoer methods to static

--- src/main/java/Echoer.java
+++ src/main/java/Echoer.java
@@ -1,23 +1,24 @@
 public class Echoer {
-    private String formatMessage(String message) {
+    
+    private static String formatMessage(String message) {
         return "     " + message + "\n";
     }
 
-    void echo(String... messages) {
+    static void echo(String... messages) {
         String border = "    ____________________________________________________________\n";
         String toEcho = border;
         for (String message : messages) {
-            toEcho += this.formatMessage(message);
+            toEcho += formatMessage(message);
         }
         toEcho += border;
         System.out.println(toEcho);
     }
 
-    void greet() {
-        this.echo("Hello! I'm Jermi", "What can I do for you?");
+    static void greet() {
+        echo("Hello! I'm Jermi", "What can I do for you?");
     }
 
-    void exit() {
-        this.echo("Bye. Hope to see you again soon!");
+    static void exit() {
+        echo("Bye. Hope to see you again soon!");
     }
 }

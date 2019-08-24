827e5d198aee51fc8114585588242fbc03efc39a
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2019-08-24 16:54:57 +0800

    Create date parser class

---
+++ src/main/java/DateParser.java
@@ -0,0 +1,13 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.ParseException;
+
+public class DateParser {
+
+    public static final String DATE_FORMAT = "dd-MM-yy hhmm";
+    private static final SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat(DATE_FORMAT);
+
+    public static Date parse(String str) throws ParseException {
+        return DATE_FORMATTER.parse(str);
+    }
+}

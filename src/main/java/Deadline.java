1fb94587809450701dd8bc06429999849f112c1b
jeongyh99 <jeong.yuhan@gmail.com>
2019-08-24 21:07:37 +0800

    Minor changes to string formatting

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -98,7 +98,7 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "D " + super.toString() + "| " + by;
     }
 }
 

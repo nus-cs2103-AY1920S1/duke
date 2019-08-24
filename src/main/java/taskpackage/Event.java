856df2296bd728dcbc700b617db799a4006d10be
honhaochen <haochenhon@hotmail.com>
2019-08-24 16:45:21 +0800

    branch-level-7

--- src/main/java/taskpackage/Event.java
+++ src/main/java/taskpackage/Event.java
@@ -9,6 +9,18 @@ public class Event extends Task {
         this.date = date;
     }
 
+    public Event(String isDone, String description, String date) {
+        super(description);
+        this.date = date;
+        if (isDone.equals("\u2713")) {
+            this.isDone = true;
+        }
+    }
+
+    protected String toDataBase() {
+        return "[E] | " + getStatusIcon() + " | " + description + " | " + date;
+    }
+
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (at: " + date + ")";
--- src/main/java/taskpackage/Task.java
+++ src/main/java/taskpackage/Task.java
@@ -1,12 +1,68 @@
 package taskpackage;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Task {
     protected String description;
     protected boolean isDone;
 
+    public static List<Task> store;
+
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+    }
+
+    public static void instantiate() {
+        //initialize Task list
+        store = new ArrayList<Task>();
+
+        try {
+            Scanner sc = new Scanner(new File("../../../data/duke.txt"));
+
+            while (sc.hasNext()) {
+                String[] input = sc.nextLine().split("\\|");
+
+                //strip any leading spaces
+                for (int i = 0; i < input.length; i++) {
+                    input[i] = input[i].strip();
+                }
+
+                if (input[0].equals("[T]")) {
+                    store.add(new ToDo(input[1], input[2]));
+                } else if (input[0].equals("[D]")) {
+                    store.add(new Deadline(input[1], input[2], input[3]));
+                } else { //Event task
+                    store.add(new Event(input[1], input[2], input[3]));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println(e);
+        }
+    }
+
+    public static void storeToDatabase() {
+        try {
+            PrintWriter pw = new PrintWriter("../../../data/duke.txt");
+            pw.close();
+            PrintWriter pw2 = new PrintWriter("../../../data/duke.txt");
+            for (Task task : store) {
+                pw2.println(task.toDataBase());
+            }
+            pw2.close();
+        } catch (FileNotFoundException e){
+            System.err.println(e);
+        }
+    }
+
+    //Will be Overridden in child class
+    protected String toDataBase() {
+        return "";
     }
 
     public String getStatusIcon() {

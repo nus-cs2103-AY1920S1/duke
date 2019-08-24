dc8150ccd61c5e14833cc0511a0796df729367f6
lye-jw <lyejianwen@gmail.com>
2019-08-24 23:10:00 +0800

    Level-8 Dates and Times

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -14,101 +17,136 @@ public class Duke {
                 + separationLine + "\n");
 
         ArrayList<Task> taskStore = new ArrayList<>(100);
+        ArrayList<SimpleDateFormat> dateFormats = new ArrayList<>() {
+            {
+                add(new SimpleDateFormat("dd/M/yyyy h a"));
+                add(new SimpleDateFormat("dd-M-yyyy h a"));
+                add(new SimpleDateFormat("dd.M.yyyy h a"));
+                add(new SimpleDateFormat("dd/M/yyyy hhmm a"));
+                add(new SimpleDateFormat("dd-M-yyyy hhmm a"));
+                add(new SimpleDateFormat("dd.M.yyyy hhmm a"));
+                add(new SimpleDateFormat("dd/M/yyyy hh:mm a"));
+                add(new SimpleDateFormat("dd-M-yyyy hh:mm a"));
+                add(new SimpleDateFormat("dd.M.yyyy hh:mm a"));
+                add(new SimpleDateFormat("dd/M/yyyy hhmm"));
+                add(new SimpleDateFormat("dd-M-yyyy hhmm"));
+                add(new SimpleDateFormat("dd.M.yyyy hhmm"));
+                add(new SimpleDateFormat("dd/M/yyyy hh:mm"));
+                add(new SimpleDateFormat("dd-M-yyyy hh:mm"));
+                add(new SimpleDateFormat("dd.M.yyyy hh:mm"));
+                add(new SimpleDateFormat("dd/M/yyyy HHmm"));
+                add(new SimpleDateFormat("dd-M-yyyy HHmm"));
+                add(new SimpleDateFormat("dd.M.yyyy HHmm"));
+                add(new SimpleDateFormat("dd/M/yyyy HH:mm"));
+                add(new SimpleDateFormat("dd-M-yyyy HH:mm"));
+                add(new SimpleDateFormat("dd.M.yyyy HH:mm"));
+                add(new SimpleDateFormat("dd MMM yyyy HHmm"));
+                add(new SimpleDateFormat("dd MMMM yyyy HHmm"));
+                add(new SimpleDateFormat("dd/M/yyyy"));
+                add(new SimpleDateFormat("dd-M-yyyy"));
+                add(new SimpleDateFormat("dd.M.yyyy"));
+                add(new SimpleDateFormat("dd MMM yyyy"));
+                add(new SimpleDateFormat("dd MMMM yyyy"));
+            }
+        };
         String userInput = scan.nextLine();
         while (!"bye".equals(userInput)) {
             try {
                 String[] inputSplit = userInput.split(" ");
                 switch (inputSplit[0]) {
-                    case "list":
-                        System.out.println(separationLine + "\n     Here are the tasks in your list:");
-                        for (Task task : taskStore) {
-                            System.out.println("     " + (taskStore.indexOf(task) + 1) + "." + task.toString());
-                        }
-                        System.out.println(separationLine + "\n");
-                        break;
-                    case "done":
-                        if (inputSplit.length != 2) {
-                            // Exception if there is no task number or multiple words after "done"
-                            throw new DukeException(separationLine +
-                                    "\n     \u2639 OOPS!!! Please specify number of a single task to mark as done.\n" + separationLine + "\n");
-                        }
-                        int specifiedDone = Integer.parseInt(inputSplit[1]); // will throw NumberFormatException if not int after "done"
-                        if (specifiedDone < 1 || specifiedDone > taskStore.size()) {
-                            // Exception if task number is beyond current number of tasks
-                            throw new DukeException(separationLine +
-                                    "\n     \u2639 OOPS!!! Please specify valid task number.\n" + separationLine + "\n");
-                        }
-                        Task doneTask = taskStore.get(specifiedDone - 1);
-                        doneTask.setDone();
-                        System.out.println(separationLine + "\n     Nice! I've marked this task as done:\n       "
-                                + doneTask + "\n" + separationLine + "\n");
-                        break;
-                    case "todo":
-                        if (inputSplit.length == 1) {
-                            // Exception if no description after "todo"
-                            throw new DukeException(separationLine +
-                                    "\n     \u2639 OOPS!!! The description of a todo cannot be empty.\n" + separationLine + "\n");
-                        }
-                        ToDo todo = new ToDo(userInput.replace("todo ", ""));
-                        taskStore.add(todo);
-                        System.out.println(separationLine + "\n     Got it. I've added this task:\n       " + todo
-                                + "\n     Now you have " + taskStore.size() + " tasks in the list." + "\n"
-                                + separationLine + "\n");
-                        break;
-                    case "deadline":
-                        if (!userInput.contains(" /by ")) {
-                            // Exception for invalid deadline format
-                            throw new DukeException(separationLine +
-                                    "\n     \u2639 OOPS!!! For deadline please use the format\n" +
-                                    "               \"deadline description /by end time\"\n"
-                                    + separationLine + "\n");
-                        }
-                        String[] splitStringD = userInput.split(" /by ");
-                        Deadline deadline = new Deadline(splitStringD[0].replace("deadline ", ""),
-                                splitStringD[1]);
-                        taskStore.add(deadline);
-                        System.out.println(separationLine + "\n     Got it. I've added this task:\n       " + deadline
-                                + "\n     Now you have " + taskStore.size() + " tasks in the list." + "\n"
-                                + separationLine + "\n");
-                        break;
-                    case "event":
-                        if (!userInput.contains(" /at ")) {
-                            // Exception for invalid deadline format
-                            throw new DukeException(separationLine +
-                                    "\n     \u2639 OOPS!!! For event please use the format\n" +
-                                    "               \"event description /at period\"\n"
-                                    + separationLine + "\n");
-                        }
-                        String[] splitStringE = userInput.split(" /at ");
-                        Event event = new Event(splitStringE[0].replace("event ", ""), splitStringE[1]);
-                        taskStore.add(event);
-                        System.out.println(separationLine + "\n     Got it. I've added this task:\n       " + event
-                                + "\n     Now you have " + taskStore.size() + " tasks in the list." + "\n"
-                                + separationLine + "\n");
-                        break;
-                    case "delete":
-                        if (inputSplit.length != 2) {
-                            // Exception if there is no task number or multiple words after "delete"
-                            throw new DukeException(separationLine +
-                                    "\n     \u2639 OOPS!!! Please specify number of a single task to delete.\n" + separationLine + "\n");
-                        }
-                        int specifiedDel = Integer.parseInt(inputSplit[1]); // will throw NumberFormatException if not int after "done"
-                        if (specifiedDel < 1 || specifiedDel > taskStore.size()) {
-                            // Exception if task number is beyond current number of tasks
-                            throw new DukeException(separationLine +
-                                    "\n     \u2639 OOPS!!! Please specify valid task number.\n" + separationLine + "\n");
-                        }
-                        Task delTask = taskStore.get(specifiedDel - 1);
-                        taskStore.remove(specifiedDel - 1);
-                        System.out.println(separationLine + "\n     Noted. I've removed this task:\n       " + delTask
-                                + "\n     Now you have " + taskStore.size() + " tasks in the list." + "\n"
-                                + separationLine + "\n");
-                        break;
-                    default:
-                        // Exception if invalid instruction
+                case "list":
+                    System.out.println(separationLine + "\n     Here are the tasks in your list:");
+                    for (Task task : taskStore) {
+                        System.out.println("     " + (taskStore.indexOf(task) + 1) + "." + task.toString());
+                    }
+                    System.out.println(separationLine + "\n");
+                    break;
+                case "done":
+                    if (inputSplit.length != 2) {
+                        // Exception if there is no task number or multiple words after "done"
                         throw new DukeException(separationLine +
-                                "\n     \u2639 OOPS!!! I'm sorry, but I don't know what that means :-(\n"
+                                "\n     \u2639 OOPS!!! Please specify number of a single task to mark as done.\n" + separationLine + "\n");
+                    }
+                    int specifiedDone = Integer.parseInt(inputSplit[1]); // will throw NumberFormatException if not int after "done"
+                    if (specifiedDone < 1 || specifiedDone > taskStore.size()) {
+                        // Exception if task number is beyond current number of tasks
+                        throw new DukeException(separationLine +
+                                "\n     \u2639 OOPS!!! Please specify valid task number.\n" + separationLine + "\n");
+                    }
+                    Task doneTask = taskStore.get(specifiedDone - 1);
+                    doneTask.setDone();
+                    System.out.println(separationLine + "\n     Nice! I've marked this task as done:\n       "
+                            + doneTask + "\n" + separationLine + "\n");
+                    break;
+                case "todo":
+                    if (inputSplit.length == 1) {
+                        // Exception if no description after "todo"
+                        throw new DukeException(separationLine +
+                                "\n     \u2639 OOPS!!! The description of a todo cannot be empty.\n" + separationLine + "\n");
+                    }
+                    ToDo todo = new ToDo(userInput.replace("todo ", ""));
+                    taskStore.add(todo);
+                    System.out.println(separationLine + "\n     Got it. I've added this task:\n       " + todo
+                            + "\n     Now you have " + taskStore.size() + " tasks in the list." + "\n"
+                            + separationLine + "\n");
+                    break;
+                case "deadline":
+                    if (!userInput.contains(" /by ")) {
+                        // Exception for invalid deadline format
+                        throw new DukeException(separationLine +
+                                "\n     \u2639 OOPS!!! For deadline please use the format\n" +
+                                "               \"deadline description /by end time\"\n"
                                 + separationLine + "\n");
+                    }
+                    String[] splitStringD = userInput.split(" /by ");
+                    Date inputDateD = convertToDate(splitStringD[1], dateFormats);
+                    String inputDateStrD = inputDateD == null ? splitStringD[1]
+                            : new SimpleDateFormat("dd MMM yyyy, hh:mm a").format(inputDateD);
+                    Deadline deadline = new Deadline(splitStringD[0].replace("deadline ", ""),
+                            inputDateStrD);
+                    taskStore.add(deadline);
+                    System.out.println(separationLine + "\n     Got it. I've added this task:\n       " + deadline
+                            + "\n     Now you have " + taskStore.size() + " tasks in the list." + "\n"
+                            + separationLine + "\n");
+                    break;
+                case "event":
+                    if (!userInput.contains(" /at ")) {
+                        // Exception for invalid deadline format
+                        throw new DukeException(separationLine +
+                                "\n     \u2639 OOPS!!! For event please use the format\n" +
+                                "               \"event description /at period\"\n"
+                                + separationLine + "\n");
+                    }
+                    String[] splitStringE = userInput.split(" /at ");
+                    Event event = new Event(splitStringE[0].replace("event ", ""), splitStringE[1]);
+                    taskStore.add(event);
+                    System.out.println(separationLine + "\n     Got it. I've added this task:\n       " + event
+                            + "\n     Now you have " + taskStore.size() + " tasks in the list." + "\n"
+                            + separationLine + "\n");
+                    break;
+                case "delete":
+                    if (inputSplit.length != 2) {
+                        // Exception if there is no task number or multiple words after "delete"
+                        throw new DukeException(separationLine +
+                                "\n     \u2639 OOPS!!! Please specify number of a single task to delete.\n" + separationLine + "\n");
+                    }
+                    int specifiedDel = Integer.parseInt(inputSplit[1]); // will throw NumberFormatException if not int after "done"
+                    if (specifiedDel < 1 || specifiedDel > taskStore.size()) {
+                        // Exception if task number is beyond current number of tasks
+                        throw new DukeException(separationLine +
+                                "\n     \u2639 OOPS!!! Please specify valid task number.\n" + separationLine + "\n");
+                    }
+                    Task delTask = taskStore.get(specifiedDel - 1);
+                    taskStore.remove(specifiedDel - 1);
+                    System.out.println(separationLine + "\n     Noted. I've removed this task:\n       " + delTask
+                            + "\n     Now you have " + taskStore.size() + " tasks in the list." + "\n"
+                            + separationLine + "\n");
+                    break;
+                default:
+                    // Exception if invalid instruction
+                    throw new DukeException(separationLine +
+                            "\n     \u2639 OOPS!!! I'm sorry, but I don't know what that means :-(\n"
+                            + separationLine + "\n");
                 }
             } catch (DukeException de) {
                 System.err.println(de.getMessage());
@@ -120,5 +158,20 @@ public class Duke {
             userInput = scan.nextLine();
         }
         System.out.println(separationLine + "\n     Bye. Hope to see you again soon!\n" + separationLine);
+    }
+
+    private static Date convertToDate(String dateString, ArrayList<SimpleDateFormat> dateFormats) {
+        Date date = null;
+        for (SimpleDateFormat sdf : dateFormats) {
+            try {
+                sdf.setLenient(false);
+                date = sdf.parse(dateString);
+            } catch (ParseException pe) {
+                 // Continue checking for matching date format
+            }
+            if (date != null) break;
+        }
+        System.out.println("Here " + date);
+        return date;
     }
 }

3d912d04d39030a3e3d447eee278a61fbdd19840
AHaliq <liqo12@hotmail.com>
2019-08-25 15:52:45 +0800

    added JUnit with dummy test

---
+++ src/test/java/DukeTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class DukeTest {
+    @Test
+    void placeHolder() {
+        assertEquals(2, 1 + 1);
+    }
+}

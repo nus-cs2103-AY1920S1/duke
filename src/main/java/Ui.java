af4862d2aa69545104768e4e1d67d3c6abf668b4
joloong <joelloong@gmail.com>
2019-08-25 13:30:16 +0800

    Change all methods to static

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -2,15 +2,15 @@ import java.util.ArrayList;
 
 public class Ui {
 
-    public void startOfInteractions() {
+    public static void startOfInteractions() {
         System.out.println("Hello I'm Duke\n" + "What can I do for you?");
     }
 
-    public void endOfInteractions() {
+    public static void endOfInteractions() {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    public void printList(ArrayList<Task> task, int counter) {
+    public static void printList(ArrayList<Task> task, int counter) {
         String isPlural = counter == 1 ? "is" : "are";
         String taskIfPlural = counter == 1 ? "task" : "tasks";
         System.out.println("Here " + isPlural + " the " + taskIfPlural + " in your list:");
@@ -19,27 +19,26 @@ public class Ui {
         }
     }
 
-    public void taskDone(Task task) {
+    public static void taskDone(Task task) {
         System.out.println("Nice! I've marked this task as done:");
         System.out.println(task);
     }
 
-    public int printAddedTask(ArrayList<Task> task, int counter) {
+    public static void printAddedTask(ArrayList<Task> task, int counter) {
         System.out.println("Got it. I've added this task:\n" + task.get(counter));
         counter++;
         String taskIfPlural = counter == 1 ? "task" : "tasks";
         System.out.println("Now you have " + counter + " " + taskIfPlural + " in the list.");
-        return counter;
     }
 
-    public void deleteTask(Task task, int counter) {
+    public static void deleteTask(Task task, int counter) {
         String taskIfPlural = counter <= 1 ? "task" : "tasks";
         System.out.println("Noted. I've removed this task:");
         System.out.println(task);
         System.out.println("Now you have " + counter + " " + taskIfPlural + " in the list.");
     }
 
-    public void printException(Exception e) {
+    public static void printException(Exception e) {
         if (e instanceof DukeException) {
             System.err.println(e);
         } else {

af3079ab562beea68ef23912e2f235b26ddcfdb7
NgWenJunFelix <felixng97@gmail.com>
2019-08-25 21:45:22 +0800

    Level-8 Branch added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -91,7 +91,11 @@ public class Duke {
                             throw new DukeMissingDescriptionException(("deadline"));
                         } else {
                             System.out.println("Got it. I've added this task:");
-                            Task current = new Deadline(information[0].trim(), information[1].trim());
+                            String[] dateAndTime = information[1].trim().split(" ");
+                            String[] date = dateAndTime[0].split("/");
+                            String dateInWords = Duke.dateToWords(date);
+                            String time = Duke.timeConverter(dateAndTime[1]);
+                            Task current = new Deadline(information[0].trim(), dateInWords +", " + time);
                             tasks.add(current);
                             System.out.println(current.toString());
                             System.out.println("Now you have " + tasks.size() + " task in the list.");
@@ -113,7 +117,11 @@ public class Duke {
                             throw new DukeMissingDescriptionException("event");
                         } else {
                             System.out.println("Got it. I've added this task:");
-                            Task current = new Event(information[0].trim(), information[1].trim());
+                            String[] dateAndTime = information[1].trim().split(" ");
+                            String[] date = dateAndTime[0].split("/");
+                            String dateInWords = Duke.dateToWords(date);
+                            String time = Duke.timeConverter(dateAndTime[1]);
+                            Task current = new Deadline(information[0].trim(), dateInWords +", " + time);
                             tasks.add(current);
                             System.out.println(current.toString());
                             System.out.println("Now you have " + tasks.size() + " task in the list.");
@@ -134,6 +142,84 @@ public class Duke {
                     }
             }
 
+        }
+    }
+
+    public static String dateToWords(String[] date) {
+        String month;
+        switch(date[1]) {
+            case "1":
+                month = "January";
+                break;
+
+            case "2":
+                month = "Febuary";
+                break;
+
+            case "3":
+                month = "March";
+                break;
+
+            case "4":
+                month = "April";
+                break;
+
+            case "5":
+                month = "May";
+                break;
+
+            case "6":
+                month = "June";
+                break;
+
+            case "7":
+                month = "July";
+                break;
+
+            case "8":
+                month = "August";
+                break;
+
+            case "9":
+                month = "September";
+                break;
+
+            case "10":
+                month = "October";
+                break;
+
+            case "11":
+                month = "November";
+                break;
+
+            case "12":
+                month = "December";
+                break;
+
+            default:
+                month = "month";
+                break;
+        }
+        String day = date[0].equals("1") ? date[0] + "st" : date[0].equals("2") ? date[0] + "nd" :
+                date[0].equals("3") ? date[0] + "rd" : date[0] + "th";
+        return day + " of " + month + " 20" + date[2];
+    }
+
+    public static String timeConverter(String time) {
+        int numberTime = Integer.parseInt(time);
+        if (numberTime < 1000) {
+            return String.valueOf(numberTime).substring(0, 1) + "." + String.valueOf(numberTime).substring(1, 3) + "am";
+        } else if (numberTime < 1200) {
+            return String.valueOf(numberTime).substring(0, 2) + "." + String.valueOf(numberTime).substring(2, 4) + "am";
+        } else if (numberTime < 1300) {
+            return String.valueOf(numberTime).substring(0, 2) + "." + String.valueOf(numberTime).substring(2, 4) + "pm";
+        } else {
+            numberTime -= 1200;
+            if (numberTime < 1000) {
+                return String.valueOf(numberTime).substring(0, 1) + "." + String.valueOf(numberTime).substring(1, 3) + "pm";
+            } else {
+                return String.valueOf(numberTime).substring(0, 2) + "." + String.valueOf(numberTime).substring(2, 4) + "pm";
+            }
         }
     }
 }

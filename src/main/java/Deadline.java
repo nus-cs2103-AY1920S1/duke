026e43c04f7d412507c4f8998e2c4de7ee376f0f
joloong <joelloong@gmail.com>
2019-08-25 11:36:04 +0800

    Process date

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,45 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Deadline extends Task {
 
     protected String by;
+    protected Date date;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        this.date = formatDate();
+    }
+
+    private Date formatDate() {
+        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+
+        String[] bySplit = by.split("/");
+        String day = bySplit[0];
+        String month = bySplit[1];
+        String year = bySplit[2];
+        if (day.length() == 1) {
+            day = "0" + day;
+        }
+        if (month.length() == 1) {
+            month = "0" + month;
+        }
+
+        Date date = new Date();
+        String dateString = day + "-" + month + "-" + year;
+        try {
+            date = format.parse(dateString);
+        } catch (ParseException e) {
+            System.err.println(e.getMessage());
+        }
+
+        return date;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + date + ")";
     }
 }

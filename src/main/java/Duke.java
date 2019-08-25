e2fa813bf07be6c92b75c0dfdc7feb2aa8d7a375
heze8 <46834785+heze8@users.noreply.github.com>
2019-08-25 16:30:09 +0800

    Completed Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,9 @@
-import java.util.Scanner;
+import bin.UI;
 
 public class Duke {
-    static final String INDENTATION = "     ";
-    static final String HORIZONTAL_LINE = INDENTATION + "____________________________________________________________\n";
 
     public static void main(String[] args) {
-        String greeting = HORIZONTAL_LINE + INDENTATION + "Hello! I'm Duke\n"
-                + INDENTATION + "What can I do for you?\n" + HORIZONTAL_LINE;
-        System.out.println(greeting);
-
-        Scanner sc = new Scanner(System.in);
-        while (sc.hasNextLine()) {
-            String command = sc.nextLine();
-            if (command.equals("bye")) {
-                System.out.println(wrapper("Bye. Hope to see you again soon!"));
-                return;
-            }
-            System.out.println(wrapper(command));
-        }
-    }
-
-    private static String wrapper(String string) {
-        return HORIZONTAL_LINE + INDENTATION + string + "\n" + HORIZONTAL_LINE;
+        UI ui = new UI();
+        ui.run();
     }
 }

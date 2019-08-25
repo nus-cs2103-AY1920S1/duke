d55acda9670a88712c22b96d80fd9479a14a676c
R-D-D-D <wangrunding@gmail.com>
2019-08-25 21:31:23 +0800

    a-exceptions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException {
         Scanner sc = new Scanner(System.in);
         ArrayList<Task> list = new ArrayList<>();
         System.out.println("Hello I'm Duke\nWhat can I do for you?");
@@ -23,37 +23,51 @@ public class Duke {
                 list.get(index - 1).complete();
                 str = sc.nextLine();
             } else {
-                System.out.println("Got it. I've added this task");
                 String[] info = str.split("/");
                 String[] type = info[0].split(" ");
                 Task task;
-                switch (type[0]) {
-                case "todo":
-                    System.out.println("todo");
-                    type[0] = "";
-                    task = new ToDo(String.join(" ", type).trim());
-                    break;
-                case "deadline":
-                    System.out.println("deadline");
-                    type[0] = "";
-                    task = new Deadline(String.join(" ", type).trim(), info[1]);
-                    break;
-                case "event":
-                    System.out.println("event");
-                    type[0] = "";
-                    task = new Event(String.join(" ", type).trim(), info[1]);
-                    break;
-                default:
-                    System.out.println("default");
-                    task = new Task(" ");
-                    break;
-                }
-                list.add(task);
-                System.out.println("  " + task.toString());
-                if (list.size() == 1) {
-                    System.out.println("Now you have 1 task in the list");
-                } else {
-                    System.out.println(String.format("Now you have %d tasks in the list", list.size()));
+                try {
+                    switch (type[0]) {
+                    case "todo":
+                        type[0] = "";
+                        if (type.length == 1) {
+                            throw new DukeException("\u2639 OOPS!!! The description of a todo cannot be empty.");
+                        }
+                        task = new ToDo(String.join(" ", type).trim());
+                        break;
+                    case "deadline":
+                        type[0] = "";
+                        if (type.length == 1) {
+                            throw new DukeException("\u2639 OOPS!!! The description of a deadline item cannot be empty.");
+                        }
+                        if (info.length == 1) {
+                            throw new DukeException("\u2639 OOPS!!! You need a / to separate out the date time for this deadline item.");
+                        }
+                        task = new Deadline(String.join(" ", type).trim(), info[1]);
+                        break;
+                    case "event":
+                        type[0] = "";
+                        if (type.length == 1) {
+                            throw new DukeException("\u2639 OOPS!!! The description of aa event cannot be empty.");
+                        }
+                        if (info.length == 1) {
+                            throw new DukeException("\u2639 OOPS!!! You need a / to separate out the date time for this event");
+                        }
+                        task = new Event(String.join(" ", type).trim(), info[1]);
+                        break;
+                    default:
+                        throw new DukeException("\u2639 OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    }
+                    list.add(task);
+                    System.out.println("Got it. I've added this task");
+                    System.out.println("  " + task.toString());
+                    if (list.size() == 1) {
+                        System.out.println("Now you have 1 task in the list");
+                    } else {
+                        System.out.println(String.format("Now you have %d tasks in the list", list.size()));
+                    }
+                } catch (DukeException e) {
+                    System.out.println(e.getMessage());
                 }
                 str = sc.nextLine();
             }
@@ -118,5 +132,11 @@ class Event extends Task {
     @Override
     public String toString() {
         return done ? String.format("[E][%c] %s (%s)", tick, content, time) : String.format("[E][%c] %s (%s)", cross, content, time);
+    }
+}
+
+class DukeException extends Exception {
+    public DukeException(String message) {
+        super(message);
     }
 }

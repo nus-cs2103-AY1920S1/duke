aa4777571a08160225381794c74d9bdd24c92c44
joloong <joelloong@gmail.com>
2019-08-25 13:32:56 +0800

    Abstract parser

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,11 +8,11 @@ public class Duke {
     private ArrayList<Task> task = new ArrayList<>();
     private int counter = 0;
     private Storage storage;
-    private Ui ui;
+    private Parser parser;
 
-    public Duke() {
-        storage = new Storage("data/duke.txt");
-        ui = new Ui();
+    public Duke(String filePath) {
+        storage = new Storage(filePath);
+        parser = new Parser();
     }
 
     /**
@@ -21,8 +21,7 @@ public class Duke {
      * @param args arguments passed into main
      */
     public static void main(String[] args) {
-        Duke duke = new Duke();
-        duke.run();
+        new Duke("data/duke.txt").run();
     }
 
     private void run() {
@@ -30,85 +29,19 @@ public class Duke {
             task = storage.readData();
             counter = task.size();
         } catch (FileNotFoundException e) {
-            ui.printException(e);
+            Ui.printException(e);
         }
 
-        Scanner sc = new Scanner(System.in);
+        Ui.startOfInteractions();
 
-        ui.startOfInteractions();
+        parser.parse(task, counter);
 
-        String textInput = sc.nextLine();
-        while (!textInput.equals("bye")) {
-            try {
-                if (textInput.equals("list")) {
-                    ui.printList(task, counter);
-                } else if (textInput.startsWith("done")) {
-                    if (textInput.equals("done") || textInput.equals("done ")) {
-                        throw new DukeException("OOPS!!! Index required.");
-                    }
-
-                    int completedIndex = Integer.parseInt(textInput.replaceFirst("done ", "")) - 1;
-                    if (completedIndex < 0 || completedIndex >= counter) {
-                        throw new DukeException("OOPS!!! Index not found.");
-                    }
-
-                    Task markAsDoneTask = task.get(completedIndex);
-                    markAsDoneTask.markAsDone();
-                    ui.taskDone(markAsDoneTask);
-                } else if (textInput.startsWith("todo")) {
-                    if (textInput.equals("todo") || textInput.equals("todo ")) {
-                        throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
-                    }
-                    String description = textInput.replaceFirst("todo ", "");
-                    task.add(new Todo(description));
-                    counter = ui.printAddedTask(task, counter);
-                } else if (textInput.startsWith("deadline")) {
-                    if (textInput.equals("deadline") || textInput.equals("deadline ")) {
-                        throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
-                    }
-                    String removeTaskWord = textInput.replaceFirst("deadline ", "");
-                    String[] taskSplit = removeTaskWord.split(" /by ");
-                    task.add(new Deadline(taskSplit[0], taskSplit[1]));
-                    counter = ui.printAddedTask(task, counter);
-                } else if (textInput.startsWith("event")) {
-                    if (textInput.equals("event") || textInput.equals("event ")) {
-                        throw new DukeException("OOPS!!! The description of a event cannot be empty.");
-                    }
-
-                    String removeTaskWord = textInput.replaceFirst("event ", "");
-                    String[] taskSplit = removeTaskWord.split(" /at ");
-                    task.add(new Event(taskSplit[0], taskSplit[1]));
-                    counter = ui.printAddedTask(task, counter);
-                } else if (textInput.startsWith("delete")) {
-                    if (textInput.equals("delete") || textInput.equals("delete ")) {
-                        throw new DukeException("OOPS!!! Index required.");
-                    }
-
-                    int deletedIndex = Integer.parseInt(textInput.replaceFirst("delete ", "")) - 1;
-                    if (deletedIndex < 0 || deletedIndex >= counter) {
-                        throw new DukeException("OOPS!!! Index not found.");
-                    }
-                    Task deletedTask = task.remove(deletedIndex);
-                    counter--;
-                    ui.deleteTask(deletedTask, counter);
-                } else {
-                    throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
-                }
-            } catch (DukeException e) {
-                ui.printException(e);
-            } finally {
-                textInput = sc.nextLine();
-            }
-        }
-        ui.endOfInteractions();
+        Ui.endOfInteractions();
 
         try {
             storage.writeData(task);
         } catch (IOException e) {
-            ui.printException(e);
+            Ui.printException(e);
         }
-
-        // Close the scanner
-        sc.close();
     }
 }
---
+++ src/main/java/Parser.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Parser {
+
+    public void parse(ArrayList<Task> task, int counter) {
+        Scanner sc = new Scanner(System.in);
+
+        String textInput = sc.nextLine();
+        while (!textInput.equals("bye")) {
+            try {
+                if (textInput.equals("list")) {
+                    Ui.printList(task, counter);
+                } else if (textInput.startsWith("done")) {
+                    if (textInput.equals("done") || textInput.equals("done ")) {
+                        throw new DukeException("OOPS!!! Index required.");
+                    }
+
+                    int completedIndex = Integer.parseInt(textInput.replaceFirst("done ", "")) - 1;
+                    if (completedIndex < 0 || completedIndex >= counter) {
+                        throw new DukeException("OOPS!!! Index not found.");
+                    }
+
+                    Task markAsDoneTask = task.get(completedIndex);
+                    markAsDoneTask.markAsDone();
+                    Ui.taskDone(markAsDoneTask);
+                } else if (textInput.startsWith("todo")) {
+                    if (textInput.equals("todo") || textInput.equals("todo ")) {
+                        throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
+                    }
+                    String description = textInput.replaceFirst("todo ", "");
+                    task.add(new Todo(description));
+                    Ui.printAddedTask(task, counter);
+                    counter++;
+                } else if (textInput.startsWith("deadline")) {
+                    if (textInput.equals("deadline") || textInput.equals("deadline ")) {
+                        throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
+                    }
+                    String removeTaskWord = textInput.replaceFirst("deadline ", "");
+                    String[] taskSplit = removeTaskWord.split(" /by ");
+                    task.add(new Deadline(taskSplit[0], taskSplit[1]));
+                    Ui.printAddedTask(task, counter);
+                    counter++;
+                } else if (textInput.startsWith("event")) {
+                    if (textInput.equals("event") || textInput.equals("event ")) {
+                        throw new DukeException("OOPS!!! The description of a event cannot be empty.");
+                    }
+
+                    String removeTaskWord = textInput.replaceFirst("event ", "");
+                    String[] taskSplit = removeTaskWord.split(" /at ");
+                    task.add(new Event(taskSplit[0], taskSplit[1]));
+                    Ui.printAddedTask(task, counter);
+                    counter++;
+                } else if (textInput.startsWith("delete")) {
+                    if (textInput.equals("delete") || textInput.equals("delete ")) {
+                        throw new DukeException("OOPS!!! Index required.");
+                    }
+
+                    int deletedIndex = Integer.parseInt(textInput.replaceFirst("delete ", "")) - 1;
+                    if (deletedIndex < 0 || deletedIndex >= counter) {
+                        throw new DukeException("OOPS!!! Index not found.");
+                    }
+                    Task deletedTask = task.remove(deletedIndex);
+                    counter--;
+                    Ui.deleteTask(deletedTask, counter);
+                } else {
+                    throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
+            } catch (DukeException e) {
+                Ui.printException(e);
+            } finally {
+                textInput = sc.nextLine();
+            }
+        }
+
+        // Close the scanner
+        sc.close();
+    }
+}

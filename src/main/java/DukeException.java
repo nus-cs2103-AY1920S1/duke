c0c4851440a353933b1c26aff32bd3cefa7dc305
Ni Tianzhen <nitianzhen1210@gmail.com>
2019-08-25 15:21:13 +0800

    Change duke exception message to use inheritance

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,13 +1,6 @@
 public class DukeException extends Exception {
 
-    protected String message;
-
     public DukeException(String message) {
-        super();
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return this.message;
+        super(message);
     }
 }

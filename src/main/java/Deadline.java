974532834e629a16b1e4bdbcd76e0b8fbcb38adb
Bernice Chio <bernicechiohuiyin@gmail.com>
2019-08-25 19:28:00 +0800

    Deadline.java: added parseTime method

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,20 @@
-public class Deadline extends Task {
-    private String time;
+import java.text.ParseException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
-    public Deadline(String description) {
+public class Deadline extends Task {
+    private Date date;
+
+    Deadline(String description) {
         super(description);
         this.type = "D";
-        this.time = "";
     }
 
-    public void setTime(String time) {
-        this.time = " (by: " + time.substring(3) + ")";
+    void parseTime(String time) throws ParseException {
+        this.date = new SimpleDateFormat("dd/MM/yyyy hhmm").parse(time);
     }
-
     @Override
     public String toString() {
-        return "[" + this.type + "][" + this.getStatusIcon() + "] " + this.description + this.time;
+        return "[" + this.type + "][" + this.getStatusIcon() + "] " + this.description + this.date;
     }
 }

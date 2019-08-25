4c95d05e855f5e2a7e9fa15b5dd036bc39fa66ed
AHaliq <liqo12@hotmail.com>
2019-08-25 16:40:09 +0800

    made greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,7 @@
+import util.Printer;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Printer.printString("Hello! I'm Duke\nWhat can I do for you?");
     }
 }

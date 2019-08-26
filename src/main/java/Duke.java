24796d0a4bad24ef760113ee7ef238d6962edb73
Karan Dev Sapra <>
2019-08-26 22:44:55 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
@@ -9,9 +10,27 @@ public class Duke {
 
         String command = scanner.nextLine();
 
+        String[] list = new String[100];
+        int listIndex = 0;
+
         while (command.equals("bye") == false) {
 
-            System.out.println(command);
+            String[] words = command.split(" ",2);
+
+            if (words[0].equals("added:")) {
+                String addMe = words.length < 2 ? "" : words[1];
+                list[listIndex++] = addMe;
+
+            } else if (command.equals("list")) {
+
+               for (int i = 0; i < listIndex; i++) {
+                   System.out.print(i+1 + ". ");
+                   System.out.println(list[i]);
+               }
+
+           } else {
+               System.out.println(command);
+           }
 
             command = scanner.nextLine();
         }

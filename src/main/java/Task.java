e786e40c8931c92be789a09b94f372b123c920ca
ArunKumarr <arunkumarr97@gmail.com>
2019-08-26 12:28:50 +0800

    Revert "level 6-updated"
    This reverts commit d96e6b1cd4eff1293f3a62a788f59afbd08ef1c8.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,5 +1,5 @@
 public class Task {
-    protected String command; protected Boolean done;
+    public String command; public Boolean done;
     public Task(String command){
         this.command = command;
         this.done = false;

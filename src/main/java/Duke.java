20601b03aa9fafe3a06551cd0a02911cc41e3604
Wang Shiyao <furytoar@gmail.com>
2019-08-26 15:05:55 +0800

    Seperate Exceptions into new file

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,38 +3,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class DukeException extends Exception {}
-class InputUnknownException extends DukeException {
-    @Override
-    public String getMessage() {
-        return "OOPS!!! I'm sorry, but I don't know what that means :-(";
-    }
-}
-class EmptyDescriptionException extends DukeException {
-    @Override
-    public String getMessage() {
-        return "What's the description of the item?";
-    }
-}
-class EmptyListIndexException extends DukeException {
-    @Override
-    public String getMessage() {
-        return " which one? (input an integer)";
-    }
-}
-class ListItemEmptyException extends DukeException {
-    @Override
-    public String getMessage() {
-        return "No such item in list!";
-    }
-}
-class EmptyTimeDueException extends DukeException {
-    @Override
-    public String getMessage() {
-        return "When is it due? eg. /by 2359 sunday";
-    }
-}
-
 public class Duke {
     private static final String saveLoadFilePath = "listSaveData.txt";
 

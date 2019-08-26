22cfd7a09530aff7ba46a34289269109fd599965
Akimatsu98 <e0322951@u.nus.edu>
2019-08-26 13:18:28 +0800

    dukeBot: completed level 2;

--- src/main/java/dukeBot.java
+++ src/main/java/dukeBot.java
@@ -4,12 +4,25 @@ public class dukeBot {
     public void start() {
         Scanner sc =  new Scanner(System.in);
 
+        // create storage for tasks
+        String[] list = new String[100];
+        int cnt = 0;
+        String firstWord = "added: ";
+
+
         while(true) {
-            String command = sc.nextLine();
-            if (command.equals("bye") || command.equals("Bye")) {
+            String input = sc.nextLine();
+            if(input.equals("bye") || input.equals("Bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else { System.out.println(command); }
+            } else if(input.equals("list")) {
+                for(int i = 0; i < cnt; i++) System.out.println(list[i]);
+            } else {
+                if (!input.equals("")) {
+                    list[cnt++] = ((cnt) + ". ").concat(input);
+                    System.out.println(firstWord.concat(input));
+                }
+            }
         }
     }
 }

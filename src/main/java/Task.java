a1fd1d82ce5acb8d1c3cb3e0676cf14aa426fb46
jhchen <chenjiehan333@gmail.com>
2019-08-27 01:54:19 +0800

    Complete Level 8

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,3 +1,8 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Task {
 
     protected String description;
@@ -28,6 +33,50 @@ public class Task {
     }
 
     public void setTime(String time) {
-        this.time = time;
+        if (time.contains("/")) {
+            DateFormat df = new SimpleDateFormat("d/MM/yyyy HHmm");
+            try {
+                Date output = df.parse(time);
+                String result = new SimpleDateFormat("d 'of' MMMM yyyy, hmm aa").format(output);
+                this.time = format(result);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        } else {
+            this.time = time;
+        }
+    }
+
+    public String format(String strTime) {
+        String[] str = strTime.split(" ");
+        String day;
+        String aa;
+        int time;
+
+        if (Integer.parseInt(str[0]) == 1 ) {
+            day = "1st";
+        } else if (Integer.parseInt(str[0]) == 2) {
+            day = "2nd";
+        } else if (Integer.parseInt(str[0]) == 3) {
+            day = "3rd";
+        } else {
+            day = str[0] + "th";
+        }
+
+
+
+        if (str[5].equals("AM")) {
+            aa = "am";
+        } else {
+            aa = "pm";
+        }
+
+        if (Integer.parseInt(str[4]) % 100 == 0) {
+            time = Integer.parseInt(str[4]) / 100;
+        } else {
+            time = Integer.parseInt(str[4]);
+        }
+
+        return day + " " + str[1] + " " + str[2] + " " + str[3] + " " + time + aa;
     }
 }

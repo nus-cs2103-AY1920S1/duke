9efef55cb5dee4f8a7a70a55014fc71677722426
Akimatsu98 <e0322951@u.nus.edu>
2019-08-26 14:17:02 +0800

    added Task class, completed level 3

--- src/main/java/dukeBot.java
+++ src/main/java/dukeBot.java
@@ -1,28 +1,38 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class dukeBot {
     public void start() {
         Scanner sc =  new Scanner(System.in);
 
         // create storage for tasks
-        String[] list = new String[100];
+        ArrayList<Task> list = new ArrayList<>();
         int cnt = 0;
         String firstWord = "added: ";
 
 
         while(true) {
             String input = sc.nextLine();
-            if(input.equals("bye") || input.equals("Bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            } else if(input.equals("list")) {
-                for(int i = 0; i < cnt; i++) System.out.println(list[i]);
-            } else {
-                if (!input.equals("")) {
-                    list[cnt++] = ((cnt) + ". ").concat(input);
-                    System.out.println(firstWord.concat(input));
+
+            if(input.startsWith("done")) {
+                int index = Integer.valueOf(input.substring(5));
+                list.get(index - 1).markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(list.get(index - 1).toString());
+                continue;
+            }
+
+            switch(input) {
+                case "bye":
+                case "Bye":
+                    System.out.println("Bye. Hope to see you again soon!"); return;
+                case "list": for(int i = 0; i < cnt; i++) System.out.println(((i + 1) + ".").concat(list.get(i).toString())); break;
+                default:
+                    if(!input.equals("")) {
+                        list.add(cnt++, new Task(input));
+                        System.out.println(firstWord.concat(input));
+                    }
                 }
             }
         }
     }
-}

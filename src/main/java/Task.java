2c070469532d6b7a86d027d6121240238c038e91
robytanama <tanamaroby@gmail.com>
2019-08-26 16:23:56 +0800

    Increment Level 8. Dates and Times Deleting Classes

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,24 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+    protected String extraInfo;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+        this.extraInfo = "";
+    }
+
+    public boolean getStatus() {
+        return isDone;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getExtraInfo() {
+        return this.extraInfo;
     }
 
     public String getStatusIcon() {

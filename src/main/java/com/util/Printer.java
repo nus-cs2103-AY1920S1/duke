6bb74f31c1e5ae47ee05fd802f731cac0bf645f4
AHaliq <liqo12@hotmail.com>
2019-08-26 11:12:56 +0800

    use checkstyle on Printer

--- src/main/java/com/util/Printer.java
+++ src/main/java/com/util/Printer.java
@@ -2,18 +2,29 @@ package com.util;
 
 public class Printer {
 
-    /** The spacing after padding for each line of text. */
+    /**
+     * The spacing after padding for each line of text.
+     */
     private static final int MARGIN_LENGTH = 1;
-    /** The spacing before the horizontal line begin. */
+    /**
+     * The spacing before the horizontal line begin.
+     */
     private static final int PADDING_LENGTH = 4;
-    /** The length the right most tip of the horizontal line will span. */
+    /**
+     * The length the right most tip of the horizontal line will span.
+     */
     private static final int HORIZONTAL_LINE_LENGTH = 64;
 
-    /** Text to be inserted before every line of text. */
-    private static final String MARGIN_AND_PADDING = repeatChar(MARGIN_LENGTH + PADDING_LENGTH, ' ');
-    /** Text to be inserted before the start of the first line and after the last line. */
+    /**
+     * Text to be inserted before every line of text.
+     */
+    private static final String MARGIN_AND_PADDING = repeatChar(MARGIN_LENGTH + PADDING_LENGTH,
+            ' ');
+    /**
+     * Text to be inserted before the start of the first line and after the last line.
+     */
     private static final String HORIZONTAL_LINE = repeatChar(PADDING_LENGTH, ' ')
-            + repeatChar(HORIZONTAL_LINE_LENGTH - PADDING_LENGTH,'_')
+            + repeatChar(HORIZONTAL_LINE_LENGTH - PADDING_LENGTH, '_')
             + "\n";
 
     /**
@@ -30,8 +41,9 @@ public class Printer {
 
     /**
      * Prints an added message for a given task toString().
-     * @param taskString    a task's toString()
-     * @param length        length of list new size()
+     *
+     * @param taskString a task's toString()
+     * @param length     length of list new size()
      */
     public static void addTaskMessage(String taskString, int length) {
         printString("Got it. I've added this task:\n  "
@@ -42,7 +54,8 @@ public class Printer {
 
     /**
      * Print with error decorator prefixing the string.
-     * @param str   string to be printed
+     *
+     * @param str string to be printed
      */
     public static void printError(String str) {
         System.out.print(formatString("☹ OOPS!!! " + str));
@@ -50,7 +63,8 @@ public class Printer {
 
     /**
      * Prints a string with formatting; border, margin and padding.
-     * @param str   string to be printed.
+     *
+     * @param str string to be printed.
      */
     public static void printString(String str) {
         System.out.print(formatString(str));
@@ -58,8 +72,9 @@ public class Printer {
 
     /**
      * Builds a string with borders and margin and padding for each line.
-     * @param str   string to be formatted
-     * @return      resulting formatted string
+     *
+     * @param str string to be formatted
+     * @return resulting formatted string
      */
     private static String formatString(String str) {
         StringBuilder formattedString = new StringBuilder();
@@ -76,9 +91,10 @@ public class Printer {
 
     /**
      * Generates a string of repeated characters.
-     * @param length    number of repetitions
-     * @param c         character to be repeated
-     * @return          resulting repeated character string
+     *
+     * @param length number of repetitions
+     * @param c      character to be repeated
+     * @return resulting repeated character string
      */
     private static String repeatChar(int length, char c) {
         return String.valueOf(c).repeat(Math.max(0, length));

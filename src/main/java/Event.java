37315a76892031c3dae411a666221d8c7adb7f8a
TanZhanMing <e0349703@u.nus.edu>
2019-08-26 20:54:29 +0800

    Level 8 (inefficient dateparser class, Work in progress)

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,10 +1,15 @@
+import java.util.Date;
+
 public class Event extends Task {
 
     protected String at;
+    protected Date date;
 
     public Event(String description, String at) {
         super(description);
         this.at = at;
+        this.date = DateParser.dateParser(at);
+        if (date != null) { this.at = date.toString(); }
     }
 
     @Override

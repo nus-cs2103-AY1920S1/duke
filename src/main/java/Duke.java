87fb84e872e255edd9dce90f7e5d8b0a5fdab167
Karan Dev Sapra <>
2019-08-26 22:09:24 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
 
-        System.out.println("Hello! I'm Duke What can I do for you?");
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
         Scanner scanner = new Scanner(System.in);
 

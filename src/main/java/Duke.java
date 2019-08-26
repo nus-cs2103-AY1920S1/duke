f302e6b32fdccec190c727d4432566493e70e07b
Karan Dev Sapra <>
2019-08-27 00:03:22 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -10,27 +11,41 @@ public class Duke {
 
         String command = scanner.nextLine();
 
+        //Task[] list = new Task[100];
         String[] list = new String[100];
         int listIndex = 0;
 
         while (command.equals("bye") == false) {
 
+            if (command.equals("list") == true) {
+                for (int i = 0; i < listIndex; i++) {
+                    System.out.print(i+1 + ". ");
+                    System.out.println(list[i]);
+                }
+            } else {
+                list[listIndex++] = command;
+            }
+
+            /*
             String[] words = command.split(" ",2);
 
             if (words[0].equals("added:")) {
-                String addMe = words.length < 2 ? "" : words[1];
-                list[listIndex++] = addMe;
+                String description = words.length < 2 ? "" : words[1];
+                list[listIndex++] = new Task(description);
 
+            } else if (words[0].equals("done") && words.length == 2) {
+                int index = Integer.valueOf(words[1]);
+                list[index].isDone = true;
             } else if (command.equals("list")) {
 
                for (int i = 0; i < listIndex; i++) {
-                   System.out.print(i+1 + ". ");
+                   System.out.print(i+1 + list[i].getStatusIcon() + ". ");
                    System.out.println(list[i]);
                }
 
            } else {
                System.out.println(command);
-           }
+           }*/
 
             command = scanner.nextLine();
         }

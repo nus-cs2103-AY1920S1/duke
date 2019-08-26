3cdf6ffa91dac37c653ffdc86eecf0d7018f237d
robytanama <tanamaroby@gmail.com>
2019-08-26 15:48:26 +0800

    Increment Level 8. Dates and Times

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -94,14 +94,35 @@ public class Duke {
                             }
                         }
                         String extraInfo = "";
-                        for (int i = marker + 1; i < inputMessage.length; i++) {
+                        String tempInfo = "";
+                        for (int i = marker + 1; i < inputMessage.length; i ++) {
                             if (i == inputMessage.length - 1) {
-                                extraInfo += inputMessage[i];
+                                tempInfo += inputMessage[i];
                             } else {
-                                extraInfo += inputMessage[i];
-                                extraInfo += " ";
+                                tempInfo += inputMessage[i];
+                                tempInfo += " ";
                             }
                         }
+                        String[] dateAndTime = tempInfo.split(" ");
+                        String[] date = dateAndTime[0].split("/");
+                        if (date.length != 3 || dateAndTime.length < 2) {
+                            throw new DukeException("     Invalid date and time format!");
+                        }
+                        if (date[0].equals("") || date[1].equals("") || date[2].equals("")) {
+                            throw new DukeException("     Invalid date and time format!");
+                        }
+                        int day = Integer.parseInt(date[0]);
+                        int month = Integer.parseInt(date[1]);
+                        int year = Integer.parseInt(date[2]);
+                        Date inputDate = new Date(day, month, year);
+                        Time inputTime = new Time(Integer.parseInt(dateAndTime[1]));
+                        if (!inputDate.isValid()) {
+                            throw new DukeException("     Sorry! Invalid date format");
+                        }
+                        if (!inputTime.isValid()) {
+                            throw new DukeException("     Sorry! Invalid time format");
+                        }
+                        extraInfo = inputDate + ", " + inputTime;
                         if (extraInfo.equals("")) {
                             throw new DukeException("     Please provide more information");
                         }

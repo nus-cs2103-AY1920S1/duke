2805cd89d77109af7c64afb7661c9019da9c1b62
Joshua Wong <joshwonghc@gmail.com>
2019-08-26 21:46:01 +0800

    Add 3 tests

---
+++ src/test/java/duke/TaskListTest.java
@@ -0,0 +1,25 @@
+package duke;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import duke.task.ToDo;
+
+class MyFirstJUnitJupiterTests {
+
+    TaskList tasks = new TaskList();
+
+    @Test
+    void taskListAdd() {
+        tasks.add(new ToDo("I love testing"));
+        assertEquals(1, tasks.getSize());
+    }
+
+    @Test
+    void taskListRemove() {
+        tasks.add(new ToDo("I will remove this test"));
+        assertEquals(1, tasks.getSize());
+        tasks.remove(1);
+        assertEquals(0, tasks.getSize());
+    }
+}

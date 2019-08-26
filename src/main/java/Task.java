03ed65198ef7fe0e18920089afd8198b2c17a036
Rui Zhi <gohruizhi@u.nus.edu>
2019-08-26 19:51:35 +0800

    Reading and writing to tasks.txt

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,12 +7,20 @@ public class Task {
         this.isDone = false;
     }
 
+    public Boolean getStatus() {
+        return this.isDone;
+    }
+
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
     }
 
     public void doTask() {
         this.isDone = true;
+    }
+
+    public String getDescription() {
+        return this.description;
     }
 
     public String toString() {

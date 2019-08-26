6faf20b70480e24073484350f042fd8b38bc9fea
Akimatsu98 <e0322951@u.nus.edu>
2019-08-26 10:33:26 +0800

    trying out first commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 public class Duke {
+    // fdajlfkdasfdafdafda
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"

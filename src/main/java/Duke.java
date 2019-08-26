7dd01163bfbaea2ca1aa9878a56c3b9c207e1753
marcfyk <marcjfyk@gmail.com>
2019-08-26 19:46:25 +0800

    updated path

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -35,7 +35,7 @@ public class Duke {
      * @param args Arguments.
      */
     public static void main(String[] args) {
-        new Duke("../data/tasks.txt").run();
+        new Duke("../../../data/tasks.txt").run();
     }
 
     /**

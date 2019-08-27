51bf7f32748e1a59b362bd7088841269b6810c2f
cheongliyuan <e0008064@u.nus.edu>
2019-08-27 13:26:38 +0800

    Remove old parsers (inputIsBye() etc) and combine into addTask() Add 3 subclasses for each type of task E/D/T Swap ifelse in main for switchcase, as there is no need for priority Add enum Tasktype for parsing purposes Swap for loop for collection Tasktype userTask Add numOfTasks to help keep track

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
-/* should have read all the task before starting. Case should be used over ifelse in dowhile loop,
-    as there is no priority in deciphering the user input. Alot of warnings of public members. will finalise them at v0.6.(or do it early?)
- */
-
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
+import java.util.Iterator;
+
+enum TaskType {
+    TODO,
+    EVENT,
+    DEADLINE,
+    LIST,
+    DONE,
+    BYE
+}
 
 public class Duke {
     public static void main(String[] args) {
@@ -13,23 +18,25 @@ public class Duke {
 
         do {
             user.setCurrentInput(input.nextLine());
+            user.setTaskType();
+            TaskType taskType = user.getTaskType();
 
-            //int switchCase = user.getSwitchCase();
-
-           // switch(switchCase){
-
-
-            if (user.inputIsBye()) {
-                //Do nothing and wait to exit program if input is 'bye'
-            } else if (user.inputIsList()) {
-                user.printUserInputs();
-            } else if (user.inputIsDone()) {
+            switch(taskType) {
+            case TODO:
+            case EVENT:
+            case DEADLINE:
+                user.addTask();
+                break;
+            case DONE:
                 user.setTaskDone();
-            } else { //If not 'list' or 'bye'
-                user.addCurrentInput();
+                break;
+            case LIST:
+                user.printUserInputs();
+                break;
+            case BYE: default:
+                break; //do nothing
             }
         } while (!user.inputIsBye());
-
         user.sayByeToUser();
     }
 }
@@ -38,6 +45,9 @@ class Task {
     protected String description;
     protected boolean isDone;
 
+    protected String typeOfTask; 
+
+    public Task() {}
     public Task(String description) {
         this.description = description;
         this.isDone = false;
@@ -59,15 +69,61 @@ class Task {
     public String getDescription() {
         return this.description;
     }
+
+    public String getTaskTypeLetter() {
+        return typeOfTask;
+    }
+}
+
+class Deadline extends Task {
+    //formatting of inputs is left in User class
+    protected String by;
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    public String getDescription() {
+        return super.getDescription() + " (by: " + by + ")";
+    }
+
+    public String getTaskTypeLetter() {
+        return "D";
+    }
+}
+
+class Event extends Task {
+    protected String at;
+    public Event(String description, String at) {
+        super(description);
+        this.at = at;
+    }
+
+    public String getDescription() {
+        return super.getDescription() + " (at: " + at + ")";
+    }
+
+    public String getTaskTypeLetter() {
+        return "E";
+    }
+}
+
+class ToDo extends Task {
+    public ToDo(String description) {
+        super(description);
+    }
+    public String getDescription() {
+        return super.getDescription();
+    }
+
+    public String getTaskTypeLetter() {
+        return "T";
+    }
 }
 
 class User {
-    private final String EXIT_COMMAND = "bye";
-    private final String LIST_COMMAND = "list";
-    private final String DONE_COMMAND = "done";
-
-    private String currentInput = "";
-    private ArrayList<String> userInputs = new ArrayList<String>();
+    private String currentInput = ""; //user input
+    private TaskType currentTaskType; //enum type, after parsing user input.
     private ArrayList<Task> userTasks = new ArrayList<Task>();
     private int numOfTasks = 0;
 
@@ -79,50 +135,120 @@ class User {
 
     }
 
-    public String getCurrentInput() {
-        return this.currentInput;
-    }
-
     public void setCurrentInput(String currentInput) {
         this.currentInput = currentInput;
     }
 
-    public void addCurrentInput() {
-        userTasks.add(new Task(this.currentInput));
-        System.out.println("    ____________________________________________________________\n"
-                + "     added " + this.getCurrentInput() + "\n"
-                + "    ____________________________________________________________\n");
-    }
+    public void setTaskType() {
+        String j = this.currentInput;
+        if (j.contains(" ")) {
+            j = j.substring(0, j.indexOf(" "));
+            j = j.replaceAll("\\s+", "");
+        }
+        j = j.toLowerCase();
+        System.out.println("type: [" + j + "]");
 
-    public boolean inputIsBye (){
-        return this.currentInput.equalsIgnoreCase(EXIT_COMMAND);
-    }
-
-    public boolean inputIsList(){
-        return this.currentInput.equalsIgnoreCase(LIST_COMMAND);
-    }
-
-    public boolean inputIsDone() {
-        String firstWord;
-        //If one more than 1 input, check if first word is "done"
-        if (this.currentInput.contains(" ")){
-            firstWord = this.currentInput.substring(0, this.currentInput.indexOf(" "));
-            return firstWord.equalsIgnoreCase(DONE_COMMAND);
-        } else { //implied: 'done' inputs without numbers are treated as tasks/ignored
-            return false;
+        switch (j) {
+        case "event":
+            this.currentTaskType = TaskType.EVENT;
+            break;
+        case "deadline":
+            this.currentTaskType = TaskType.DEADLINE;
+            break;
+        case "todo":
+            this.currentTaskType = TaskType.TODO;
+            break;
+        case "list":
+            this.currentTaskType = TaskType.LIST;
+            break;
+        case "done":
+            this.currentTaskType = TaskType.DONE;
+            break;
+        case "bye":
+            this.currentTaskType = TaskType.BYE;
+            break;
+        default:
+            break;
         }
     }
 
-    public void setTaskDone() {
+    public void addTask() { //addTask/event.deadline
+        //todo: first space to last char
+        //event/deadline: first space to "/", after "/" to last char
+        TaskType taskType = this.getTaskType(); //used just for switchcase.
+        //only description and date gets passed into subclass of Tasks
+        String j = this.getCurrentInput();
+        String description = "";
+        String date = "";
+
+        switch (taskType) {
+        case DEADLINE:
+            description = j.substring(j.indexOf(" ") + 1, j.indexOf("/") - 1); //+1/-1 to remove spaces
+            date = j.substring((j.indexOf("/") + 1));
+            userTasks.add(new Deadline(description, date));
+            break;
+        case EVENT:
+            description = j.substring(j.indexOf(" ") + 1, j.indexOf("/") - 1); //+1/-1 to remove spaces
+            System.out.println("des: [" + description + "]");
+            date = j.substring((j.indexOf("/") + 1));
+            System.out.println("date: [" + date + "]");
+            userTasks.add(new Event(description, date));
+            break;
+        case TODO:
+            description = j.substring(j.indexOf(" ") + 1);
+            userTasks.add(new ToDo(description));
+            break;
+        default:
+            break;
+        }
+
+        //j = j.replaceAll("\\s+","");
+        this.addTaskCount();
+
+        Iterator<Task> Iterator = userTasks.iterator();
+
+        int i = 1;
+        for (Task task : userTasks) {
+            if (i == this.getTaskCount()) { // new task always added to last in Array list
+                System.out.println("    ____________________________________________________________\n"
+                        + "     Got it. I've added this task: \n"
+                        + "       [" + task.getTaskTypeLetter() + "][" + task.getStatusIcon()
+                        + "] " + task.getDescription() + "\n"
+                        + "     Now you have " + numOfTasks + " tasks in the list. \n" //task(s) always
+                        + "    ____________________________________________________________\n");
+            }
+            i++;
+        }
+    }
+
+    public int getTaskCount() {
+        return this.numOfTasks;
+    }
+    private void addTaskCount() {
+        this.numOfTasks++;
+    }
+
+    private void decreaseTaskCount () {
+        if (this.numOfTasks >= 0) {
+            this.numOfTasks--;
+        }
+    }
+
+    public boolean inputIsBye () {
+        String EXIT_COMMAND = "bye";
+        return this.currentInput.equalsIgnoreCase(EXIT_COMMAND);
+    }
+
+    public void setTaskDone () {
         String temp;
         //get 2nd number
         temp = this.currentInput.substring(this.currentInput.indexOf(" "));
         //remove space infront of number, causing errors
-        temp = temp.replaceAll("\\s+","");
+        temp = temp.replaceAll("\\s+", "");
 
         int taskNumber = Integer.parseInt(temp);
         int count = 1;
-        //i dont know how to access specific element of Arraylist with custom class without looping through every element. Do tell.
+        //getDescription to output date for event/deadline
         for (Task task : userTasks) {
             if (count == taskNumber) {
                 task.markIsDone();
@@ -135,18 +261,25 @@ class User {
         }
     }
 
-    public void printUserInputs() { //catch empty list?
+    public void printUserInputs () { //catch empty list?
         int count = 1;
         System.out.println("    ____________________________________________________________");
         for (Task temp : userTasks) {
-            System.out.println("    " + count + ".[" + temp.getStatusIcon() + "] "
-                    + temp.getDescription() );
+            System.out.println("    " + count + ".[" + temp.getTaskTypeLetter() + "]"
+                    + "[" + temp.getStatusIcon() + "] " + temp.getDescription());
             count++;
         }
         System.out.println("    ____________________________________________________________\n");
     }
 
-    public void sayByeToUser() {
+    public TaskType getTaskType () {
+        return this.currentTaskType;
+    }
+
+    public String getCurrentInput () {
+        return this.currentInput;
+    }
+    public void sayByeToUser () {
         System.out.println("    ____________________________________________________________\n"
                 + "     Bye. Hope to see you again soon!\n"
                 + "    ____________________________________________________________\n");

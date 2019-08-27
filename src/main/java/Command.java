78c58a6ea8ab83694416800b2122a69ba19ad376
JohnNzj <johnng1@hotmail.com>
2019-08-27 19:51:04 +0800

    Add Ui.java, TaskList.java, Storage.java, Parser.java, Command.java. Modify Duke.java & duke.txt

---
+++ src/main/java/Command.java
@@ -0,0 +1,74 @@
+public class Command {
+
+    private String command;
+
+    public Command(String command) {
+        this.command = command;
+    }
+
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+        switch (command) {
+        case "bye":
+            break;
+        case "list":
+            ui.listTask(tasks);
+            break;
+        case "done":
+            int number = ui.readNumber();
+            tasks.markDone(number);
+            ui.printDone(number, tasks);
+            storage.updateDone(number);
+            break;
+        case "todo":
+            String taskname = ui.readLine().trim();
+            ui.checkValidity(taskname);
+            Task t = new Todo(taskname);
+            storage.addTodo(taskname);
+            tasks.add(t);
+            ui.printAdd(t);
+            break;
+        case "deadline":
+            String deadline = ui.readLine().trim();
+            ui.checkValidity(deadline);
+            String[] arrDeadline = deadline.split("/by");
+            String timeDeadline = Parser.convertDateAndTime(arrDeadline[1].trim());
+            Task taskDeadline = new Deadline(arrDeadline[0].trim(), timeDeadline);
+            storage.addDeadline(deadline);
+            tasks.add(taskDeadline);
+            ui.printAdd(taskDeadline);
+            break;
+        case "event":
+            String event = ui.readLine().trim();
+            ui.checkValidity(event);
+            String[] arrEvent = event.split("/at");
+            String time = Parser.convertDateAndTime(arrEvent[1].trim());
+            Task taskEvent = new Event(arrEvent[0].trim(), time);
+            storage.addEvent(event);
+            tasks.add(taskEvent);
+            ui.printAdd(taskEvent);
+            break;
+        case "delete":
+            int deletionNumber = ui.readNumber();
+            storage.delete(deletionNumber);
+            Task toDelete = tasks.getList().get(deletionNumber - 1);
+            tasks.delete(deletionNumber);
+            ui.printDelete(toDelete);
+            break;
+        default:
+            try {
+                throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public boolean isExit() {
+        boolean flag = false;
+        if (command.equals("bye")) {
+            flag = true;
+        }
+        return flag;
+    }
+
+}
---
+++ src/main/java/Ui.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Ui {
+
+    private Scanner sc;
+
+    public Ui() {
+        sc = new Scanner(System.in);
+    }
+
+    public void showWelcome() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+    }
+
+    public void showBye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public String readCommand() {
+        String command = sc.next();
+        return command;
+    }
+
+    public int readNumber() {
+        return sc.nextInt();
+    }
+
+    public String readLine() {
+        return sc.nextLine();
+    }
+
+    public void printAdd(Task t) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + t);
+        System.out.println("Now you have " + TaskList.size() + " " + (TaskList.size() == 1 ? "task" : "tasks") + " in the list.");
+    }
+
+    public void printDelete(Task toDelete) {
+        System.out.println("Noted. I've removed this task:");
+        System.out.println("  " + toDelete);
+        System.out.println("Now you have " + TaskList.size() + " " + (TaskList.size() == 1 ? "task" : "tasks") + " in the list.");
+    }
+
+    public void checkValidity(String taskname) {
+        try {
+            if (taskname.equals("")) {
+                throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+            }
+        } catch (DukeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void printDone(int number, TaskList tasks) {
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("  " + tasks.getList().get(number - 1));
+    }
+
+    public void listTask(TaskList tasks) {
+        System.out.println("Here are the " + (tasks.getList().size() == 1 ? "task" : "tasks") + " in your list:");
+        for (int i = 1; i <= tasks.getList().size(); i++) {
+            System.out.println(i + "." + tasks.getList().get(i - 1));
+        }
+    }
+}
+

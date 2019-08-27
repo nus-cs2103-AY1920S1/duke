af47333c540be7d404ce2ebf40a23e71e6213636
limxu <limxuanhui_17@hotmail.com>
2019-08-28 03:52:27 +0800

    Revert "Level-1"
    This reverts commit 113e475aa77117793a96fdcd2c87ed84a60ea3c1.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,17 +6,5 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
-        System.out.println("Hello! I'm Duke" + "\n" + "What can I do for you?"); // greet user
-        while (true) {
-            Scanner scanner = new Scanner(System.in); // reads user input
-            String line = scanner.nextLine();
-            if (line.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            } else {
-                System.out.println(line); // echos user input
-            }
-        }
     }
 }

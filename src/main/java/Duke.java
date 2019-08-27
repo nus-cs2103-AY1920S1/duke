4a8281720960dfabbd6c59019f57bffe552b20fd
Daryl Chan <dckj95@gmail.com>
2019-08-27 15:53:03 +0800

    Fix checkstyle
    - Add Javadoc for main method
    - Fix `case` indentation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    /**
+     * Starts the Duke application.
+     * @param args Command-line arguments
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Storage storage = new Storage();
@@ -39,95 +43,95 @@ public class Duke {
         String command = tokens[0];
 
         switch (command) {
-            case "list": {
-                ArrayList<Task> items = storage.getTasks();
+        case "list": {
+            ArrayList<Task> items = storage.getTasks();
 
-                StringBuilder output = new StringBuilder();
-                output.append("Here are the tasks in your list:\n");
-                for (int i = 0; i < items.size(); i++) {
-                    output.append(String.format("%d.%s%n", i + 1, items.get(i).toString()));
-                }
-                print(output.toString().trim());
+            StringBuilder output = new StringBuilder();
+            output.append("Here are the tasks in your list:\n");
+            for (int i = 0; i < items.size(); i++) {
+                output.append(String.format("%d.%s%n", i + 1, items.get(i).toString()));
+            }
+            print(output.toString().trim());
 
-                break;
+            break;
+        }
+
+        case "done":
+        case "delete": {
+            if (tokens.length <= 1) {
+                throw new DukeException("Missing task ID.");
             }
 
-            case "done":
-            case "delete": {
-                if (tokens.length <= 1) {
-                    throw new DukeException("Missing task ID.");
-                }
-
-                Task task;
-                int taskId;
-                try {
-                    taskId = Integer.parseInt(tokens[1]) - 1;
-
-                    if (command.equals("done")) {
-                        task = storage.getTasks().get(taskId);
-                        task.markAsDone();
-                    } else {
-                        task = storage.getTasks().remove(taskId);
-                    }
-                } catch (Exception e) {
-                    throw new DukeException("Invalid task ID.");
-                }
+            Task task;
+            int taskId;
+            try {
+                taskId = Integer.parseInt(tokens[1]) - 1;
 
                 if (command.equals("done")) {
-                    print(String.format("Nice! I've marked this task as done:%n%s", task.toString()));
+                    task = storage.getTasks().get(taskId);
+                    task.markAsDone();
                 } else {
-                    print(String.format(
+                    task = storage.getTasks().remove(taskId);
+                }
+            } catch (Exception e) {
+                throw new DukeException("Invalid task ID.");
+            }
+
+            if (command.equals("done")) {
+                print(String.format("Nice! I've marked this task as done:%n%s", task.toString()));
+            } else {
+                print(String.format(
                         "Noted. I've removed this task:%n%s%nNow you have %d tasks in the list.",
                         task.toString(), storage.getTaskCount()
-                    ));
-                }
-
-                break;
+                ));
             }
 
-            case "todo":
-            case "deadline":
-            case "event": {
-                if (tokens.length <= 1) {
-                    throw new DukeException("The description of a task cannot be empty.");
-                }
-                String description = tokens[1];
+            break;
+        }
 
-                Task newTask;
+        case "todo":
+        case "deadline":
+        case "event": {
+            if (tokens.length <= 1) {
+                throw new DukeException("The description of a task cannot be empty.");
+            }
+            String description = tokens[1];
 
-                if (command.equals("todo")) {
-                    newTask = new Todo(description);
-                } else if (command.equals("deadline")) {
-                    if (!description.contains(" /by ")) {
-                        throw new DukeException("A deadline must have a date.");
-                    }
+            Task newTask;
 
-                    String[] deadlineTokens = description.split(" /by ", 2);
-                    newTask = new Deadline(deadlineTokens[0], deadlineTokens[1]);
-                } else {
-                    if (!description.contains(" /at ")) {
-                        throw new DukeException("An event must have a date.");
-                    }
-
-                    String[] eventTokens = description.split(" /at ", 2);
-                    newTask = new Event(eventTokens[0], eventTokens[1]);
+            if (command.equals("todo")) {
+                newTask = new Todo(description);
+            } else if (command.equals("deadline")) {
+                if (!description.contains(" /by ")) {
+                    throw new DukeException("A deadline must have a date.");
                 }
 
-                if (storage.addTask(newTask)) {
-                    print(String.format(
+                String[] deadlineTokens = description.split(" /by ", 2);
+                newTask = new Deadline(deadlineTokens[0], deadlineTokens[1]);
+            } else {
+                if (!description.contains(" /at ")) {
+                    throw new DukeException("An event must have a date.");
+                }
+
+                String[] eventTokens = description.split(" /at ", 2);
+                newTask = new Event(eventTokens[0], eventTokens[1]);
+            }
+
+            if (storage.addTask(newTask)) {
+                print(String.format(
                         "Got it. I've added this task:%n%s%nNow you have %d tasks in the list.",
                         newTask.toString(), storage.getTaskCount()
-                    ));
-                } else {
-                    throw new DukeException("Task could not be saved.");
-                }
-
-                break;
+                ));
+            } else {
+                throw new DukeException("Task could not be saved.");
             }
 
-            default: {
-                throw new DukeException("I'm sorry, but I don't know what that means :-(");
-            }
+            break;
+        }
+
+        default: {
+            throw new DukeException("I'm sorry, but I don't know what that means :-(");
+        }
         }
     }
 

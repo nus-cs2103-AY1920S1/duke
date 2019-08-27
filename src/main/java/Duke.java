e42b83c0e21c04e2abd18792ffe15f620ecf6720
Wang Rong Shirley <e0035380@u.nus.edu>
2019-08-28 07:21:48 +0800

    added level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
 
+        final String LINE = "____________________________________________________________";
+        final String TAB = "    ";
         String cmd;
         Scanner scan = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<String>();
 
         /*
         String logo = " ____        _        \n"
@@ -15,29 +19,39 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         */
 
-        String line = "____________________________________________________________";
-        String tab = "    ";
-
-        printDuke(line);
+        printDuke(LINE);
         printDuke("Hello! I'm Duke");
         printDuke("What can I do for you?");
-        printDuke(line);
+        printDuke(LINE);
 
         while(true) {
-            cmd = scan.next();
+            cmd = scan.nextLine();
             if (cmd.equals("bye")) {
-                printDuke(line);
+                printDuke(LINE);
                 printDuke("Bye. Hope to see you again soon!");
-                printDuke(line);
+                printDuke(LINE);
                 break;
             }
-            printDuke(line);
-            printDuke(cmd);
-            printDuke(line);
+            if (cmd.equals("list")) {
+                printDuke(LINE);
+                printDuke(list);
+                printDuke(LINE);
+            } else {
+                list.add(cmd);
+                printDuke(LINE);
+                System.out.println(TAB + "added: " + cmd);
+                printDuke(LINE);
+            }
         }
     }
 
     public static void printDuke(String toPrint) {
         System.out.println("    " + toPrint);
+    }
+
+    public static void printDuke(ArrayList toPrint) {
+        for (int i = 1; i <= toPrint.size(); i++) {
+            System.out.println("    " + i + ". " + toPrint.get(i - 1));
+        }
     }
 }

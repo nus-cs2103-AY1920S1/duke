e6448a8946cbd594e8a3395133ea72a6ea69a081
brebeek <huyc1998@gmail.com>
2019-08-27 13:53:04 +0800

    minor bug fixes to level 8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -13,6 +13,6 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by.toString() + ")";
+        return "[D]" + super.toString() + " (by: " + new SimpleDateFormat("yyyy/MM/dd hh:mm").format(by) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -13,6 +13,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (at: " + at.toString() + ")";
+        return "[D]" + super.toString() + " (at: " + new SimpleDateFormat("yyyy/MM/dd hh:mm").format(at) + ")";
     }
 }

99efa7515cd495cac77a01c48f759dea1ef5dba4
choongyx <yongxin.choong98@gmail.com>
2019-08-27 13:44:05 +0800

    Implement dates and times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,10 +1,20 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Deadline extends Task {
 
-    protected String by;
+    Date by;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, String deadLine) throws DukeException {
         super(description);
-        this.by = by;
+        try {
+            DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HHmm");
+            this.by = dateFormat.parse(deadLine);
+            System.out.println(this.by);
+        } catch (Exception e){
+            throw new DukeException("Wrong date and time format");
+        }
     }
 
     @Override

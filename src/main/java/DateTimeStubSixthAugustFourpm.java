8a29bbbb91f085fc12d42efb1c49658689d82da6
therealbuyer <yekai.tan@gmail.com>
2019-08-27 23:48:51 +0800

    A-JUnit

---
+++ src/main/java/DateTimeStubSixthAugustFourpm.java
@@ -0,0 +1,13 @@
+
+public class DateTimeStubSixthAugustFourpm extends DateTime{
+
+    public DateTimeStubSixthAugustFourpm() {
+        super(16, 00, 6, "Aug", 2019);
+    }
+
+    @Override 
+    public String toString(){
+        return "6 Aug 4pm";
+    }
+
+}
---
+++ src/main/java/DateTimeStubSixthAugustTwopm.java
@@ -0,0 +1,13 @@
+
+public class DateTimeStubSixthAugustTwopm extends DateTime{
+
+    public DateTimeStubSixthAugustTwopm() {
+        super(14, 00, 6, "Aug", 2019);
+    }
+
+    @Override 
+    public String toString(){
+        return "6 Aug 2pm";
+    }
+
+}

4b0a48397b250795e88d301485fff082395a05a3
ShiHaoYap <shihaoyap@gmail.com>
2019-08-27 20:55:30 +0800

    Level-8

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,18 @@
-public class Event extends Task{
+import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    protected String at;
+public class Event extends Task{
+    protected LocalDateTime at;
+    protected DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
 
     public Event(String description, String at){
         super(description);
-        this.at = at;
+        this.at = LocalDateTime.parse(at, format1);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        return "[E]" + super.toString() + " (at: " + at.format(format1) + ")";
     }
 }

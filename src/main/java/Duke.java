b068e06ae4ac64cd057b0cfcda3887c934efb9fe
lingzw <lingzw64@gmail.com>
2019-08-28 01:07:54 +0800

    Revert "level-6"
    This reverts commit 440966feb9739656e91f0f52a3f185bd957c5e09.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
 import com.sun.source.tree.WhileLoopTree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -14,16 +18,49 @@ public class Duke {
                 "    ____________________________________________________________");
 
         Scanner input = new Scanner(System.in);
+        TaskList taskList = new TaskList("tasklist");
         String user = input.nextLine();
 
-        while (!user.equals("bye")) {
-            System.out.println("    ____________________________________________________________\n" +
-                    "     " + user +"\n" +
-                    "    ____________________________________________________________");
-            user =input.nextLine();
-        }
+        String[] cases = {"list","done","todo","deadline","event","delete"};
 
-        System.out.print("    ____________________________________________________________\n" +
+        while (!user.equals("bye")) {
+            int i = 0;
+            for(i = 0; i < cases.length; i++) {
+                if (user.contains(cases[i])) break;
+            }
+            try {
+                switch (i) {
+                    case 0:
+                        taskList.listTasks();
+                        break;
+                    case 1:
+                        taskList.completetask(user);
+                        break;
+                    case 2:
+                        taskList.addtodo(user.split(" ", 2)[1]);
+                        break;
+                    case 3:
+                        taskList.adddeadline(user.split(" ", 2)[1]);
+                        break;
+                    case 4:
+                        taskList.addevent(user.split(" ", 2)[1]);
+                        break;
+                    case 5:
+                        taskList.removetask(user);
+                        break;
+                    default:
+                        System.out.println("    ____________________________________________________________\n" +
+                                "     ☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n" +
+                                "    ____________________________________________________________");
+                }
+            }catch (IndexOutOfBoundsException e){
+                System.out.println("    ____________________________________________________________\n" +
+                        "     ☹ OOPS!!! The description of a task cannot be empty.\n" +
+                        "    ____________________________________________________________");
+            }
+            user = input.nextLine();
+        }
+        System.out.println("    ____________________________________________________________\n" +
                 "     Bye. Hope to see you again soon!\n" +
                 "    ____________________________________________________________");
     }

927003f5411805fcce62febbf5cbaf845c22460c
muhammadaljunied <muhammadaljunied@u.nus.edu>
2019-08-27 20:27:21 +0800

    Restructured project package to match seedu.duke Added Gradle Support Added Checkstyle plugin Added Shadow plugin Added JUnitTest Dependency

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,11 @@
+package seedu.duke;
+
 public class Duke {
+
+    /**
+     * Main Method.
+     * @param args string arguments for console.
+     */
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"

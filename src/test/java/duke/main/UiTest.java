31d9e8daa9c2f4b6836d5e2f0eb34bdc86c455e7
jeongyh99 <jeong.yuhan@gmail.com>
2019-08-27 14:18:11 +0800

    Commit JUnit integration

---
+++ src/test/java/duke/main/UiTest.java
@@ -0,0 +1,13 @@
+package duke.main;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UiTest {
+
+    @Test
+    public void test() {
+        assertEquals(2 ,4);
+    }
+
+}
---
+++ src/test/java/duke/task/ToDoTest.java
@@ -0,0 +1,6 @@
+package duke.task;
+
+public class ToDoTest {
+
+
+}

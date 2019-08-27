30a0c2ccc86f827d83e70235ee11217604d4aad2
ning-sy210 <ningshy97@gmail.com>
2019-08-27 20:11:29 +0800

    Added exception handling for Duke

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,18 @@ public class Main {
         duke.greet();
 
         Scanner sc = new Scanner(System.in);
-        String action = sc.nextLine();
+        String instruction = sc.nextLine();
 
-        while (!action.equals("bye")) {
-            duke.evaluate(action);
-            action = sc.nextLine();
+        while (!instruction.equals("bye")) {
+            try {
+                duke.evaluate(instruction);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.out.println(Duke.HORIZONTAL_LINE);        
+
+            } 
+
+            instruction = sc.nextLine();
         }
 
         duke.exit();

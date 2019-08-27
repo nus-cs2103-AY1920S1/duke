73fa34d77471a4f9426a4f81a80ad98896e98d38
arjavibahety <arjavibahety@gmail.com>
2019-08-28 01:33:23 +0800

    completed level 8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,20 @@
+import java.util.Date;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected Date by;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, Date by) {
         super(description);
         this.by = by;
+    }
+
+    public Date getBy() {
+        return by;
     }
 
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + by + ")";
-    } 
+    }
 }

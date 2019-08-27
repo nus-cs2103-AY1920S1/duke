cf3d119ff18723d62fda1e4e3fd397ba76e8838a
tohcejasmine <e0104958@u.nus.edu>
2019-08-27 13:41:57 +0800

    Level-7: Save tasks in hard disk automatically
     On branch branch-Level-7
     Changes to be committed:
    	modified:   src/main/java/Duke.java
    	new file:   src/main/java/DukeData.java
    	modified:   src/main/java/DukeException.java
    	modified:   src/main/java/Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,9 +1,11 @@
 public class Task {
     protected String description;
+    protected String subDescription; // For /by, /at
     protected boolean isDone;
 
-    public Task(String description) {
+    public Task(String description, String subDescription) {
         this.description = description;
+        this.subDescription = subDescription;
         this.isDone = false;
     }
 
@@ -13,57 +15,59 @@ public class Task {
     }
 
     public String getDescription() {
-        return (description);
+        return description;
     }
+    public String getSubDescription() {return subDescription; }
 
     public void markDone() {
-        this.isDone = true;
+        isDone = true;
     }
+    public boolean isDone() { return isDone; }
 
     //Display "[status-icon] task-description"
     public String toString() {
         return "[" + this.getStatusIcon() + "] " +
                 this.description;
     }
+
+    // Mainly for use in saving data to hard disk
+    public String getTaskType() { return "?"; }
 }
 
 class ToDo extends Task {
     public ToDo(String description) {
-        super(description);
+        super(description, null);
     }
 
     @Override
     public String toString() {
         return "[T]" + super.toString();
     }
+    public String getTaskType() { return "T"; }
 }
 
 class Deadline extends Task {
-
-    protected String by;
-
     public Deadline(String description, String by) {
-        super(description);
-        this.by = by;
+        super(description, by);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + subDescription + ")";
     }
+    public String getTaskType() { return "D"; }
+
 }
 
 class Event extends Task {
-
-    protected String at;
-
     public Event(String description, String at) {
-        super(description);
-        this.at = at;
+        super(description, at);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        return "[E]" + super.toString() + " (at: " + subDescription + ")";
     }
+    public String getTaskType() { return "E"; }
+
 }

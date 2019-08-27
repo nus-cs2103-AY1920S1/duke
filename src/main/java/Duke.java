9d60368f8a5ef05d2b1222795272e75f36dde935
ning-sy210 <ningshy97@gmail.com>
2019-08-27 13:45:41 +0800

    Duke can now greet users upon program startup

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    private static final String HORIZONTAL_LINE = "    ____________________________________________________________\n";
+
+    private String makeSpace(int n) {
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            str.append(' ');
+        }
+        return str.toString();
+    }
+    
+    public void greet() {
+        System.out.print(HORIZONTAL_LINE);
+        
+        String logo = makeSpace(5) + " ____        _        \n"
+                + makeSpace(5) + "|  _ \\ _   _| | _____ \n"
+                + makeSpace(5) + "| | | | | | | |/ / _ \\\n"
+                + makeSpace(5) + "| |_| | |_| |   <  __/\n"
+                + makeSpace(5) + "|____/ \\__,_|_|\\_\\___|\n";
+
+        String welcomeMessage = String.format("%s\n%sHello! I'm Duke!\n%sWhat can I do for you?", logo, makeSpace(5), 
+                makeSpace(5));
+        System.out.println(welcomeMessage);
+        System.out.println(HORIZONTAL_LINE);
     }
 }

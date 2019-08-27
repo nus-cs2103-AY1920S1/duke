312a23347f57910e69c368e4e3b387f5e3ed3259
chen-xi-cx <chenxi_95@hotmail.com>
2019-08-28 02:54:37 +0800

    Add Level-9 increment: add find command

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -47,6 +47,9 @@ public class Parser {
                         split("/at");
                 return new EventCommand(arguments);
             }
+        case "find":
+            String keyword = userInput.trim().substring(separatedInputs[0].length()).trim();
+            return new FindCommand(keyword);
         default:
             throw new DukeException("invalid input");
         }

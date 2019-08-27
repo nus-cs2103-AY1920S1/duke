a09c35505cea710a40b40282c232942972fc3625
dawo5010 <me@dawo.me>
2019-08-27 22:23:48 +0800

    Achieved A-JUnit: Added JUnit testing.

---
+++ src/test/java/duke/command/ParserTest.java
@@ -0,0 +1,19 @@
+package duke.command;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParserTest {
+
+    @Test
+    void joinStrings_normalInput_joinedString() {
+        assertEquals("read book", Parser.joinStrings(new String[]{"read", "book"}));
+    }
+
+    @Test
+    void splitByIdentifier_normalInput_splitString() {
+        assertArrayEquals(new String[]{"return book", "02/12/2019 1800"},
+                Parser.splitByIdentifier(new String[]{"return book", "/by", "02/12/2019 1800"}, "/by"));
+    }
+}
---
+++ src/test/java/duke/command/TaskListTest.java
@@ -0,0 +1,89 @@
+package duke.command;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TaskListTest {
+
+    Storage storage = new Storage("data/duke.txt");
+    TaskList taskList = new TaskList(storage);
+
+    @Test
+    void createTodo_normalInput_createdSuccessfully() {
+        try {
+            assertEquals("\t Got it. I've added this task:\n" +
+                    "\t   [T][✗] read book\n" +
+                    "\t Now you have 2 tasks in the list.\n",
+                    taskList.createTodo(new String[] {"read book"}));
+        } catch (Exception e) {
+            fail();
+        }
+
+    }
+
+    @Test
+    void createTodo_wrongInput_exceptionThrown() {
+        try {
+            assertEquals("\t Got it. I've added this task:\n" +
+                    "\t   [T][✗] read book\n" +
+                    "\t Now you have 2 tasks in the list.\n",
+                    taskList.createTodo(new String[]{}));
+            fail();
+        } catch (DukeException e) {
+            assertEquals("\t ☹ OOPS!!! The description of a todo cannot be empty.\n", e.toString());
+        }
+    }
+
+    @Test
+    void createDeadline_normalInput_createdSuccessfully() {
+        try {
+            assertEquals("\t Got it. I've added this task:\n" +
+                    "\t   [D][✗] return book (by: 02/12/2019 1800)\n" +
+                    "\t Now you have 3 tasks in the list.\n",
+                    taskList.createDeadline(new String[] {"return book", "/by", "02/12/2019 1800"}));
+        } catch (Exception e) {
+            fail();
+        }
+
+    }
+
+    @Test
+    void createDeadline_wrongInput_exceptionThrown() {
+        try {
+            assertEquals("\t Got it. I've added this task:\n" +
+                    "\t   [D][✗] return book (by: 02/12/2019 1800)\n" +
+                    "\t Now you have 3 tasks in the list.\n",
+                    taskList.createDeadline(new String[]{}));
+            fail();
+        } catch (DukeException e) {
+            assertEquals("\t ☹ OOPS!!! The description and due date of a deadline cannot be empty.\n", e.toString());
+        }
+    }
+    @Test
+    void createEvent_normalInput_createdSuccessfully() {
+        try {
+            assertEquals("\t Got it. I've added this task:\n" +
+                    "\t   [E][✗] project meeting (at: 06/08/2019 1400)\n" +
+                    "\t Now you have 1 task in the list.\n",
+                    taskList.createEvent(new String[] {"project meeting", "/at", "06/08/2019 1400"}));
+        } catch (Exception e) {
+            fail();
+        }
+
+    }
+
+    @Test
+    void createEvent_wrongInput_exceptionThrown() {
+        try {
+            assertEquals("\t Got it. I've added this task:\n" +
+                    "\t   [E][✗] project meeting (at: 06/08/2019 1400)\n" +
+                    "\t Now you have 1 task in the list.\n",
+                    taskList.createEvent(new String[]{}));
+            fail();
+        } catch (DukeException e) {
+            assertEquals("\t ☹ OOPS!!! The description and due date " +
+                    "of an event cannot be empty.\n", e.toString());
+        }
+    }
+}

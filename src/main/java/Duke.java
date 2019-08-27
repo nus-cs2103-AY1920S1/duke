0776dd725d6bb3ea74b611e66e8c7ba1f7bd8551
ManasVegi <manasvegi@gmail.com>
2019-08-28 00:43:49 +0800

    Final Merge (after Disaster Management)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,19 +17,39 @@ public class Duke {
         String textToAdd = "";
         for (Task t : tasks) {
             String type = "";
-            String time = "";
             if (t instanceof Event) {
                 type = "E";
-                time = ((Event) t).getTime();
             } else if (t instanceof ToDo) {
                 type = "T";
             } else if (t instanceof Deadline) {
                 type = "D";
-                time = ((Deadline) t).getTime();
             }
             textToAdd += "[" + type + "] -- " + "[" + t.getStatusIcon() + "] -- " + t.getDescription();
-            if (t instanceof Event || t instanceof Deadline) {
-                textToAdd += " -- " + time;
+            if (t instanceof Event) {
+                Calendar time = ((Event) t).getTime();
+                textToAdd += " -- " + time.get(Calendar.DAY_OF_MONTH) + "/" +
+                time.get(Calendar.MONTH) + "/" + time.get(Calendar.YEAR) + " ";
+                if (time.get(Calendar.HOUR_OF_DAY) < 10) {
+                    textToAdd += "0";
+                }
+                textToAdd += time.get(Calendar.HOUR_OF_DAY);
+                if (time.get(Calendar.MINUTE) < 10) {
+                    textToAdd += "0";
+                }
+                textToAdd += time.get(Calendar.MINUTE);
+            }
+            if (t instanceof Deadline) {
+                Calendar time = ((Deadline) t).getTime();
+                textToAdd += " -- " + time.get(Calendar.DAY_OF_MONTH) + "/" +
+                        time.get(Calendar.MONTH) + "/" + time.get(Calendar.YEAR) + " ";
+                if (time.get(Calendar.HOUR_OF_DAY) < 10) {
+                    textToAdd += "0";
+                }
+                textToAdd += time.get(Calendar.HOUR_OF_DAY);
+                if (time.get(Calendar.MINUTE) < 10) {
+                    textToAdd += "0";
+                }
+                textToAdd += time.get(Calendar.MINUTE);
             }
             textToAdd += "\n";
         }
@@ -58,12 +78,25 @@ public class Duke {
                 String s = reader.nextLine();
                 String[] tempArray = s.split(" -- ");
                 Task t;
-                if (tempArray[0].equals("[E]")) {
-                    t = new Event(tempArray[2], tempArray[3]);
+                if (tempArray[0].equals("[E]") || tempArray[0].equals("[D]")) {
+                    String dateTime = tempArray[3];
+                    Calendar dateAndTime = Calendar.getInstance();
+                    String[] date = dateTime.split(" ")[0].split("/");
+                    int time = Integer.parseInt(dateTime.split(" ")[1]);
+                    int hours = time / 100;
+                    int minutes = time % 100;
+                    dateAndTime.set(Integer.parseInt(date[2]),
+                            Integer.parseInt(date[1]),
+                            Integer.parseInt(date[0]),
+                            hours,
+                            minutes);
+                    if (tempArray[0].equals("[D]")) {
+                        t = new Deadline(tempArray[2], dateAndTime);
+                    } else {
+                        t = new Event(tempArray[2], dateAndTime);
+                    }
                 } else if (tempArray[0].equals("[T]")) {
                     t = new ToDo(tempArray[2]);
-                } else if (tempArray[0].equals("[D]")) {
-                    t = new Deadline(tempArray[2], tempArray[3]);
                 } else {
                     throw new DukeException("Not a valid Task type");
                 }
@@ -178,7 +211,7 @@ public class Duke {
                                 throw new DukeException(" :( OOPS!!! An event must have a time.");
                             } else {
                                 String instruction = command.split("event ")[1];
-                                String[] details = instruction.split(" /by ");
+                                String[] details = instruction.split(" /at ");
                                 String[] dateAndTime = details[1].split(" ");
                                 Calendar date = Calendar.getInstance();
                                 String[] dateArray = dateAndTime[0].split("/");

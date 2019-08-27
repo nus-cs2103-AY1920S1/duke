4f2617c2c3143ff98b75b72eba3c369427638155
jeongyh99 <jeong.yuhan@gmail.com>
2019-08-27 14:24:00 +0800

    Commit JUnit integration 2

---
+++ src/test/java/duke/exception/DukeExceptionTest.java
@@ -0,0 +1,13 @@
+package duke.exception;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DukeExceptionTest {
+
+    @Test
+    public void test() {
+        assertEquals(2 ,4);
+    }
+}

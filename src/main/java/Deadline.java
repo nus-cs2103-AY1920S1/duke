3c6a874b91acb4a8484f1b95272cd61023ac2d34
Sahilgat <nisgat@live.com>
2019-08-27 15:13:40 +0800

    Add level 8 features: Store date and time as LocalDateTime object instead of String

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,25 @@
+import java.time.LocalDateTime;
+import java.time.Month;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDateTime by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = parseDateTime(by);
     }
+    private LocalDateTime parseDateTime(String by) {
+        String[] splited = by.split(" ");
+        String[] dateFields = splited[0].split("/");
+        int hour = Integer.parseInt(splited[1].substring(0, 2));
+        int minute = Integer.parseInt(splited[1].substring(2));
+        return LocalDateTime.of(Integer.parseInt(dateFields[2]), Integer.parseInt(dateFields[1]), Integer.parseInt(dateFields[0]), hour, minute);
+    }
+    //Todo: Catch exceptions here
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.toLocalDate() + ", " + by.toLocalTime() + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,24 @@
+import java.time.LocalDateTime;
+
 public class Event extends Task {
 
-    protected String at;
+    protected LocalDateTime at;
 
     public Event(String description, String at) {
         super(description);
-        this.at = at;
+        this.at = parseDateTime(at);
     }
+    private LocalDateTime parseDateTime(String at) {
+        String[] splited = at.split(" ");
+        String[] dateFields = splited[0].split("/");
+        int hour = Integer.parseInt(splited[1].substring(0, 2));
+        int minute = Integer.parseInt(splited[1].substring(2));
+        return LocalDateTime.of(Integer.parseInt(dateFields[2]), Integer.parseInt(dateFields[1]), Integer.parseInt(dateFields[0]), hour, minute);
+    }
+    //Todo: Catch exceptions here
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        return "[E]" + super.toString() + " (at: " + at.toLocalDate() + ", " + at.toLocalTime() + ")";
     }
 }

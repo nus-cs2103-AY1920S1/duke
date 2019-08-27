a26b8bb1c35b6ad31b67ab982e655c6cb5ed11aa
Tishya Khanna <tishya.khanna01@gmail.com>
2019-08-28 00:33:16 +0800

    Level-4: Amended Task to prepare for inheritance

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,32 +1,32 @@
 public class Task {
     String name;
     boolean completed;
+    String type;
+
 
     public Task(String taskInput) {
         name = taskInput;
         completed = false;
+        type = null;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    //Change the "done" variable and return the String as completed
     public String taskComplete() {
         completed = true;
         String firstLine = "Nice! I've marked this task as done:\n";
         String secondLine = "      " + this;
+
         return firstLine+secondLine;
     }
 
-    //Print with the "Done" variable
     @Override
     public String toString() {
         if(completed) {
-            return  "[\u2713] " + name;
+            return "[" + type  + "]" + "[\u2713] " + name;
         } else {
-            return  "[\u2718] " + name;
+            return "[" + type + "]" + "[\u2718] " + name;
         }
 
     }
+
+
 }

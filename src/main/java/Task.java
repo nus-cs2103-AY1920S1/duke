e8d8ea54ed4c44b12a75fc6e11da9570f66ef2f1
yeodonghan <yeodonghan@gmail.com>
2019-08-28 01:16:14 +0800

    Revert "level-7"
    This reverts commit 7945ed150add42be4aa4cd8e1887a325860b7328.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,10 @@
 public class Task {
     protected String description;
     protected boolean isDone;
-    protected int status;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
-        this.status = 0;
     }
 
     public String getStatusIcon() {
@@ -16,18 +14,11 @@ public class Task {
     //...
     public void markAsDone() {
         this.isDone = true;
-        this.status = 1;
     }
 
     public String toString() {
         return "[" + this.getStatusIcon() + "] " + this.description;
     }
 
-    public int getStatus() {
-        return this.status;
-    }
 
-    public String getDescription() {
-        return description;
-    }
 }

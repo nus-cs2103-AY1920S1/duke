e905f234297802cc09f0889a216d1d8b8ef6295c
ArunKumarr <arunkumarr97@gmail.com>
2019-08-27 09:06:35 +0800

    branch-level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,52 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+import java.util.Date;
+
 public class Deadline extends Task {
-    private String midcommand; private String date;
-    public Deadline(String command){
+
+    private String midcommand; private String informalDate; private Date formalDate; private boolean isTimeDefined;
+
+    public Deadline(String command) throws ParseException {
         super(command);
         this.done = false;
-        String[]splitteddate = command.split("/");
-        date = splitteddate[1];
+        String[]splitteddate = command.split("/",2);
+
+        try {
+            SimpleDateFormat ft = new SimpleDateFormat("dd/MM/yyyy HHmm");
+            formalDate = ft.parse(splitteddate[1].substring(3));
+            //System.out.println(formalDate.toString());
+            isTimeDefined = true;
+        }catch(Exception e){
+            informalDate = splitteddate[1].substring(3);
+            isTimeDefined = false;
+        }
+
         midcommand = splitteddate[0];
     }
+
     public String printer(){
-        if(done){
-            String result = "[D][✓] " + midcommand + "(by: " + date.substring(3) + ")";
-            return result;
+        if(isTimeDefined){
+            //DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+            DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HHmm");
+            String formattedDate = dateFormat.format(formalDate);
+            if(done){
+                String result = "[D][✓] " + midcommand + "(by: " + formattedDate + ")";
+                return result;
+            }else{
+                String result = "[D][✗] " + midcommand + "(by: " + formattedDate + ")";
+                return result;
+            }
         }else{
-            String result = "[D][✗] " + midcommand + "(by: " + date.substring(3) + ")";
-            return result;
+            if(done){
+                String result = "[D][✓] " + midcommand + "(by: " + informalDate.substring(3) + ")";
+                return result;
+            }else{
+                String result = "[D][✗] " + midcommand + "(by: " + informalDate.substring(3) + ")";
+                return result;
+            }
         }
     }
 
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Scanner;
 

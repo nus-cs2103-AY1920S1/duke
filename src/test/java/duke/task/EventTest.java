ffb6916d00a3842877c3f919ab12d03c9fb6939d
Shuming <geshuming2@gmail.com>
2019-08-27 23:26:24 +0800

    Fix: package and import for JUnit test classes

--- src/test/java/duke/task/EventTest.java
+++ src/test/java/duke/task/EventTest.java
@@ -1,8 +1,8 @@
+package duke.task;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-
-import duke.task.Event;
 
 class EventTest {
     @Test
--- src/test/java/duke/task/TodoTest.java
+++ src/test/java/duke/task/TodoTest.java
@@ -1,8 +1,8 @@
+package duke.task;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-
-import duke.task.Todo;
 
 class TodoTest {
     @Test

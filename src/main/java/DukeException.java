74d098307caedbb56796459ef4a0ebd63b1f3436
Tishya Khanna <tishya.khanna01@gmail.com>
2019-08-28 07:46:01 +0800

    Level-5: Add Exceptions to handle 2 different cases

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,8 +1,8 @@
 public class DukeException extends Exception {
 
-    public DukeException(String message) {
+    public DukeException() {
 
-        super(message);
+        super("\u2639 OOPS!!! I'm sorry, but I don't know what that means :-(");
     }
 }
 
---
+++ src/main/java/incompleteInputException.java
@@ -0,0 +1,7 @@
+public class incompleteInputException extends Exception {
+
+    public incompleteInputException(String message) {
+
+        super(message);
+    }
+}

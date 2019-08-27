25793ec5ddf3f6cec1b84522c4efe35031143539
ning-sy210 <ningshy97@gmail.com>
2019-08-27 16:25:36 +0800

    Added Deadline.java

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,18 @@
+public class Deadline extends Task {
+    private String date;
+    private String time;
+
+    public Deadline(String desc, String date, String time) {
+        super(desc);
+        this.date = date;
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        if (time.isEmpty()) {
+            return String.format("[D][%s] %s (by: %s)", getStatusIcon(), description, date);
+        }
+        return String.format("[D][%s] %s (by: %s %s)", getStatusIcon(), description, date, time);
+    }
+}

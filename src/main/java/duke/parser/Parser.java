7ab9e5b85cb20ecb9fb9a76ea0e5ce19ad69dd03
Shuming <geshuming2@gmail.com>
2019-08-27 18:28:13 +0800

    Fix checkstyle errors

--- src/main/java/duke/parser/Parser.java
+++ src/main/java/duke/parser/Parser.java
@@ -3,7 +3,11 @@ package duke.parser;
 import duke.command.Command;
 import duke.exception.DukeException;
 import duke.storage.Storage;
-import duke.task.*;
+import duke.task.Deadline;
+import duke.task.Event;
+import duke.task.Task;
+import duke.task.TaskList;
+import duke.task.Todo;
 import duke.ui.Ui;
 
 public class Parser {
--- src/main/java/duke/task/Task.java
+++ src/main/java/duke/task/Task.java
@@ -1,6 +1,6 @@
 package duke.task;
 
-abstract public class Task {
+public abstract class Task {
     public String description;
     public boolean isDone;
 

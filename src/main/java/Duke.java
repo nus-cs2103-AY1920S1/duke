f40ece7422689513cc337dc83630dae6b3dd4b15
Joanna-YJA <joannajoules@gmail.com>
2019-08-28 00:47:00 +0800

    Level 6 updates

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,19 +16,107 @@ public class Duke {
         while (!exited && scanner.hasNextLine()) {
             String input = scanner.nextLine();
 
-           try{
-               giveRespond(input);
-           } catch (IllegalArgumentException e1 ){
-               System.out.println(e1);
-           } catch (EmptyDescException e2){
-               System.out.println(e2);
-           }
+            try {
+                giveRespond(input);
+            } catch (IllegalArgumentException e1) {
+                System.out.println(e1);
+            } catch (EmptyDescException e2) {
+                System.out.println(e2);
+            }
         }
     }
 
+    public void determineInputType(String input) throws IllegalArgumentException, EmptyDescException{
+        if (input.contains("done")) {
+            int taskNum = Integer.parseInt(input.substring(5)) - 1;
+            Task updatedTask = toDoList.get(taskNum);
+            updatedTask.markAsDone();
+            System.out.println("Nice! I've marked this task as done: ");
+            System.out.println(updatedTask);
+
+
+        } else if (input.contains("todo")) {
+
+            if (input.length() < 5) {
+                throw new EmptyDescException("todo");
+            } else {
+
+                String desc = input.substring(5);
+
+                Todo newTodo = new Todo(desc);
+
+                toDoList.add(newTodo);
+
+                int numTask = toDoList.size();
+
+
+                System.out.println("Got it. I've added this task: \n" + "  "
+                        + newTodo + "Now you have " +
+                        numTask + " tasks in the list.");
+            }
+
+
+        } else if (input.contains("deadline")) {
+
+            if (input.length() < 9) {
+                throw new EmptyDescException("deadline");
+            } else {
+
+                int deadlineIndex = input.indexOf('/') + 4;
+                String deadline = input.substring(deadlineIndex);
+                String desc = input.substring(9, deadlineIndex - 5);
+
+                Deadline newDeadline = new Deadline(desc, deadline);
+
+                toDoList.add(newDeadline);
+
+                int numTask = toDoList.size();
+
+
+                System.out.println("Got it. I've added this task: \n" + "  "
+                        + newDeadline + "Now you have " +
+                        numTask + " tasks in the list.");
+            }
+        } else if (input.contains("event")) {
+
+            if (input.length() < 6) {
+                throw new EmptyDescException("event");
+            } else {
+
+                int timeIndex = input.indexOf('/') + 4;
+                String time = input.substring(timeIndex);
+
+                String desc = input.substring(6, timeIndex - 5);
+
+
+                Event newEvent = new Event(desc, time);
+
+                toDoList.add(newEvent);
+
+                int numTask = toDoList.size();
+
+
+                System.out.println("Got it. I've added this task: \n" + "  "
+                        + newEvent + "Now you have " +
+                        numTask + " tasks in the list.");
+            }
+        } else if (input.contains("delete")) {
+            int taskNum = Integer.parseInt(input.substring(7)) - 1;
+            Task removedTask = toDoList.remove(taskNum);
+            System.out.println("Noted. I've removed this task:\n" + removedTask +
+                    "Now you have " + toDoList.size() + " tasks in the list.");
+
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+
     public void giveRespond(String input) throws IllegalArgumentException, EmptyDescException {
         switch (input) {
+
             case "bye":
+
                 exited = true;
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
@@ -44,88 +132,7 @@ public class Duke {
                 break;
 
             default:
-                if (input.contains("done")) {
-                    int taskNum = Integer.parseInt(input.substring(5)) - 1;
-                    Task updatedTask = toDoList.get(taskNum);
-                    updatedTask.markAsDone();
-                    System.out.println("Nice! I've marked this task as done: ");
-                    System.out.println(updatedTask);
-
-
-                } else if (input.contains("todo")) {
-
-                    if (input.length() < 5) {
-                        throw new EmptyDescException("todo");
-                    } else {
-
-                        String desc = input.substring(5);
-
-                        Todo newTodo = new Todo(desc);
-
-                        toDoList.add(newTodo);
-
-                        int numTask = toDoList.size();
-
-
-                        System.out.println("Got it. I've added this task: \n" + "  "
-                                + newTodo + "Now you have " +
-                                numTask + " tasks in the list.");
-                    }
-
-
-                } else if (input.contains("deadline")) {
-
-                    if (input.length() < 9) {
-                        throw new EmptyDescException("deadline");
-                    } else {
-
-                        int deadlineIndex = input.indexOf('/') + 4;
-                        String deadline = input.substring(deadlineIndex);
-                        String desc = input.substring(9, deadlineIndex - 5);
-
-                        Deadline newDeadline = new Deadline(desc, deadline);
-
-                        toDoList.add(newDeadline);
-
-                        int numTask = toDoList.size();
-
-
-                        System.out.println("Got it. I've added this task: \n" + "  "
-                                + newDeadline + "Now you have " +
-                                numTask + " tasks in the list.");
-                    }
-                } else if (input.contains("event")) {
-
-                    if (input.length() < 6) {
-                        throw new EmptyDescException("event");
-                    } else {
-
-                        int timeIndex = input.indexOf('/') + 4;
-                        String time = input.substring(timeIndex);
-
-                        String desc = input.substring(6, timeIndex - 5);
-
-
-                        Event newEvent = new Event(desc, time);
-
-                        toDoList.add(newEvent);
-
-                        int numTask = toDoList.size();
-
-
-                        System.out.println("Got it. I've added this task: \n" + "  "
-                                + newEvent + "Now you have " +
-                                numTask + " tasks in the list.");
-                    }
-                } else if(input.contains("delete")){
-                    int taskNum = Integer.parseInt(input.substring(7)) - 1;
-                   Task removedTask =  toDoList.remove(taskNum);
-                    System.out.println("Noted. I've removed this task:\n" + removedTask +
-                            "Now you have " + toDoList.size() + " tasks in the list.");
-
-                } else {
-                    throw new IllegalArgumentException();
-                }
+                determineInputType(input);
         }
     }
 
@@ -136,3 +143,4 @@ public class Duke {
     }
 
 }
+

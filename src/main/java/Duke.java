5dee39e25d9700ff3c5e07257c1ee2af7e2a12d7
madanalogy <Ahmed_Bahajjaj@u.nus.edu>
2019-08-27 21:18:28 +0800

    Cleaned up code for style

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,61 +1,61 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
 /**
- *  Week 2 iP Deliverable
+ *  CS2103 Week 2 iP Deliverable
  *  @author Ahmed Bahajjaj
  */
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean hi = true;
-        String input = "";
-        int value = 0;
         ArrayList<Task> tasks = new ArrayList<>();
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         while(hi) {
             try{
-                input = sc.nextLine();
-                String[] cmd = input.split(" ");
-                switch (cmd[0]) {
-                    case "bye":
-                        System.out.println("Bye. Hope to see you again soon!");
-                        hi = false;
-                        break;
-                    case "list":
-                        if (tasks.size() == 0) {
-                            System.out.println("List is empty!");
-                        } else {
-                            System.out.println("Here are the tasks in your list:");
-                            for (int i = 0; i < tasks.size(); i++) {
-                                System.out.println((i + 1) + "." + tasks.get(i));
-                            }
+                String input = sc.nextLine();
+                String task = input.split(" ")[0];
+                int value;
+                switch (task) {
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    hi = false;
+                    break;
+                case "list":
+                    if (tasks.size() == 0) {
+                        System.out.println("List is empty!");
+                    } else {
+                        System.out.println("Here are the tasks in your list:");
+                        for (int i = 0; i < tasks.size(); i++) {
+                            System.out.println((i + 1) + "." + tasks.get(i));
                         }
-                        break;
-                    case "done":
-                        value = Integer.parseInt(input.substring(input.length() - 1));
-                        tasks.get(value - 1).setDone();
-                        System.out.println("Nice! I've marked this task as done:");
-                        System.out.println(tasks.get(value - 1));
-                        break;
-                    case "delete":
-                        value = Integer.parseInt(input.substring(input.length() - 1));
-                        System.out.println("Noted. I've removed this task:");
-                        System.out.println(tasks.get(value - 1));
-                        tasks.remove(value - 1);
-                        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-                        break;
-                    default:
-                        tasks.add(create(cmd, input));
-                        System.out.println("Got it. I've added this task:");
-                        System.out.println(tasks.get(tasks.size() - 1));
-                        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-                        break;
+                    }
+                    break;
+                case "done":
+                    value = Integer.parseInt(input.substring(input.length() - 1));
+                    tasks.get(value - 1).setDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(tasks.get(value - 1));
+                    break;
+                case "delete":
+                    value = Integer.parseInt(input.substring(input.length() - 1));
+                    System.out.println("Noted. I've removed this task:");
+                    System.out.println(tasks.get(value - 1));
+                    tasks.remove(value - 1);
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                    break;
+                default:
+                    tasks.add(create(task, input));
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(tasks.get(tasks.size() - 1));
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                    break;
                 }
-            } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("OOPS!!! The description of a task cannot be empty! :(");
-            } catch (UnsupportedOperationException e) {
+            } catch (ArrayIndexOutOfBoundsException ex) {
+                System.out.println("OOPS!!! The details of an Event/Deadline cannot be empty! :(");
+            } catch (UnsupportedOperationException ex) {
                 System.out.println("OOPS!!! I'm sorry, but I don't know what that means! :(");
-            } catch (Exception e) {
+            } catch (Exception ex) {
                 System.out.println("OOPS!!! Something broke. Terminating now :(");
                 hi = false;
             }
@@ -79,24 +79,24 @@ public class Duke {
 
     /**
      * Creates Task from input command and description
-     * @param cmd User input separated by spaces
+     * @param task User input separated by spaces
      * @param input User input unprocessed
      * @return Task
      * @throws UnsupportedOperationException Command not understood
      */
-    private static Task create(String[] cmd, String input) throws UnsupportedOperationException {
-        input = input.replace(cmd[0], "");
-        switch (cmd[0]) {
-            case "todo":
-                return new Todo(input, "");
-            case "deadline":
-                cmd = process(input);
-                return new Deadline(cmd[0], cmd[1]);
-            case "event":
-                cmd = process(input);
-                return new Event(cmd[0], cmd[1]);
-            default:
-                throw new UnsupportedOperationException();
+    private static Task create(String task, String input) throws UnsupportedOperationException {
+        input = input.replace(task, "");
+        switch (task) {
+        case "todo":
+            return new Todo(input, "");
+        case "deadline":
+            String[] dead = process(input);
+            return new Deadline(dead[0], dead[1]);
+        case "event":
+            String[] event = process(input);
+            return new Event(event[0], event[1]);
+        default:
+            throw new UnsupportedOperationException();
         }
     }
 }

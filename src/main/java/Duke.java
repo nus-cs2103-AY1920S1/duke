0e88723ef907e79135527f01fb8897cf4efa02af
TanZhanMing <e0349703@u.nus.edu>
2019-08-27 22:13:47 +0800

    Rushed level 9 for midnight deadline. Note to self: change list and find code from input parser to ui class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,7 +7,8 @@ public class Duke {
     public enum Action {
         ADD,
         REMOVE,
-        DONE
+        DONE,
+        FIND
     }
 
     public static void main(String[] args) throws IOException {

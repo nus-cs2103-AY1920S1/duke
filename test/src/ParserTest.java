6afc7f475bc722e3781facc101739aa65ab4c545
Oscar-B-Liang <dedicus545@gmail.com>
2019-08-27 12:05:22 +0800

    A-JUnit Improved

--- test/src/ParserTest.java
+++ test/src/ParserTest.java
@@ -1,6 +1,8 @@
 import duke.Commands.Command;
 import duke.DirectProcessor.Parser;
 import duke.DirectProcessor.TaskList;
+import duke.Duke;
+import duke.DukeException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,37 +11,67 @@ public class ParserTest {
 
     @Test
     public void addTodoTest() {
-        Command c = Parser.parse("todo Eat dinner");
-        assertEquals("duke.Commands.AddCommand", c.getClass().getName());
+        try {
+            Command c = Parser.parse("todo Eat dinner");
+            assertEquals("duke.Commands.AddCommand", c.getClass().getName());
+        } catch (DukeException e) {
+            System.out.println(e.getMessage());
+            assertEquals(1, 2);
+        }
     }
 
     @Test
     public void addEventTest() {
-        Command c = Parser.parse("event Eat dinner /at 19/08/2019 18:00:00");
-        assertEquals("duke.Commands.AddCommand", c.getClass().getName());
+        try {
+            Command c = Parser.parse("event Eat dinner /at 19/08/2019 18:00:00");
+            assertEquals("duke.Commands.AddCommand", c.getClass().getName());
+        } catch (DukeException e) {
+            System.out.println(e.getMessage());
+            assertEquals(1, 2);
+        }
     }
 
     @Test
     public void addDeadlineTest() {
-        Command c = Parser.parse("deadline Eat dinner /by 19/08/2019 18:00:00");
-        assertEquals("duke.Commands.AddCommand", c.getClass().getName());
+        try {
+            Command c = Parser.parse("deadline Eat dinner /by 19/08/2019 18:00:00");
+            assertEquals("duke.Commands.AddCommand", c.getClass().getName());
+        } catch (DukeException e) {
+            System.out.println(e.getMessage());
+            assertEquals(1, 2);
+        }
     }
 
     @Test
     public void finishTest() {
-        Command c = Parser.parse("done 2");
-        assertEquals("duke.Commands.FinishCommand", c.getClass().getName());
+        try {
+            Command c = Parser.parse("done 2");
+            assertEquals("duke.Commands.FinishCommand", c.getClass().getName());
+        } catch (DukeException e) {
+            System.out.println(e.getMessage());
+            assertEquals(1, 2);
+        }
     }
 
     @Test
     public void listTest() {
-        Command c = Parser.parse("list");
-        assertEquals("duke.Commands.ListCommand", c.getClass().getName());
+        try {
+            Command c = Parser.parse("list");
+            assertEquals("duke.Commands.ListCommand", c.getClass().getName());
+        } catch (DukeException e) {
+            System.out.println(e.getMessage());
+            assertEquals(1, 2);
+        }
     }
 
     @Test
     public void exitTest() {
-        Command c = Parser.parse("bye");
-        assertEquals("duke.Commands.ExitCommand", c.getClass().getName());
+        try {
+            Command c = Parser.parse("bye");
+            assertEquals("duke.Commands.ExitCommand", c.getClass().getName());
+        } catch (DukeException e) {
+            System.out.println(e.getMessage());
+            assertEquals(1, 2);
+        }
     }
 }

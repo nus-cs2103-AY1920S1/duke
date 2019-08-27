c986ff041d4560d7f15d3550b20cd6d54a6b0331
ShaunNgTX <ngtzexuan95@gmail.com>
2019-08-27 18:50:05 +0800

    Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,6 @@
 import java.util.Objects;
 import java.util.Scanner;
+import java.lang.String;
 
 public class Duke {
     public static void main(String[] args) {
@@ -12,14 +13,36 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?\n");
         Scanner scanner = new Scanner(System.in);
-        String inputString = null;
-        while (!(Objects.equals(inputString, BYE))) {
+        String inputString;
+        Task listOfItems[] = new Task[100];
+        int numOfItems = 0;
+        
+        while (true) {
             inputString = scanner.nextLine();
-            if (!(Objects.equals(inputString, BYE))) {
-                System.out.println("\t\t" + inputString + "\n");
-            } else {
+            String TempInputString = inputString;
+            String wordSplit[] = TempInputString.split(" ");
+            //System.out.println("\n" + TempInputString +"\n" + wordSplit[0] + "\n" + wordSplit[1]);
+            if ((Objects.equals(inputString, BYE))) {
                 System.out.println("\t\tBye. Hope to see you again soon!\n");
+                return;
+            }
+            else if ((Objects.equals(wordSplit[0], "done") && (Integer.parseInt(wordSplit[1]) <= numOfItems))){
+                int numbering = Integer.parseInt((TempInputString.split(" "))[1]) - 1;
+                listOfItems[numbering].setDone();
+                System.out.println("\t\tNice, I've marked this item as done\n\t\t["
+                        + listOfItems[numbering].getStatusIcon() + "] " + listOfItems[numbering].description + "\n");
+            }
+            else if (Objects.equals(inputString, "list")) {
+                for (int i=0; i<numOfItems; i++ ) {
+                    System.out.println("\t\t[" + listOfItems[i].getStatusIcon() + "] " + listOfItems[i].description + "\n" );
+                }
+            }
+            else {
+                Task item = new Task(inputString);
+                listOfItems[numOfItems++] = item;
+                System.out.println("\t\t" + inputString + "\n");
             }
         }
     }
 }
+

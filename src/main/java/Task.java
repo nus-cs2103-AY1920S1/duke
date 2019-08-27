7945ed150add42be4aa4cd8e1887a325860b7328
yeodonghan <>
2019-08-27 12:32:39 +0800

    level-7

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,12 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+    protected int status;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+        this.status = 0;
     }
 
     public String getStatusIcon() {
@@ -14,11 +16,18 @@ public class Task {
     //...
     public void markAsDone() {
         this.isDone = true;
+        this.status = 1;
     }
 
     public String toString() {
         return "[" + this.getStatusIcon() + "] " + this.description;
     }
 
+    public int getStatus() {
+        return this.status;
+    }
 
+    public String getDescription() {
+        return description;
+    }
 }

993ca54005b7c9285fb9ce62f2963be3df4da7f8
ManasVegi <manasvegi@gmail.com>
2019-08-29 01:02:15 +0800

    Create and implement Parser

---
+++ src/main/java/Parser.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+
+public class Parser {
+    private boolean valid = true;
+    private ArrayList<String> list = new ArrayList<>();
+    private String type;
+
+    public Parser(String command) {
+        String[] commandWords = command.split(" ");
+        String order = commandWords[0];
+        if (order.equals("todo")) {
+            type = "todo";
+            if (command.split("todo ").length > 1) {
+                list.add(command.split("todo ")[1]);
+            }
+        } else if (order.equals("event")) {
+            type = "event";
+            String instruction = command.split("event ")[1];
+            String[] details = instruction.split(" /at ");
+            String[] dateAndTime = details[1].split(" ");
+            String[] dateArray = dateAndTime[0].split("/");
+            int time = Integer.parseInt(dateAndTime[1]);
+            int hours = time / 100;
+            int minutes = time % 100;
+            list.add(details[0]);
+            list.add(dateArray[0]);
+            list.add(dateArray[1]);
+            list.add(dateArray[2]);
+            list.add("" + hours);
+            list.add("" + minutes);
+        } else if (order.equals("deadline")) {
+            type = "deadline";
+            String instruction = command.split("deadline ")[1];
+            String[] details = instruction.split(" /by ");
+            String[] dateAndTime = details[1].split(" ");
+            String[] dateArray = dateAndTime[0].split("/");
+            int time = Integer.parseInt(dateAndTime[1]);
+            int hours = time / 100;
+            int minutes = time % 100;
+            list.add(details[0]);
+            list.add(dateArray[0]);
+            list.add(dateArray[1]);
+            list.add(dateArray[2]);
+            list.add("" + hours);
+            list.add("" + minutes);
+        } else if (order.equals("done") || order.equals("delete")) {
+            type = order;
+            list.add(command.split(order + " ")[1]);
+        } else if (order.equals("list") || order.equals("bye")) {
+            type = order;
+        } else {
+            valid = false;
+        }
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public ArrayList<String> getList() {
+        return list;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+}

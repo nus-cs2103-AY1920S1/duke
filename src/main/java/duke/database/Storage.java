66fb595feebf94f5b1d9a7881b6ae091e7222e89
shyanjie <shyanjieteo@hotmail.com>
2019-08-29 04:54:57 +0800

    Revert "Creating jar file"
    This reverts commit 0fb187b854e9fb59be038bc9e7b847afd00d50c7.

--- src/main/java/duke/database/Storage.java
+++ src/main/java/duke/database/Storage.java
@@ -97,46 +97,46 @@ public class Storage {
         Task task;
 
         switch (data[0].trim()) {
-        case "T":
-            task = new Todo(data[2].trim());
-            try {
-                if (Integer.parseInt(data[1].trim()) == 1) {
-                    task.markAsDone();
+            case "T":
+                task = new Todo(data[2].trim());
+                try {
+                    if (Integer.parseInt(data[1].trim()) == 1) {
+                        task.markAsDone();
+                    }
+                } catch (NumberFormatException ex) {
+                    ui.showLine();
+                    System.out.println(ui.INDENT_COMMENT + "\u2639 OOPS !!! " + "Error in Database Data");
                 }
-            } catch (NumberFormatException ex) {
-                ui.showLine();
-                System.out.println(ui.INDENT_COMMENT + "\u2639 OOPS !!! " + "Error in Database Data");
-            }
-            break;
+                break;
 
-        case "D":
-            task = new Deadline(data[2].trim(), data[3].trim());
+            case "D":
+                task = new Deadline(data[2].trim(), data[3].trim());
 
-            try {
-                if (Integer.parseInt(data[1].trim()) == 1) {
-                    task.markAsDone();
+                try {
+                    if (Integer.parseInt(data[1].trim()) == 1) {
+                        task.markAsDone();
+                    }
+                } catch (NumberFormatException ex) {
+                    ui.showLine();
+                    System.out.println(ui.INDENT_COMMENT + "\u2639 OOPS !!! " + "Error in Database Data");
                 }
-            } catch (NumberFormatException ex) {
-                ui.showLine();
-                System.out.println(ui.INDENT_COMMENT + "\u2639 OOPS !!! " + "Error in Database Data");
-            }
-            break;
+                break;
 
-        case "E":
-            task = new Event(data[2].trim(), data[3].trim());
+            case "E":
+                task = new Event(data[2].trim(), data[3].trim());
 
-            try {
-                if (Integer.parseInt(data[1].trim()) == 1) {
-                    task.markAsDone();
+                try {
+                    if (Integer.parseInt(data[1].trim()) == 1) {
+                        task.markAsDone();
+                    }
+                } catch (NumberFormatException ex) {
+                    ui.showLine();
+                    System.out.println(ui.INDENT_COMMENT + "\u2639 OOPS !!! " + "Error in Database Data");
                 }
-            } catch (NumberFormatException ex) {
-                ui.showLine();
-                System.out.println(ui.INDENT_COMMENT + "\u2639 OOPS !!! " + "Error in Database Data");
-            }
-            break;
+                break;
 
-        default:
-            throw new DukeException(ui.INDENT_COMMENT + "\u2639 OOPS !!! " + "Error in reading data");
+            default:
+                throw new DukeException(ui.INDENT_COMMENT + "\u2639 OOPS !!! " + "Error in reading data");
         }
 
         return task;
@@ -194,8 +194,7 @@ public class Storage {
             }
             writer.close();
         } catch (IOException e) {
-            ui.showLine();
-            System.out.println(ui.INDENT_COMMENT + "Unable to Update File");
+            System.out.println("Unable to Update File");
         }
 
     }

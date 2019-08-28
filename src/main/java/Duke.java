1eb4d2605993216d6e78c6f702e1a66d5f872dd7
Joanna-YJA <joannajoules@gmail.com>
2019-08-28 15:37:23 +0800

    Level 7 updates v2.0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,40 +1,168 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.Path;
-import java.nio.charset.StandardCharsets;
-
-
-
 
 
 public class Duke {
-    boolean exited = false;
-    List<Task> toDoList = new ArrayList<>();
+    private boolean exited = false;
+    private List<Task> toDoList = new ArrayList<>();
     //set a variable to store the fixed file path of duke.txt
-    static String filePath = "/Users/joannayap/Downloads/duke/src/main/data/duke.txt";
+    private static String filePath = "/Users/joannayap/Downloads/duke/src/main/data/duke.txt";
 
 
-    public void writeToFile(String textToAdd) throws IOException{
+    public void writeToFile(String textToAdd) throws IOException {
         //Create a file writer object to represent the hard disk
         FileWriter fw = new FileWriter(filePath, true);
         fw.write(textToAdd);
         fw.close();
     }
 
+
+    public void appendToFile(String type, String desc, String date) {
+        try {
+           if(date == null)
+               writeToFile(type + " | 1 | " + desc + "\n");
+           else
+               writeToFile(type + " | 1 | " + desc + " | " + date +"\n");
+
+        } catch (IOException e) {
+            System.out.println("Something went wrong " + e.getMessage());
+        }
+
+    }
     //create a method to update a specific line in file ( when the task is marked as done)
 
-    public static void updateText(int lineNumber) throws IOException{
+    public static void updateText(int lineNumber) throws IOException {
         Path path = Paths.get(filePath);
-     //read all the line in the files
-        List<String>  lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+        //read all the line in the files
+        List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
         String oldText = lines.get(lineNumber);
         lines.set(lineNumber, oldText.substring(0, 3) + " 0 " + oldText.substring(6, oldText.length()));
         Files.write(path, lines, StandardCharsets.UTF_8);
+    }
+
+    public static void deleteText(int lineNumber) throws IOException {
+        Path path = Paths.get(filePath);
+        //read all the line in the files
+        List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+        lines.remove(lineNumber);
+        Files.write(path, lines, StandardCharsets.UTF_8);
+    }
+
+
+    public void markTaskDone(String input) {
+        //parse out the task number
+        int taskNum = Integer.parseInt(input.substring(5)) - 1;
+
+        //Ge the specific task object from toDoList
+        Task updatedTask = toDoList.get(taskNum);
+
+        //Mark the task as done in duke.txt
+        try {
+            updateText(taskNum);
+        } catch (IOException e) {
+            System.out.println("Something went wrong " + e.getMessage());
+        }
+        updatedTask.markAsDone();
+        System.out.println("Nice! I've marked this task as done: ");
+        System.out.println(updatedTask);
+    }
+
+
+    public void addTodoTask(String input) throws EmptyDescException {
+        if (input.length() < 5) {
+            throw new EmptyDescException("todo");
+        } else {
+
+            String desc = input.substring(5);
+
+            appendToFile("T", desc, null);
+
+            Todo newTodo = new Todo(desc);
+
+            toDoList.add(newTodo);
+
+            int numTask = toDoList.size();
+
+
+            System.out.println("Got it. I've added this task: \n" + "  "
+                    + newTodo + "Now you have " +
+                    numTask + " tasks in the list.");
+        }
+    }
+
+
+    public void addDeadlineTask(String input) throws EmptyDescException {
+        if (input.length() < 9) {
+            throw new EmptyDescException("deadline");
+        } else {
+
+
+            int deadlineIndex = input.indexOf('/') + 4;
+            String deadline = input.substring(deadlineIndex);
+            String desc = input.substring(9, deadlineIndex - 5);
+
+
+            appendToFile("D", desc, deadline);
+
+            Deadline newDeadline = new Deadline(desc, deadline);
+
+            toDoList.add(newDeadline);
+
+            int numTask = toDoList.size();
+
+
+            System.out.println("Got it. I've added this task: \n" + "  "
+                    + newDeadline + "Now you have " +
+                    numTask + " tasks in the list.");
+        }
+    }
+
+    public void addEventTask(String input) throws EmptyDescException {
+        if (input.length() < 6) {
+            throw new EmptyDescException("event");
+        } else {
+
+            int timeIndex = input.indexOf('/') + 4;
+            String time = input.substring(timeIndex);
+
+            String desc = input.substring(6, timeIndex - 5);
+
+            appendToFile("E", desc, time);
+
+
+            Event newEvent = new Event(desc, time);
+
+            toDoList.add(newEvent);
+
+            int numTask = toDoList.size();
+
+
+            System.out.println("Got it. I've added this task: \n" + "  "
+                    + newEvent + "Now you have " +
+                    numTask + " tasks in the list.");
+        }
+    }
+
+    public void deleteTask(String input) {
+        int taskNum = Integer.parseInt(input.substring(7)) - 1;
+        //delete text from duke.txt
+        try {
+            deleteText(taskNum);
+        } catch (IOException e) {
+            System.out.println("Something went wrong " + e.getMessage());
+        }
+
+        Task removedTask = toDoList.remove(taskNum);
+        System.out.println("Noted. I've removed this task:\n" + removedTask +
+                "Now you have " + toDoList.size() + " tasks in the list.");
+
     }
 
     public void respond() {
@@ -56,116 +184,28 @@ public class Duke {
         }
     }
 
-    public void determineInputType(String input) throws IllegalArgumentException, EmptyDescException{
+    public void determineInputType(String input) throws IllegalArgumentException, EmptyDescException {
         if (input.contains("done")) {
-            int taskNum = Integer.parseInt(input.substring(5)) - 1;
-            Task updatedTask = toDoList.get(taskNum);
-            try{
-                updateText(taskNum);
-            } catch (IOException e){
-                System.out.println("Something went wrong " + e.getMessage());
-            }
-            updatedTask.markAsDone();
-            System.out.println("Nice! I've marked this task as done: ");
-            System.out.println(updatedTask);
+
+
+            markTaskDone(input);
 
 
         } else if (input.contains("todo")) {
 
-            if (input.length() < 5) {
-                throw new EmptyDescException("todo");
-            } else {
-
-                String desc = input.substring(5);
-
-              try{
-                  writeToFile("T | 1 | " + desc + "\n");
-              } catch (IOException e){
-                  System.out.println("Something went wrong " + e.getMessage());
-              }
-
-                Todo newTodo = new Todo(desc);
-
-                toDoList.add(newTodo);
-
-                int numTask = toDoList.size();
-
-
-                System.out.println("Got it. I've added this task: \n" + "  "
-                        + newTodo + "Now you have " +
-                        numTask + " tasks in the list.");
-            }
+            addTodoTask(input);
 
 
         } else if (input.contains("deadline")) {
 
-            if (input.length() < 9) {
-                throw new EmptyDescException("deadline");
-            } else {
+            addDeadlineTask(input);
 
-                int deadlineIndex = input.indexOf('/') + 4;
-                String deadline = input.substring(deadlineIndex);
-                String desc = input.substring(9, deadlineIndex - 5);
-
-
-                try{
-                    writeToFile("D | 1 | " + desc + " | " + deadline+ "\n");
-                } catch (IOException e){
-                    System.out.println("Something went wrong " + e.getMessage());
-                }
-
-
-
-
-                Deadline newDeadline = new Deadline(desc, deadline);
-
-                toDoList.add(newDeadline);
-
-                int numTask = toDoList.size();
-
-
-                System.out.println("Got it. I've added this task: \n" + "  "
-                        + newDeadline + "Now you have " +
-                        numTask + " tasks in the list.");
-            }
         } else if (input.contains("event")) {
 
-            if (input.length() < 6) {
-                throw new EmptyDescException("event");
-            } else {
+            addEventTask(input);
 
-                int timeIndex = input.indexOf('/') + 4;
-                String time = input.substring(timeIndex);
-
-                String desc = input.substring(6, timeIndex - 5);
-
-                try{
-                    writeToFile("E | 1 | " + desc + " | " + time+ "\n");
-                } catch (IOException e){
-                    System.out.println("Something went wrong " + e.getMessage());
-                }
-
-
-
-
-
-                Event newEvent = new Event(desc, time);
-
-                toDoList.add(newEvent);
-
-                int numTask = toDoList.size();
-
-
-                System.out.println("Got it. I've added this task: \n" + "  "
-                        + newEvent + "Now you have " +
-                        numTask + " tasks in the list.");
-            }
         } else if (input.contains("delete")) {
-            int taskNum = Integer.parseInt(input.substring(7)) - 1;
-            Task removedTask = toDoList.remove(taskNum);
-            System.out.println("Noted. I've removed this task:\n" + removedTask +
-                    "Now you have " + toDoList.size() + " tasks in the list.");
-
+            deleteTask(input);
         } else {
             throw new IllegalArgumentException();
         }

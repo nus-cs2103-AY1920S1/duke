317bceb797f90523af8befcf7152b476cd3e1769
Cheong Siu Hong <cheongsiuhong@gmail.com>
2019-08-28 17:32:28 +0800

    Added JUnit tests for the Ui.

---
+++ src/test/java/duke/UiTest.java
@@ -0,0 +1,63 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UiTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private static final String LINE_BREAK = "______________________________________________________________________";
+    private static final String HELLO_STRING = "Hello! I'm duke.Duke\nWhat can I do for you?";
+    private static final String BYE_STRING = "Bye. Hope to see you again soon!";
+    private static final String ERROR_PREPEND = "☹ OOPS!!!";
+
+    public String format(String sequence) {
+        String indented = sequence.replaceAll("(?m)^", "\t\t");
+        return String.format(
+                "\t%s\n%s\n\t%s\n",
+                LINE_BREAK,
+                indented,
+                LINE_BREAK
+        );
+    }
+
+    @Test
+    public void testSay() {
+        System.setOut(new PrintStream(outContent));
+        Ui ui = new Ui();
+        ui.say("yeet");
+        assertEquals(format("yeet"), outContent.toString());
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testGreeting() {
+        System.setOut(new PrintStream(outContent));
+        Ui ui = new Ui();
+        ui.greet();
+        assertEquals(format(HELLO_STRING), outContent.toString());
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testFarewell() {
+        System.setOut(new PrintStream(outContent));
+        Ui ui = new Ui();
+        ui.farewell();
+        assertEquals(format(BYE_STRING), outContent.toString());
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testSayError() {
+        System.setOut(new PrintStream(outContent));
+        Ui ui = new Ui();
+        ui.sayError("The answer is not 42.");
+        assertEquals(format(ERROR_PREPEND + " " + "The answer is not 42."), outContent.toString());
+        System.setOut(originalOut);
+    }
+}

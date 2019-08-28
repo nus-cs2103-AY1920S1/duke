b0bcb35b44ff8fa4fdd56ecedc55d24292404254
dalisc <42921826+dalisc@users.noreply.github.com>
2019-08-29 05:09:54 +0800

    reverted

---
+++ src/main/java/Parser.java
@@ -0,0 +1,40 @@
+public class Parser {
+    protected String command;
+    protected String input;
+    protected String[] inputArr;
+
+    protected String todoDescript;
+
+    protected String[] eventCommand;
+    protected String eventDescript;
+    protected String at;
+
+    protected String[] deadlineCommand;
+    protected String deadlineDescript;
+    protected String by;
+
+    public Parser(String input) {
+        input = input.trim();
+        String[] inputArr = input.split("\\s+");
+        String command = inputArr[0];
+        this.command = command;
+        this.input = input;
+        this.inputArr = inputArr;
+    }
+
+    public void parseEvent() {
+        this.eventCommand = this.input.split("/at");
+        this.eventDescript = this.eventCommand[0].substring(6);
+        this.at = this.eventCommand[1].substring(1);
+    }
+
+    public void parseDeadline() {
+        this.deadlineCommand = this.input.split("/by");
+        this.deadlineDescript = deadlineCommand[0].substring(9);
+        this.by = deadlineCommand[1].substring(1);
+    }
+
+    public void parseToDo() {
+        this.todoDescript = this.input.substring(5);
+    }
+}
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,33 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class TaskList<Task> {
+    ArrayList<Task> list = new ArrayList<>();
+    public TaskList(ArrayList<Task> list) {
+        this.list = list;
+    }
+
+    public TaskList() {
+    }
+
+    public ArrayList<Task> getList() {
+        return this.list;
+    }
+    public Task getTask(int i) {
+        return this.list.get(i);
+    }
+
+    public void save(String filePath) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
+        for (int i = 0; i < list.size(); i++) {
+            Task task = list.get(i);
+            if (i > 0) {
+                writer.newLine();
+            }
+            writer.write(task.toString());
+        }
+        writer.close();
+    }
+}

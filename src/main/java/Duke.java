723a8a540888d1df15f1e0455f466488d6a9bbb7
cherylnqj <cheryl.nqj@gmail.com>
2019-08-28 17:59:22 +0800

    Save tasks into task list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,17 @@
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.PrintWriter;
 
 public class Duke {
+    static String listString = "";
+    static ArrayList<Task> listArr = new ArrayList<Task>();
+    static String indent = "    ";
+
     public static void main(String[] args) {
-        String indent = "    ";
         Scanner sc = new Scanner(System.in);
-        ArrayList<Task> listArr = new ArrayList<Task>();
         int listSize = 0;
         int listPointer;
-        String listString;
 
         printIndentedString("Hello! I'm Duke\n" +
                 indent + " " + "What can I do for you?", indent);
@@ -17,25 +20,23 @@ public class Duke {
 
         while (!inputArr[0].equals("bye")) {
             if (inputArr[0].equals("list")) {
-                listString = "Here are the tasks in your list:\n" + indent + " ";
-                for (int i = 0; i < listArr.size(); i++) {
-                    listString += (i + 1) + ". " + listArr.get(i);
-                    if (i != (listArr.size() - 1)) {
-                        listString += '\n' + indent + ' ';
-                    }
-                }
-                printIndentedString(listString, indent);
+                printIndentedString("Here are the tasks in your list:\n" + listString, indent);
             } else if (inputArr[0].equals("done")) {
                 listPointer = Integer.parseInt(inputArr[1]);
                 listArr.get(listPointer - 1).markAsDone();
                 printIndentedString("Nice! I've marked this task as done: \n"
                         + indent + "   " + listArr.get(listPointer - 1), indent);
+                updateTodoString();
+                updateTodoFile(listString);
             } else if(inputArr[0].equals("delete")) {
                 listPointer = Integer.parseInt(inputArr[1]);
-                printIndentedString("Noted. I've removed this task: \n"
-                        + indent + "   " + listArr.get(listPointer - 1) + "\n" + indent +
-                        "Now you have " + listArr.size() + " tasks in the list.", indent);
+                Task deletedTask = listArr.get(listPointer - 1);
                 listArr.remove(listPointer - 1);
+                printIndentedString("Noted. I've removed this task: \n"
+                        + indent + "   " + deletedTask + "\n" + indent +
+                        "Now you have " + listArr.size() + " tasks in the list.", indent);
+                updateTodoString();
+                updateTodoFile(listString);
             } else if(inputArr[0].equals("todo") || inputArr[0].equals("deadline") || inputArr[0].equals("event")){
                 String befTaskAddMessage = "Got it. I've added this task: \n" + indent + "   ";
                 String aftTaskAddMessage = "Now you have " + (listSize + 1) + " tasks in the list.";
@@ -44,12 +45,16 @@ public class Duke {
                             listArr.add(new Task(inputArr[1], "todo"));
                             printIndentedString(befTaskAddMessage + listArr.get(listSize) + "\n " + indent + aftTaskAddMessage, indent);
                             listSize++;
+                            updateTodoString();
+                            updateTodoFile(listString);
                     } else if(inputArr[0].equals("deadline")) {
                         if(input.contains(" /by ")) {
                             String deadline = input.split(" /by ")[1];
                             listArr.add(new Task(inputArr[1].split(" /by ")[0], "deadline", deadline));
                             printIndentedString(befTaskAddMessage + listArr.get(listSize) + "\n " + indent + aftTaskAddMessage, indent);
                             listSize++;
+                            updateTodoString();
+                            updateTodoFile(listString);
                         } else {
                             printIndentedString("☹ OOPS!!! Deadlines require a specific datetime after /by.", indent);
                         }
@@ -59,6 +64,8 @@ public class Duke {
                             listArr.add(new Task(inputArr[1].split(" /at ")[0], "event", eventDateTime));
                             printIndentedString(befTaskAddMessage + listArr.get(listSize) + "\n " + indent + aftTaskAddMessage, indent);
                             listSize++;
+                            updateTodoString();
+                            updateTodoFile(listString);
                         } else {
                             printIndentedString("☹ OOPS!!! Events require a specific datetime after /at.", indent);
                         }
@@ -80,5 +87,25 @@ public class Duke {
         System.out.println(indent + " " + string);
         System.out.println(indent + "____________________________________________________________");
         System.out.println();
+    }
+
+    public static void updateTodoString() {
+        listString = "" + indent + " ";
+        for (int i = 0; i < listArr.size(); i++) {
+            listString += (i + 1) + ". " + listArr.get(i);
+            if (i != (listArr.size() - 1)) {
+                listString += '\n' + indent + ' ';
+            }
+        }
+    }
+
+    public static void updateTodoFile(String todoString) {
+        try {
+            PrintWriter writer = new PrintWriter("../../../data/duke.txt", "UTF-8");
+            writer.printf(todoString);
+            writer.close();
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
     }
 }

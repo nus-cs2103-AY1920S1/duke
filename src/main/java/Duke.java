b245ed8bf7393718868c13066a179a661499d51d
Neo Si Hao <neosihao@gmail.com>
2019-08-28 14:09:44 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String welcomeMessage = "    ____________________________________________________________\n"
+                + "     Hello! I'm Duke\n"
+                + "     What can I do for you?\n"
+                + "    ____________________________________________________________\n";
+        System.out.println(welcomeMessage);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println("    ____________________________________________________________\n"
+                    + "     " + input + "\n"
+                    + "    ____________________________________________________________\n");;
+            input = sc.nextLine();
+        }
+        System.out.println("    ____________________________________________________________\n"
+                + "     Bye. Hope to see you again soon!\n"
+                + "    ____________________________________________________________\n");
     }
 }

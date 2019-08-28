3b20438ec4c6a246305ec3fe4317966da4d06d95
Neo Si Hao <neosihao@gmail.com>
2019-08-29 02:04:38 +0800

    Level-3, A-Task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<Task> list = new ArrayList<Task>();
         String welcomeMessage = "    ____________________________________________________________\n"
                 + "     Hello! I'm Duke\n"
                 + "     What can I do for you?\n"
@@ -12,17 +12,24 @@ public class Duke {
         System.out.println(welcomeMessage);
         String input = sc.nextLine();
         while (!input.equals("bye")) {
-            if (!input.equals("list")) {
-                list.add(input);
+            if (input.equals("list")) {
                 System.out.println("    ____________________________________________________________\n"
-                        + "     added: " + input + "\n"
-                        + "    ____________________________________________________________\n");
-            } else {
-                System.out.println("    ____________________________________________________________");
+                        + "     Here are the tasks in your list:");
                 for (int i = 1; i <= list.size(); i++) {
-                    System.out.println("     " + i + ". " + list.get(i-1));
+                    System.out.println("     " + i + "." + list.get(i-1));
                 }
                 System.out.println("    ____________________________________________________________\n");
+            } else {
+                String[] inputStringArray = input.split("\\s+");
+                if (inputStringArray[0].equals("done")) {
+                    list.get(Integer.parseInt(inputStringArray[1]) - 1).markAsDone();
+                } else {
+                    Task task = new Task(input);
+                    list.add(task);
+                    System.out.println("    ____________________________________________________________\n"
+                            + "     added: " + input + "\n"
+                            + "    ____________________________________________________________\n");
+                }
             }
             input = sc.nextLine();
         }

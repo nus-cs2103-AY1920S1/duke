f99d923aaa88b1a6c4e473db32af005eaee78d16
Alvinnyk <alvinnyk95@gmail.com>
2019-08-28 16:44:08 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,19 +15,32 @@ public class Duke {
                 "     ____________________________________________________________\n");
 
         Scanner sc = new Scanner(System.in);
+        String data[] = new String[100];
+        int counter = 0;
 
         do{
-            String input = sc.next();
+            String input = sc.nextLine();
 
             if(input.equals("bye")){
                 System.out.println("     ____________________________________________________________\n" +
                         "     Bye. Hope to see you again soon!\n" +
                         "     ____________________________________________________________");
                 break;
+            } else if(input.equals("list")) {
+                int i;
+                System.out.println("     ____________________________________________________________\n");
+                for(i = 1; i <= counter; i++){
+                    System.out.println("     " + i + ". " + data[i - 1]);
+                }
+                System.out.println("     ____________________________________________________________");
+
             } else {
                 System.out.println("     ____________________________________________________________\n" +
-                        "     " + input + "\n" +
+                        "     added: " + input + "\n" +
                         "     ____________________________________________________________");
+
+                data[counter] = input;
+                counter ++;
             }
         } while(true);
     }

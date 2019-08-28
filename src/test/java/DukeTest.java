72be2b94d257a03a8fed22184b8db1f68374766f
arjavibahety <arjavibahety@gmail.com>
2019-08-28 15:29:06 +0800

    completed minimal goals for A-Junit

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DukeTest {
+    @Test
+    public void testTaskGetDescription() {
+        Task task = new Task("complete hw");
+        assertEquals("complete hw", task.getDescription());
+    }
+
+    @Test
+    public void  testTodoGetIsDone() {
+        Todo td = new Todo("assignments");
+        assertEquals(false, td.getIsDone());
+    }
+
+    @Test
+    public void testDeadlineGetBy() {
+        Deadline dd = new Deadline("splashdown poster", new Date(1000000000));
+        assertEquals(new Date(1000000000), dd.getBy());
+    }
+
 }

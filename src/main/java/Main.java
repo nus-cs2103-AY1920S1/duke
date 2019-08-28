42195f14a262a5f5733ff20b9987473f398ca517
ning-sy210 <ningshy97@gmail.com>
2019-08-28 22:13:43 +0800

    Slight changes to catch various exceptions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 public class Main {
@@ -11,10 +12,20 @@ public class Main {
         while (!instruction.equals("bye")) {
             try {
                 duke.evaluate(instruction);
+            } catch (DateTimeParseException e) {
+                System.out.println("     Unable to parse Date & Time of task!\n");
+                System.out.println("     Please follow the following format for Date & Time!");
+                System.out.println("       - Date: 'dd/mm/yy' e.g. 10/02/19 (10 Feb 2019)");
+                System.out.println("       - Time: 'hhmm' (24-hr format) e.g. 0730 (07:30AM)");
+
+                System.out.println(Duke.HORIZONTAL_LINE);        
+            } catch (IncorrectDukeCommand e) {
+                System.out.println("     Looks like a Duke Command has failed due to an incorrect input format!\n");
+                System.out.println(e.getMessage());
+                System.out.println(Duke.HORIZONTAL_LINE);        
             } catch (Exception e) {
                 System.out.println(e.getMessage());
                 System.out.println(Duke.HORIZONTAL_LINE);        
-
             } 
 
             instruction = sc.nextLine();

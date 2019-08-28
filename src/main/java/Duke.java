cb5eb10a2c70eb46375d3cefb3df610340fd30e5
niclausliu <niclauszimu@outlook.com>
2019-08-28 15:29:37 +0800

    Implement add and list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int index = 0;
 
         String greeting = "     Hello! I'm Duke\n" +
                 "     What can I do for you?";
@@ -14,7 +16,15 @@ public class Duke {
                 System.out.println("     Bye. Hope to see you again soon!");
                 break;
             }
-            System.out.println("     " + input);
+            if (input.equals("list")) {
+                for (int i = 0; i < index; i++) {
+                    System.out.printf("     %d: %s\n", i + 1, tasks[i]);
+                }
+            } else {
+                tasks[index] = input;
+                index++;
+                System.out.println("     added: " + input);
+            }
         }
     }
 }

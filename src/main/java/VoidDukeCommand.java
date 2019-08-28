84d844675a42033a321654d5c48e983cab1f678d
ning-sy210 <ningshy97@gmail.com>
2019-08-28 16:33:29 +0800

    Slight UI improvement

--- src/main/java/VoidDukeCommand.java
+++ src/main/java/VoidDukeCommand.java
@@ -4,6 +4,6 @@ public class VoidDukeCommand extends NoSuchElementException {
     private static final long serialVersionUID = 1L;
     
     public VoidDukeCommand() {
-        super("I can't do anything if you don't tell me what to do... \ud83d\ude2d");
+        super("     I can't do anything if you don't tell me what to do... \ud83d\ude2d");
     }
 }

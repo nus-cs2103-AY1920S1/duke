743a4363579554206569cc51ae6897186243aae1
czkay <chong.zi.kang@gmail.com>
2019-08-28 21:17:37 +0800

    Tweaked code to adhere to coding standard

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -3,16 +3,16 @@ import java.time.format.DateTimeFormatter;
 
 public class Deadline extends Task {
     private LocalDateTime by;
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
     public Deadline(String s, String by) {
         super(s);
-        this.by = LocalDateTime.parse(by, formatter);
+        this.by = LocalDateTime.parse(by, dateTimeFormatter);
     }
 
     @Override
     public String toString() {
         String mark = (isDone ? "✓" : "✗");
-        return String.format("[D][%s] %s (by: %s)", mark, taskDescription, by.format(formatter).toString());
+        return String.format("[D][%s] %s (by: %s)", mark, taskDescription, by.format(dateTimeFormatter).toString());
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -3,16 +3,16 @@ import java.time.format.DateTimeFormatter;
 
 public class Event extends Task {
     private LocalDateTime at;
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
     public Event(String s, String at) {
         super(s);
-        this.at = LocalDateTime.parse(at, formatter);
+        this.at = LocalDateTime.parse(at, dateTimeFormatter);
     }
 
     @Override
     public String toString() {
         String mark = (isDone ? "✓" : "✗");
-        return String.format("[E][%s] %s (at: %s)", mark, taskDescription, at.format(formatter).toString());
+        return String.format("[E][%s] %s (at: %s)", mark, taskDescription, at.format(dateTimeFormatter).toString());
     }
 }

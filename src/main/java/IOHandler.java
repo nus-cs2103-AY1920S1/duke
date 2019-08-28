63199e2767a73f85c4089ca5aaf823bcaf07b53d
ning-sy210 <ningshy97@gmail.com>
2019-08-28 13:37:02 +0800

    IOHandler can now overwrite existing save file

--- src/main/java/IOHandler.java
+++ src/main/java/IOHandler.java
@@ -1,16 +1,36 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class IOHandler {
+    private String saveFileName;
+
+    public IOHandler() {
+        saveFileName = "C:\\Users\\NINGS\\OneDrive\\Documents\\duke\\data\\duketasks.txt";
+    }
+
     void addToLocalSave(Task task) throws IOException {
         PrintWriter printWriter = null;
         Parser parser = new Parser();
 
-        String fileName = "C:\\Users\\NINGS\\OneDrive\\Documents\\duke\\data\\duketasks.txt";
-        FileWriter fileWriter = new FileWriter(fileName, true);
+        FileWriter fileWriter = new FileWriter(saveFileName, true);
         printWriter = new PrintWriter(fileWriter);
         printWriter.println(parser.writeTaskAsText(task));
+
+        printWriter.close();
+    }
+
+    void overwriteLocalSave(ArrayList<Task> listOfTasks) throws IOException {
+        PrintWriter printWriter = null;
+        Parser parser = new Parser();
+
+        FileWriter fileWriter = new FileWriter(saveFileName, false);
+        printWriter = new PrintWriter(fileWriter);
+        
+        for (Task task : listOfTasks) {
+            printWriter.println(parser.writeTaskAsText(task));
+        }
 
         printWriter.close();
     }

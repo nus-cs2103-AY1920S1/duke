05e0b8dd59a58d678419af6cc11e7168dc73e99e
AhmadHatziq <e0031550@u.nus.edu>
2019-08-28 14:59:51 +0800

    Moved the date parsing from the constructor to the parseBy method

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -8,7 +8,7 @@ public class Deadline extends Task {
     protected String by; // hour;
     // protected int day, month, year;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, String by) { //sets idDone to the default value, false
         // by = 2/12/2019 1800
         // deadline cs /by 21/12/2019 0800
         super(description);
@@ -16,63 +16,7 @@ public class Deadline extends Task {
 
         if (!by.contains("of")) { // If string by contains the word "of", by string is already formatted
                                  // For use when reading from saved file
-
-
-            taskType = possibleTaskTypes.DEADLINE;
-
-            String[] words = by.split("/");
-            String[] years = by.split(" ");
-
-            int day = Integer.parseInt(words[0]);
-            int month = Integer.parseInt(words[1]);
-            String hour = years[1];
-            int year = Integer.parseInt(words[2].split(" ")[0]);
-
-            String dayString;
-            if ((day == 1) || (day == 21) || (day == 31)) {
-                dayString = "st";
-            } else if ((day == 2) || (day == 22)) {
-                dayString = "nd";
-            } else if ((day == 3) || (day == 23)) {
-                dayString = "rd";
-            } else {
-                dayString = "th";
-            }
-
-            String[] possibleMonths = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November",
-                    "December"};
-
-
-            String monthString = possibleMonths[month - 1];
-
-            String hoursString = hour;
-            String amOrpm = "";
-
-            if (Integer.parseInt(hoursString.substring(0, 2)) < 12) {
-                amOrpm = "am";
-            } else if (Integer.parseInt(hoursString.substring(0, 2)) < 24) {
-                amOrpm = "pm";
-            }
-
-            String minuteString = "";
-
-            if (Integer.parseInt(hoursString.substring(2)) == 0) {
-                minuteString = ""
-                ;
-            } else {
-                minuteString = "." + hoursString.substring(2);
-            }
-
-            int hourString = -1;
-
-            if (Integer.parseInt(hoursString.substring(0, 2)) > 12) {
-                hourString = (Integer.parseInt(hoursString.substring(0, 2)) - 12);
-            } else {
-                hourString = Integer.parseInt(hoursString.substring(0, 2));
-            }
-
-
-            this.by = day + dayString + " of " + monthString + " " + year + ", " + hourString + minuteString + amOrpm;
+            this.by = parseBy(by);
         }
 
     }
@@ -85,65 +29,8 @@ public class Deadline extends Task {
 
         if (!by.contains("of")) { // If string by contains the word "of", by string is already formatted
             // For use when reading from saved file
-
-
-            taskType = possibleTaskTypes.DEADLINE;
-
-            String[] words = by.split("/");
-            String[] years = by.split(" ");
-
-            int day = Integer.parseInt(words[0]);
-            int month = Integer.parseInt(words[1]);
-            String hour = years[1];
-            int year = Integer.parseInt(words[2].split(" ")[0]);
-
-            String dayString;
-            if ((day == 1) || (day == 21) || (day == 31)) {
-                dayString = "st";
-            } else if ((day == 2) || (day == 22)) {
-                dayString = "nd";
-            } else if ((day == 3) || (day == 23)) {
-                dayString = "rd";
-            } else {
-                dayString = "th";
-            }
-
-            String[] possibleMonths = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November",
-                    "December"};
-
-
-            String monthString = possibleMonths[month - 1];
-
-            String hoursString = hour;
-            String amOrpm = "";
-
-            if (Integer.parseInt(hoursString.substring(0, 2)) < 12) {
-                amOrpm = "am";
-            } else if (Integer.parseInt(hoursString.substring(0, 2)) < 24) {
-                amOrpm = "pm";
-            }
-
-            String minuteString = "";
-
-            if (Integer.parseInt(hoursString.substring(2)) == 0) {
-                minuteString = ""
-                ;
-            } else {
-                minuteString = "." + hoursString.substring(2);
-            }
-
-            int hourString = -1;
-
-            if (Integer.parseInt(hoursString.substring(0, 2)) > 12) {
-                hourString = (Integer.parseInt(hoursString.substring(0, 2)) - 12);
-            } else {
-                hourString = Integer.parseInt(hoursString.substring(0, 2));
-            }
-
-
-            this.by = day + dayString + " of " + monthString + " " + year + ", " + hourString + minuteString + amOrpm;
+            this.by = parseBy(by);
         }
-
     }
 
     @Override
@@ -157,6 +44,58 @@ public class Deadline extends Task {
     }
 
     public String parseBy(String by){
-        return ("");
+        taskType = possibleTaskTypes.DEADLINE;
+
+        String[] words = by.split("/");
+        String[] years = by.split(" ");
+
+        int day = Integer.parseInt(words[0]);
+        int month = Integer.parseInt(words[1]);
+        String hour = years[1];
+        int year = Integer.parseInt(words[2].split(" ")[0]);
+
+        String dayString;
+        if ((day == 1) || (day == 21) || (day == 31)) {
+            dayString = "st";
+        } else if ((day == 2) || (day == 22)) {
+            dayString = "nd";
+        } else if ((day == 3) || (day == 23)) {
+            dayString = "rd";
+        } else {
+            dayString = "th";
+        }
+
+        String[] possibleMonths = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November",
+                "December"};
+
+
+        String monthString = possibleMonths[month - 1];
+
+        String hoursString = hour;
+        String amOrpm = "";
+
+        if (Integer.parseInt(hoursString.substring(0, 2)) < 12) {
+            amOrpm = "am";
+        } else if (Integer.parseInt(hoursString.substring(0, 2)) < 24) {
+            amOrpm = "pm";
+        }
+
+        String minuteString = "";
+
+        if (Integer.parseInt(hoursString.substring(2)) == 0) {
+            minuteString = "";
+        } else {
+            minuteString = "." + hoursString.substring(2);
+        }
+
+        int hourString = -1;
+
+        if (Integer.parseInt(hoursString.substring(0, 2)) > 12) {
+            hourString = (Integer.parseInt(hoursString.substring(0, 2)) - 12);
+        } else {
+            hourString = Integer.parseInt(hoursString.substring(0, 2));
+        }
+
+        return ( day + dayString + " of " + monthString + " " + year + ", " + hourString + minuteString + amOrpm );
     }
 }

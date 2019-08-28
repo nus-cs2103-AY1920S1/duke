117025932531bf89674ec1af6bfc44711c990447
ning-sy210 <ningshy97@gmail.com>
2019-08-28 22:14:23 +0800

    Added exception handling for Parser

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,10 +1,11 @@
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.time.LocalDateTime;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Parser {
-    String convertStringToTime(String stringDate, String taskType) {
+    String convertStringToTime(String stringDate, String taskType) throws DateTimeParseException {
         String convertedTime;
 
         switch (taskType) {
@@ -12,18 +13,29 @@ public class Parser {
             DateTimeFormatter deadlineInputFormatter = DateTimeFormatter.ofPattern("dd/MM/yy HHmm");
             LocalDateTime deadlineDateTime = LocalDateTime.parse(stringDate, deadlineInputFormatter);
 
-            DateTimeFormatter deadlineOutputFormatter = DateTimeFormatter.ofPattern("yyyy MMMM dd hh:mma");
+            DateTimeFormatter deadlineOutputFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy hh:mma");
             convertedTime = deadlineDateTime.format(deadlineOutputFormatter);
             break;
         case "event":
             String[] dateTime = stringDate.split(" ");
+
+            if (dateTime.length != 2) {
+                throw new IncorrectDukeCommand("     The Date or Time is missing!");
+            }
+
             String eventDate = dateTime[0];
             String eventTime = dateTime[1];
 
             DateTimeFormatter eventDateInputFormatter = DateTimeFormatter.ofPattern("dd/MM/yy");
-            DateTimeFormatter eventDateOutputFormatter = DateTimeFormatter.ofPattern("yyyy MMMM dd");
+            DateTimeFormatter eventDateOutputFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy");
             LocalDate eventDateLDT = LocalDate.parse(eventDate, eventDateInputFormatter);
             String dateOutput = eventDateLDT.format(eventDateOutputFormatter);
+
+            if (!eventTime.contains("-")) {
+                String errorMessage = "     You seem to be missing the Start/End time of your event!\n"
+                        + "     Please follow this format for an Event: 'dd/mm/yy hhmm-hhmm'";
+                throw new IncorrectDukeCommand(errorMessage);
+            }
 
             String[] timeStartEnd = eventTime.split("-");
 

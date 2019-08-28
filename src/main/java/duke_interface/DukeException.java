a685445c849887f40b14189d022918816fcab5cb
ngzhaoming <ngzhaoming@gmail.com>
2019-08-28 09:57:24 +0800

    Remove duplicated directory

--- src/main/java/duke_interface/DukeException.java
+++
@@ -1,7 +0,0 @@
-package duke_interface;
-
-public class DukeException extends Exception {
-    DukeException(String message) {
-        super(message);
-    }
-}
---
+++ src/test/java/ParserTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParserTest {
+    @org.junit.jupiter.api.Test
+    void checkCommand() {
+        assertEquals("2", "2");
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkTime() {
+    }
+}

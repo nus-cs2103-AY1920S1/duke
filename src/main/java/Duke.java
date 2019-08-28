78076f5f181a69cc340a233403d0324f6e098f21
niclausliu <niclauszimu@outlook.com>
2019-08-28 21:15:20 +0800

    Implement error handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,16 +3,10 @@ import java.util.Scanner;
 public class Duke {
     private static final Task[] tasks = new Task[100];
     private static int numOfTasks;
-    private static boolean end;
-
-    private static void handleBye() {
-        System.out.println("     Bye. Hope to see you again soon!");
-        end = true;
-    }
 
     private static void handleList() {
         if (numOfTasks == 0) {
-            System.out.println("There are no tasks for now!");
+            System.out.println("     There are no tasks for now!");
             return;
         }
         System.out.println("     Here are the tasks in your list:");
@@ -21,21 +15,29 @@ public class Duke {
         }
     }
 
-    private static void handleDone(String line) {
-        String[] input = line.split(" ");
-        Task task = tasks[Integer.parseInt(input[1]) - 1];
-        task.markAsDone();
-        System.out.println("     Nice! I've marked this task as done: ");
-        System.out.printf("       %s\n", task);
+    private static void handleDone(String line) throws DukeException {
+        try {
+            String[] input = line.split(" ");
+            Task task = tasks[Integer.parseInt(input[1]) - 1];
+            task.markAsDone();
+            System.out.println("     Nice! I've marked this task as done: ");
+            System.out.printf("       %s\n", task);
+        } catch (Exception e) {
+            throw new DukeException("Oops! Please enter a valid task number.");
+        }
     }
 
-    private static void handleToDo(String line) {
+    private static void handleToDo(String line) throws DukeException {
         String[] input = line.split(" ");
         String description = "";
         for (int i = 1; i < input.length; i++) {
             description = description.concat(" " + input[i]);
         }
-        Task task = new Todo(description.trim());
+        description = description.trim();
+        if (description.isBlank()) {
+            throw new DukeException("Oops! The task description cannot be empty.");
+        }
+        Task task = new Todo(description);
         tasks[numOfTasks] = task;
         numOfTasks++;
         System.out.println("     Got it. I've added this task: ");
@@ -43,7 +45,7 @@ public class Duke {
         System.out.printf("     Now you have %d tasks in the list.\n", numOfTasks);
     }
 
-    private static void handleDeadLine(String line) {
+    private static void handleDeadLine(String line) throws DukeException {
         String[] input = line.split(" ");
         String description = "";
         String by = "";
@@ -54,7 +56,15 @@ public class Duke {
             else if (isDescription) description = description.concat(" " + word);
             else by = by.concat(" " + word);
         }
-        Task task = new Deadline(description.trim(), by.trim());
+        description = description.trim();
+        by = by.trim();
+        if (description.isBlank()) {
+            throw new DukeException("Oops! The task description cannot be empty.");
+        }
+        if (by.isBlank()) {
+            throw new DukeException("Oops! The task deadline cannot be empty.");
+        }
+        Task task = new Deadline(description, by);
         tasks[numOfTasks] = task;
         numOfTasks++;
         System.out.println("     Got it. I've added this task: ");
@@ -62,7 +72,7 @@ public class Duke {
         System.out.printf("     Now you have %d tasks in the list.\n", numOfTasks);
     }
 
-    private static void handleEvent(String line) {
+    private static void handleEvent(String line) throws DukeException {
         String[] input = line.split(" ");
         String description = "";
         String at = "";
@@ -73,7 +83,15 @@ public class Duke {
             else if (isDescription) description = description.concat(" " + word);
             else at = at.concat(" " + word);
         }
-        Task task = new Event(description.trim(), at.trim());
+        description = description.trim();
+        at = at.trim();
+        if (description.isBlank()) {
+            throw new DukeException("Oops! The task description cannot be empty.");
+        }
+        if (at.isBlank()) {
+            throw new DukeException("Oops! The task dates/times cannot be empty.");
+        }
+        Task task = new Event(description, at);
         tasks[numOfTasks] = task;
         numOfTasks++;
         System.out.println("     Got it. I've added this task: ");
@@ -82,7 +100,7 @@ public class Duke {
     }
 
     private static void handleException() {
-        System.out.println("Oops! I'm sorry. I don't know what that means.");
+        System.out.println("     Oops! I'm sorry. I don't know what that means.");
     }
 
     public static void main(String[] args) {
@@ -92,30 +110,36 @@ public class Duke {
                 "     What can I do for you?";
         System.out.println(greeting);
 
-        while (!end) {
+        while (true) {
             String line = sc.nextLine();
+            if (line.equals("bye")) {
+                System.out.println("     Bye. Hope to see you again soon!");
+                break;
+            }
+            if (line.equals("list")) {
+                handleList();
+                continue;
+            }
             String[] input = line.split(" ");
-            switch (input[0]) {
-                case "bye":
-                    handleBye();
-                    break;
-                case "list":
-                    handleList();
-                    break;
-                case "done":
-                    handleDone(line);
-                    break;
-                case "todo":
-                    handleToDo(line);
-                    break;
-                case "deadline":
-                    handleDeadLine(line);
-                    break;
-                case "event":
-                    handleEvent(line);
-                    break;
-                default:
-                    handleException();
+            try {
+                switch (input[0]) {
+                    case "done":
+                        handleDone(line);
+                        break;
+                    case "todo":
+                        handleToDo(line);
+                        break;
+                    case "deadline":
+                        handleDeadLine(line);
+                        break;
+                    case "event":
+                        handleEvent(line);
+                        break;
+                    default:
+                        throw new DukeException("Oops! I don't understand your commmand.");
+                }
+            } catch (DukeException e) {
+                System.out.println("     " + e.getMessage());
             }
         }
     }

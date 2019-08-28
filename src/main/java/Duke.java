502c70de2ce49c2088dfac9a883883ad44348283
niclausliu <niclauszimu@outlook.com>
2019-08-28 16:04:50 +0800

    Implement ability to mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int index = 0;
 
         String greeting = "     Hello! I'm Duke\n" +
@@ -17,13 +17,23 @@ public class Duke {
                 break;
             }
             if (input.equals("list")) {
+                System.out.println("     Here are the tasks in your list:");
                 for (int i = 0; i < index; i++) {
-                    System.out.printf("     %d: %s\n", i + 1, tasks[i]);
+                    System.out.printf("     %d.[%s] %s\n", i + 1, tasks[i].getStatusIcon(), tasks[i].getDescription());
                 }
+            } else if (input.length() == 6
+                    && input.substring(0, 5).equals("done ")
+                    && Character.getNumericValue(input.charAt(5)) > 0
+                    && Character.getNumericValue(input.charAt(5)) <= index) {
+                Task task = tasks[Character.getNumericValue(input.charAt(5)) - 1];
+                task.markAsDone();
+                System.out.println("     Nice! I've marked this task as done: ");
+                System.out.printf("       [%s] %s\n", task.getStatusIcon(), task.getDescription());
             } else {
-                tasks[index] = input;
+                Task task = new Task(input);
+                tasks[index] = task;
                 index++;
-                System.out.println("     added: " + input);
+                System.out.printf("     added: [%s] %s\n", task.getStatusIcon(), task.getDescription());
             }
         }
     }

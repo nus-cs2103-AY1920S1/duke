82471cbbabd3cbc47f7c0c04372491b03bd46349
hellodommy <dominique@u.nus.edu>
2019-08-28 13:14:43 +0800

    Add A-JUnit (JUnit Testing)

---
+++ src/test/java/DateTimeTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DateTimeTest {
+
+    @Test
+    public void TestDateTime_validInput_success() {
+        assertEquals("31st of April 2019, 4:32pm", (DateTime.create("31/4/2019 1632")).toString());
+        assertEquals("22nd of September 2030, 12:00am", (DateTime.create("22/9/2030 0000")).toString());
+        assertEquals("3rd of March 1930, 8:20am", (DateTime.create("3/3/1930 0820")).toString());
+        assertEquals("16th of February 1966, 7:12pm", (DateTime.create("16/2/1966 1912")).toString());
+    }
+
+}
---
+++ src/test/java/SaveTaskTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SaveTaskTest {
+
+    @Test
+    public void saveTask_validTask_success() {
+        assertEquals("E | 0 | have project meeting | 12/3/2020 1430", (new Event("have project meeting", "12/3/2020 1430")).toSave());
+        assertEquals("T | 0 | do homework", (new Todo("do homework")).toSave());
+        assertEquals("D | 0 | complete level 6 | 20/8/2019 2359", (new Deadline("complete level 6", "20/8/2019 2359")).toSave());
+    }
+}

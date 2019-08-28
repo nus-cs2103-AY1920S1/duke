9c535c46a75be0315090437ed113b2ecb72f7060
charliechoong <e0202817@u.nus.edu>
2019-08-28 13:20:04 +0800

    Able to write to harddisk when a task is added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
-import java.nio.file.SimpleFileVisitor;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -13,36 +14,38 @@ public class Duke {
         printReply("Hello! I'm Duke\n\t What can I do for you?");
         String command = sc.nextLine();
         try {
-            FileWriter fw = new FileWriter(filePath);
+            FileWriter fw = new FileWriter(filePath, true);
             while (!command.equals("bye")) {
-                String[] parts = command.split(" ", 2);
-                if (command.equals("list")) {
-                    printList(taskList);
-                } else if (parts[0].equals("done")) {
-                    int taskNum = Integer.parseInt(parts[1]);
-                    printReply(taskList.get(taskNum - 1).markAsDone());
-                } else if (parts[0].equals("delete")) {
-                    int taskNum = Integer.valueOf(parts[1]);
-                    removeFromList(taskList, taskNum);
-                } else if (parts[0].equals("todo")) {
-                    addToList(new Todo(parts[1]), taskList, fw, filePath);
-                } else if (parts[0].equals("deadline")) {
-                    String[] subparts = parts[1].split(" /by ");
-                    addToList(new Deadline(subparts[0], subparts[1]), taskList, fw, filePath);
-                } else if (parts[0].equals("event")) {
-                    String[] subparts = parts[1].split(" /at ");
-                    addToList(new Event(subparts[0], subparts[1]), taskList, fw, filePath);
-                } else {
-                    throw new DukeException("");
+                try {
+                    String[] parts = command.split(" ", 2);
+                    if (command.equals("list")) {
+                        printList(taskList);
+                    } else if (parts[0].equals("done")) {
+                        int taskNum = Integer.parseInt(parts[1]);
+                        printReply(taskList.get(taskNum - 1).markAsDone());
+                    } else if (parts[0].equals("delete")) {
+                        int taskNum = Integer.parseInt(parts[1]);
+                        removeFromList(taskList, taskNum);
+                    } else if (parts[0].equals("todo")) {
+                        addToList(new Todo(parts[1]), taskList, fw, filePath);
+                    } else if (parts[0].equals("deadline")) {
+                        String[] subparts = parts[1].split(" /by ");
+                        addToList(new Deadline(subparts[0], subparts[1]), taskList, fw, filePath);
+                    } else if (parts[0].equals("event")) {
+                        String[] subparts = parts[1].split(" /at ");
+                        addToList(new Event(subparts[0], subparts[1]), taskList, fw, filePath);
+                    } else {
+                        throw new DukeException("");
+                    }
+                } catch (DukeException e) {
+                    printReply("OOPS!!! I'm sorry, but I don't know what that means :-(");
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    printReply("OOPS!!! The description of a " + command.split(" ", 2)[0] + " cannot be empty");
                 }
                 command = sc.nextLine();
             }
             fw.close();
-        } catch (DukeException e) {
-            printReply("OOPS!!! I'm sorry, but I don't know what that means :-(");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            printReply("OOPS!!! The description of a " + command.split(" ", 2)[0] + " cannot be empty");
-        } catch (IOException e) {
+        }catch (IOException e) {
             System.out.println("File not found.");
         }
         printReply("Bye. Hope to see you again soon!");

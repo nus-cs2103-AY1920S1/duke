9132da09b39bd04909c838a5d470a880fa9c77ed
limxu <limxuanhui_17@hotmail.com>
2019-08-28 11:29:53 +0800

    Level-2. Add, List: added a for loop to print elements in list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -11,13 +12,20 @@ public class Duke {
 
         System.out.println("Hello! I'm Duke" + "\n" + "What can I do for you?"); // greet user
         Scanner scanner = new Scanner(System.in); // reads user input
+        List<String> toDos = new ArrayList<>(); // instantiate array list of strings
         while (true) {
             String line = scanner.nextLine();
             if (line.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else {
-                System.out.println(line); // echos user input
+            } else if (line.equals("list")) {
+                for (int i = 1; i < toDos.size() + 1; i++) {
+                    System.out.println(i + ". " + toDos.get(i - 1));
+                }
+            }
+            else {
+                toDos.add(line); // add user input to array list
+                System.out.println("added: " + line); // echos user input
             }
         }
     }

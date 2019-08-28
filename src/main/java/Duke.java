ce1b5b0b706e855cc526c7dcf2e31562b809af77
ryantay232 <ryantay232@gmail.com>
2019-08-28 18:28:41 +0800

    Modified Duke for Level-8. Branched to branch-Level-8.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<Task> taskList = new ArrayList<Task>();
-        String lines = "    ____________________________________________________________\n";
+        ArrayList<Task> tasks = new ArrayList<Task>();
+        String line = "    ____________________________________________________________\n";
         String indent = "    ";
-        String logo = lines
+        String logo = line
                     + "     ____        _           \n"
                     + "    |  _ \\ _   _| | _____   \n"
                     + "    | | | | | | | |/ / _ \\  \n"
@@ -15,37 +15,37 @@ public class Duke {
                     + "    |____/ \\__,_|_|\\_\\___|\n"
                     + "     Hello! I'm Duke          \n"
                     + "     What can I do for you?   \n"
-                    + lines;
+                    + line;
         System.out.println(logo);
         String command = sc.nextLine();
         String[] comd = command.split("\\s");
         while (!comd[0].equals("bye")) {
             if (comd[0].isEmpty()) {}
             else if (comd[0].equals("list")) {
-                System.out.print(lines);
+                System.out.print(line);
                 System.out.println(indent + " Here are the tasks in your list:");
-                for (int i = 0; i < taskList.size(); i++) {
-                    System.out.println(indent + " " + (i + 1) + "." + taskList.get(i));
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(indent + " " + (i + 1) + "." + tasks.get(i));
                 }
-                System.out.println(lines);
+                System.out.println(line);
             } else if (comd[0].equals("done")) {
                 try {
                     if (comd.length <= 1) {
                         throw new DukeException(" OOPS!!! The task number cannot be empty.\n");
                     }
                     int n = Integer.parseInt(comd[1]) - 1;
-                    if (n < 0 || n > taskList.size() - 1) {
+                    if (n < 0 || n > tasks.size() - 1) {
                         throw new DukeException(" OOPS!!! The number inputted is not on the list.\n");
                     }
-                    taskList.get(n).markAsDone();
-                    System.out.print(lines);
+                    tasks.get(n).markAsDone();
+                    System.out.print(line);
                     System.out.println(indent + " Nice! I've marked this task as done: \n" +
-                            indent + "   " + taskList.get(n));
-                    System.out.println(lines);
+                            indent + "   " + tasks.get(n));
+                    System.out.println(line);
                 } catch (DukeException ex) {
-                    System.out.print(lines);
+                    System.out.print(line);
                     System.out.print(indent + ex.getMessage());
-                    System.out.println(lines);
+                    System.out.println(line);
                 }
             } else if (comd[0].equals("delete")) {
                 try {
@@ -53,19 +53,19 @@ public class Duke {
                         throw new DukeException(" OOPS!!! The task number cannot be empty.\n");
                     }
                     int n = Integer.parseInt(comd[1]) - 1;
-                    if (n < 0 || n > taskList.size() - 1) {
+                    if (n < 0 || n > tasks.size() - 1) {
                         throw new DukeException(" OOPS!!! The number inputted is not on the list.\n");
                     }
-                    System.out.print(lines);
+                    System.out.print(line);
                     System.out.println(indent + " Noted. I've removed this task: \n" +
-                            indent + "   " + taskList.get(n) + "\n" +
-                            indent + " Now you have " + (taskList.size() - 1) + " tasks in the list.");
-                    System.out.println(lines);
-                    taskList.remove(n);
+                            indent + "   " + tasks.get(n) + "\n" +
+                            indent + " Now you have " + (tasks.size() - 1) + " tasks in the list.");
+                    System.out.println(line);
+                    tasks.remove(n);
                 } catch (DukeException ex) {
-                    System.out.print(lines);
+                    System.out.print(line);
                     System.out.print(indent + ex.getMessage());
-                    System.out.println(lines);
+                    System.out.println(line);
                 }
             } else {
                 try {
@@ -76,49 +76,49 @@ public class Duke {
                         } else {
                             String event = command.substring(5);
                             task = new ToDo(event);
-                            taskList.add(task);
+                            tasks.add(task);
                         }
                     } else if (comd[0].equals("deadline")) {
                         if (comd.length <= 1) {
                             throw new DukeException(" OOPS!!! The description of a deadline cannot be empty.\n");
                         } else {
-                            if (!command.contains("/")) {
+                            if (!command.contains("/by")) {
                                 throw new DukeException(" OOPS!!! The time is missing a \"/\"\n");
                             }
-                            String event = command.substring(9, command.lastIndexOf('/'));
-                            String date = command.substring(command.lastIndexOf('/') + 4);
+                            String event = command.substring(9, command.indexOf('/') - 1);
+                            String date = command.substring(command.indexOf("/by") + 4);
                             task = new Deadline(event, date);
-                            taskList.add(task);
+                            tasks.add(task);
                         }
                     } else if (comd[0].equals("event")) {
                         if (comd.length <= 1) {
                             throw new DukeException(" OOPS!!! The description of a event cannot be empty.\n");
                         } else {
-                            if (!command.contains("/")) {
+                            if (!command.contains("/at")) {
                                 throw new DukeException(" OOPS!!! The time is missing a \"/\"\n");
                             }
-                            String event = command.substring(6, command.lastIndexOf('/'));
-                            String date = command.substring(command.lastIndexOf('/') + 4);
+                            String event = command.substring(6, command.indexOf('/') - 1);
+                            String date = command.substring(command.indexOf("/at") + 4);
                             task = new Event(event, date);
-                            taskList.add(task);
+                            tasks.add(task);
                         }
                     } else {
                         throw new DukeException(" OOPS!!! I'm sorry, but I don't know what that means :-(\n");
                     }
-                    System.out.print(lines);
+                    System.out.print(line);
                     System.out.println(indent + " Got it. I've added this task: \n" +
                             indent + "   " + task + "\n" +
-                            indent + " Now you have " + taskList.size() + " tasks in the list.");
-                    System.out.println(lines);
+                            indent + " Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.println(line);
                 } catch (DukeException ex) {
-                    System.out.print(lines);
+                    System.out.print(line);
                     System.out.print(indent + ex.getMessage());
-                    System.out.println(lines);
+                    System.out.println(line);
                 }
             }
             command = sc.nextLine();
             comd = command.split("\\s");
         }
-        System.out.println(lines + indent + " Bye. Hope to see you again soon!\n" + lines);
+        System.out.println(line + indent + " Bye. Hope to see you again soon!\n" + line);
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,27 @@
+import java.time.LocalDateTime;
+
 public class Event extends Task {
-    private String date;
+    private LocalDateTime dateTime;
 
     public Event(String description, String date) {
         super(description);
-        this.date = date;
+        String[] dateSplit = date.split("\\s");
+        String[] d = dateSplit[0].split("/");
+        int day = Integer.parseInt(d[0]);
+        int month = Integer.parseInt(d[1]);
+        int year = Integer.parseInt(d[2]);
+        int hour = Integer.parseInt(dateSplit[1].substring(0, 2));
+        int min = Integer.parseInt(dateSplit[1].substring(2));
+        dateTime = LocalDateTime.of(year, month, day, hour, min);
+    }
+
+    private String getDate() {
+        return dateTime.getDayOfMonth() + "/" + dateTime.getMonthValue() + "/" + dateTime.getYear() +
+                " " + String.format("%02d", dateTime.getHour()) + String.format("%02d", dateTime.getMinute());
     }
 
     @Override
     public String toString() {
-        return "[E][" + getStatusIcon() + "] " + description + "(at: " + date + ")";
+        return "[E][" + getStatusIcon() + "] " + description + "(at: " + getDate() + ")";
     }
 }

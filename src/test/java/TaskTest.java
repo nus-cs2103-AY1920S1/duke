0cee57e5abe8d058fdddc628dd8053517746c5b5
alxkohh <alxkohh@gmail.com>
2019-08-28 14:40:37 +0800

    Completed A-JUnit increment

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TaskTest {
+
+    @Test
+    public void testGetStatusIcon() {
+        Task t = new Task("read book");
+        String expected = "\u2718";
+        assertEquals(expected, t.getStatusIcon());
+    }
+
+    @Test
+    public void testGetDescription() {
+        Task t = new Task("cook dinner");
+        String expected = "cook dinner";
+        assertEquals(expected, t.getDescription());
+    }
+
+    @Test
+    public void testMarkAsDone() {
+        Task t = new Task("clean room");
+        t.markAsDone();
+        assertEquals(true, t.isDone);
+    }
 }
--- src/test/java/TodoTest.java
+++ src/test/java/TodoTest.java
@@ -1,2 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TodoTest {
+
+    @Test
+    public void testToString() {
+        Todo t = new Todo("eat lunch");
+        String expected = "[T][\u2718] eat lunch";
+        assertEquals(expected, t.toString());
+    }
+
+    @Test
+    public void testToTextFileString() {
+        Todo t = new Todo("exercise");
+        String expected = "T|0|exercise";
+        assertEquals(expected, t.toTextFileString());
+    }
+
 }

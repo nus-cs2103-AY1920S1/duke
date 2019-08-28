18ac4334a66071c28697f2ce8c01c52551f8e6c7
berttwm <berttwm@gmail.com>
2019-08-28 22:17:53 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,18 @@
 import java.util.Scanner;
 
+import java.util.ArrayList;
+
 public class Duke {
+    //variables
+    public static ArrayList<String> storedStrings = new ArrayList<>();
+
+    //main
     public static void main(String[] args) {
         Duke.greet();
         readInput();
     }
 
+    //implementation methods
     public static void readInput(){
         Scanner scanner = new Scanner(System.in);
         while(scanner.hasNext()){
@@ -13,8 +20,14 @@ public class Duke {
             if(input.equals("bye")){
                 Duke.exit();
                 break;
-            } else {
-                System.out.println(input);
+            } else if(input.equals("list")){
+                for(int i = 1; i <= Duke.storedStrings.size(); i++){
+                    System.out.print(i + ". ");
+                    System.out.println(Duke.storedStrings.get(i-1));
+                }
+            } else{
+                Duke.storedStrings.add(input);
+                System.out.println("added: " + input);
             }
         }
     }
@@ -30,5 +43,14 @@ public class Duke {
 
     public static void exit(){
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    /**
+     * Adds input to list
+     *
+     * @param input string you want to add to list
+     */
+    public static void addToList(String input){
+
     }
 }

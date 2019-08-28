ab851da8194df6209cb82477659af74a733a7456
Chen Su <chensu@comp.nus.edu.sg>
2019-08-28 20:28:30 +0800

    Add JUnit tests to test the behavior of the code.

---
+++ src/test/java/DukeTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DukeTest {
+
+    @Test
+    public void testGetCommandType() {
+        assertEquals("todo", new Parser("todo read book").getCommandType());
+    }
+
+    @Test
+    public void testStringConversion() {
+        assertEquals("[T][✗]read book", new Todo("read book", false).toString());
+    }
+
+}

0604a339bc38e69ee621f931ea1ca4b47e8ad81d
Jonathan Tjendana <jonathan.tjendana@gmail.com>
2019-08-28 20:59:44 +0800

    A-JUnit

---
+++ src/test/java/TodoTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoTest {
+    @Test
+    public void printTodo(){
+        Todo todo = new Todo("borrow book");
+        assertEquals("[T][✘] borrow book", todo.toString());
+    }
+
+    @Test
+    public void printTodoToFile(){
+        Todo todo = new Todo("borrow book");
+        assertEquals("T | 0 | borrow book", todo.toStringFile());
+    }
+}

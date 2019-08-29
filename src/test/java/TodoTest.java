6f9e8c795de64c72cd2ce3bc789a6d0f036ad3ed
hooncp <hooncheep@gmail.com>
2019-08-29 18:25:33 +0800

    add j-unit testing

---
+++ src/test/java/TodoTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoTest {
+    @Test
+    public void toStringTodoTest(){
+        assertEquals("[T][-] Help me", new Todo("Help me").toString());
+    }
+
+    @Test
+    public void toDataStringTodoTest(){
+        assertEquals("T | 0 | Help me", new Todo("Help me").toDataString());
+    }
+}

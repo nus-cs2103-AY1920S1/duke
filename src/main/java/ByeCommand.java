e4e4959823398f68becac59f42cd9c475909006a
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2019-08-29 22:18:58 +0800

    Add ByeCommand

---
+++ src/main/java/ByeCommand.java
@@ -0,0 +1,11 @@
+public class ByeCommand implements Command {
+    @Override
+    public boolean isExit() {
+        return true;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+        return;
+    }
+}

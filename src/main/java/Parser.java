659364f61851d2f6555eccd7c01eaf074739b28d
yeodonghan <yeodonghan@gmail.com>
2019-08-30 03:09:53 +0800

    Level-9

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -25,6 +25,8 @@ public class Parser {
                     fullCommand.split(" ")[0].equals("deadline") ||
                     fullCommand.split(" ")[0].equals("event")) {
             return new CommandAdd(fullCommand);
+        } else if (fullCommand.split(" ")[0].equals("find")) {
+            return new CommandFind(fullCommand.split(" ", 2)[1]);
         } else {
                 throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
         }

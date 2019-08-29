b0f939afb72af69e98dd7c6bd46476dc77154cdd
yeodonghan <yeodonghan@gmail.com>
2019-08-30 03:31:59 +0800

    Merged [Level-9] to the master branch.

--- src/main/java/CommandFind.java
+++ src/main/java/CommandFind.java
@@ -1,11 +1,27 @@
+/**
+ * Represents the find command to find specific tasks.
+ */
 public class CommandFind extends Command {
 
+    /**
+     * The keyword(s) of the task.
+     */
     private String string;
 
+    /**
+     * Constructs the find command.
+     * @param str keyword(s)
+     */
     public CommandFind(String str) {
         this.string = str;
     }
 
+    /**
+     * Executes the find command.
+     * @param tasks the task list
+     * @param ui the UI
+     * @param storage the storage writer
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         ui.printString("Here are the matching tasks in your list:");

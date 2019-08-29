2327b2af7a1d14d152a65d1b51642479d9eea923
nattanyz <natalietan8@gmail.com>
2019-08-29 20:48:25 +0800

    complete Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,38 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?"); // todo: replace with configurable message
+        read();
+    }
+
+    /**
+     * Reads input from the user and passes it to process().
+     */
+    public static void read() {
+        String inputString; // stores input from user
+        Scanner in = new Scanner(System.in);
+
+        // continuously take in user input until terminated
+        while(true) {
+            inputString = in.nextLine();
+            if (inputString.equals("bye")) {
+                break;
+            }
+            process(inputString);
+        }
+
+        // say goodbye upon exit
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    /**
+     * Creates the given input string.
+     * For Level-1, simply echoes the string.
+     *
+     * @param input input string given by user.
+     */
+    public static void process(String input) {
+        // print given input to console
+        System.out.println(input);
     }
 }

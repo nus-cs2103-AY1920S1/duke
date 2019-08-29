59732e9199999018d2dda6e55327facd0fbe522c
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2019-08-29 22:22:54 +0800

    Add DoneCommand

---
+++ src/main/java/DoneCommand.java
@@ -0,0 +1,21 @@
+public class DoneCommand implements Command {
+    private int index;
+
+    public DoneCommand(int index) {
+        this.index = index;
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
+        tasks.markAsDone(index);
+        storage.save(tasks);
+
+        ui.showMessage("Nice! I've marked this task as done:");
+        ui.showMessage("  " + tasks.get(index));
+    }
+}

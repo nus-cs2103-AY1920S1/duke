d0d5586dd97427d073b2a088bcc310531194abae
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2019-08-29 22:12:25 +0800

    Add AddCommand.java

---
+++ src/main/java/AddCommand.java
@@ -0,0 +1,27 @@
+public class AddCommand implements Command {
+    private Task task;
+
+    public AddCommand(Task task) {
+        this.task = task;
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
+        tasks.addTask(task);
+        storage.save(tasks);
+
+        ui.showMessage("Got it. I've added this task:");
+        ui.showMessage("  " + task);
+        printTotalTask(tasks, ui);
+    }
+
+    private void printTotalTask(TaskList tasks, Ui ui) {
+        boolean isPlural = tasks.size() > 1;
+        ui.showMessage("Now you have " + tasks.size() + " task" + (isPlural ? "s" : "") + " in the list.");
+    }
+}

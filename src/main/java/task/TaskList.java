8f9be114f7aaa104398a00cdf98d60d5e546e0bc
ning-sy210 <ningshy97@gmail.com>
2019-08-30 03:16:36 +0800

    TaskList now has a temporary search list so that users can mark task as done or delete directly from the search results.

--- src/main/java/task/TaskList.java
+++ src/main/java/task/TaskList.java
@@ -11,15 +11,18 @@ import java.util.ArrayList;
 public class TaskList {
     private ArrayList<Task> tasks;
     private Storage storage;
+    public ArrayList<Task> temporarySearchList;
 
     public TaskList(Storage storage) {
         tasks = new ArrayList<>();
         this.storage = storage;
+        temporarySearchList = null;
     }
 
     public TaskList(ArrayList<Task> tasks, Storage storage) {
         this.tasks = tasks;
         this.storage = storage;
+        temporarySearchList = null;
     }
 
     public boolean hasTask() {
@@ -30,29 +33,59 @@ public class TaskList {
         return tasks.size();
     }
 
-    public void markTaskAsDone(int taskNumber) throws DukeException, IOException {
-        if (tasks.get(taskNumber).isDone) {
-            String errorMessage = Ui.spaces(5) + "task.Task has already been marked done!";
+    public void markTaskAsDone(int taskNumber, boolean useTempSearchList) throws DukeException, IOException {
+        ArrayList<Task> taskListToUse;
+        String errorMessage = null;
+
+        if (useTempSearchList) {
+            taskListToUse = temporarySearchList;
+        } else {
+            taskListToUse = tasks;
+        }
+
+        if (taskNumber <= 0) {
+            errorMessage = "Number cannot be negative!";
+        } else if (taskListToUse.size() == 0) {
+            errorMessage = "You don't have any tasks yet!";
+        } else if (taskNumber > taskListToUse.size()) {
+            errorMessage = "You don't have that many tasks!";
+        }
+        
+        if (taskListToUse.get(taskNumber - 1).isDone) {
+            errorMessage = Ui.spaces(5) + "task.Task has already been marked done!";
+        }
+
+        if (errorMessage != null) {
             throw new DukeException(errorMessage);
         }
         
+        taskListToUse.get(taskNumber - 1).isDone = true;
         storage.overwriteLocalSave(tasks);
 
-        tasks.get(taskNumber).isDone = true;
         System.out.println(Ui.spaces(5) + "Nice! I've marked this task as done:\n"
-                + Ui.spaces(5) + tasks.get(taskNumber));
+                + Ui.spaces(7) + taskListToUse.get(taskNumber - 1));
     }
 
-    public void listAllTasks() throws DukeException {
+    public void listAllTasks(String type) throws DukeException {
         if (tasks.size() == 0) {
             String errorMessage = Ui.spaces(5) + "There are no task(s) to list!";
             throw new DukeException(errorMessage);
         }
 
-        System.out.println(Ui.spaces(5) + "Here are the tasks in your list:");
+        ArrayList<Task> taskListToUse;
 
-        for (int i = 0; i < tasks.size(); i++) {
-            System.out.printf("%s%d. %s\n", Ui.spaces(7), i + 1, tasks.get(i));
+        if (type.equals("list")) {
+            taskListToUse = tasks;
+            System.out.println(Ui.spaces(5) + "Here are the tasks in your list:");
+        } else if (type.equals("find")) {
+            taskListToUse = temporarySearchList;
+            System.out.println(Ui.spaces(5) + "Here are the matching tasks in your list:");
+        } else {
+            return;
+        }
+
+        for (int i = 0; i < taskListToUse.size(); i++) {
+            System.out.printf("%s%d. %s\n", Ui.spaces(7), i + 1, taskListToUse.get(i));
         }
     }
 
@@ -83,14 +116,21 @@ public class TaskList {
         System.out.printf("%sNow you have %d task(s) in your list.\n", Ui.spaces(5), tasks.size());
     }
 
-    public void delete(int taskNumber) throws IOException, IncorrectDukeCommand {
+    public void delete(int taskNumber, boolean useTempSearchList) throws IOException, IncorrectDukeCommand {
         String errorMessage;
+        ArrayList<Task> taskListToUse;
+
+        if (useTempSearchList) {
+            taskListToUse = temporarySearchList;
+        } else {
+            taskListToUse = tasks;
+        }
 
         if (taskNumber <= 0) {
             errorMessage = "Number cannot be negative!";
-        } else if (tasks.size() == 0) {
+        } else if (taskListToUse.size() == 0) {
             errorMessage = "You don't have any tasks yet!";
-        } else if (taskNumber > tasks.size()) {
+        } else if (taskNumber > taskListToUse.size()) {
             errorMessage = "You don't have that many tasks!";
         } else {
             errorMessage = null;
@@ -100,12 +140,33 @@ public class TaskList {
             throw new IncorrectDukeCommand(errorMessage);
         }
         
-        Task removedTask = tasks.remove(taskNumber - 1);
+        Task removedTask = taskListToUse.remove(taskNumber - 1);
         
         storage.overwriteLocalSave(tasks);
 
         System.out.println(Ui.spaces(5) + "Noted. I've removed this task:\n"
                 + Ui.spaces(7) + removedTask + "\n\n"
                 + Ui.spaces(5) + String.format("Now you have %d task(s) in your list.", tasks.size()));
+    }
+
+    public void find(String keyword) throws DukeException {
+        if (tasks.isEmpty()) {
+            throw new DukeException("There is nothing to search from!");
+        }
+        
+        ArrayList<Task> tempList = new ArrayList<>();
+
+        for (Task t : tasks) {
+            if (t.toString().contains(keyword)) {
+                tempList.add(t);
+            }
+        }
+
+        if (tempList.isEmpty()) {
+            throw new DukeException("There isn't a task that can be found with that keyword!");
+        } else {
+            temporarySearchList = tempList;
+            listAllTasks("find");
+        }
     }
 }

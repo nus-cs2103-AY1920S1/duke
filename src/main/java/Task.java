967c94f4a9081d249c07e2207309d9a54ed73da1
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2019-08-29 16:17:13 +0800

    Add parse method at Task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,6 @@
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public abstract class Task {
     protected static final SimpleDateFormat parser = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
@@ -17,6 +19,14 @@ public abstract class Task {
 
     public void markAsDone() {
         isDone = true;
+    }
+
+    protected Date parseDate(String date) throws DukeException {
+        try {
+            return parser.parse(date);
+        } catch(ParseException ex) {
+            throw new DukeException("There is an error in parsing date.");
+        }
     }
 
     @Override

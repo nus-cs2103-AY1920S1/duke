a6f1dfe7e826f7b9b0f2910d6440e23c94d18c38
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2019-08-30 01:15:40 +0800

    Adds javadoc to Ui class

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -7,36 +7,56 @@ public class Ui {
         scanner = new Scanner(System.in);
     }
 
+    /**
+     * Greets user.
+     */
     public void showWelcome() {
         showLine();
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         showLine();
     }
 
-    public void showGoodbye() {
-        showLine();
-        System.out.println("Bye. Hope to see you again soon!");
-        showLine();
-    }
-
+    /**
+     * Prints separator line.
+     */
     public void showLine() {
         System.out.println("____________________________________________________________");
     }
 
+    /**
+     * Prints the error message when loading data from hard disk.
+     * 
+     * @param ex The exception.
+     */
     public void showLoadingError(DukeException ex) {
         showLine();
         showErrorMessage(ex);
         showLine();
     }
 
+    /**
+     * Prints error message.
+     * 
+     * @param ex The exception.
+     */
     public void showErrorMessage(DukeException ex) {
         System.out.println(ex);
     }
 
+    /**
+     * Prints normal message.
+     * 
+     * @param message The message.
+     */
     public void showMessage(String message) {
         System.out.println(message);
     }
 
+    /**
+     * Reads user command.
+     * 
+     * @return user command.
+     */
     public String readCommand() {
         return scanner.nextLine();
     }

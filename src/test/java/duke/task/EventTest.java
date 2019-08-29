ca9f1c47980f5c135848ffefbf52ebf4554c919b
kelvinnharris <kelvinnharris@gmail.com>
2019-08-29 15:10:43 +0800

    Add JavaDoc to test classes

--- src/test/java/duke/task/EventTest.java
+++ src/test/java/duke/task/EventTest.java
@@ -6,7 +6,16 @@ import java.text.ParseException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * Unit test for event class.
+ */
 public class EventTest {
+
+    /**
+     * Create an event object with correct input and format.
+     *
+     * @result An event object is created successfully.
+     */
     @Test
     public void eventCreation_normalInput_outputMatches() {
         Task task = null;
@@ -18,6 +27,11 @@ public class EventTest {
         assertEquals(task.toString(), "[E][\u2718] party (at: 29/08/2019 2200)");
     }
 
+    /**
+     * Create an event object with wrong date format.
+     *
+     * @result An exception will be thrown.
+     */
     @Test
     public void checkDateFormat_wrongDateFormat_exceptionThrown() {
         Task task = null;
@@ -28,6 +42,11 @@ public class EventTest {
         }
     }
 
+    /**
+     * Check the event object file string format.
+     *
+     * @result The format matches correctly.
+     */
     @Test
     public void checkFileStringFormat_correctFormat_success() {
         Task task = null;

b414abe7c24dfb819b46efd54d968ea759873e28
elsakoh <estherngo@esthers-mbp-2.lan>
2019-08-29 09:13:29 +0800

    error handling for all 3 tasks as well as bad commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -41,41 +41,58 @@ public class Duke {
             String line = sc.nextLine().toLowerCase();
             String[] splited = line.split(" ");
             String check = splited[0];
-
+            //bye
             if (check.equals("bye")) {
-                System.out.println(
-                    addDoubleLine(
-                    "     Bye. Hope to see you again soon!"
-                    )
-                );
+                System.out.println(addDoubleLine("     Bye. Hope to see you again soon!"));
                 System.exit(0);
+            //list
             }else if(check.equals("list")){
                 handleList(list);
+            //done
             }else if(check.equals("done")){
                 String taskNum = splited[1];
                 Task current = list.get(Integer.parseInt(taskNum) - 1);
                 current.markAsDone();
                 System.out.println(addDoubleLine("     Nice! I've marked this task as done:\n" + "    " + current.toString()));
+            //make a todo task
             }else if(check.equals("todo")){
-                list.add(new Todo(line.replace("todo ", "")));
-                Task current = list.get(list.size()-1);
-                printMsg(current, list.size());
+                String description = line.replace("todo", "").trim();
+                //error handling
+                if(description.equals("")){
+                    System.out.println(addDoubleLine("    ☹ OOPS!!! The description of a todo cannot be empty."));
+                
+                }else{ //successful addition 
+                    list.add(new Todo(description));
+                    Task current = list.get(list.size()-1);
+                    printMsg(current, list.size());
+                }
+            //make an event task
             }else if(check.equals("event")){
                 //removes the command and splits it into 2
-                String [] splitDate = line.replace("event ", "").split("/at");
-                list.add(new Event(splitDate[0], splitDate[1]));
-                Task current = list.get(list.size()-1);
-                printMsg(current, list.size());
+                String [] splitDate = line.replace("event", "").split("/at");
+                if(splitDate.length < 2){
+                    System.out.println(addDoubleLine("    ☹ OOPS!!! Events require both a description and a date \n    (e.g. event go to concert /at 13 Feb)"));
+                }else{
+                    //if it reaches here it is successful
+                    list.add(new Event(splitDate[0].trim(), splitDate[1].trim()));
+                    Task current = list.get(list.size()-1);
+                    printMsg(current, list.size());
+                
+                }
+            //make deadline task
             }else if(check.equals("deadline")){
-                String [] splitDate = line.replace("deadline ", "").split("/by");
-                list.add(new Deadline(splitDate[0], splitDate[1]));
-                Task current = list.get(list.size()-1);
-                printMsg(current, list.size());
+                String [] splitDate = line.replace("deadline", "").split("/by");
+                if(splitDate.length < 2){
+                    System.out.println(addDoubleLine("    ☹ OOPS!!! Deadlines require both a description and a date by \n    (e.g. deadline homework3 /by tomorrow)"));
+                }else{
+                    list.add(new Deadline(splitDate[0].trim(), splitDate[1].trim()));
+                    Task current = list.get(list.size()-1);
+                    printMsg(current, list.size());
+                }
+            //error handling
+            }else{
+                System.out.println(addDoubleLine("     ☹ OOPS!!! I'm sorry, but I don't know what that means :-("));
             }
-            // }else{
-            //     list.add(new Task(line, Type.E));
-            //     System.out.println(addDoubleLine("     added: " + line));
-            // }
         }
     }
     public static void main(String[] args) {

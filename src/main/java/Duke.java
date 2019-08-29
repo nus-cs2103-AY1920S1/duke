1383ae6216601e0f3a31d14e557a1c92a0d9ac84
Xelyion <xelyionzero@gmail.com>
2019-08-29 10:02:55 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,32 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String blankLines = "____________________________________________________________";
+        System.out.println("    Hello from\n" + logo);
+        System.out.println(blankLines);
+        System.out.println("    Hello, I'm Duke!\n    What can I do for you?");
+        System.out.println(blankLines);
+
+        Scanner input = new Scanner(System.in);
+        String command = input.next();
+        while(!command.equals("bye")){
+            switch(command){
+                case "list":
+                    System.out.println("    list");
+                    command = input.next();
+                    break;
+
+                case "blah":
+                    System.out.println("    blah");
+                    command = input.next();
+                    break;
+
+                default:
+                    System.out.println("    Invalid Command");
+                    command = input.next();
+                    break;
+            }
+        }
+        System.out.println("    Bye. Hope to see you again!");
     }
 }

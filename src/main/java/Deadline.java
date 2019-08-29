6174bea972806b9d16842c00380f21f4a9e6ea3c
hoholyin <holyin97@gmail.com>
2019-08-29 09:14:37 +0800

    A-Jar

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,27 +1,27 @@
 
 public class Deadline extends Task {
-  private String date;
+    private String date;
 
-  public Deadline(String n, int index, String date) {
-    super(n, index);
-    this.date = date;
-  }
+    public Deadline(String n, int index, String date) {
+        super(n, index);
+        this.date = date;
+    }
 
-  public Deadline(String n, int index, String date, boolean completed) {
-    super(n, index, completed);
-    this.date = date;
-  }
+    public Deadline(String n, int index, String date, boolean completed) {
+        super(n, index, completed);
+        this.date = date;
+    }
 
-  public String getDate() {
-    return date;
-  }
+    public String getDate() {
+        return date;
+    }
 
-  @Override
-  public String toString() {
-    String result = "[D][";
-    result = this.completed ? result + "\u2713" + "]" : result + "\u2718" + "]";
-    result += " " + this.name;
-    result += " (by: " + this.date + ")";
-    return result;
-  }
+    @Override
+    public String toString() {
+        String result = "[D][";
+        result = this.completed ? result + "\u2713" + "]" : result + "\u2718" + "]";
+        result += " " + this.name;
+        result += " (by: " + this.date + ")";
+        return result;
+    }
 }
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,7 +9,7 @@ public class Duke {
     private Ui ui;
 
     public static void main(String[] args) {
-       new Duke("./saved/taskList_history.txt").run();
+        new Duke("./saved/taskList_history.txt").run();
     }
 
     public Duke(String path) {
@@ -30,32 +30,33 @@ public class Duke {
     private void run() {
         ui.welcomeMessage();
         Scanner sc = new Scanner(System.in);
-            while (true) {
-                try {
-                    String input = sc.nextLine();
-                    String[] inputParts = input.split(" ", 2);
-                    String command = inputParts[0];
-                    if (command.equals("bye")) {
-                        storage.saveHistory(tasks.getTaskList());
-                        ui.byeMessage();
-                        break;
-                    } else if (command.equals("list")) {
-                        ui.drawLine();
-                        tasks.displayList();
-                        ui.drawLineNewLine();
-                    } else if (command.equals("done")) {
-                        tasks.markItemComplete(Integer.parseInt(inputParts[1]), ui);
-                    } else if (command.equals("delete")) {
-                        tasks.deleteItem(Integer.parseInt(inputParts[1]), ui);
-                    } else {
-                        tasks.registerNewTask(inputParts, ui);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (DukeException e) {
-                    ui.printErrorMessage(e.getMessage());
+        boolean shouldExit = false;
+        while (!shouldExit) {
+            try {
+                String input = sc.nextLine();
+                String[] inputParts = input.split(" ", 2);
+                String command = inputParts[0];
+                if (command.equals("bye")) {
+                    storage.saveHistory(tasks.getTaskList());
+                    ui.byeMessage();
+                    shouldExit = true;
+                } else if (command.equals("list")) {
+                    ui.drawLine();
+                    tasks.displayList();
+                    ui.drawLineNewLine();
+                } else if (command.equals("done")) {
+                    tasks.markItemComplete(Integer.parseInt(inputParts[1]), ui);
+                } else if (command.equals("delete")) {
+                    tasks.deleteItem(Integer.parseInt(inputParts[1]), ui);
+                } else {
+                    tasks.registerNewTask(inputParts, ui);
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (DukeException e) {
+                ui.printErrorMessage(e.getMessage());
             }
+        }
         sc.close();
     }
 

77e298a40663b7518a5b4af798a21d34aad09681
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2019-08-29 22:16:27 +0800

    Add DeleteCommand

---
+++ src/main/java/DeleteCommand.java
@@ -0,0 +1,27 @@
+public class DeleteCommand implements Command {
+    private int index;
+
+    public DeleteCommand(int index) {
+        this.index = index;
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
+        Task task = tasks.deleteTask(index);
+        storage.save(tasks);
+    
+        ui.showMessage("Noted. I've removed this task:");
+        ui.showMessage("  " + task);
+        printTotalTask(tasks, ui);
+    }
+
+    private void printTotalTask(TaskList tasks, Ui ui) {
+        boolean isPlural = tasks.size() > 1;
+        ui.showMessage("Now you have " + tasks.size() + " task" + (isPlural ? "s" : "") + " in the list.");
+    }
+}

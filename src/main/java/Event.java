8236320da3959037c5856be3982e1fe0b34fd707
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2019-08-29 13:59:27 +0800

    Adds toStringForHardDisk method

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -7,6 +7,17 @@ public class Event extends Task {
     }
 
     @Override
+    public String toStringForHardDisk() {
+        String[] datas = new String[4];
+        datas[0] = "D";
+        datas[1] = isDone ? "1" : "0";
+        datas[2] = description;
+        datas[3] = at;
+
+        return String.join(" | ", datas);
+    }
+
+    @Override
     public String toString() {
         return "[E]" + super.toString() + " (at: " + at + ")";
     }

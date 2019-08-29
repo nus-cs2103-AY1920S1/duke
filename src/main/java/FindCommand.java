9870585f5f85b7a5e7166eadd59581d3f9eb37f2
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2019-08-30 00:10:59 +0800

    Added FindCommand Class

---
+++ src/main/java/FindCommand.java
@@ -0,0 +1,23 @@
+public class FindCommand implements Command {
+    private String keyword;
+    public FindCommand(String keyword) {
+        this.keyword = keyword;
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
+        ui.showMessage("Here are the matching tasks in your list:");
+        int found = 0;
+        for (int i = 1; i <= tasks.size(); ++i) {
+            if (tasks.get(i).getDescription().contains(keyword)) {
+                found++;
+                ui.showMessage(found + "." + tasks.get(i));
+            }
+        }
+    }
+}

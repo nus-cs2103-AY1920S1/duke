6513b3861cb7be600c99c4ae1195d7e20000a6e3
ArgVampir <kris_.k@hotmail.com>
2019-08-30 13:05:57 +0800

    added Java Doc

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,9 +1,17 @@
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 class Parser {
-    public void formatTime(String time) throws ParseException {
+
+    /**
+     * Handles the conversion of String to Time
+     * @param  time in String
+     * @return Date
+     */
+
+    public Date formatTime(String time) throws ParseException {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HHmm");
-        sdf.parse(time);
+        return sdf.parse(time);
     }
 }

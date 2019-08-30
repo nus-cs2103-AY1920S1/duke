5f877a52f5158b96f3c20bc16a22b33f7a80b1dd
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2019-08-30 21:18:53 +0800

    Fix basic app elements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,13 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 import duke.command.Command;
 import duke.exception.DukeException;
@@ -15,6 +21,31 @@ import duke.ui.Ui;
  * Main Class of the Application, which requires the workings of all packages.
  */
 public class Duke extends Application {
+
+    /**
+     *  Represents the scroll pane for the application.
+     */
+    private ScrollPane scrollPane;
+
+    /**
+     *  Represents the text box for the applications.
+     */
+    private VBox dialogContainer;
+
+    /**
+     * Represents the text field for the application.
+     */
+    private TextField userInput;
+
+    /**
+     * Represents the send button for the application.
+     */
+    private Button sendButton;
+
+    /**
+     * Represents the rendered scene for the application.
+     */
+    private Scene scene;
 
     /**
      * Represents the reader and the writer for the output text file.
@@ -90,8 +121,46 @@ public class Duke extends Application {
 
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Duke: To-Do");
-        Scene scene = new Scene(helloWorld);
+
+        // Set Stage
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        // Set Features
+        stage.setTitle("Duke: Abstraction");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        // You will need to import `javafx.scene.layout.Region` for this.
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
 
         stage.setScene(scene);
         stage.show();

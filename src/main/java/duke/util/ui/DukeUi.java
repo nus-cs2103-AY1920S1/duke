4f0bb8ead90b78f0901ddea18ccc55bbc7741784
Jia Hao <pohjiahao@hotmail.com>
2019-08-30 10:22:37 +0800

    A-JavaFx Part-2 Complete

--- src/main/java/duke/util/ui/DukeUi.java
+++ src/main/java/duke/util/ui/DukeUi.java
@@ -2,17 +2,77 @@ package duke.util.ui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class DukeUi extends Application {
 
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+        //Step 1. Setting up required components
 
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
+        /* The container for the content of the chat to scroll.
+            |-----ScrollPane-----|
+            ||-------VBox-------||
+            ||                  ||
+            ||------------------||
+            |--------------------|
+         */
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        //Input textfield
+        userInput = new TextField();
+
+        //Send button
+        sendButton = new Button("Send");
+
+        //Add all elements into the main window
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+        stage.setScene(scene);
+        stage.show();
+
+        //Step 2. Formatting the window to look as expected
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        //Dialog area size and responsiveness
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        //Input textfield
+        userInput.setPrefWidth(325.0);
+
+        //Send button
+        sendButton.setPrefWidth(55.0);
+
+        //Main window
+        mainLayout.setPrefSize(400.0, 600.0);
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
     }
 }

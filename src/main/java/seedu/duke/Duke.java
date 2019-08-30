7745f7946fe7049924f771f598b53b9ad4eaa74b
AhmadHatziq <e0031550@u.nus.edu>
2019-08-31 00:15:39 +0800

    Implemented JavaDoc for Duke.java and Parser.java classes.

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -13,27 +13,46 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * Project Duke is a Personal Assistant Chatbot
+ * that helps a person to keep track of various things.
+ */
 public class Duke {
 
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
 
+    /**
+     * Instantiates a Duke object.
+     *
+     * @param filePath absolute filepath of the where the text file is stored. Eg "C:\\Users\\hatzi\\Documents\\Sourcetree\\duke\\data\\tasks.txt".
+     */
     public Duke(String filePath){
         ui = new Ui();
         storage = new Storage(filePath);
     }
 
+    /**
+     * Executes the Duke Command Line Interface
+     */
     public void run(){
-        // Variable initialization
+        // Variable initialization. description will hold the description of all tasks.
+        // extraDescription will hold either the
+        // dateTime attribute of Deadline class or location attribute of Event class
         String output = ""; String taskType = ""; String description = ""; String extraDescription = ""; int taskNum = -1;
 
         ui.showWelcome();
 
         try {
+
+            // Loads the data from txt file to the TaskList object, tasks
             tasks = new TaskList(this.storage.load());
+
         } catch (FileNotFoundException e){
+
             System.out.println(e.getMessage());
+
         }
 
         // Creates scanner object to handle input
@@ -41,28 +60,33 @@ public class Duke {
         String fullCommand = in.nextLine().trim();
 
         while ( true ){
-            taskType = Parser.parseCommand(fullCommand); // taskType contains the first word of the command input string
+
+            taskType = Parser.parseCommand(fullCommand);
 
             try {
-                // LIST case
+
                 if (taskType.equals(possibleTasks.LIST.toString().toLowerCase())) {
+                    // LIST command
 
                      ui.printList(tasks);
 
                 } else if (taskType.equals(possibleTasks.DONE.toString().toLowerCase())) {
-                    // DONE case
+                    // DONE command
 
                     taskNum = Parser.getFinishedTaskNum(fullCommand);
-                    taskNum--; // ArrayList index == taskNum - 1
+
+                    // taskList index (starts from 0) differs from taskNum (starts from 1) by 1
+                    taskNum--;
 
                     tasks.getTask(taskNum).setDone();
 
                     ui.printDoneSequence(tasks, taskNum);
 
                 } else if (taskType.equals(possibleTasks.TODO.toString().toLowerCase())) {
-                    // TODO case
+                    // TODO command
 
                     if (fullCommand.length() < 5){
+                        // fullCommand contains only the string "todo"
                         throw new DukeException ("☹ OOPS!!! The description of a todo cannot be empty.");
                     }
 
@@ -74,15 +98,21 @@ public class Duke {
 
                     ui.printTodoSequence(tasks, newTodo);
 
-                    // DEADLINE case
                 } else if (taskType.equals(possibleTasks.DEADLINE.toString().toLowerCase())) {
+                    // DEADLINE command
 
-                    if ( ( fullCommand.length() < 9 )){ // Input is only "deadline"
+                    if ( ( fullCommand.length() < 9 )){
+
+                        // fullCommand contains only the string "deadline"
                         throw new DukeException("☹ OOPS!!! The description of an event cannot be empty.");
-                    } else if ( (fullCommand.lastIndexOf('/') < 1) || (  4+fullCommand.lastIndexOf('/') > fullCommand.length()   ) )  {
-                        throw new DukeException("☹ OOPS!!! The time period of an event cannot be empty.");
-                    }
 
+                    } else if ( (fullCommand.lastIndexOf('/') < 1) || (  4+fullCommand.lastIndexOf('/') > fullCommand.length()   ) )  {
+
+                        // fullCommand does not contain '/' chars or there are no char after "/by"
+                        throw new DukeException("☹ OOPS!!! The time period of an event cannot be empty.");
+
+                    }
+                    
                     description = Parser.getDeadlineDescription(fullCommand);
                     extraDescription = Parser.getDeadlineDateTime(fullCommand);
 
@@ -92,13 +122,19 @@ public class Duke {
 
                     ui.printDeadlineSequence(tasks, newDeadline);
 
-                    // EVENT case
                 } else if (taskType.equals(possibleTasks.EVENT.toString().toLowerCase())) {
+                    // EVENT command
 
                     if ( ( fullCommand.length() < 6 )){ // Input is only "event"
+
+                        // fullCommand contains only the string "event
                         throw new DukeException("☹ OOPS!!! The description of an event cannot be empty.");
+
                     } else if ( (fullCommand.lastIndexOf('/') < 1) || (  4+fullCommand.lastIndexOf('/') > fullCommand.length()   ) )  {
+
+                        // fullCommand does not contain '/' char or there are no chars after "/at"
                         throw new DukeException("☹ OOPS!!! The time period of an event cannot be empty.");
+
                     }
 
                     description = Parser.getEventDescription(fullCommand);
@@ -110,29 +146,35 @@ public class Duke {
 
                     ui.printEventSequence(tasks, newEvent);
 
-                    // DELETE case
                 } else if (taskType.equals(possibleTasks.DELETE.toString().toLowerCase())){
+                    // DELETE command
 
                     taskNum = Parser.getDeletedTaskNum(fullCommand);
-                    taskNum--; // ArrayList index == taskNum - 1
+
+                    // taskList index (starts from 0) differs from taskNum (starts from 1) by 1
+                    taskNum--;
 
                     if (taskNum >= tasks.getSize()){
+
+                        // taskNum does not exist
                         throw new DukeException("Task no. " + (taskNum+1) + " does not exist");
+
                     }
 
                     Task taskToDelete = tasks.getTask(taskNum);
+
                     tasks.deleteTask(taskNum);
 
                     ui.printDeleteSequence(tasks, taskToDelete);
                     taskToDelete = null;
 
-                    //BYE case
                 } else if (taskType.equals(possibleTasks.BYE.toString().toLowerCase())){
+                    // BYE command
 
                     ui.printByeSequence();
 
-                    // Clear the txt file and adds headers
                     storage.clearFileBeforeSaving();
+                    // Clear the txt file and adds headers
 
                     // Saves the task list to the file, following the pre-defined format
                     for (int i = 0; i < tasks.getSize(); i++){
@@ -140,18 +182,28 @@ public class Duke {
                     }
 
                     break;
+                    // Exits while loop once "bye" is entered
 
-                } else { // An invalid task command is given
+                } else {
+                    // An unrecognizable command is detected
                     throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
             } catch (DukeException e){
+
                 ui.showLoadingError(e);
+
             } catch (StringIndexOutOfBoundsException e){
+
                 System.out.println(e.getMessage());
+
             } catch (NumberFormatException e){
+
                 System.out.println(e.getMessage());
+
             } catch (IOException e){
+
                 System.out.println(e.getCause());
+
             }
 
             fullCommand = in.nextLine().trim();
@@ -160,10 +212,17 @@ public class Duke {
 
     }
 
+    /**
+     * Main methid to run Duke.
+     * @param args
+     */
     public static void main(String[] args) {
         new Duke("C:\\Users\\hatzi\\Documents\\Sourcetree\\duke\\data\\tasks.txt").run();
     }
 
+    /**
+     * possibleTasks is an enumeration of the constant, pre-defined, recognizable commands.
+     */
     enum possibleTasks{
         BYE,
         LIST,
--- src/main/java/seedu/duke/parser/Parser.java
+++ src/main/java/seedu/duke/parser/Parser.java
@@ -1,38 +1,84 @@
 package seedu.duke.parser;
 
+/**
+ * Parser class helps to parse the user string.
+ */
 public class Parser {
 
+    /**
+     * Default constructor of the Parser class.
+     */
     public Parser (){
     }
 
+    /**
+     * Returns the first word of the string fullCommand.
+     * @param fullCommand Input of the user, read from Scanner object.
+     * @return First word of a string, delimited by whitespaces, " ".
+     */
     public static String parseCommand(String fullCommand){
         return ( fullCommand.split(" ")[0] );
     }
 
+    /**
+     * Returns the todo task description of a Todo object.
+     * @param fullCommand Input of the user, read from Scanner object.
+     * @return Substring from index 5 (whitespace after "todo") to end of string.
+     */
     public static String getTodoDescription(String fullCommand){
         return ( fullCommand.substring(5) );
     }
 
+    /**
+     * Returns the deadline task description of a Deadline string.
+     * @param fullCommand Input of the user, read from Scanner object.
+     * @return Substring from index 9 (whitespace after "deadline") to the '/' char inside "/by".
+     */
     public static String getDeadlineDescription(String fullCommand){
         return ( fullCommand.substring(9, fullCommand.indexOf('/')) );
     }
 
+    /**
+     * Returns the unparsed DateTime string of a Deadline string.
+     * @param fullCommand Input of the user, read from Scanner object.
+     * @return Substring from 4 chars after '/' in "/by".
+     */
     public static String getDeadlineDateTime(String fullCommand){
         return ( fullCommand.substring(4 + fullCommand.indexOf('/')) );
     }
 
+    /**
+     * Returns the event task description of an Event string.
+     * @param fullCommand Input of the user, read from Scanner object.
+     * @return Substring from index 6 (whitespace after "event") to the '/' char inside "/at"
+     */
     public static String getEventDescription(String fullCommand){
         return ( fullCommand.substring(6, fullCommand.lastIndexOf('/')) );
     }
 
+    /**
+     * Returns the event location of an Event string.
+     * @param fullCommand Input of the user, read from Scanner object.
+     * @return Substring from 4 chars after '/' in "/at"
+     */
     public static String getEventLocation(String fullCommand){
         return ( fullCommand.substring(4 + fullCommand.lastIndexOf('/')) );
     }
 
+    /**
+     * Returns the specified task number for a "done" string command.
+     * @param fullCommand Input of the user, read from Scanner object.
+     * @return Int of the substring from index 5 ( whitespace after "done" string).
+     */
     public static int getFinishedTaskNum(String fullCommand){
         return ( Integer.parseInt(fullCommand.substring(5)) ) ;
     }
 
+    /**
+     * Returns the specified task number for a "delete" string command.
+     * @param fullCommand Input of the user, read from Scanner object.
+     * @return Int of the substring from index 7 ( whitespace after "delete" string).
+     */
     public static int getDeletedTaskNum(String fullCommand){
         return ( Integer.parseInt(fullCommand.substring(7)) );
     }

137d67e407b6b78b4c3bd381f29d9fe5a7120b65
czkay <chong.zi.kang@gmail.com>
2019-08-30 13:47:31 +0800

    Add InvalidTimeException to Duke

---
+++ src/main/java/InvalidTimeException.java
@@ -0,0 +1,9 @@
+/**
+ * A DukeException for when the user inputs the time of a Deadline or Event task in an improper format.
+ */
+public class InvalidTimeException extends DukeException {
+
+    public InvalidTimeException(String msg) {
+        super(msg);
+    }
+}

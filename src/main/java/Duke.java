3c56402055f02ad353db45e5941f1a6b7f1486fa
Xelyion <xelyionzero@gmail.com>
2019-08-30 15:02:51 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -13,23 +14,38 @@ public class Duke {
         System.out.println("    Hello, I'm Duke!\n    What can I do for you?");
         System.out.println(blankLines);
 
+        ArrayList<String> myList = new ArrayList<String>();
         Scanner input = new Scanner(System.in);
-        String command = input.next();
+        String command = input.nextLine();
+
+
         while(!command.equals("bye")){
             switch(command){
                 case "list":
-                    System.out.println("    list");
-                    command = input.next();
+                    System.out.println(blankLines);
+                    if (myList.isEmpty()){
+                        System.out.println("    List is empty");
+                    } else {
+                        int index = 1;
+                        for (String x : myList) {
+                            System.out.println("    " + index + "." + x);
+                            index++;
+                        }
+                    }
+                    command = input.nextLine();
                     break;
 
                 case "blah":
+                    System.out.println(blankLines);
                     System.out.println("    blah");
-                    command = input.next();
+                    command = input.nextLine();
                     break;
 
                 default:
-                    System.out.println("    Invalid Command");
-                    command = input.next();
+                    System.out.println(blankLines);
+                    System.out.println("    added: " + command);
+                    myList.add(command);
+                    command = input.nextLine();
                     break;
             }
         }

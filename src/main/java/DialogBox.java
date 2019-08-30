03eb69a71bae5378bd9b66d59855df524fc66160
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2019-08-30 21:53:55 +0800

    Beautify user interaction

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,7 +1,12 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
 
 /**
  * A simple Dialog Box which takes returns a picture and the text involved.
@@ -23,15 +28,50 @@ public class DialogBox extends HBox {
      * @param label the label also known as the text;
      * @param image the image to be shown to the user;
      */
-    public DialogBox(Label label, ImageView image) {
+    private DialogBox(Label label, ImageView image) {
         text = label;
         displayPicture = image;
 
         text.setWrapText(true);
         displayPicture.setFitWidth(100.0);
         displayPicture.setFitHeight(100.0);
-
+        Circle clip = new Circle(50, 50, 50);
+        displayPicture.setClip(clip);
+        ;
+        this.setPadding(new Insets(10, 10, 10, 10));
         this.setAlignment(Pos.TOP_LEFT);
         this.getChildren().addAll(displayPicture, text);
+    }
+
+    /**
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     */
+    private void flip() {
+        this.setAlignment(Pos.TOP_RIGHT);
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(tmp);
+        this.getChildren().setAll(tmp);
+    }
+
+    /**
+     * Creates a DialogBox based on user output and photo.
+     * @param label the text involved.
+     * @param image the photo involved.
+     * @return a new Dialog Box based on user output and photo.
+     */
+    public static DialogBox getUserDialog(Label label, ImageView image) {
+        return new DialogBox(label, image);
+    }
+
+    /**
+     * Creates a DialogBox based on duke output and photo.
+     * @param label the text involved.
+     * @param image the photo involved.
+     * @return a new Dialog Box based on duke output and photo.
+     */
+    public static DialogBox getDukeDialog(Label label, ImageView image) {
+        var dialogBox = new DialogBox(label, image);
+        dialogBox.flip();
+        return dialogBox;
     }
 }

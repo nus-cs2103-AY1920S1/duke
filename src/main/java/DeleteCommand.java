a471757710b227d8c8ba3f16fb84d31931ebfe76
hellodommy <dominique@u.nus.edu>
2019-08-31 19:31:17 +0800

    Handle invalid delete commands exception

--- src/main/java/DeleteCommand.java
+++ src/main/java/DeleteCommand.java
@@ -5,12 +5,20 @@ public class DeleteCommand extends Command {
     private int taskNumber;
 
     public DeleteCommand(String command) {
-        this.taskNumber = Integer.parseInt(command);
+        try {
+            this.taskNumber = Integer.parseInt(command);
+        } catch (NumberFormatException e) {
+            throw new DukeException("    OOPS!!! Please specify the index of the task to be deleted.");
+        }
     }
 
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
-        tasks.deleteTask(taskNumber, ui);
+        try {
+            tasks.deleteTask(taskNumber, ui);
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException("    OOPS!!! Your specified task number is out of range.");
+        }
         try {
             storage.store(tasks);
         } catch (IOException e) {

6cf8df04e15d8fdf94e74cbe404a6c3716b72903
Yen-Peng <e0318790@u.nus.edu>
2019-08-31 15:57:23 +0800

    Add the ability to mark tasks as done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -25,11 +25,11 @@ public class Duke {
                 switch(input) {
                     case "list":
                         System.out.println("____________________________________________________________\n");
-
+                        System.out.println("Here are the tasks in your list:");
                         for (int i = 0; i < numOfItems; i++) {
                             count++;
                             assert tasks[i] != null;
-                            System.out.println(count + ". " + tasks[i].getDescription().toString());
+                            System.out.println(count + ". " + "[" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription().toString());
                         }
                         System.out.println("____________________________________________________________\n");
                         break;
@@ -42,11 +42,26 @@ public class Duke {
                         break;
 
                     default:
-                        Task currTask = new Task(input);
-                        tasks[numOfItems++] = currTask;
-                        System.out.println("____________________________________________________________\n");
-                        System.out.println("added: " + input);
-                        System.out.println("____________________________________________________________\n");
+                        int i = input.indexOf(' ');
+                        String word = input.substring(0, i);
+                        String rest = input.substring(i);
+                        String second = input.split(" ")[1];
+
+                        if (word.equals("done")) {
+                            int select = Integer.parseInt(second);
+                            tasks[select-1].markAsDone(tasks[select-1]);
+                            System.out.println("____________________________________________________________\n"
+                                    + "Nice! I've marked this task as done:\n"
+                                    + "[" + tasks[select-1].getStatusIcon() + "] " + tasks[select-1].getDescription().toString() + "\n"
+                                    + "____________________________________________________________\n");
+                        } else {
+
+                            Task currTask = new Task(input);
+                            tasks[numOfItems++] = currTask;
+                            System.out.println("____________________________________________________________\n");
+                            System.out.println("added: " + input);
+                            System.out.println("____________________________________________________________\n");
+                        }
                 }
 
             } catch (IOException e) {
@@ -55,4 +70,3 @@ public class Duke {
         }
     }
 }
-
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -4,12 +4,22 @@
 
 public class Task {
     protected String description;
+    protected boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); // returns check mark if done, cross symbol if undone
     }
 
     public String getDescription() {
         return (description);
+    }
+
+    public void markAsDone(Task t) {
+        t.isDone = true;
     }
 }

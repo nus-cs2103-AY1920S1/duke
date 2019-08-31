41214e50c6464cdcb1eccd17a490eb5fedfa363a
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2019-08-31 20:38:46 +0800

    Add fxml files

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,77 +1,62 @@
+import java.io.IOException;
+import java.util.Collections;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
-import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.shape.Circle;
 
 /**
- * A simple Dialog Box which takes returns a picture and the text involved.
+ * An example of a custom control using FXML.
+ * This control represents a dialog box consisting of an ImageView to represent the speaker's face and a label
+ * containing text from the speaker.
  */
 public class DialogBox extends HBox {
-
-    /**
-     * The text to be shown to the user.
-     */
-    private Label text;
-
-    /**
-     * The display picture to be shown to the user.
-     */
+    @FXML
+    private Label dialog;
+    @FXML
     private ImageView displayPicture;
 
-    /**
-     * Constructs a new Dialog Box which shows the text and image involved.
-     * @param label the label also known as the text;
-     * @param image the image to be shown to the user;
-     */
-    private DialogBox(Label label, ImageView image) {
-        text = label;
-        displayPicture = image;
+    private DialogBox(String text, Image img) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        text.setWrapText(true);
-        displayPicture.setFitWidth(100.0);
-        displayPicture.setFitHeight(100.0);
+        dialog.setText(text);
+        displayPicture.setImage(img);
         Circle clip = new Circle(50, 50, 50);
         displayPicture.setClip(clip);
-        ;
-        this.setPadding(new Insets(10, 10, 10, 10));
-        this.setAlignment(Pos.TOP_LEFT);
-        this.getChildren().addAll(displayPicture, text);
     }
 
     /**
      * Flips the dialog box such that the ImageView is on the left and text on the right.
      */
     private void flip() {
-        this.setAlignment(Pos.TOP_RIGHT);
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
-        FXCollections.reverse(tmp);
-        this.getChildren().setAll(tmp);
+        Collections.reverse(tmp);
+        getChildren().setAll(tmp);
+        setAlignment(Pos.TOP_LEFT);
     }
 
-    /**
-     * Creates a DialogBox based on user output and photo.
-     * @param label the text involved.
-     * @param image the photo involved.
-     * @return a new Dialog Box based on user output and photo.
-     */
-    public static DialogBox getUserDialog(Label label, ImageView image) {
-        return new DialogBox(label, image);
+    public static DialogBox getUserDialog(String text, Image img) {
+        return new DialogBox(text, img);
     }
 
-    /**
-     * Creates a DialogBox based on duke output and photo.
-     * @param label the text involved.
-     * @param image the photo involved.
-     * @return a new Dialog Box based on duke output and photo.
-     */
-    public static DialogBox getDukeDialog(Label label, ImageView image) {
-        var dialogBox = new DialogBox(label, image);
-        dialogBox.flip();
-        return dialogBox;
+    public static DialogBox getDukeDialog(String text, Image img) {
+        var db = new DialogBox(text, img);
+        db.flip();
+        return db;
     }
 }
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,3 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Region;
-import javafx.stage.Stage;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-
 import duke.command.Command;
 import duke.exception.DukeException;
 import duke.parser.DataParser;
@@ -22,35 +9,7 @@ import duke.ui.Ui;
 /**
  * Main Class of the Application, which requires the workings of all packages.
  */
-public class Duke extends Application {
-
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
-    /**
-     *  Represents the scroll pane for the application.
-     */
-    private ScrollPane scrollPane;
-
-    /**
-     *  Represents the text box for the applications.
-     */
-    private VBox dialogContainer;
-
-    /**
-     * Represents the text field for the application.
-     */
-    private TextField userInput;
-
-    /**
-     * Represents the send button for the application.
-     */
-    private Button sendButton;
-
-    /**
-     * Represents the rendered scene for the application.
-     */
-    private Scene scene;
-
+public class Duke {
     /**
      * Represents the reader and the writer for the output text file.
      */
@@ -123,99 +82,12 @@ public class Duke extends Application {
         ui.sendFarewell();;
     }
 
-    @Override
-    public void start(Stage stage) {
-
-        // Set Stage
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        // Set Features
-        stage.setTitle("Duke: Abstraction");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        stage.setScene(scene);
-        stage.show();
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        // You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-        AnchorPane.setLeftAnchor(userInput, 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        // Set Handling of User Input
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-
-        // Ensures scrolling to the end
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-    }
-
-    /**
-     * Iteration 1:
-     * Creates a label with the specified text and adds it to the dialog container.
-     * @param text String containing text to add
-     * @return a label with the specified text that has word wrap enabled.
-     */
-    private Label getDialogLabel(String text) {
-        Label textReceived = new Label(text);
-        textReceived.setWrapText(true);
-
-        return textReceived;
-    }
-
-    /**
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply.
-     * Follwong which, appends them to the dialog container.
-     * Clears the user input after processing.
-     */
-    private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(returnResponse(userInput.getText()));
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-        );
-        userInput.clear();
-    }
-
     /**
      *  Takes in a input and returns a response which duke says.
      * @param input the input received from the user.
      * @return a response in the form of a String.
      */
-    private String returnResponse(String input) {
+    protected String getResponse(String input) {
         return "Duke says again: " + input;
     }
 

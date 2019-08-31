f31093236f05d3909995dda083661bfbed9acf5c
hellodommy <dominique@u.nus.edu>
2019-08-31 19:29:07 +0800

    Handle invalid done commands exception

--- src/main/java/DoneCommand.java
+++ src/main/java/DoneCommand.java
@@ -5,12 +5,20 @@ public class DoneCommand extends Command {
     private int taskNumber;
 
     public DoneCommand(String command) {
-        this.taskNumber = Integer.parseInt(command);
+        try {
+            this.taskNumber = Integer.parseInt(command);
+        } catch (NumberFormatException e) {
+            throw new DukeException("    OOPS!!! Please specify a task number to be marked as done.");
+        }
     }
 
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
-        tasks.completeTask(taskNumber, ui);
+        try {
+            tasks.completeTask(taskNumber, ui);
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException("    OOPS!!! Your specified task number is out of range.");
+        }
         try {
             storage.store(tasks);
         } catch (IOException e) {

955be96e029e089cebe5e3d50591216296289be5
MackyMaguire <zhoutianyu68@gmail.com>
2019-08-31 22:18:35 +0800

    Level 8. Dates and Times

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -1,20 +1,35 @@
 package duke.task;
 
-public class Deadline extends Task {
-    protected String time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-    public Deadline(String name, String time, boolean isDone) {
+public class Deadline extends Task {
+    protected String dateTime;
+
+    public Deadline(String name, String dateTime, boolean isDone) {
         super(name, isDone);
-        this.time = time;
-        //Format date;
+        this.dateTime = formatDateTime(dateTime);
+    }
+
+    private String formatDateTime(String dateTime) {
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HHmm");
+        SimpleDateFormat newFormat = new SimpleDateFormat("d 'of' MMMMMMMMM yyyy, h:mm a");
+        try {
+            Date date = format.parse(dateTime);
+            System.out.println(date);
+            return newFormat.format(date);
+        } catch (ParseException e) {
+            return dateTime;
+        }
     }
 
     public String getTime() {
-        return this.time;
+        return this.dateTime;
     }
 
     @Override
     public String toString() {
-        return String.format("[D] | %s | by: %s", super.toString(), this.time);
+        return String.format("[D] | %s | by: %s", super.toString(), this.dateTime);
     }
 }
--- src/main/java/duke/task/Event.java
+++ src/main/java/duke/task/Event.java
@@ -1,20 +1,34 @@
 package duke.task;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Event extends Task {
-    protected String time;
+    protected String dateTime;
 
     public Event(String name, String time, boolean isDone) {
         super(name, isDone);
-        this.time = time;
-        //Format date;
+        this.dateTime = formatDateTime(dateTime);
+    }
+
+    private String formatDateTime(String dateTime) {
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HHmm");
+        SimpleDateFormat newFormat = new SimpleDateFormat("d 'of' MMMMMMMMM yyyy, h:mm a");
+        try {
+            Date date = format.parse(dateTime);
+            return newFormat.format(date);
+        } catch (ParseException e) {
+            return dateTime;
+        }
     }
 
     public String getTime() {
-        return this.time;
+        return this.dateTime;
     }
 
     @Override
     public String toString() {
-        return String.format("[E] | %s | at: %s", super.toString(), this.time);
+        return String.format("[E] | %s | at: %s", super.toString(), this.dateTime);
     }
 }

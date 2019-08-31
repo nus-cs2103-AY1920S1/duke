13b8abe47f87ad78c0a77ec0bf2b0cced1f8845e
Yen-Peng <e0318790@u.nus.edu>
2019-08-31 15:44:27 +0800

    Add the ability to greet the user, echos commands entered by user, and exits when the user types bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String greet = "____________________________________________________________\n"
+                + "Hello! I'm Duke\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n";
+
+        System.out.println(greet);
+
+        while(true) {
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                String input = br.readLine();
+
+                switch(input) {
+                    case "bye":
+                        System.out.println("____________________________________________________________\n"
+                                + "Bye. Hope to see you again soon!\n"
+                                + "____________________________________________________________\n");
+                        System.exit(0);
+                        break;
+
+                    default:
+                        System.out.println("____________________________________________________________\n"
+                                + input + "\n"
+                                + "____________________________________________________________\n");
+
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }

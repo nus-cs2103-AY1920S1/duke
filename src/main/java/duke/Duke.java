fd0eec2c4cbb7be9400b93a3e124069ea6cd8e99
Parcly Taxel <reddeloostw@gmail.com>
2019-09-01 03:05:05 +0800

    Implement level 10, for real this time (FXML)

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -5,62 +5,26 @@ import duke.storage.Storage;
 import duke.task.TaskList;
 import java.io.FileNotFoundException;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
- * The main class.
+ * The main logical class, responsible for initialisation of data structures
+ * and passing arguments to them for processing.
  */
 public class Duke extends Application {
     private TaskList tasks;
-    private Storage storage;
+    private final Storage storage;
 
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
-
-    private void handleUserInput() {
-        String cmd = userInput.getText();
-        String resp;
-
+    String getResponse(String cmd) {
         try {
             Command c = Command.parse(cmd);
-            resp = c.execute(tasks, storage);
-            if (c.isExit()) {
-                Platform.exit();
-            }
+            return c.execute(tasks, storage);
         } catch (IllegalArgumentException e) {
-            resp = e.getMessage();
+            return e.getMessage();
         }
-
-        Label userText = new Label(cmd);
-        Label dukeText = new Label(resp);
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-        );
-        userInput.clear();
     }
 
-    /**
-     * Initializes a new Duke session.
-     */
-    public Duke() {
+    Duke() {
         storage = new Storage("tasks.txt");
         try {
             tasks = new TaskList(storage.read());
@@ -70,57 +34,6 @@ public class Duke extends Application {
     }
 
     @Override
-    public void init() {
-    }
-
-    @Override
     public void start(Stage stage) {
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput, 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        // Event handlers
-        sendButton.setOnMouseClicked(event -> handleUserInput());
-        userInput.setOnAction(event -> handleUserInput());
     }
 }

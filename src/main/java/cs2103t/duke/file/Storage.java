841a10a84b063cbd1cedbb4902aee7ce8ae0c33a
kxyoke <kxyoke@gmail.com>
2019-08-31 16:46:40 +0800

    Revise to coding standard

--- src/main/java/cs2103t/duke/file/Storage.java
+++ src/main/java/cs2103t/duke/file/Storage.java
@@ -9,8 +9,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
--- src/main/java/cs2103t/duke/task/TaskType.java
+++ src/main/java/cs2103t/duke/task/TaskType.java
@@ -4,15 +4,41 @@ public enum TaskType {
     T, D, E, LIST, DELETE, DONE, EXIT;
 
     public static TaskType convertToTaskType(String typeString) {
+        TaskType type;
         switch (typeString.toUpperCase()) {
-        case "T": case "TODO": return T;
-        case "D": case "DEADLINE": return D;
-        case "E": case "EVENT": return E;
-        case "LIST": return LIST;
-        case "DELETE": return DELETE;
-        case "DONE": return DONE;
-        case "EXIT": case "BYE": return EXIT;
-        default: return null;
+        case "T":
+            //Fallthrough
+        case "TODO":
+            type = T;
+            break;
+        case "D":
+            //Fallthrough
+        case "DEADLINE":
+            type = D;
+            break;
+        case "E":
+            //Fallthrough
+        case "EVENT":
+            type = E;
+            break;
+        case "LIST":
+            type = LIST;
+            break;
+        case "DELETE":
+            type = DELETE;
+            break;
+        case "DONE":
+            type = DONE;
+            break;
+        case "EXIT":
+            //Fallthrough
+        case "BYE":
+            type = EXIT;
+            break;
+        default:
+            type = null;
+            break;
         }
+        return type;
     }
 }

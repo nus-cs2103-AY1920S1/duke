1ed42df957226985b01cc518ae1ccd373c5ae96d
ROHITREDDYBALAM <sairohitreddyb@gmail.com>
2019-09-01 01:47:49 +0800

    set up Gradle to run Unit Tests

--- src/test/java/TestTask.java
+++ src/test/java/TestTask.java
@@ -3,21 +3,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Contains the JUnit tests for the Task class.
+ *
  */
 public class TestTask {
-    @Test
+
     /**
      * Tests the toString method.
      */
-    public void testStringConversion(){
-        assertEquals("[" + "\u2718" + "] test",new Task("test").toString());
+    @Test
+    public void testStringConversion() {
+        assertEquals("[" + "0" + "] test",new Task("test").toString());
     }
 
     /**
      * Tests the markAsDone method.
      */
     @Test
-    public void testMarkAsDone(){
+    public void testMarkAsDone() {
         Task t = new Task("test");
         t.markAsDone();
         assertEquals(true, t.isDone);
--- src/test/java/TestToDo.java
+++ src/test/java/TestToDo.java
@@ -1,14 +1,16 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * Contains the JUnit tests for ToDo class.
  */
 public class TestToDo {
+
     /**
      * Tests the toString method of Todo class.
      */
     @Test
-    public void testStringConversion(){
-       assertEquals("[T][" +  "\u2718" + "] test",new ToDo("test").toString());
+    public void testStringConversion() {
+        assertEquals("[T][" +  "0" + "] test",new ToDo("test").toString());
     }
 }

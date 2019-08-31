28aa72dfe792e43953c3701be057b23792a892f6
lzw12345 <lingzw64@gmail.com>
2019-09-01 00:12:07 +0800

    updating parser class

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,5 +1,6 @@
 import java.util.Date;
 import java.util.LinkedList;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Parser {
@@ -7,6 +8,7 @@ public class Parser {
     private boolean isDone;
     private String description;
     private String date;
+    // include a /s* after first \s*
     private Pattern Command_format= Pattern.compile("(?<commandWord>\\w+)"
             +" (?:(\\[))?(?<arguments>(?:[\\w+])?)(?:(\\])?)"
             +"(?<description>([\\w\\s]+))"
@@ -15,7 +17,29 @@ public class Parser {
     }
 
     public Parser parse (String fullCommand){
-        if
+        Matcher matcher = Command_format.matcher(fullCommand);
+        if(!matcher.find()){
+            System.out.println("    ____________________________________________________________\n" +
+                    "     ☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n" +
+                    "    ____________________________________________________________");
+        }
+        switch(matcher.group("commandWord")){
+        case "list":
+            commandType = CommandType.LIST;
+            break;
+        case "todo":
+            commandType = CommandType.ADDTODO;
+            break;
+        case "deadline":
+            commandType = CommandType.ADDDEADLINE;
+            break;
+        case "event":
+            commandType = CommandType.ADDEVENT;
+            break;
+            case "delete":
+
+        }
+
 
     }
 }

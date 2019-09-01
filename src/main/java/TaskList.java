1309c5a3096a6b75b68cb8997a9a264ac7c601b5
SebastianLie <jackcass629@gmail.com>
2019-09-01 10:17:22 +0800

    Add Gradle Support, Checkstyle plugin

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,7 +1,3 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class TaskList {

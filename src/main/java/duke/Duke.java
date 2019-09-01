c21af60f2085b05a84db486cc809e6bb959683e1
Na-Nazhou <nazhou.na9@gmail.com>
2019-09-01 18:48:42 +0800

    Design the layout and add JavaFX controls

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -7,11 +7,23 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.Region;
 
 public class Duke extends Application {
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
+
+    private Scene scene;
+    private TextField userInput;
+    private Button sendButton;
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
 
     public Duke() {
         this(Storage.DEFAULT_STORAGE_FILEPATH);
@@ -30,10 +42,56 @@ public class Duke extends Application {
 
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+        //Step 1. Setting up required components
 
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);  // Setting the scene to be the AnchorPane
         stage.setScene(scene); // Setting the stage to show our screen
+
+        //Step 2. Formatting the window to look as expected
+        stage.setTitle("Duke");
+        stage.setResizable(true);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(400, 570); // set preferred size
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS); // only horizontal scroll bar is shown
+
+        // vvalue: the current vertical scroll position of the ScrollPane.
+        scrollPane.setVvalue(1.0);
+        // fitToWidth: if true and if the contained node is a Resizable,
+        // then the node will be kept resized to match the width of the ScrollPane's viewport.
+        scrollPane.setFitToWidth(true);
+
+        // You will need to import `javafx.scene.layout.Region` for this.
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(340.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        // topAnchor: distance from the anchor pane's top insets to the child's top edge.
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
         stage.show(); // Render the stage.
     }
 

212a0bafae582aea43b15e309bb3935188273cd5
febee99 <febee99@gmail.com>
2019-09-01 23:03:29 +0800

    Add JavaFx controls to handle user input

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -6,11 +6,26 @@ import duke.parser.Parser;
 import duke.storage.Storage;
 import duke.tasks.TaskList;
 import duke.ui.Ui;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
 
-public class Duke {
+public class Duke extends Application {
     private final Storage storage;
     private final Ui ui;
     private TaskList tasks;
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
 
     /**
      * Initialise new duke.Duke session.
@@ -26,9 +41,9 @@ public class Duke {
         }
     }
 
-    public static void main(String[] args) {
-        new Duke().run();
-    }
+//    public static void main(String[] args) {
+//        new Duke().run();
+//    }
 
     /**
      * Starts the session and parses user input.
@@ -49,5 +64,89 @@ public class Duke {
                 ui.showLine();
             }
         }
+    }
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            //Step 1. Setting up required components
+            //The container for the content of the chat to scroll.
+            scrollPane = new ScrollPane();
+            dialogContainer = new VBox();
+            scrollPane.setContent(dialogContainer);
+
+            userInput = new TextField();
+            sendButton = new Button("Send");
+
+            AnchorPane mainLayout = new AnchorPane();
+            mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+            scene = new Scene(mainLayout);
+
+            stage.setScene(scene);
+            stage.show();
+
+            //Step 2. Formatting the window to look as expected
+            stage.setTitle("Duke");
+            stage.setResizable(false);
+            stage.setMinHeight(600.0);
+            stage.setMinWidth(400.0);
+
+            mainLayout.setPrefSize(400.0, 600.0);
+
+            scrollPane.setPrefSize(385, 535);
+            scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+            scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+            scrollPane.setVvalue(1.0);
+            scrollPane.setFitToWidth(true);
+
+            // You will need to import `javafx.scene.layout.Region` for this.
+            dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+            userInput.setPrefWidth(325.0);
+
+            sendButton.setPrefWidth(55.0);
+
+            AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+            AnchorPane.setBottomAnchor(sendButton, 1.0);
+            AnchorPane.setRightAnchor(sendButton, 1.0);
+
+            AnchorPane.setLeftAnchor(userInput, 1.0);
+            AnchorPane.setBottomAnchor(userInput, 1.0);
+
+            //Step 3. Add functionality to handle user input.
+            sendButton.setOnMouseClicked((event) -> {
+                dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
+                userInput.clear();
+            });
+
+            userInput.setOnAction((event) -> {
+                dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
+                userInput.clear();
+            });
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    /**
+     * Iteration 1:
+     * Creates a label with the specified text and adds it to the dialog container.
+     *
+     * @param text String containing text to add
+     * @return a label with the specified text that has word wrap enabled.
+     */
+    private Label getDialogLabel(String text) {
+        // You will need to import `javafx.scene.control.Label`.
+        Label textToAdd = new Label(text);
+        textToAdd.setWrapText(true);
+
+        return textToAdd;
     }
 }

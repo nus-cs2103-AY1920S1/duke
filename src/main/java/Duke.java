1daaa63dbd66cea9a264102c82a3df8f821d38ea
Rui Zhi <gohruizhi@u.nus.edu>
2019-09-01 18:33:23 +0800

    Set up for Javafx

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
-public class Duke {
+
+public class Duke extends Application {
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
+
 
     public Duke(String filePath) {
         ui = new Ui();
@@ -54,5 +61,14 @@ public class Duke {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HHmm");
         String sDate = sdf.format(date);
         return sDate;
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }

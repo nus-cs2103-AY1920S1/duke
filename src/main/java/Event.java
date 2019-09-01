ba24fc192789ae8df9a5fd48f130e5635ade4ad3
Xelyion <xelyionzero@gmail.com>
2019-09-01 22:20:18 +0800

    Level 8. Date and Times

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,3 +1,6 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Represents an event to be completed
  * Contains a description of the task
@@ -7,12 +10,35 @@
 
 public class Event extends Task {
     protected String time;
+    protected Date date;
 
-    public Event (String descriptionAndTime) {
+    public Event (String descriptionAndTime){
         super(descriptionAndTime);
         String[] splitString = descriptionAndTime.split("/at");
         this.description = splitString[0].substring(6, splitString[0].length() - 1);
         this.time = splitString[1].substring(1, splitString[1].length());
+        try {
+            understandDate();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    /**
+     * Determines date from time input if format fits "DD/MM/YYYY HHMM"
+     *
+     */
+    public void understandDate() throws Exception{
+        String[] splitString = time.split(" ");
+        if (splitString.length == 2) {
+            if (splitString[1].length() == 4) {
+                Date date = new SimpleDateFormat("dd/MM/yyyy").parse(splitString[0]);
+                long hour = Long.parseLong(splitString[1].substring(0,2));
+                long min = Long.parseLong(splitString[1].substring(2,4));
+                date.setTime(date.getTime() + ((hour * 60 + min)*60000));
+                this.time = date.toString();
+            }
+        }
     }
 
     @Override

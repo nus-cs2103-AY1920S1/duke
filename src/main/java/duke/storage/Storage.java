ddcbd5961f9006d6cf208e48093e648d34a48317
TSAI, HSIAO-HAN <sunnyhome3224@gmail.com>
2019-09-01 16:49:49 +0800

    check style

--- src/main/java/duke/storage/Storage.java
+++ src/main/java/duke/storage/Storage.java
@@ -47,29 +47,29 @@ public class Storage {
         while (readFileScanner.hasNext()) {
             String[] todoTask = readFileScanner.nextLine().split(" \\| ");
             switch (todoTask[0]) {
-                case "T":
-                    Task task1 = new Todo(todoTask[2]);
-                    if (todoTask[1].equals("1")) {
-                        task1.markAsDone();
-                    }
-                    tasks.add(task1);
-                    break;
-                case "D":
-                    Task task2 = new Deadline(todoTask[2], todoTask[3]);
-                    if (todoTask[1].equals("1")) {
-                        task2.markAsDone();
-                    }
-                    tasks.add(task2);
-                    break;
-                case "E":
-                    Task task3 = new Event(todoTask[2], todoTask[3]);
-                    if (todoTask[1].equals("1")) {
-                        task3.markAsDone();
-                    }
-                    tasks.add(task3);
-                    break;
-                default:
-                    throw new DukeException("Something in file go wrong.");
+            case "T":
+                Task task1 = new Todo(todoTask[2]);
+                if (todoTask[1].equals("1")) {
+                    task1.markAsDone();
+                }
+                tasks.add(task1);
+                break;
+            case "D":
+                Task task2 = new Deadline(todoTask[2], todoTask[3]);
+                if (todoTask[1].equals("1")) {
+                    task2.markAsDone();
+                }
+                tasks.add(task2);
+                break;
+            case "E":
+                Task task3 = new Event(todoTask[2], todoTask[3]);
+                if (todoTask[1].equals("1")) {
+                    task3.markAsDone();
+                }
+                tasks.add(task3);
+                break;
+            default:
+                throw new DukeException("Something in file go wrong.");
             }
         }
         readFileScanner.close();

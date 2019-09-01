b1880b458fb0a0352e6c16d92e4fc5a964f2949b
Kevin Lim <lyskevin@gmail.com>
2019-09-01 10:56:45 +0800

    Added test suite to the ParserTest class to test for deadline commands with valid inputs

--- src/test/java/duke/parser/ParserTest.java
+++ src/test/java/duke/parser/ParserTest.java
@@ -7,6 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParserTest {
 
+    private static final String[] DESCRIPTIONS = {"me", "you", "her", "i", "we"};
+    private static final String[] VALID_DATES = {"01/01/2019", "02/02/2019", "03/03/2019", "04/04/2019",
+            "05/05/2019", "06/06/2019", "07/07/2019", "08/08/2019", "09/09/2019", "10/10/2019",
+            "11/11/2019", "12/12/2019"};
+    private static final String[] VALID_TIMES = {"06:00:00", "12:00:00", "18:00:00", "23:59:59"};
+
     private class TaskListStub extends TaskList {
         @Override
         public int size() {
@@ -14,21 +20,43 @@ public class ParserTest {
         }
     }
 
+    /**
+     * Tests the deadline command using invalid inputs.
+     */
     @Test
-    public void todoCommand_validInput_correctCommandResult() {
-        String[] todoDescriptions = {"me", "you", "her", "i", "we"};
+    public void deadlineCommand_invalidInput_incorrectCommandResult() {
+
+    }
+
+    /**
+     * Tests the deadline command using valid inputs.
+     */
+    @Test
+    public void deadlineCommand_validInput_relevantCommandResult() {
         String successMessage = "Got it. I've added this task:\n"
                 + "%s\nNow you have %d task(s) in the list.\n";
-        TaskListStub taskListStub = new TaskListStub();
-        for (int i = 0; i < todoDescriptions.length; i++) {
-            Command command = Parser.parse("todo " + todoDescriptions[i]);
-            command.setTaskListToExecuteOn(taskListStub);
-            String expectedDescription = "[T][\u2718] " + todoDescriptions[i];
-            String expected = String.format(successMessage, expectedDescription, 1);
-            assertEquals(expected, command.execute().getMessage());
+        for (int i = 0; i < DESCRIPTIONS.length; i++) {
+            for (int j = 0; j < VALID_DATES.length; j++) {
+                for (int k = 0; k < VALID_TIMES.length; k++) {
+                    String description = DESCRIPTIONS[i];
+                    String date = VALID_DATES[j];
+                    String time = VALID_TIMES[k];
+                    String commandString = String.format("deadline %s /by %s %s", description,
+                            date, time);
+                    Command command = Parser.parse(commandString);
+                    command.setTaskListToExecuteOn(new TaskListStub());
+                    String expectedCommandResult = String.format("[D][\u2718] %s (by: %s %s)",
+                            description, date, time);
+                    String expected = String.format(successMessage, expectedCommandResult, 1);
+                    assertEquals(expected, command.execute().getMessage());
+                }
+            }
         }
     }
 
+    /**
+     * Tests the todo command using invalid inputs.
+     */
     @Test
     public void todoCommand_invalidInput_incorrectCommandResult() {
         String[] invalidTodoInputs = {"todo"};
@@ -36,6 +64,22 @@ public class ParserTest {
         for (int i = 0; i < invalidTodoInputs.length; i++) {
             String actual = Parser.parse(invalidTodoInputs[i]).execute().getMessage();
             assertEquals(expected, actual);
+        }
+    }
+
+    /**
+     * Tests the todo command using valid inputs.
+     */
+    @Test
+    public void todoCommand_validInput_relevantCommandResult() {
+        String successMessage = "Got it. I've added this task:\n"
+                + "%s\nNow you have %d task(s) in the list.\n";
+        for (int i = 0; i < DESCRIPTIONS.length; i++) {
+            Command command = Parser.parse("todo " + DESCRIPTIONS[i]);
+            command.setTaskListToExecuteOn(new TaskListStub());
+            String expectedDescription = "[T][\u2718] " + DESCRIPTIONS[i];
+            String expected = String.format(successMessage, expectedDescription, 1);
+            assertEquals(expected, command.execute().getMessage());
         }
     }
 

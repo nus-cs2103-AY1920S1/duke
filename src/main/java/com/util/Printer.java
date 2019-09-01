df58b207f6266d7bfa0826d58f5775038344eb4d
AHaliq <liqo12@hotmail.com>
2019-09-01 11:17:42 +0800

    added comments for indent functions

--- src/main/java/com/util/Printer.java
+++ src/main/java/com/util/Printer.java
@@ -62,10 +62,21 @@ public class Printer {
         System.out.print(formatString(str));
     }
 
+    /**
+     * Indent each line with INDENT string.
+     * @param str   string to be indented
+     * @return      formatted string
+     */
     public static String indentString(String str) {
         return indentString(str, INDENT);
     }
 
+    /**
+     * Given a specific indent string, append it before each line.
+     * @param str   string to be indented
+     * @param ind   indent string
+     * @return      formatted string
+     */
     public static String indentString(String str, String ind) {
         StringBuilder formattedString = new StringBuilder();
         for (String s: str.split("\n")) {

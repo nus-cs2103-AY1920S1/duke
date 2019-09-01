5cbcb1188942cc8e707aacc664d910575336c3b8
charliechoong <e0202817@u.nus.edu>
2019-09-01 11:57:15 +0800

    Add JUnit tests for DateParser and Todo class methods

--- src/main/java/seedu/duke/parser/DateParser.java
+++ src/main/java/seedu/duke/parser/DateParser.java
@@ -13,5 +13,4 @@ public class DateParser {
             return null;
         }
     }
-
 }
--- src/main/java/seedu/duke/task/Deadline.java
+++ src/main/java/seedu/duke/task/Deadline.java
@@ -18,6 +18,7 @@ public class Deadline extends Task{
     public String toFile(){
         return "D | " + (isDone?"1":"0") + " | " + this.description + " | " + this.strBy + "\n";
     }
+
     @Override
     public String toString(){
         return "[D]" + super.toString() + " (by: " + strBy + ")";

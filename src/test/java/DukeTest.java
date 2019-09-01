4ee897f938ba31b4514bd2f992c9df2a2ca37a35
AHaliq <liqo12@hotmail.com>
2019-09-01 21:35:42 +0800

    made two test for Printer

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,10 +1,17 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import com.util.Printer;
 
 public class DukeTest {
+
     @Test
-    void placeHolder() {
-        assertEquals(2, 1 + 1);
+    void testPrinterSurroundQuotes() {
+        assertEquals("\"hi\"", Printer.surroundQuotes("hi"));
+    }
+
+    @Test
+    void testPrinterIndentString() {
+        assertEquals("    hi\n", Printer.indentString("hi"));
     }
 }

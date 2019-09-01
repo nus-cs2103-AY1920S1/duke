6ac7a95e2091b8786aa78dfc016e91bbac83d45a
choongyx <yongxin.choong98@gmail.com>
2019-09-02 02:15:34 +0800

    JavaFX Tutorial Part 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,13 @@
 
 import java.io.IOException;
 
-public class Duke {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+
+public class Duke extends Application{
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
@@ -20,6 +26,10 @@ public class Duke {
             ui.showError(e.getMessage());
             tasks = new TaskList();
         }
+    }
+
+    public Duke() {
+
     }
 
     public void run() throws IOException {
@@ -41,7 +51,16 @@ public class Duke {
         ui.exit();
     }
 
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+    }
+
     public static void main(String[] args) throws IOException {
-        new Duke("/data/tasks.txt").run();
+        new Duke().run();
     }
 }

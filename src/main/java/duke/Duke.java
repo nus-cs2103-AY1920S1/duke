1f88442ac4706df7b356b143e8de2054cb8b08cc
dorcastan <dorcas.tann@gmail.com>
2019-09-02 19:26:17 +0800

    Add: GUI layout skeleton
    Set elements in position for Duke GUI, according to JavaFX Tutorial
    Part 2.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -5,12 +5,13 @@ import duke.task.TaskList;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.FontWeight;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.control.Label;
-import javafx.scene.text.Font;
-import javafx.stage.StageStyle;
 
 /**
  * Main class for the Duke application.
@@ -20,6 +21,12 @@ public class Duke extends Application {
     private Storage storage;
     private TaskList tasks;
     private TextUi ui;
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
 
     /**
      * Provides a constructor with zero parameters for JavaFX Launcher
@@ -46,16 +53,64 @@ public class Duke extends Application {
     }
 
     /**
+     * Initialises and runs the Duke application.
+     *
+     * @param args  Standard arguments
+     */
+    public static void main(String[] args) {
+        new Duke("data/duke.txt").run();
+    }
+
+    /**
      * Sets the stage and scene for JavaFX, using the given Stage.
      *
      * @param stage     Stage to be set
      */
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Welcome to Duke!");
-        helloWorld.setFont(Font.font("serif", FontWeight.BOLD,
-                FontPosture.REGULAR, 50));
-        Scene scene = new Scene(helloWorld);
+        // Step 1: Set up required components
+
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button();
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        // Step 2: Format window
+
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
 
         stage.setScene(scene);
         stage.show();
@@ -87,12 +142,4 @@ public class Duke extends Application {
         }
     }
 
-    /**
-     * Initialises and runs the Duke application.
-     *
-     * @param args  Standard arguments
-     */
-    public static void main(String[] args) {
-        new Duke("data/duke.txt").run();
-    }
 }

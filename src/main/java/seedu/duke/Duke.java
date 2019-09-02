8f9af144a534a256c8478cdc68a233d8cc8d37f7
Lim Kang Yee <kangyee2408@gmail.com>
2019-09-02 08:50:38 +0800

    remove unnecessary code due to fxml.

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -19,136 +19,12 @@ import java.text.ParseException;
  * Loads task information from data file in hard drive when initialized
  * or creates file in hard drive if it does not exist.
  */
-public class Duke extends Application {
+public class Duke {
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
-
-    /**
-     * Class constructor that takes in a filepath.
-     * Creates a task list from  loading information from data file.
-     * It is able to create an empty TaskList if there is problems loading the data file or
-     * creating the data file.
-     *
-     * @param filePath String of file path to create data file or load data file from.
-     * @throws Exception If file cannot be created or loaded from correctly.
-     */
-    /*public Duke(String filePath) {
-        ui = new Ui();
-        storage = new Storage(filePath);
-        try {
-            tasks = new TaskList(storage.load());
-        } catch (Exception e) {
-            ui.showLoadingError();
-            tasks = new TaskList();
-        }
-    }*/
 
     public Duke() {}
-
-   @Override
-    public void start(Stage stage) {
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        // You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        /*sendButton.setOnMouseClicked((event) -> {
-            dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
-            userInput.clear();
-        });
-
-        userInput.setOnAction((event) -> {
-            dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
-            userInput.clear();
-        });*/
-
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-
-    }
-
-    /**
-     * Iteration 1:
-     * Creates a label with the specified text and adds it to the dialog container.
-     * @param text String containing text to add
-     * @return a label with the specified text that has word wrap enabled.
-     */
-    /*private Label getDialogLabel(String text) {
-        // You will need to import `javafx.scene.control.Label`.
-        Label textToAdd = new Label(text);
-        textToAdd.setWrapText(true);
-
-        return textToAdd;
-    }*/
-
-    /**
-     * Iteration 2:
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
-     */
-    private void handleUserInput() {
-        //Label userText = new Label(userInput.getText());
-        //Label dukeText = new Label(getResponse(userInput.getText()));
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userInput.getText(), user),
-                DialogBox.getDukeDialog(getResponse(userInput.getText()), duke)
-        );
-        userInput.clear();
-    }
 
     /**
      * You should have your own function to generate a response to user input.
@@ -174,36 +50,18 @@ public class Duke extends Application {
             ui.showLoadingError();
             tasks = new TaskList();
         }
-        //boolean isExit = false;
-        //while (!isExit) {
-            try {
-                String fullCommand = input;
-                        //ui.readCommand();
-                //ui.showLine();
-                Command c = Parser.parse(fullCommand, ui);
-                return c.execute(tasks, ui, storage);
-                //isExit = c.isExit();
-            } catch (DukeException e) {
-               return e.toString();
-            } catch (ParseException e) {
-                return ui.showParseError();
-            } catch (ArrayIndexOutOfBoundsException e) {
-                return ui.showParseError();
-            } catch (Exception e) {
-                return ui.showExceptionMsg(e);
-            } /*finally {
-                return ui.showLine();
-            }*/
-
-        //}
+        try {
+            String fullCommand = input;
+            Command c = Parser.parse(fullCommand, ui);
+            return c.execute(tasks, ui, storage);
+        } catch (DukeException e) {
+            return e.toString();
+        } catch (ParseException e) {
+            return ui.showParseError();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return ui.showParseError();
+        } catch (Exception e) {
+            return ui.showExceptionMsg(e);
+        }
     }
-
-    /**
-     * Drives duke class as main method.
-     *
-     * @param args String[]
-     */
-    /*public static void main(String[] args) {
-        new Duke("data/duke.txt").run();
-    }*/
 }

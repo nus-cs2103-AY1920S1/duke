9bd68d7190cc67cbf9883690784f32854e113715
e0031374 <e0031374@u.nus.edu>
2019-09-03 02:31:55 +0800

    Middle of GUI tutorial 3, just before the part on image and imageView with DaUser and DaDuke Profiles

---
+++ src/main/java/Main.java
@@ -0,0 +1,108 @@
+//made in L3 to reduce clutter in Duke
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label; //L3
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+
+    //GUI L3: Iteration1
+    /**
+     *  * Iteration 1:
+     *   * Creates a label with the specified text and adds it to the dialog container.
+     *    * @param text String containing text to add
+     *     * @return a label with the specified text that has word wrap enabled.
+     *      */
+    private Label getDialogLabel(String text) {
+        Label textToAdd = new Label(text);
+        textToAdd.setWrapText(true);
+
+        return textToAdd;
+    }
+
+
+    //added in GUI L1
+    @Override
+    public void start(Stage stage) {
+        // GUI L2 Step 1 setting up required componenets
+
+        // The container for the content of chat to scroll
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+
+        // L2 Step2
+        //
+
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        //L3 
+        sendButton.setOnMouseClicked((event) -> {
+            dialogContainer.getChildren()
+                .add(getDialogLabel(userInput.getText()));
+            userInput.clear();
+        });
+
+        userInput.setOnAction((event) -> {
+            dialogContainer.getChildren()
+                .add(getDialogLabel(userInput.getText()));
+            userInput.clear();
+        });
+
+        dialogContainer.heightProperty()
+            .addListener(
+                (observable) -> scrollPane.setVvalue(1.0)
+            );
+
+
+        stage.setScene(scene);
+        stage.show();
+    }
+}

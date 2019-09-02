f5597837364765a946b51ac25add16a0a69f24c2
weiboon <lumweiboon@gmail.com>
2019-09-02 11:02:19 +0800

    Level-10

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,162 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
 /**
  * Represents the main class of Duke program.
  */
 
-public class Duke {
-	private static final String taskListArchivalPath = "data/duke.txt";
+public class Duke extends Application {
+	private static final String taskListArchivalPath = "/Users/lumweiboon/Duke/src/main/java/data/duke.txt";
 	private Storage storage;
 	private TaskList tasks;
 	private Ui ui;
+	private ScrollPane scrollPane;
+	private VBox dialogContainer;
+	private TextField userInput;
+	private Button sendButton;
+	private Scene scene;
+
+	private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+	private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
+	@Override
+	public void start(Stage stage) {
+		//Step 1. Setting up required components
+
+		//The container for the content of the chat to scroll.
+		scrollPane = new ScrollPane();
+		dialogContainer = new VBox();
+		scrollPane.setContent(dialogContainer);
+
+		userInput = new TextField();
+		sendButton = new Button("Send");
+
+		AnchorPane mainLayout = new AnchorPane();
+		mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+		scene = new Scene(mainLayout);
+
+		stage.setScene(scene);
+		stage.show();
+
+		stage.setTitle("Duke");
+		stage.setResizable(false);
+		stage.setMinHeight(600.0);
+		stage.setMinWidth(400.0);
+
+		mainLayout.setPrefSize(400.0, 600.0);
+
+		scrollPane.setPrefSize(385, 535);
+		scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+		scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+		scrollPane.setVvalue(1.0);
+		scrollPane.setFitToWidth(true);
+
+		// You will need to import `javafx.scene.layout.Region` for this.
+		dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+		userInput.setPrefWidth(325.0);
+
+		sendButton.setPrefWidth(55.0);
+
+		AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+		AnchorPane.setBottomAnchor(sendButton, 1.0);
+		AnchorPane.setRightAnchor(sendButton, 1.0);
+
+		AnchorPane.setLeftAnchor(userInput , 1.0);
+		AnchorPane.setBottomAnchor(userInput, 1.0);
+
+		sendButton.setOnMouseClicked((event) -> {
+			dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
+			userInput.clear();
+		});
+
+		userInput.setOnAction((event) -> {
+			dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
+			userInput.clear();
+		});
+
+		dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+
+		//Part 3. Add functionality to handle user input.
+		sendButton.setOnMouseClicked((event) -> {
+			handleUserInput();
+		});
+
+		userInput.setOnAction((event) -> {
+			handleUserInput();
+		});
+	}
+
+	/**
+	 * Iteration 1:
+	 * Creates a label with the specified text and adds it to the dialog container.
+	 * @param text String containing text to add
+	 * @return a label with the specified text that has word wrap enabled.
+	 */
+	private Label getDialogLabel(String text) {
+		// You will need to import `javafx.scene.control.Label`.
+		Label textToAdd = new Label(text);
+		textToAdd.setWrapText(true);
+
+		return textToAdd;
+	}
+
+	/**
+	 * Iteration 2:
+	 * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+	 * the dialog container. Clears the user input after processing.
+	 */
+	private void handleUserInput() {
+		Label userText = new Label(userInput.getText());
+		Label dukeText = new Label(getResponse(userInput.getText()));
+		dialogContainer.getChildren().addAll(
+			DialogBox.getUserDialog(userText.getText(), new ImageView(user).getImage()),
+			DialogBox.getDukeDialog(dukeText.getText(), new ImageView(duke).getImage())
+		);
+		userInput.clear();
+	}
+
+	/**
+	 * You should have your own function to generate a response to user input.
+	 * Replace this stub with your completed method.
+	 */
+	public String getResponse(String input) {
+		// need to return string here from UI
+		try {
+			Command c = Parser.parse(input);
+			c.execute(tasks, ui, storage);
+		} catch (DukeException ex) {
+			ui.showError(ex.getMessage());
+		}
+		return ui.getOutput();
+	}
 
 	/**
 	 * Constructor of Duke class. Initializes Storage, Ui and TaskList.
-	 * @param filePath the string representation of the path of the archival file. The file
-	 * 	 *  *                 this is used to load previous tasks. When the program is terminated
-	 * 	 *  *                 tasks are also updated into this file.
 	 */
-	public Duke(String filePath) {
+	public Duke() {
 		ui = new Ui();
-		storage = new Storage(filePath);
+		storage = new Storage(taskListArchivalPath);
 		try {
 			tasks = new TaskList(storage.load());
 		} catch (DukeException ex) {
@@ -51,7 +191,7 @@ public class Duke {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		new Duke(taskListArchivalPath).run();
+		new Duke().run();
 	}
 }
 
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -6,12 +6,14 @@ import java.util.Scanner;
  */
 public class Ui {
 	private Scanner scanner;
+	private StringBuilder outputHolder;
 
 	/**
 	 * Default constructor for UI, initializes a scanner object to read user inputs.
 	 */
 	public Ui() {
 		scanner = new Scanner(System.in);
+		outputHolder = new StringBuilder();
 	}
 
 	/**
@@ -19,14 +21,14 @@ public class Ui {
 	 * @param ex exception thrown from loading tasks from the archival file
 	 */
 	public void showLoadingErrors(Exception ex) {
-		System.out.println(ex.getMessage());
+		this.outputHolder.append(ex.getMessage());
 	}
 
 	/**
 	 * show the welcome message to the user
 	 */
 	public void showWelcome() {
-		System.out.println("Welcome to Duke. What can I do for you today?");
+		this.outputHolder.append("Welcome to Duke. What can I do for you today?");
 	}
 
 	/**
@@ -34,14 +36,14 @@ public class Ui {
 	 * @param toShow the string to be displayed to the user
 	 */
 	public void displayLine(String toShow) {
-		System.out.println(toShow);
+		this.outputHolder.append(toShow);
 	}
 
 	/**
 	 * simply display a break line to organise the outputs
 	 */
 	public void showBreakLine() {
-		System.out.println("------------------------------------------------------------------------------");
+		this.outputHolder.append("------------------------------------------------------------------------------");
 	}
 
 	/**
@@ -56,7 +58,7 @@ public class Ui {
 	 * show the exit message for duke. This method is run when the user closes the program through the bye command
 	 */
 	public void showExitMessage() {
-		System.out.println("Bye. Hope to see you again soon!");
+		this.outputHolder.append("Bye. Hope to see you again soon!");
 	}
 
 	/**
@@ -64,7 +66,7 @@ public class Ui {
 	 * @param errorMessage a string representation of the error thrown from Duke
 	 */
 	public void showError(String errorMessage) {
-		System.out.println(errorMessage);
+		this.outputHolder.append(errorMessage);
 	}
 
 	/**
@@ -73,9 +75,19 @@ public class Ui {
 	 * @param taskList the taskList representing all tasks added. Required to calculate the total number of tasks in
 	 *                 memory
 	 */
-	public static void alertLatestTaskAdded(Task task, TaskList taskList) {
-		System.out.println("Got it. I've added this task: ");
-		System.out.println(task);
-		System.out.printf("Now you have %d tasks in the list.\n", taskList.getTaskListSize());
+	public void alertLatestTaskAdded(Task task, TaskList taskList) {
+		this.outputHolder.append("Got it. I've added this task: \n");
+		this.outputHolder.append(task + "\n");
+		this.outputHolder.append(String.format("Now you have %d tasks in the list.\n", taskList.getTaskListSize()));
+	}
+
+	public String getOutput() {
+		String output =  this.outputHolder.toString();
+		resetUI();
+		return output;
+	}
+
+	public void resetUI() {
+		this.outputHolder = new StringBuilder();
 	}
 }

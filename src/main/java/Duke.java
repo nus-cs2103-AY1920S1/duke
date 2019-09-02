3eb1ada8091cc865369fea8e91aad603d7db49f0
choongyx <yongxin.choong98@gmail.com>
2019-09-02 11:59:23 +0800

    JavaFX Tutorial 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,21 +3,19 @@
  * @author Choong Yong Xin
  */
 
-import java.io.IOException;
-
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.layout.Region;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+
+import java.io.IOException;
 
 public class Duke extends Application {
     private Storage storage;

0d377daabdd6cbb99902cbdc963dba6b17ff32b5
dawo5010 <me@dawo.me>
2019-09-02 22:07:16 +0800

    Finished Part 2 of JavaFX Tutorial.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,8 +7,13 @@ import duke.command.TaskList;
 import duke.command.Ui;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.layout.Region;
 
 /**
  * Duke class. Creates a new Ui, TaskList and Storage object, then runs the main method of the program.
@@ -20,7 +25,13 @@ public class Duke extends Application{
     /** Stores the Storage object used to read/write from file. */
     private Storage storage = new Storage("data/duke.txt");;
     /** Stores the TaskList object used to add/delete tasks. */
-    private TaskList taskList = new TaskList(storage);;
+    private TaskList taskList = new TaskList(storage);
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
 
     /**
      * This method runs the Duke chat bot. In particular, it prints the intro, and determines which command should
@@ -99,11 +110,48 @@ public class Duke extends Application{
 
     @Override
     public void start (Stage stage) {
-        Label helloWorld = new Label("Hello World!");
-        Scene scene = new Scene(helloWorld);
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
 
         stage.setScene(scene);
         stage.show();
+
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400, 600);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
     }
 
     /**

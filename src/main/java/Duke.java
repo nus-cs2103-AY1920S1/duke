d9802deb4805120feeb21e4b31b93fba01135ad8
nishcafe <46063774+nishcafe@users.noreply.github.com>
2019-09-02 15:03:01 +0800

    Level 10

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,8 +6,13 @@ import tool.Ui;
 
 import java.util.*;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class Duke {
+
+public class Duke extends Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Storage storage = new Storage("src/main/java/data/duke.txt");
@@ -24,5 +29,14 @@ public class Duke {
             }
 
         }
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }

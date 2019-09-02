651d5f00a1616cd15a743999ffcfbfbb72456478
Gihun Ko <rhrlgns96@gmail.com>
2019-09-02 19:51:52 +0800

    change javafx version from 12.0.2 to 12. implement javafx tut2.

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -2,9 +2,13 @@ package seedu.duke;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 import seedu.duke.core.Command;
 import seedu.duke.core.DukeController;
 import seedu.duke.core.Storage;
@@ -17,6 +21,7 @@ import java.text.ParseException;
 import java.util.List;
 import java.util.Scanner;
 
+//Proper JavaFX implementation needed.
 public class Duke extends Application {
     private static String DIRECTORY_PATH = "D:/project/CS2103T/duke/data";
     private static String FILEPATH = DIRECTORY_PATH + "/duke.txt";
@@ -44,9 +49,45 @@ public class Duke extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
-        Label helloWorld = new Label("Hello World!");
-        Scene scene = new Scene(helloWorld);
+    public void start(Stage stage) {
+        ScrollPane scrollPane = new ScrollPane();
+        VBox dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+        TextField userInput = new TextField();
+        Button sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        stage.setTitle("seedu.duke.Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(395, 568.0);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        Scene scene = new Scene(mainLayout);
 
         stage.setScene(scene);
         stage.show();

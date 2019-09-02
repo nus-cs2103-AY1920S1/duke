079e506ccbd938df5435c88dd2953e82dfaabb47
alxkohh <alxkohh@gmail.com>
2019-09-02 20:44:35 +0800

    Add GUI tutorial 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,170 +10,70 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.layout.Region;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+
 
 /**
  * Driver class which contains the main method of the program. Also encapsulates Duke,
  * a to-do list. Each Duke object has a storage file, a list of tasks, and a user
  * interface.
  */
-public class Duke extends Application {
+public class Duke {
 
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Storage storage;
+    private TaskList tasks;
+    private Ui ui;
 
-//    private Storage storage;
-//    private TaskList tasks;
-//    private Ui ui;
-//
-//    /**
-//     * Constructor.
-//     *
-//     * @param filePath file path of text file used to load and store tasks.
-//     */
-//    public Duke(String filePath) {
-//        ui = new Ui();
-//        storage = new Storage(filePath);
-//        try {
-//            tasks = new TaskList(storage.load());
-//        } catch (DukeException e) {
-//            ui.showLoadingError();
-//            tasks = new TaskList();
-//        }
-//    }
-//
-//    /**
-//     * Runs the application. A scanner is instantiated to read standard input.
-//     */
-//    public void run() {
-//        ui.showWelcome();
-//        Scanner sc = new Scanner(System.in);
-//        while (sc.hasNextLine()) {
-//            try {
-//                String input = sc.nextLine();
-//                Command c = Parser.parse(input);
-//                c.execute(tasks, ui, storage);
-//            } catch (DukeException e) {
-//                ui.showError(e.getMessage());
-//            }
-//        }
-//    }
-//
-//    /**
-//     * Main method, the program's entry point. A Duke object is instantiated to call run(), an instance method.
-//     *
-//     * @param args an array of command-line arguments for the application
-//     */
-//    public static void main(String[] args) {
-//        File file = new File("data");
-//        file.mkdir();
-//        new Duke("data/tasks.txt").run();
-//    }
-
-    @Override
-    public void start(Stage stage) {
-        //Step 1. Setting up required components
-
-        //The container for the content of the chat to scroll.
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        //Step 2. Formatting the window to look as expected
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        // You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        //Step 3. Add functionality to handle user input.
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-
-        //Scroll down to the end every time dialogContainer's height changes.
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+    /**
+     * Constructor.
+     *
+     * @param filePath file path of text file used to load and store tasks.
+     */
+    public Duke(String filePath) {
+        ui = new Ui();
+        storage = new Storage(filePath);
+        try {
+            tasks = new TaskList(storage.load());
+        } catch (DukeException e) {
+            ui.showLoadingError();
+            tasks = new TaskList();
+        }
     }
 
     /**
-     * Iteration 1:
-     * Creates a label with the specified text and adds it to the dialog container.
-     * @param text String containing text to add
-     * @return a label with the specified text that has word wrap enabled.
+     * Runs the application. A scanner is instantiated to read standard input.
      */
-    private Label getDialogLabel(String text) {
-        // You will need to import `javafx.scene.control.Label`.
-        Label textToAdd = new Label(text);
-        textToAdd.setWrapText(true);
-
-        return textToAdd;
+    public void run() {
+        ui.showWelcome();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            try {
+                String input = sc.nextLine();
+                Command c = Parser.parse(input);
+                c.execute(tasks, ui, storage);
+            } catch (DukeException e) {
+                ui.showError(e.getMessage());
+            }
+        }
     }
 
     /**
-     * Iteration 2:
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
+     * Main method, the program's entry point. A Duke object is instantiated to call run(), an instance method.
+     *
+     * @param args an array of command-line arguments for the application
      */
-    private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(getResponse(userInput.getText()));
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-        );
-        userInput.clear();
+    public static void main(String[] args) {
+        File file = new File("data");
+        file.mkdir();
+        new Duke("data/tasks.txt").run();
     }
 
     /**
      * You should have your own function to generate a response to user input.
      * Replace this stub with your completed method.
      */
-    private String getResponse(String input) {
+
+     public String getResponse(String input) {
         return "Duke heard: " + input;
     }
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+
+
+/**
+ * A GUI for Duke using FXML.
+ */
+public class Main extends Application {
+
+    private Duke duke = new Duke("data/tasks.txt");
+
+    @Override
+    public void start(Stage stage) {
+        /*File file = new File("data");
+        file.mkdir();
+        duke.run();*/
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setDuke(duke);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

6f75b910805bc11136e9ddfd4ddefe2fa9d3a7cc
TanZhanMing <e0349703@u.nus.edu>
2019-09-03 16:42:45 +0800

    Create a base template for GUI. However, something went wrong and main class Duke could not be found.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,47 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
 import java.io.IOException;
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * The main class for Duke.
  */
 
-public class Duke {
+public class Duke extends Application {
+
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
+    static Ui ui = new Ui();
+    static ArrayList<Task> taskList = new ArrayList<>(100);
+    static InputParser inputParser = new InputParser(taskList);
+    static Scanner scan = new Scanner(System.in);
+    static String input = "";
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+    }
+
+    public String getResponse(String input) throws IOException {
+        while (!input.equals("bye")) {
+            input = scan.nextLine();
+            inputParser.determineAction(input);
+        }
+        return null;
+    }
+
+    //public String getResponse(String input) {
+    //}
 
     public enum Action {
         ADD,
@@ -16,12 +51,6 @@ public class Duke {
     }
 
     public static void main(String[] args) throws IOException {
-
-        Ui ui = new Ui();
-        ArrayList<Task> taskList = new ArrayList<>(100);
-        InputParser inputParser = new InputParser(taskList);
-        Scanner scan = new Scanner(System.in);
-        String input = "";
 
         try {
             FileReading.checkFileExists(taskList);

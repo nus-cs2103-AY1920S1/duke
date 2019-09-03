c2353fef59fc93b70fdd6fc72cd55f60d81a10d6
ckb055 <chenkaibin7@gmail.com>
2019-09-03 19:30:11 +0800

    Gradle, and Level-10

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,16 @@
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.File;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * Duke, a personal chat bot assistant that helps you manage your tasks
@@ -12,11 +19,44 @@ import java.io.File;
  * @version 1.0
  * @since 19-08-2019
  */
-public class Duke {
+public class Duke extends Application {
 
+    /**
+     * JavaFX tools
+     */
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    /**
+     * Duke tools
+     */
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
+
+    /**
+     * Images for the user and Duke
+     */
+
+    private Image user = new Image(this.getClass().getResourceAsStream
+            ("/images/DaUser.png"));
+    private Image duke = new Image(this.getClass().getResourceAsStream
+            ("/images/DaDuke.png"));
+
+    /**
+     * Empty constructor.
+     */
+    public Duke() {
+
+    }
+
+    /**
+     * Contructs a duke object that accesses the file from the specified filepath
+     * @param filePath of the TaskList
+     */
 
     public Duke(String filePath) {
         ui = new Ui();
@@ -28,6 +68,7 @@ public class Duke {
             tasks = new TaskList();
         }
     }
+
 
     public void run() {
         ui.showWelcome();
@@ -47,7 +88,100 @@ public class Duke {
         }
     }
 
+    @Override
+    public void start(Stage stage) {
+        //Step 1. Setting up required components
+
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+    //Step 2. Formatting the window to look as expected
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        // You will need to import `javafx.scene.layout.Region` for this.
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        //Part 3. Add functionality to handle user input.
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput();
+        });
+
+        userInput.setOnAction((event) -> {
+            handleUserInput();
+        });
+
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+    }
+
+    private Label getDialogLabel(String text) {
+        // You will need to import `javafx.scene.control.Label`.
+        Label textToAdd = new Label(text);
+        textToAdd.setWrapText(true);
+
+        return textToAdd;
+    }
+
+    /**
+     * Main method to run Duke
+     * @param args the filepath of the tasklist in the harddisk
+     */
     public static void main(String[] args) {
         new Duke("C:\\repos\\duke\\out\\data\\TaskList.txt").run();
     }
+
+
+    /**
+     * To handle user input
+     */
+    private void handleUserInput() {
+        Label userText = new Label(userInput.getText());
+        Label dukeText = new Label(getResponse(userInput.getText()));
+        dialogContainer.getChildren().addAll(
+                new DialogBox(userText, new ImageView(user)),
+                new DialogBox(dukeText, new ImageView(duke))
+        );
+        userInput.clear();
+    }
+
+    // Duke's response, customisable
+    private String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
+
 }

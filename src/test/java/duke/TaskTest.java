6035487a144fc14b7a6eaa90470a15414fa7266b
qiujiaaa <ooiqiujia@hotmail.com>
2019-09-04 00:43:39 +0800

    update test cases and some javadoc comments

--- src/test/java/duke/TaskTest.java
+++ src/test/java/duke/TaskTest.java
@@ -9,11 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TaskTest {
 
     @Test
-    public void test_constructor() {
-        assertEquals("[T][" + "✓" + "] do tutorials", (new Todo("do tutorials")).toString());
-    }
-
-    @Test
     public void test_markAsDone() {
         Todo todo = new Todo("do work");
         todo.markAsDone();
@@ -24,11 +19,5 @@ public class TaskTest {
     public void test_getDescription() {
         Todo todo = new Todo("do work");
         assertEquals("do work", todo.getDescription());
-    }
-
-    @Test
-    public void test_getStatusIcon() {
-        Todo todo = new Todo("do work");
-        assertEquals("✗", todo.getStatusIcon());
     }
 }

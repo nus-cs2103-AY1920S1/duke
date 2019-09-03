aebc73b112707304e37fda2778f187e253816665
envere <keithtxhh@gmail.com>
2019-09-04 00:05:25 +0800

    separated command import statements

--- src/main/java/duke/util/Parser.java
+++ src/main/java/duke/util/Parser.java
@@ -1,6 +1,11 @@
 package duke.util;
 
-import duke.command.*;
+import duke.command.Command;
+import duke.command.DeleteCommand;
+import duke.command.DoneCommand;
+import duke.command.ListCommand;
+import duke.command.ExitCommand;
+import duke.command.AddCommand;
 import duke.exception.DukeException;
 
 /**

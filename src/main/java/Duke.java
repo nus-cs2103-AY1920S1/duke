e0b0d09fb5037fd00844dbf2ba69adceca5e5fbb
MistyRainforest <adamtan015@gmail.com>
2019-09-03 20:01:32 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,46 @@
-public class Duke {
+import java.util.Scanner;
+
+public class Duke{
     /**
      * Main driver class for Duke.
      *
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        dukePrint("Hello! I'm Duke", "What can I do for you?");
+        Scanner consoleScanner = new Scanner(System.in);
+        String userInput = "";
+        while (!userInput.equals("exit")) {
+            userInput = consoleScanner.next();
+            if (!userInput.equals("exit")) {
+                echo(userInput);
+            }
+        }
+
+        dukePrint("Bye. Hope to see you again soon!");
     }
+
+    /**
+     * Echos string.
+     * @param echoedString targeted String to be echoed
+     */
+    private static void echo(String echoedString) {
+        dukePrint(echoedString);
+    }
+
+    /**
+     * Prints what Duke says in correct format.
+     * @param texts any number of String arguments
+     *              each prints on a new line.
+     */
+
+    private static void dukePrint(String... texts) {
+        System.out.println("    _____________________________");
+        for (String text : texts) {
+            System.out.println("    " + text);
+        }
+        System.out.println("    _____________________________");
+
+    }
+
+
 }

26d0001a5dc91c07824161353098169c5f660847
Lawnce Goh <lawnce@gmail.com>
2019-09-03 19:50:26 +0800

    Level-10. GUI

--- src/main/test/duke/initials/EventTest.java
+++ src/main/test/duke/initials/EventTest.java
@@ -1,13 +1,14 @@
 package duke.initials;
 
 import org.junit.jupiter.api.Test;
+import duke.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EventTest {
 
     @Test
-    void getData() {
+    void testGetData() {
         Event event = new Event("read book", "18th of February 2019, 6:00pm");
         assertEquals("E|0| read book| 18th of February 2019, 6:00pm", event.getData());
     }

b0650b44d4a369069ba66a0f6cfba32b45096f79
ChengGeng97 <47176493+ChengGeng97@users.noreply.github.com>
2019-09-03 19:37:43 +0800

    Implement JavaFX - Followed the tutorials. - The cute animals are placeholders. Don't get used to them.

--- src/test/java/TextToTaskTranslatorTest.java
+++ src/test/java/TextToTaskTranslatorTest.java
@@ -1,7 +1,8 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class TextToTaskTranslatorTest {
-
+    
 }

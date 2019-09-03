f6c9890b2616f7215dd1b087510f65ac2cf2317b
cambrian-dk <daryl.kohyikai@gmail.com>
2019-09-03 23:56:02 +0800

    add initial code for A-JUnit and A-Gradle

---
+++ src/test/java/duke/test/TodoTest.java
@@ -0,0 +1,10 @@
+package duke.test;
+
+import duke.task.Todo;
+public class TodoTest{
+    public static void main(String[] args)throws Exception{
+
+        //test setup
+        Todo t = new Todo("Test");
+    }
+}

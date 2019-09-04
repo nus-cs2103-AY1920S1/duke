ec0d29e473a86361e47edbda3fd9eb4658853e31
Chow Jia Ying <chowjiaying211@gmail.com>
2019-09-04 08:58:17 +0800

    Import DateTime class to the DateTimeTest class
    The change is needed because DateTime is now packaged.

--- src/test/java/DateTimeTest.java
+++ src/test/java/DateTimeTest.java
@@ -1,4 +1,6 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import duke.task.DateTime;
 import org.junit.jupiter.api.Test;
 
 class DateTimeTest {

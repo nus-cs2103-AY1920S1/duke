c3673dec1849c0c33eba06a06738300c8de097f3
CowSaysBaa <mememe479@gmail.com>
2019-09-04 21:05:47 +0800

    Updated test cases to actually work now

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,17 +1,28 @@
+import main.DateTime;
+import main.DukeException;
+import task.Deadlines;
 import org.junit.jupiter.api.Test;
-
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DukeTest {
     @Test
     public void testDateTime(){
-        assertEquals("10/10/2001 1000", new DateTime("10/10/2001 1000").toString());
+        try {
+            assertEquals("10/10/2001 1000", new DateTime("10/10/2001 1000").toString());
+        } catch (DukeException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Test
-    public void testUi(){
-        assertEquals("hi", new Ui().printMessage("hi"));
+    public void testDeadlines(){
+        try {
+            assertEquals("[D][\u2713] test (by: 10/10/1000 1000)", new Deadlines("test",
+                    new DateTime("10/10/1000 1000"), true).toString());
+        } catch (DukeException e){
+            System.out.println(e.getMessage());
+        }
     }
 
     @Test

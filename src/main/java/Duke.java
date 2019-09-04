5131533e9b345a731c10fa6bbfa4a73bc16bba03
Karan Dev Sapra <>
2019-09-04 18:43:33 +0800

    Iteration 2 – Adding Dialog Boxes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,37 +1,14 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Region;
-import javafx.stage.Stage;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
 
 /**
  * Class where the main logic is executed
  */
-public class Duke extends Application {
+public class Duke {
 
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
-
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-
-
-    public Duke() {
-        super();
-    }
 
     /**
      * Intialises all the classes needed to run this Application.
@@ -59,7 +36,7 @@ public class Duke extends Application {
     /**
      * Runs the main logic of the application
      */
-    public void run() {
+    public void run (String input) {
         ui.greeting();
         boolean isExit = false;
         while (!isExit) {
@@ -84,97 +61,12 @@ public class Duke extends Application {
      * @throws IOException
      */
 
-
+    /*
     public static void main(String[] args) throws DukeException, IOException {
 
         Application.launch(Duke.class, args);
         //new Duke("../data/duke.txt", "../data").run();
-    }
+    }*/
 
-    /**
-     * This is the primary stage that JavaFX provides.
-     * @param stage Primary Stage
-     * @throws Exception
-     */
-    @Override
-    public void start(Stage stage) throws Exception {
-
-        //Step 1. Setting up required components
-
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send!");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        //Step 2. Formatting the window to look as expected
-
-        stage.setTitle("My name is Duke!");
-
-        stage.setResizable(false);
-        stage.setMinHeight(600);
-        stage.setMinWidth(400);
-
-        mainLayout.setPrefSize(400, 600);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325);
-        sendButton.setPrefWidth(55);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setBottomAnchor(userInput,1.0);
-        AnchorPane.setLeftAnchor(userInput, 1.0);
-
-        //Step 3. Add functionality to handle user input.
-        sendButton.setOnMouseClicked(mouseEvent -> {
-            dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
-            userInput.clear();
-        });
-
-        userInput.setOnAction((event) -> {
-            dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
-            userInput.clear();
-        });
-
-        //Scroll down to the end every time dialogContainer's height changes.
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-
-
-    }
-
-    /**
-     * Iteration 1:
-     * Creates a label with the specified text and adds it to the dialog container.
-     * @param text String containing text to add
-     * @return a label with the specified text that has word wrap enabled.
-     */
-    private Label getDialogLabel (String text) {
-
-        Label textToAdd = new Label(text);
-        textToAdd.setWrapText(true);
-
-        return textToAdd;
-    }
 
 }
---
+++ src/main/java/Launcher.java
@@ -0,0 +1,10 @@
+import javafx.application.Application;
+
+public class Launcher {
+
+    public static void main(String[] args) throws DukeException {
+
+        Application.launch(FX.class, args);
+    }
+
+}

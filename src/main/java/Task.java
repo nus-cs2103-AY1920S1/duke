94d66b1daacd44a801aefa282461bce2fd8b85ec
ngswbryan <46245100+ngswbryan@users.noreply.github.com>
2019-09-04 09:00:23 +0800

    Coding Standards updated

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -10,8 +10,7 @@ public abstract class Task {
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols 
     }
-
-
+    
     public void markAsDone() {
         this.isDone = true;
     }

326a7e2a9df7e3c1bb0e85ddfb208110672ddf46
hello <linmeian@gmail.com>
2019-09-04 23:46:49 +0800

    Update Duke.java
    taken from suggested approach from cs2103 website

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,96 +1,42 @@
-import java.util.Scanner;
-import java.util.ArrayList;
-
 public class Duke {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Parser p = new Parser();
-        TaskList listOfTasks = new TaskList();
-        SaveToFile sf = new SaveToFile();
-        
+    private Storage storage;
+    private TaskList tasks;
+    private Ui ui;
 
-
-        while(sc.hasNext()) {
-            String nextCmd = sc.nextLine();
-            ArrayList<String> descriptions = p.parse(nextCmd);
-            ArrayList<String> taskDescriptions = new ArrayList<>();
-            String keyword = descriptions.get(0);
-
-            if(keyword.equals("bye")) {
-                System.out.println("Bye! Hope to see you again!");
-                break;
-            }
-
-            switch (keyword) {
-                case "list":
-                for(int i = 1; i <= listOfTasks.size(); i++) {
-                    System.out.println(i + ". " + listOfTasks.getTask(i - 1));
-                }
-                break;
-
-                case "done":
-                int indexOfTask = Integer.parseInt(descriptions.get(1));
-                listOfTasks.getTask(indexOfTask - 1).finishTask();
-                System.out.println("Task status updated.");
-                sf.updateFile(listOfTasks.getTaskList());
-                break;
-
-                case "delete":
-                int indexOfTask2 = Integer.parseInt(descriptions.get(1));
-                listOfTasks.deleteTask(indexOfTask2 - 1);
-                System.out.println("List of tasks updated.");
-                sf.updateFile(listOfTasks.getTaskList());
-                break;
-
-                case "todo":
-                try {
-                    taskDescriptions.add(descriptions.get(0));
-                    taskDescriptions.add(descriptions.get(1));
-                    Tasks todo = new Tasks(taskDescriptions);
-                    listOfTasks.addTask(todo);
-                    sf.updateFile(listOfTasks.getTaskList());
-                    System.out.println("Got it, todo added to tasks");
-                } catch(IndexOutOfBoundsException e) {
-                    System.out.println("OOPS!! The description for todo cannot be empty!");
-                }  
-                break;
-
-                case "deadline":
-                try {
-                    taskDescriptions.add(descriptions.get(0));
-                    taskDescriptions.add(descriptions.get(1));
-                    taskDescriptions.add(descriptions.get(2));
-                    Tasks deadline = new Tasks(taskDescriptions);
-                    listOfTasks.addTask(deadline);
-                    sf.updateFile(listOfTasks.getTaskList());
-                    System.out.println("Got it, new deadline added to list");
-                } catch(IndexOutOfBoundsException e) {
-                    System.out.println("OOPS!! I'm sorry, but I don't know what that means :-(");
-                }  
-                break;
-
-                case "event":
-                try {
-                    taskDescriptions.add(descriptions.get(0));
-                    taskDescriptions.add(descriptions.get(1));
-                    taskDescriptions.add(descriptions.get(2));
-                    Tasks event = new Tasks(taskDescriptions);
-                    listOfTasks.addTask(event);
-                    sf.updateFile(listOfTasks.getTaskList());
-                    System.out.println("Got it, new event added to schedule");
-                } catch(IndexOutOfBoundsException e) {
-                    System.out.println("OOPS!! I'm sorry, but I don't know what that means :-(");
-                } 
-                break;
-
-                default:
-                System.out.println("OOPS!! I'm sorry, but I don't know what that means :-("); 
-                
-            }
-
-
-            
+    public Duke(String filePath) {
+        ui = new Ui();
+        storage = new Storage(filePath);
+        try {
+            tasks = new TaskList(storage.load());
+        } catch (DukeException e) {
+            ui.showLoadingError();
+            tasks = new TaskList();
         }
     }
+
+    public void run() {
+        ui.showWelcome();
+        boolean isExit = false;
+        while (!isExit) {
+            try {
+                String fullCommand = ui.readCommand();
+                ui.showLine(); // show the divider line ("_______")
+                Command c = Parser.parse(fullCommand);
+                c.execute(tasks, ui, storage);
+                isExit = c.isExit();
+            } catch (DukeException e) {
+                ui.showError(e.getMessage());
+            } finally {
+                ui.showLine();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        new Duke("data/tasks.txt").run();
+    }
+
+
 }

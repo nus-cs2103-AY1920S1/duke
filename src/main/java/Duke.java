3a33f95d35d8ce70fc6ebe9ad7cb75d7395f4880
olivercheok20 <olivercheok20@gmail.com>
2019-09-04 16:56:54 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // Create a scanner to take in user input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(
+                formatMessage("Hello, I'm Duke\nWhat can I do for you?")
+        );
+
+        String command = sc.nextLine();
+
+        while (!command.equals("bye")) {
+            System.out.println(
+                    formatMessage(command)
+            );
+            command = sc.nextLine();
+        }
+
+        System.out.println(
+                formatMessage("Bye. Hope to see you again soon!")
+        );
+    }
+
+    private static String formatMessage(String message) {
+        String formatted = "    ____________________________________________________________\n";
+        String[] lines = message.split("\n");
+        for (String line : lines) {
+            formatted += "     " + line + "\n";
+        }
+        formatted += "    ____________________________________________________________\n";
+
+        return formatted;
     }
 }

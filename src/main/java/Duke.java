88723afbe406c661a18a98280d768a2922d730f9
Wang Rong Shirley <e0035380@u.nus.edu>
2019-09-04 12:55:08 +0800

    added level 3, added Task.java to represent Task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,7 +8,7 @@ public class Duke {
         final String TAB = "    ";
         String cmd;
         Scanner scan = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<Task> list = new ArrayList<Task>();
 
         /*
         String logo = " ____        _        \n"
@@ -24,20 +24,44 @@ public class Duke {
         printDuke("What can I do for you?");
         printDuke(LINE);
 
-        while(true) {
+        Boolean isNotBye = true;
+        while(isNotBye) {
             cmd = scan.nextLine();
-            if (cmd.equals("bye")) {
-                printDuke(LINE);
-                printDuke("Bye. Hope to see you again soon!");
-                printDuke(LINE);
-                break;
-            }
-            if (cmd.equals("list")) {
-                printDuke(LINE);
-                printDuke(list);
-                printDuke(LINE);
-            } else {
-                list.add(cmd);
+            String arr[] = cmd.split(" ", 2);
+            String firstWord = arr[0];
+
+            switch (firstWord) {
+                case "bye":
+                    printDuke(LINE);
+                    printDuke("Bye. Hope to see you again soon!");
+                    printDuke(LINE);
+                    isNotBye = false;
+                    break;
+                case "list":
+                    printDuke(LINE);
+                    System.out.println("    Here are the tasks in your list:");
+                    if (list.isEmpty()) {
+                        break;
+                    }
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.println("    " + (i + 1) + ". [" + list.get(i).getStatusIcon() + "]"  + list.get(i).getDesc());
+                    }
+
+                    printDuke(LINE);
+                    break;
+                case "done":
+                    printDuke(LINE);
+                    int taskNum = Integer.valueOf(arr[1]);
+                    list.get(taskNum - 1).isDone(true);
+                    printDuke("Nice! I've marked this task as done");
+                    System.out.println("    " + taskNum + ". [" + list.get(taskNum - 1).getStatusIcon() + "]"
+                                        + list.get(taskNum - 1).getDesc());
+                    //printDuke();
+                    printDuke(LINE);
+                    break;
+                default:
+                Task t = new Task(cmd);
+                list.add(t);
                 printDuke(LINE);
                 System.out.println(TAB + "added: " + cmd);
                 printDuke(LINE);
@@ -47,11 +71,5 @@ public class Duke {
 
     public static void printDuke(String toPrint) {
         System.out.println("    " + toPrint);
-    }
-
-    public static void printDuke(ArrayList toPrint) {
-        for (int i = 1; i <= toPrint.size(); i++) {
-            System.out.println("    " + i + ". " + toPrint.get(i - 1));
-        }
     }
 }

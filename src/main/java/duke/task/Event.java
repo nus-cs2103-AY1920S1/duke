9d74b4e7068178948f7e3fd12f3a3736638a23bc
ambervoong <voongyuxuan@gmail.com>
2019-09-04 11:18:32 +0800

    Implement A-Varargs in the Event class.

--- src/main/java/duke/task/Event.java
+++ src/main/java/duke/task/Event.java
@@ -6,10 +6,15 @@ public class Event extends Task {
     private String dateTime;
 
     // Constructor
-
     public Event(String description, String dateTime) {
         super(description);
         this.dateTime = dateTime;
+    }
+
+    // Trying out using varargs.
+    public Event(String ... strings) {
+        super(strings[0]);
+        this.dateTime = strings[1];
     }
 
     @Override

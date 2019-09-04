31a0bc624dadc7ad4770061d9c32f28ad63b0a0f
hello <linmeian@gmail.com>
2019-09-04 23:46:04 +0800

    Update Parser.java
    edited parser to remove all the logic parts and shift into command

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,53 +1,14 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Parser {
-    private String inputString;
 
+    public Command parse(String fullCmd) {
+        Scanner sc = new Scanner(fullCmd);
+        String cmd = sc.next().trim();
+        String remaining = sc.nextLine().trim();
 
-    protected ArrayList<String> parse(String s) {
-        ArrayList<String> output = new ArrayList<String>();
-        String[] separated = s.split(" ");
-        if(separated.length == 1) {
-            output.add(separated[0]);
-            return output;
-        } else if(separated.length == 2) {
-            output.add(separated[0]);
-            output.add(separated[1]);
-            return output;
-        } else {
-            int index = 1;
-            StringBuilder sb = new StringBuilder();
-            output.add(separated[0]);
-            while(index < separated.length) {
-                if(!separated[index].equals("/by")) {
-                    if(!separated[index].equals("/at")) {
-                        sb.append(" ");
-                        sb.append(separated[index]);
-                        index++;
-                    } else {
-                        break;
-                    }
-                } else {
-                    break;
-                }
-            }
-            output.add(sb.toString());
-            sb.setLength(0);
-            while(index < separated.length) {
-                if(index != separated.length - 1) {
-                    sb.append(separated[index]);
-                    sb.append(" ");
-                    index++;
-                } else {
-                    sb.append(separated[index]);  
-                    index++;
-                }
-            }
-            if(sb.length() != 0) {
-                output.add(sb.toString());
-            }
-        }
-        return output;
-    } 
+        return new Command(cmd, remaining);
+    }
+
 
 }

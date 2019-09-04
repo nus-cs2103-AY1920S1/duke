dc58e7705e750ef4a1af0d3aa4af3939743d8d7c
czkay <chong.zi.kang@gmail.com>
2019-09-04 15:53:16 +0800

    Fix syntax error

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -51,7 +51,7 @@ public class MainWindow extends AnchorPane {
      * the dialog container. Clears the user input after processing.
      */
     @FXML
-    private void handleUserInput() throws {
+    private void handleUserInput() {
         String input = userInput.getText();
         String response = duke.getResponse(input);
         dialogContainer.getChildren().addAll(

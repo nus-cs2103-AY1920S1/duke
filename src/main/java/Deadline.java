806ae5f8e7e1984116996df8746565615717fedd
hello <linmeian@gmail.com>
2019-09-04 12:58:22 +0800

    Update Deadline.java
    add constructor and tostring for deadline class

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,2 +1,20 @@
 import java.util.ArrayList;
 
+public class Deadline extends Tasks {
+    private String tag = "D";
+    private String taskDescription;
+    private String status;
+    private time;
+
+    public Deadline(ArrayList<String> inputList) {
+        super(inputList.get(1), inputList.get(2));
+        this.taskDescription = super.taskDetails;
+        this.status = super.status;
+        this.time = super.time;
+    }
+    
+    @Override
+    public String toString() {
+        return tag + " | " + status + " | " + taskDescription + " | " + time;
+    }
+}

8b5b46616e6ad1c58c78481c7544e327673adb2a
joloong <joelloong@gmail.com>
2019-09-05 16:46:47 +0800

    Improve code quality

--- src/main/java/task/Parser.java
+++ src/main/java/task/Parser.java
@@ -5,69 +5,82 @@ public class Parser {
     /**
      * Parse the inputs from the user.
      * 
-     * @param TaskList Current TaskList that contains an ArrayList of tasks and
-     *                 number of tasks.
-     * @return New TaskList that contains an ArrayList of tasks and number of tasks.
+     * @param textInput Inputs from the user.
+     * @return Formatted string of the input after parsing.
      */
     public static String parse(String textInput) {
         try {
-            if (textInput.equals("list")) {
-                return Ui.printList();
-            } else if (textInput.startsWith("done")) {
-                if (textInput.equals("done") || textInput.equals("done ")) {
-                    throw new DukeException("OOPS!!! Index required.");
-                }
-
-                int completedIndex = Integer.parseInt(textInput.replaceFirst("done ", "")) - 1;
-                if (completedIndex < 0 || completedIndex >= TaskList.getCounter()) {
-                    throw new DukeException("OOPS!!! Index not found.");
-                }
-
-                return TaskList.doneTask(completedIndex);
-            } else if (textInput.startsWith("todo")) {
-                if (textInput.equals("todo") || textInput.equals("todo ")) {
-                    throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
-                }
-                String description = textInput.replaceFirst("todo ", "");
-                return TaskList.addTask(new Todo(description));
-            } else if (textInput.startsWith("deadline")) {
-                if (textInput.equals("deadline") || textInput.equals("deadline ")) {
-                    throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
-                }
-                String removeTaskWord = textInput.replaceFirst("deadline ", "");
-                String[] taskSplit = removeTaskWord.split(" /by ");
-                return TaskList.addTask(new Deadline(taskSplit[0], taskSplit[1]));
-            } else if (textInput.startsWith("event")) {
-                if (textInput.equals("event") || textInput.equals("event ")) {
-                    throw new DukeException("OOPS!!! The description of a event cannot be empty.");
-                }
-
-                String removeTaskWord = textInput.replaceFirst("event ", "");
-                String[] taskSplit = removeTaskWord.split(" /at ");
-                return TaskList.addTask(new Event(taskSplit[0], taskSplit[1]));
-            } else if (textInput.startsWith("delete")) {
-                if (textInput.equals("delete") || textInput.equals("delete ")) {
-                    throw new DukeException("OOPS!!! Index required.");
-                }
-
-                int deletedIndex = Integer.parseInt(textInput.replaceFirst("delete ", "")) - 1;
-                if (deletedIndex < 0 || deletedIndex >= TaskList.getCounter()) {
-                    throw new DukeException("OOPS!!! Index not found.");
-                }
-
-                return TaskList.deleteTask(deletedIndex);
-            } else if (textInput.startsWith("find")) {
-                if (textInput.equals("find") || textInput.equals("find ")) {
-                    throw new DukeException("OOPS!!! Keyword required!");
-                }
-
-                String keyWord = textInput.replaceFirst("find ", "");
-                return TaskList.findTask(keyWord);
-            } else {
-                throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
-            }
+            return parseCommands(textInput);
         } catch (DukeException e) {
             return Ui.printException(e);
         }
+    }
+
+    private static String parseTasks(String textInput) throws DukeException {
+        if (textInput.startsWith("todo")) {
+            if (isInvalidCommand(textInput, "todo")) {
+                throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
+            }
+            String description = textInput.replaceFirst("todo ", "");
+            return TaskList.addTask(new Todo(description));
+        } else if (textInput.startsWith("deadline")) {
+            if (isInvalidCommand(textInput, "deadline")) {
+                throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
+            }
+            String removeTaskWord = textInput.replaceFirst("deadline ", "");
+            String[] taskSplit = removeTaskWord.split(" /by ");
+            return TaskList.addTask(new Deadline(taskSplit[0], taskSplit[1]));
+        } else if (textInput.startsWith("event")) {
+            if (isInvalidCommand(textInput, "event")) {
+                throw new DukeException("OOPS!!! The description of a event cannot be empty.");
+            }
+
+            String removeTaskWord = textInput.replaceFirst("event ", "");
+            String[] taskSplit = removeTaskWord.split(" /at ");
+            return TaskList.addTask(new Event(taskSplit[0], taskSplit[1]));
+        } else {
+            throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
+        }
+    }
+
+    private static String parseCommands(String textInput) throws DukeException {
+        if (textInput.equals("list")) {
+            return Ui.printList();
+        } else if (textInput.startsWith("done")) {
+            if (isInvalidCommand(textInput, "done")) {
+                throw new DukeException("OOPS!!! Index required.");
+            }
+
+            int completedIndex = Integer.parseInt(textInput.replaceFirst("done ", "")) - 1;
+            if (completedIndex < 0 || completedIndex >= TaskList.getCounter()) {
+                throw new DukeException("OOPS!!! Index not found.");
+            }
+
+            return TaskList.doneTask(completedIndex);
+        } else if (textInput.startsWith("delete")) {
+            if (isInvalidCommand(textInput, "delete")) {
+                throw new DukeException("OOPS!!! Index required.");
+            }
+
+            int deletedIndex = Integer.parseInt(textInput.replaceFirst("delete ", "")) - 1;
+            if (deletedIndex < 0 || deletedIndex >= TaskList.getCounter()) {
+                throw new DukeException("OOPS!!! Index not found.");
+            }
+
+            return TaskList.deleteTask(deletedIndex);
+        } else if (textInput.startsWith("find")) {
+            if (isInvalidCommand(textInput, "find")) {
+                throw new DukeException("OOPS!!! Keyword required!");
+            }
+
+            String keyWord = textInput.replaceFirst("find ", "");
+            return TaskList.findTask(keyWord);
+        } else {
+            return parseTasks(textInput);
+        }
+    }
+
+    private static boolean isInvalidCommand(String textInput, String command) {
+        return textInput.equals(command) || textInput.equals(command + " ");
     }
 }

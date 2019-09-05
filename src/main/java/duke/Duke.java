20e427237c94eec9a8984e926807c5a9e85d14ba
Lim Jun Hup <limjunhup1997@gmail.com>
2019-09-05 11:29:09 +0800

    implement javaFX

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,19 +1,39 @@
 package duke;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
 import duke.command.Command;
 import duke.exception.DukeException;
 
 import java.io.IOException;
+
+
 
 /**
  * This class is the main class of the program. It helps users to help track their Duke.tasks
  * by creating a task list for them when users key in the respective input. The task list
  * is then automatically saved into a txt file which allows the users to view and keep track.
  */
-public class Duke {
+public class Duke extends Application{
 	private Storage storage;
 	private TaskList tasks;
 	private Ui ui;
+	
+	private ScrollPane scrollPane;
+	private VBox dialogContainer;
+	private TextField userInput;
+	private Button sendButton;
+	private Scene scene;
+	
 	
 	/**
 	 * Class constructor
@@ -34,6 +54,7 @@ public class Duke {
 	}
 	
 	public static void main(String[] args) throws IOException {
+		Application.launch(Duke.class, args);
 		new Duke("/users/junhup/desktop/duke/src/duke.txt").run();
 	}
 	
@@ -53,6 +74,58 @@ public class Duke {
 				ui.showError(e);
 			}
 		}
+	}
+	
+	@Override
+	public void start(Stage stage) {
+		//Step 1. Setting up required components
+		
+		//The container for the content of the chat to scroll.
+		scrollPane = new ScrollPane();
+		dialogContainer = new VBox();
+		scrollPane.setContent(dialogContainer);
+		
+		userInput = new TextField();
+		sendButton = new Button("Send");
+		
+		AnchorPane mainLayout = new AnchorPane();
+		mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+		
+		scene = new Scene(mainLayout);
+		
+		stage.setTitle("Duke");
+		stage.setResizable(false);
+		stage.setMinHeight(600.0);
+		stage.setMinWidth(400.0);
+		
+		mainLayout.setPrefSize(400.0, 600.0);
+		
+		scrollPane.setPrefSize(385, 535);
+		scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+		scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+		
+		scrollPane.setVvalue(1.0);
+		scrollPane.setFitToWidth(true);
+		
+		// You will need to import `javafx.scene.layout.Region` for this.
+		dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+		
+		userInput.setPrefWidth(325.0);
+		
+		sendButton.setPrefWidth(55.0);
+		
+		AnchorPane.setTopAnchor(scrollPane, 1.0);
+		
+		AnchorPane.setBottomAnchor(sendButton, 1.0);
+		AnchorPane.setRightAnchor(sendButton, 1.0);
+		
+		AnchorPane.setLeftAnchor(userInput , 1.0);
+		AnchorPane.setBottomAnchor(userInput, 1.0);
+		
+		stage.setScene(scene);
+		stage.show();
+		
+		// more code to be added here later
 	}
 }
 

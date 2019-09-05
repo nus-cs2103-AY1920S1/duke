9ff4f0da2ef0998e45512295a00d5737431bfbf9
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2019-09-05 15:13:34 +0800

    Fix GUI Listing for many tasks

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -11,6 +11,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.scene.shape.Circle;
 
 /**
@@ -35,7 +36,7 @@ public class DialogBox extends HBox {
         }
 
         dialog.setText(text);
-        setMinHeight(dialog.getHeight());
+        setMinHeight(Region.USE_PREF_SIZE);
         displayPicture.setImage(img);
         Circle clip = new Circle(50, 50, 50);
         displayPicture.setClip(clip);

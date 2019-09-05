99e99a94543bb669575e9b82cf4f3e1d073d5e31
michelleykw <michelleykw99@gmail.com>
2019-09-05 15:28:58 +0800

    Edit error

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,8 +1,7 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TodoTest {
+public class DukeTest {
     @Test
     public void dummyTest() {
         assertEquals(2,2);

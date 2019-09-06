cf404d7e0aef50107638047190379121569dcc5a
JunHong <teojunhong96@gmail.com>
2019-09-06 22:39:36 +0800

    include assertion to parts of code.

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -14,7 +14,7 @@ public class Ui {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        return "Hello from\n" + logo;
+        return  logo;
     }
 
     /**

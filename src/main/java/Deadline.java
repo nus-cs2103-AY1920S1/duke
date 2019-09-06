69263fc4a442c2e708b16812abf3fc0dbedefec6
Yen-Peng <e0318790@u.nus.edu>
2019-09-06 14:11:30 +0800

    Teach SmartBaby to understand dates and times.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,17 +1,47 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 /**
- * Represents a task with deadline.
+ * Represents a deadline object.
  */
-
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate myDate;
+    protected LocalTime myTime;
+    protected DateTimeFormatter myFormattedDate;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, String date) {
         super(description);
-        this.by = by;
+        this.date = date;
+        this.type = "deadline";
+        this.symbol = "D";
+
+        String[] dateTimeString = date.split(" ");
+        String dateString = dateTimeString[0];
+        String year = dateString.split("/")[2];
+        String month = dateString.split("/")[1];
+        if (month.length() == 1) {
+            month = "0" + month;
+        }
+        String day = dateString.split("/")[0];
+        if (day.length() == 1) {
+            day = "0" + day;
+        }
+        String timeString = dateTimeString[1];
+        String hour = timeString.substring(0, 2);
+        String minute = timeString.substring(2, 4);
+
+        myFormattedDate = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        myDate = LocalDate.parse(String.format("%s-%s-%s", year, month, day));
+        myTime = LocalTime.parse(hour + ":" + minute);
+    }
+
+    public String getDateString() {
+        return this.date;
     }
 
     @Override
     public String toString() {
-        return "[D]" + this.getStatusIcon() + this.getDescription() + " (by: " + by + ")";
+        return "[" + this.getSymbol() + "]" + this.getStatusIcon() + this.getDescription() + " (by: " + myDate.format(myFormattedDate) + " " + myTime + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,17 +1,48 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 /**
- * Represents an event.
+ * Represents an event object.
  */
-
 public class Event extends Task {
-    protected String at;
+    protected LocalDate myDate;
+    protected LocalTime myTime;
+    protected DateTimeFormatter myFormattedDate;
 
-    public Event(String description, String at) {
+    public Event(String description, String date) {
         super(description);
-        this.at = at;
+        this.date = date;
+        this.type = "event";
+        this.symbol = "E";
+
+        String[] dateTimeString = date.split(" ");
+        String dateString = dateTimeString[0];
+        String year = dateString.split("/")[2];
+        String month = dateString.split("/")[1];
+        if (month.length() == 1) {
+            month = "0" + month;
+        }
+        String day = dateString.split("/")[0];
+        if (day.length() == 1) {
+            day = "0" + day;
+        }
+        String timeString = dateTimeString[1];
+        String hour = timeString.substring(0, 2);
+        String minute = timeString.substring(2, 4);
+
+        myFormattedDate = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        myDate = LocalDate.parse(String.format("%s-%s-%s", year, month, day));
+        myTime = LocalTime.parse(hour + ":" + minute);
+    }
+
+    public String getDateString() {
+        return this.date;
     }
 
     @Override
     public String toString() {
-        return "[E]" + this.getStatusIcon() + this.getDescription() + " (at: " + at + ")";
+
+        return "[" + this.getSymbol() + "]" + this.getStatusIcon() + this.getDescription() + " (at: " + myDate.format(myFormattedDate) + " " + myTime + ")";
     }
 }

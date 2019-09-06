544ec7eefda87908fa4c7bb8abb161d53d32f4cc
SimNee <chuasimnee@Chuas-MacBook-Pro.local>
2019-09-06 12:57:07 +0800

    Level-10

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,4 +1,10 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
 
 public class Storage {
 

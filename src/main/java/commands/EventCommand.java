bf459bcdefd074e910b856ea31a65ada42aba2f6
DivineDX <chia.dexun97@gmail.com>
2019-09-07 01:23:52 +0800

    Fix exception bug

--- src/main/java/commands/EventCommand.java
+++ src/main/java/commands/EventCommand.java
@@ -9,12 +9,14 @@ import task.Events;
 import task.Task;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 /**
  * Encapsulates command to create an Event Task to be added to List of Tasks.
  */
 public class EventCommand extends Command {
     private String args;
+    private static String EVENT_WRONG_FORMAT = "Invalid format. Please follow the format:\nevent [description] /at DD/MM/YYYY HHMM - DD/MM/YYYY HHMM";
 
     public EventCommand(String args) {
         this.args = args;
@@ -31,23 +33,27 @@ public class EventCommand extends Command {
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
         String[] splitStr = args.split("/at");
-        String[] dateString = splitStr[1].trim().split(" - "); //e.g. 2/12/2019 1800 - 2/12/2019 1800
-
-        if (splitStr.length == 1) {
-            throw new DukeException("Invalid format. Please include '/at' to state your start and end dates. "
-                    + "\nE.g. event meeting /at 12/7/2019 2000 - 18/12/2019 1800");
-        } else if (dateString.length != 2) {
-            throw new DukeException("Invalid Format. Please enter both the start and end dates"
-                    + "\nE.g. event meeting /at 12/7/2019 2000 - 18/12/2019 1800");
-        } else if (splitStr[0].trim().isEmpty()) {
+        if (splitStr[0].trim().isEmpty()) {
             throw new DukeException("☹ OOPS!!! The description of an event cannot be empty");
+        } else if (splitStr.length == 1) {
+            throw new DukeException(EVENT_WRONG_FORMAT);
+        }
+
+        String[] dateString;
+        try {
+            dateString = splitStr[1].trim().split(" - "); //e.g. 2/12/2019 1800 - 2/12/2019 1800
+            if (dateString.length != 2) {
+                throw new DukeException(EVENT_WRONG_FORMAT);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new DukeException(EVENT_WRONG_FORMAT);
         }
 
         LocalDateTime start = Parser.parseDateTime(dateString[0]);
         LocalDateTime end = Parser.parseDateTime(dateString[1]);
 
         if (start.isAfter(end)) {
-            throw new DukeException("☹ OOPS!!! Start DateTime cannnot be after End DateTime!");
+            throw new DukeException("☹ OOPS!!! Start DateTime cannot be after End DateTime!");
         }
 
         Task task = new Events(false, splitStr[0].trim(), start, end);

8c6174d957e18138374ac180a53a6a7140d90cfe
lye-jw <lyejianwen@gmail.com>
2019-09-06 21:26:31 +0800

    Implement exit GUI with 'bye' input command

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -8,6 +8,9 @@ import duke.task.TaskList;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import javafx.application.Application;
 import javafx.fxml.FXML;
@@ -123,6 +126,18 @@ public class Duke extends Application {
         }
         if (isExit) {
             ui.printExitMessage();
+            // Thread.sleep() and Timeout.SECONDS.sleep() don't work as System shuts down without printing exit message.
+            // Use ScheduledExecutorService to make System exit after a delay, after exit message is printed.
+            ScheduledExecutorService exe = Executors.newSingleThreadScheduledExecutor();
+            Runnable command = new Runnable() {
+                @Override
+                public void run() {
+                    System.exit(0);
+                }
+            };
+            // schedule() takes Runnable, delay, unit
+            exe.schedule(command, 3, TimeUnit.SECONDS);
+            exe.shutdown();
         }
     }
 }

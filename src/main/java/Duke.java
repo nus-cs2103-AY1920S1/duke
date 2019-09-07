91c7b8c9a7459d01e62716d043d17d39d1dcb9a9
bakwxh <omega1653@gmail.com>
2019-09-07 22:50:10 +0800

    Added UI up until dialoug boxes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
 import java.util.Scanner;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * @author bakwxh
  * @version 0.1
  */
-public class Duke {
+public class Duke extends Application {
 	/**
 	 * Storage object.
 	 */
@@ -22,6 +34,20 @@ public class Duke {
 	 */
 	private Parser parser;
 
+	/**
+	 * Empty parameter constructor.
+	 */
+	
+	private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    
+	public Duke() {}
+	
     /**
      * Constructor.
      * @param filePath Path to save file.
@@ -91,8 +117,106 @@ public class Duke {
 	 * Main method.
 	 * @param args Args.
 	 */
-	public static void main(final String[] args) {
+	public static void main(String[] args) {
 		String dir = System.getProperty("user.dir") + "/savedData.txt";
 		new Duke(dir).run();
+	}
+
+	@Override
+    public void start(Stage stage) {
+         //Step 1. Setting up required components
+         
+        //The container for the content of the chat to scroll.
+         scrollPane = new ScrollPane();
+         dialogContainer = new VBox();
+         scrollPane.setContent(dialogContainer);
+
+         userInput = new TextField();
+         sendButton = new Button("Send");
+     
+         AnchorPane mainLayout = new AnchorPane();
+         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+         scene = new Scene(mainLayout);
+     
+         stage.setTitle("Duke");
+         stage.setResizable(false);
+         stage.setMinHeight(600.0);
+         stage.setMinWidth(400.0);
+         
+         mainLayout.setPrefSize(400.0, 600.0);
+         
+         scrollPane.setPrefSize(385, 535);
+         scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+         scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+         
+         scrollPane.setVvalue(1.0);
+         scrollPane.setFitToWidth(true);
+
+         dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+         
+         userInput.setPrefWidth(325.0);
+         
+         sendButton.setPrefWidth(55.0);
+         
+         AnchorPane.setTopAnchor(scrollPane, 1.0);
+         
+         AnchorPane.setBottomAnchor(sendButton, 1.0);
+         AnchorPane.setRightAnchor(sendButton, 1.0);
+         
+         AnchorPane.setLeftAnchor(userInput , 1.0);
+         AnchorPane.setBottomAnchor(userInput, 1.0);
+         
+         sendButton.setOnMouseClicked((event) -> {
+             handleUserInput();
+         });
+
+         userInput.setOnAction((event) -> {
+             handleUserInput();
+         });
+         
+         dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+         
+         stage.setScene(scene);
+         stage.show();
+
+        // more code to be added here later
+    }
+	
+//	/**
+//	 * Iteration 1:
+//	 * Creates a label with the specified text and adds it to the dialog container.
+//	 * @param text String containing text to add
+//	 * @return a label with the specified text that has word wrap enabled.
+//	 */
+//	private Label getDialogLabel(String text) {
+//	    // You will need to import `javafx.scene.control.Label`.
+//	    Label textToAdd = new Label(text);
+//	    textToAdd.setWrapText(true);
+//
+//	    return textToAdd;
+//	}
+	
+	/**
+	 * Iteration 2:
+	 * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+	 * the dialog container. Clears the user input after processing.
+	 */
+	private void handleUserInput() {
+	    Label userText = new Label(userInput.getText());
+	    Label dukeText = new Label(getResponse(userInput.getText()));
+	    dialogContainer.getChildren().addAll(
+	            new DialogBox(userText, new ImageView(user)),
+	            new DialogBox(dukeText, new ImageView(duke))
+	    );
+	    userInput.clear();
+	}
+	
+	/**
+	 * You should have your own function to generate a response to user input.
+	 * Replace this stub with your completed method.
+	 */
+	private String getResponse(String input) {
+	    return "Duke heard: " + input;
 	}
 }

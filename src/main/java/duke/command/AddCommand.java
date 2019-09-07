ad4333379d73cfd822baa630a9727ab882e98648
aidilfbk <e0303295@u.nus.edu>
2019-09-07 22:35:08 +0800

    Restrict access to AddCommand.task property

--- src/main/java/duke/command/AddCommand.java
+++ src/main/java/duke/command/AddCommand.java
@@ -10,7 +10,7 @@ import java.util.List;
 abstract class AddCommand implements Command {
     protected static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
     protected final String command;
-    protected final Task task;
+    private final Task task;
 
     public AddCommand(String command) {
         this.command = command;

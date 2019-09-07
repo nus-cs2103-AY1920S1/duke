1615cf0d41c97bfbf734e15b8e4f33dc64e0400a
lye-jw <lyejianwen@gmail.com>
2019-09-07 21:16:06 +0800

    A-Lambdas

--- src/main/java/duke/command/ExitCommand.java
+++ src/main/java/duke/command/ExitCommand.java
@@ -25,12 +25,8 @@ public class ExitCommand extends Command {
         // Thread.sleep() and Timeout.SECONDS.sleep() don't work as System shuts down without printing exit message.
         // Use ScheduledExecutorService to make System exit after a delay, after exit message is printed.
         ScheduledExecutorService exe = Executors.newSingleThreadScheduledExecutor();
-        Runnable command = new Runnable() {
-            @Override
-            public void run() {
-                System.exit(0);
-            }
-        };
+        Runnable command = () -> System.exit(0);
+
         // schedule() takes Runnable, delay, unit
         exe.schedule(command, 2, TimeUnit.SECONDS);
         exe.shutdown();

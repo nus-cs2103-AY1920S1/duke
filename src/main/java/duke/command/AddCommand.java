e83c327fbe7f558b4fbf88c31a444c75c2f36ce4
aidilfbk <e0303295@u.nus.edu>
2019-09-07 22:34:40 +0800

    Add JavaDoc for AddCommand.instantiateTask method

--- src/main/java/duke/command/AddCommand.java
+++ src/main/java/duke/command/AddCommand.java
@@ -18,6 +18,11 @@ abstract class AddCommand implements Command {
         assert task != null;
     }
 
+    /**
+     * Parses the given command to produce a Task instance with all the relevant properties filled in.
+     *
+     * @return The Task instance to add with data from the command.
+     */
     protected abstract Task instantiateTask();
 
     @Override

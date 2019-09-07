97f11b81b04fde72e211ad75d8d8f74fb7991680
Chaitanya Baranwal <chaitanyabaranwal@gmail.com>
2019-09-08 02:16:59 +0800

    Add assertions to CommandHandler

--- src/main/java/CommandHandler.java
+++ src/main/java/CommandHandler.java
@@ -30,38 +30,47 @@ public class CommandHandler {
     public String execute(String input) throws DukeException {
         Task task;
         String returnString = "";
-        switch (this.parser.parseCommand(input)) {
+        String commandType = this.parser.parseCommand(input);
+        switch (commandType) {
         case "bye":
+            assert input.split(" ")[0].equals("bye") : "First word in input should be `bye`.";
             returnString = this.ui.printExit();
             System.exit(0);
             break;
         case "list":
+            assert input.split(" ")[0].equals("list") : "First word in input should be `list`.";
             returnString = this.ui.showTaskList(this.tl);
             break;
         case "done":
+            assert input.split(" ")[0].equals("done") : "First word in input should be `done`.";
             task = this.tl.taskDone(this.parser.parseInteger(input));
             returnString = this.ui.showTaskMarkedDone(task);
             break;
         case "delete":
+            assert input.split(" ")[0].equals("delete") : "First word in input should be `delete`.";
             task = this.tl.removeTask(this.parser.parseInteger(input));
             returnString = this.ui.showTaskDeletion(task, this.tl);
             break;
         case "todo":
+            assert input.split(" ")[0].equals("todo") : "First word in input should be `todo`.";
             task = this.parser.parseTodo(input);
             tl.addTask(task);
             returnString = this.ui.showTaskAddition(task, this.tl);
             break;
         case "event":
+            assert input.split(" ")[0].equals("event") : "First word in input should be `event`.";
             task = this.parser.parseEvent(input);
             tl.addTask(task);
             returnString = this.ui.showTaskAddition(task, tl);
             break;
         case "deadline":
+            assert input.split(" ")[0].equals("deadline") : "First word in input should be `deadline`.";
             task = this.parser.parseDeadline(input);
             tl.addTask(task);
             returnString = this.ui.showTaskAddition(task, tl);
             break;
         case "find":
+            assert input.split(" ")[0].equals("find") : "First word in input should be `find`.";
             TaskList newTl = tl.findTasks(input);
             returnString = this.ui.showTaskList(newTl);
             break;

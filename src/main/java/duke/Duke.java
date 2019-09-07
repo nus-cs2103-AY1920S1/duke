d60199b956297bd4209c77b12aef5923c21fd761
ning-sy210 <ningshy97@gmail.com>
2019-09-08 01:39:29 +0800

    Implemented functional GUI for Duke

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -8,7 +8,6 @@ import exception.VoidDukeCommand;
 
 import java.io.IOException;
 import java.time.format.DateTimeParseException;
-import java.util.Scanner;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -42,68 +41,18 @@ public class Duke extends Application {
     private Ui ui;
     private ExceptionHandler exceptionHandler;
     private Storage storage;
+    private TaskList taskList;
 
     public Duke() {
         this.ui = new Ui();
         this.exceptionHandler = new ExceptionHandler();
         this.storage = new Storage("data");
-    }
-
-    private void run() {
-        TaskList taskList;
 
         try {
             taskList = new TaskList(storage.readSaveFile(), storage);
         } catch (IOException e) {
             taskList = new TaskList(storage);
         }
-
-        if (!taskList.hasTask()) {
-            ui.welcome();
-        } else {
-            ui.welcomeBack();
-
-            try {
-                taskList.listAllTasks("list");
-            } catch (DukeException e) {
-                exceptionHandler.showDukeError(e);
-            }
-
-            Ui.showLine();
-        }
-
-        Scanner sc = new Scanner(System.in);
-        String instruction = sc.nextLine();
-        Parser parser = new Parser();
-
-        while (!instruction.equals("bye")) {
-            try {
-                parser.parseInstruction(instruction, taskList);
-            } catch (DateTimeParseException e) {
-                exceptionHandler.showParseDateTimeError();
-                Ui.showLine();
-            } catch (VoidDukeCommand e) {
-                exceptionHandler.showVoidDukeCommandError();
-                Ui.showLine();
-            } catch (IncorrectDukeCommand e) {
-                exceptionHandler.showDukeCommandEvaluationError(e);       
-                Ui.showLine();
-            } catch (InvalidDukeCommand e) {
-                exceptionHandler.showUnknownDukeCommandError();
-                Ui.showLine();
-            } catch (IOException e) {
-                exceptionHandler.showDukeIoError();
-                Ui.showLine();
-            } catch (DukeException e) {
-                exceptionHandler.showDukeError(e);   
-                Ui.showLine();
-            } 
-
-            instruction = sc.nextLine();
-        }
-
-        sc.close();
-        ui.exit();
     }
 
     @Override
@@ -170,6 +119,21 @@ public class Duke extends Application {
         });
 
         dialogContainer.heightProperty().addListener(observable -> scrollPane.setVvalue(1.0));
+
+        Label welcomeLabel;
+
+        if (!taskList.hasTask()) {
+            welcomeLabel = getDialogLabel(ui.welcome());
+        } else {
+            try {
+                welcomeLabel = getDialogLabel(ui.welcomeBack() + "\n" + taskList.listAllTasks("list"));
+            } catch (DukeException e) {
+                System.out.println("something went wrong...");
+                welcomeLabel = null;
+            }
+        }
+
+        dialogContainer.getChildren().add(DialogBox.getDukeDialog(welcomeLabel, new ImageView(duke)));
     }
 
     /**
@@ -195,19 +159,35 @@ public class Duke extends Application {
         Label dukeText = new Label(getResponse(userInput.getText()));
 
         dialogContainer.getChildren().addAll(
-                new DialogBox(userText, new ImageView(user)),
-                new DialogBox(dukeText, new ImageView(duke))
+                DialogBox.getUserDialog(userText, new ImageView(user)),
+                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
         );
 
         userInput.clear();
     }
 
     private String getResponse(String input) {
-        return "Duke heard: " + input;
-    }
+        Parser parser = new Parser();
+        String dukeResponse;
+        String errorMessage;
 
-    public static void main(String[] args) {
-        Duke duke = new Duke();
-        duke.run();
+        try {
+            dukeResponse = parser.parseInstruction(input, taskList);
+            return dukeResponse;
+        } catch (DateTimeParseException e) {
+            errorMessage = exceptionHandler.showParseDateTimeError();
+        } catch (VoidDukeCommand e) {
+            errorMessage = exceptionHandler.showVoidDukeCommandError();
+        } catch (IncorrectDukeCommand e) {
+            errorMessage = exceptionHandler.showDukeCommandEvaluationError(e);
+        } catch (InvalidDukeCommand e) {
+            errorMessage = exceptionHandler.showUnknownDukeCommandError();
+        } catch (IOException e) {
+            errorMessage = exceptionHandler.showDukeIoError();
+        } catch (DukeException e) {
+            errorMessage = exceptionHandler.showDukeError(e);
+        }
+
+        return errorMessage;
     }
 }

11038e986104dbdab3ec65fe61311240cabfe4e1
hua-lun <chuahualun@gmail.com>
2019-09-08 19:17:56 +0800

    Added Statistics class to keep track of number of tasks done

---
+++ src/main/java/Statisitcs.java
@@ -0,0 +1,108 @@
+/**
+ * This is the Statisitcs class. It shows the number of tasks that have been completed in the past week.
+ * @author Hua Lun
+ */
+
+import java.time.LocalDate;
+import java.util.Formatter;
+
+public class Statisitcs {
+
+    static int todoDone = 0;
+    static int eventDone = 0;
+    static int deadlineDone = 0;
+    static String dateStart = "2019-09-06";
+
+    private Formatter statsFile;
+
+    /**
+     * <p>
+     *     openFile is used to open the specific text file.
+     * </p>
+     */
+
+    public void openFile() {
+        try{
+            statsFile = new Formatter("stats.txt");
+        } catch (Exception e){
+            System.out.println("you have an error");
+        }
+    }
+
+
+    /**
+     * <p>
+     *     addTodoDone adds 1 to the todoDone counter.
+     * </p>
+     */
+
+    public void addTodoDone() {
+        todoDone++;
+    }
+
+    /**
+     * <p>
+     *     addEventDone adds 1 to the eventDone counter.
+     * </p>
+     */
+
+    public void addEventDone() {
+        eventDone++;
+    }
+
+    /**
+     * <p>
+     *     addDeadlineDone adds 1 to the deadlineDone counter.
+     * </p>
+     */
+
+    public void addDeadlineDone() {
+        deadlineDone++;
+    }
+
+    /**
+     * <p>
+     *     resetStats resets all the data collected.
+     * </p>
+     */
+
+    public void resetStats() {
+        todoDone = 0;
+        eventDone = 0;
+        deadlineDone = 0;
+        LocalDate newDate = LocalDate.now();
+        dateStart = newDate.toString();
+    }
+
+    /**
+     * <p>
+     *     getsStats provides the number of tasks done for each category.
+     * </p>
+     * @return statistics of tasks done.
+     */
+
+    public String getStats() {
+
+        return "Since " + dateStart + "\n" +
+                "You have done: " + "\n" +
+                todoDone + " Todo tasks" + "\n" +
+                eventDone + " Event tasks" + "\n" +
+                deadlineDone + " Deadline tasks";
+    }
+
+    /**
+     * <p>
+     *     addRecords adds all the statistics into the text file.
+     * </p>
+     */
+
+    public void addRecords() {
+        StringBuilder sB = new StringBuilder();
+        sB.append(dateStart);
+        sB.append("\n");
+        sB.append(getStats());
+        statsFile.format(sB.toString());
+    }
+
+
+}

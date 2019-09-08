c55da3967c79c461d73716ba26f640696ae4b7d7
Chee Yuan <chong-1997@hotmail.com>
2019-09-08 23:32:35 +0800

    Add sort by type and description feature

---
+++ src/main/java/duke/command/SortCommand.java
@@ -0,0 +1,61 @@
+package duke.command;
+
+import duke.DukeException;
+import duke.TaskList;
+import duke.task.Deadline;
+import duke.task.Event;
+import duke.task.Task;
+import duke.task.Todo;
+
+import java.util.Comparator;
+
+public class SortCommand extends Command {
+
+    public static final String name = "sort";
+
+    public SortCommand(String fullCommand) {
+        super(fullCommand);
+    }
+
+    @Override
+    public String execute(TaskList tasks) throws DukeException {
+        String sortType = fullCommand.split(" ")[1];
+        switch (sortType) {
+        case "type":
+            sortByType(tasks);
+            return "Sorted by type";
+        case "description":
+            sortByDescription(tasks);
+            return "Sorted by description";
+        default:
+            return "Sort command requires a valid argument";
+        }
+    }
+
+    private void sortByDescription(TaskList tasks) {
+        tasks.getTasks().sort(Comparator.comparing(Task::getDescription));
+    }
+
+    private void sortByType(TaskList tasks) {
+        tasks.getTasks().sort((t1, t2) -> {
+            try {
+                return getTaskSymbol(t1) - getTaskSymbol(t2);
+            } catch (DukeException e) {
+                e.printStackTrace();
+                return 0;
+            }
+        });
+    }
+
+    private char getTaskSymbol(Task task) throws DukeException {
+        if (task instanceof Deadline) {
+            return Deadline.SYMBOL;
+        } else if (task instanceof Event) {
+            return Event.SYMBOL;
+        } else if (task instanceof Todo) {
+            return Todo.SYMBOL;
+        } else {
+            throw new DukeException("INVALID TASK SYMBOL FOUND IN SORT");
+        }
+    }
+}

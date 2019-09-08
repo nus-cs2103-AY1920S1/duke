2d6da407f6f11ba7732da4b79ac488f4ae350290
choongyx <yongxin.choong98@gmail.com>
2019-09-08 17:51:43 +0800

    JUnit tests modification

--- src/test/java/DeadlineTest.java
+++ src/test/java/DeadlineTest.java
@@ -5,7 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DeadlineTest {
     @Test
     public void dummyTest() {
-        Deadline test = new Deadline("Return book", "12/12/2019 1200");
+        Deadline test = null;
+        try {
+            test = new Deadline("Return book", "12/12/2019 1200");
+        } catch (WrongDateFormatDukeException e) {
+            e.printStackTrace();
+        }
         assertEquals("[D][" + "0" + "] Return book (by: 12/12/2019 1200)", test.toString());
         assertEquals("D | " + "0" + " | Return book | 12/12/2019 1200", test.stringForAppend());
     }

a9ab620cc6f1ca228c0aa442837c5e0d9db2f441
Kendrick Ang <phantom2457@hotmail.com>
2019-09-08 16:29:15 +0800

    add more test cases and refactor tests

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -2,6 +2,7 @@ package duke;
 
 import duke.command.*;
 import duke.exception.DukeException;
+import duke.exception.DukeIndexOutOfBoundsException;
 import duke.exception.DukeMissingDescriptionException;
 import duke.exception.DukeUnknownInputException;
 
@@ -30,44 +31,94 @@ public class Parser {
 
         switch (commands[0]) {
         case "todo":
-            if (args.length == 0) {
-                throw new DukeMissingDescriptionException(
-                        "OOPS! The description of a todo cannot be empty.");
-            }
-            return new AddCommand(TODO, args, false);
+            return parseTodo(args);
         case "event":
-            return new AddCommand(EVENT, args, false);
+            return parseEvent(args);
         case "deadline":
-            String[] deadlineArgs = String.join(" ", args).split(" /by ");
-            String deadline = deadlineArgs[1];
-            // enforce example format 2/12/2019 1800
-            if (deadline.split(" ").length != 2
-                    || deadline.split(" ")[0].split("/").length != 3
-                    || Integer.valueOf(deadline.split(" ")[1]) < 0
-                    || Integer.valueOf(deadline.split(" ")[1]) > 2400) {
-                throw new DukeUnknownInputException("Parser: Unknown deadline String format passed :(");
-            }
-            return new AddCommand(DEADLINE, args, false);
+            return parseDeadline(args);
         case "find":
-            if (args.length == 0) {
-                throw new DukeMissingDescriptionException(
-                        "OOPS! The keyword of a search cannot be empty.");
-            }
-            String keyword = args[0].strip();
-            return new FindCommand(keyword, false);
+            return parseFind(args);
         case "done":
-            int doneIdx = Integer.valueOf(args[0]);
-            return new DoneCommand(doneIdx, false);
+            return parseDone(args);
         case "delete":
-            int deleteIdx = Integer.valueOf(args[0]);
-            return new DeleteCommand(deleteIdx, false);
+            return parseDelete(args);
         case "list":
-            return new ListCommand(false);
+            return parseList(args);
         case "bye":
-            return new ExitCommand(true);
+            return parseBye(args);
         default:
-            throw new DukeMissingDescriptionException(
-                    "OOPS! I'm sorry, but I don't know what that means...");
+            throw new DukeMissingDescriptionException("OOPS! I'm sorry, but I don't know what that means...");
         }
+    }
+
+    /*
+    Business logic for each command. Refactor concept: Extract Method
+     */
+    private static Command parseTodo(String[] args) {
+        if (args.length == 0) {
+            throw new DukeMissingDescriptionException("OOPS! The description of a todo cannot be empty.");
+        }
+        return new AddCommand(TODO, args, false);
+    }
+
+    private static Command parseEvent(String[] args) {
+        String[] eventArgs = String.join(" ", args).split(" /at ");
+        if (eventArgs.length != 2) {
+            throw new DukeUnknownInputException("Incorrect argument count for event!");
+        }
+
+        return new AddCommand(EVENT, args, false);
+    }
+
+    private static Command parseDeadline(String[] args) {
+        String[] deadlineArgs = String.join(" ", args).split(" /by ");
+        if (deadlineArgs.length != 2) {
+            throw new DukeUnknownInputException("Incorrect argument count for deadline!");
+        }
+        String deadline = deadlineArgs[1];
+        // enforce example format 2/12/2019 1800
+        if (deadline.split(" ").length != 2
+                || deadline.split(" ")[0].split("/").length != 3
+                || Integer.valueOf(deadline.split(" ")[1]) < 0
+                || Integer.valueOf(deadline.split(" ")[1]) > 2400) {
+            throw new DukeUnknownInputException("Unknown deadline String format passed :(");
+        }
+
+        return new AddCommand(DEADLINE, args, false);
+    }
+
+    private static Command parseFind(String[] args) {
+        if (args.length == 0) {
+            throw new DukeMissingDescriptionException("OOPS! The keyword of a search cannot be empty.");
+        }
+
+        String keyword = args[0].strip();
+        return new FindCommand(keyword, false);
+    }
+
+    private static Command parseDone(String[] args) {
+        int doneIdx = Integer.valueOf(args[0]);
+        if (doneIdx < 0) {
+            throw new DukeIndexOutOfBoundsException("Attempting to mark task not in list!");
+        }
+
+        return new DoneCommand(doneIdx, false);
+    }
+
+    private static Command parseDelete(String[] args) {
+        int deleteIdx = Integer.valueOf(args[0]);
+        if (deleteIdx < 0) {
+            throw new DukeIndexOutOfBoundsException("Attempting to delete task not in list!");
+        }
+
+        return new DeleteCommand(deleteIdx, false);
+    }
+
+    private static Command parseList(String[] args) {
+        return new ListCommand(false);
+    }
+
+    private static Command parseBye(String[] args) {
+        return new ExitCommand(true);
     }
 }
--- src/main/java/duke/task/TaskFactory.java
+++ src/main/java/duke/task/TaskFactory.java
@@ -20,11 +20,11 @@ public class TaskFactory {
             // example args: {"borrow", "book"}
             return new Todo(String.join(" ", args));
         case EVENT:
-            // example args: {"attend festival", "2pm"}
+            // example eventArgs: {"attend festival", "2pm-4pm"}
             String[] eventArgs = String.join(" ", args).split(" /at ");
             return new Event(eventArgs[0].strip(), eventArgs[1].strip());
         case DEADLINE:
-            // example args: {"homework", "2/12/2019 1800"}
+            // example deadlineArgs: {"homework", "2/12/2019 1800"}
             String[] deadlineArgs = String.join(" ", args).split(" /by ");
             return new Deadline(deadlineArgs[0].strip(), deadlineArgs[1].strip());
         default:

518f30e9c2cf8b324f9e08151dd23cb3cb61fcf7
sandydays <sandhyar725@gmail.com>
2019-09-10 00:17:01 +0800

    Add JUnit testing.

---
+++ src/test/java/TodoCommandTest.java
@@ -0,0 +1,36 @@
+import duke.Storage;
+import duke.TaskList;
+import duke.Ui;
+import duke.command.TodoCommand;
+import duke.task.Task;
+import duke.task.Todo;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoCommandTest {
+
+    @Test
+    // Since this method returns void, testing it using its side effects
+    public void executingCommand_todoCommand_success() {
+        // Setting up actual TaskList
+        TaskList t = new TaskList();
+        Ui ui = new Ui();
+        Storage s = new Storage("data/tasksTest.txt");
+        new TodoCommand("buy groceries").execute(t, ui, s);
+
+        // Setting up expected list of tasks
+        LinkedList<Task> tExpected = new LinkedList<>();
+        tExpected.add(new Todo("buy groceries"));
+
+        // Comparing the list of tasks
+        assertEquals(tExpected.get(0).toString(), t.getTasks().get(0).toString());
+    }
+
+    @Test
+    public void exiting_todoCommand_noExitingOrFalse() {
+        assertEquals(false, new TodoCommand("buy groceries").isExit());
+    }
+}
---
+++ src/test/java/TodoTest.java
@@ -0,0 +1,26 @@
+import duke.task.Task;
+import duke.task.Todo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoTest {
+
+    @Test
+    public void todoSavingAsString_todo_expectedString() {
+        Task t = new Todo("buy groceries");
+        assertEquals("T | 0 | buy groceries", t.toSave());
+
+        t.markAsDone();
+        assertEquals("T | 1 | buy groceries", t.toSave());
+    }
+
+    @Test
+    public void todoPrintingAsString_todo_expectedString() {
+        Task t = new Todo("buy groceries");
+        assertEquals("[T][\u2718] buy groceries", t.toString());
+
+        t.markAsDone();
+        assertEquals("[T][\u2713] buy groceries", t.toString());
+    }
+}

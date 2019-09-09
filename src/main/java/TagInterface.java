55fd538b9e3ff89f20a23802db2cd8824c6711fb
e0031374 <e0031374@u.nus.edu>
2019-09-09 12:01:11 +0800

    JUnit tests for TagAbstractWrapper.java

---
+++ src/main/java/TagInterface.java
@@ -0,0 +1,9 @@
+/**
+ * Interface for Tags to implement
+ *  the rationale is to ensure developers need to implement
+ *  a new TagWrapper with hashcode sothe object is not lost .
+ */
+public interface TagInterface {
+    
+    
+}
--- src/main/java/TagStore.java
+++ src/main/java/TagStore.java
@@ -1,12 +1,40 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 /**
  * Store all tags
  *
  */
 public class TagStore implements TagObserver {
+    HashMap<TagInterface, List<String>> objKeyStore;
+    HashMap<String, List<TagInterface>> tagKeyStore;
+
+    public TagStore() {
+        this.objKeyStore = new HashMap<>();
+        this.tagKeyStore = new HashMap<>();
+    }
+
     public TagStore(TagObservable o) {
         o.registerObserver(this);
     }
 
+    /**
+     * Initializes List in Hashmap before it can be used.
+     */
+    private void initTagKeyStore(String tagStr) {
+        List<TagInterface> objList = new ArrayList<>();
+        tagKeyStore.put(tagStr, objList);
+    }
+
+    /**
+     * Initializes List in Hashmap before it can be used.
+     */
+    private void initObjKeyStore(TagInterface objKey) {
+        List<String> tagList = new ArrayList<>();
+        objKeyStore.put(objKey, tagList);
+    }
+
     public void update(TagObservable o) {
+
     }
 }

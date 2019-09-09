f1e640b7dc0aa1eaf55a8c5d402b1d0d5e341d43
liawsy <liawsy99@gmail.com>
2019-09-09 23:28:09 +0800

    GUI greets user once application is open

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -37,6 +37,9 @@ public class MainWindow extends AnchorPane {
 
     public void setDuke(Duke d) {
         duke = d;
+        dialogContainer.getChildren().addAll(
+                DialogBox.getDukeDialog(d.greet(), dukeImage)
+        );
     }
 
     /**

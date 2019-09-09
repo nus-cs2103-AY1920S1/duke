2d0321e9554f04be0408dbafc93f4b7d7b67e61f
e0031374 <e0031374@u.nus.edu>
2019-09-09 15:15:27 +0800

    saved my progress for now gotta do 2102 first

--- src/main/java/TagStore.java
+++ src/main/java/TagStore.java
@@ -18,6 +18,55 @@ public class TagStore implements TagObserver {
         o.registerObserver(this);
     }
 
+
+
+    /**
+     * Testing method, to evaluate if it has been added
+     */
+    public boolean containsTag(String strTag, TagInterface obj) {
+        /* TODO this part, and return streams based on query */
+    }
+
+    /**
+     * inserts a tag and the object into the store 
+     */
+    public void insertTag(String strTag, TagInterface obj) {
+        insertTagKeyStore(strTag, obj);
+        insertObjKeyStore(obj, strTag);
+    }
+
+    /**
+     * Insert a n object based on tag into a tag->obj store.
+     */
+    private void insertTagKeyStore(String strKey, TagInterface objVal) {
+        if (! tagKeyStore.containsKey(strKey)) {
+            initTagKeyStore(strKey);
+        }
+
+        List<TagInterface> xs = tagKeyStore.get(strKey);
+        if (xs.contains(objVal)) {
+            return;
+        } else {
+            xs.add(objVal);
+        }
+    }
+
+    /**
+     * Insert an object based on the tag it has, obj->tag store.
+     */
+    private void insertObjKeyStore(TagInterface objKey, String strVal) {
+        if (! objKeyStore.containsKey(objKey)) {
+            initObjKeyStore(objKey);
+        }
+
+        List<String> xs = objKeyStore.get(objKey);
+        if (xs.contains(strVal)) {
+            return;
+        } else {
+            xs.add(strVal);
+        }
+    }
+
     /**
      * Initializes List in Hashmap before it can be used.
      */

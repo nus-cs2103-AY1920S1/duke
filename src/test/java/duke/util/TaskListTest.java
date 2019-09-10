6a67b8e78155b3e2e1dce2fb60d6074ba1bd7c46
hoholyin <holyin97@gmail.com>
2019-09-10 08:54:21 +0800

    updated simple test

---
+++ src/test/java/duke/util/TaskListTest.java
@@ -0,0 +1,69 @@
+package duke.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskListTest {
+
+    @Test
+    void getTaskList() {
+        assertEquals(2, 2);
+    }
+
+    @Test
+    void getListAsString() {
+    }
+
+    @Test
+    void markItemComplete() {
+    }
+
+    @Test
+    void deleteItem() {
+    }
+
+    @Test
+    void reIndex() {
+    }
+
+    @Test
+    void registerNewTask() {
+    }
+
+    @Test
+    void checkCommandValidity() {
+    }
+
+    @Test
+    void addToList() {
+    }
+
+    @Test
+    void createDateAndTime() {
+    }
+
+    @Test
+    void is24hrFormat() {
+    }
+
+    @Test
+    void createTime() {
+    }
+
+    @Test
+    void createDate() {
+    }
+
+    @Test
+    void isInteger() {
+    }
+
+    @Test
+    void echo() {
+    }
+
+    @Test
+    void findItem() {
+    }
+}

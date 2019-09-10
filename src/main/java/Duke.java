a6332a330f5718cbaea2460aa7357cef38f28982
zhangxuan97 <zxrox@yahoo.com.sg>
2019-09-10 16:09:18 +0800

    coding quality updates

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,9 @@
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-
 import command.Command;
 import main.DukeException;
 import main.Parser;
 import main.Storage;
 import main.TaskList;
 import main.Ui;
-
 
 /**
  * Main class.
@@ -34,7 +19,6 @@ public class Duke {
      */
     public Duke() {
         String filePath = "/Users/zhangxuan/Desktop/CS2103/duke/data/tasks.txt";
-        // Currently hardcoded.
         ui = new Ui();
         storage = new Storage(filePath);
         if (!storage.isValidFilePath()) {
@@ -61,14 +45,12 @@ public class Duke {
      */
     public String run(String fullCommand) {
         String response = "";
-        //response += Ui.DIVIDER + "\n";
         try {
             Command c = Parser.parse(fullCommand);
             response += c.execute(tasks, ui, storage);
         } catch (DukeException e) {
             response += ui.getErrorMessage(e.getMessage());
         }
-        //response += Ui.DIVIDER + "\n";
         return response;
     }
     /**

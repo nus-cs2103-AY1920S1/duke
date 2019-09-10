5cbf986b2437c4d90e058ff8abaf3a541f16219b
Kyzure <kyzurene@gmail.com>
2019-09-11 00:56:59 +0800

    Added better design for Java fx.

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -6,11 +6,16 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 
 /**
  * An example of a custom control using FXML.
@@ -18,10 +23,18 @@ import javafx.scene.layout.HBox;
  * face and a label containing text from the speaker.
  */
 public class DialogBox extends HBox {
+    private static final double X_TRANSFORM_LABEL = 25;
+    private static final double X_TRANSFORM_IMAGE = 18;
+
     @FXML
     private Label dialog;
     @FXML
-    private ImageView displayPicture;
+    private Circle myCircle;
+
+    private Font fontRegular = Font.loadFont(
+            this.getClass().getResource("/font/Minecraftia-Regular.ttf").toExternalForm(), 10);
+
+    private boolean isUser;
 
     /**
      * Constructor for the dialogBox.
@@ -29,7 +42,9 @@ public class DialogBox extends HBox {
      * @param text The text to be displayed - Which can be the user or Duke.
      * @param img The image to be displayed - Which can be the user or Duke.
      */
-    private DialogBox(String text, Image img) {
+    private DialogBox(String text, Image img, boolean isUser) {
+        this.isUser = isUser;
+        
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
             fxmlLoader.setController(this);
@@ -39,8 +54,25 @@ public class DialogBox extends HBox {
             e.printStackTrace();
         }
 
+        if(this.isUser) {
+            this.dialog.setTranslateX(-X_TRANSFORM_LABEL);
+            this.myCircle.setTranslateX(-X_TRANSFORM_IMAGE);
+            this.dialog.setAlignment(Pos.TOP_RIGHT);
+            myCircle.setStroke(Color.GREENYELLOW);
+            myCircle.setFill(new ImagePattern(img));
+            myCircle.setEffect(new DropShadow(+14d, 0d, +2d, Color.SEAGREEN));
+        } else {
+            this.dialog.setTranslateX(X_TRANSFORM_LABEL);
+            this.myCircle.setTranslateX(X_TRANSFORM_IMAGE);
+            this.dialog.setAlignment(Pos.TOP_LEFT);
+            myCircle.setStroke(Color.CRIMSON);
+            myCircle.setFill(new ImagePattern(img));
+            myCircle.setEffect(new DropShadow(+14d, 0d, +2d, Color.RED));
+        }
+        
+        dialog.setFont(fontRegular);
+        dialog.setTextFill(Color.BLACK);
         dialog.setText(text);
-        displayPicture.setImage(img);
     }
 
     /**
@@ -61,7 +93,7 @@ public class DialogBox extends HBox {
      * @return DialogBox containing user's text and image
      */
     public static DialogBox getUserDialog(String text, Image img) {
-        return new DialogBox(text, img);
+        return new DialogBox(text, img, true);
     }
 
     /**
@@ -72,8 +104,9 @@ public class DialogBox extends HBox {
      * @return DialogBox containing Duke's text and image
      */
     public static DialogBox getDukeDialog(String text, Image img) {
-        DialogBox db = new DialogBox(text, img);
+        DialogBox db = new DialogBox(text, img, false);
         db.flip();
         return db;
     }
+
 }

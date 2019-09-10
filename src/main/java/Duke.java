39619ba65ea3f9eea6e190fc7353d0c35e7d0b3f
Murugesan Karthika <mdkkarthika@gmail.com>
2019-09-10 20:46:02 +0800

    Merge branch 'gradle'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,21 @@
-import javafx.application.Application;
+/*import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
+import javafx.stage.Stage;*/
 
 import java.io.*;
 import java.util.Scanner;
 import java.util.ArrayList;
-public class Duke extends Application {
+public class Duke {
 
-    @Override
+    /*@Override
     public void start(Stage stage) {
         Label helloWorld = new Label("Hello World!"); // Creating a new Label control
         Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
 
         stage.setScene(scene); // Setting the stage to show our screen
         stage.show(); // Render the stage.
-    }
+    }*/
 
     private Storage storage;
     private TaskList tasks;
@@ -38,9 +38,9 @@ public class Duke extends Application {
         ui.run(tasks, storage);
     }
 
-    /*public static void main(String[] args) {
+    public static void main(String[] args) {
         new Duke("../../../data/tasks.txt").run();
-    }*/
+    }
 
     /*public void runn() {
         System.out.println("Hello! I'm Duke");

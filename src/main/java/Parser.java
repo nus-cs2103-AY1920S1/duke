f57ab44b65d1908883bb237cd8478a18a8e8f537
hello <linmeian@gmail.com>
2019-09-10 23:02:36 +0800

    Update Parser.java
    to catch scanner scanning empty string exception

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,12 +1,18 @@
 import java.util.Scanner;
+import java.util.NoSuchElementException;
 
 public class Parser {
 
-    public Command parse(String fullCmd) {
+    public static Command parse(String fullCmd) {
         Scanner sc = new Scanner(fullCmd);
         String cmd = sc.next().trim();
-        String remaining = sc.nextLine().trim();
-
+        String remaining;
+        try {
+            remaining = sc.nextLine().trim();
+        } catch(NoSuchElementException e) {
+            remaining = "";
+        }
+        sc.close();
         return new Command(cmd, remaining);
     }
 

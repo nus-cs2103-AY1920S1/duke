56af194b22917e34afac3151ee01abccf8d1834d
hoholyin <holyin97@gmail.com>
2019-09-10 13:37:27 +0800

    Re-add JUnit

---
+++ src/test/java/duke/task/EventTest.java
@@ -0,0 +1,13 @@
+package duke.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventTest {
+
+    @Test
+    void getDate() {
+        assertEquals(2, 2);
+    }
+}

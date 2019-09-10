aa784e8ff2b8adf926e057be97609221ee99f221
Bruce Ong <bruceskellator@hotmail.com>
2019-09-10 12:35:43 +0800

    "Refactored the code to utilise JavaFXML"

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,39 +1,18 @@
 package duke;
 
-import javafx.application.Application;
-
-import javafx.stage.Stage;
-
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.Region;
-
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-
-import javafx.util.Duration;
-
-import javafx.animation.PauseTransition;
-
-import java.io.File;
-import java.io.IOException;
+import commands.Command;
 
 import exceptions.DukeException;
 
-import commands.Command;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Duke is the driver class of the program.
  * Parser, Storage, TaskList, Ui objects are instantiated here and
  * passed to Command objects in order to execute the appropriate actions.
  */
-public class Duke extends Application {
+public class Duke {
 
     /**
      * the Storage object that reads from and writes to the file.
@@ -51,15 +30,16 @@ public class Duke extends Application {
     /**
      * boolean indicating whether the program should be exited.
      */
-    static boolean shouldExitProgram = false;
+    private static boolean shouldExitProgram = false;
 
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/eminem.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/lelouch2.jpeg"));
+    /**
+     * Getter method for the boolean indicating whether program should be exited.
+     *
+     * @return boolean shouldExitProgram
+     */
+    public static boolean getShouldExitProgram() {
+        return shouldExitProgram;
+    }
 
     /**
      * Setter method for the boolean indicating whether program should be exited.
@@ -95,7 +75,7 @@ public class Duke extends Application {
      * @param fullCommand String that user typed as text
      * @return String output reply from Duke
      */
-    private String run(String fullCommand) {
+    public String getResponse(String fullCommand) {
         String reply;
         boolean isExit = false;
         try {
@@ -114,88 +94,5 @@ public class Duke extends Application {
         }
         assert !reply.isEmpty() : "Reply should not be empty";
         return ui.showOpeningLine() + reply + ui.showClosingLine();
-    }
-
-    @Override
-    public void start(Stage stage) {
-        //Step 1. Setting up required components
-        //The container for the content of the chat to scroll.
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        //Step 2. Formatting the window to look as expected
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(700.0);
-        stage.setMinWidth(600.0);
-
-        mainLayout.setPrefSize(600.0, 700.0);
-
-        scrollPane.setPrefSize(585, 635);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        // You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(80.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput, 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        //Scroll down to the end every time dialogContainer's height changes.
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-
-        // Output welcome text on the GUI
-        String welcomeText = ui.showWelcome();
-        Label welcomeLabel = new Label(welcomeText);
-        dialogContainer.getChildren().addAll(DialogBox.getDukeDialog(welcomeLabel, new ImageView(duke)));
-        userInput.clear();
-
-        //Step 3. Add functionality to handle user input.
-        sendButton.setOnMouseClicked((event) -> handleUserInput());
-
-        userInput.setOnAction((event) -> handleUserInput());
-    }
-
-    /**
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
-     */
-    private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(run(userInput.getText()));
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-        );
-        userInput.clear();
-        if (shouldExitProgram) {
-            PauseTransition delay = new PauseTransition(Duration.seconds(1));
-            delay.setOnFinished( event -> System.exit(0) );
-            delay.play();
-        }
     }
 }

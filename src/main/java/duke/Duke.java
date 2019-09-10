6b920ba3709728d14cd2cc432cb8d37858096057
Ellieyee <ellieyeewenna@gmail.com>
2019-09-10 20:42:54 +0800

    Refactor Duke subsystem (#27)

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,6 +2,7 @@ package duke;
 
 import duke.command.Command;
 import duke.command.CommandHistoryStack;
+import duke.command.EmptyHistoryException;
 import duke.command.UnknownCommandException;
 import duke.parser.ParserManager;
 import duke.task.TaskList;
@@ -36,18 +37,8 @@ public class Duke {
     public String getResponse(String input) {
         if(!input.trim().toLowerCase().equals("bye")) {
             try {
-                if (isUndoCommand(input)) {
-                    this.taskList = commandHistory.pop();
-                    dataStorage.storeTaskList(this.taskList);
-                    return commandHistory.getUndoMessage(this.taskList);
-                } else {
-                    Command command = parserManager.parseCommand(this.taskList, input.trim());
-                    assert command != null;
-                    commandHistory.update(command, this.taskList);
-                    String response = command.execute(this.taskList);
-                    dataStorage.storeTaskList(this.taskList);
-                    return response;
-                }
+                if (isUndoCommand(input)) { return executeUndo(); }
+                return executeCommand(input);
             } catch (UnknownCommandException e) {
                 return e.getMessage();
             } catch (IndexOutOfBoundsException e) {
@@ -59,7 +50,37 @@ public class Duke {
         return Ui.getGoodbyeMessage();
     }
 
-    private static boolean isUndoCommand(String fullCommand) {
+    /**
+     * Returns true if command complies to Undo Command Format, else returns false
+     */
+    private boolean isUndoCommand(String fullCommand) {
         return fullCommand.toLowerCase().trim().equals("undo");
+    }
+
+    /**
+     * Revert existing tasklist to state before last modification
+     * @return - String containing message for successful undo
+     * @throws EmptyHistoryException - if no more previous tasks to undo
+     */
+    private String executeUndo() throws EmptyHistoryException {
+        this.taskList = commandHistory.pop();
+        dataStorage.storeTaskList(this.taskList);
+        return commandHistory.getUndoMessage(this.taskList);
+    }
+
+    /**
+     * Executes command based on input and updates tasklist based on given change
+     * @param input given directly by user
+     * @return - String containing successful execution of command
+     * @throws UnknownCommandException - Format errors of command
+     * @throws RuntimeException - invalid input such as indexoutofbounds or numberformatexceptions
+     */
+    private String executeCommand(String input) throws UnknownCommandException, RuntimeException {
+        Command command = parserManager.parseCommand(this.taskList, input.trim());
+        assert command != null;
+        commandHistory.update(command, this.taskList);
+        String response = command.execute(this.taskList);
+        dataStorage.storeTaskList(this.taskList);
+        return response;
     }
 }

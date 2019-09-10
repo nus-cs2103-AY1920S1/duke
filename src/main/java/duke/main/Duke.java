855e907f754d439f0cb5f2a3f8928582686baa64
Lawnce Goh <lawnce@gmail.com>
2019-09-10 18:20:23 +0800

    A-CodeQuality

--- src/main/java/duke/main/Duke.java
+++ src/main/java/duke/main/Duke.java
@@ -1,4 +1,7 @@
+package duke.main;
+
 import duke.command.Command;
+import duke.controllers.MainWindow;
 import duke.exception.DukeException;
 import duke.parser.Parser;
 import duke.storage.Storage;
@@ -17,10 +20,9 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
 import java.text.ParseException;
 
-public class Duke extends Application {
+public class Duke {
 
     private Storage storage;
     private TaskList tasks;
@@ -57,7 +59,6 @@ public class Duke extends Application {
      *
      */
     public void run() {
-        ui.showWelcome();
         boolean isExit = false;
         while (!isExit) {
             try {
@@ -76,68 +77,6 @@ public class Duke extends Application {
         }
     }
 
-    @Override
-    public void start(Stage stage) {
-        //The container for the content of the chat to scroll.
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-        format(stage, mainLayout);
-
-        Scene scene = new Scene(mainLayout);
-        stage.setScene(scene);
-        stage.show() ;
-        //Part 3. Add functionality to handle user input.
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-
-    }
-
-    public void format(Stage stage, AnchorPane mainLayout) {
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        // You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-    }
-
     /**
      * Iteration 1:
      * Creates a label with the specified text and adds it to the dialog container.
@@ -145,7 +84,6 @@ public class Duke extends Application {
      * @return a label with the specified text that has word wrap enabled.
      */
     private Label getDialogLabel(String text) {
-        // You will need to import `javafx.scene.control.Label`.
         Label textToAdd = new Label(text);
         textToAdd.setWrapText(true);
 
@@ -153,34 +91,33 @@ public class Duke extends Application {
     }
 
     /**
-     * Iteration 2:
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
-     */
-    private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(getResponse(userInput.getText()));
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-        );
-        userInput.clear();
-    }
-
-    /**
      * You should have your own function to generate a response to user input.
      * Replace this stub with your completed method.
      */
-    private String getResponse(String input) {
-        return "Duke heard: " + input;
-    }
-
+ public String getResponse(String input) {
+     String[] wordArray = input.split(" ");
+     String command = wordArray[0];
+//     String remainingWords = input.substring(command.length() + 1);
+     boolean isExit = false;
+     while (!isExit) {
+         try {
+             Command c = Parser.parse(command);
+             c.execute(tasks, ui, storage);
+             isExit = c.isExit();
+         } catch (DukeException e) {
+             ui.showError(e.getMessage());
+         } catch (ParseException e) {
+             System.out.println(e.getMessage());
+         }
+     }
+     return "";
+ }
 
 
     /**
      * Runs the main method to load the duke.txt file into the program and then calls the run method
      * @param args
-     * @throws DukeException when an error of Duke occurs
+     * @throws DukeException when an error of duke.main.Duke occurs
      * @throws ParseException when a string is not parsed properly
      */
     public static void main(String[] args) throws DukeException, ParseException{

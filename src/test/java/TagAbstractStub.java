d7cd3d01aaa7a3eff8569e2af43ad3bcfe84238b
e0031374 <e0031374@u.nus.edu>
2019-09-10 14:21:13 +0800

    TagStore.java complete and with some test

---
+++ src/test/java/TagAbstractStub.java
@@ -0,0 +1,16 @@
+public class TagAbstractStub extends TagAbstractWrapper {
+    private String containsStr;
+    public TagAbstractStub(String str) {
+        this.containsStr = str;
+    } 
+
+    @Override
+    public String tagCode() {
+        return this.toString();
+    }
+
+    @Override
+    public String toString() {
+        return this.containsStr;
+    }
+}

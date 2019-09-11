a1b01bef7ff82e23365e1120c46090d0cbeadf56
Jun Wei <42290013+bigjunnn@users.noreply.github.com>
2019-09-11 21:25:27 +0800

    Update DeadLineTest.java

--- src/test/java/DeadLineTest.java
+++ src/test/java/DeadLineTest.java
@@ -6,24 +6,24 @@ import tasks.DeadLine;
 
 public class DeadLineTest {
 
-  @Test
-  public void testStringConversion() {
-    assertEquals("[D] [ ] return book (by:Sunday)", new DeadLine("return book", "Sunday").toString());
-    assertEquals("[D] [ ] sign book (by:Monday)", new DeadLine("sign book", "Monday").toString());
-  }
+    @Test
+    public void testStringConversion() {
+        assertEquals("[D] [ ] return book (by:Sunday)", new DeadLine("return book", "Sunday").toString());
+        assertEquals("[D] [ ] sign book (by:Monday)", new DeadLine("sign book", "Monday").toString());
+    }
 
-  @Test
-  public void testSetAsDone() {
-    DeadLine d = new DeadLine("return book", "Sunday");
-    d.setAsDone();
-    assertTrue(d.getBoolean());
-  }
+    @Test
+    public void testSetAsDone() {
+        DeadLine d = new DeadLine("return book", "Sunday");
+        d.setAsDone();
+        assertTrue(d.getBoolean());
+    }
 
-  @Test
-  public void testGetStatusIcon() {
-    DeadLine d = new DeadLine("return book", "Sunday");
-    assertEquals(" ", d.getStatusIcon());
-    d.setAsDone();
-    assertEquals("X", d.getStatusIcon());
-  }
+    @Test
+    public void testGetStatusIcon() {
+        DeadLine d = new DeadLine("return book", "Sunday");
+        assertEquals(" ", d.getStatusIcon());
+        d.setAsDone();
+        assertEquals("X", d.getStatusIcon());
+    }
 }

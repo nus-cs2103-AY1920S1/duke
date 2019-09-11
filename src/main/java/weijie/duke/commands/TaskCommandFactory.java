cbda83b274f2ca2c87ac14cb7628fbcf1db7e5d5
Huang Weijie <h_weijie@yahoo.com>
2019-09-11 10:25:50 +0800

    [Feat] Added heroku deployment

--- src/main/java/weijie/duke/commands/TaskCommandFactory.java
+++ src/main/java/weijie/duke/commands/TaskCommandFactory.java
@@ -69,12 +69,7 @@ public class TaskCommandFactory {
      *         be found.
      */
     private Object[] generateConstructorDependencies(Class<?>[] constructorClasses) {
-//        IntStream.range(0, constructorClasses.length)
-//                .boxed()
-//                .flatMap(index -> registeredDependencies.stream()
-//                        .filter(dependency -> constructorClasses[index].isInstance(dependency))
-//                        .limit(1))
-//                .toArray();
+
         Object[] dependencies = Arrays.stream(constructorClasses)
                 .flatMap(paramClass -> registeredDependencies.stream()
                         .filter(paramClass::isInstance)

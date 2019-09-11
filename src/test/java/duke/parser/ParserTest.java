9926f0fcb1014aceea33130f4b042d7c30684bb5
gabrielseow <e0316039@u.nus.edu>
2019-09-11 15:25:16 +0800

    A-JUnit halway complete

---
+++ src/test/java/duke/parser/ParserTest.java
@@ -0,0 +1,25 @@
+package java.duke.parser;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ParserTest {
+
+    @Test
+    void parse() {
+    }
+
+    @Test
+    void findTasksByKeyword() {
+    }
+
+    @Test
+    void convertDateTime() {
+        assertEquals(2,2);
+        assertEquals(2,3);
+    }
+
+    @Test
+    void parseSaveData() {
+    }
+}

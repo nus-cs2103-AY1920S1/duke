64d6825343b32d041b66f35e95af3296b1705d78
brianyenna <brianyen.na@gmail.com>
2019-09-11 19:52:12 +0800

    Updated Parser. Replace ByeCommand with SaveCommand as "bye" no longer closes the window. Instead, typing "save" will not explicitly save the data.
    Note that this is not technically a refactoring as external behaviour is changed.

--- src/main/java/duke/commands/SaveCommand.java
+++ src/main/java/duke/commands/SaveCommand.java
@@ -5,24 +5,21 @@ import duke.utils.Storage;
 import duke.utils.TaskList;
 import duke.utils.UiResponse;
 
-/** Command invoked when "bye" command is encountered. */
-public class ByeCommand extends Command {
+/** Command invoked when "save" command is encountered. */
+public class SaveCommand extends Command {
     /**
      * Upon execution, will change the <code>isExit</code> boolean is true. Will also
      * save the TaskList a final time before Duke exits.
      *
      * @param ui       UiResponse object that is responsible for returning a String response to GUI
-     * @param storage  Storage object respnsible for saving the Tasks into a pre-defined format
+     * @param storage  Storage object responsible for saving the Tasks into a pre-defined format
      * @param allTasks TaskList object containing all tasks.
      * @return String representing Duke's response
      * @throws DukeException re-thrown from underlying method calls.
      */
     public String execute(UiResponse ui, Storage storage, TaskList allTasks) throws DukeException {
-        //Will cause while loop in Duke to exit
-        this.isExit = true;
-
         //Save TaskList
         storage.save(allTasks);
-        return "Bye! Duke will close the window now!";
+        return "Duke has saved the latest data!";
     }
 }

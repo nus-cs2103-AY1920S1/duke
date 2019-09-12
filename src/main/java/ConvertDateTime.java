0fbc0062422932e7c5594421f5cb0403535fef7c
LeonardTay748 <leonardtay748@gmail.com>
2019-09-12 21:11:28 +0800

    Made Date&Time Converter into class

---
+++ src/main/java/ConvertDateTime.java
@@ -0,0 +1,91 @@
+public class ConvertDateTime {
+    public String str;
+
+    ///////////////// constructor ////////////////////////////
+    public ConvertDateTime(String timeframe){
+
+        int in = timeframe.indexOf('/');                           //find first instance of '/'
+        int day = Integer.parseInt(timeframe.substring(0, in));    //sift out day
+        String sub1 = timeframe.substring(in+1);                   //substring: month onwards
+        int in2 = sub1.indexOf('/');
+        int month =  Integer.parseInt(sub1.substring(0, in2));     //sift out month
+        String sub2 = sub1.substring(in2+1);                       //substring: year onwards
+        String year = sub2.substring(0, 4);                        //sift out year
+        String time = sub2.substring(5);                           //sift out time (24hr clock format)
+
+        String converted_day;
+        switch (day){
+            case 1:
+            case 21:
+            case 31:
+                converted_day = Integer.toString(day) + "st";
+                break;
+            case 2:
+            case 22:
+                converted_day = Integer.toString(day) + "nd";
+                break;
+            case 3:
+            case 33:
+                converted_day = Integer.toString(day) + "rd";
+                break;
+            default:
+                converted_day = Integer.toString(day) + "th";
+                break;
+        }
+
+        String converted_month;
+        switch (month){
+            case 1:
+                converted_month = "January"; break;
+            case 2:
+                converted_month = "February"; break;
+            case 3:
+                converted_month = "March"; break;
+            case 4:
+                converted_month = "April"; break;
+            case 5:
+                converted_month = "May"; break;
+            case 6:
+                converted_month = "June"; break;
+            case 7:
+                converted_month = "July"; break;
+            case 8:
+                converted_month = "August"; break;
+            case 9:
+                converted_month = "September"; break;
+            case 10:
+                converted_month = "October"; break;
+            case 11:
+                converted_month = "November"; break;
+            case 12:
+                converted_month = "December"; break;
+            default:
+                converted_month = ""; break;
+        }
+
+        String period;                                 //indicate AM or PM
+        int hour = Integer.parseInt(time.substring(0,2));
+        if(hour>=12)
+            period = "pm";
+        else
+            period = "am";
+
+        String converted_hr;
+        if((Integer.parseInt(time.substring(0,2))) > 12) {
+            converted_hr = Integer.toString((Integer.parseInt(time.substring(0, 2))) - 12);
+        }
+        else{
+            converted_hr = Integer.toString((Integer.parseInt(time.substring(0, 2))));
+        }
+
+        String converted_min;
+        int min = Integer.parseInt(time.substring(2));
+        if(min>0)
+            converted_min = ":" + time.substring(2);
+        else
+            converted_min = "";
+
+        this.str = converted_day + " of " + converted_month + " " + year + ", " + converted_hr + converted_min + period ;
+    }
+
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -28,106 +28,18 @@ public class Duke{
     //private TaskList tasks;
     private Ui ui;
 
-    /////////////////////////// Date & Time Converter: returns the converted format as a string //////////////////////////
-    static String Convert(String timeframe){
-
-          int in = timeframe.indexOf('/');                           //find first instance of '/'
-          int day = Integer.parseInt(timeframe.substring(0, in));    //sift out day
-          String sub1 = timeframe.substring(in+1);                   //substring: month onwards
-          int in2 = sub1.indexOf('/');
-          int month =  Integer.parseInt(sub1.substring(0, in2));     //sift out month
-          String sub2 = sub1.substring(in2+1);                       //substring: year onwards
-          String year = sub2.substring(0, 4);                        //sift out year
-          String time = sub2.substring(5);                           //sift out time (24hr clock format)
-
-          String converted_day;
-          switch (day){
-              case 1:
-              case 21:
-              case 31:
-                  converted_day = Integer.toString(day) + "st";
-                  break;
-              case 2:
-              case 22:
-                  converted_day = Integer.toString(day) + "nd";
-                  break;
-              case 3:
-              case 33:
-                  converted_day = Integer.toString(day) + "rd";
-                  break;
-              default:
-                  converted_day = Integer.toString(day) + "th";
-                  break;
-          }
-
-          String converted_month;
-          switch (month){
-              case 1:
-                  converted_month = "January"; break;
-              case 2:
-                  converted_month = "February"; break;
-              case 3:
-                  converted_month = "March"; break;
-              case 4:
-                  converted_month = "April"; break;
-              case 5:
-                  converted_month = "May"; break;
-              case 6:
-                  converted_month = "June"; break;
-              case 7:
-                  converted_month = "July"; break;
-              case 8:
-                  converted_month = "August"; break;
-              case 9:
-                  converted_month = "September"; break;
-              case 10:
-                  converted_month = "October"; break;
-              case 11:
-                  converted_month = "November"; break;
-              case 12:
-                  converted_month = "December"; break;
-              default:
-                  converted_month = ""; break;
-          }
-
-          String period;                                 //indicate AM or PM
-          int hour = Integer.parseInt(time.substring(0,2));
-          if(hour>=12)
-              period = "pm";
-          else
-              period = "am";
-
-        String converted_hr;
-        if((Integer.parseInt(time.substring(0,2))) > 12) {
-              converted_hr = Integer.toString((Integer.parseInt(time.substring(0, 2))) - 12);
-        }
-        else{
-            converted_hr = Integer.toString((Integer.parseInt(time.substring(0, 2))));
-        }
-
-          String converted_min;
-          int min = Integer.parseInt(time.substring(2));
-          if(min>0)
-              converted_min = ":" + time.substring(2);
-          else
-              converted_min = "";
-
-          return converted_day + " of " + converted_month + " " + year + ", " + converted_hr + converted_min + period ;
-    }
-
-    /////////////////////////////// End of DATE & TIME Converter ///////////////////////////////////////////////////
-
-
-    /////////////////////////////// Duke constructor  ///////////////////////////////////////////////////
+    /////////////////////////////// Duke constructors  ///////////////////////////////////////////////////
     public Duke(String filePath) {
         ui = new Ui();
         storage = new Storage(filePath);
 
     }
-    /////////////////////////////// End of Duke constructor //////////////////////////////////////////
     public Duke() {
         this("src/main/java/Duke.txt");
     }
+
+    /////////////////////////////// End of Duke constructor //////////////////////////////////////////
+
 
     //////////////////////////////////// start of run //////////////////////////////////////////////
     public void run() throws IOException {
@@ -195,8 +107,8 @@ public class Duke{
                             String temp = timeFrame.substring(3);
                             String sub = userInput.substring(9, index - 1);
 
-                            String new_timeFrame = Convert(temp);
-                            Task t = new Task(sub, 'D', 0, new_timeFrame);
+                            ConvertDateTime new_timeFrame = new ConvertDateTime(temp);
+                            Task t = new Task(sub, 'D', 0, new_timeFrame.str);
                             taskList.add(t);
                             no_of_task++;
                             storage.AutoSave(taskList, no_of_task);
@@ -212,8 +124,8 @@ public class Duke{
                                 String temp = timeFrame.substring(3);
                                 String sub = userInput.substring(6, index - 1);
 
-                                String new_timeFrame = Convert(temp);
-                                Task t = new Task(sub, 'E', 0, new_timeFrame);
+                                ConvertDateTime new_timeFrame = new ConvertDateTime(temp);
+                                Task t = new Task(sub, 'E', 0, new_timeFrame.str);
                                 taskList.add(t);
                                 no_of_task++;
                                 storage.AutoSave(taskList, no_of_task);

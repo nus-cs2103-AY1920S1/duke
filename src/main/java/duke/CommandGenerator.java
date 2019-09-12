2852a6ac8ae3a299518f7f37661dd1fa97505b51
Chen Su <chensu@comp.nus.edu.sg>
2019-09-12 10:23:22 +0800

    Add assertions to document important assumptions.

--- src/main/java/duke/CommandGenerator.java
+++ src/main/java/duke/CommandGenerator.java
@@ -23,6 +23,8 @@ public class CommandGenerator {
                     return new DeadlineCommand(fullCommand);
                 case "event":
                     return new EventCommand(fullCommand);
+                default:
+                    assert false : parser.getCommandType();
             }
         }
         return new Command(fullCommand);

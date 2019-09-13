9179c1b6749f9c14ae3356a836f821c2a5da4ffb
Kendrick Ang <phantom2457@hotmail.com>
2019-09-13 13:02:44 +0800

    revert test cases in DeadlineTest

--- src/test/java/duke/task/DeadlineTest.java
+++ src/test/java/duke/task/DeadlineTest.java
@@ -11,11 +11,4 @@ class DeadlineTest {
         DeadlineStub deadline = new DeadlineStub("Test", "031219 1800", false);
         assertEquals("031219 1800", deadline.getDeadline());
     }
-
-    @Test
-    void deadlineToStringManualInputCorrectFormat() {
-        // remove dependency
-        DeadlineStub deadline = new DeadlineStub("Testing", "031219 1800", false);
-        assertEquals("[D][X] Testing (by: 03 December 2019, 06:00PM, GMT+8)", deadline.toString());
-    }
 }

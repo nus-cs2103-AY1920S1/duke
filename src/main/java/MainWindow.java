9e2418884ed863036f312df388cb206af864a24b
robytanama <tanamaroby@gmail.com>
2019-09-13 14:04:43 +0800

    Fixed the logo due to copyright issues

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -22,8 +22,8 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("images/Joker.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("images/Morgana.jpg"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("images/User.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("images/Bot.png"));
 
     @FXML
     public void initialize() {

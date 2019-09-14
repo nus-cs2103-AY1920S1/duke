b5556c384306ae9d90ea55debc0aa1fb753e8a1a
AhmadHatziq <e0031550@u.nus.edu>
2019-09-14 19:46:50 +0800

    Updated java docs for core classes

--- src/main/java/seedu/duke/core/Launcher.java
+++ src/main/java/seedu/duke/core/Launcher.java
@@ -6,8 +6,12 @@ import javafx.application.Application;
  * A launcher class to workaround classpath issues.
  */
 public class Launcher {
+    /**
+     * Main entry point for Duke GUI.
+     *
+     * @param args Main entry point for Duke GUI.
+     */
     public static void main(String[] args) {
-
         Application.launch(Main.class, args);
     }
 }

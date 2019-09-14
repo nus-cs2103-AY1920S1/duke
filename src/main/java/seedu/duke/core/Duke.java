51c5beeb052f20eae5f2c1fba92cc657adbf8527
AhmadHatziq <e0031550@u.nus.edu>
2019-09-14 15:22:55 +0800

    Remove leftover code from Duke main, which has been ported to Command subclasses

--- src/main/java/seedu/duke/core/Duke.java
+++ src/main/java/seedu/duke/core/Duke.java
@@ -115,7 +115,6 @@ public class Duke {
      * @return Duke's response.
      */
     public String getResponse(String input) {
-        String command = Parser.parseCommand(input);
 
         try {
             // Loads the data from txt file to the TaskList object, tasks.
@@ -130,9 +129,13 @@ public class Duke {
             System.out.println(e.getMessage());
         }
 
-        String output = "";
+        String outputString = "";
+        Command command = new UnknownCommand();
+
         try {
-            output = executeTasksGui(input);
+
+            command = Parser.getCommand(input, gui);
+            outputString = command.execute(input, gui, tasks, taskStorage, stats, statStorage);
 
             // Saves tasks to txt file before returning output to GUI.
             this.taskStorage.clearTaskFileBeforeSaving();
@@ -140,25 +143,30 @@ public class Duke {
                 this.taskStorage.writeToTaskFile(this.tasks.getTask(i).toSaveString());
             }
 
-            // Saves stats file before returning output to GUI.
+            // Saves stats file before returning output.
             statStorage.saveStatFile(stats);
 
-            return output;
+
         } catch (DukeException e) {
+
             DukeErrorInterface dei = (e1) -> e1.getMessage();
-            return dei.getLoadingError(e);
+            outputString = dei.getLoadingError(e);
+
         } catch (IOException e) {
-            return e.getMessage();
+
+            outputString = e.getMessage();
+
         }
+
+        return outputString;
     }
 
     /**
      * Executes the Duke Command Line Interface.
      */
     public void run() {
-        Boolean isCommandLineInterface = true;
         System.out.println(cli.getWelcomeString());
-        // Boolean isBye = false;
+
         try {
             // Loads the data from txt file to the TaskList object, tasks.
             tasks = new TaskList(this.taskStorage.loadTasks());
@@ -189,353 +197,8 @@ public class Duke {
                 outputString = e.getMessage();
             }
             System.out.println(outputString);
-            //fullCommand = in.nextLine().trim();
         }
 
-        /*
-        while (!isBye) {
-            try {
-                isBye = executeTasksCli(fullCommand);
-            } catch (DukeException e) {
-                DukeErrorInterface dei = (e1) -> e1.getMessage();
-                System.out.println(dei.getLoadingError(e));
-            } catch (StringIndexOutOfBoundsException e) {
-                System.out.println(e.getMessage());
-            } catch (NumberFormatException e) {
-                System.out.println(e.getMessage());
-            } catch (IOException e) {
-                System.out.println(e.getCause());
-            }
-            if (isBye) {
-                break;
-            }
-            fullCommand = in.nextLine().trim();
-        }
-
-         */
-    }
-
-    /**
-     * Executes the Command Line Interface for Duke.
-     *
-     * @param fullCommand User input string.
-     * @return Boolean isBye. If command is "bye", will return true and exit the while loop.
-     * @throws DukeException To catch some invalid commands.
-     * @throws IOException To catch file error when interacting with Storage class.
-     */
-    public Boolean executeTasksCli(String fullCommand) throws DukeException, IOException {
-
-        /*
-        // parseCommand will return the first word of fullCommand
-        String taskType = Parser.parseCommand(fullCommand);
-
-        switch(taskType) {
-
-        case ("list"):
-            System.out.println(listRoutine(cli, tasks));
-            break;
-        case ("done"):
-            System.out.println(doneRoutine(cli, tasks, fullCommand));
-            break;
-        case("todo"):
-            System.out.println(todoRoutine(cli, tasks, fullCommand));
-            break;
-        case("deadline"):
-            System.out.println(deadlineRoutine(cli, tasks, fullCommand));
-            break;
-        case("event"):
-            System.out.println(eventRoutine(cli, tasks, fullCommand));
-            break;
-        case("delete"):
-            System.out.println(deleteRoutine(cli, tasks, fullCommand));
-            break;
-        case("find"):
-            System.out.println(findRoutine(cli, tasks, fullCommand));
-            break;
-        case("bye"):
-            System.out.println(byeRoutineCli(cli, tasks, taskStorage, stats, statStorage));
-            return true;
-        case("stats"):
-            String statCommandType = Parser.parseStatCommand(fullCommand);
-            switch(statCommandType){
-            case ("all"):
-                System.out.println(getAllStatsRoutine(cli, stats));
-                break;
-            case ("reset"):
-                System.out.println(resetStatsRoutine(cli, stats));
-                break;
-            case ("event"):
-                System.out.println(completedEventStatsRoutine(cli, stats, tasks));
-                break;
-            default:
-                unknownCommandRoutine();
-            }
-            break;
-
-        default:
-            unknownCommandRoutine();
-            return false;
-        }
-        return false;
-
-         */
-        return true;
-    }
-
-    /**
-     * Executes the Graphical User Interface logic for Duke.
-     *
-     * @param fullCommand Input string obtained from GUI.
-     * @return Output string from Ui class.
-     * @throws DukeException Handles case when command is recognized eg"list" but following description is unrecognized.
-     * @throws IOException Storage class txt file handling.
-     */
-    public String executeTasksGui(String fullCommand) throws DukeException, IOException {
-        String taskType = Parser.parseCommand(fullCommand);
-
-        switch (taskType){
-        case ("list"):
-            return listRoutine(gui, tasks);
-        case ("done"):
-            return doneRoutine(gui, tasks, fullCommand);
-        case("todo"):
-            return todoRoutine(gui, tasks, fullCommand);
-        case("deadline"):
-            return deadlineRoutine(gui, tasks, fullCommand);
-        case("event"):
-            return eventRoutine(gui, tasks, fullCommand);
-        case("delete"):
-            return deleteRoutine(gui, tasks, fullCommand);
-        case("find"):
-            return findRoutine(gui, tasks, fullCommand);
-        case("bye"):
-            return byeRoutineGui(gui, tasks);
-        default:
-            unknownCommandRoutine();
-            return null;
-        }
-    }
-
-    /**
-     * Handles the case when the user command is unrecognized.
-     *
-     * @throws DukeException Error class.
-     */
-    public void unknownCommandRoutine() throws DukeException {
-        throw new DukeException(":-( OOPS!!! I'm sorry, but I don't know what that means :-(");
-    }
-
-    public String completedEventStatsRoutine(Ui ui, Statistic stats, TaskList tasks) {
-        stats.incrementTotalCommandsExecuted();
-        return ui.getCompletedEventStatSequence(stats,tasks );
-    }
-
-    /**
-     * Executes and returns the String for the LIST command.
-     *
-     * @param ui Ui object.
-     * @param tasks Tasklist object.
-     * @return String that needs to be printed to the user after LIST command.
-     */
-    public String listRoutine(Ui ui, TaskList tasks) {
-        stats.incrementTotalCommandsExecuted();
-        return ui.getListSequence(tasks);
-    }
-
-    /**
-     * Executes and returns the String for BYE command for CLI.
-     * For CLI, txt file is saved during BYE command.
-     *
-     *
-     */
-    public String byeRoutineCli(Ui ui, TaskList tasks, Storage taskStorage, Statistic stat, Storage statStorage) throws IOException {
-        stats.incrementTotalCommandsExecuted();
-        // Clear the tasks txt file and adds headers.
-        taskStorage.clearTaskFileBeforeSaving();
-
-        // Saves the task list to the file, following the pre-defined format.
-        for (int i = 0; i < tasks.getSize(); i++) {
-            taskStorage.writeToTaskFile(tasks.getTask(i).toSaveString());
-        }
-
-        // Saves the stats data to the stats file.
-        statStorage.saveStatFile(stat);
-
-        return ui.getByeSequence();
-    }
-
-    /**
-     * Executes and returns the String for BYE command for GUI.
-     * For GUI, txt file is saved after every command.
-     *
-     * @param ui Ui object.
-     * @param tasks TaskList object.
-     * @return Bye string sequence
-     */
-    public String byeRoutineGui(Ui ui, TaskList tasks) {
-        stats.incrementTotalCommandsExecuted();
-        // For GUI, txt file is always saved after each change
-        return (ui.getByeSequence());
-    }
-
-    /**
-     * Executes and returns the String for FIND command.
-     *
-     * @param ui Ui object.
-     * @param tasks TaskList object.
-     * @param fullCommand User command String.
-     * @return Find string sequence.
-     */
-    public String findRoutine(Ui ui, TaskList tasks, String fullCommand) {
-        stats.incrementTotalCommandsExecuted();
-        // Parser will parse the command and obtain the searchString.
-        // findSimilarTasks will return a TaskList containing only matching tasks.
-        TaskList similarTasks = tasks.findSimilarTasks(Parser.getFindTask(fullCommand));
-        return ui.getFoundTasks(similarTasks);
-    }
-
-    /**
-     * Executes and returns the String for DELETE command.
-     *
-     * @param ui Ui object.
-     * @param tasks TaskList object.
-     * @param fullCommand User command String.
-     * @return Delete string sequence.
-     * @throws DukeException Duke exception to handle invalid task index.
-     */
-    public String deleteRoutine(Ui ui, TaskList tasks, String fullCommand) throws DukeException {
-        stats.incrementTotalCommandsExecuted();
-        int taskNum = Parser.getDeletedTaskNum(fullCommand);
-
-        // taskList index (starts from 0) differs from taskNum (starts from 1) by 1.
-        taskNum--;
-
-        if (taskNum >= tasks.getSize()) {
-            // taskNum does not exist.
-            throw new DukeException("Task no. " + (taskNum + 1) + " does not exist");
-        }
-
-        Task taskToDelete = tasks.getTask(taskNum);
-        tasks.deleteTask(taskNum);
-
-        stats.incrementTotalTasksDeleted();
-
-        return ui.getDeleteSequence(tasks, taskToDelete);
-    }
-
-    /**
-     * Executes and returns the String EVENT command.
-     *
-     * @param ui Ui object.
-     * @param tasks TaskList object.
-     * @param fullCommand User command string.
-     * @return Event String sequence.
-     * @throws DukeException Handles the error case when there is no description or location.
-     */
-    public String eventRoutine(Ui ui, TaskList tasks, String fullCommand) throws DukeException {
-        stats.incrementTotalCommandsExecuted();
-        if ((fullCommand.length() < 6)) {
-            // Input is only "event".
-
-            // fullCommand contains only the string "event".
-            throw new DukeException("☹ OOPS!!! The description of an event cannot be empty.");
-
-        } else if ((fullCommand.lastIndexOf('/') < 1)
-                || (4 + fullCommand.lastIndexOf('/') > fullCommand.length()))  {
-            // fullCommand does not contain '/' char or there are no chars after "/at".
-            throw new DukeException(":-( OOPS!!! The location of an event cannot be empty.");
-        }
-        String description = Parser.getEventDescription(fullCommand);
-        String extraDescription = Parser.getEventLocation(fullCommand);
-
-        Event newEvent = new Event(description, extraDescription);
-
-        tasks.addTask(newEvent);
-
-        return ui.getEventSequence(tasks, newEvent);
-    }
-
-    /**
-     * Executes and returns the String DEADLINE command.
-     *
-     * @param ui Ui object.
-     * @param tasks TaskList object.
-     * @param fullCommand User command string.
-     * @return Deadline string sequence
-     * @throws DukeException To handle the case when description or time period is empty.
-     */
-    public String deadlineRoutine(Ui ui, TaskList tasks, String fullCommand) throws DukeException {
-        stats.incrementTotalCommandsExecuted();
-        if ((fullCommand.length() < 9)) {
-
-            // fullCommand contains only the string "deadline".
-            throw new DukeException("☹ OOPS!!! The description of an event cannot be empty.");
-
-        } else if ((fullCommand.lastIndexOf('/') < 1)
-                || (4 + fullCommand.lastIndexOf('/') > fullCommand.length()))  {
-
-            // fullCommand does not contain '/' chars or there are no char after "/by".
-            throw new DukeException("☹ OOPS!!! The time period of an event cannot be empty.");
-
-        }
-
-        String description = Parser.getDeadlineDescription(fullCommand);
-        String extraDescription = Parser.getDeadlineDateTime(fullCommand);
-
-        assert description != null : "Description should not be null";
-        assert extraDescription != null : "Extra Description should not be null";
-
-        Deadline newDeadline = new Deadline(description, extraDescription);
-
-        tasks.addTask(newDeadline);
-
-        return ui.getDeadlineSequence(tasks, newDeadline);
-    }
-
-    /**
-     * Executes and returns the String TODO command.
-     *
-     * @param ui Ui object.
-     * @param tasks TaskList object.
-     * @param fullCommand User command string.
-     * @return Todo string sequence.
-     * @throws DukeException Handles the case when description is empty.
-     */
-    public String todoRoutine(Ui ui, TaskList tasks, String fullCommand) throws DukeException {
-        stats.incrementTotalCommandsExecuted();
-        if (fullCommand.length() < 5) {
-            // fullCommand contains only the string "todo".
-            throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
-        }
-
-        String description = Parser.getTodoDescription(fullCommand);
-
-        Todo newTodo = new Todo(description);
-
-        tasks.addTask(newTodo);
-
-        return ui.getTodoSequence(tasks, newTodo);
-    }
-
-    /**
-     * Executes and returns the String for DONE command.
-     *
-     * @param ui Ui object.
-     * @param tasks TaskList object.
-     * @param fullCommand User command string.
-     * @return Done string sequence.
-     */
-    public String doneRoutine(Ui ui, TaskList tasks, String fullCommand) {
-        stats.incrementTotalCommandsExecuted();
-
-        int taskNum = Parser.getFinishedTaskNum(fullCommand);
-
-        // taskList index (starts from 0) differs from taskNum (starts from 1) by 1,
-        taskNum--;
-
-        tasks.getTask(taskNum).setDone(stats);
-
-        return ui.getDoneSequence(tasks, taskNum);
     }
 
     /**
@@ -547,29 +210,6 @@ public class Duke {
         new Duke("C:\\Users\\hatzi\\Documents\\Sourcetree\\duke\\data\\tasks.txt").run();
     }
 
-    public String resetStatsRoutine(Ui ui, Statistic stats) {
-        stats.resetStats();
-        return ui.getResetStatSequence(stats);
-    }
-
-    public String getAllStatsRoutine(Ui ui, Statistic stats) {
-        stats.incrementTotalCommandsExecuted();
-        return ui.getAllStatSequence(stats);
-    }
-
-    /**
-     * possibleTasks  is an enumeration of the constant, pre-defined, recognizable commands.
-     */
-    enum PossibleTasks {
-        BYE,
-        LIST,
-        DONE,
-        DELETE,
-        EVENT,
-        TODO,
-        DEADLINE,
-        FIND
-    }
 }
 
 

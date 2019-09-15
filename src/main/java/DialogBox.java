25ad658600ebf1fdff87a9cb74bd726376148f57
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2019-09-15 21:46:41 +0800

    Fix text-clipping issue with dialog boxes

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -11,6 +11,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 
 /**
  * An example of a custom control using FXML.
@@ -40,6 +41,10 @@ public class DialogBox extends HBox {
 
         dialog.setText(text);
         displayPicture.setImage(img);
+        
+        setMinHeight(
+            Region.USE_PREF_SIZE
+        );
     }
 
     /**

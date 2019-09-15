97cfb137df9ed8beb063784b3b92c22250e0cab5
qiujiaaa <ooiqiujia@hotmail.com>
2019-09-15 19:55:48 +0800

    update user picture

--- src/main/java/duke/ui/MainWindow.java
+++ src/main/java/duke/ui/MainWindow.java
@@ -26,7 +26,7 @@ public class MainWindow extends AnchorPane {
     private Duke duke;
 
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/Snufkin.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/Moomin.png"));
 
     public void showWelcomeMessage() {
         dialogContainer.getChildren().addAll(

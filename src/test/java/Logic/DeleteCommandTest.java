0f4352ed8a9de5336081dec5acad6a72a9f5e18a
AeroSpectr3 <32993514+AeroSpectr3@users.noreply.github.com>
2019-09-16 07:17:39 +0800

    Added tests

--- src/test/java/Logic/DeleteCommandTest.java
+++ src/test/java/Logic/DeleteCommandTest.java
@@ -9,26 +9,25 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TodoCommandTest {
+public class DeleteCommandTest {
 
     @Test
-    void execute_todo_test(){
+    void execute_delete_test(){
         UI ui = new UI_CLI();
         Storage storage = new Storage();
 
         Tasklist expectedTasks = new Tasklist();
-        expectedTasks.add(new todo("Borrow a Book"));
 
         Tasklist actualTasks = new Tasklist();
+        actualTasks.add(new todo("Borrow a Book"));
 
-        Command command = new TodoCommand("Borrow a Book");
+        Command command = new DeleteCommand("1");
         String actualOutput = command.execute(actualTasks, ui, storage);
         String expectedOutput = "Got it. I've added this task:\n" +
-                                "[T][x] Borrow a Book\n" +
-                                "Now you have 1 tasks in this list\n";
+                "[T][x] Borrow a Book\n" +
+                "Now you have 1 tasks in this list\n";
 
-        assertEquals(expectedTasks.get(0).getDetails(), actualTasks.get(0).getDetails());
+        //assertEquals(expectedTasks.get(0).getDetails(), actualTasks.get(0).getDetails());
         assertEquals(actualOutput, expectedOutput);
     }
-
 }
--- src/test/java/Logic/WrongCommandTest.java
+++ src/test/java/Logic/WrongCommandTest.java
@@ -9,26 +9,23 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TodoCommandTest {
+public class WrongCommandTest {
 
     @Test
-    void execute_todo_test(){
+    void execute_delete_test(){
         UI ui = new UI_CLI();
         Storage storage = new Storage();
 
         Tasklist expectedTasks = new Tasklist();
-        expectedTasks.add(new todo("Borrow a Book"));
 
         Tasklist actualTasks = new Tasklist();
+        actualTasks.add(new todo("Borrow a Book"));
 
-        Command command = new TodoCommand("Borrow a Book");
+        CommandParser commandParser = new CommandParser(" ");
+        Command command = commandParser.parseCommand("This is a invalid command");
         String actualOutput = command.execute(actualTasks, ui, storage);
-        String expectedOutput = "Got it. I've added this task:\n" +
-                                "[T][x] Borrow a Book\n" +
-                                "Now you have 1 tasks in this list\n";
+        String expectedOutput = "OOPS!!! I'm sorry, but I don't know what that means :-(\n";
 
-        assertEquals(expectedTasks.get(0).getDetails(), actualTasks.get(0).getDetails());
         assertEquals(actualOutput, expectedOutput);
     }
-
 }

2413970a98653b7daa2506d446e25f81b05cf85a
tohcejasmine <e0104958@u.nus.edu>
2019-09-16 10:59:02 +0800

    Add in todos for extension

---
+++ src/main/java/com/util/DukeStatistics.java
@@ -0,0 +1,18 @@
+package com.util;
+
+public class DukeStatistics {
+
+    // TODO number of tasks mark done
+        // TODO so far
+        // TODO in the past week/ in every week
+        // TODO number of done task for each type
+    // TODO number of tasks of each type
+    // TODO number of times used command
+    // TODO number of tasks deleted
+        // TODO number of done/not done tasks deleted
+    
+    public DukeStatistics() {
+
+    }
+
+}

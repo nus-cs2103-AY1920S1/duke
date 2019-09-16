87dc83f361bcd8dbda4f8de9d68aaa20733d3f00
abhiman22 <abhimanyadav@gmail.com>
2019-09-16 23:03:18 +0800

    Improve Duke UI.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
-            fxmlLoader.<MainWindow>getController().setDuke(duke);
+            fxmlLoader.<MainWindow>getController().setVariables(duke, stage);
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,3 +1,4 @@
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -5,6 +6,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * Controller for MainWindow. Provides the layout for the other controls.
@@ -20,6 +23,7 @@ public class MainWindow extends AnchorPane {
     private Button sendButton;
 
     private Duke duke;
+    private Stage stage;
 
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/User.png"));
     private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/Duke.png"));
@@ -36,8 +40,9 @@ public class MainWindow extends AnchorPane {
      * @param d The duke object to be referenced by this object's 
      *     duke object.
      */
-    public void setDuke(Duke d) {
+    public void setVariables(Duke d, Stage s) {
         duke = d;
+        stage = s;
     }
 
     /**
@@ -65,5 +70,15 @@ public class MainWindow extends AnchorPane {
                 DialogBox.getDukeDialog(response, dukeImage)
         );
         userInput.clear();
+        if (input.toLowerCase().equals("bye")) {
+            stageCloser(stage);
+        }
     }
+
+    private void stageCloser(Stage stage) {
+        PauseTransition delay = new PauseTransition(Duration.seconds(1.5));
+        delay.setOnFinished(event -> stage.close());
+        delay.play();
+    }
+
 }

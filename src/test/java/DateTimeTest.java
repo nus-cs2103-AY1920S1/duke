a81a5c2bd26742e182103265abb19114c8bcceb0
shawnlsj97 <slsj1997@gmail.com>
2019-09-16 16:33:13 +0800

    Fix import statements in unit tests

--- src/test/java/DateTimeTest.java
+++ src/test/java/DateTimeTest.java
@@ -1,12 +1,14 @@
-import duke.date.DateTime;
+import trackr.date.DateTime;
 import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DateTimeTest {
 
     @Test
-    public void testGetTime() {
+    public void testGetTime() throws ParseException {
         DateTime dt = new DateTime("12/12/2019 1800");
         assertEquals("6:00pm", dt.getTime());
     }
--- src/test/java/TodoTest.java
+++ src/test/java/TodoTest.java
@@ -1,5 +1,6 @@
-import duke.task.Todo;
+import trackr.task.Todo;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TodoTest {

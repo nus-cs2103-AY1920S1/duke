afd933e668dffb032b97a501895b0d4474f99112
CowSaysBaa <mememe479@gmail.com>
2019-09-16 18:44:21 +0800

    Completed javadocs

--- src/main/java/main/Ui.java
+++ src/main/java/main/Ui.java
@@ -4,8 +4,17 @@ import task.Task;
 
 import java.util.ArrayList;
 
+/**
+ * Represents the printing of outputs
+ */
 public class Ui {
 
+    /**
+     * Prints out the contents of a list.
+     *
+     * @param arr The array to print.
+     * @return String of the output.
+     */
     public String printList(ArrayList<Task> arr) {
         String output = "Here are the tasks in your list:\n";
         for (int i = 0; i < arr.size(); i++) {
@@ -14,6 +23,12 @@ public class Ui {
         return output;
     }
 
+    /**
+     * Prints out the command to add a task to an array.
+     *
+     * @param arr The array to be added to.
+     * @return String of the output.
+     */
     public String printAdd(ArrayList<Task> arr) {
         String output = "Got it. I've added this task: \n";
         output += arr.get(arr.size() - 1) + "\n";
@@ -21,6 +36,13 @@ public class Ui {
         return output;
     }
 
+    /**
+     * Prints out the done command.
+     *
+     * @param arr   The array to print.
+     * @param index The index of the task in the list to be marked as done.
+     * @return String of the output.
+     */
     public String printDone(ArrayList<Task> arr, int index) {
         arr.get(index).markAsDone();
 
@@ -28,6 +50,13 @@ public class Ui {
         return output;
     }
 
+    /**
+     * Prints out the command to remove an element from the array.
+     *
+     * @param arr  The array to print.
+     * @param task The task to be removed.
+     * @return String of the output.
+     */
     public String printRemove(ArrayList<Task> arr, Task task) {
 
         String output = "Noted. I've removed this task: \n" + task + "\n";
@@ -35,18 +64,39 @@ public class Ui {
         return output;
     }
 
+    /**
+     * Returns the bye message.
+     */
     public String printBye() {
         return "Bye. Hope to see you again soon!";
     }
 
+    /**
+     * Prints out the error message.
+     *
+     * @param errorMessage The error message to be printed out.
+     * @return String of the output.
+     */
     public String printError(String errorMessage) {
         return errorMessage;
     }
 
+    /**
+     * Prints out the message.
+     *
+     * @param message The error message to be printed out.
+     * @return String of the output.
+     */
     public String printMessage(String message) {
         return message;
     }
 
+    /**
+     * Prints out the list of tasks with the search keyword.
+     *
+     * @param arr The array where the search takes place.
+     * @return String of the output.
+     */
     public String printFound(ArrayList<Task> arr) {
         String output = "Here are the matching tasks in your list:\n";
         for (int i = 0; i < arr.size(); i++) {

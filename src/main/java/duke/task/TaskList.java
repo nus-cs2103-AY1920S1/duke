24ace1efeeef85c5d9c9c99c584ca00a6d6ceb70
madanalogy <Ahmed_Bahajjaj@u.nus.edu>
2019-09-17 14:36:42 +0800

    Credits

--- src/main/java/duke/task/TaskList.java
+++ src/main/java/duke/task/TaskList.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 /**
  * Object interface for duke to manage task list.
+ *
+ * Solution of extending ArrayList adopted thanks to suggestion of https://github.com/nexolute
  */
 public class TaskList extends ArrayList<Task> {
 }

4492c4cc4da8b15901b19319c49448488133adcd
ShuTingY <shuting_y@u.nus.edu>
2019-09-18 00:47:37 +0800

    -Fix cross-platform -adds delay when command "bye" is enter.

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -1,6 +1,7 @@
 package duke;
 
 import duke.ui.UiText;
+import javafx.animation.PauseTransition;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -9,6 +10,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 /**
  * Controller for MainWindow. Provides the layout for the other controls.
@@ -58,7 +60,11 @@ public class MainWindow extends AnchorPane {
         );
         userInput.clear();
         if (duke.isExit()) {
-            Platform.exit();
+            PauseTransition delay = new PauseTransition((Duration.seconds(1)));
+            delay.setOnFinished(event -> Platform.exit());
+            delay.play();
+
+            //Platform.exit();
         }
     }
 }

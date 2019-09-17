6d9eb22b5a88f54ed88fa8e61c7fb42041d0ab41
Ryan Chew <ryanchewjingrong1997@gmail.com>
2019-09-18 00:32:15 +0800

    Fix JavaFX startup classpath issues.

--- src/main/java/org/duke/ui/javafx/DukeFx.java
+++ src/main/java/org/duke/ui/javafx/DukeFx.java
@@ -35,10 +35,6 @@ public class DukeFx extends Application {
     private DukeRootPane root;
     private Stage stage;
 
-    public static void main(String[] args) {
-        Application.launch(args);
-    }
-
 
     @Override
     public void start(Stage stage) {
---
+++ src/main/java/org/duke/ui/javafx/DukeMain.java
@@ -0,0 +1,9 @@
+package org.duke.ui.javafx;
+
+import javafx.application.Application;
+
+public class DukeMain {
+    public static void main(String[] args) {
+        Application.launch(DukeFx.class, args);
+    }
+}

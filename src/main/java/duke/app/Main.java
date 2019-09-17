894c7f948112729bd985015b92bfccd46f3406da
ambervoong <voongyuxuan@gmail.com>
2019-09-17 21:15:35 +0800

    Fix bug with welcome message display. Updated UI and Ui.png.

--- src/main/java/duke/app/Main.java
+++ src/main/java/duke/app/Main.java
@@ -4,6 +4,8 @@ import duke.ui.MainWindow;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -26,6 +28,16 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             stage.setTitle("Duke");
             AnchorPane ap = fxmlLoader.load();
+
+            Image img = new Image(this.getClass().getResourceAsStream("/images/tentacle.png"));
+            ImageView imgView = new ImageView(img);
+            imgView.setFitHeight(300);
+            imgView.setFitWidth(100);
+            imgView.setX(500);
+            imgView.setY(0);
+
+            ap.getChildren().add(imgView);
+
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setDuke(duke);

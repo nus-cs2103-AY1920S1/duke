63ca57716b1f5bd1f50e0f62ba971658933965bb
marqueurs404 <marcus.ong404@gmail.com>
2019-09-17 16:59:13 +0800

    Fix jar issues

--- src/main/java/core/Storage.java
+++ src/main/java/core/Storage.java
@@ -11,6 +11,7 @@ import task.Todo;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -36,6 +37,8 @@ public class Storage {
      */
     public void save(TaskList taskList) throws DukeIoException {
         try {
+            File file = new File(path.toString());
+            file.getParentFile();
             if (!Files.exists(path)) {
                 Files.createFile(path);
             }

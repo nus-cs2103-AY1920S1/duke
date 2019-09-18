0489ddcd108cd42de404450214881d044a7c5d24
zephyr <shejiayu1998@qq.com>
2019-09-18 17:24:31 +0800

    Set min height and width for label

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -14,6 +14,7 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 
 import java.io.IOException;
@@ -47,6 +48,8 @@ public class DialogBox extends HBox {
         }
 
         dialog.setText(text);
+        dialog.setMinHeight(Region.USE_PREF_SIZE);
+        dialog.setMinWidth(Region.USE_PREF_SIZE);
         displayPicture.setImage(img);
     }
 

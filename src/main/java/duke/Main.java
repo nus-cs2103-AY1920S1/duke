25a2438f5b3b06d2c9c83ee39fdd3f59dd39beba
jonchan51 <32188705+jonchan51@users.noreply.github.com>
2019-09-19 00:16:50 +0800

    Enable resizing of GUI

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Your Duke");
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
             fxmlLoader.<MainWindow>getController().sendStartUpMessage();

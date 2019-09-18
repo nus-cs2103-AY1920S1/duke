d3b4721af616d52d27684e661971815f5a035fec
sandydays <sandhyar725@gmail.com>
2019-09-18 22:29:50 +0800

    Add screenshot of duke app. Add app photo and description to title bar.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * A GUI for Duke using FXML.
@@ -22,6 +24,8 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.getIcons().add(new Image(this.getClass().getResourceAsStream("/images/DaDuke.png")));
+            stage.setTitle("Duke: One app to manage all your tasks");
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             fxmlLoader.<MainWindow>getController().openingMessage("Hello! I'm Duke\nWhat can I do for you?");
             stage.show();

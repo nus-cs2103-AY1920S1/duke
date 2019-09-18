0aa70c9dc0c2a19a8cbc9fdfee59601c12cfb28c
1nefootstep <sheuxiang@gmail.com>
2019-09-18 08:25:56 +0800

    Add priority sort test and change wrong var names

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -23,7 +23,7 @@ class DukeTest {
     }
 
     @Test
-    void dukeTodoDeadlineTest() {
+    void dukeDeadlineParseTest() {
         Parser p = new Parser();
         TaskList tasks = new TaskList();
         Command c = p.parse("deadline project work /by 19/02/2019 1122");
@@ -37,15 +37,15 @@ class DukeTest {
     }
 
     @Test
-    void dukeTodoEventTest() {
+    void dukeEventParseTest() {
         Parser p = new Parser();
         TaskList tasks = new TaskList();
         Command c = p.parse("event world temperature raises by 3 degrees /at 01/01/2056 0000");
         c.execute(tasks);
         Date d = new GregorianCalendar(2056,0,1,0,0).getTime();
-        Event expectedDeadline = new Event("world temperature raises by 3 degrees", d);
+        Event expectedEvent = new Event("world temperature raises by 3 degrees", d);
         String actualTaskInfo = tasks.getFullTaskInfo(0);
-        String expectedTaskInfo = expectedDeadline.toString();
+        String expectedTaskInfo = expectedEvent.toString();
 
         assertEquals(actualTaskInfo, expectedTaskInfo);
     }
@@ -57,11 +57,38 @@ class DukeTest {
         p.parse("event world temperature raises by 3 degrees /at 01/01/2056 0000").execute(tasks);
         p.parse("priority 1 HIGH").execute(tasks);
         Date d = new GregorianCalendar(2056,0,1,0,0).getTime();
-        Event expectedDeadline = new Event("world temperature raises by 3 degrees", d);
-        expectedDeadline.setPriority(PriorityLevel.HIGH);
+        Event expectedEvent = new Event("world temperature raises by 3 degrees", d);
+        expectedEvent.setPriority(PriorityLevel.HIGH);
         String actualTaskInfo = tasks.getFullTaskInfo(0);
-        String expectedTaskInfo = expectedDeadline.toString();
+        String expectedTaskInfo = expectedEvent.toString();
 
         assertEquals(actualTaskInfo, expectedTaskInfo);
+    }
+
+    @Test
+    void dukePrioritySortTest() {
+        TaskList tasks = new TaskList();
+        Event event = new Event(
+                "world temperature raises by 3 degrees",
+                new GregorianCalendar(2056,0,1,0,0).getTime()
+        );
+        Deadline deadline = new Deadline(
+                "project work",
+                new GregorianCalendar(2019,1,19,11,22).getTime()
+        );
+        TodoTask todo = new TodoTask("Winner Winner Chicken Dinner");
+        tasks.addTask(event);
+        tasks.addTask(deadline);
+        tasks.addTask(todo);
+        tasks.setPriorityOfTask(2, PriorityLevel.MEDIUM);
+
+        assertEquals(tasks.getFullTaskInfo(0), todo.toString());
+
+        tasks.setPriorityOfTask(1, PriorityLevel.HIGH);
+        assertEquals(tasks.getFullTaskInfo(0), event.toString());
+
+        tasks.setPriorityOfTask(0, PriorityLevel.MEDIUM);
+        tasks.setPriorityOfTask(2, PriorityLevel.HIGH);
+        assertEquals(tasks.getFullTaskInfo(0), deadline.toString());
     }
 }

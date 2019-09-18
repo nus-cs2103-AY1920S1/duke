b6c083ced57bbd684185fcdf9d38c407cd7128c8
gabrielseow <e0316039@u.nus.edu>
2019-09-18 20:23:23 +0800

    complete A-JavaFX

---
+++ src/main/java/duke/Duke.java
@@ -0,0 +1,172 @@
+package duke;
+
+import duke.storage.Storage;
+import duke.tasklist.TaskList;
+import duke.ui.Ui;
+import duke.dukeexception.DukeException;
+import duke.command.Command;
+import duke.parser.Parser;
+import duke.control.DialogBox;
+
+import javafx.scene.control.Label;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.Region;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+/**
+ * duke.view.Main driver class for Duke application. Duke application helps the user to manage tasks and allows the user to
+ * add, remove, mark a task as completed, list all tasks and find all tasks containing a keyword.
+ */
+public class Duke extends Application {
+    private Storage storage;
+    private TaskList tasks;
+    private Ui ui;
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
+    private static final String ROOT_DIRECTORY= "C:/Users/gbrls/OneDrive/Desktop/duke-master/src/main/java/duke/";
+
+    /**
+     * Loads a save file and generates a new Ui, Storage and TaskList object stored as instance variables.
+     */
+    public Duke() {
+        String filePath = ROOT_DIRECTORY + "data/tasks.text";
+        ui = new Ui();
+        storage = new Storage(filePath);
+        try {
+            tasks = new TaskList(storage.load());
+        } catch (DukeException e) {
+            ui.showLoadingError();
+            tasks = new TaskList();
+        }
+    }
+
+    @Override
+    public void start(Stage stage) {
+        //Step 1. Setting up required components
+
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        //Step 2. Formatting the window to look as expected
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        // You will need to import `javafx.scene.layout.Region` for this.
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        //Part 3. Add functionality to handle user input.
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput();
+        });
+
+        userInput.setOnAction((event) -> {
+            handleUserInput();
+        });
+
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+    }
+
+    public String getResponse(String input) {
+        try {
+            Command command = Parser.parse(input);
+            command.execute(tasks, ui, storage);
+        } catch (DukeException e) {
+            ui.showError(e.getMessage());
+        }
+        return "Duke heard: " + ui.getOutput();
+    }
+
+    /**
+     * Iteration 2:
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    private void handleUserInput() {
+        Label userText = new Label(userInput.getText());
+        Label dukeText = new Label(getResponse(userInput.getText()));
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userText, new ImageView(user)),
+                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
+        );
+        userInput.clear();
+    }
+
+    /**
+     * Handles user input and
+     */
+    public void run() {
+        ui.showWelcome();
+        boolean isExit = false;
+        while (!isExit) {
+            try {
+                String fullCommand = ui.readCommand();
+                ui.showLine(); // show the divider line ("_______")
+                Command command = Parser.parse(fullCommand);
+                command.execute(tasks, ui, storage);
+                isExit = command.isExit();
+            } catch (DukeException e) {
+                ui.showError(e.getMessage());
+            } finally {
+                ui.showLine();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Duke().run();
+    }
+
+}
--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Ui {
 
     private Scanner sc;
+    private String output;
 
     public Ui() {
         sc = new Scanner(System.in);
@@ -15,21 +16,22 @@ public class Ui {
     }
 
     public void showAddTaskMessage(String taskDescription, int listSize) {
-        String output = "Got it. I've added this task:\n"
+        output = "Got it. I've added this task:\n"
                 + taskDescription
                 + "\nNow you have " + listSize + " tasks in the list.";
-        System.out.println(output);
+        //System.out.println(output);
     }
 
     public void showDeleteTaskMessage(String taskDescription, int listSize) {
-        String output = "Noted. I've removed this task: \n"
+        output = "Noted. I've removed this task: \n"
                 + taskDescription
                 + "\nNow you have " + listSize + " tasks in the list.";
-        System.out.println(output);
+        //System.out.println(output);
     }
 
     public void showDoneMessage(String taskDescription) {
-        System.out.println("Nice! I've marked this task as done: \n" + taskDescription);
+        //System.out.println("Nice! I've marked this task as done: \n" + taskDescription);
+        output = "Nice! I've marked this task as done: \n" + taskDescription;
     }
 
     public void showList(String[] taskDescriptionArray) {
@@ -37,12 +39,12 @@ public class Ui {
         if (taskLen == 0) {
             System.out.println("You do not have any tasks in your list");
         } else {
-            String output = "Here are the tasks in your list: ";
+            output = "Here are the tasks in your list: ";
             for (int i = 0; i < taskLen; i ++) {
                 String taskDescription = taskDescriptionArray[i];
                 output += "\n" + (i + 1) + "." + taskDescription;
             }
-            System.out.println(output);
+            //System.out.println(output);
         }
     }
 
@@ -51,12 +53,12 @@ public class Ui {
         if (taskLen == 0) {
             System.out.println("There are no matching tasks in your list");
         } else {
-            String output = "Here are the matching tasks in your list: ";
+            output = "Here are the matching tasks in your list: ";
             for (int i = 0; i < taskLen; i ++) {
                 String taskDescription = taskDescriptionArray[i];
                 output += "\n" + (i + 1) + "." + taskDescription;
             }
-            System.out.println(output);
+            //System.out.println(output);
         }
     }
 
@@ -68,27 +70,35 @@ public class Ui {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String welcomeMessage = "Hello! I'm Duke\n" + "What can I do for you?";
 
-        System.out.println("Hello from\n" + logo);
-        showLine();
-        System.out.println(welcomeMessage);
-        showLine();
+        output = "Hello from\n" + logo;
+        output += getLine();
+        output += welcomeMessage;
+        output += getLine();
     }
 
     public void showLine() {
         System.out.println("____________________________________________________________");
     }
 
+    private String getLine() {
+        return "____________________________________________________________";
+    }
+
     public void showLoadingError() {
         //Consider making DukeLoadingError
-        System.out.println("No save file found... creating new save file");
+        output = "No save file found... creating new save file";
     }
 
     public void showError(String errorMessage) {
-        System.out.println(errorMessage);
+        output = errorMessage;
     }
 
     public void showExit() {
-        System.out.println("Bye. Hope to see you again soon!");
+        output = "Bye. Hope to see you again soon!";
+    }
+
+    public String getOutput() {
+        return output;
     }
 
 }

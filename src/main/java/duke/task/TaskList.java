84f8ef37c62ebaaf9a145abcf7232ece695ea8a9
madanalogy <Ahmed_Bahajjaj@u.nus.edu>
2019-09-19 11:54:32 +0800

    Removed JavaFX Web Dependency

--- src/main/java/duke/task/TaskList.java
+++ src/main/java/duke/task/TaskList.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 
 /**
  * Object interface for duke to manage task list.
- *
- * Solution of extending ArrayList adopted thanks to suggestion of https://github.com/nexolute
+ * <p>
+ * Solution of extending ArrayList adopted thanks to suggestion of https://github.com/nexolute.
+ * </p>
  */
 public class TaskList extends ArrayList<Task> {
 }

25e4bcacadd7dc676536e937edab844e21f33257
minpyaemoe <minpyaemoe@gmail.com>
2019-09-19 17:52:54 +0800

    Fixed forward slash and backward slash for windows and mac

--- src/main/java/duke/component/Storage.java
+++ src/main/java/duke/component/Storage.java
@@ -33,8 +33,8 @@ public class Storage {
         assert currentWorkingDirectoryPath != null
                 : "The path to the current working directory cannot be a null object.";
 
-        this.directoryPath = currentWorkingDirectoryPath + "\\data";
-        this.filePath = currentWorkingDirectoryPath + "\\data\\Duke.txt";
+        this.directoryPath = currentWorkingDirectoryPath + "/data";
+        this.filePath = currentWorkingDirectoryPath + "/data/Duke.txt";
         this.directory = new File(directoryPath);
         this.inputFile = new File(filePath);
     }

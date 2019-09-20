222801235c2961cc6b3048424e268bf407ff6753
choongyx <yongxin.choong98@gmail.com>
2019-09-20 14:26:31 +0800

    Update to make Jar Cross platform

--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -1,10 +1,10 @@
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class EventTest {
     @Test
-    public void dummyTest() {
+    public void dummyTest() throws WrongDateFormatDukeException {
         Event test = new Event("Project meeting", "12/12/2019 1200");
         assertEquals("[E][" + "0" + "] Project meeting (at: 12/12/2019 1200)", test.toString());
         assertEquals("E | " + "0" + " | Project meeting | 12/12/2019 1200", test.stringForAppend());

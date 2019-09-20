2b0e01d878496f82a3eb1890cc2210a1d4a0d4cd
ckb055 <chenkaibin7@gmail.com>
2019-09-20 16:00:24 +0800

    Corrected 'bye command'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -44,7 +44,7 @@ public class Duke extends Application {
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
-    private static final String TASKLIST_FILEPATH = "src/main/resources/taskList/TaskList.txt";
+    private static final String TASKLIST_FILEPATH = "data/TaskList.txt";
 
     /**
      * Images for the user and Duke
@@ -84,6 +84,7 @@ public class Duke extends Application {
         boolean isExit = false;
         while (!isExit) {
             try {
+                Runtime.getRuntime().exit(0);
                 String fullCommand = ui.readCommand();
                 System.out.println(ui.showLine()); // show the divider line ("_______")
                 Command c = Parser.parse(fullCommand);
@@ -208,9 +209,16 @@ public class Duke extends Application {
 
     // Duke's response, customisable
     String getResponse(String input) {
+        boolean isExit = false;
         try {
             Command c = Parser.parse(input);
-            return c.execute(tasks, ui, storage);
+            isExit = c.isExit();
+            if (isExit) {
+                Runtime.getRuntime().exit(0);
+                return c.execute(tasks, ui, storage);
+            } else {
+                return c.execute(tasks, ui, storage);
+            }
         } catch (DukeException e) {
             return e.getMessage();
         }

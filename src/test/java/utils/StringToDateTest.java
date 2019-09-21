5f3f49b9da9d91c70fa67163e648b51ed7ce6ab6
abhinav <ambhinav@yahoo.com>
2019-09-21 23:17:30 +0800

    updated String to date tests

--- src/test/java/utils/StringToDateTest.java
+++ src/test/java/utils/StringToDateTest.java
@@ -3,8 +3,6 @@ package utils;
 import exceptions.DukeException;
 import org.junit.jupiter.api.Test;
 
-import java.text.ParseException;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -12,13 +10,13 @@ class StringToDateTest {
 
     @Test
     void constructor_invalidDateFormat_expectedException() {
-        assertThrows(ParseException.class, () -> {
+        assertThrows(DukeException.class, () -> {
             new StringToDate("10-10");
         });
     }
 
     @Test
-    void toString_string_formattedDateOutput() throws ParseException, DukeException {
+    void toString_string_formattedDateOutput() throws DukeException {
         assertEquals("10-10-2019 18:00", new StringToDate("10-10-2019 18:00").toString());
     }
 

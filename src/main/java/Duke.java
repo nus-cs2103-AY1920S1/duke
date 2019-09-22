8f33e00948506b8ba17b3ec70399835264cd6ad5
Ho Wei Chin <hwc.howeichin@gmail.com>
2019-09-22 18:02:27 +0800

    Add files via upload

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,16 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
+import java.io.IOException;
 
 public class Duke extends Application {
     //private static Ui uiManager = new Ui();
     private static GUI gui = new GUI();
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws IOException {
         //Step 1. Setting up required components
         gui.init(stage);
 
     }
-    //public Duke() {}
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
 
 }

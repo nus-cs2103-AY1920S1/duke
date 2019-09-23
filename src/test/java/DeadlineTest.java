7565041366d8b25d43dca859e871eecb7a673df0
alexwxh <e0273907@u.nus.edu>
2019-09-24 02:39:58 +0800

    update test files

--- src/test/java/DeadlineTest.java
+++ src/test/java/DeadlineTest.java
@@ -1,13 +1,15 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
+import duke.task.*;
+import duke.execution.DukeException;
 
 public class DeadlineTest {
     @Test
     public void deadline_toStringTest(){
         String testInput_1 = "test";
         String testInput_2 = "2/9/2019 2000";
-        String expectedFileResult = "D | 0 | test | 2/9/2019 2000\n";
+        String expectedFileResult = "D | 0 | test | 2/9/2019 2000";
         String expectedPrintResult = "[D][✘]test (by: 2/9/2019 2000)";
         try {
             Deadline deadlineTest = new Deadline(testInput_1, testInput_2);
--- src/test/java/TodoTest.java
+++ src/test/java/TodoTest.java
@@ -1,11 +1,12 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import duke.task.*;
 
 public class TodoTest {
     @Test
     public void todo_toStringTest(){
         String testInput = "test";
-        String expectedFileResult = "T | 0 | test\n";
+        String expectedFileResult = "T | 0 | test";
         String expectedPrintResult = "[T][✘]test";
         Todo todoTest = new Todo(testInput);
         assertEquals(expectedFileResult, todoTest.toFileString());

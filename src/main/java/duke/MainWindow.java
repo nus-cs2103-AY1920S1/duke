c6e12d8bdc76e649ee3df754626ec47c4b8d408c
nattanyz <natalietan8@gmail.com>
2019-09-30 09:54:12 +0800

    manage to compile and run, opens GUI window

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -23,8 +23,8 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/kawaii_robot.svg"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/kawaii_robot_power.svg"));
 
     @FXML
     public void initialize() {

62aee7fc24e9cd1f22663003724ac783337ea0af
jakq <42934003+jakq@users.noreply.github.com>
2019-09-30 19:25:38 +0800

    updated JUnit testing

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -22,8 +22,8 @@ public class ParserTest {
         String[] result2 = new String[]{"deadline", "return book", "18/8/2019 2000"};
         assertArrayEquals(result2, Parser.processTask(test2));
 
-        String[] test3 = new String[]{"event", "reading", "circle", "/at", "18/8/8/2019", "1700-1900"};
-        String[] result3 = new String[]{"event", "reading circle", "18/8/8/2019 1700-1900"};
+        String[] test3 = new String[]{"event", "reading", "circle", "/at", "18/8/2019", "1700-1900"};
+        String[] result3 = new String[]{"event", "reading circle", "18/8/2019 1700-1900"};
         assertArrayEquals(result3, Parser.processTask(test3));
     }
 
@@ -33,15 +33,18 @@ public class ParserTest {
     @Test
     public void testProcessTask_fail() {
         String[] test1 = new String[]{"todo"};
-        String[] fail1 = new String[]{"The description of a todo cannot be empty.", "", ""};
+        String[] fail1 = new String[]{"The description of a todo cannot be empty.\n(Enter \"help\" for commands in "
+                + "duke.)", "", ""};
         assertArrayEquals(fail1, Parser.processTask(test1));
 
         String[] test2 = new String[]{"deadline", "return book", ""};
-        String[] fail2 = new String[]{"The date of a deadline must be in the format \"dd/MM/yyyy HHmm\"", "", ""};
+        String[] fail2 = new String[]{"\u2639 OOPS!!! I'm sorry, but the date of a deadline must be in the format"
+                + " \"/by dd/MM/yyyy HHmm\"\n(Enter \"help\" for commands in duke.)", "", ""};
         assertArrayEquals(fail2, Parser.processTask(test2));
 
         String[] test3 = new String[]{"event", "reading circle", ""};
-        String[] fail3 = new String[]{"The date of a event must be in the format \"dd/MM/yyyy HHmm-HHmm\"", "", ""};
+        String[] fail3 = new String[]{"\u2639 OOPS!!! I'm sorry, but the date of a event must be in the format"
+                + " \"/at dd/MM/yyyy HHmm-HHmm\"\n(Enter \"help\" for commands in duke.)", "", ""};
         assertArrayEquals(fail3, Parser.processTask(test3));
     }
 }
--- src/test/java/TaskListTest.java
+++ src/test/java/TaskListTest.java
@@ -11,36 +11,35 @@ public class TaskListTest {
 
     /**
      * Tests addTask of TaskList object with successful todo, deadline, event, bye and unknown inputs.
+     * Ensure that TaskList's Storage has no preexisting tasks before running this test.
      */
     @Test
     public void testAddTask() {
+        TaskList tasklist = new TaskList(new Storage());
         String todo = "todo borrow book";
         String resTodo = "Got it. I've added this task:\n  [T][\u2718] borrow book\nNow you have 1 tasks in the list.";
-        assertEquals(resTodo, new TaskList(new Storage("C:\\Users\\AngKa\\duke\\src\\main\\java\\test.txt"))
-                        .addTask(todo));
+        assertEquals(resTodo, tasklist.addTask(todo));
+        tasklist.addTask("delete 1");
 
-        Task.decCurrTotal();
         String deadline = "deadline return book /by 18/8/2019 2000";
         String resDeadline = "Got it. I've added this task:\n  [D][\u2718] return book (by: 18/8/2019 2000)\nNow you "
                                + "have 1 tasks in the list.";
-        assertEquals(resDeadline, new TaskList(new Storage("C:\\Users\\AngKa\\duke\\src\\main\\java\\test.txt"))
-                        .addTask(deadline));
+        assertEquals(resDeadline, tasklist.addTask(deadline));
+        tasklist.addTask("delete 1");
 
-        Task.decCurrTotal();
-        String event = "event reading circle /at 18/8/8/2019 1700-1900";
-        String resEvent = "Got it. I've added this task:\n  [E][\u2718] reading circle (at: 18/8/8/2019 1700-1900)\n"
+        String event = "event reading circle /at 18/8/2019 1700-1900";
+        String resEvent = "Got it. I've added this task:\n  [E][\u2718] reading circle (at: 18/8/2019 1700-1900)\n"
                             + "Now you have 1 tasks in the list.";
-        assertEquals(resEvent, new TaskList(new Storage("C:\\Users\\AngKa\\duke\\src\\main\\java\\test.txt"))
-                .addTask(event));
+        assertEquals(resEvent, tasklist.addTask(event));
+        tasklist.addTask("delete 1");
 
         String bye = "bye";
-        String resBye = "bye";
-        assertEquals(resBye, new TaskList(new Storage("C:\\Users\\AngKa\\duke\\src\\main\\java\\test.txt"))
-                        .addTask(bye));
+        String resBye = "Bye. Hope to see you again soon!";
+        assertEquals(resBye, tasklist.addTask(bye));
 
         String unknown = "unknown";
-        String resUnknown = "\u2639 OOPS!!! I'm sorry, but I don't know what that means :-(";
-        assertEquals(resUnknown, new TaskList(new Storage("C:\\Users\\AngKa\\duke\\src\\main\\java\\test.txt"))
-                        .addTask(unknown));
+        String resUnknown = "\u2639 OOPS!!! I'm sorry, but I don't know what that means :-(\nEnter \"help\" for "
+                + "commands in duke.";
+        assertEquals(resUnknown, tasklist.addTask(unknown));
     }
 }
